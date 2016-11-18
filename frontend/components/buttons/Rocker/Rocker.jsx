d6c653b8ea97ff9bd1ceede72751812560aeb236
Kyle Knight <kyle@kolide.co>
2016-11-18 09:41:52 -0600

    Adding in new Rocker Switch (#498)

---
+++ frontend/components/buttons/Rocker/Rocker.jsx
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react';
+import { noop } from 'lodash';
+import classnames from 'classnames';
+
+class Rocker extends Component {
+
+  static propTypes = {
+    className: PropTypes.string,
+    handleChange: PropTypes.func,
+    name: PropTypes.string,
+    options: PropTypes.shape({
+      aText: PropTypes.string,
+      aIcon: PropTypes.string,
+      bText: PropTypes.string,
+      bIcon: PropTypes.string,
+    }),
+    value: PropTypes.string,
+  };
+
+  static defaultProps = {
+    handleChange: noop,
+  };
+
+  render () {
+    const { className, handleChange, name, options, value } = this.props;
+    const { aText, aIcon, bText, bIcon } = options;
+    const baseClass = 'kolide-rocker';
+
+    const rockerClasses = classnames(baseClass, className);
+
+    return (
+      <div className={rockerClasses}>
+        <label className={`${baseClass}__label`} htmlFor={name}>
+          <input className={`${baseClass}__checkbox`} type="checkbox" value={value} name={name} id={name} onChange={handleChange} />
+          <span className={`${baseClass}__switch ${baseClass}__switch--opt-b`}>
+            <span className={`${baseClass}__text`}>
+              <i className={`kolidecon kolidecon-${bIcon}`} /> {bText}
+            </span>
+          </span>
+          <span className={`${baseClass}__switch ${baseClass}__switch--opt-a`}>
+            <span className={`${baseClass}__text`}>
+              <i className={`kolidecon kolidecon-${aIcon}`} /> {aText}
+            </span>
+          </span>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default Rocker;
--- frontend/pages/HomePage/HomePage.jsx
+++ frontend/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
 import Avatar from '../../components/Avatar';
+import Rocker from '../../components/buttons/Rocker';
 import paths from '../../router/paths';
 import userInterface from '../../interfaces/user';
 
@@ -15,12 +16,19 @@ export class HomePage extends Component {
     const { user } = this.props;
     const { LOGOUT } = paths;
     const baseClass = 'home-page';
+    const rockerOpts = {
+      aText: 'List',
+      aIcon: 'list-select',
+      bText: 'Grid',
+      bIcon: 'grid-select',
+    };
 
     return (
       <div className={baseClass}>
         {user && <Avatar size="small" className={`${baseClass}__avatar`} user={user} />}
         <span>You are successfully logged in! </span>
         {user && <Link to={LOGOUT}>Logout</Link>}
+        <Rocker name="view-type" value="grid" options={rockerOpts} />
       </div>
     );
   }
