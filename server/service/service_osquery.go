581000bb643623afef8fd060517dfac948565021
Mike Arpaia <mike@arpaia.co>
2017-01-13 13:21:54 -0700

    Revert "Add some basic decorators to the osquery config (#875)" (#953)
    This reverts commit 1d029073e5730fc43a20573905ad27a685509b07.

--- server/service/service_osquery.go
+++ server/service/service_osquery.go
@@ -73,15 +73,7 @@ func (svc service) GetClientConfig(ctx context.Context) (*kolide.OsqueryConfig, 
 
 	config := &kolide.OsqueryConfig{
 		Options: options,
-		Decorators: kolide.Decorators{
-			Interval: map[string][]string{
-				"3600": []string{
-					"SELECT uuid AS host_uuid FROM system_info;",
-					"SELECT hostname FROM system_info;",
-				},
-			},
-		},
-		Packs: kolide.Packs{},
+		Packs:   kolide.Packs{},
 	}
 
 	packs, err := svc.ListPacksForHost(ctx, host.ID)
