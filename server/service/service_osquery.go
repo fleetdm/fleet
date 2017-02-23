0b4b921f17933cf1a991844ea190812a84907900
Zachary Wasserman <zachwass2000@gmail.com>
2017-02-23 12:36:08 -0800

    Re-add default decorators to osquery config (#1269)
    These decorators were removed in #953 due to an osquery bug. That bug is now
    fixed, and we are adding the decorators back. We also now use `load` decorators
    rather than `interval` decorators because they seem to function more reliably.

--- server/service/service_osquery.go
+++ server/service/service_osquery.go
@@ -80,7 +80,13 @@ func (svc service) GetClientConfig(ctx context.Context) (*kolide.OsqueryConfig, 
 
 	config := &kolide.OsqueryConfig{
 		Options: options,
-		Packs:   kolide.Packs{},
+		Decorators: kolide.Decorators{
+			Load: []string{
+				"SELECT uuid AS host_uuid FROM system_info;",
+				"SELECT hostname AS hostname FROM system_info;",
+			},
+		},
+		Packs: kolide.Packs{},
 	}
 
 	packs, err := svc.ListPacksForHost(ctx, host.ID)
