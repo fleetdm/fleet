fleetdm/fleet

commit c6b285c56e046717bd0b5ac3c352d7fd463dbb0a

Remove soft-deletion pattern (#2327)
* Perform migration to delete any entries with `deleted` set, and
subsequently drop columns `deleted` and `deleted_at`.
* Remove `deleted` and `deleted_at` references.

Closes #2146

server/datastore/mysql/packs.go
@@ -35,8 +35,7 @@ func applyPackSpec(tx *sqlx.Tx, spec *kolide.PackSpec) error {
 			name = VALUES(name),
 			description = VALUES(description),
 			platform = VALUES(platform),
-			disabled = VALUES(disabled),
-			deleted = false
+			disabled = VALUES(disabled)
 	`
 	if _, err := tx.Exec(query, spec.Name, spec.Description, spec.Platform, spec.Disabled); err != nil {
 		return errors.Wrap(err, "insert/update pack")
@@ -203,7 +202,7 @@ func (d *Datastore) PackByName(name string, opts ...kolide.OptionalArg) (*kolide
 	sqlStatement := `
 		SELECT *
 			FROM packs
-			WHERE name = ? AND NOT deleted
+			WHERE name = ?
 	`
 	var pack kolide.Pack
 	err := db.Get(&pack, sqlStatement, name)
@@ -220,35 +219,16 @@ func (d *Datastore) PackByName(name string, opts ...kolide.OptionalArg) (*kolide
 // NewPack creates a new Pack
 func (d *Datastore) NewPack(pack *kolide.Pack, opts ...kolide.OptionalArg) (*kolide.Pack, error) {
 	db := d.getTransaction(opts)
-	var (
-		deletedPack kolide.Pack
-		query       string
-	)
-	err := db.Get(&deletedPack,
-		"SELECT * FROM packs WHERE name = ? AND deleted", pack.Name)
-	switch err {
-	case nil:
-		query = `
-		REPLACE INTO packs
-			( name, description, platform, disabled, deleted)
-			VALUES ( ?, ?, ?, ?, ?)
-		`
-	case sql.ErrNoRows:
-		query = `
-		INSERT INTO packs
-			( name, description, platform, disabled, deleted)
-			VALUES ( ?, ?, ?, ?, ?)
-		`
-	default:
-		return nil, errors.Wrap(err, "check for existing pack")
-	}
 
-	deleted := false
-	result, err := db.Exec(query, pack.Name, pack.Description, pack.Platform, pack.Disabled, deleted)
-	if err != nil && isDuplicate(err) {
-		return nil, alreadyExists("Pack", deletedPack.ID)
-	} else if err != nil {
-		return nil, errors.Wrap(err, "creating new pack")
+	query := `
+	INSERT INTO packs
+		(name, description, platform, disabled)
+		VALUES ( ?, ?, ?, ?, ?)
+	`
+
+	result, err := db.Exec(query, pack.Name, pack.Description, pack.Platform, pack.Disabled)
+	if err != nil {
+		return nil, errors.Wrap(err, "inserting pack")
 	}
 
 	id, _ := result.LastInsertId()
@@ -261,7 +241,7 @@ func (d *Datastore) SavePack(pack *kolide.Pack) error {
 	query := `
 			UPDATE packs
 			SET name = ?, platform = ?, disabled = ?, description = ?
-			WHERE id = ? AND NOT deleted
+			WHERE id = ?
 	`
 
 	results, err := d.db.Exec(query, pack.Name, pack.Platform, pack.Disabled, pack.Description, pack.ID)
@@ -278,14 +258,14 @@ func (d *Datastore) SavePack(pack *kolide.Pack) error {
 	return nil
 }
 
-// DeletePack soft deletes a kolide.Pack so that it won't show up in results
+// DeletePack deletes a kolide.Pack so that it won't show up in results.
 func (d *Datastore) DeletePack(name string) error {
 	return d.deleteEntityByName("packs", name)
 }
 
 // Pack fetch kolide.Pack with matching ID
 func (d *Datastore) Pack(pid uint) (*kolide.Pack, error) {
-	query := `SELECT * FROM packs WHERE id = ? AND NOT deleted`
+	query := `SELECT * FROM packs WHERE id = ?`
 	pack := &kolide.Pack{}
 	err := d.db.Get(pack, query, pid)
 	if err == sql.ErrNoRows {
@@ -299,7 +279,7 @@ func (d *Datastore) Pack(pid uint) (*kolide.Pack, error) {
 
 // ListPacks returns all kolide.Pack records limited and sorted by kolide.ListOptions
 func (d *Datastore) ListPacks(opt kolide.ListOptions) ([]*kolide.Pack, error) {
-	query := `SELECT * FROM packs WHERE NOT deleted`
+	query := `SELECT * FROM packs`
 	packs := []*kolide.Pack{}
 	err := d.db.Select(&packs, appendListOptionsToSQL(query, opt))
 	if err != nil && err != sql.ErrNoRows {
@@ -390,7 +370,6 @@ func (d *Datastore) ListLabelsForPack(pid uint) ([]*kolide.Label, error) {
 		pt.type = ?
 			AND
 		pt.pack_id = ?
-	AND NOT l.deleted
 	`
 
 	labels := []*kolide.Label{}

server/datastore/datastore_test.go
@@ -62,7 +62,6 @@ var testFunctions = [...]func(*testing.T, kolide.Datastore){
 	testMarkHostSeen,
 	testCleanupIncomingHosts,
 	testDuplicateNewQuery,
-	testIdempotentDeleteHost,
 	testChangeEmail,
 	testChangeLabelDetails,
 	testMigrationStatus,

server/kolide/packs.go
@@ -122,7 +122,6 @@ type PackService interface {
 // Pack is the structure which represents an osquery query pack.
 type Pack struct {
 	UpdateCreateTimestamps
-	DeleteFields
 	ID          uint   `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`

frontend/components/queries/QueryResultsTable/QueryResultsTable.tests.jsx
@@ -29,8 +29,6 @@ const queryResult = {
 
 const campaignWithNoQueryResults = {
   created_at: '0001-01-01T00:00:00Z',
-  deleted: false,
-  deleted_at: null,
   hosts_count: {
     failed: 0,
     successful: 0,

frontend/test/stubs.js
@@ -59,8 +59,6 @@ export const flatConfigStub = {
 export const hostStub = {
   created_at: '2017-01-10T19:18:55Z',
   updated_at: '2017-01-10T20:13:52Z',
-  deleted_at: null,
-  deleted: false,
   id: 1,
   detail_updated_at: '2017-01-10T20:01:48Z',
   seen_time: '2017-01-10T20:13:54Z',
@@ -95,8 +93,6 @@ export const hostStub = {
 export const labelStub = {
   created_at: '2017-01-16T23:11:01Z',
   updated_at: '2017-01-16T23:11:01Z',
-  deleted_at: null,
-  deleted: false,
   id: 1,
   name: 'All Hosts',
   description: '',
@@ -116,8 +112,6 @@ export const labelStub = {
 export const packStub = {
   created_at: '0001-01-01T00:00:00Z',
   updated_at: '0001-01-01T00:00:00Z',
-  deleted_at: null,
-  deleted: false,
   id: 3,
   name: 'Pack Name',
   description: 'Pack Description',
@@ -130,8 +124,6 @@ export const packStub = {
 
 export const queryStub = {
   created_at: '2016-10-17T07:06:00Z',
-  deleted: false,
-  deleted_at: null,
   description: '',
   differential: false,
   id: 1,

server/datastore/datastore_hosts_test.go
@@ -110,28 +110,6 @@ func testDeleteHost(t *testing.T, ds kolide.Datastore) {
 	assert.NotNil(t, err)
 }
 
-func testIdempotentDeleteHost(t *testing.T, ds kolide.Datastore) {
-	host, err := ds.NewHost(&kolide.Host{
-		DetailUpdateTime: time.Now(),
-		LabelUpdateTime:  time.Now(),
-		SeenTime:         time.Now(),
-		NodeKey:          "1",
-		UUID:             "1",
-		HostName:         "foo.local",
-	})
-	require.Nil(t, err)
-	require.NotNil(t, host)
-	id := host.ID
-	err = ds.DeleteHost(host.ID)
-	assert.Nil(t, err)
-
-	host, err = ds.Host(host.ID)
-	assert.NotNil(t, err)
-
-	err = ds.DeleteHost(id)
-	assert.Nil(t, err)
-}
-
 func testListHost(t *testing.T, ds kolide.Datastore) {
 	hosts := []*kolide.Host{}
 	for i := 0; i < 10; i++ {

server/datastore/inmem/queries.go
@@ -38,8 +38,8 @@ func (d *Datastore) SaveQuery(query *kolide.Query) error {
 	return nil
 }
 
-// DeleteQueries (soft) deletes the existing query objects with the provided
-// IDs. The number of deleted queries is returned along with any error.
+// DeleteQueries deletes the existing query objects with the provided IDs. The
+// number of deleted queries is returned along with any error.
 func (d *Datastore) DeleteQueries(ids []uint) (uint, error) {
 	d.mtx.Lock()
 	defer d.mtx.Unlock()

server/datastore/mysql/campaigns.go
@@ -30,7 +30,7 @@ func (d *Datastore) NewDistributedQueryCampaign(camp *kolide.DistributedQueryCam
 
 func (d *Datastore) DistributedQueryCampaign(id uint) (*kolide.DistributedQueryCampaign, error) {
 	sql := `
-		SELECT * FROM distributed_query_campaigns WHERE id = ? AND NOT deleted
+		SELECT * FROM distributed_query_campaigns WHERE id = ?
 	`
 	campaign := &kolide.DistributedQueryCampaign{}
 	if err := d.db.Get(campaign, sql, id); err != nil {
@@ -47,7 +47,6 @@ func (d *Datastore) SaveDistributedQueryCampaign(camp *kolide.DistributedQueryCa
 			status = ?,
 			user_id = ?
 		WHERE id = ?
-		AND NOT deleted
 	`
 	result, err := d.db.Exec(sqlStatement, camp.QueryID, camp.Status, camp.UserID, camp.ID)
 	if err != nil {

commit 5c4c20bd42df0472a95e3c4536e97cebf25ee0e2

Fix regression creating packs
This is another error introduced in
https://github.com/kolide/fleet/pull/2327 we did not catch previously
due to insufficient unit test coverage. Test is now added

server/datastore/mysql/packs.go
@@ -223,7 +223,7 @@ func (d *Datastore) NewPack(pack *kolide.Pack, opts ...kolide.OptionalArg) (*kol
 	query := `
 	INSERT INTO packs
 		(name, description, platform, disabled)
-		VALUES ( ?, ?, ?, ?, ?)
+		VALUES ( ?, ?, ?, ? )
 	`
 
 	result, err := db.Exec(query, pack.Name, pack.Description, pack.Platform, pack.Disabled)

server/datastore/datastore_packs_test.go
@@ -25,6 +25,20 @@ func testDeletePack(t *testing.T, ds kolide.Datastore) {
 	assert.NotNil(t, err)
 }
 
+func testNewPack(t *testing.T, ds kolide.Datastore) {
+	pack := &kolide.Pack{
+		Name: "foo",
+	}
+
+	pack, err := ds.NewPack(pack)
+	require.NoError(t, err)
+	assert.NotEqual(t, uint(0), pack.ID)
+
+	pack, err = ds.Pack(pack.ID)
+	require.NoError(t, err)
+	assert.Equal(t, "foo", pack.Name)
+}
+
 func testGetPackByName(t *testing.T, ds kolide.Datastore) {
 	pack := test.NewPack(t, ds, "foo")
 	assert.NotEqual(t, uint(0), pack.ID)

server/datastore/datastore_test.go
@@ -23,6 +23,7 @@ var testFunctions = [...]func(*testing.T, kolide.Datastore){
 	testSaveQuery,
 	testListQuery,
 	testDeletePack,
+	testNewPack,
 	testEnrollHost,
 	testAuthenticateHost,
 	testAuthenticateHostCaseSensitive,