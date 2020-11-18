19e82b3cbcffe513f990d1b5d6043e141826c7c3
Zachary Wasserman <zach@dactiv.llc>
2020-11-17 16:25:33 -0800

    Run tests on actual handler

--- server/service/debug_handler_test.go
+++ server/service/debug_handler_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/bmizerany/assert"
+	"github.com/fleetdm/fleet/server/config"
 	"github.com/fleetdm/fleet/server/kolide"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/mock"
@@ -33,34 +34,34 @@ func (m *mockService) User(ctx context.Context, userId uint) (*kolide.User, erro
 	return nil, args.Error(1)
 }
 
-func TestDebugHandlerAuthenticationTokenMissing(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) {}
+var testConfig = config.KolideConfig{
+	Auth: config.AuthConfig{
+		JwtKey: "insecure",
+	},
+}
 
-	mw := debugAuthenticationMiddleware{service: &mockService{}, jwtKey: "insecure"}
+func TestDebugHandlerAuthenticationTokenMissing(t *testing.T) {
+	handler := MakeDebugHandler(&mockService{}, testConfig, nil)
+
 	req := httptest.NewRequest(http.MethodGet, "https://fleetdm.com/debug/pprof/profile", nil)
 	res := httptest.NewRecorder()
 
-	mw.Middleware(http.HandlerFunc(handler)).ServeHTTP(res, req)
-
+	handler.ServeHTTP(res, req)
 	assert.Equal(t, http.StatusUnauthorized, res.Code)
-
 }
 
 func TestDebugHandlerAuthenticationTokenInvalid(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) {}
+	handler := MakeDebugHandler(&mockService{}, testConfig, nil)
 
-	mw := debugAuthenticationMiddleware{service: &mockService{}, jwtKey: "insecure"}
 	req := httptest.NewRequest(http.MethodGet, "https://fleetdm.com/debug/pprof/profile", nil)
 	req.Header.Add("Authorization", "BEARER foobar")
 	res := httptest.NewRecorder()
 
-	mw.Middleware(http.HandlerFunc(handler)).ServeHTTP(res, req)
-
+	handler.ServeHTTP(res, req)
 	assert.Equal(t, http.StatusUnauthorized, res.Code)
 }
 
 func TestDebugHandlerAuthenticationSessionInvalid(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) {}
 	svc := &mockService{}
 	svc.On(
 		"GetSessionByKey",
@@ -68,18 +69,17 @@ func TestDebugHandlerAuthenticationSessionInvalid(t *testing.T) {
 		"session",
 	).Return(nil, errors.New("invalid session"))
 
-	mw := debugAuthenticationMiddleware{service: svc, jwtKey: "insecure"}
+	handler := MakeDebugHandler(svc, testConfig, nil)
+
 	req := httptest.NewRequest(http.MethodGet, "https://fleetdm.com/debug/pprof/profile", nil)
 	req.Header.Add("Authorization", "BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uX2tleSI6InNlc3Npb24iLCJpYXQiOjE1MTYyMzkwMjJ9.YZIL9fKxfVg7fCms4CTKCPT2w8x8N3e2pciV_h0OvTk")
 	res := httptest.NewRecorder()
 
-	mw.Middleware(http.HandlerFunc(handler)).ServeHTTP(res, req)
-
+	handler.ServeHTTP(res, req)
 	assert.Equal(t, http.StatusUnauthorized, res.Code)
 }
 
 func TestDebugHandlerAuthenticationDisabled(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) {}
 	svc := &mockService{}
 	svc.On(
 		"GetSessionByKey",
@@ -91,19 +91,17 @@ func TestDebugHandlerAuthenticationDisabled(t *testing.T) {
 		mock.Anything,
 		uint(42),
 	).Return(&kolide.User{Enabled: false}, nil)
+	handler := MakeDebugHandler(svc, testConfig, nil)
 
-	mw := debugAuthenticationMiddleware{service: svc, jwtKey: "insecure"}
 	req := httptest.NewRequest(http.MethodGet, "https://fleetdm.com/debug/pprof/profile", nil)
 	req.Header.Add("Authorization", "BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uX2tleSI6InNlc3Npb24iLCJpYXQiOjE1MTYyMzkwMjJ9.YZIL9fKxfVg7fCms4CTKCPT2w8x8N3e2pciV_h0OvTk")
 	res := httptest.NewRecorder()
 
-	mw.Middleware(http.HandlerFunc(handler)).ServeHTTP(res, req)
-
+	handler.ServeHTTP(res, req)
 	assert.Equal(t, http.StatusForbidden, res.Code)
 }
 
 func TestDebugHandlerAuthenticationSuccess(t *testing.T) {
-	handler := func(w http.ResponseWriter, r *http.Request) {}
 	svc := &mockService{}
 	svc.On(
 		"GetSessionByKey",
@@ -116,12 +114,12 @@ func TestDebugHandlerAuthenticationSuccess(t *testing.T) {
 		uint(42),
 	).Return(&kolide.User{Enabled: true}, nil)
 
-	mw := debugAuthenticationMiddleware{service: svc, jwtKey: "insecure"}
-	req := httptest.NewRequest(http.MethodGet, "https://fleetdm.com/debug/pprof/profile", nil)
+	handler := MakeDebugHandler(svc, testConfig, nil)
+
+	req := httptest.NewRequest(http.MethodGet, "https://fleetdm.com/debug/pprof/cmdline", nil)
 	req.Header.Add("Authorization", "BEARER eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uX2tleSI6InNlc3Npb24iLCJpYXQiOjE1MTYyMzkwMjJ9.YZIL9fKxfVg7fCms4CTKCPT2w8x8N3e2pciV_h0OvTk")
 	res := httptest.NewRecorder()
 
-	mw.Middleware(http.HandlerFunc(handler)).ServeHTTP(res, req)
-
+	handler.ServeHTTP(res, req)
 	assert.Equal(t, http.StatusOK, res.Code)
 }
