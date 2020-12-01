3bd64a52b9475e4c07c5e125e8e3b1aacf65fd3f
Noah Talerman <noahtal@umich.edu>
2020-12-01 14:29:02 -0500

    Add toContain() in RegistrationForm tests.

--- frontend/components/forms/RegistrationForm/RegistrationForm.tests.jsx
+++ frontend/components/forms/RegistrationForm/RegistrationForm.tests.jsx
@@ -8,28 +8,28 @@ describe('RegistrationForm - component', () => {
     const form = mount(<RegistrationForm page={1} />);
 
     expect(form.find('AdminDetails').length).toEqual(1);
-    expect(form.text()).toInclude('Setup user');
+    expect(form.text()).toContain('Setup user');
   });
 
   it('renders OrgDetails on the second page', () => {
     const form = mount(<RegistrationForm page={2} />);
 
     expect(form.find('OrgDetails').length).toEqual(1);
-    expect(form.text()).toInclude('Organization details');
+    expect(form.text()).toContain('Organization details');
   });
 
   it('renders KolideDetails on the third page', () => {
     const form = mount(<RegistrationForm page={3} />);
 
     expect(form.find('KolideDetails').length).toEqual(1);
-    expect(form.text()).toInclude('Set Fleet URL');
+    expect(form.text()).toContain('Set Fleet URL');
   });
 
   it('renders ConfirmationPage on the fourth page', () => {
     const form = mount(<RegistrationForm page={4} />);
 
     expect(form.find('ConfirmationPage').length).toEqual(1);
-    expect(form.text()).toInclude('Success');
+    expect(form.text()).toContain('Success');
   });
 });
 
