9302a569f21fc4b809e040047d4ffb9539957798
Zachary Wasserman <zachwass2000@gmail.com>
2020-12-17 15:31:53 -0800

    Implement retrieval of server certificate at startup
    This will allow a --insecure flag by writing the retrieved cert to a
    file and passing it to osquery.

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/fleetdm/orbit
 go 1.15
 
 require (
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869
+	github.com/kr/pretty v0.2.1 // indirect
 	github.com/oklog/run v1.1.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.6.1
--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 
+	"github.com/fleetdm/orbit/src/certificate"
 	"github.com/fleetdm/orbit/src/osquery"
 	"github.com/oklog/run"
+)
+
+const (
+	serverURL = "localhost:8080"
+	certPath  = "/tmp/fleet.pem"
 )
 
 func main() {
@@ -19,12 +26,22 @@ func main() {
 		log.Fatalf("no osquery found: %v", err)
 	}
 
+	serverCert, err := certificate.FetchPEM(serverURL)
+	if err != nil {
+		log.Fatalf("retrieve server cert: %v", err)
+	}
+	err = ioutil.WriteFile(certPath, serverCert, os.ModePerm)
+	if err != nil {
+		log.Fatalf("write server cert: %v", err)
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
 	var g run.Group
 	g.Add(run.SignalHandler(ctx, os.Interrupt, os.Kill))
 
 	r, _ := osquery.NewRunner(
-		osquery.WithFlags(osquery.FleetFlags("localhost:8080")),
+		osquery.WithFlags(osquery.FleetFlags(serverURL)),
+		osquery.WithFlags([]string{"--tls_server_certs", certPath}),
 	)
 	g.Add(r.Execute, r.Interrupt)
 
