c4d062cd7e7d3276e33f894cffa33c34122a16f9
Noah Talerman <noahtal@umich.edu>
2021-01-25 14:12:14 -0800

    Add correct pluralization to hosts and results counts

--- frontend/components/queries/QueryProgressDetails/QueryProgressDetails.jsx
+++ frontend/components/queries/QueryProgressDetails/QueryProgressDetails.jsx
@@ -11,10 +11,19 @@ const baseClass = 'query-progress-details';
 
 const QueryProgressDetails = ({ campaign, className, onRunQuery, onStopQuery, queryIsRunning, queryTimerMilliseconds, disableRun }) => {
   const { hosts_count: hostsCount } = campaign;
-  const { Metrics: metrics } = campaign;
+  const { Metrics: metrics = {} } = campaign;
   const { errors } = campaign;
   const totalHostsCount = get(campaign, ['totals', 'count'], 0);
   const totalRowsCount = get(campaign, ['query_results', 'length'], 0);
+
+  const onlineHostsTotalDisplay = metrics.OnlineHosts === 1 ? '1 host' : `${metrics.OnlineHosts} hosts`;
+  const onlineResultsTotalDisplay = totalRowsCount === 1 ? '1 result' : `${totalRowsCount} results`;
+  const offlineHostsTotalDisplay = metrics.OfflineHosts === 1 ? '1 host' : `${metrics.OfflineHosts} hosts`;
+  const failedHostsTotalDisplay = hostsCount.failed === 1 ? '1 host' : `${hostsCount.failed} hosts`;
+  let totalErrorsDisplay = '0 errors';
+  if (errors) {
+    totalErrorsDisplay = errors.length === 1 ? '1 error' : `${errors.length} errors`;
+  }
 
   const runQueryBtn = (
     <div className={`${baseClass}__btn-wrapper`}>
@@ -54,9 +63,9 @@ const QueryProgressDetails = ({ campaign, className, onRunQuery, onStopQuery, qu
     <div className={`${baseClass} ${className}`}>
       <div className={`${baseClass}__wrapper`}>
         <div className={`${baseClass}__text-wrapper`}>
-          <span className={`${baseClass}__text-online`}>Online - {metrics.OnlineHosts} hosts returning {totalRowsCount} results</span>
-          <span className={`${baseClass}__text-offline`}>Offline - {metrics.OfflineHosts} hosts returning 0 results</span>
-          <span className={`${baseClass}__text-error`}>Failed - {hostsCount.failed} hosts returning {errors ? errors.length : '0'} errors</span>
+          <span className={`${baseClass}__text-online`}>Online - {onlineHostsTotalDisplay} returning {onlineResultsTotalDisplay}</span>
+          <span className={`${baseClass}__text-offline`}>Offline - {offlineHostsTotalDisplay} returning 0 results</span>
+          <span className={`${baseClass}__text-error`}>Failed - {failedHostsTotalDisplay} returning {totalErrorsDisplay}</span>
         </div>
         <ProgressBar
           error={hostsCount.failed}
