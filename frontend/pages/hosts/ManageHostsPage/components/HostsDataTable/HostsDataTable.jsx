2838741463d77af97e9231655fc3b44c8763caa8
Gabriel Hernandez <ghernandez345@gmail.com>
2021-02-16 11:34:06 +0000

    get label network calls working in hostdatatable

--- frontend/pages/hosts/ManageHostsPage/components/HostsDataTable/HostsDataTable.jsx
+++ frontend/pages/hosts/ManageHostsPage/components/HostsDataTable/HostsDataTable.jsx
@@ -1,28 +1,41 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useTable, useGlobalFilter, useSortBy, useAsyncDebounce } from 'react-table';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+
 
 // TODO: move this file closer to HostsDataTable
 import hostInterface from 'interfaces/host';
 import { humanHostMemory, humanHostUptime } from 'kolide/helpers';
 import InputField from 'components/forms/fields/InputField';
+import { setPagination } from 'redux/nodes/components/ManageHostsPage/actions';
 
 import TextCell from '../TextCell/TextCell';
 import StatusCell from '../StatusCell/StatusCell';
 import LinkCell from '../LinkCell/LinkCell';
 
-const useListenForTableChanges = (tableState, changeTableDataHandler) => {
-  useEffect(() => {
-    // changeTableDataHandler();
-    console.log('TABLE STATE:', tableState);
-  }, [tableState.sortBy, tableState.globalFilter]);
-};
+// const useListenForTableChanges = (tableState, changeTableDataHandler) => {
+//   useEffect(() => {
+//     // changeTableDataHandler();
+//     console.log('TABLE STATE:', tableState);
+//   }, [tableState.sortBy, tableState.globalFilter]);
+// };
 
 // This data table uses react-table for implementation. The relevant documentation of the library
 // can be found here https://react-table.tanstack.com/docs/api/useTable
 const HostsDataTable = (props) => {
   console.log('render HostsDataTable');
-  const columns = React.useMemo(() => {
+  const { selectedLabel = {}, selectedFilter = '' } = props;
+  const dispatch = useDispatch();
+  const loadingHosts = useSelector(state => state.entities.hosts.loading);
+  const hosts = useSelector(state => state.entities.hosts.data);
+
+  useEffect(() => {
+    console.log('fetching');
+    dispatch(setPagination(undefined, undefined, selectedFilter));
+  }, [dispatch, selectedFilter]);
+
+  const columns = useMemo(() => {
     return [
       { Header: 'Hostname', accessor: 'hostname', Cell: cellProps => <LinkCell value={cellProps.cell.value} host={cellProps.row.original} /> },
       { Header: 'Status', accessor: 'status', Cell: cellProps => <StatusCell value={cellProps.cell.value} /> },
@@ -36,9 +49,8 @@ const HostsDataTable = (props) => {
     ];
   }, []);
 
-  const { hosts } = props;
-  const data = React.useMemo(() => {
-    return hosts;
+  const data = useMemo(() => {
+    return Object.values(hosts);
   }, [hosts]);
 
   const {
@@ -60,8 +72,7 @@ const HostsDataTable = (props) => {
     debouncedGlobalFilter(value);
   }, [setSearchQuery, debouncedGlobalFilter]);
 
-  const { onChangeTableData } = props;
-  useListenForTableChanges(state, onChangeTableData);
+  if (loadingHosts) return null;
 
   return (
     <React.Fragment>
