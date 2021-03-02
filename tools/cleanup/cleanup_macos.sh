643201a8ef99cf91e030fcea0b7b9d885546eab0
Zach Wasserman <zach@fleetdm.com>
2021-03-02 11:24:02 -0800

    Add logs directory to macos cleanup script

--- tools/cleanup/cleanup_macos.sh
+++ tools/cleanup/cleanup_macos.sh
@@ -3,4 +3,4 @@
 sudo launchctl stop com.fleetdm.orbit
 sudo launchctl unload /Library/LaunchDaemons/com.fleetdm.orbit.plist
 
-sudo rm -rf /Library/LaunchDaemons/com.fleetdm.orbit.plist /var/lib/orbit/ /usr/local/bin/orbit
+sudo rm -rf /Library/LaunchDaemons/com.fleetdm.orbit.plist /var/lib/orbit/ /usr/local/bin/orbit /var/log/orbit
