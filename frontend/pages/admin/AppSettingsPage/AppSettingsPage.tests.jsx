7022ea04df17e16b2440768ff66a94212544abe9
Noah Talerman <noahtal@umich.edu>
2021-03-09 09:38:15 -0800

    Remove SMTP warning from organization settings page. Update tests.

--- frontend/pages/admin/AppSettingsPage/AppSettingsPage.tests.jsx
+++ frontend/pages/admin/AppSettingsPage/AppSettingsPage.tests.jsx
@@ -8,20 +8,6 @@ const { connectedComponent, reduxMockStore } = testHelpers;
 const baseStore = {
   app: { config: flatConfigStub, enrollSecret: [] },
 };
-const storeWithoutSMTPConfig = {
-  ...baseStore,
-  app: {
-    config: { ...flatConfigStub, configured: false },
-    enrollSecret: [],
-  },
-};
-const storeWithSMTPConfig = {
-  ...baseStore,
-  app: {
-    config: { ...flatConfigStub, configured: true },
-    enrollSecret: [],
-  },
-};
 
 describe('AppSettingsPage - component', () => {
   it('renders', () => {
@@ -29,41 +15,5 @@ describe('AppSettingsPage - component', () => {
     const page = mount(connectedComponent(AppSettingsPage, { mockStore }));
 
     expect(page.find('AppSettingsPage').length).toEqual(1);
-  });
-
-  it('renders a warning if SMTP has not been configured', () => {
-    const mockStore = reduxMockStore(storeWithoutSMTPConfig);
-    const page = mount(
-      connectedComponent(AppSettingsPage, { mockStore }),
-    ).find('AppSettingsPage');
-
-    const smtpWarning = page.find('WarningBanner');
-
-    expect(smtpWarning.length).toEqual(1);
-    expect(smtpWarning.find('KolideIcon').length).toEqual(1);
-    expect(smtpWarning.text()).toContain('Warning:SMTP is not currently configured in Fleet. The "Add new user" feature requires that SMTP is configured in order to send invitation emails. Users may also be added with "fleetctl user create".');
-  });
-
-  it('dismisses the smtp warning when "DISMISS" is clicked', () => {
-    const mockStore = reduxMockStore(storeWithoutSMTPConfig);
-    const page = mount(
-      connectedComponent(AppSettingsPage, { mockStore }),
-    );
-
-    const smtpWarning = page.find('WarningBanner');
-    const dismissButton = smtpWarning.find('Button').first();
-
-    dismissButton.simulate('click');
-
-    expect(page.find('WarningBanner').html()).toBeFalsy();
-  });
-
-  it('does not render a warning if SMTP has been configured', () => {
-    const mockStore = reduxMockStore(storeWithSMTPConfig);
-    const page = mount(
-      connectedComponent(AppSettingsPage, { mockStore }),
-    ).find('AppSettingsPage');
-
-    expect(page.find('WarningBanner').html()).toBeFalsy();
   });
 });
