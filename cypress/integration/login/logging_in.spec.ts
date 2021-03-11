7e8ced4fd1cc24719f2bab5cefe6246a085add21
Zach Wasserman <zach@fleetdm.com>
2021-03-11 12:23:49 -0800

    Improve E2E testing workflows

--- cypress/integration/login/logging_in.spec.ts
+++ cypress/integration/login/logging_in.spec.ts
@@ -1,6 +1,6 @@
-describe('Searching for a host', () => {
-  it('Logs into the applications', () => {
-    cy.visit('https://localhost:8080');
+describe('Login', () => {
+  it('Logs in successfully', () => {
+    cy.visit('/');
     cy.get(':nth-child(1) > .input-icon-field__input')
       .type('test@fleetdm.com');
 
@@ -12,5 +12,20 @@ describe('Searching for a host', () => {
 
     cy.url().should('include', '/hosts/manage');
     cy.contains('All Hosts');
+  });
+
+  it('Fails with invalid password', () => {
+    cy.visit('/');
+    cy.get(':nth-child(1) > .input-icon-field__input')
+      .type('test@fleetdm.com');
+
+    cy.get(':nth-child(2) > .input-icon-field__input')
+      .type('bad_password');
+
+    cy.get('.button')
+      .click();
+
+    cy.url().should('include', '/login');
+    cy.contains('username or email and password do not match');
   });
 });
