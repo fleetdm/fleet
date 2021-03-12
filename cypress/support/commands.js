39fc25cf5c00790e15b0d23b5cc0ce62ac9d5cd6
Zach Wasserman <zach@fleetdm.com>
2021-03-12 11:49:28 -0800

    Refactor cypress tests and expand testing (#450)
    - Add cypress commands `setup` and `login`.
    - Test setup page.
    - Test add new host dialog (MVP establishing testing patterns).
    - Add `testing-library/cypress`.
    - Add `cypress` directory to lint config.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import '@testing-library/cypress/add-commands';
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,16 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setup', () => {
+  cy.exec('make e2e-reset-db e2e-setup', { timeout: 5000 });
+});
+
+Cypress.Commands.add('login', (username, password) => {
+  username ||= 'test';
+  password ||= 'admin123#';
+  cy.request('POST', '/api/v1/fleet/login', { username, password })
+    .then((resp) => {
+      window.localStorage.setItem('KOLIDE::auth_token', resp.body.token);
+    });
+});
