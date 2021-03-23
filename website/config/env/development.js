a4bcdb1fdbe6423d705db82914025ca72873c156
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-03-23 04:36:35 -0500

    Fix lint rules and simplify website development by checking in a dev-only config file that sets the default port to 2024 instead of 1337, so you dont have to do that with a command-line option.

---
+++ website/config/env/development.js
@@ -0,0 +1,17 @@
+/**
+ * Development environment settings
+ * (sails.config.*)
+ *
+ * The configuration in this file only applies when Sails is running
+ * in the local development environment.
+ */
+
+module.exports = {
+
+  // Develop locally on http://localhost:2024
+  // (instead of the standard default for Sails apps, http://localhost:1337.
+  // This helps avoid conflicts since `fleetctl preview` will very often already
+  // be running on port 1337 on your computer.)
+  port: 2024,
+
+};
