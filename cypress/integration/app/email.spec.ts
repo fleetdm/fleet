2dd9db72d8f60f0de577fa993a033725cc816214
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-04-19 17:57:08 -0400

    WIP: move email test into settingsflow

--- cypress/integration/app/email.spec.ts
+++ cypress/integration/app/email.spec.ts
@@ -10,18 +10,3 @@ describe("Fleet Email API", () => {
       .should("include", "application/json");
   });
 });
-
-describe("Hello from Fleet Email API", () => {
-  it("if last email is Hello from Fleet", () => {
-    cy.request("http://localhost:8025/api/v2/messages").then((response) => {
-      console.log(response.body);
-      expect(response.status).to.eq(200);
-      expect(response.body.items[0].To[0]).to.have.property("Domain");
-      expect(response.body.items[0].To[0].Mailbox).to.equal("test");
-      expect(response.body.items[0].To[0].Domain).to.equal("fleetdm.com");
-      expect(response.body.items[0].Content.Headers.Subject[0]).to.equal(
-        "Hello from Fleet"
-      );
-    });
-  });
-});
