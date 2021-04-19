a0769e3cd8bf72a411f1a3cdab3adc4f37208626
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2021-04-19 18:14:43 -0400

    Fix react key issue (#648)
    * Fix key issue
    * Fix non-unique hostname selection bug

--- frontend/components/forms/fields/SelectTargetsDropdown/SelectTargetsInput/SelectTargetsInput.jsx
+++ frontend/components/forms/fields/SelectTargetsDropdown/SelectTargetsInput/SelectTargetsInput.jsx
@@ -73,7 +73,7 @@ class SelectTargetsInput extends Component {
         scrollMenuIntoView={false}
         tabSelectsValue={false}
         value={selectedTargets}
-        valueKey="display_text"
+        valueKey="id"
       />
     );
   }
