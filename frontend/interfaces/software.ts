0be353cbd105ea34da0136bcd241596f297c1a0f
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-04-27 15:21:05 -0400

    Fix linting tab

--- frontend/interfaces/software.ts
+++ frontend/interfaces/software.ts
@@ -3,6 +3,6 @@ import PropTypes from "prop-types";
 export default PropTypes.shape({
   type: PropTypes.string,
   name: PropTypes.string,
-version: PropTypes.string,
+  version: PropTypes.string,
   id: PropTypes.number,
 });
