dab312b379419ec8461d62eb8aa60fa7e4f5ea93
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-05-03 18:59:31 -0500

    modernize some of the bits relying on reassigning arguments

--- website/api/helpers/compile-markdown-docs.js
+++ website/api/helpers/compile-markdown-docs.js
@@ -49,30 +49,22 @@ module.exports = {
       jsMenuPath: path.join('views/partials/doc-menus', compileFromPath+'.jsmenu'),
       outputExtension: 'ejs',
       beforeConvert: (mdString, proceed)=>{// This function is applied to each template before the markdown is converted to markup
-
-        // This is an HTML comment because it is easy to over-match and "accidentally"
-        // add it underneath each code block as well (being an HTML comment ensures it
-        // doesn't show up or break anything)
-        // var TMP_LANG_MARKER_EXPR = /<!-- __LANG=\%[^%]*\%__ -->/;
-        var LANG_MARKER_PREFIX = '<!-- __LANG=%';
-        var LANG_MARKER_SUFFIX = '%__ -->';
-
-        // Based on the github-flavored markdown's language annotation, (e.g. ```js```)
-        // add a temporary marker to code blocks that can be parsed post-md-compilation
-        // by the `afterConvert()` lifecycle hook
-        mdString = mdString.replace(/(```)([a-zA-Z0-9\-]*)(\s*\n)/g, '$1\n' + LANG_MARKER_PREFIX + '$2' + LANG_MARKER_SUFFIX + '$3');
-
-        return proceed(undefined, mdString);
+        // Based on the github-flavored markdown's language annotation, (e.g. ```js```) add a temporary marker to code blocks that can be parsed post-md-compilation by the `afterConvert()` lifecycle hook
+        // Note: This is an HTML comment because it is easy to over-match and "accidentally" add it underneath each code block as well (being an HTML comment ensures it doesn't show up or break anything)
+        let LANG_MARKER_PREFIX = '<!-- __LANG=%';
+        let LANG_MARKER_SUFFIX = '%__ -->';
+        let modifiedMd = mdString.replace(/(```)([a-zA-Z0-9\-]*)(\s*\n)/g, '$1\n' + LANG_MARKER_PREFIX + '$2' + LANG_MARKER_SUFFIX + '$3');
+        return proceed(undefined, modifiedMd);
       },
       afterConvert: (html, proceed)=>{// This function is applied to each template after the markdown is converted to markup
 
         // Replace github emoji with HTML
-        html = html.replace(/\:white_check_mark\:/g, '<i class="sails-icon icon-plus"></i>');
-        html = html.replace(/\:white_large_square\:/g, '<i class="sails-icon icon-minus"></i>');
-        html = html.replace(/\:heavy_multiplication_x\:/g, '<i class="sails-icon icon-times"></i>');
+        let modifiedHtml = html.replace(/\:white_check_mark\:/g, '<i class="sails-icon icon-plus"></i>');
+        modifiedHtml = modifiedHtml.replace(/\:white_large_square\:/g, '<i class="sails-icon icon-minus"></i>');
+        modifiedHtml = modifiedHtml.replace(/\:heavy_multiplication_x\:/g, '<i class="sails-icon icon-times"></i>');
 
         // Replace ((bubble))s with HTML
-        html = html.replace(/\(\(([^())]*)\)\)/g, '<bubble type="$1" class="colors"><span is="bubble-heart"></span></bubble>');
+        modifiedHtml = modifiedHtml.replace(/\(\(([^())]*)\)\)/g, '<bubble type="$1" class="colors"><span is="bubble-heart"></span></bubble>');
 
         // Flag <h2>, <h3>, <h4>, and <h5> tags
         // with the `permalinkable` directive
@@ -81,7 +73,7 @@ module.exports = {
         // if the page is #/documentation/reference/req
         // and the slug is "transport-compatibility"
         // then the final URL will be #/documentation/reference/req?q=transport-compatibility
-        var $ = cheerio.load(html);
+        var $ = cheerio.load(modifiedHtml);
         $('h2, h3, h4, h5').each(function() {
           var content = $(this).text() || '';
 
@@ -100,13 +92,13 @@ module.exports = {
           }
 
         });
-        html = $.html();
+        modifiedHtml = $.html();
 
         // Convert URL fragment links (i.e. for client-side routes) into
         // web-root-relative URLs (i.e. they'll have a leading slash).
         // (this is because there are lots of links left over from when sailsjs.org
         // used client-side routes for navigation around the documentation pages.)
-        html = html.replace(/(href=")\.?\/?#\/?([^"]*)"/g, '$1/$2"');
+        modifiedHtml = modifiedHtml.replace(/(href=")\.?\/?#\/?([^"]*)"/g, '$1/$2"');
 
         // e.g.
         //   href="/#/asdjgasdg"     =>  href="/asdjgasdg"
@@ -117,10 +109,10 @@ module.exports = {
         // Get rid of the '.html' at the end of ANY internal web-root-relative URL that
         // point at the documentation pages. Any links form the docs that start with
         // '/documentation' and ends in '.html' will have the file extension stripped off.
-        html = html.replace(/(href="\/documentation)([^"]*)\.html"/g, '$1$2"');
+        modifiedHtml = modifiedHtml.replace(/(href="\/documentation)([^"]*)\.html"/g, '$1$2"');
 
         // Add target=_blank to external links (e.g. http://google.com or https://chase.com)
-        html = html.replace(/(href="https?:\/\/([^"]+)")/g, (match)=>{
+        modifiedHtml = modifiedHtml.replace(/(href="https?:\/\/([^"]+)")/g, (match)=>{
           // Check if this is an external link that is ALSO not a link to some page
           // on `(*.)?sailsjs.com` or `(*.)?sailsjs.org`.
           var isExternal = ! match.match(/^href=\"https?:\/\/([^\.]+\.)*sailsjs\.(org|com)/g);
@@ -154,34 +146,34 @@ module.exports = {
         // console.log('RAN AFTER HOOK, found: ',html.match(/(<code)([^>]*)(>\s*)(\&lt;!--\s*__LANG=\%[^\%]*\%__\s*--\&gt;)/g));
 
         // Interpret `js` as `javascript`
-        html = html.replace(
+        modifiedHtml = modifiedHtml.replace(
           // $1     $2     $3   $4
           /(<code)([^>]*)(>\s*)(\&lt;!-- __LANG=\%js\%__ --\&gt;)\s*/gm,
           '$1 class="javascript"$2$3'
         );
 
         // Interpret `sh` and `bash` as `bash`
-        html = html.replace(
+        modifiedHtml = modifiedHtml.replace(
           // $1     $2     $3   $4
           /(<code)([^>]*)(>\s*)(\&lt;!-- __LANG=\%(bash|sh)\%__ --\&gt;)\s*/gm,
           '$1 class="bash"$2$3'
         );
 
         // When unspecified, default to `text`
-        html = html.replace(
+        modifiedHtml = modifiedHtml.replace(
           // $1     $2     $3   $4
           /(<code)([^>]*)(>\s*)(\&lt;!-- __LANG=\%\%__ --\&gt;)\s*/gm,
           '$1 class="nohighlight"$2$3'
         );
 
         // Finally, nab the rest, leaving the code language as-is.
-        html = html.replace(
+        modifiedHtml = modifiedHtml.replace(
           // $1     $2     $3   $4               $5    $6
           /(<code)([^>]*)(>\s*)(\&lt;!-- __LANG=\%)([^%]+)(\%__ --\&gt;)\s*/gm,
           '$1 class="$5"$2$3'
         );
 
-        return proceed(undefined, html);
+        return proceed(undefined, modifiedHtml);
       },
     }], (err)=>{
       if (err) { return exits.error(err); }
