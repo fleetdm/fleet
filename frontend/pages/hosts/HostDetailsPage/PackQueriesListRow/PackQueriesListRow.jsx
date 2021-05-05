3f29b798b118f535ccb0c1c85c1f0bfe0b43644e
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-05-05 11:49:24 -0400

    Fix props and proptypes

--- frontend/pages/hosts/HostDetailsPage/PackQueriesListRow/PackQueriesListRow.jsx
+++ frontend/pages/hosts/HostDetailsPage/PackQueriesListRow/PackQueriesListRow.jsx
@@ -1,29 +1,23 @@
 import React, { Component } from "react";
 
-// THIS IS COMING FROM ZACH
-// import softwareInterface from "interfaces/software";
+import queryInterface from "interfaces/query";
 
 const baseClass = "pack-queries-list-row";
 
 class PackQueriesListRow extends Component {
-  // static propTypes = {
-  //   software: softwareInterface.isRequired,
-  // };
-
-  // BRING IN PROPS BEFORE THIS CAN RENDER
-  // static propTypes = {
-  //   query: PropTypes.object, // fix this proptype
-  // };
+  static propTypes = {
+    query: queryInterface.isRequired,
+  };
 
   render() {
     const { query } = this.props;
-    const { name, description, frequency, last_executed } = query;
+    const { name, description, interval, last_executed } = query;
 
     return (
       <tr>
         <td className={`${baseClass}__name`}>{name}</td>
         <td className={`${baseClass}__description`}>{description}</td>
-        <td className={`${baseClass}__frequency`}>{frequency}</td>
+        <td className={`${baseClass}__frequency`}>{interval} seconds</td>
         <td className={`${baseClass}__last-run`}>{last_executed}</td>
       </tr>
     );
