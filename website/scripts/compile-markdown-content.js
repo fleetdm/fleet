a3b41902b396e36d7be8f1c5d7027c67cd9b8746
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-05-09 23:01:58 -0500

    Use .sailsrc to hold onto menu content, so it's injected into config for easier access (rather than reading JSON files whenever needed)

--- website/scripts/compile-markdown-content.js
+++ website/scripts/compile-markdown-content.js
@@ -9,17 +9,22 @@ module.exports = {
 
   fn: async function () {
 
-    sails.log('Running custom shell script... (`sails run compile-markdown-content`)');
+    let path = require('path');
 
-    let generatedFilesBySection = await sails.helpers.flow.simultaneously({
+    // sails.log('Running custom shell script... (`sails run compile-markdown-content`)');
+
+    let filesGeneratedBySection = await sails.helpers.flow.simultaneously({
       documentation: async() => await sails.helpers.compileMarkdownContent('docs/'),
       queryLibrary: async() => await sails.helpers.compileMarkdownContent('handbook/queries/')
     });
+    // console.log(filesGeneratedBySection);
 
     // Now take the compiled menu file and inject it into the .sailsrc file so it
-    // can be accessed for the purposes of config.
-    // TODO
-    console.log(generatedFilesBySection);
+    // can be accessed for the purposes of config using `sails.config.compiledFromMarkdown`.
+    let sailsrcPath = path.resolve(sails.config.appPath, '.sailsrc');
+    let sailsrcContent = await sails.helpers.fs.readJson(sailsrcPath);
+    sailsrcContent.compiledFromMarkdown = filesGeneratedBySection;
+    await sails.helpers.fs.writeJson(sailsrcPath, sailsrcContent, true);
 
   }
 
