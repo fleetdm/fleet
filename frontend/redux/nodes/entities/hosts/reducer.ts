1d640320fd4077a4bcea06a428ddf0a5a38bc250
Gabriel Hernandez <ghernandez345@gmail.com>
2021-05-19 16:47:23 +0100

    fix build TS errors

--- frontend/redux/nodes/entities/hosts/reducer.ts
+++ frontend/redux/nodes/entities/hosts/reducer.ts
@@ -1,7 +1,12 @@
+import { PayloadAction } from "@reduxjs/toolkit";
 import config from "./config";
 import { TRANSFER_HOSTS_SUCCESS, TRANSFER_HOSTS_FAILURE } from "./actions";
 
-export default (state = config.initialState, { type, payload }) => {
+// TODO: figure out redux typing
+export default (
+  state = config.initialState,
+  { type, payload }: PayloadAction<any>
+): any => {
   switch (type) {
     case TRANSFER_HOSTS_SUCCESS:
       return {
