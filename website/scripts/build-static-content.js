a1b72498b1c91cd708b65bbd68a04715ee636a76
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-05-20 01:29:26 -0500

    expand comments

--- website/scripts/build-static-content.js
+++ website/scripts/build-static-content.js
@@ -11,27 +11,39 @@ module.exports = {
 
     let path = require('path');
 
-    let filesGeneratedBySection = {
-      documentation: await sails.helpers.compileMarkdownContent('docs/'),
-      queryLibrary: await sails.helpers.compileMarkdownContent('handbook/queries/')
-    };
+    // Compile queries from YAML to markdown.
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // FUTURE: Be smarter about how we compile the YAML.
+    // i.e. dissect some of the code from here https://github.com/uncletammy/doc-templater/blob/2969726b598b39aa78648c5379e4d9503b65685e/lib/compile-markdown-tree-from-remote-git-repo.js#L16-L22
+    // and use those building blocks directly instead of depending on doctemplater
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // TODO
+
+    // Compile HTML from markdown.
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // FUTURE: Make this work in parallel as shown here by improving doctemplater to avoid the alreadyExists error
     // (this actually only fails the very first time, but still, thinking is that it's not worth leaving a hack in
     // here for a trivial build perf boost right now, especially since it only affects website deploys)
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // ```
     // let filesGeneratedBySection = await sails.helpers.flow.simultaneously({
     //   documentation: async() => await sails.helpers.compileMarkdownContent('docs/'),
     //   queryLibrary: async() => await sails.helpers.compileMarkdownContent('handbook/queries/')
     // });
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // console.log(filesGeneratedBySection);
+    // ```
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    let filesGeneratedBySection = {
+      documentation: await sails.helpers.compileMarkdownContent('docs/'),
+      queryLibrary: await sails.helpers.compileMarkdownContent('handbook/queries/')
+    };
 
     // Compile and generate XML sitemap
     // (see "refresh" action in sailsjs.com repo for example)
     // TODO
 
-    // Now take the compiled menu file and inject it into the .sailsrc file so it
-    // can be accessed for the purposes of config using `sails.config.builtStaticContent`.
+    // Replace .sailsrc file.
+    // > This takes the compiled menu file from doc-templater and injects it into the .sailsrc file so it
+    // > can be accessed for the purposes of config using `sails.config.builtStaticContent`.
     let sailsrcPath = path.resolve(sails.config.appPath, '.sailsrc');
     let oldSailsrcJson = await sails.helpers.fs.readJson(sailsrcPath);
     await sails.helpers.fs.writeJson.with({
