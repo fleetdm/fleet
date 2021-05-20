52a940ab74f70a11363aa65b836963f041ea886b
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-05-20 03:00:36 -0500

    optimize build by running everything in parallel

--- website/scripts/build-static-content.js
+++ website/scripts/build-static-content.js
@@ -20,46 +20,47 @@ module.exports = {
     // The data we're compiling will get built into this dictionary and then written on top of the .sailsrc file.
     let builtStaticContent = {};
 
-    // Compile queries from YAML to markdown.
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // FUTURE: Be smarter (faster) about how we get and compile this YAML.
-    // i.e. dissect some of the code from here https://github.com/uncletammy/doc-templater/blob/2969726b598b39aa78648c5379e4d9503b65685e/lib/compile-markdown-tree-from-remote-git-repo.js#L16-L22
-    // and use those building blocks directly instead of depending on doctemplater later and thus
-    // unnecessarily duplicating work.   i.e. the "clone repo" step can be pulled up ahead of the simultaneously, etc.
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // Clone repo
-    let topLvlCachePath = path.resolve(sails.config.paths.tmp, `built-static-content/`);
-    await sails.helpers.fs.rmrf(topLvlCachePath);
-    let repoCachePath = path.join(topLvlCachePath, `cloned-repo-${Date.now()}-${Math.round(Math.random()*100)}`);
-    await sails.helpers.process.executeCommand(`git clone git://github.com/fleetdm/fleet.git ${repoCachePath}`);
+    await sails.helpers.flow.simultaneously([
+      async()=>{// Compile queries from YAML to markdown.
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // FUTURE: Be smarter (faster) about how we get and compile this YAML.
+        // i.e. dissect some of the code from here https://github.com/uncletammy/doc-templater/blob/2969726b598b39aa78648c5379e4d9503b65685e/lib/compile-markdown-tree-from-remote-git-repo.js#L16-L22
+        // and use those building blocks directly instead of depending on doctemplater later and thus
+        // unnecessarily duplicating work.   i.e. the "clone repo" step can be pulled up ahead of the simultaneously, etc.
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // Clone repo
+        let topLvlCachePath = path.resolve(sails.config.paths.tmp, `built-static-content/`);
+        await sails.helpers.fs.rmrf(topLvlCachePath);
+        let repoCachePath = path.join(topLvlCachePath, `cloned-repo-${Date.now()}-${Math.round(Math.random()*100)}`);
+        await sails.helpers.process.executeCommand(`git clone git://github.com/fleetdm/fleet.git ${repoCachePath}`);
 
-    // Parse YAML query library
-    let yaml = await sails.helpers.fs.read(path.join(repoCachePath, 'docs/1-Using-Fleet/standard-query-library/standard-query-library.yml'));
-    builtStaticContent.queries = YAML.parseAllDocuments(yaml).map((yamlDocument) => yamlDocument.toJSON().spec );
-
-    // Legacy:  (TODO: remove this later on, once in-progress frontend work is done)
-    await sails.helpers.compileMarkdownContent('handbook/queries/'); // TODO: pair w/ rachel and swap how page is built now that this part will work differently
-
-
-    // Compile HTML from markdown.
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // FUTURE: Make this work in parallel as shown here by improving doctemplater to avoid the alreadyExists error
-    // (this actually only fails the very first time, but still, thinking is that it's not worth leaving a hack in
-    // here for a trivial build perf boost right now, especially since it only affects website deploys)
-    // ```
-    // let builtStaticContent = await sails.helpers.flow.simultaneously({
-    //   documentation: async() => await sails.helpers.compileMarkdownContent('docs/'),
-    //   queryLibrary: async() => await sails.helpers.compileMarkdownContent('handbook/queries/')
-    // });
-    // console.log(builtStaticContent);
-    // ```
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    builtStaticContent.documentationTree = await sails.helpers.compileMarkdownContent('docs/');
-
-
-    // Compile and generate XML sitemap
-    // (see "refresh" action in sailsjs.com repo for example)
-    // TODO
+        // Parse YAML query library
+        let yaml = await sails.helpers.fs.read(path.join(repoCachePath, 'docs/1-Using-Fleet/standard-query-library/standard-query-library.yml'));
+        builtStaticContent.queries = YAML.parseAllDocuments(yaml).map((yamlDocument) => yamlDocument.toJSON().spec );
+      },
+      async()=>{// Legacy: Compile stuff we won't actually use for much longer
+        // Legacy:  (TODO: remove this later on, once in-progress frontend work is done)
+        await sails.helpers.compileMarkdownContent('handbook/queries/'); // TODO: pair w/ rachel and swap how page is built now that this part will work differently
+      },
+      async()=>{// Compile HTML from markdown docs.
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // FUTURE: Make this work in parallel as shown here by improving doctemplater to avoid the alreadyExists error
+        // (this actually only fails the very first time, but still, thinking is that it's not worth leaving a hack in
+        // here for a trivial build perf boost right now, especially since it only affects website deploys)
+        // ```
+        // let builtStaticContent = await sails.helpers.flow.simultaneously({
+        //   documentation: async() => await sails.helpers.compileMarkdownContent('docs/'),
+        //   queryLibrary: async() => await sails.helpers.compileMarkdownContent('handbook/queries/')
+        // });
+        // console.log(builtStaticContent);
+        // ```
+        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        builtStaticContent.documentationTree = await sails.helpers.compileMarkdownContent('docs/');
+      },
+      async()=>{// Compile and generate XML sitemap
+        // TODO (see "refresh" action in sailsjs.com repo for example)
+      },
+    ]);
 
 
     // Replace .sailsrc file.
