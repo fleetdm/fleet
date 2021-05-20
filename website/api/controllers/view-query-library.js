03d8697f47ee909b278871072654a91b85dc6fd8
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-05-20 02:55:45 -0500

    Compile YAML and set up query library page

--- website/api/controllers/view-query-library.js
+++ website/api/controllers/view-query-library.js
@@ -18,8 +18,14 @@ module.exports = {
 
   fn: async function () {
 
+    if (!sails.config.builtStaticContent.queries) {
+      throw new Error('Missing `sails.config.builtStaticContent.queries`!  Try doing `sails run build-static-content`.');
+    }
+
     // Respond with view.
-    return {};
+    return {
+      queries: sails.config.builtStaticContent.queries
+    };
 
   }
 
--- website/scripts/build-static-content.js
+++ website/scripts/build-static-content.js
@@ -7,34 +7,39 @@ module.exports = {
   description: 'Generate HTML partials from source files in fleetdm/fleet repo (e.g. docs in markdown, or queries in YAML), and configure metadata about the generated files so it is available in `sails.config.builtStaticContent`.',
 
 
-  fn: async function () {
+  inputs: {
+    dry: { type: 'boolean', description: 'Whether to make this a dry run.  (.sailsrc file will not be overwritten.  Everything else still happens).' },
+  },
+
+
+  fn: async function ({ dry }) {
 
     let path = require('path');
+    let YAML = require('yaml');
+
+    // The data we're compiling will get built into this dictionary and then written on top of the .sailsrc file.
+    let builtStaticContent = {};
 
     // Compile queries from YAML to markdown.
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // FUTURE: Be smarter about how we compile the YAML.
+    // FUTURE: Be smarter (faster) about how we get and compile this YAML.
     // i.e. dissect some of the code from here https://github.com/uncletammy/doc-templater/blob/2969726b598b39aa78648c5379e4d9503b65685e/lib/compile-markdown-tree-from-remote-git-repo.js#L16-L22
-    // and use those building blocks directly instead of depending on doctemplater
+    // and use those building blocks directly instead of depending on doctemplater later and thus
+    // unnecessarily duplicating work.   i.e. the "clone repo" step can be pulled up ahead of the simultaneously, etc.
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    // TODO
+    // Clone repo
+    let topLvlCachePath = path.resolve(sails.config.paths.tmp, `built-static-content/`);
+    await sails.helpers.fs.rmrf(topLvlCachePath);
+    let repoCachePath = path.join(topLvlCachePath, `cloned-repo-${Date.now()}-${Math.round(Math.random()*100)}`);
+    await sails.helpers.process.executeCommand(`git clone git://github.com/fleetdm/fleet.git ${repoCachePath}`);
 
-    let needsToBeClonedForFirstTime;
-    await sails.helpers.process.executeCommand.with({
-      command: 'git status',
-      dir: path.resolve(sails.config.paths.tmp, 'built-static-content/cloned-repo')
-    })
-    .tolerate('noSuchDir', ()=>{
-      needsToBeClonedForFirstTime = true;
-    });
+    // Parse YAML query library
+    let yaml = await sails.helpers.fs.read(path.join(repoCachePath, 'docs/1-Using-Fleet/standard-query-library/standard-query-library.yml'));
+    builtStaticContent.queries = YAML.parseAllDocuments(yaml).map((yamlDocument) => yamlDocument.toJSON().spec );
 
+    // Legacy:  (TODO: remove this later on, once in-progress frontend work is done)
+    await sails.helpers.compileMarkdownContent('handbook/queries/'); // TODO: pair w/ rachel and swap how page is built now that this part will work differently
 
-    if (!needsToBeClonedForFirstTime) {
-
-    }
-
-    // ;
-    // path.resolve(sails.config.paths.tmp, 'built-static-content/cloned-repo')
 
     // Compile HTML from markdown.
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -42,35 +47,38 @@ module.exports = {
     // (this actually only fails the very first time, but still, thinking is that it's not worth leaving a hack in
     // here for a trivial build perf boost right now, especially since it only affects website deploys)
     // ```
-    // let filesGeneratedBySection = await sails.helpers.flow.simultaneously({
+    // let builtStaticContent = await sails.helpers.flow.simultaneously({
     //   documentation: async() => await sails.helpers.compileMarkdownContent('docs/'),
     //   queryLibrary: async() => await sails.helpers.compileMarkdownContent('handbook/queries/')
     // });
-    // console.log(filesGeneratedBySection);
+    // console.log(builtStaticContent);
     // ```
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    let filesGeneratedBySection = {
-      documentation: await sails.helpers.compileMarkdownContent('docs/'),
-      queryLibrary: await sails.helpers.compileMarkdownContent('handbook/queries/')
-    };
+    builtStaticContent.documentationTree = await sails.helpers.compileMarkdownContent('docs/');
+
 
     // Compile and generate XML sitemap
     // (see "refresh" action in sailsjs.com repo for example)
     // TODO
 
+
     // Replace .sailsrc file.
     // > This takes the compiled menu file from doc-templater and injects it into the .sailsrc file so it
     // > can be accessed for the purposes of config using `sails.config.builtStaticContent`.
-    let sailsrcPath = path.resolve(sails.config.appPath, '.sailsrc');
-    let oldSailsrcJson = await sails.helpers.fs.readJson(sailsrcPath);
-    await sails.helpers.fs.writeJson.with({
-      force: true,
-      destination: sailsrcPath,
-      json: {
-        ...oldSailsrcJson,
-        builtStaticContent: filesGeneratedBySection,
-      }
-    });
+    if (dry) {
+      console.log('Dry run: Would have folded the following onto .sailsrc as "builtStaticContent":', builtStaticContent);
+    } else {
+      let sailsrcPath = path.resolve(sails.config.appPath, '.sailsrc');
+      let oldSailsrcJson = await sails.helpers.fs.readJson(sailsrcPath);
+      await sails.helpers.fs.writeJson.with({
+        force: true,
+        destination: sailsrcPath,
+        json: {
+          ...oldSailsrcJson,
+          builtStaticContent: builtStaticContent,
+        }
+      });
+    }
 
   }
 
