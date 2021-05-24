77db164a0c7dc65489dc5cc146cad5bfdda8051c
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2021-05-24 18:55:36 -0500

    Spiff PackInfoSidePanel

--- frontend/components/side_panels/PackInfoSidePanel/PackInfoSidePanel.jsx
+++ frontend/components/side_panels/PackInfoSidePanel/PackInfoSidePanel.jsx
@@ -8,20 +8,15 @@ const baseClass = "pack-info-side-panel";
 const PackInfoSidePanel = () => {
   return (
     <SecondarySidePanelContainer className={baseClass}>
-      <h3 className={`${baseClass}__title`}>
-        <KolideIcon name="packs" />
-        &nbsp; What&apos;s a query pack?
-      </h3>
+      <h3 className={`${baseClass}__title`}>What&apos;s a query pack?</h3>
       <p>
-        Osquery supports grouping of queries (called <b>query packs</b>) which
-        run on a scheduled basis and log the results to a configurable
-        destination.
+        Osquery supports grouping of queries (called query packs) which run on a
+        scheduled basis and log the results to a configurable destination.
       </p>
       <p>
         Query Packs are useful for monitoring specific attributes of hosts over
         time and can be used for alerting and incident response investigations.
-        By default, queries added to packs run every hour (
-        <b>interval = 3600s</b>).
+        By default, queries added to packs run every hour (interval = 3600s).
       </p>
 
       <p>Queries can be run in two modes:</p>
@@ -40,8 +35,8 @@ const PackInfoSidePanel = () => {
 
       <h4 className={`${baseClass}__subtitle`}>Where do I find results?</h4>
       <p>
-        Packs are distributed to specified <b>targets</b>. Targets may be{" "}
-        <b>individual hosts</b> or groups of hosts called <b>labels.</b>
+        Packs are distributed to specified targets. Targets may be individual
+        hosts or groups of hosts called labels.
       </p>
       <p>
         The results of queries run via query packs are stored in log files for
