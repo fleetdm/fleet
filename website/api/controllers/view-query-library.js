ac220ba6e5171a54bf4021d337f04cbed0da1f15
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-05-26 03:24:38 -0500

    Obviate doc-templater dependency + dynamic sitemap.xml (#827)
    * trivial
    
    * Simplify build-static-content script and rip out the old markdown compilation for query library
    
    * improve error msg
    
    * trivial
    
    * move helper
    
    * bring in the skeleton
    
    * Compile handbook as well, and bring more stuff inline
    
    * instead of generating sitemap.xml file, could just serve it as a route
    
    * Serve sitemap.xml on the fly
    
    * add failsafe to prevent search engine accidents
    
    * add remaining hand-coded pages to sitemap
    
    * rearrange routes and get rid of commented-out ones
    
    * Update build-static-content.js
    
    * stub out the remaining pieces
    
    * Add assertion (Which actually helped catch a real duplicate query: get-mac-os-disk-free-space-percentage)
    
    * clean out inadvertently committed stuff in sailsrc
    
    * route and serve data for correct query by slug + fix error message re duplicate query slugs + added assertion for duplicate doc page slugs
    
    * yaml == dev dependency
    
    * remove doc-templater dependency, as promised
    
    * stub out handbook page
    
    * clarify comments & remove unnecessary skipAssets
    
    * Update build-static-content.js
    
    * res.badConfig()
    
    * add missing exit that I left out back in https://github.com/fleetdm/fleet/commit/ec95df6a4b8b0344fcbdb71f892d1185ec094f6e
    
    * remove unused file
    
    * update comments before commenting out and moving over to basic-documentation.less
    
    * move example styling of generated HTML over to docs/handbook
    
    * include both links
    
    * Fix sitemap.xml URLs in local dev by fixing baseUrl config for local development (since Fleet itself is on 1337).
    
    * followup to https://github.com/fleetdm/fleet/commit/d55c777590f4b1dac4b7500cce1bf8f70b652fd9
    
    * Include query pages in sitemap.xml (+make urls generated for docs/handbook in build script slightly more real) -- but also don't serve sitemap
    
    * sails.config.builtStaticContent.allPages » sails.config.buildStaticContent.markdownPages  (also remove unnecessary trailing slash trimming)
    
    * trivial
    
    * check config when serving sitemap + smarter error message for contributors
    
    * hook up GitHub link to edit the query
    
    * remove html ids
    
    * Update query-detail.ejs
    
    * somre more setup re https://github.com/fleetdm/fleet/issues/368#issuecomment-848566533

--- website/api/controllers/view-query-library.js
+++ website/api/controllers/view-query-library.js
@@ -8,18 +8,15 @@ module.exports = {
 
 
   exits: {
-
-    success: {
-      viewTemplatePath: 'pages/query-library'
-    }
-
+    success: { viewTemplatePath: 'pages/query-library' },
+    badConfig: { responseType: 'badConfig' },
   },
 
 
   fn: async function () {
 
-    if (!sails.config.builtStaticContent.queries) {
-      throw new Error('Missing `sails.config.builtStaticContent.queries`!  Try doing `sails run build-static-content`.');
+    if (!_.isObject(sails.config.builtStaticContent) || !_.isArray(sails.config.builtStaticContent.queries)) {
+      throw {badConfig: 'builtStaticContent.queries'};
     }
 
     // Respond with view.
