cc2c6e4d616c7f8b6a2df23dfcc8a852c01490fc
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2021-05-28 12:26:45 -0500

    Implement query-libary page for fleetdm.com

--- website/assets/js/pages/query-detail.page.js
+++ website/assets/js/pages/query-detail.page.js
@@ -20,6 +20,16 @@ parasails.registerPage('query-detail', {
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    //…
+    castRemediationArray: function (remediation) {
+      console.log(remediation);
+      if (_.isArray(remediation) && remediation.length) {
+        return remediation.filter((item) => _.isString(item) && item.length);
+      }
+      if (_.isString(remediation) && remediation.length) {
+        return [remediation];
+      }
+      return ['N/A'];
+    }
+
   }
 });
