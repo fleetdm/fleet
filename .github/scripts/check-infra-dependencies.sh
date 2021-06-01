a445a5834e806adc417dd88e4b237db96487fc2d
xiang <xiang13225080@163.com>
2021-06-02 00:10:56 +0800

    add retry
    Signed-off-by: xiang <xiang13225080@163.com>

--- .github/scripts/check-infra-dependencies.sh
+++ .github/scripts/check-infra-dependencies.sh
@@ -17,6 +17,7 @@ fi
 # use 'all' to check all these infras.
 INFRA_NAME=${1}
 INFRAS=()
+RETRYNUM=10
 
 if [ "$INFRA_NAME" == "all" ]; then
     INFRAS=("mysql" "redis" "mailhog" "saml_idp")
@@ -24,13 +25,14 @@ else
     INFRAS=("$INFRA_NAME")
 fi
 
-for INFRA in ${INFRAS[@]}; do
+checkInfraFun() {
+    INFRA=$1
     echo "check whether the $INFRA is ready"
     if [ "$INFRA" == "mysql" ]; then
-        docker-compose exec -T mysql_test bash -c 'echo "SHOW DATABASES;" | mysql -uroot -ptoor'
+        ! docker-compose exec -T mysql_test bash -c 'echo "SHOW DATABASES;" | mysql -uroot -ptoor' && return 1
         echo "mysql is ready!"
     elif [ "$INFRA" == "redis" ]; then
-        docker-compose exec -T redis bash -c "redis-cli ping"
+        ! docker-compose exec -T redis bash -c "redis-cli ping" && return 1
         echo "redis is ready!"
     elif [ "$INFRA" == "mailhog" ]; then
         echo "TODO"
@@ -38,5 +40,19 @@ for INFRA in ${INFRAS[@]}; do
     elif [ "$INFRA" == "saml_idp" ]; then
         echo "TODO"
         echo "saml_idp is ready!"
+    fi
+}
+
+for INFRA in ${INFRAS[@]}; do
+    n=0
+    success=false
+    until [ "$n" -ge $RETRYNUM ]; do
+        checkInfraFun $INFRA && success=true && break
+        n=$((n+1))
+        sleep 1 
+    done
+
+    if [ ! $success ]; then
+        exit 1
     fi
 done
