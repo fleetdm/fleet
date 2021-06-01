ee0f263708357699fe02ba797ca9ccf9c56854db
Gabriel Hernandez <ghernandez345@gmail.com>
2021-06-01 14:31:04 +0100

    add messaing for no permissions

--- frontend/components/AuthenticatedAdminRoutes/AuthenticatedAdminRoutes.jsx
+++ frontend/components/AuthenticatedAdminRoutes/AuthenticatedAdminRoutes.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { push } from "react-router-redux";
-
-import paths from "../../router/paths";
-import userInterface from "../../interfaces/user";
+import paths from "router/paths";
+import userInterface from "interfaces/user";
+import { renderFlash } from "redux/nodes/notifications/actions";
 
 export class AuthenticatedAdminRoutes extends Component {
   static propTypes = {
@@ -22,6 +22,9 @@ export class AuthenticatedAdminRoutes extends Component {
 
     if (global_role !== "admin") {
       dispatch(push(HOME));
+      dispatch(
+        renderFlash("error", "You do not have permissions for that page")
+      );
     }
     return false;
   }
