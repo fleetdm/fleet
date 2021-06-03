25c562ebe55401b96821782975f0ecbf4601f26a
Zach Wasserman <zach@fleetdm.com>
2021-06-03 15:48:21 -0700

    Add action descriptions

--- server/kolide/authz.go
+++ server/kolide/authz.go
@@ -1,8 +1,12 @@
 package kolide
 
 const (
-	ActionRead      = "read"
-	ActionWrite     = "write"
+	// ActionRead refers to reading an entity.
+	ActionRead = "read"
+	// ActionWrite refers to writing (CRUD operations) an entity.
+	ActionWrite = "write"
+	// ActionWriteRole is a write to a user's global roles and teams.
 	ActionWriteRole = "write_role"
-	ActionRun       = "run"
+	// ActionRun is the action for running a live query.
+	ActionRun = "run"
 )
