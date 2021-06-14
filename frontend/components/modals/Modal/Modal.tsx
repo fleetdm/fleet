0708bceaa629c2cc31edfea4842658b83293ee51
Martavis Parker <martavis@auraticdevelopment.com>
2021-06-14 16:41:36 -0700

    fixes #952 - added esc key binding to modals

--- frontend/components/modals/Modal/Modal.tsx
+++ frontend/components/modals/Modal/Modal.tsx
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import classnames from "classnames";
+import { useEffect } from "react";
 
 const baseClass = "modal";
 
@@ -10,28 +11,39 @@ interface IModalProps {
   className?: string;
 }
 
-class Modal extends Component<IModalProps> {
-  render(): JSX.Element {
-    const { children, className, onExit, title } = this.props;
-    const modalContainerClassName = classnames(
-      `${baseClass}__modal_container`,
-      className
-    );
+const Modal = ({ children, onExit, title, className }: IModalProps) => {
+  useEffect(() => {
+    const closeWithEscapeKey = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onExit();
+      }
+    };
 
-    return (
-      <div className={`${baseClass}__background`}>
-        <div className={modalContainerClassName}>
-          <div className={`${baseClass}__header`}>
-            <span>{title}</span>
-            <div className={`${baseClass}__ex`}>
-              <button className="button button--unstyled" onClick={onExit} />
-            </div>
+    document.addEventListener("keydown", closeWithEscapeKey);
+    
+    return () => {
+      document.removeEventListener("keydown", closeWithEscapeKey);
+    };
+  }, []);
+  
+  const modalContainerClassName = classnames(
+    `${baseClass}__modal_container`,
+    className
+  );
+
+  return (
+    <div className={`${baseClass}__background`}>
+      <div className={modalContainerClassName}>
+        <div className={`${baseClass}__header`}>
+          <span>{title}</span>
+          <div className={`${baseClass}__ex`}>
+            <button className="button button--unstyled" onClick={onExit} />
           </div>
-          <div className={`${baseClass}__content`}>{children}</div>
         </div>
+        <div className={`${baseClass}__content`}>{children}</div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Modal;
