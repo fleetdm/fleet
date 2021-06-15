d060ca327ea5b0aab4d76f900cf683f38cf11aa3
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-06-15 15:32:43 -0400

    Better test names

--- cypress/integration/core/observer.spec.ts
+++ cypress/integration/core/observer.spec.ts
@@ -4,10 +4,15 @@ if (Cypress.env("FLEET_TIER") === "core") {
       cy.setup();
       cy.login();
       cy.seedCore();
+      cy.addDockerHost();
       cy.logout();
     });
 
-    it("Can perform the appropriate actions", () => {
+    afterEach(() => {
+      cy.stopDockerHost();
+    });
+
+    it("Can perform the appropriate core global observer actions", () => {
       cy.login("oliver@organization.com", "user123#");
       cy.visit("/");
 
