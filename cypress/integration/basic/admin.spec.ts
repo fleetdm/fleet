244d13ef65fe5ee79407eec79658b00d053be77d
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2021-06-15 10:42:09 -0500

    Refine Cypress tests for global admin role

--- cypress/integration/basic/admin.spec.ts
+++ cypress/integration/basic/admin.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable cypress/no-unnecessary-waiting */
 if (Cypress.env("FLEET_TIER") === "basic") {
   describe("Basic tier - Admin user", () => {
     beforeEach(() => {
@@ -26,13 +25,11 @@ if (Cypress.env("FLEET_TIER") === "basic") {
       cy.contains("button", /add new host/i).click();
 
       // See the “Select a team for this new host” in the Add new host modal. This modal appears after the user selects the “Add new host” button
-      cy.wait(1000);
       cy.get(".Select-control").click();
-      cy.wait(1000);
       cy.findByText(/no team/i).should("exist");
       // cy.findByText(/apples/i).should("exist");
       // cy.findByText(/oranges/i).should("exist");
-      // ^^TODO investigate why this dropdown is not being populated with the appropriate teams (i.e. Apples and Oranges) when Cypress attempts this test; note that issue only occurs during the automated test run and the dropdown is populated correctly if you comment out the assertions for apples and organges and manually navigate to the modal after the test run is finished
+      // ^^TODO add back these assertions after dropdown bug is fixed
 
       cy.contains("button", /done/i).click();
 
@@ -51,33 +48,26 @@ if (Cypress.env("FLEET_TIER") === "basic") {
       });
 
       // On the Packs pages (manage, new, and edit), they should…
-      // ^^TODO
+      // ^^General admin functionality for packs page is being tested in app/packflow.spec.ts
 
       // On the Settings pages, they should…
-      // See the “Teams” navigation item
+      // See the “Teams” navigation item and access the Settings - Teams page
       cy.visit("/settings/organization");
       cy.get(".react-tabs").within(() => {
         cy.findByText(/teams/i).click();
       });
+      // Access the Settings - Team details page
       cy.findByText(/apples/i).click();
       cy.findByText(/Add and remove members from Apples/i).should("exist");
 
-      cy.visit("/settings/organization");
-      cy.findByText(/teams/i).should("exist");
-
-      // Access the Settings - Teams page
-      // ^^ TODO
-
-      // Access the Settings - Team details page
-      // ^^ TODO
 
       // See the “Team” section in the create user modal. This modal is summoned when the “Create user” button is selected
+      cy.visit("/settings/organization");
       cy.get(".react-tabs").within(() => {
         cy.findByText(/users/i).click();
       });
       cy.findByRole("button", { name: /create user/i }).click();
-      // cy.findByText(/team/i).should("exist");
-      // ^^TODO add this test after bug is fixed
+      cy.findByText(/assign teams/i).should("exist");
     });
   });
 }
