88515422d0e64eb637efeccde724ec4679d6efa5
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-06-21 18:05:10 -0400

    Rendering all on hostdetailspage.jsx

--- frontend/pages/hosts/HostDetailsPage/HostDetailsPage.jsx
+++ frontend/pages/hosts/HostDetailsPage/HostDetailsPage.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router";
 import ReactTooltip from "react-tooltip";
 import { isEmpty, noop, pick, reduce } from "lodash";
 
+import FleetIcon from "components/icons/FleetIcon";
 import Spinner from "components/loaders/Spinner";
 import Button from "components/buttons/Button";
 import Modal from "components/modals/Modal";
@@ -334,6 +335,89 @@ export class HostDetailsPage extends Component {
     const { host } = this.props;
     const wrapperClassName = `${baseClass}__table`;
 
+    const softwarez = [
+      {
+        id: 1,
+        name: "Figma.app",
+        version: "4.2.0",
+        source: "apps",
+        generated_cpe: "",
+        vulnerabilities: [],
+      },
+      {
+        id: 2,
+        name: "Google Chrome.app",
+        version: "91.0.4472.101",
+        source: "apps",
+        generated_cpe: "cpe:2.3:a:google:chrome:91.0.4472.77:*:*:*:*:*:*:*",
+        vulnerabilities: [
+          {
+            cve: "CVE-2013-6662",
+            details_link: "https://nvd.nist.gov/vuln/detail/CVE-2013-6662",
+          },
+          {
+            cve: "CVE-2014-6662",
+            details_link: "https://nvd.nist.gov/vuln/detail/CVE-2014-6662",
+          },
+          {
+            cve: "CVE-2015-6662",
+            details_link: "https://nvd.nist.gov/vuln/detail/CVE-2015-6662",
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Make Believe.app",
+        version: "91.0.4472.101",
+        source: "apps",
+        generated_cpe: "cpe:2.3:a:google:chrome:91.0.4472.77:*:*:*:*:*:*:*",
+        vulnerabilities: [
+          {
+            cve: "CVE-2016-6662",
+            details_link: "https://nvd.nist.gov/vuln/detail/CVE-2016-6662",
+          },
+        ],
+      },
+    ];
+
+    let vulsList = [];
+
+    const vulnerabilitiesListMaker = (softwarezz) => {
+      softwarezz.forEach((software) => {
+        let softwareName = software.name;
+        software.vulnerabilities.forEach((vulnerability) => {
+          vulsList.push({
+            name: softwareName,
+            cve: vulnerability.cve,
+            details_link: vulnerability.details_link,
+          });
+        });
+      });
+    };
+
+    vulnerabilitiesListMaker(softwarez);
+
+    const renderVulsCount = (list) => {
+      if (list.length === 1) {
+        return "1 vulnerability detected";
+      }
+      if (list.length > 1) {
+        return `${list.length} vulnerabilities detected`;
+      }
+    };
+
+    const renderVul = (vul, index) => {
+      return (
+        <li key={index}>
+          Read more about{" "}
+          <a href={vul.details_link} target="_blank" rel="noopener noreferrer">
+            <em>{vul.name}</em> {vul.cve} vulnerability{" "}
+            <FleetIcon name="external-link" />
+          </a>
+        </li>
+      );
+    };
+
     return (
       <div className="section section--software">
         <p className="section__header">Software</p>
@@ -349,29 +433,51 @@ export class HostDetailsPage extends Component {
             </p>
           </div>
         ) : (
-          <div className={`${baseClass}__wrapper`}>
-            <table className={wrapperClassName}>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>Type</th>
-                  <th>Installed Version</th>
-                </tr>
-              </thead>
-              <tbody>
-                {!!host.software.length &&
-                  host.software.map((software) => {
-                    return (
-                      <SoftwareListRow
-                        key={`software-row-${software.id}`}
-                        software={software}
-                      />
-                    );
-                  })}
-              </tbody>
-            </table>
-          </div>
+          <>
+            <div className={`${baseClass}__vul-wrapper`}>
+              <div className={`${baseClass}__vul-count`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                >
+                  <path
+                    d="M0 8C0 12.4183 3.5817 16 8 16C12.4183 16 16 12.4183 16 8C16 3.5817 12.4183 0 8 0C3.5817 0 0 3.5817 0 8ZM14 8C14 11.3137 11.3137 14 8 14C4.6863 14 2 11.3137 2 8C2 4.6863 4.6863 2 8 2C11.3137 2 14 4.6863 14 8ZM7 12V10H9V12H7ZM7 4V9H9V4H7Z"
+                    fill="#8B8FA2"
+                  />
+                </svg>{" "}
+                {renderVulsCount(vulsList)}
+              </div>
+              <div className={`${baseClass}__vul-list`}>
+                <ul>{vulsList.map((vul, index) => renderVul(vul, index))}</ul>
+              </div>
+            </div>
+            <div className={`${baseClass}__wrapper`}>
+              <table className={wrapperClassName}>
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Type</th>
+                    <th>Installed Version</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {!!host.software.length &&
+                    host.software.map((software) => {
+                      return (
+                        <SoftwareListRow
+                          key={`software-row-${software.id}`}
+                          software={software}
+                        />
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+          </>
         )}
       </div>
     );
