faec76d7832c8ff333b5aae7fd60271e287c3658
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2021-06-23 16:25:59 -0700

    vulnerabilities array type
    Co-authored-by: Martavis Parker <47053705+martavis@users.noreply.github.com>

--- frontend/interfaces/software.ts
+++ frontend/interfaces/software.ts
@@ -5,11 +5,11 @@ export default PropTypes.shape({
   name: PropTypes.string,
   version: PropTypes.string,
   id: PropTypes.number,
-  vulnerabilities: PropTypes.shape({
+  vulnerabilities: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     uid: PropTypes.number,
     username: PropTypes.string,
     type: PropTypes.string,
     groupname: PropTypes.string,
-  }),
+  })),
 });
