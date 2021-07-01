e62f8b07dc4e9c1fb05e6617d5e91d7eb30c82f7
Zach Wasserman <zach@fleetdm.com>
2021-07-01 15:07:47 -0700

    Fix migration for MariaDB compatibility
    Fixes #1279

--- server/datastore/mysql/migrations/tables/20210601000008_TeamsEnrollSecrets.go
+++ server/datastore/mysql/migrations/tables/20210601000008_TeamsEnrollSecrets.go
@@ -15,7 +15,7 @@ func Up_20210601000008(tx *sql.Tx) error {
 	sql := `
 		ALTER TABLE enroll_secrets
 		ADD COLUMN team_id INT UNSIGNED,
-		ADD FOREIGN KEY fk_team_id (team_id) REFERENCES teams (id) ON DELETE CASCADE ON UPDATE CASCADE
+		ADD FOREIGN KEY fk_enroll_secrets_team_id (team_id) REFERENCES teams (id) ON DELETE CASCADE ON UPDATE CASCADE
 	`
 	if _, err := tx.Exec(sql); err != nil {
 		return errors.Wrap(err, "add team_id to enroll_secrets")
