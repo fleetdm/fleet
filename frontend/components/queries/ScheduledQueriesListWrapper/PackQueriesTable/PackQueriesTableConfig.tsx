470a46c6e67ac7e73276b8566c4b6b2bd5ade767
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-07-16 14:57:04 -0700

    Fix linting

--- frontend/components/queries/ScheduledQueriesListWrapper/PackQueriesTable/PackQueriesTableConfig.tsx
+++ frontend/components/queries/ScheduledQueriesListWrapper/PackQueriesTable/PackQueriesTableConfig.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/prop-types */
+// disable this rule as it was throwing an error in Header and Cell component
+// definitions for the selection row for some reason when we dont really need it.
 import React from "react";
 
 import Checkbox from "components/forms/fields/Checkbox";
