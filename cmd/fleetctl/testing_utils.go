9863b0f4bbf8f88afbda6c360e0e6a88b5da42e7
Tomas Touceda <chiiph@gmail.com>
2021-07-19 16:48:49 -0300

    Issue 1361 fleetctl teams (#1405)
    * WIP
    
    * Add get user_roles and apply for a user_roles spec to fleetctl
    
    * Uncomment other tests
    
    * Update test to check output
    
    * Update test with the new struct
    
    * Mock token so that it doesn't pick up the one in the local machine
    
    * Address review comments
    
    * Fix printJSON and printYaml
    
    * Fix merge conflict error
    
    * WIP
    
    * wip
    
    * wip
    
    * Finish implementation
    
    * Address review comments
    
    * Fix flaky test

--- cmd/fleetctl/testing_utils.go
+++ cmd/fleetctl/testing_utils.go
@@ -14,10 +14,14 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func runServerWithMockedDS(t *testing.T) (*httptest.Server, *mock.Store) {
+func runServerWithMockedDS(t *testing.T, opts ...service.TestServerOpts) (*httptest.Server, *mock.Store) {
 	ds := new(mock.Store)
 	var users []*fleet.User
+	var admin *fleet.User
 	ds.NewUserFunc = func(user *fleet.User) (*fleet.User, error) {
+		if user.GlobalRole != nil && *user.GlobalRole == fleet.RoleAdmin {
+			admin = user
+		}
 		users = append(users, user)
 		return user, nil
 	}
@@ -34,12 +38,12 @@ func runServerWithMockedDS(t *testing.T) (*httptest.Server, *mock.Store) {
 		return nil
 	}
 	ds.UserByIDFunc = func(id uint) (*fleet.User, error) {
-		return users[0], nil
+		return admin, nil
 	}
 	ds.ListUsersFunc = func(opt fleet.UserListOptions) ([]*fleet.User, error) {
 		return users, nil
 	}
-	_, server := service.RunServerForTestsWithDS(t, ds)
+	_, server := service.RunServerForTestsWithDS(t, ds, opts...)
 	os.Setenv("FLEET_SERVER_ADDRESS", server.URL)
 
 	return server, ds
