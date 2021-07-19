ccb54e338a44d075f0c22aca852929e5319c28ee
Tomas Touceda <chiiph@gmail.com>
2021-07-19 18:22:23 -0300

    Implement with interfaces instead of assertions

--- server/fleet/translator.go
+++ server/fleet/translator.go
@@ -22,6 +22,78 @@ type StringIdentifierToIDPayload struct {
 	ID         uint   `json:"id"`
 }
 
+type IDGetter interface {
+	GetID() (uint, error)
+}
+
+type EmailToUserIDTranslator struct {
+	email string
+	ds    Datastore
+}
+
+func NewEmailToUserIDTranslator(ds Datastore, email string) *EmailToUserIDTranslator {
+	return &EmailToUserIDTranslator{email: email, ds: ds}
+}
+
+func (e *EmailToUserIDTranslator) GetID() (uint, error) {
+	user, err := e.ds.UserByEmail(e.email)
+	if err != nil {
+		return 0, err
+	}
+	return user.ID, nil
+}
+
+type LabelNameToIDTranslator struct {
+	label string
+	ds    Datastore
+}
+
+func NewLabelNameToIDTranslator(ds Datastore, label string) *LabelNameToIDTranslator {
+	return &LabelNameToIDTranslator{label: label, ds: ds}
+}
+
+func (e *LabelNameToIDTranslator) GetID() (uint, error) {
+	labelIDs, err := e.ds.LabelIDsByName([]string{e.label})
+	if err != nil {
+		return 0, err
+	}
+	return labelIDs[0], nil
+}
+
+type TeamNameToIDTranslator struct {
+	team string
+	ds   Datastore
+}
+
+func NewTeamNameToIDTranslator(ds Datastore, team string) *TeamNameToIDTranslator {
+	return &TeamNameToIDTranslator{team: team, ds: ds}
+}
+
+func (e *TeamNameToIDTranslator) GetID() (uint, error) {
+	team, err := e.ds.TeamByName(e.team)
+	if err != nil {
+		return 0, err
+	}
+	return team.ID, nil
+}
+
+type HostIdentifierToIDTranslator struct {
+	host string
+	ds   Datastore
+}
+
+func NewHostIdentifierToIDTranslator(ds Datastore, host string) *HostIdentifierToIDTranslator {
+	return &HostIdentifierToIDTranslator{host: host, ds: ds}
+}
+
+func (e *HostIdentifierToIDTranslator) GetID() (uint, error) {
+	host, err := e.ds.HostByIdentifier(e.host)
+	if err != nil {
+		return 0, err
+	}
+	return host.ID, nil
+}
+
 type TranslatorService interface {
 	Translate(ctx context.Context, payloads []TranslatePayload) ([]TranslatePayload, error)
 }
--- server/service/translator.go
+++ server/service/translator.go
@@ -46,42 +46,32 @@ func (svc Service) Translate(ctx context.Context, payloads []fleet.TranslatePayl
 	var finalPayload []fleet.TranslatePayload
 
 	for _, payload := range payloads {
-		var toIDFunc func(string) (interface{}, error)
-		var idExtractorFunc func(interface{}) uint
-
-		switch payload.Type {
-		case fleet.TranslatorTypeUserEmail:
-			toIDFunc = func(email string) (interface{}, error) { return svc.ds.UserByEmail(email) }
-			idExtractorFunc = func(item interface{}) uint { return item.(*fleet.User).ID }
-		case fleet.TranslatorTypeLabel:
-			toIDFunc = func(name string) (interface{}, error) {
-				labelIDs, err := svc.ds.LabelIDsByName([]string{name})
-				if err != nil {
-					return nil, err
-				}
-				return labelIDs[0], nil
-			}
-			idExtractorFunc = func(item interface{}) uint { return item.(uint) }
-		case fleet.TranslatorTypeTeam:
-			toIDFunc = func(name string) (interface{}, error) { return svc.ds.TeamByName(name) }
-			idExtractorFunc = func(item interface{}) uint { return item.(*fleet.Team).ID }
-		case fleet.TranslatorTypeHost:
-			toIDFunc = func(name string) (interface{}, error) { return svc.ds.HostByIdentifier(name) }
-			idExtractorFunc = func(item interface{}) uint { return item.(*fleet.Host).ID }
-		default:
-			return nil, fleet.NewErrorf(fleet.ErrNoUnknownTranslate, "Type %s is unknown.", payload.Type)
-		}
+		var idgetter fleet.IDGetter
 
 		toId := fleet.StringIdentifierToIDPayload{}
 		err := json.Unmarshal(payload.Payload, &toId)
 		if err != nil {
 			return nil, err
 		}
-		user, err := toIDFunc(toId.Identifier)
+
+		switch payload.Type {
+		case fleet.TranslatorTypeUserEmail:
+			idgetter = fleet.NewEmailToUserIDTranslator(svc.ds, toId.Identifier)
+		case fleet.TranslatorTypeLabel:
+			idgetter = fleet.NewLabelNameToIDTranslator(svc.ds, toId.Identifier)
+		case fleet.TranslatorTypeTeam:
+			idgetter = fleet.NewTeamNameToIDTranslator(svc.ds, toId.Identifier)
+		case fleet.TranslatorTypeHost:
+			idgetter = fleet.NewHostIdentifierToIDTranslator(svc.ds, toId.Identifier)
+		default:
+			return nil, fleet.NewErrorf(fleet.ErrNoUnknownTranslate, "Type %s is unknown.", payload.Type)
+		}
+
+		id, err := idgetter.GetID()
 		if err != nil {
 			return nil, err
 		}
-		toId.ID = idExtractorFunc(user)
+		toId.ID = id
 		newPayload, err := repackageNewPayload(toId, payload)
 		if err != nil {
 			return nil, err
