af18173cffa721119effeda39e4b1b8e8e7b78a2
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2021-07-22 15:49:09 -0500

    Add more typings; refactor template strings

--- frontend/pages/Homepage/ActivityFeed/ActivityFeed.tsx
+++ frontend/pages/Homepage/ActivityFeed/ActivityFeed.tsx
@@ -5,13 +5,37 @@ import moment from "moment";
 import Fleet from "fleet";
 import { find, lowerCase } from "lodash";
 
-import { IActivity } from "interfaces/activity";
+import { IActivity, ActivityType } from "interfaces/activity";
 
 import Avatar from "components/Avatar";
 import Button from "components/buttons/Button";
 import Spinner from "components/loaders/Spinner";
 
 const baseClass = "activity-feed";
+
+const DEFAULT_GRAVATAR_URL =
+  "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=blank&size=200";
+
+const TAGGED_TEMPLATES = {
+  liveQueryActivityTemplate: (activity: IActivity) => {
+    const count = activity.details?.targets_count;
+    return typeof count === undefined
+      ? "ran a live query"
+      : `ran a live query on ${count} ${count === 1 ? "host" : "hosts"}`;
+  },
+  defaultActivityTemplate: (activity: IActivity) => {
+    const entityName = find(activity.details, (_, key) =>
+      key.includes("_name")
+    );
+    return !entityName ? (
+      `${lowerCase(activity.type)}`
+    ) : (
+      <span>
+        {lowerCase(activity.type)} <b>{entityName}</b>
+      </span>
+    );
+  },
+};
 
 const ActivityFeed = (): JSX.Element => {
   const [activities, setActivities] = useState([]);
@@ -39,28 +63,14 @@ const ActivityFeed = (): JSX.Element => {
   };
 
   const getDetail = (activity: IActivity) => {
-    const { type, details } = activity;
-    const entityName = find(details, (_, key) => key.includes("_name"));
-
-    if (type === "live_query") {
-      return `ran a live query on ${details?.targets_count} hosts`;
+    if (activity.type === ActivityType.LiveQuery) {
+      return TAGGED_TEMPLATES.liveQueryActivityTemplate(activity);
     }
-
-    if (entityName) {
-      return (
-        <span>
-          {lowerCase(type)} <b>{entityName}</b>
-        </span>
-      );
-    }
-
-    return lowerCase(type);
+    return TAGGED_TEMPLATES.defaultActivityTemplate(activity);
   };
 
   const renderActivityBlock = (activity: IActivity, i: number) => {
-    const gravatarURL =
-      activity.actor_gravatar ||
-      "https://www.gravatar.com/avatar/00000000000000000000000000000000?d=blank&size=200";
+    const gravatarURL = activity.actor_gravatar || DEFAULT_GRAVATAR_URL;
     return (
       <div className={`${baseClass}__block`} key={i}>
         <Avatar
