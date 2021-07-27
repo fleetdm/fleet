a188d030157514521b4371bb96f36f7fe9467e50
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2021-07-27 18:04:43 -0400

    Homepage: Refactor platform counts in host summary (#1490)
    Co-authored by: @gillespi314

--- frontend/interfaces/label.ts
+++ frontend/interfaces/label.ts
@@ -1,15 +1,34 @@
-import PropTypes from "prop-types";
+import PropTypes, { string } from "prop-types";
 
 export default PropTypes.shape({
+  created_at: PropTypes.string,
+  description: PropTypes.string,
+  display_text: PropTypes.string,
   hosts_count: PropTypes.number,
+  host_ids: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+  ),
   id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-  title: PropTypes.string,
-  type: PropTypes.string,
+  name: PropTypes.string,
+  label_type: PropTypes.string,
+  title: PropTypes.string, // confirm on rest api doc
+  type: PropTypes.string, // confirm on rest api doc
+  count: PropTypes.number, // confirm on rest api doc
 });
 
 export interface ILabel {
+  created_at: string;
+  description: string;
+  display_text: string;
   hosts_count: number;
+  host_ids: number[] | null;
   id: number | string;
-  title: string;
-  type: string;
+  label_membership_type: string;
+  label_type: string;
+  name: string;
+  query: string;
+  updated_at: string;
+  title: string; // confirm on rest api doc
+  type: string; // confirm on rest api doc
+  count: number;
 }
