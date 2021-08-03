ab184e1c625a5d535dc2e076b7981087f8168d51
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2021-08-03 10:15:49 -0500

    Change DataTable conditions to render selections

--- frontend/components/TableContainer/DataTable/DataTable.tsx
+++ frontend/components/TableContainer/DataTable/DataTable.tsx
@@ -237,7 +237,7 @@ const DataTable = ({
                       {primarySelectActionButtonText &&
                         renderPrimarySelectAction()}
                     </div>
-                    {renderAreAllSelected()}
+                    {toggleAllPagesSelected && renderAreAllSelected()}
                     {shouldRenderToggleAllPages && (
                       <Button
                         onClick={onToggleAllPagesClick}
