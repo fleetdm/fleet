cab2732d0bfab9c07b88c3308d133c1bb223ca1d
Zach Wasserman <zach@fleetdm.com>
2021-08-02 18:34:37 -0700

    Update usage of os.IsNotExist
    Per [godoc](https://pkg.go.dev/os#IsNotExist), this is the preferred method.

--- server/vulnerabilities/db.go
+++ server/vulnerabilities/db.go
@@ -1,6 +1,7 @@
 package vulnerabilities
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -73,7 +74,7 @@ const batchSize = 800
 
 func GenerateCPEDB(path string, items *cpedict.CPEList) error {
 	err := os.Remove(path)
-	if err != nil && !os.IsNotExist(err) {
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
 		return err
 	}
 	db, err := CPEDB(path)
