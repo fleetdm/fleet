b92c6b519c2e75bae1de4ea3c8787a5117324b85
Mike McNeil <mikermcneil@users.noreply.github.com>
2021-08-06 02:14:31 -0500

    Add stub of webhook handler for GitHub bot (at this point, a mostly-verbatim copy paste of sailsbot -- will work backwards from this to what we actually need in Fleet)

--- website/config/routes.js
+++ website/config/routes.js
@@ -63,7 +63,7 @@ module.exports.routes = {
   //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
   //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
   'POST /api/v1/webhooks/receive-usage-analytics': { action: 'webhooks/receive-usage-analytics', csrf: false },
-
+  '/api/v1/webhooks/github': { action: 'webhooks/receive-from-github', csrf: false },
 
 
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
