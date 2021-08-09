8e6f04d7c57f3c7f80b39e180770c332e68be9e9
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2021-08-09 12:38:13 -0400

    Spiffier buttons in UI (#1583)

--- frontend/pages/hosts/HostDetailsPage/HostDetailsPage.jsx
+++ frontend/pages/hosts/HostDetailsPage/HostDetailsPage.jsx
@@ -55,6 +55,9 @@ import {
 import EmptySoftware from "./EmptySoftware";
 
 import BackChevron from "../../../../assets/images/icon-chevron-down-9x6@2x.png";
+import DeleteIcon from "../../../../assets/images/icon-action-delete-14x14@2x.png";
+import TransferIcon from "../../../../assets/images/icon-action-transfer-16x16@2x.png";
+import QueryIcon from "../../../../assets/images/icon-action-query-16x16@2x.png";
 
 const baseClass = "host-details";
 
@@ -286,7 +289,7 @@ export class HostDetailsPage extends Component {
           <Button onClick={onDestroyHost} variant="alert">
             Delete
           </Button>
-          <Button onClick={toggleDeleteHostModal(null)} variant="inverse">
+          <Button onClick={toggleDeleteHostModal(null)} variant="inverse-alert">
             Cancel
           </Button>
         </div>
@@ -318,7 +321,7 @@ export class HostDetailsPage extends Component {
             variant="inverse"
             className={`${baseClass}__transfer-button`}
           >
-            Transfer
+            Transfer <img src={TransferIcon} alt="Transfer host icon" />
           </Button>
         )}
         <div data-tip data-for="query" data-tip-disable={isOnline}>
@@ -328,7 +331,7 @@ export class HostDetailsPage extends Component {
             disabled={isOffline}
             className={`${baseClass}__query-button`}
           >
-            Query
+            Query <img src={QueryIcon} alt="Query host icon" />
           </Button>
         </div>
         <ReactTooltip
@@ -342,8 +345,8 @@ export class HostDetailsPage extends Component {
             You can’t query <br /> an offline host.
           </span>
         </ReactTooltip>
-        <Button onClick={toggleDeleteHostModal()} variant="active">
-          Delete
+        <Button onClick={toggleDeleteHostModal()} variant="inverse">
+          Delete <img src={DeleteIcon} alt="Delete host icon" />
         </Button>
       </div>
     );
