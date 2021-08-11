96823c897c9a6bd3de19be19f26f7128e11f54af
Tomas Touceda <chiiph@gmail.com>
2021-08-11 12:52:20 -0300

    Finally fix tests

--- server/service/service_appconfig.go
+++ server/service/service_appconfig.go
@@ -190,9 +190,7 @@ func appConfigFromAppConfigPayload(p fleet.AppConfigPayload, config fleet.AppCon
 	}
 
 	if settings := p.HostSettings; settings != nil {
-		if settings.AdditionalQueries != nil {
-			config.AdditionalQueries = settings.AdditionalQueries
-		}
+		config.AdditionalQueries = settings.AdditionalQueries
 		if settings.EnableHostUsers != nil {
 			config.EnableHostUsers = *settings.EnableHostUsers
 		}
