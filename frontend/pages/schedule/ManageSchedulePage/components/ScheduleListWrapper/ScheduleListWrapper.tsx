fed6e875fac029eaed97ec9cfc4db1764460f1da
Noah Talerman <noahtal@umich.edu>
2021-08-16 10:26:45 -0400

    Disable pagination on the Schedule page

--- frontend/pages/schedule/ManageSchedulePage/components/ScheduleListWrapper/ScheduleListWrapper.tsx
+++ frontend/pages/schedule/ManageSchedulePage/components/ScheduleListWrapper/ScheduleListWrapper.tsx
@@ -137,6 +137,7 @@ const ScheduleListWrapper = (props: IScheduleListWrapperProps): JSX.Element => {
         onQueryChange={onQueryChange}
         inputPlaceHolder="Search"
         searchable={false}
+        disablePagination
         onPrimarySelectActionClick={onRemoveScheduledQueryClick}
         primarySelectActionButtonVariant="text-icon"
         primarySelectActionButtonIcon="close"
