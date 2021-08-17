51c27e07d0f6e4bc20726e0a3edae79a4e247045
Tomas Touceda <chiiph@gmail.com>
2021-08-17 15:22:56 -0300

    Add data migration

---
+++ server/datastore/mysql/migrations/data/20210817112308_MigrateAppConfig.go
@@ -0,0 +1,109 @@
+package data
+
+import (
+	"database/sql"
+	"encoding/json"
+
+	"github.com/fleetdm/fleet/v4/server/datastore/mysql/migrations/tables"
+	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/pkg/errors"
+)
+
+func init() {
+	tables.MigrationClient.AddMigration(Up_20210817112308, Down_20210817112308)
+}
+
+func Up_20210817112308(tx *sql.Tx) error {
+	row := tx.QueryRow(
+		`SELECT 
+       			org_name,
+				org_logo_url,
+				server_url,
+				smtp_configured,
+				smtp_sender_address,
+				smtp_server,
+				smtp_port,
+				smtp_authentication_type,
+				smtp_enable_ssl_tls,
+				smtp_authentication_method,
+				smtp_domain,
+				smtp_user_name,
+				smtp_password,
+				smtp_verify_ssl_certs,
+				smtp_enable_start_tls,
+				entity_id,
+				issuer_uri,
+				idp_image_url,
+				metadata,
+				metadata_url,
+				idp_name,
+				enable_sso,
+				host_expiry_enabled,
+				host_expiry_window,
+				live_query_disabled,
+				additional_queries,
+				enable_sso_idp_login,
+				agent_options,
+				enable_analytics,
+				vulnerability_databases_path,
+				enable_host_users,
+				enable_software_inventory
+				FROM app_configs LIMIT 1`,
+	)
+	config := &fleet.AppConfig{}
+	var vulnPath *string
+	err := row.Scan(
+		&config.OrgInfo.OrgName,
+		&config.OrgInfo.OrgLogoURL,
+		&config.ServerSettings.ServerURL,
+		&config.SMTPSettings.SMTPConfigured,
+		&config.SMTPSettings.SMTPSenderAddress,
+		&config.SMTPSettings.SMTPServer,
+		&config.SMTPSettings.SMTPPort,
+		&config.SMTPSettings.SMTPAuthenticationType,
+		&config.SMTPSettings.SMTPEnableTLS,
+		&config.SMTPSettings.SMTPAuthenticationMethod,
+		&config.SMTPSettings.SMTPDomain,
+		&config.SMTPSettings.SMTPUserName,
+		&config.SMTPSettings.SMTPPassword,
+		&config.SMTPSettings.SMTPVerifySSLCerts,
+		&config.SMTPSettings.SMTPEnableStartTLS,
+		&config.SSOSettings.EntityID,
+		&config.SSOSettings.IssuerURI,
+		&config.SSOSettings.IDPImageURL,
+		&config.SSOSettings.Metadata,
+		&config.SSOSettings.MetadataURL,
+		&config.SSOSettings.IDPName,
+		&config.SSOSettings.EnableSSO,
+		&config.HostExpirySettings.HostExpiryEnabled,
+		&config.HostExpirySettings.HostExpiryWindow,
+		&config.ServerSettings.LiveQueryDisabled,
+		&config.HostSettings.AdditionalQueries,
+		&config.SSOSettings.EnableSSOIdPLogin,
+		&config.AgentOptions,
+		&config.ServerSettings.EnableAnalytics,
+		&vulnPath,
+		&config.HostSettings.EnableHostUsers,
+		&config.HostSettings.EnableSoftwareInventory,
+	)
+	if err != nil {
+		return errors.Wrap(err, "scanning config row")
+	}
+	if vulnPath != nil {
+		config.VulnerabilitySettings.DatabasesPath = *vulnPath
+	}
+
+	configBytes, err := json.Marshal(config)
+	if err != nil {
+		return errors.Wrap(err, "marshaling config")
+	}
+	_, err = tx.Exec(
+		`INSERT INTO kv_json(json_key, json_value) VALUES(?, ?) ON DUPLICATE KEY UPDATE json_value = VALUES(json_value)`,
+		"config", configBytes,
+	)
+	return nil
+}
+
+func Down_20210817112308(tx *sql.Tx) error {
+	return nil
+}
