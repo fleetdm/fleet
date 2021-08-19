91f12a7fa13b7a06bb79cbd5900dcdc05f01db9b
Zach Wasserman <zach@fleetdm.com>
2021-08-18 18:03:25 -0700

    Improve hiding of UI flash messages
    Use the React hooks for modifying state rather than operating directly
    on the DOM for hiding of the flash messages.

--- frontend/components/flash_messages/FlashMessage/FlashMessage.jsx
+++ frontend/components/flash_messages/FlashMessage/FlashMessage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 
@@ -21,18 +21,27 @@ const FlashMessage = ({
     [`${baseClass}--full-width`]: fullWidth,
   });
 
-  useEffect(() => {
-    if (alertType === "success" && isVisible) {
-      setTimeout(function () {
-        document.getElementById(`${klass}`).style.visibility = "visible";
-      }, 0); // Ensures successive, success alerts are visible
-      setTimeout(function () {
-        document.getElementById(`${klass}`).style.visibility = "hidden";
-      }, 4000); // Hides success alerts after 4 seconds
-    }
-  });
+  const [hide, setHide] = useState(false);
 
-  if (!isVisible) {
+  // This useEffect handles hiding successful flash messages after a 4s timeout. By putting the
+  // notification in the dependency array, we can properly reset whenever a new flash message comes through.
+  useEffect(() => {
+    // Any time this hook runs, we reset the hide to false (so that subsequent messages that will be
+    // using this same component instance will be visible).
+    setHide(false);
+
+    if (alertType === "success" && isVisible) {
+      // After 4 seconds, set hide to true.
+      const timer = setTimeout(() => setHide(true), 4000);
+      // Return a cleanup function that will clear this reset, in case another render happens
+      // after this. We want that render to set a new timeout (if needed).
+      return () => clearTimeout(timer);
+    }
+
+    return undefined; // No cleanup when we don't set a timeout.
+  }, [notification, alertType, isVisible, setHide]);
+
+  if (hide || !isVisible) {
     return false;
   }
 
