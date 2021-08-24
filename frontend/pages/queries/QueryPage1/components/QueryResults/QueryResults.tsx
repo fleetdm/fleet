e5b1fd20e51c732a160705eaaeb732de8e081196
Martavis Parker <martavis@auraticdevelopment.com>
2021-08-24 10:25:11 -0700

    syntax fixes

--- frontend/pages/queries/QueryPage1/components/QueryResults/QueryResults.tsx
+++ frontend/pages/queries/QueryPage1/components/QueryResults/QueryResults.tsx
@@ -5,12 +5,12 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import moment from "moment";
 import classnames from "classnames";
 import FileSaver from "file-saver";
-import { filter, forEach, get, keys, omit } from "lodash";
+import { filter, get, keys, omit } from "lodash";
 
 import PATHS from "router/paths"; // @ts-ignore
 import convertToCSV from "utilities/convert_to_csv"; // @ts-ignore
 import filterArrayByHash from "utilities/filter_array_by_hash";
-import { ICampaign, ICampaignQueryResult } from "interfaces/campaign";
+import { ICampaign } from "interfaces/campaign";
 
 import Button from "components/buttons/Button"; // @ts-ignore
 import FleetIcon from "components/icons/FleetIcon"; // @ts-ignore
@@ -59,7 +59,9 @@ const QueryResults = ({
 
   const [pageTitle, setPageTitle] = useState<string>(PAGE_TITLES.RUNNING);
   const [csvQueryName, setCsvQueryName] = useState<string>("Query Results");
-  const [resultsFilter, setResultsFilter] = useState<{[key: string]: any}>({});
+  const [resultsFilter, setResultsFilter] = useState<{ [key: string]: any }>(
+    {}
+  );
   const [activeColumn, setActiveColumn] = useState<string>("");
   const [navTabIndex, setNavTabIndex] = useState(0);
 
@@ -80,113 +82,6 @@ const QueryResults = ({
 
       return false;
     };
-  };
-
-  const renderTableHeaderColumn = (column: string, index: number) => {
-    const filterable = column === "hostname" ? "host_hostname" : column;
-    const filterIconClassName = classnames(`${baseClass}__filter-icon`, {
-      [`${baseClass}__filter-icon--is-active`]: activeColumn === column,
-    });
-
-    return (
-      <th key={`query-results-table-header-${index}`}>
-        <span>
-          <FleetIcon className={filterIconClassName} name="filter" />
-          {column}
-        </span>
-        <InputField
-          name={column}
-          onChange={onFilterAttribute(filterable)}
-          onFocus={() => setActiveColumn(column)}
-          value={resultsFilter[filterable]}
-        />
-      </th>
-    );
-  };
-
-  const renderTableHeaderRow = (rows: any) => {
-    if (!rows) {
-      return false;
-    }
-    
-    const queryAttrs = omit(rows[0], ["host_hostname"]);
-    const queryResultColumns = keys(queryAttrs);
-
-    return (
-      <tr>
-        {renderTableHeaderColumn("hostname", -1)}
-        {queryResultColumns.map((column, i) => {
-          return renderTableHeaderColumn(column, i);
-        })}
-      </tr>
-    );
-  };
-
-  const renderTableRows = (rows: any) => {
-    const filteredRows = filterArrayByHash(rows, resultsFilter);
-
-    return filteredRows.map((row: any) => {
-      return <QueryResultsRow queryResult={row} />;
-    });
-  };
-
-  const renderTable = () => {
-    // const { queryIsRunning, campaign } = this.props;
-    // const { query_results: queryResults } = campaign;
-    // const loading = queryIsRunning && (!queryResults || !queryResults.length);
-
-    // if (loading) {
-    //   return <Spinner />;
-    // }
-
-    return (
-      <div className={`${baseClass}__results-table-container`}>
-        <Button
-          className={`${baseClass}__export-btn`}
-          onClick={onExportQueryResults}
-          variant="text-link"
-        >
-          Export results
-        </Button>
-        <div className={`${baseClass}__results-table-wrapper`}>
-          <table className={`${baseClass}__table`}>
-            <thead>{renderTableHeaderRow(queryResults)}</thead>
-            <tbody>{renderTableRows(queryResults)}</tbody>
-          </table>
-        </div>
-      </div>
-    );
-  };
-
-  const renderErrorsTable = () => {
-    // const { renderTableHeaderRow, renderTableRows } = this;
-
-    // const { queryIsRunning, campaign } = this.props;
-    // const { errors } = campaign;
-
-    // const loading = queryIsRunning && (!errors || !errors.length);
-
-    // if (loading) {
-    //   return <Spinner />;
-    // }
-
-    return (
-      <div className={`${baseClass}__error-table-container`}>
-        <Button
-          className={`${baseClass}__export-btn`}
-          onClick={onExportErrorsResults}
-          variant="text-link"
-        >
-          Export errors
-        </Button>
-        <div className={`${baseClass}__error-table-wrapper`}>
-          <table className={`${baseClass}__table`}>
-            <thead>{renderTableHeaderRow(errors)}</thead>
-            <tbody>{renderTableRows(errors)}</tbody>
-          </table>
-        </div>
-      </div>
-    );
   };
 
   const onExportQueryResults = (evt: React.MouseEvent<HTMLButtonElement>) => {
@@ -231,7 +126,95 @@ const QueryResults = ({
     }
   };
 
-  const doneAndRunQueryBtns = (
+  const renderTableHeaderColumn = (column: string, index: number) => {
+    const filterable = column === "hostname" ? "host_hostname" : column;
+    const filterIconClassName = classnames(`${baseClass}__filter-icon`, {
+      [`${baseClass}__filter-icon--is-active`]: activeColumn === column,
+    });
+
+    return (
+      <th key={`query-results-table-header-${index}`}>
+        <span>
+          <FleetIcon className={filterIconClassName} name="filter" />
+          {column}
+        </span>
+        <InputField
+          name={column}
+          onChange={onFilterAttribute(filterable)}
+          onFocus={() => setActiveColumn(column)}
+          value={resultsFilter[filterable]}
+        />
+      </th>
+    );
+  };
+
+  const renderTableHeaderRow = (rows: any) => {
+    if (!rows) {
+      return false;
+    }
+
+    const queryAttrs = omit(rows[0], ["host_hostname"]);
+    const queryResultColumns = keys(queryAttrs);
+
+    return (
+      <tr>
+        {renderTableHeaderColumn("hostname", -1)}
+        {queryResultColumns.map((column, i) => {
+          return renderTableHeaderColumn(column, i);
+        })}
+      </tr>
+    );
+  };
+
+  const renderTableRows = (rows: any) => {
+    const filteredRows = filterArrayByHash(rows, resultsFilter);
+
+    return filteredRows.map((row: any) => {
+      return <QueryResultsRow queryResult={row} />;
+    });
+  };
+
+  const renderTable = () => {
+    return (
+      <div className={`${baseClass}__results-table-container`}>
+        <Button
+          className={`${baseClass}__export-btn`}
+          onClick={onExportQueryResults}
+          variant="text-link"
+        >
+          Export results
+        </Button>
+        <div className={`${baseClass}__results-table-wrapper`}>
+          <table className={`${baseClass}__table`}>
+            <thead>{renderTableHeaderRow(queryResults)}</thead>
+            <tbody>{renderTableRows(queryResults)}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  };
+
+  const renderErrorsTable = () => {
+    return (
+      <div className={`${baseClass}__error-table-container`}>
+        <Button
+          className={`${baseClass}__export-btn`}
+          onClick={onExportErrorsResults}
+          variant="text-link"
+        >
+          Export errors
+        </Button>
+        <div className={`${baseClass}__error-table-wrapper`}>
+          <table className={`${baseClass}__table`}>
+            <thead>{renderTableHeaderRow(errors)}</thead>
+            <tbody>{renderTableRows(errors)}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  };
+
+  const renderFinishedButtons = () => (
     <div className={`${baseClass}__btn-wrapper`}>
       <Button
         className={`${baseClass}__done-btn`}
@@ -250,7 +233,7 @@ const QueryResults = ({
     </div>
   );
 
-  const stopQueryBtn = (
+  const renderStopQueryButton = () => (
     <div className={`${baseClass}__btn-wrapper`}>
       <Button
         className={`${baseClass}__stop-btn`}
@@ -262,7 +245,9 @@ const QueryResults = ({
     </div>
   );
 
-  const hasNoResults = isQueryFinished && (!hostsCount.successful || !queryResults || !queryResults.length);
+  const hasNoResults =
+    isQueryFinished &&
+    (!hostsCount.successful || !queryResults || !queryResults.length);
   return (
     <div className={baseClass}>
       <div className={`${baseClass}__wrapper`}>
@@ -279,14 +264,12 @@ const QueryResults = ({
           </span>
         </div>
       </div>
-      {isQueryFinished ? doneAndRunQueryBtns : stopQueryBtn}
+      {isQueryFinished ? renderFinishedButtons : renderStopQueryButton}
       {isQueryFinished && (
         <div className={`${baseClass}__nav-header`}>
           <Tabs selectedIndex={navTabIndex} onSelect={(i) => setNavTabIndex(i)}>
             <TabList>
-              <Tab data-text={NAV_TITLES.RESULTS}>
-                {NAV_TITLES.RESULTS}
-              </Tab>
+              <Tab data-text={NAV_TITLES.RESULTS}>{NAV_TITLES.RESULTS}</Tab>
               <Tab data-text={NAV_TITLES.ERRORS} disabled={!errors?.length}>
                 <span>{errors?.length}</span>
                 {NAV_TITLES.ERRORS}
@@ -298,15 +281,15 @@ const QueryResults = ({
                   Your live query returned no results.
                   <br />
                   <span>
-                    Expecting to see results? Check to see if the hosts you targeted reported &ldquo;Online&rdquo; or check out the &ldquo;Errors&rdquo; table.
+                    Expecting to see results? Check to see if the hosts you
+                    targeted reported &ldquo;Online&rdquo; or check out the
+                    &ldquo;Errors&rdquo; table.
                   </span>
                 </p>
               )}
               {!hasNoResults && renderTable()}
             </TabPanel>
-            <TabPanel>
-              {renderErrorsTable()}
-            </TabPanel>
+            <TabPanel>{renderErrorsTable()}</TabPanel>
           </Tabs>
         </div>
       )}
