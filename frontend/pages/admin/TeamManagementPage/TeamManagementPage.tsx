5cf05fb41d33d696cf4ac00fbee0848d2a6817d7
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-08-31 18:30:09 -0400

    Error and Empty States part 3 (Teams page)

--- frontend/pages/admin/TeamManagementPage/TeamManagementPage.tsx
+++ frontend/pages/admin/TeamManagementPage/TeamManagementPage.tsx
@@ -6,24 +6,29 @@ import teamActions from "redux/nodes/entities/teams/actions";
 // ignore TS error for now until these are rewritten in ts.
 // @ts-ignore
 import { renderFlash } from "redux/nodes/notifications/actions";
+import Button from "components/buttons/Button";
+// @ts-ignore
+import FleetIcon from "components/icons/FleetIcon";
 import TableContainer from "components/TableContainer";
 
+import TeamsError from "./components/TeamsError";
 import CreateTeamModal from "./components/CreateTeamModal";
 import DeleteTeamModal from "./components/DeleteTeamModal";
 import EditTeamModal from "./components/EditTeamModal";
-import EmptyTeams from "./components/EmptyTeams";
 import { ICreateTeamFormData } from "./components/CreateTeamModal/CreateTeamModal";
 import { IEditTeamFormData } from "./components/EditTeamModal/EditTeamModal";
 import { generateTableHeaders, generateDataSet } from "./TeamTableConfig";
 
 const baseClass = "team-management";
+const noTeamsClass = "no-teams";
 
 // TODO: should probably live close to the store.js file and imported in.
-interface RootState {
+interface IRootState {
   entities: {
     teams: {
       isLoading: boolean;
       data: { [id: number]: ITeam };
+      errors: any;
     };
   };
 }
@@ -46,6 +51,7 @@ const TeamManagementPage = (): JSX.Element => {
   const [showDeleteTeamModal, setShowDeleteTeamModal] = useState(false);
   const [showEditTeamModal, setShowEditTeamModal] = useState(false);
   const [teamEditing, setTeamEditing] = useState<ITeam>();
+  const [searchString, setSearchString] = useState<string>("");
 
   const toggleCreateTeamModal = useCallback(() => {
     setShowCreateTeamModal(!showCreateTeamModal);
@@ -70,6 +76,7 @@ const TeamManagementPage = (): JSX.Element => {
   // NOTE: called once on the initial render of this component.
   const onQueryChange = useCallback(
     (queryData) => {
+      setSearchString(queryData.searchQuery);
       const { pageIndex, pageSize, searchQuery } = queryData;
       dispatch(
         teamActions.loadAll({
@@ -79,7 +86,7 @@ const TeamManagementPage = (): JSX.Element => {
         })
       );
     },
-    [dispatch]
+    [dispatch, setSearchString]
   );
 
   const onCreateSubmit = useCallback(
@@ -160,12 +167,62 @@ const TeamManagementPage = (): JSX.Element => {
     }
   };
 
+  const NoTeamsComponent = useCallback(() => {
+    return (
+      <div className={`${noTeamsClass}`}>
+        <div className={`${noTeamsClass}__inner`}>
+          <div className={`${noTeamsClass}__inner-text`}>
+            {searchString === "" ? (
+              <>
+                <h1>Set up team permissions</h1>
+                <p>
+                  Keep your organization organized and efficient by ensuring
+                  every user has the correct access to the right hosts.
+                </p>
+                <p>
+                  Want to learn more?&nbsp;
+                  <a
+                    href="https://github.com/fleetdm/fleet/tree/master/docs/1-Using-Fleet/role-based-access-control-and-teams.md"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Read about teams&nbsp;
+                    <FleetIcon name="external-link" />
+                  </a>
+                </p>
+                <Button
+                  variant="brand"
+                  className={`${noTeamsClass}__create-button`}
+                  onClick={toggleCreateTeamModal}
+                >
+                  Create Team
+                </Button>
+              </>
+            ) : (
+              <>
+                <h2>No members match the current search criteria.</h2>
+                <p>
+                  Expecting to see members? Try again in a few seconds as the
+                  system catches up.
+                </p>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }, [searchString]);
+
   const tableHeaders = generateTableHeaders(onActionSelection);
   const loadingTableData = useSelector(
-    (state: RootState) => state.entities.teams.isLoading
+    (state: IRootState) => state.entities.teams.isLoading
   );
-  const teams = useSelector((state: RootState) =>
+  const teams = useSelector((state: IRootState) =>
     generateDataSet(state.entities.teams.data)
+  );
+
+  const teamsError = useSelector(
+    (state: IRootState) => state.entities.teams.errors
   );
 
   return (
@@ -173,23 +230,28 @@ const TeamManagementPage = (): JSX.Element => {
       <p className={`${baseClass}__page-description`}>
         Create, customize, and remove teams from Fleet.
       </p>
-      <TableContainer
-        columns={tableHeaders}
-        data={teams}
-        isLoading={loadingTableData}
-        defaultSortHeader={"name"}
-        defaultSortDirection={"asc"}
-        inputPlaceHolder={"Search"}
-        actionButtonText={"Create team"}
-        actionButtonVariant={"primary"}
-        onActionButtonClick={toggleCreateTeamModal}
-        onQueryChange={onQueryChange}
-        resultsTitle={"teams"}
-        emptyComponent={EmptyTeams}
-        showMarkAllPages={false}
-        isAllPagesSelected={false}
-        searchable
-      />
+      {Object.keys(teamsError).length > 0 ? (
+        <TeamsError />
+      ) : (
+        <TableContainer
+          columns={tableHeaders}
+          data={teams}
+          isLoading={loadingTableData}
+          defaultSortHeader={"name"}
+          defaultSortDirection={"asc"}
+          inputPlaceHolder={"Search"}
+          actionButtonText={"Create team"}
+          actionButtonVariant={"primary"}
+          hideActionButton={teams.length === 0 && searchString === ""}
+          onActionButtonClick={toggleCreateTeamModal}
+          onQueryChange={onQueryChange}
+          resultsTitle={"teams"}
+          emptyComponent={NoTeamsComponent}
+          showMarkAllPages={false}
+          isAllPagesSelected={false}
+          searchable={teams.length > 0 && searchString !== ""}
+        />
+      )}
       {showCreateTeamModal ? (
         <CreateTeamModal
           onCancel={toggleCreateTeamModal}
