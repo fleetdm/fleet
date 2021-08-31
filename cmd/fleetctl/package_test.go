98ed7a57c781346b3b9beb8a0a95cd9e2f15d23d
Benjamin Edwards <edwards.benw@gmail.com>
2021-08-30 23:54:18 -0400

    added tests for package command, run go mod tidy & go mod verify

--- cmd/fleetctl/package_test.go
+++ cmd/fleetctl/package_test.go
@@ -4,11 +4,20 @@ import "testing"
 
 func TestPackage(t *testing.T) {
 
+	// --type is required
 	runAppCheckErr(t, []string{"package", "deb"}, "Required flag \"type\" not set")
+
+	// if you provide -fleet-url & --enroll-secret are required together
 	runAppCheckErr(t, []string{"package", "--type=deb", "--fleet-url=https://localhost:8080"}, "--enroll-secret and --fleet-url must be provided together")
 	runAppCheckErr(t, []string{"package", "--type=deb", "--enroll-secret=foobar"}, "--enroll-secret and --fleet-url must be provided together")
+
+	// --insecure and --fleet-certificate are mutually exclusive
 	runAppCheckErr(t, []string{"package", "--type=deb", "--insecure", "--fleet-certificate=test123"}, "--insecure and --fleet-certificate may not be provided together")
 
+	// run package tests, each should output their respective package type
 	runAppForTest(t, []string{"package", "--type=deb", "--insecure"})
+	runAppForTest(t, []string{"package", "--type=rpm", "--insecure"})
+	runAppForTest(t, []string{"package", "--type=msi", "--insecure"})
+	//runAppForTest(t, []string{"package", "--type=pkg", "--insecure"}) TODO: had a hard time getting xar installed on Ubuntu
 
 }
