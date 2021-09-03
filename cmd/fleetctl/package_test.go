ad06ea62a48f2cf26f660a8fcaeaf86549ee80a4
Benjamin Edwards <edwards.benw@gmail.com>
2021-09-03 13:01:50 -0400

    validate that package files exist

--- cmd/fleetctl/package_test.go
+++ cmd/fleetctl/package_test.go
@@ -1,6 +1,12 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+)
 
 func TestPackage(t *testing.T) {
 
@@ -15,9 +21,29 @@ func TestPackage(t *testing.T) {
 	runAppCheckErr(t, []string{"package", "--type=deb", "--insecure", "--fleet-certificate=test123"}, "--insecure and --fleet-certificate may not be provided together")
 
 	// run package tests, each should output their respective package type
+	// orbit-osquery_0.0.3_amd64.deb
 	runAppForTest(t, []string{"package", "--type=deb", "--insecure"})
+	// orbit-osquery-0.0.3.x86_64.rpm
 	runAppForTest(t, []string{"package", "--type=rpm", "--insecure"})
+	// orbit-osquery_0.0.3.msi
 	runAppForTest(t, []string{"package", "--type=msi", "--insecure"})
+
 	//runAppForTest(t, []string{"package", "--type=pkg", "--insecure"}) TODO: had a hard time getting xar installed on Ubuntu
+
+
+	dir, err := os.Getwd()
+	require.NoError(t, err)
+	filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		require.NoError(t, err)
+
+		// TODO validate contents
+		switch filepath.Ext(path) {
+		case ".msi", ".deb", ".rpm", ".pkg":
+			info, err := d.Info()
+			require.NoError(t, err)
+			require.Greater(t, info.Size(), int64(0))
+		}
+		return nil
+	})
 
 }
