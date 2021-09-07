b29be2b7579634415add5590bc5bc4e612e842ec
eashaw <caglc@live.com>
2021-09-07 18:40:21 -0500

    created /press-kit page and updated routes and policies

---
+++ website/api/controllers/view-press-kit.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View press kit',
+
+
+  description: 'Display "Press kit" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/press-kit'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
