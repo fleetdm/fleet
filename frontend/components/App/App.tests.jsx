c84227742c36f3ee5ffad30b59569332f2c27ab9
Martavis Parker <martavis@auraticdevelopment.com>
2021-09-09 16:17:28 -0700

    brought tests back

--- frontend/components/App/App.tests.jsx
+++ frontend/components/App/App.tests.jsx
@@ -7,76 +7,74 @@ import local from "../../utilities/local";
 
 const { connectedComponent, reduxMockStore } = helpers;
 
-// TODO: commented out because tests are failing with context api
-// in Github Actions
-// describe("App - component", () => {
-//   const store = { app: {}, auth: {}, notifications: {} };
-//   const mockStore = reduxMockStore(store);
-//   const component = mount(connectedComponent(ConnectedApp, { mockStore }));
+describe("App - component", () => {
+  const store = { app: {}, auth: {}, notifications: {} };
+  const mockStore = reduxMockStore(store);
+  const component = mount(connectedComponent(ConnectedApp, { mockStore }));
 
-//   afterEach(() => {
-//     local.setItem("auth_token", null);
-//   });
+  afterEach(() => {
+    local.setItem("auth_token", null);
+  });
 
-//   it("renders", () => {
-//     expect(component).toBeTruthy();
-//   });
+  it("renders", () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it("loads the current user if there is an auth token but no user", () => {
-//     local.setItem("auth_token", "ABC123");
+  it("loads the current user if there is an auth token but no user", () => {
+    local.setItem("auth_token", "ABC123");
 
-//     const spy = jest
-//       .spyOn(authActions, "fetchCurrentUser")
-//       .mockImplementation(() => {
-//         return (dispatch) => {
-//           dispatch({ type: "LOAD_USER_ACTION" });
-//           return Promise.resolve();
-//         };
-//       });
-//     const application = connectedComponent(ConnectedApp, { mockStore });
+    const spy = jest
+      .spyOn(authActions, "fetchCurrentUser")
+      .mockImplementation(() => {
+        return (dispatch) => {
+          dispatch({ type: "LOAD_USER_ACTION" });
+          return Promise.resolve();
+        };
+      });
+    const application = connectedComponent(ConnectedApp, { mockStore });
 
-//     mount(application);
-//     expect(spy).toHaveBeenCalled();
-//   });
+    mount(application);
+    expect(spy).toHaveBeenCalled();
+  });
 
-//   it("does not load the current user if is it already loaded", () => {
-//     local.setItem("auth_token", "ABC123");
+  it("does not load the current user if is it already loaded", () => {
+    local.setItem("auth_token", "ABC123");
 
-//     const spy = jest
-//       .spyOn(authActions, "fetchCurrentUser")
-//       .mockImplementation(() => {
-//         return { type: "LOAD_USER_ACTION" };
-//       });
-//     const storeWithUser = {
-//       app: {},
-//       auth: {
-//         user: {
-//           id: 1,
-//           email: "hi@thegnar.co",
-//         },
-//       },
-//       notifications: {},
-//     };
-//     const mockStoreWithUser = reduxMockStore(storeWithUser);
-//     const application = connectedComponent(ConnectedApp, {
-//       mockStore: mockStoreWithUser,
-//     });
+    const spy = jest
+      .spyOn(authActions, "fetchCurrentUser")
+      .mockImplementation(() => {
+        return { type: "LOAD_USER_ACTION" };
+      });
+    const storeWithUser = {
+      app: {},
+      auth: {
+        user: {
+          id: 1,
+          email: "hi@thegnar.co",
+        },
+      },
+      notifications: {},
+    };
+    const mockStoreWithUser = reduxMockStore(storeWithUser);
+    const application = connectedComponent(ConnectedApp, {
+      mockStore: mockStoreWithUser,
+    });
 
-//     mount(application);
-//     expect(spy).not.toHaveBeenCalled();
-//   });
+    mount(application);
+    expect(spy).not.toHaveBeenCalled();
+  });
 
-//   it("does not load the current user if there is no auth token", () => {
-//     local.clear();
+  it("does not load the current user if there is no auth token", () => {
+    local.clear();
 
-//     const spy = jest
-//       .spyOn(authActions, "fetchCurrentUser")
-//       .mockImplementation(() => {
-//         throw new Error("should not have been called");
-//       });
-//     const application = connectedComponent(ConnectedApp, { mockStore });
+    const spy = jest
+      .spyOn(authActions, "fetchCurrentUser")
+      .mockImplementation(() => {
+        throw new Error("should not have been called");
+      });
+    const application = connectedComponent(ConnectedApp, { mockStore });
 
-//     mount(application);
-//     expect(spy).not.toHaveBeenCalled();
-//   });
-// });
+    mount(application);
+    expect(spy).not.toHaveBeenCalled();
+  });
+});
--- frontend/pages/LoginPage/LoginPage.tests.jsx
+++ frontend/pages/LoginPage/LoginPage.tests.jsx
@@ -6,58 +6,56 @@ import LoginPage from "./LoginPage";
 
 const ssoSettings = { sso_enabled: false };
 
-// TODO: commented out because tests are failing with context api
-// in Github Actions
-// describe("LoginPage - component", () => {
-//   describe("when the user is not logged in", () => {
-//     const mockStore = reduxMockStore({ auth: { ssoSettings } });
+describe("LoginPage - component", () => {
+  describe("when the user is not logged in", () => {
+    const mockStore = reduxMockStore({ auth: { ssoSettings } });
 
-//     it("renders the LoginForm", () => {
-//       const page = mount(connectedComponent(LoginPage, { mockStore }));
+    it("renders the LoginForm", () => {
+      const page = mount(connectedComponent(LoginPage, { mockStore }));
 
-//       expect(page.find("LoginForm").length).toEqual(1);
-//     });
-//   });
+      expect(page.find("LoginForm").length).toEqual(1);
+    });
+  });
 
-//   describe("when the users session is not recognized", () => {
-//     const mockStore = reduxMockStore({
-//       auth: {
-//         errors: { base: "Unable to authenticate the current user" },
-//         ssoSettings,
-//       },
-//     });
+  describe("when the users session is not recognized", () => {
+    const mockStore = reduxMockStore({
+      auth: {
+        errors: { base: "Unable to authenticate the current user" },
+        ssoSettings,
+      },
+    });
 
-//     it("renders the LoginForm base errors", () => {
-//       const page = mount(connectedComponent(LoginPage, { mockStore }));
-//       const loginForm = page.find("LoginForm");
+    it("renders the LoginForm base errors", () => {
+      const page = mount(connectedComponent(LoginPage, { mockStore }));
+      const loginForm = page.find("LoginForm");
 
-//       expect(loginForm.length).toEqual(1);
-//       expect(loginForm.prop("serverErrors")).toEqual({
-//         base: "Unable to authenticate the current user",
-//       });
-//     });
-//   });
+      expect(loginForm.length).toEqual(1);
+      expect(loginForm.prop("serverErrors")).toEqual({
+        base: "Unable to authenticate the current user",
+      });
+    });
+  });
 
-//   describe("when the user is logged in", () => {
-//     beforeEach(() => {
-//       local.setItem("auth_token", "fake-auth-token");
-//     });
+  describe("when the user is logged in", () => {
+    beforeEach(() => {
+      local.setItem("auth_token", "fake-auth-token");
+    });
 
-//     const user = { id: 1, firstName: "Bill", lastName: "Shakespeare" };
+    const user = { id: 1, firstName: "Bill", lastName: "Shakespeare" };
 
-//     it("redirects to the home page", () => {
-//       const mockStore = reduxMockStore({ auth: { user, ssoSettings } });
-//       const props = { pathname: "/login" };
-//       const redirectAction = {
-//         type: "@@router/CALL_HISTORY_METHOD",
-//         payload: {
-//           method: "push",
-//           args: ["/"],
-//         },
-//       };
+    it("redirects to the home page", () => {
+      const mockStore = reduxMockStore({ auth: { user, ssoSettings } });
+      const props = { pathname: "/login" };
+      const redirectAction = {
+        type: "@@router/CALL_HISTORY_METHOD",
+        payload: {
+          method: "push",
+          args: ["/"],
+        },
+      };
 
-//       mount(connectedComponent(LoginPage, { props, mockStore }));
-//       expect(mockStore.getActions()).toContainEqual(redirectAction);
-//     });
-//   });
-// });
+      mount(connectedComponent(LoginPage, { props, mockStore }));
+      expect(mockStore.getActions()).toContainEqual(redirectAction);
+    });
+  });
+});
