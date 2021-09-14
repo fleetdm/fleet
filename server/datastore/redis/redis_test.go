7fa9e894474ec50d94b668e99b4a36f5d9f880bc
Martin Angers <martin.n.angers@gmail.com>
2021-09-14 10:11:00 -0400

    Improve retry connect test

--- server/datastore/redis/redis_test.go
+++ server/datastore/redis/redis_test.go
@@ -15,49 +15,74 @@ import (
 
 type netError struct {
 	error
-	timeout   bool
-	temporary bool
-	count     int // once this reaches 0, mockDial does not return an error
+	timeout      bool
+	temporary    bool
+	allowedCalls int // once this reaches 0, mockDial does not return an error
+	countCalls   int
 }
 
 func (t *netError) Timeout() bool   { return t.timeout }
 func (t *netError) Temporary() bool { return t.temporary }
 
+var errFromConn = errors.New("SUCCESS")
+
 type redisConn struct{}
 
-func (redisConn) Close() error                                       { return io.EOF }
-func (redisConn) Err() error                                         { return io.EOF }
-func (redisConn) Do(_ string, _ ...interface{}) (interface{}, error) { return nil, io.EOF }
-func (redisConn) Send(_ string, _ ...interface{}) error              { return io.EOF }
-func (redisConn) Flush() error                                       { return io.EOF }
-func (redisConn) Receive() (interface{}, error)                      { return nil, io.EOF }
+func (redisConn) Close() error                                       { return errFromConn }
+func (redisConn) Err() error                                         { return errFromConn }
+func (redisConn) Do(_ string, _ ...interface{}) (interface{}, error) { return nil, errFromConn }
+func (redisConn) Send(_ string, _ ...interface{}) error              { return errFromConn }
+func (redisConn) Flush() error                                       { return errFromConn }
+func (redisConn) Receive() (interface{}, error)                      { return nil, errFromConn }
 
 func TestConnectRetry(t *testing.T) {
 	mockDial := func(err error) func(net, addr string, opts ...redis.DialOption) (redis.Conn, error) {
 		return func(net, addr string, opts ...redis.DialOption) (redis.Conn, error) {
 			var ne *netError
 			if errors.As(err, &ne) {
-				ne.count--
-				if ne.count <= 0 {
+				ne.countCalls++
+				if ne.allowedCalls <= 0 {
 					return redisConn{}, nil
 				}
+				ne.allowedCalls--
 			}
 			return nil, err
 		}
 	}
 
 	cases := []struct {
-		err      error
-		retries  int
-		min, max time.Duration
+		err       error
+		retries   int
+		wantCalls int
+		min, max  time.Duration
 	}{
-		{io.EOF, 0, 0, 100 * time.Millisecond},                                                  // non-retryable, no retry configured
-		{&netError{io.EOF, true, false, 10}, 0, 0, 100 * time.Millisecond},                      // retryable, but no retry configured
-		{io.EOF, 3, 0, 100 * time.Millisecond},                                                  // non-retryable, retry configured
-		{&netError{io.EOF, true, false, 10}, 2, time.Second, 3 * time.Second},                   // retryable, retry configured
-		{&netError{io.EOF, false, true, 10}, 2, time.Second, 3 * time.Second},                   // retryable, retry configured
-		{&netError{io.EOF, false, false, 10}, 2, 0, 100 * time.Millisecond},                     // net error, but non-retryable
-		{&netError{io.EOF, true, false, 2}, 10, 100 * time.Millisecond, 500 * time.Millisecond}, // retryable, but succeeded after one
+		// the min-max time intervals are based on the backoff default configuration as
+		// used in the Dial func of the redis pool. It starts with 500ms interval,
+		// multiplies by 1.5 on each attempt, and has a randomization of 0.5 that must
+		// be accounted for. Example ranges of intervals are given at
+		// https://github.com/fleetdm/fleet/pull/1962#issue-729635664
+		// and were used to calculate the (approximate) expected range.
+		{
+			io.EOF, 0, 1, 0, 100 * time.Millisecond,
+		}, // non-retryable, no retry configured
+		{
+			&netError{error: io.EOF, timeout: true, allowedCalls: 10}, 0, 1, 0, 100 * time.Millisecond,
+		}, // retryable, but no retry configured
+		{
+			io.EOF, 3, 1, 0, 100 * time.Millisecond,
+		}, // non-retryable, retry configured
+		{
+			&netError{error: io.EOF, timeout: true, allowedCalls: 10}, 2, 3, 625 * time.Millisecond, 3500 * time.Millisecond,
+		}, // retryable, retry configured
+		{
+			&netError{error: io.EOF, temporary: true, allowedCalls: 10}, 2, 3, 625 * time.Millisecond, 3500 * time.Millisecond,
+		}, // retryable, retry configured
+		{
+			&netError{error: io.EOF, allowedCalls: 10}, 2, 1, 0, 100 * time.Millisecond,
+		}, // net error, but non-retryable
+		{
+			&netError{error: io.EOF, timeout: true, allowedCalls: 1}, 10, 2, 250 * time.Millisecond, 750 * time.Millisecond,
+		}, // retryable, but succeeded after one retry
 	}
 	for _, c := range cases {
 		t.Run(c.err.Error(), func(t *testing.T) {
@@ -71,9 +96,21 @@ func TestConnectRetry(t *testing.T) {
 			require.GreaterOrEqual(t, diff, c.min)
 			require.LessOrEqual(t, diff, c.max)
 			require.Error(t, err)
+
+			wantErr := io.EOF
+			var ne *netError
+			if errors.As(c.err, &ne) {
+				require.Equal(t, c.wantCalls, ne.countCalls)
+				if ne.allowedCalls == 0 {
+					wantErr = errFromConn
+				}
+			} else {
+				require.Equal(t, c.wantCalls, 1)
+			}
+
 			// the error is returned as part of the cluster.Refresh error, hence the
 			// check with Contains.
-			require.Contains(t, err.Error(), io.EOF.Error())
+			require.Contains(t, err.Error(), wantErr.Error())
 		})
 	}
 }
