c45ebf53f4c26854176f61b4f635d6a33027c237
eashaw <caglc@live.com>
2021-09-15 13:05:36 -0500

    handling redirect

--- website/api/controllers/docs/view-basic-documentation.js
+++ website/api/controllers/docs/view-basic-documentation.js
@@ -44,15 +44,33 @@ module.exports = {
     // console.log('pageUrlSuffix:',pageUrlSuffix);
     // console.log('SECTION_URL_PREFIX + "/" + _.trim(pageUrlSuffix, "/"):',SECTION_URL_PREFIX + '/' + _.trim(pageUrlSuffix, '/'));
     // console.log('thisPage:',thisPage);
+
+    // if the url doesn't match any existing page, or the page it matches doesn't match the url provided
+    // then it might have extra slashes or uppercase characters (e.g. fleetdm.com/docs///usiNG-fleet////)
+    // Note: because this also handles the docs landing page and a `pageUrlSuffix` might not have
+    // been provided, we won't be rechecking the url if thisPage.url is '/docs'
+    if (!thisPage || (thisPage.url !== '/docs/'+pageUrlSuffix && thisPage.url !== '/docs')) {
+      // creating a regex to match instances of multiple slashes
+      let multipleSlashesRegex = /\/{2,}/g;
+      // Creating a lowercase double-slashless url to search with
+      let modifiedPageUrlSuffix = pageUrlSuffix.toLowerCase().replace(multipleSlashesRegex, '/');
+      // Finding the appropriate page content using the modified url.
+      let revisedPage = _.find(sails.config.builtStaticContent.markdownPages, {
+        url: _.trimRight(SECTION_URL_PREFIX + '/' + _.trim(modifiedPageUrlSuffix, '/'), '/')
+      });
+      // If we matched a page with the modified url, then redirect to that.
+      if(revisedPage && revisedPage.url) {
+        throw {redirect: revisedPage.url};
+      } else {
+        // otherwise, throw a 404 error.
+        throw 'notFound';
+      }
+    }
+
     if (!thisPage) {
       throw 'notFound';
     }
 
-    if (false) {
-      // TODO: add "redirect" exit and handle mismatched capitalization / extra slashes by redirecting to the correct URL.  e.g. "http://localhost:2024/docs//usiNG-fleet///" Partial example of this: https://github.com/sailshq/sailsjs.com/blob/b53c6e6a90c9afdf89e5cae00b9c9dd3f391b0e7/api/controllers/documentation/view-documentation.js#L161-L166
-      let revisedUrl = 'todo';
-      throw {redirect: revisedUrl};
-    }
 
     // Respond with view.
     return {
