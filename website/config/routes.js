d4e7f5b6ae085e844f80ad97569b0496ffe7543b
eashaw <caglc@live.com>
2021-09-15 11:00:22 -0500

    added title to locals in routes

--- website/config/routes.js
+++ website/config/routes.js
@@ -13,19 +13,59 @@ module.exports.routes = {
   //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
-  'GET /':                   { action: 'view-homepage-or-redirect', locals: { isHomepage: true } },
-  'GET /company/contact':    { action: 'view-contact' },
-  'GET /get-started':        { action: 'view-get-started' },
-  'GET /pricing':            { action: 'view-pricing' },
-  'GET /press-kit':          { action: 'view-press-kit' },
+  'GET /': {
+    action: 'view-homepage-or-redirect',
+    locals: { isHomepage: true }
+  },
+  'GET /company/contact': {
+    action: 'view-contact',
+    locals:{
+      title: 'Contact us | Fleet for osquery',
+    }
+  },
+  'GET /get-started': {
+    action: 'view-get-started' ,
+    locals:{
+      title: 'Get Started | Fleet for osquery',
+    }
+  },
+  'GET /pricing': {
+    action: 'view-pricing',
+    locals:{
+      title: 'Pricing | Fleet for osquery',
+      description: 'howdy there :o'
+    }
+  },
+  'GET /press-kit': {
+    action: 'view-press-kit',
+    locals:{
+      title: 'Press kit | Fleet for osquery',
+    }
+  },
 
-  'GET /queries':            { action: 'view-query-library' },
-  'GET /queries/:slug':      { action: 'view-query-detail' },
+  'GET /queries': {
+    action: 'view-query-library',
+    locals:{
+      title: 'Queries | Fleet for osquery',
+    }
+  },
+  'GET /queries/:slug': {
+    action: 'view-query-detail',
+    locals:{
+      title: 'Query details | Fleet for osquery',
+    }
+  },
 
-  'GET /docs/?*':            { skipAssets: false, action: 'docs/view-basic-documentation' },// handles /docs and /docs/foo/bar
-  // 'GET /handbook/?*':        { skipAssets: false, action: 'handbook/view-basic-handbook' },// handles /handbook and /handbook/foo/bar
+  'GET /docs/?*': { skipAssets: false, action: 'docs/view-basic-documentation' },// handles /docs and /docs/foo/bar
 
-  'GET /transparency':       { action: 'view-transparency' },
+  // 'GET /handbook/?*':  { skipAssets: false, action: 'handbook/view-basic-handbook' },// handles /handbook and /handbook/foo/bar
+
+  'GET /transparency': {
+    action: 'view-transparency',
+    locals:{
+      title: 'Transparency | Fleet for osquery',
+    }
+  },
 
 
 
