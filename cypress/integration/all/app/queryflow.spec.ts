51486fc86989754b6e7c92743fa21310d96de938
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-09-15 16:07:56 -0400

    Ne2e for new query UI and inherited queries on team schedule

--- cypress/integration/all/app/queryflow.spec.ts
+++ cypress/integration/all/app/queryflow.spec.ts
@@ -13,136 +13,160 @@ describe(
     it("Create, check, edit, and delete a query successfully and create, edit, and delete a global scheduled query successfully", () => {
       cy.visit("/queries/manage");
 
-      cy.findByRole("button", { name: /create new query/i }).should("exist");
-      // cy.findByRole("button", { name: /create new query/i }).click();
+      cy.findByRole("button", { name: /create new query/i }).click();
 
-      // cy.findByLabelText(/query name/i)
-      //   .click()
-      //   .type("Query all window crashes");
+      // Using class selector because third party element doesn't work with Cypress Testing Selector Library
+      cy.get(".ace_scroller")
+        .click({ force: true })
+        .type("{selectall}SELECT * FROM windows_crashes;");
 
-      // // Using class selector because third party element doesn't work with Cypress Testing Selector Library
-      // cy.get(".ace_scroller")
-      //   .click({ force: true })
-      //   .type("{selectall}{backspace}SELECT * FROM windows_crashes;");
+      cy.findByRole("button", { name: /save/i }).click();
 
-      // cy.findByLabelText(/description/i)
-      //   .click()
-      //   .type("See all window crashes");
+      cy.findByLabelText(/name/i).click().type("Query all window crashes");
 
-      // cy.findByRole("button", { name: /save/i }).click();
+      cy.findByLabelText(/description/i)
+        .click()
+        .type("See all window crashes");
 
-      // cy.findByRole("button", { name: /save as new/i }).click();
+      cy.findByRole("button", { name: /save query/i }).click();
 
-      // Just refreshes to create new query, needs success alert to user that they created a query
-      // cy.visit("/queries/manage");
+      cy.findByText(/query created/i).should("exist");
+      cy.findByText(/back to queries/i).should("exist");
+      cy.visit("/queries/manage");
 
-      // cy.findByText(/query all/i).click();
+      cy.findByText(/query all/i).click();
 
-      // cy.findByText(/edit & run query/i).should("exist");
+      cy.findByText(/run query/i).should("exist");
 
-      // cy.get(".ace_scroller")
-      //   .click({ force: true })
-      //   .type(
-      //     "{selectall}{backspace}SELECT datetime, username FROM windows_crashes;"
-      //   );
+      cy.get(".ace_scroller")
+        .click({ force: true })
+        .type("{selectall}SELECT datetime, username FROM windows_crashes;");
 
-      // cy.findByRole("button", { name: /save/i }).click();
+      cy.findByRole("button", { name: /^Save$/ }).click();
 
-      // cy.findByRole("button", { name: /save changes/i }).click();
+      cy.findByText(/query updated/i).should("be.visible");
 
-      // cy.findByText(/query updated/i).should("be.visible");
+      // Test Schedules
+      cy.visit("/schedule/manage");
 
-      // // Test Schedules
-      // cy.visit("/schedule/manage");
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
 
-      // cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.findByRole("button", { name: /schedule a query/i }).click();
 
-      // cy.findByRole("button", { name: /schedule a query/i }).click();
+      cy.findByText(/select query/i).click();
 
-      // cy.findByText(/select query/i).click();
+      cy.findByText(/query all window crashes/i).click();
 
-      // cy.findByText(/query all window crashes/i).click();
+      cy.get(
+        ".schedule-editor-modal__form-field--frequency > .dropdown__select"
+      ).click();
 
-      // cy.get(
-      //   ".schedule-editor-modal__form-field--frequency > .dropdown__select"
-      // ).click();
+      cy.findByText(/every week/i).click();
 
-      // cy.findByText(/every week/i).click();
+      cy.findByText(/show advanced options/i).click();
 
-      // cy.findByText(/show advanced options/i).click();
+      cy.get(
+        ".schedule-editor-modal__form-field--logging > .dropdown__select"
+      ).click();
 
-      // cy.get(
-      //   ".schedule-editor-modal__form-field--logging > .dropdown__select"
-      // ).click();
+      cy.findByText(/ignore removals/i).click();
 
-      // cy.findByText(/ignore removals/i).click();
+      cy.get(".schedule-editor-modal__form-field--shard > .input-field")
+        .click()
+        .type("50");
 
-      // cy.get(".schedule-editor-modal__form-field--shard > .input-field")
-      //   .click()
-      //   .type("50");
+      cy.get(".schedule-editor-modal__btn-wrap")
+        .contains("button", /schedule/i)
+        .click();
 
-      // cy.get(".schedule-editor-modal__btn-wrap")
-      //   .contains("button", /schedule/i)
-      //   .click();
+      cy.visit("/schedule/manage");
 
-      //   cy.visit("/schedule/manage");
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.findByText(/query all window crashes/i).should("exist");
 
-      //   cy.wait(3000); // eslint-disable-line cypress/no-unnecessary-waiting
-      //   cy.findByText(/query all window crashes/i).should("exist");
+      cy.findByText(/actions/i).click();
+      cy.findByText(/edit/i).click();
 
-      //   cy.findByText(/actions/i).click();
-      //   cy.findByText(/edit/i).click();
+      cy.get(
+        ".schedule-editor-modal__form-field--frequency > .dropdown__select"
+      ).click();
 
-      //   cy.get(
-      //     ".schedule-editor-modal__form-field--frequency > .dropdown__select"
-      //   ).click();
+      cy.findByText(/every 6 hours/i).click();
 
-      //   cy.findByText(/every 6 hours/i).click();
+      cy.findByText(/show advanced options/i).click();
 
-      //   cy.findByText(/show advanced options/i).click();
+      cy.findByText(/ignore removals/i).click();
+      cy.findByText(/snapshot/i).click();
 
-      //   cy.findByText(/ignore removals/i).click();
-      //   cy.findByText(/snapshot/i).click();
+      cy.get(".schedule-editor-modal__form-field--shard > .input-field")
+        .click()
+        .type("{selectall}{backspace}10");
 
-      //   cy.get(".schedule-editor-modal__form-field--shard > .input-field")
-      //     .click()
-      //     .type("{selectall}{backspace}10");
+      cy.get(".schedule-editor-modal__btn-wrap")
+        .contains("button", /schedule/i)
+        .click();
 
-      //   cy.get(".schedule-editor-modal__btn-wrap")
-      //     .contains("button", /schedule/i)
-      //     .click();
+      //e2e tests for team schedules
+      cy.visit("/settings/teams");
 
-      //   cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-      //   cy.findByText(/actions/i).click();
-      //   cy.findByText(/remove/i).click();
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.findByRole("button", { name: /create team/i }).click();
 
-      //   cy.get(".remove-scheduled-query-modal__btn-wrap")
-      //     .contains("button", /remove/i)
-      //     .click();
+      cy.findByLabelText(/team name/i)
+        .click()
+        .type("Apples");
 
-      //   cy.findByText(/query all window crashes/i).should("not.exist");
+      cy.get(".create-team-modal__btn-wrap")
+        .contains("button", /create/i)
+        .click();
+
+      cy.visit("/schedule/manage");
+
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.findByText(/all teams/i).click();
+      cy.findByText(/apples/i).click();
+
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.findByText(/query all window crashes/i).should("not.exist");
+      cy.findByText(/inherited query/i).click();
+      cy.findByText(/query all window crashes/i).should("exist");
+
+      //end e2e test for team schedules
+
+      cy.visit("/schedule/manage");
+
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+      cy.findByText(/actions/i).click();
+      cy.findByText(/remove/i).click();
+
+      cy.get(".remove-scheduled-query-modal__btn-wrap")
+        .contains("button", /remove/i)
+        .click();
+
+      cy.findByText(/query all window crashes/i).should("not.exist");
+
       //   // End Test Schedules
 
-      //   cy.visit("/queries/manage");
+      cy.visit("/queries/manage");
 
-      //   cy.findByText(/query all window crashes/i)
-      //     .parent()
-      //     .parent()
-      //     .within(() => {
-      //       cy.get(".fleet-checkbox__input").check({ force: true });
-      //     });
+      cy.findByText(/query all window crashes/i)
+        .parent()
+        .parent()
+        .within(() => {
+          cy.get(".fleet-checkbox__input").check({ force: true });
+        });
 
-      //   cy.findByRole("button", { name: /delete/i }).click();
+      cy.findByRole("button", { name: /delete/i }).click();
 
-      //   // Can't figure out how attach findByRole onto modal button
-      //   // Can't use findByText because delete button under modal
-      //   cy.get(".remove-query-modal")
-      //     .contains("button", /delete/i)
-      //     .click();
+      // Can't figure out how attach findByRole onto modal button
+      // Can't use findByText because delete button under modal
+      cy.get(".remove-query-modal")
+        .contains("button", /delete/i)
+        .click();
 
-      //   cy.findByText(/successfully removed query/i).should("be.visible");
+      cy.findByText(/successfully removed query/i).should("be.visible");
 
-      //   cy.findByText(/query all/i).should("not.exist");
+      cy.findByText(/query all/i).should("not.exist");
     });
   }
 );
