e94ae8a711d4f4ebc9622e665961d422dd9651ab
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-09-24 10:20:49 -0400

    Longer wait to e2e error

--- cypress/integration/premium/teamflow.spec.ts
+++ cypress/integration/premium/teamflow.spec.ts
@@ -215,7 +215,7 @@ describe("Teams flow", () => {
     cy.visit("/settings/teams");
     // Allow rendering to settle
     // TODO this might represent a bug in the React code.
-    cy.wait(100); // eslint-disable-line cypress/no-unnecessary-waiting
+    cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
 
     cy.contains("Mystic").get(".Select-arrow-zone").click();
 
