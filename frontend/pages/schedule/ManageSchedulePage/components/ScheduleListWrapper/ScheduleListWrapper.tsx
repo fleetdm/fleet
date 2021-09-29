d8a8771d925df6af134804c81c587020c5ec6cac
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-09-29 18:05:32 -0400

    Modify verbage to not include advanced for team maintainers

--- frontend/pages/schedule/ManageSchedulePage/components/ScheduleListWrapper/ScheduleListWrapper.tsx
+++ frontend/pages/schedule/ManageSchedulePage/components/ScheduleListWrapper/ScheduleListWrapper.tsx
@@ -71,8 +71,9 @@ const ScheduleListWrapper = (props: IScheduleListWrapperProps): JSX.Element => {
           <div className={`${noScheduleClass}__inner-text`}>
             <h2>You don&apos;t have any queries scheduled.</h2>
             <p>
-              Schedule a query, or go to your osquery packs via the
-              &lsquo;Advanced&rsquo; button.
+              {!isTeamMaintainer
+                ? "Schedule a query, or go to your osquery packs via the &lsquo;Advanced&rsquo; button."
+                : "Schedule a query to run on hosts assigned to this team."}
             </p>
             <div className={`${noScheduleClass}__-cta-buttons`}>
               <Button
