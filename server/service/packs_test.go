7280630035a71c07c7d584ec17e52547c340ecf2
Martin Angers <martin.n.angers@gmail.com>
2021-10-06 15:53:18 -0400

    Fix the GetPack test

--- server/service/packs_test.go
+++ server/service/packs_test.go
@@ -4,27 +4,29 @@ import (
 	"context"
 	"testing"
 
-	"github.com/fleetdm/fleet/v4/server/datastore/mysql"
+	"github.com/fleetdm/fleet/v4/server/authz"
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/mock"
 	"github.com/fleetdm/fleet/v4/server/test"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetPack(t *testing.T) {
-	ds := mysql.CreateMySQLDS(t)
-	defer ds.Close()
-
+	ds := new(mock.Store)
 	svc := newTestService(ds, nil, nil)
 
-	pack := &fleet.Pack{
-		Name: "foo",
+	ds.PackFunc = func(ctx context.Context, id uint) (*fleet.Pack, error) {
+		return &fleet.Pack{
+			ID:      1,
+			TeamIDs: []uint{1},
+		}, nil
 	}
-	_, err := ds.NewPack(context.Background(), pack)
-	assert.Nil(t, err)
-	assert.NotZero(t, pack.ID)
 
-	packVerify, err := svc.GetPack(test.UserContext(test.UserAdmin), pack.ID)
-	assert.Nil(t, err)
+	pack, err := svc.GetPack(test.UserContext(test.UserAdmin), 1)
+	require.NoError(t, err)
+	require.Equal(t, uint(1), pack.ID)
 
-	assert.Equal(t, pack.ID, packVerify.ID)
+	_, err = svc.GetPack(test.UserContext(test.UserNoRoles), 1)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), authz.ForbiddenErrorMessage)
 }
