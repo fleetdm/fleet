185469d236d1066192f032e714c1742272059e3f
Tomas Touceda <chiiph@gmail.com>
2021-10-06 12:34:16 -0300

    Allow team maintainers to bulk delete hosts for teams they belong

--- server/service/hosts.go
+++ server/service/hosts.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/pkg/errors"
 )
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -43,7 +44,7 @@ func deleteHostsEndpoint(ctx context.Context, request interface{}, svc fleet.Ser
 }
 
 func (svc Service) DeleteHosts(ctx context.Context, ids []uint, opts fleet.HostListOptions, lid *uint) error {
-	if err := svc.authz.Authorize(ctx, &fleet.Host{}, fleet.ActionWrite); err != nil {
+	if err := svc.authz.Authorize(ctx, &fleet.Host{}, fleet.ActionList); err != nil {
 		return err
 	}
 
@@ -52,6 +53,10 @@ func (svc Service) DeleteHosts(ctx context.Context, ids []uint, opts fleet.HostL
 	}
 
 	if len(ids) > 0 {
+		err := svc.checkWriteForHostIDs(ctx, ids)
+		if err != nil {
+			return err
+		}
 		return svc.ds.DeleteHosts(ctx, ids)
 	}
 
@@ -63,5 +68,25 @@ func (svc Service) DeleteHosts(ctx context.Context, ids []uint, opts fleet.HostL
 	if len(hostIDs) == 0 {
 		return nil
 	}
+
+	err = svc.checkWriteForHostIDs(ctx, hostIDs)
+	if err != nil {
+		return err
+	}
 	return svc.ds.DeleteHosts(ctx, hostIDs)
+}
+
+func (svc Service) checkWriteForHostIDs(ctx context.Context, ids []uint) error {
+	for _, id := range ids {
+		host, err := svc.ds.Host(ctx, id)
+		if err != nil {
+			return errors.Wrap(err, "get host for delete")
+		}
+
+		// Authorize again with team loaded now that we have team_id
+		if err := svc.authz.Authorize(ctx, host, fleet.ActionWrite); err != nil {
+			return err
+		}
+	}
+	return nil
 }
