f30518348db1f80611c1d7aef3de7f700b7f14d0
eashaw <caglc@live.com>
2021-10-07 12:50:35 -0500

    Update basic-documentation.page.js

--- website/assets/js/pages/docs/basic-documentation.page.js
+++ website/assets/js/pages/docs/basic-documentation.page.js
@@ -166,13 +166,12 @@ parasails.registerPage('basic-documentation', {
         // find the child <a> element
         let linkToCopy = _.first($(elem).find('a.markdown-link'));
         // If the link's href is missing, we'll copy the current url (and remove any hashes) to the clipboard instead
-        if(linkToCopy.href) {
+        if(linkToCopy) {
           navigator.clipboard.writeText(linkToCopy.href);
         } else {
           navigator.clipboard.writeText(elem.baseURI.split('#')[0]);
         }
         $(elem).addClass('copied');
-        return;
       });
     });
 
