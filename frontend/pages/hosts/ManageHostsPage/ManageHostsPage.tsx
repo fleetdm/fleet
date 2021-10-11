387edaf758ea57c34837cc65cf4ec0883e04673d
Luke Heath <luke@fleetdm.com>
2021-10-11 13:58:56 -0500

    Fix double HTTP requests on hosts search query

--- frontend/pages/hosts/ManageHostsPage/ManageHostsPage.tsx
+++ frontend/pages/hosts/ManageHostsPage/ManageHostsPage.tsx
@@ -343,7 +343,7 @@ const ManageHostsPage = ({
     }
 
     retrieveHosts(options);
-  }, [location, tableQueryData, labels]);
+  }, [location, labels]);
 
   const handleLabelChange = ({ slug }: ILabel) => {
     if (!slug) {
@@ -530,8 +530,8 @@ const ManageHostsPage = ({
 
     // Rebuild queryParams to dispatch new browser location to react-router
     const newQueryParams: { [key: string]: any } = {};
-    if (!isEmpty(searchQuery)) {
-      newQueryParams.query = searchQuery;
+    if (!isEmpty(searchText)) {
+      newQueryParams.query = searchText;
     }
 
     newQueryParams.order_key = sort[0].key || DEFAULT_SORT_HEADER;
