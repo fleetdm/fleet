04acf351999494d77e8b64790a5b4ec9582768e1
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-10-20 17:01:19 -0400

    Fix hosts.spec.ts uncommented tests

--- cypress/integration/all/app/hosts.spec.ts
+++ cypress/integration/all/app/hosts.spec.ts
@@ -11,6 +11,7 @@ describe(
       cy.login();
       cy.addDockerHost();
       cy.clearDownloads();
+      cy.seedQueries();
     });
 
     afterEach(() => {
@@ -65,98 +66,86 @@ describe(
     // Test commented out
     // Pending fix to prevent consistent failing in GitHub
 
-    // it("Can delete a host from host details page", () => {
-    //   let hostname = "";
+    it("Can query a host and delete a host from host details page", () => {
+      let hostname = "";
 
-    //   cy.visit("/hosts/manage");
+      cy.visit("/hosts/manage");
 
-    //   cy.get("tbody").within(() => {
-    //     cy.get(".button--text-link").first().as("hostLink");
-    //   });
+      cy.wait(3000);
 
-    //   cy.get("@hostLink")
-    //     // Set hostname variable for later assertions
-    //     .then((el) => {
-    //       console.log(el);
-    //       hostname = el.text();
-    //       return el;
-    //     })
-    //     .click()
-    //     .then(() => {
-    //       cy.findByText(/about this host/i).should("exist");
-    //       cy.findByText(hostname).should("exist");
+      cy.get("tbody").within(() => {
+        cy.get(".button--text-link").first().as("hostLink");
+      });
 
-    //       // Open delete host modal and cancel
-    //       cy.get('img[alt="Delete host icon"]').click();
-    //       cy.get(".modal__modal_container").within(() => {
-    //         cy.findByText(/delete host/i).should("exist");
-    //         cy.findByRole("button", { name: /cancel/i }).click();
-    //       });
-    //       cy.findByText(/delete host/i).should("not.exist");
+      cy.get("@hostLink")
+        // Set hostname variable for later assertions
+        .then((el) => {
+          console.log(el);
+          hostname = el.text();
+          return el;
+        })
+        .click()
+        .then(() => {
+          cy.findByText(/about this host/i).should("exist");
+          cy.findByText(hostname).should("exist");
 
-    //       // Open delete host modal and delete host
-    //       cy.get('img[alt="Delete host icon"]').click();
-    //       cy.get(".modal__modal_container")
-    //         .within(() => {
-    //           cy.findByText(/delete host/i).should("exist");
-    //           cy.findByRole("button", { name: /delete/i }).click();
-    //         })
-    //         .then(() => {
-    //           cy.findByText(/successfully deleted/i).should("exist");
-    //           cy.findByText(/generate installer/i).should("exist");
-    //           cy.findByText(/about this host/i).should("not.exist");
-    //           cy.findByText(hostname).should("not.exist");
-    //         });
-    //     });
-    // });
+          // Open query host modal and cancel
+          cy.get('img[alt="Query host icon"]').click();
+          cy.get(".modal__modal_container").within(() => {
+            cy.findByText(/select a query/i).should("exist");
+            cy.get(".modal__ex").click();
+          });
+          cy.findByText(/select a query/i).should("not.exist");
 
-    // Test commented out
-    // Pending fix to prevent consistent failing in GitHub
+          // Open query host modal and select query
+          cy.get('img[alt="Query host icon"]').click();
+          cy.get(".modal__modal_container")
+            .within(() => {
+              cy.findByText(/select a query/i).should("exist");
+              cy.findByText(/detect presence/i).click();
+            })
+            .then(() => {
+              cy.findByText(/run query/i).click();
+              cy.get(".data-table").within(() => {
+                cy.findByText(hostname).should("exist");
+              });
+            });
+        });
 
-    // it("Can query a host from host details page", () => {
-    //   cy.seedQueries();
+      cy.visit("/hosts/manage");
 
-    //   let hostname = "";
+      cy.wait(3000);
+      cy.get("@hostLink")
+        // Set hostname variable for later assertions
+        .then((el) => {
+          console.log(el);
+          hostname = el.text();
+          return el;
+        })
+        .click()
+        .then(() => {
+          // Open delete host modal and cancel
+          cy.get('img[alt="Delete host icon"]').click();
+          cy.get(".modal__modal_container").within(() => {
+            cy.findByText(/delete host/i).should("exist");
+            cy.findByRole("button", { name: /cancel/i }).click();
+          });
+          cy.findByText(/delete host/i).should("not.exist");
 
-    //   cy.visit("/hosts/manage");
-
-    //   cy.get("tbody").within(() => {
-    //     cy.get(".button--text-link").first().as("hostLink");
-    //   });
-
-    //   cy.get("@hostLink")
-    //     // Set hostname variable for later assertions
-    //     .then((el) => {
-    //       hostname = el.text();
-    //       return el;
-    //     })
-    //     .click()
-    //     .then(() => {
-    //       cy.findByText(/about this host/i).should("exist");
-    //       cy.findByText(hostname).should("exist");
-
-    //       // Open query host modal and cancel
-    //       cy.get('img[alt="Query host icon"]').click();
-    //       cy.get(".modal__modal_container").within(() => {
-    //         cy.findByText(/select a query/i).should("exist");
-    //         cy.get(".modal__ex").click();
-    //       });
-    //       cy.findByText(/select a query/i).should("not.exist");
-
-    //       // Open query host modal and select query
-    //       cy.get('img[alt="Query host icon"]').click();
-    //       cy.get(".modal__modal_container")
-    //         .within(() => {
-    //           cy.findByText(/select a query/i).should("exist");
-    //           cy.findByText(/detect presence/i).click();
-    //         })
-    //         .then(() => {
-    //           cy.findByText(/edit & run query/i).should("exist");
-    //           cy.get(".target-select").within(() => {
-    //             cy.findByText(hostname).should("exist");
-    //           });
-    //         });
-    //     });
-    // });
+          // Open delete host modal and delete host
+          cy.get('img[alt="Delete host icon"]').click();
+          cy.get(".modal__modal_container")
+            .within(() => {
+              cy.findByText(/delete host/i).should("exist");
+              cy.findByRole("button", { name: /delete/i }).click();
+            })
+            .then(() => {
+              cy.findByText(/add your devices to fleet/i).should("exist");
+              cy.findByText(/generate installer/i).should("exist");
+              cy.findByText(/about this host/i).should("not.exist");
+              cy.findByText(hostname).should("not.exist");
+            });
+        });
+    });
   }
 );
