950953359c296f40a92948cb8b352f84ed637c13
Zach Wasserman <zach@fleetdm.com>
2021-10-20 18:03:41 -0700

    Make migrations compatible with GTID replication
    Fixes an issue some deployments encountered when migrations used a
    statement that is unsupported in GTID replication mode (#2462).
    
    Local dev MySQL now enforces this consistency, so it should be easier to
    maintain compatibility going forward.

--- server/datastore/mysql/migrations/tables/20210819131107_AddCascadeToHostSoftware.go
+++ server/datastore/mysql/migrations/tables/20210819131107_AddCascadeToHostSoftware.go
@@ -32,30 +32,30 @@ func Up_20210819131107(tx *sql.Tx) error {
 
 	// Clear any orphan software and host_software
 	// Note that we can't use CREATE TEMPORARY TABLE here as it caused problems in some MySQL
-	// configurations. See https://github.com/fleetdm/fleet/issues/2462.
-	_, err = tx.Exec(`CREATE TABLE temp_host_software AS SELECT * FROM host_software;`)
+	// configurations (GTID replication). See https://github.com/fleetdm/fleet/issues/2462.
+	_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS temp_host_software LIKE host_software`)
 	if err != nil {
-		return errors.Wrap(err, "save current host software to a temp table")
+		return errors.Wrap(err, "")
 	}
-	_, err = tx.Exec(`DELETE FROM host_software;`)
-	if err != nil {
-		return errors.Wrap(err, "clear all host software")
-	}
-
 	if _, err := tx.Exec(`
-		ALTER TABLE host_software
+		ALTER TABLE temp_host_software
 		ADD FOREIGN KEY host_software_hosts_fk(host_id) REFERENCES hosts (id) ON DELETE CASCADE,
 		ADD FOREIGN KEY host_software_software_fk(software_id) REFERENCES software (id) ON DELETE CASCADE
 	`); err != nil {
 		return errors.Wrap(err, "add fk on host_software hosts & software")
 	}
 
-	_, err = tx.Exec(`INSERT IGNORE INTO host_software SELECT * FROM temp_host_software;`)
+	_, err = tx.Exec(`INSERT IGNORE INTO temp_host_software SELECT * FROM host_software;`)
 	if err != nil {
-		return errors.Wrap(err, "reinserting host software")
+		return errors.Wrap(err, "reinsert host software")
 	}
 
-	_, err = tx.Exec(`DROP TABLE temp_host_software;`)
+	_, err = tx.Exec(`DROP TABLE host_software;`)
+	if err != nil {
+		return errors.Wrap(err, "clear all host software")
+	}
+
+	_, err = tx.Exec(`RENAME TABLE temp_host_software TO host_software`)
 	if err != nil {
 		return errors.Wrap(err, "dropping temp table")
 	}
