30014555285bbf645df0ee473f1c4287e95cdef7
Martavis Parker <martavis@auraticdevelopment.com>
2021-10-22 12:39:36 -0700

    styled and modal

--- frontend/pages/Homepage/cards/WelcomeHost/WelcomeHost.tsx
+++ frontend/pages/Homepage/cards/WelcomeHost/WelcomeHost.tsx
@@ -3,15 +3,22 @@ import PATHS from "router/paths";
 import { Link } from "react-router";
 import { useQuery } from "react-query";
 import { useDispatch } from "react-redux";
+import moment from "moment";
 
 import { IHost } from "interfaces/host";
+import { IHostPolicy } from "interfaces/host_policy";
 import hostAPI from "services/entities/hosts"; //@ts-ignore
 import { renderFlash } from "redux/nodes/notifications/actions";
 
 import Spinner from "components/loaders/Spinner";
+import Button from "components/buttons/Button";
+import Modal from "components/modals/Modal";
 import LaptopMac from "../../../../../assets/images/laptop-mac.png";
 import LinkArrow from "../../../../../assets/images/icon-arrow-right-vibrant-blue-10x18@2x.png";
-import ErrorIcon from "../../../../../assets/images/icon-action-disable-red-16x16@2x.png";
+import IconDisabled from "../../../../../assets/images/icon-action-disable-red-16x16@2x.png";
+import IconPassed from "../../../../../assets/images/icon-check-circle-green-16x16@2x.png";
+import IconError from "../../../../../assets/images/icon-exclamation-circle-red-16x16@2x.png";
+import IconChevron from "../../../../../assets/images/icon-chevron-purple-9x6@2x.png";
 
 interface IHostResponse {
   host: IHost;
@@ -23,6 +30,8 @@ const HOST_ID = 37;
 const WelcomeHost = (): JSX.Element => {
   const dispatch = useDispatch();
   const [refetchStartTime, setRefetchStartTime] = useState<number | null>(null);
+  const [currentPolicyShown, setCurrentPolicyShown] = useState<IHostPolicy>();
+  const [showPolicyModal, setShowPolicyModal] = useState<boolean>(false);
   const [
     showRefetchLoadingSpinner,
     setShowRefetchLoadingSpinner,
@@ -39,16 +48,10 @@ const WelcomeHost = (): JSX.Element => {
     {
       select: (data: IHostResponse) => data.host,
       onSuccess: (returnedHost) => {
+        setShowRefetchLoadingSpinner(returnedHost.refetch_requested);
         if (returnedHost.refetch_requested) {
-          // If the API reports that a Fleet refetch request is pending, we want to check back for fresh
-          // host details. Here we set a one second timeout and poll the API again using
-          // fullyReloadHost. We will repeat this process with each onSuccess cycle for a total of
-          // 60 seconds or until the API reports that the Fleet refetch request has been resolved
-          // or that the host has gone offline.
+          // Code duplicated from HostDetailsPage. See comments there.
           if (!refetchStartTime) {
-            // If our 60 second timer wasn't already started (e.g., if a refetch was pending when
-            // the first page loads), we start it now if the host is online. If the host is offline,
-            // we skip the refetch on page load.
             if (returnedHost.status === "online") {
               setRefetchStartTime(Date.now());
               setTimeout(() => {
@@ -94,6 +97,32 @@ const WelcomeHost = (): JSX.Element => {
     }
   );
   
+  const onRefetchHost = async () => {
+    if (host) {
+      setShowRefetchLoadingSpinner(true);
+
+      try {
+        await hostAPI.refetch(host).then(() => {
+          setRefetchStartTime(Date.now());
+          setTimeout(() => fullyReloadHost(), 1000);
+        });
+      } catch (error) {
+        console.log(error);
+        dispatch(renderFlash("error", `Host "${host.hostname}" refetch error`));
+        setShowRefetchLoadingSpinner(false);
+      }
+    }
+  };
+
+  const handlePolicyModal = (id: number) => {
+    const policy = host?.policies.find((p) => p.id === id);
+
+    if (policy) {
+      setCurrentPolicyShown(policy);
+      setShowPolicyModal(true);
+    }
+  };
+  
   if (isLoadingHost) {
     return (
       <div className={baseClass}>
@@ -109,7 +138,7 @@ const WelcomeHost = (): JSX.Element => {
     return (
       <div className={baseClass}>
         <div className={`${baseClass}__error`}>
-          <p><img alt="" src={ErrorIcon} />Your device is not communicating with Fleet.</p>
+          <p><img alt="" src={IconDisabled} />Your device is not communicating with Fleet.</p>
           <p>Join the #fleet Slack channel for help troubleshooting.</p>
         </div>
       </div>
@@ -120,7 +149,7 @@ const WelcomeHost = (): JSX.Element => {
     return (
       <div className={baseClass}>
         <div className={`${baseClass}__error`}>
-          <p><img alt="" src={ErrorIcon} />No policies apply to your device.</p>
+          <p><img alt="" src={IconDisabled} />No policies apply to your device.</p>
           <p>Join the #fleet Slack channel for help troubleshooting.</p>
         </div>
       </div>
@@ -133,7 +162,7 @@ const WelcomeHost = (): JSX.Element => {
         <div className={`${baseClass}__intro`}>
           <img alt="" src={LaptopMac} />
           <div className="info">
-            <Link to={PATHS.HOST_DETAILS(host)}>
+            <Link to={PATHS.HOST_DETAILS(host)} className="external-link">
               {host.hostname}
               <img src={LinkArrow} />
             </Link>
@@ -144,14 +173,56 @@ const WelcomeHost = (): JSX.Element => {
           <p>Fleet already ran the following checks to assess the security of your device: </p>
         </div>
         <div className={`${baseClass}__policies`}>
-          {host.policies?.map((p) => (
-            <div className="policy-block">{p.query_name}</div>
+          {host.policies?.slice(0, 10).map((p) => (
+            <div className="policy-block">
+              <div className="info">
+                <img alt={p.response} src={p.response === "passing" ? IconPassed : IconError} />
+                {p.query_name}
+              </div>
+              <Button variant="text-icon" onClick={() => handlePolicyModal(p.id)}><img alt="" src={IconChevron} /></Button>
+            </div>
           ))}
+          {host.policies?.length > 10 && (
+            <Link to={PATHS.HOST_DETAILS(host)} className="external-link">
+              Go to Host details to see all checks
+              <img src={LinkArrow} />
+            </Link>
+          )}
         </div>
         <div className={`${baseClass}__blurb`}>
           <p>Resolved a failing check? Refetch your device information to verify.</p>
         </div>
-        <div className={`${baseClass}__refetch`}></div>
+        <div className={`${baseClass}__refetch`}>
+          <Button
+            variant="blue-green"
+            className={`refetch-spinner ${showRefetchLoadingSpinner ? "spin" : ""}`}
+            onClick={onRefetchHost}
+            disabled={showRefetchLoadingSpinner}
+          >
+            Refetch
+          </Button>
+          <span>Last updated {moment(host.detail_updated_at).fromNow()}</span>
+        </div>
+        {showPolicyModal && (
+          <Modal
+            title={currentPolicyShown?.query_name || ""}
+            onExit={() => setShowPolicyModal(false)}
+            className={`${baseClass}__policy-modal`}
+          >
+            <>
+              <p>{currentPolicyShown?.query_description}</p>
+              {currentPolicyShown?.resolution && (
+                <p>
+                  <b>Resolve:</b>
+                  {currentPolicyShown.resolution}
+                </p>
+              )}
+              <div className="done">
+                <Button variant="brand" onClick={() => setShowPolicyModal(false)}>Done</Button>
+              </div>
+            </>
+          </Modal>
+        )}
       </div>
     );
   }
