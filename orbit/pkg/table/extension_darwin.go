8258593db945af8abf8a2499ff31840b9811d42d
Victor Vrantchan <vrancean+github@gmail.com>
2021-10-29 21:18:25 -0400

    add more tables

--- orbit/pkg/table/extension_darwin.go
+++ orbit/pkg/table/extension_darwin.go
@@ -6,11 +6,18 @@ import (
 	"github.com/kolide/osquery-go"
 	"github.com/kolide/osquery-go/plugin/table"
 
+	"github.com/macadmins/osquery-extension/tables/macos_profiles"
 	"github.com/macadmins/osquery-extension/tables/mdm"
+	"github.com/macadmins/osquery-extension/tables/munki"
 )
 
 func platformTables() []osquery.OsqueryPlugin {
 	var plugins []osquery.OsqueryPlugin
-	plugins = append(plugins, table.NewPlugin("mdm", mdm.MDMInfoColumns(), mdm.MDMInfoGenerate))
+	plugins = append(plugins,
+		table.NewPlugin("mdm", mdm.MDMInfoColumns(), mdm.MDMInfoGenerate),
+		table.NewPlugin("macos_profiles", macos_profiles.MacOSProfilesColumns(), macos_profiles.MacOSProfilesGenerate),
+		table.NewPlugin("munki_info", munki.MunkiInfoColumns(), munki.MunkiInfoGenerate),
+		table.NewPlugin("munki_installs", munki.MunkiInstallsColumns(), munki.MunkiInstallsGenerate),
+	)
 	return plugins
 }
