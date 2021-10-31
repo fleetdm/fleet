a37194ae54041bdb3babdf0497ff47bec28e6ea4
Martavis Parker <martavis@auraticdevelopment.com>
2021-10-31 08:08:36 -0700

    added checkbox story

---
+++ frontend/components/forms/fields/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+
+import Checkbox from ".";
+
+import { ICheckboxProps } from "./Checkbox";
+
+import "../../../../index.scss";
+
+export default {
+  component: Checkbox,
+  title: 'Components/FormFields/Checkbox',
+  args: {
+    value: false,
+    disabled: false,
+    indeterminate: false,
+    className: "",
+    name: "",
+    wrapperClassName: "",
+    onChange: () => {},
+  }
+} as Meta;
+
+const Template: Story<ICheckboxProps> = (props) => <Checkbox {...props} />;
+
+export const Default = Template.bind({});
