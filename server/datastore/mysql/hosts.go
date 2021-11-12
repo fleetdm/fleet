de6d7b4877e3dbffc980284120df75966064ac7a
Tomas Touceda <chiiph@gmail.com>
2021-11-12 16:59:27 -0300

    Rename column

--- server/datastore/mysql/hosts.go
+++ server/datastore/mysql/hosts.go
@@ -473,7 +473,7 @@ func (d *Datastore) applyHostFilters(opt fleet.HostListOptions, sql string, filt
 		LEFT JOIN host_seen_times hst ON (h.id=hst.host_id)
 		LEFT JOIN teams t ON (h.team_id = t.id)
 		LEFT JOIN (
-		    select host_id, count(*) from policy_membership_history where passes=false and id in (select max(id) as id from policy_membership_history where passes=false group by host_id, policy_id)				    	
+		    select host_id, count(*) as count from policy_membership_history where passes=false and id in (select max(id) as id from policy_membership_history where passes=false group by host_id, policy_id)				    	
 		    GROUP BY host_id
 		) as failing_policies ON (h.id=failing_policies.host_id)
 		%s
