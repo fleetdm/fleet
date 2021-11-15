bde15488bdb4f1a26a0625ac0b7db298f03f74fc
Martin Angers <martin.n.angers@gmail.com>
2021-11-15 10:59:44 -0500

    Start moving error handling in service layer

--- server/service/client_debug.go
+++ server/service/client_debug.go
@@ -1,10 +1,9 @@
 package service
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/pkg/errors"
 )
 
 // DebugPprof calls the /debug/pprof/ endpoints.
@@ -12,12 +11,12 @@ func (c *Client) DebugPprof(name string) ([]byte, error) {
 	endpoint := "/debug/pprof/" + name
 	response, err := c.AuthenticatedDo("GET", endpoint, "", nil)
 	if err != nil {
-		return nil, errors.Wrapf(err, "GET %s", endpoint)
+		return nil, fmt.Errorf("GET %s: %w", endpoint, err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		return nil, errors.Errorf(
+		return nil, fmt.Errorf(
 			"get pprof received status %d",
 			response.StatusCode,
 		)
@@ -25,7 +24,7 @@ func (c *Client) DebugPprof(name string) ([]byte, error) {
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, errors.Wrap(err, "read pprof response body")
+		return nil, fmt.Errorf("read pprof response body: %w", err)
 	}
 
 	return body, nil
