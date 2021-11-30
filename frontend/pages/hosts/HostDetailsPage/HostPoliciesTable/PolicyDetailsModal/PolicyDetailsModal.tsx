2a39f2ce2512a60baa3299a12ae4d4538fa6cf51
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-11-30 11:22:42 -0500

    Fix lint

--- frontend/pages/hosts/HostDetailsPage/HostPoliciesTable/PolicyDetailsModal/PolicyDetailsModal.tsx
+++ frontend/pages/hosts/HostDetailsPage/HostPoliciesTable/PolicyDetailsModal/PolicyDetailsModal.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "components/buttons/Button";
 import Modal from "components/Modal";
 
-import { IHostPolicy } from "interfaces/host_policy";
+import { IHostPolicy } from "interfaces/policy";
 
 interface IPolicyDetailsProps {
   onCancel: () => void;
