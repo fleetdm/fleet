dec95192e06b233561e130fa55d70d895ab33900
Luke Heath <luke@fleetdm.com>
2021-12-02 21:50:05 -0600

    Fix duplicate hosts on render when host_id URL params are present

--- frontend/pages/queries/QueryPage/QueryPage.tsx
+++ frontend/pages/queries/QueryPage/QueryPage.tsx
@@ -55,6 +55,9 @@ const QueryPage = ({
     setLastEditedQueryObserverCanRun,
   } = useContext(QueryContext);
 
+  const [queryParamHostsAdded, setQueryParamHostsAdded] = useState<boolean>(
+    false
+  );
   const [step, setStep] = useState<string>(QUERIES_PAGE_STEPS[1]);
   const [selectedTargets, setSelectedTargets] = useState<ITarget[]>([]);
   const [isLiveQueryRunnable, setIsLiveQueryRunnable] = useState<boolean>(true);
@@ -93,7 +96,7 @@ const QueryPage = ({
     "hostFromURL",
     () => hostAPI.load(parseInt(URLQuerySearch.host_ids as string, 10)),
     {
-      enabled: !!URLQuerySearch.host_ids,
+      enabled: !!URLQuerySearch.host_ids && !queryParamHostsAdded,
       select: (data: IHostResponse) => data.host,
       onSuccess: (data) => {
         const targets = selectedTargets;
@@ -103,6 +106,10 @@ const QueryPage = ({
 
         targets.push(hostTarget as IHost);
         setSelectedTargets([...targets]);
+
+        if (!queryParamHostsAdded) {
+          setQueryParamHostsAdded(true);
+        }
       },
     }
   );
