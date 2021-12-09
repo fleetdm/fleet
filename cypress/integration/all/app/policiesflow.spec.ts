e252fac9a7a5f8101dad0c2d9803436468d8ef47
Rachel Elysia Perkins <rachel@fleetdm.com>
2021-12-09 15:52:41 -0800

    Update policiesflow

--- cypress/integration/all/app/policiesflow.spec.ts
+++ cypress/integration/all/app/policiesflow.spec.ts
@@ -8,7 +8,7 @@ describe(
       cy.setup();
       cy.login();
     });
-    it("Can create, check, and delete a policy successfully", () => {
+    it("Can create, update, and delete a policy successfully", () => {
       cy.visit("/policies/manage");
       cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
 
@@ -16,61 +16,122 @@ describe(
       cy.findByText(/add a policy/i).click();
       cy.findByText(/create your own policy/i).click();
 
-      // Confirm that policy was added successfully
-      // cy.findByText(/successfully added policy/i).should("exist");
-      // cy.findByText(/select query/i).should("not.exist");
-      // cy.get(".policies-list-wrapper").within(() => {
-      //   cy.findByText(/1 query/i).should("exist");
-      //   cy.findByText(/yes/i).should("exist");
-      //   cy.findByText(
-      //     /Detect Linux hosts with high severity vulnerable versions of OpenSSL/i
-      //   ).should("exist");
+      cy.get(".ace_scroller")
+        .click({ force: true })
+        .type(
+          "{selectall}SELECT 1 FROM users WHERE username = 'backup' LIMIT 1;"
+        );
 
-      // Click on link in table and confirm that policies filter block diplays as expected on manage hosts page
-      //   cy.get("tbody").within(() => {
-      //     cy.get("tr")
-      //       .first()
-      //       .within(() => {
-      //         cy.get("td").last().children().first().should("exist").click();
-      //       });
-      //   });
-      // });
-      // cy.get(".manage-hosts__policies-filter-block").within(() => {
-      //   cy.findByText(
-      //     /Detect Linux hosts with high severity vulnerable versions of OpenSSL/i
-      //   ).should("exist");
-      //   cy.findByText(/yes/i).should("not.exist");
-      //   cy.findByText(/failing/i)
-      //     .should("exist")
-      //     .click();
-      //   cy.findByText(/yes/i).should("exist");
-      //   cy.get('img[alt="Remove policy filter"]').click();
-      //   cy.findByText(
-      //     /Detect Linux hosts with high severity vulnerable versions of OpenSSL/i
-      //   ).should("not.exist");
-      // });
+      cy.findByRole("button", { name: /save policy/i }).click();
+
+      // save modal
+      cy.get(".policy-form__policy-save-modal-name")
+        .click()
+        .type("Does the device have a user named 'backup'?");
+
+      cy.get(".policy-form__policy-save-modal-description")
+        .click()
+        .type("Returns yes or no for having a user named 'backup'");
+
+      cy.get(".policy-form__policy-save-modal-resolution")
+        .click()
+        .type("Create a user named 'backup'");
+
+      cy.findByRole("button", { name: /^Save$/ }).click();
+
+      // Confirm that policy was added successfully
+      cy.findByText(/policy created/i).should("exist");
+
+      cy.visit("/policies/manage");
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+
+      // Add a default policy
+      cy.findByText(/add a policy/i).click();
+
+      cy.findByText(/gatekeeper enabled/i).click();
+      cy.findByRole("button", { name: /save policy/i }).click();
+      cy.findByRole("button", { name: /^Save$/ }).click();
+
+      // Confirm that policy was added successfully
+      cy.findByText(/policy created/i).should("exist");
+
+      cy.visit("/policies/manage");
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+
+      cy.get(".policies-list-wrapper").within(() => {
+        cy.findByText(/backup/i).should("exist");
+        cy.findByText(/gatekeeper/i).should("exist");
+
+        // Click on link in table and confirm that policies filter block diplays as expected on manage hosts page
+        cy.get("tbody").within(() => {
+          cy.get("tr")
+            .first()
+            .within(() => {
+              cy.get("td").last().children().first().should("exist").click();
+            });
+        });
+      });
+
+      // confirm policy functionality on manage host page
+      cy.get(".manage-hosts__policies-filter-block").within(() => {
+        cy.findByText(/user named 'backup'/i).should("exist");
+        cy.findByText(/no/i).should("exist").click();
+        cy.findByText(/yes/i).should("exist");
+        cy.get('img[alt="Remove policy filter"]').click();
+        cy.findByText(/user named 'backup'/i).should("not.exist");
+      });
 
       // Click on policies tab to return to manage policies page
-      // cy.get(".site-nav-container").within(() => {
-      //   cy.findByText(/policies/i).click();
-      // });
+      cy.get(".site-nav-container").within(() => {
+        cy.findByText(/policies/i).click();
+      });
+
+      // Update policy
+      cy.findByText(/gatekeeper enabled/i).click();
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+
+      // TODO: Detached dom issues
+      // cy.findByText(/gatekeeper enabled on macOS/i)
+      //   .click()
+      //   .type("{selectall}Gatekeeper enabled on macOS");
+      // cy.findByText(/checks to make sure/i)
+      //   .click()
+      //   .type(
+      //     "{selectall}Gatekeeper helps ensure only trusted software is running"
+      //   );
+      // cy.findByText(/failing device/i)
+      //   .click()
+      //   .type("{selectall}Run /user/sbin/spctl--master -enable");
+
+      cy.get(".ace_scroller")
+        .click({ force: true })
+        .type(
+          "{selectall}SELECT 1 FROM gatekeeper WHERE assessments_enabled = 1;"
+        );
+
+      cy.findByRole("button", { name: /^Save$/ }).click();
+
+      // Confirm that policy was added successfully
+      cy.findByText(/policy updated/i).should("exist");
 
       // Delete policy
-      // cy.get("tbody").within(() => {
-      //   cy.get("tr")
-      //     .first()
-      //     .within(() => {
-      //       cy.get(".fleet-checkbox__input").check({ force: true });
-      //     });
-      // });
-      // cy.findByRole("button", { name: /remove/i }).click();
-      // cy.get(".remove-policies-modal").within(() => {
-      //   cy.findByRole("button", { name: /cancel/i }).should("exist");
-      //   cy.findByRole("button", { name: /remove/i }).click();
-      // });
-      // cy.findByText(
-      //   /Detect Linux hosts with high severity vulnerable versions of OpenSSL/i
-      // ).should("not.exist");
+      cy.visit("/policies/manage");
+      cy.wait(2000); // eslint-disable-line cypress/no-unnecessary-waiting
+
+      cy.get("tbody").within(() => {
+        cy.get("tr")
+          .first()
+          .within(() => {
+            cy.get(".fleet-checkbox__input").check({ force: true });
+          });
+      });
+      cy.findByRole("button", { name: /delete/i }).click();
+      cy.get(".remove-policies-modal").within(() => {
+        cy.findByRole("button", { name: /cancel/i }).should("exist");
+        cy.findByRole("button", { name: /delete/i }).click();
+      });
+      cy.findByText(/removed policy/i).should("exist");
+      cy.findByText(/backup/i).should("not.exist");
     });
   }
 );
