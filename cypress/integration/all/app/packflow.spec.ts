c491616eb594199798621373094e8ffd4c7b9ffb
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2021-12-13 13:44:28 -0800

    Manage Host Page Bug: Update observers to not see generate installer CTA (#3352)

--- cypress/integration/all/app/packflow.spec.ts
+++ cypress/integration/all/app/packflow.spec.ts
@@ -24,6 +24,7 @@ describe(
       cy.findByRole("button", { name: /save query pack/i }).click();
 
       cy.visit("/packs/manage");
+      cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
       cy.findByText(/errors and crashes/i).click();
 
       cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
