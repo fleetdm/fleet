6524305594063610a032da2ab6fe019a7fc4ec8c
Zach Wasserman <zach@fleetdm.com>
2021-12-19 17:06:17 -0800

    Build Orbit releases in GitHub Actions (#3413)
    Complete build including macOS signing+notarization.

---
+++ orbit/.gon.hcl
@@ -0,0 +1,6 @@
+# Use default credentials from environment
+
+notarize {
+  path = "./dist/orbit_darwin_amd64.zip"
+  bundle_id = "com.fleetdm.orbit"
+}
--- tools.go
+++ tools.go
@@ -5,5 +5,7 @@ package tools
 
 import (
 	_ "github.com/fleetdm/goose"
+	_ "github.com/goreleaser/goreleaser"
 	_ "github.com/kevinburke/go-bindata"
+	_ "github.com/mitchellh/gon/cmd/gon"
 )
