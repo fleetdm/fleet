729d3e1f2dc81449a27f5b5c9a4e8d1051bf16da
eashaw <caglc@live.com>
2021-12-21 11:26:56 -0600

    style updates, order confirmation

--- website/assets/js/pages/customers/new-license.page.js
+++ website/assets/js/pages/customers/new-license.page.js
@@ -23,17 +23,22 @@ parasails.registerPage('new-license', {
     // Success state when form has been submitted
     cloudSuccess: false,
     showBillingForm: false,
+    showQuotedPrice: false,
     quotedPrice: undefined,
     numberOfHostsQuoted: undefined,
     thirtyDaysFromTodayInMS: Date.now() + 30*24*60*60*1000,
-    // stripePaymentOptions: {},
+    calendlyLink: '',
+    orderComplete: false,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function() {
+    let today = new Date(Date.now());
 
+    // note: we keep loading spinner present indefinitely so that it is apparent that a new page is loading
+    this.calendlyLink = 'https://calendly.com/fleetdm/demo?month='+today.getFullYear()+'-'+today.getMonth();
   },
   mounted: async function() {
     //…
@@ -48,28 +53,39 @@ parasails.registerPage('new-license', {
     submittedPaymentForm: async function() {
       // After payment is submitted, take the user to their dashboard
       this.syncing = true;
+      this.orderComplete = true;
+      this.syncing = false;
+      // window.location = '/customers/dashboard';
+    },
+    clickGoToDashboard: async function() {
+      // After payment is submitted, take the user to their dashboard
+      this.syncing = true;
       window.location = '/customers/dashboard';
     },
 
     submittedQuoteForm: async function(quote) {
 
-      if(quote.numberOfHosts > 100) {
-        let today = new Date(Date.now());
-        this.syncing = true;
-        // note: we keep loading spinner present indefinitely so that it is apparent that a new page is loading
-        window.location = 'https://calendly.com/fleetdm/demo?month='+today.getFullYear()+'-'+today.getMonth();
-      } else {
-        this.numberOfHostsQuoted = quote.numberOfHosts;
+      this.showQuotedPrice = true;
+      this.quotedPrice = quote.quotedPrice;
+      this.numberOfHostsQuoted = quote.numberOfHosts;
+      if(quote.numberOfHosts <= 100) {
         this.formData.quoteId = quote.id;
-        this.quotedPrice = quote.quotedPrice;
         this.showBillingForm = true;
+        // this.syncing = true;
       }
 
+    },
+
+    clickScheduleDemo: async function() {
+      this.syncing = true;
+      // note: we keep loading spinner present indefinitely so that it is apparent that a new page is loading
+      window.location = this.calendlyLink;
     },
 
     clickResetForm: async function() {
       this.formData = {};
       this.showBillingForm = false;
+      this.showQuotedPrice = false;
     },
 
 
