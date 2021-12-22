97ef20451fecbaf8b2765b96783ebf1bd2e27b39
Benjamin Edwards <edwards.benw@gmail.com>
2021-12-22 15:23:52 -0500

    rename resources

--- tools/terraform/percona/network.tf
+++ tools/terraform/percona/network.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate" "loadtest_fleetdm_com" {
+resource "aws_acm_certificate" "certificate" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
@@ -7,9 +7,9 @@ resource "aws_acm_certificate" "loadtest_fleetdm_com" {
   }
 }
 
-resource "aws_route53_record" "loadtest_fleetdm_com_validation" {
+resource "aws_route53_record" "record" {
   for_each = {
-    for dvo in aws_acm_certificate.loadtest_fleetdm_com.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -25,6 +25,6 @@ resource "aws_route53_record" "loadtest_fleetdm_com_validation" {
 }
 
 resource "aws_acm_certificate_validation" "percona" {
-  certificate_arn         = aws_acm_certificate.loadtest_fleetdm_com.arn
-  validation_record_fqdns = [for record in aws_route53_record.loadtest_fleetdm_com_validation : record.fqdn]
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.record : record.fqdn]
 }
