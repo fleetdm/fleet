597144bfacfe12f39ffc2968b01149d0608c109b
Martin Angers <martin.n.angers@gmail.com>
2022-01-10 14:43:39 -0500

    Migrate most users endpoints to the new pattern (#3366)

---
+++ server/service/sessions_test.go
@@ -0,0 +1,8 @@
+package service
+
+// TODO(mna): when migrating Session-related endpoints, add auth tests for those
+// endpoints (the auth is session-based, not user-based).
+//_, err = svc.GetInfoAboutSessionsForUser(ctx, 999)
+//checkAuthErr(t, tt.shouldFailTeamWrite, err)
+//_, err = svc.GetInfoAboutSessionsForUser(ctx, 888)
+//checkAuthErr(t, tt.shouldFailGlobalWrite, err)
