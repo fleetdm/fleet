e64f1a63ba77e76cfb5f294a78738033bb86ed31
Luke Heath <luke@fleetdm.com>
2022-01-12 18:16:50 -0600

    Correct instances of multiple requests for same resource (#3631)

--- frontend/components/TeamsDropdown/TeamsDropdown.tsx
+++ frontend/components/TeamsDropdown/TeamsDropdown.tsx
@@ -34,7 +34,8 @@ const generateDropdownOptions = (
 interface ITeamsDropdownProps {
   currentUserTeams: ITeam[];
   selectedTeamId: number;
-  includeAll?: boolean;
+  includeAll?: boolean; // Include "All Teams" option for all users
+  disableAll?: boolean; // Disable "All Teams" option for global users
   isDisabled?: boolean;
   onChange: (newSelectedValue: number) => void;
   onOpen?: () => void;
@@ -46,7 +47,8 @@ const baseClass = "component__team-dropdown";
 const TeamsDropdown = ({
   currentUserTeams,
   selectedTeamId,
-  includeAll,
+  includeAll = false,
+  disableAll = false,
   isDisabled,
   onChange,
   onOpen,
@@ -56,7 +58,10 @@ const TeamsDropdown = ({
 
   const teamOptions = useMemo(
     () =>
-      generateDropdownOptions(currentUserTeams, isOnGlobalTeam || includeAll),
+      generateDropdownOptions(
+        currentUserTeams,
+        (isOnGlobalTeam && !disableAll) || includeAll
+      ),
     [currentUserTeams, isOnGlobalTeam]
   );
 
