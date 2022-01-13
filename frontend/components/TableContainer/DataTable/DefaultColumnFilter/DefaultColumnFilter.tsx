39b7c7d9f9316a2f907da0ff81e4efe2cad8bfe9
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-01-13 11:06:32 -0600

    Add pagination and multi-column sort to live query results table UI (#3647)
    * Refactor live query results to react-table

---
+++ frontend/components/TableContainer/DataTable/DefaultColumnFilter/DefaultColumnFilter.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { FilterProps, TableInstance } from "react-table";
+
+import SearchField from "components/forms/fields/SearchField";
+
+const DefaultColumnFilter = ({
+  column,
+}: FilterProps<TableInstance>): JSX.Element => {
+  const { setFilter } = column;
+
+  return (
+    <div className={"filter-cell"}>
+      <SearchField
+        placeholder=""
+        onChange={(searchString) => {
+          setFilter(searchString || undefined); // Set undefined to remove the filter entirely
+        }}
+      />
+    </div>
+  );
+};
+
+export default DefaultColumnFilter;
