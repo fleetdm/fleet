c43b30fab92a72750b703594a0f33db2f92a9045
Tomas Touceda <chiiph@gmail.com>
2022-01-18 11:29:57 -0300

    Better jitter (#3716)
    * Better jitter
    
    * Fix lint
    
    * Use milliseconds
    
    * Make duration milliseconds
    
    * Update based on Lucas' suggestion
    
    * Add changes file
    
    * Panic on error
    
    * Fix compilation error

--- cmd/fleet/serve.go
+++ cmd/fleet/serve.go
@@ -280,7 +280,11 @@ the way that the Fleet server works.
 				RedisScanKeysCount: config.Osquery.AsyncHostRedisScanKeysCount,
 				CollectorInterval:  config.Osquery.AsyncHostCollectInterval,
 			}
-			svc, err := service.NewService(ds, task, resultStore, logger, osqueryLogger, config, mailService, clock.C, ssoSessionStore, liveQueryStore, carveStore, *license, failingPolicySet)
+
+			// TODO: gather all the different contexts and use just one
+			ctx, cancelFunc := context.WithCancel(context.Background())
+			defer cancelFunc()
+			svc, err := service.NewService(ctx, ds, task, resultStore, logger, osqueryLogger, config, mailService, clock.C, ssoSessionStore, liveQueryStore, carveStore, *license, failingPolicySet)
 			if err != nil {
 				initFatal(err, "initializing service")
 			}
@@ -370,9 +374,6 @@ the way that the Fleet server works.
 			// Instantiate a gRPC service to handle launcher requests.
 			launcher := launcher.New(svc, logger, grpc.NewServer(), healthCheckers)
 
-			// TODO: gather all the different contexts and use just one
-			ctx, cancelFunc := context.WithCancel(context.Background())
-			defer cancelFunc()
 			eh := errorstore.NewHandler(ctx, redisPool, logger, config.Logging.ErrorRetentionPeriod)
 
 			rootMux := http.NewServeMux()
