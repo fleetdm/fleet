2f8c16b4af46d4f64b6b48adcd6a08077d4711a2
Martin Angers <martin.n.angers@gmail.com>
2022-01-19 15:52:40 -0500

    Add appconfig auth check test

--- server/service/appconfig_test.go
+++ server/service/appconfig_test.go
@@ -10,8 +10,100 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/fleetdm/fleet/v4/server/contexts/viewer"
+	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/mock"
+	"github.com/fleetdm/fleet/v4/server/ptr"
 	"github.com/stretchr/testify/require"
 )
+
+func TestAppConfigAuth(t *testing.T) {
+	ds := new(mock.Store)
+	svc := newTestService(ds, nil, nil)
+
+	// start a TLS server and use its URL as the server URL in the app config,
+	// required by the CertificateChain service call.
+	srv := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {}))
+	defer srv.Close()
+
+	ds.AppConfigFunc = func(ctx context.Context) (*fleet.AppConfig, error) {
+		return &fleet.AppConfig{
+			ServerSettings: fleet.ServerSettings{
+				ServerURL: srv.URL,
+			},
+		}, nil
+	}
+	ds.SaveAppConfigFunc = func(ctx context.Context, conf *fleet.AppConfig) error {
+		return nil
+	}
+
+	testCases := []struct {
+		name            string
+		user            *fleet.User
+		shouldFailWrite bool
+		shouldFailRead  bool
+	}{
+		{
+			"global admin",
+			&fleet.User{GlobalRole: ptr.String(fleet.RoleAdmin)},
+			false,
+			false,
+		},
+		{
+			"global maintainer",
+			&fleet.User{GlobalRole: ptr.String(fleet.RoleMaintainer)},
+			true,
+			false,
+		},
+		{
+			"global observer",
+			&fleet.User{GlobalRole: ptr.String(fleet.RoleObserver)},
+			true,
+			false,
+		},
+		{
+			"team admin",
+			&fleet.User{Teams: []fleet.UserTeam{{Team: fleet.Team{ID: 1}, Role: fleet.RoleAdmin}}},
+			true,
+			false,
+		},
+		{
+			"team maintainer",
+			&fleet.User{Teams: []fleet.UserTeam{{Team: fleet.Team{ID: 1}, Role: fleet.RoleMaintainer}}},
+			true,
+			false,
+		},
+		{
+			"team observer",
+			&fleet.User{Teams: []fleet.UserTeam{{Team: fleet.Team{ID: 1}, Role: fleet.RoleObserver}}},
+			true,
+			false,
+		},
+		{
+			"user",
+			&fleet.User{ID: 777},
+			true,
+			false,
+		},
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := viewer.NewContext(context.Background(), viewer.Viewer{User: tt.user})
+
+			_, err := svc.AppConfig(ctx)
+			checkAuthErr(t, tt.shouldFailRead, err)
+
+			_, err = svc.ModifyAppConfig(ctx, []byte(`{}`))
+			checkAuthErr(t, tt.shouldFailWrite, err)
+
+			_, err = svc.Version(ctx)
+			checkAuthErr(t, tt.shouldFailRead, err)
+
+			_, err = svc.CertificateChain(ctx)
+			checkAuthErr(t, tt.shouldFailRead, err)
+		})
+	}
+}
 
 func TestCertificateChain(t *testing.T) {
 	server, teardown := setupCertificateChain(t)
