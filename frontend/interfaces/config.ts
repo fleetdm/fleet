dbd6649293f9a4f963d362850342aca0fdc6a724
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-02-02 07:32:32 -0600

    Fix lint

--- frontend/interfaces/config.ts
+++ frontend/interfaces/config.ts
@@ -113,17 +113,15 @@ export interface IConfig {
   destination_url: string;
   host_percentage: number;
   days_count: number;
-  logging?: {
-    debug: boolean;
-    json: boolean;
-    result: {
-      plugin: string;
-      config: {
-        status_log_file: string;
-        result_log_file: string;
-        enable_log_rotation: boolean;
-        enable_log_compression: boolean;
-      };
+  debug: boolean;
+  json: boolean;
+  result: {
+    plugin: string;
+    config: {
+      status_log_file: string;
+      result_log_file: string;
+      enable_log_rotation: boolean;
+      enable_log_compression: boolean;
     };
   };
   status: {
