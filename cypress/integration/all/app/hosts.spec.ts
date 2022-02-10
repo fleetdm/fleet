645e39ab37c48cd958eb0cda4c351115ae5f80d5
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-02-10 15:16:14 -0600

    Fix dom issue, add default timeout where needed

--- cypress/integration/all/app/hosts.spec.ts
+++ cypress/integration/all/app/hosts.spec.ts
@@ -1,4 +1,5 @@
 import _ = require("cypress/types/lodash");
+import { closeSync } from "fs";
 import * as path from "path";
 
 let hostname = "";
@@ -16,8 +17,8 @@ describe("Hosts flow", () => {
     cy.viewport(1200, 660);
   });
   after(() => {
-    // cy.logout();
-    // cy.stopDockerHost();
+    cy.logout();
+    cy.stopDockerHost();
   });
   describe("Manage hosts page", () => {
     beforeEach(() => {
@@ -116,170 +117,134 @@ describe("Hosts flow", () => {
         cy.getAttached(".button--text-link").first().click();
       });
     });
-    it(
-      "runs query on an existing host",
-      {
-        retries: {
-          runMode: 2,
-        },
-        defaultCommandTimeout: 10000,
-      },
-      () => {
-        cy.getAttached(".host-details__action-button-container").within(() => {
-          cy.getAttached('img[alt="Query host icon"]').click();
-        });
+    it("runs query on an existing host", () => {
+      cy.getAttached(".host-details__action-button-container").within(() => {
+        cy.getAttached('img[alt="Query host icon"]').click();
+      });
 
-        cy.getAttached(".select-query-modal__modal").within(() => {
-          cy.getAttached(".modal-query-button").eq(2).click();
-        });
+      cy.getAttached(".select-query-modal__modal").within(() => {
+        cy.getAttached(".modal-query-button").eq(2).click();
+      });
 
-        cy.getAttached(".query-form__button-wrap--new-query").within(() => {
-          cy.findByText(/run query/i)
-            .should("exist")
-            .click();
-        });
-        cy.getAttached(".query-page__wrapper").within(() => {
-          cy.getAttached(".data-table").within(() => {
-            cy.findByText(hostname).should("exist");
-          });
-          cy.findByText(/run/i).click();
-        });
-      }
-    );
-    it(
-      "renders and searches the host's users",
-      {
-        retries: {
-          runMode: 2,
-        },
-        defaultCommandTimeout: 10000,
-      },
-      () => {
-        cy.getAttached(".section--users").within(() => {
-          cy.getAttached("tbody>tr").should("have.length.greaterThan", 0);
-          cy.findByPlaceholderText(/search/i).type("Ash");
-          cy.getAttached("tbody>tr").should("have.length", 0);
-          cy.getAttached(".empty-users").within(() => {
-            cy.findByText(/no users matched/i).should("exist");
-          });
-        });
-      }
-    );
-    it(
-      "renders and searches the host's software,  links to filter hosts by software",
-      {
-        retries: {
-          runMode: 2,
-        },
-        defaultCommandTimeout: 10000,
-      },
-      () => {
-        cy.getAttached(".react-tabs__tab-list").within(() => {
-          cy.findByText(/software/i).click();
-        });
-        let initialCount = 0;
-        cy.getAttached(".section--software").within(() => {
-          cy.getAttached(".table-container__results-count")
-            .invoke("text")
-            .then((text) => {
-              const fullText = text;
-              const pattern = /[0-9]+/g;
-              const newCount = fullText.match(pattern);
-              initialCount = parseInt(newCount[0]);
-              console.log("softwareCount", initialCount);
-              expect(initialCount).to.be.at.least(1);
-            });
-          cy.findByPlaceholderText(/filter software/i).type("lib");
-          console.log("softwareCount", initialCount);
-          // Ensures search completes
-          cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-          cy.getAttached(".table-container__results-count")
-            .invoke("text")
-            .then((text) => {
-              const fullText = text;
-              const pattern = /[0-9]+/g;
-              const newCount = fullText.match(pattern);
-              const searchCount = parseInt(newCount[0]);
-              expect(searchCount).to.be.lessThan(initialCount);
-            });
-          cy.getAttached(".software-link").first().click({ force: true });
-          cy.getAttached(".manage-hosts__software-filter-block").within(() => {
-            cy.getAttached(".manage-hosts__software-filter-name-card").should(
-              "exist"
-            );
-            cy.getAttached(".data-table").within(() => {
-              cy.findByText(hostname).should("exist");
-            });
-          });
-          cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-          cy.getAttached(".manage-hosts__software-filter-block").within(() => {
-            cy.getAttached(".manage-hosts__software-filter-name-card").should(
-              "exist"
-            );
-            cy.getAttached(".data-table").within(() => {
-              cy.findByText(hostname).should("exist");
-            });
-          });
-        });
-      }
-    );
-    it(
-      "renders host's schedule",
-      {
-        retries: {
-          runMode: 2,
-        },
-        defaultCommandTimeout: 10000,
-      },
-      () => {
-        cy.getAttached(".react-tabs__tab-list").within(() => {
-          cy.findByText(/schedule/i).click();
-        });
+      cy.getAttached(".query-form__button-wrap--new-query").within(() => {
+        cy.findByText(/run query/i)
+          .should("exist")
+          .click();
+      });
+      cy.getAttached(".query-page__wrapper").within(() => {
         cy.getAttached(".data-table").within(() => {
-          cy.findByText(/query name/i).should("exist");
+          cy.findByText(hostname).should("exist");
         });
-      }
-    );
-    it(
-      "renders host's policies and links to filter hosts by policy status",
-      {
-        retries: {
-          runMode: 2,
-        },
-        defaultCommandTimeout: 10000,
-      },
-      () => {
-        cy.getAttached(".react-tabs__tab-list").within(() => {
-          cy.findByText(/policies/i).click();
+        cy.findByText(/run/i).click();
+      });
+    });
+    it("renders and searches the host's users", () => {
+      cy.getAttached(".section--users").within(() => {
+        cy.getAttached("tbody>tr").should("have.length.greaterThan", 0);
+        cy.findByPlaceholderText(/search/i).type("Ash");
+        cy.getAttached("tbody>tr").should("have.length", 0);
+        cy.getAttached(".empty-users").within(() => {
+          cy.findByText(/no users matched/i).should("exist");
         });
-        cy.getAttached(".section--policies").within(() => {
-          cy.findByText(/failing 1 policy/i).should("exist");
-          cy.getAttached(".policy-link").first().click({ force: true });
-          cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-          cy.getAttached(".manage-hosts__policies-filter-name-card").should(
-            "exist"
-          );
-        });
-      }
-    );
+      });
+    });
+    it("renders and searches the host's software,  links to filter hosts by software", () => {
+      cy.getAttached(".react-tabs__tab-list").within(() => {
+        cy.findByText(/software/i).click();
+      });
+      let initialCount = 0;
+      cy.getAttached(".section--software").within(() => {
+        cy.getAttached(".table-container__results-count")
+          .invoke("text")
+          .then((text) => {
+            const fullText = text;
+            const pattern = /[0-9]+/g;
+            const newCount = fullText.match(pattern);
+            initialCount = parseInt(newCount[0]);
+            console.log("softwareCount", initialCount);
+            expect(initialCount).to.be.at.least(1);
+          });
+        cy.findByPlaceholderText(/filter software/i).type("lib");
+        // Ensures search completes
+        cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
+        cy.getAttached(".table-container__results-count")
+          .invoke("text")
+          .then((text) => {
+            const fullText = text;
+            const pattern = /[0-9]+/g;
+            const newCount = fullText.match(pattern);
+            const searchCount = parseInt(newCount[0]);
+            expect(searchCount).to.be.lessThan(initialCount);
+          });
+        cy.getAttached(".software-link").first().click({ force: true });
+      });
+      cy.getAttached(".manage-hosts__software-filter-block").within(() => {
+        cy.getAttached(".manage-hosts__software-filter-name-card").should(
+          "exist"
+        );
+      });
+      cy.getAttached(".data-table").within(() => {
+        cy.findByText(hostname).should("exist");
+      });
+    });
+    it("renders host's schedule", () => {
+      cy.getAttached(".react-tabs__tab-list").within(() => {
+        cy.findByText(/schedule/i).click();
+      });
+      cy.getAttached(".data-table").within(() => {
+        cy.findByText(/query name/i).should("exist");
+      });
+    });
+    it("renders host's policies and links to filter hosts by policy status", () => {
+      cy.getAttached(".react-tabs__tab-list").within(() => {
+        cy.findByText(/policies/i).click();
+      });
+      cy.getAttached(".section--policies").within(() => {
+        cy.findByText(/failing 1 policy/i).should("exist");
+        cy.getAttached(".policy-link").first().click({ force: true });
+      });
+      cy.getAttached(".manage-hosts__policies-filter-name-card").should(
+        "exist"
+      );
+      cy.getAttached(".data-table").within(() => {
+        cy.findByText(hostname).should("exist");
+      });
+    });
     it(
       "refetches host vitals",
       {
         retries: {
           runMode: 2,
         },
-        defaultCommandTimeout: 10000,
+        defaultCommandTimeout: 15000,
       },
       () => {
         cy.getAttached(".hostname-container").within(() => {
           cy.contains("button", /refetch/i).click();
           cy.findByText(/fetching/i).should("exist");
-          // Ensures fetching completes
-          cy.wait(5000); // eslint-disable-line cypress/no-unnecessary-waiting
           cy.contains("button", /refetch/i).should("exist");
           cy.findByText(/few seconds/i).should("exist");
         });
       }
     );
+    it("deletes an existing host", () => {
+      cy.getAttached(".host-details__action-button-container")
+        .within(() => {
+          cy.findByText(/delete/i).click();
+        })
+        .then(() => {
+          cy.getAttached(".modal__modal_container")
+            .within(() => {
+              cy.findByText(/delete host/i).should("exist");
+              cy.findByRole("button", { name: /delete/i }).click();
+            })
+            .then(() => {
+              cy.findByText(/add your devices to fleet/i).should("exist");
+              cy.findByText(/generate installer/i).should("exist");
+              cy.findByText(/about this host/i).should("not.exist");
+              cy.findByText(hostname).should("not.exist");
+            });
+        });
+    });
   });
 });
