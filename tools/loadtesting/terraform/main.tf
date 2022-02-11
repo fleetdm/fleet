e4978d5ad3ca8570c6723981d7754798d4652e42
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2022-02-11 12:31:05 -0500

    Remove defunct commented stuff

--- tools/loadtesting/terraform/main.tf
+++ tools/loadtesting/terraform/main.tf
@@ -14,13 +14,6 @@ provider "aws" {
 }
 
 terraform {
-  // these values are hard-coded to prevent chicken before the egg situations
-  #  backend "s3" {
-  #    bucket = "fleet-terraform-remote-state"
-  #    region = "us-east-2"
-  #    key = "fleet/"
-  #    dynamodb_table = "fleet-terraform-state-lock"
-  #  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -49,38 +42,3 @@ provider "docker" {
     password = data.aws_ecr_authorization_token.token.password
   }
 }
-
-#resource "aws_s3_bucket" "remote_state" {
-#  bucket = "${var.prefix}-terraform-remote-state"
-#  acl    = "private"
-#  versioning {
-#    enabled = true
-#  }
-#  lifecycle {
-#    prevent_destroy = true
-#  }
-#  tags = {
-#    Name = "S3 Remote Terraform State Store"
-#  }
-#}
-#
-#resource "aws_s3_bucket_public_access_block" "fleet_terraform_state" {
-#  bucket              = aws_s3_bucket.remote_state.id
-#  block_public_acls   = true
-#  block_public_policy = true
-#}
-#
-#resource "aws_dynamodb_table" "fleet_terraform_state_lock" {
-#  name         = "fleet-terraform-state-lock"
-#  hash_key     = "LockID"
-#  billing_mode = "PAY_PER_REQUEST"
-#
-#  attribute {
-#    name = "LockID"
-#    type = "S"
-#  }
-#
-#  tags = {
-#    Name = "DynamoDB Terraform State Lock Table"
-#  }
-#}
