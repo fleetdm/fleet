c29b183220c38b003e38ecb6b91dc7350d752587
Michal Nicpon <michal@michalnicp.ca>
2022-02-11 16:42:38 -0700

    Cache team agent options

--- server/datastore/cached_mysql/cached_mysql.go
+++ server/datastore/cached_mysql/cached_mysql.go
@@ -2,11 +2,23 @@ package cached_mysql
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/patrickmn/go-cache"
+)
+
+const (
+	appConfigKey                      = "AppConfig:%s"
+	defaultAppConfigExpiration        = 1 * time.Second
+	packsHostKey                      = "Packs:host:%d"
+	defaultPacksExpiration            = 1 * time.Minute
+	scheduledQueriesKey               = "ScheduledQueries:pack:%d"
+	defaultScheduledQueriesExpiration = 1 * time.Minute
+	teamAgentOptionsKey               = "TeamAgentOptions:team:%d"
+	defaultTeamAgentOptionsExpiration = 1 * time.Minute
 )
 
 type cachedMysql struct {
@@ -16,28 +28,26 @@ type cachedMysql struct {
 
 	packsExp            time.Duration
 	scheduledQueriesExp time.Duration
+	teamAgentOptionsExp time.Duration
 }
-
-const (
-	appConfigKey                      = "AppConfig"
-	packsKey                          = "Packs"
-	scheduledQueriesKey               = "ScheduledQueries"
-	defaultAppConfigExpiration        = 1 * time.Second
-	defaultPacksExpiration            = 1 * time.Minute
-	defaultScheduledQueriesExpiration = 1 * time.Minute
-)
 
 type Option func(*cachedMysql)
 
-func WithPacksExpiration(t time.Duration) Option {
+func WithPacksExpiration(d time.Duration) Option {
 	return func(o *cachedMysql) {
-		o.packsExp = t
+		o.packsExp = d
 	}
 }
 
-func WithScheduledQueriesExpiration(t time.Duration) Option {
+func WithScheduledQueriesExpiration(d time.Duration) Option {
 	return func(o *cachedMysql) {
-		o.scheduledQueriesExp = t
+		o.scheduledQueriesExp = d
+	}
+}
+
+func WithTeamAgentOptionsExpiration(d time.Duration) Option {
+	return func(o *cachedMysql) {
+		o.teamAgentOptionsExp = d
 	}
 }
 
@@ -47,6 +57,7 @@ func New(ds fleet.Datastore, opts ...Option) fleet.Datastore {
 		c:                   cache.New(5*time.Minute, 10*time.Minute),
 		packsExp:            defaultPacksExpiration,
 		scheduledQueriesExp: defaultScheduledQueriesExpiration,
+		teamAgentOptionsExp: defaultTeamAgentOptionsExpiration,
 	}
 	for _, fn := range opts {
 		fn(c)
@@ -62,13 +73,15 @@ func (ds *cachedMysql) NewAppConfig(ctx context.Context, info *fleet.AppConfig) 
 
 	ds.c.Set(appConfigKey, ac, defaultAppConfigExpiration)
 
-	return ac.Clone()
+	return ac, nil
 }
 
 func (ds *cachedMysql) AppConfig(ctx context.Context) (*fleet.AppConfig, error) {
-	cachedAc, found := ds.c.Get(appConfigKey)
-	if found {
-		return cachedAc.(*fleet.AppConfig).Clone()
+	if x, found := ds.c.Get(appConfigKey); found {
+		ac, ok := x.(*fleet.AppConfig)
+		if ok {
+			return ac.Clone()
+		}
 	}
 
 	ac, err := ds.Datastore.AppConfig(ctx)
@@ -93,12 +106,11 @@ func (ds *cachedMysql) SaveAppConfig(ctx context.Context, info *fleet.AppConfig)
 }
 
 func (ds *cachedMysql) ListPacksForHost(ctx context.Context, hid uint) ([]*fleet.Pack, error) {
-	key := fmt.Sprintf("%s_%d", packsKey, hid)
-	cachedPacks, found := ds.c.Get(key)
-	if found && cachedPacks != nil {
-		casted, ok := cachedPacks.([]*fleet.Pack)
+	key := fmt.Sprintf(packsHostKey, hid)
+	if x, found := ds.c.Get(key); found {
+		cachedPacks, ok := x.([]*fleet.Pack)
 		if ok {
-			return casted, nil
+			return cachedPacks, nil
 		}
 	}
 
@@ -112,17 +124,16 @@ func (ds *cachedMysql) ListPacksForHost(ctx context.Context, hid uint) ([]*fleet
 	return packs, nil
 }
 
-func (ds *cachedMysql) ListScheduledQueriesInPack(ctx context.Context, id uint) ([]*fleet.ScheduledQuery, error) {
-	key := fmt.Sprintf("%s_%d", scheduledQueriesKey, id)
-	cachedScheduledQueries, found := ds.c.Get(key)
-	if found && cachedScheduledQueries != nil {
-		casted, ok := cachedScheduledQueries.([]*fleet.ScheduledQuery)
+func (ds *cachedMysql) ListScheduledQueriesInPack(ctx context.Context, packID uint) ([]*fleet.ScheduledQuery, error) {
+	key := fmt.Sprintf(scheduledQueriesKey, packID)
+	if x, found := ds.c.Get(key); found {
+		scheduledQueries, ok := x.([]*fleet.ScheduledQuery)
 		if ok {
-			return casted, nil
+			return scheduledQueries, nil
 		}
 	}
 
-	scheduledQueries, err := ds.Datastore.ListScheduledQueriesInPack(ctx, id)
+	scheduledQueries, err := ds.Datastore.ListScheduledQueriesInPack(ctx, packID)
 	if err != nil {
 		return nil, err
 	}
@@ -130,4 +141,48 @@ func (ds *cachedMysql) ListScheduledQueriesInPack(ctx context.Context, id uint) 
 	ds.c.Set(key, scheduledQueries, ds.scheduledQueriesExp)
 
 	return scheduledQueries, nil
+}
+
+func (ds *cachedMysql) TeamAgentOptions(ctx context.Context, teamID uint) (*json.RawMessage, error) {
+	key := fmt.Sprintf(teamAgentOptionsKey, teamID)
+	if x, found := ds.c.Get(key); found {
+		if agentOptions, ok := x.(*json.RawMessage); ok {
+			return agentOptions, nil
+		}
+	}
+
+	agentOptions, err := ds.Datastore.TeamAgentOptions(ctx, teamID)
+	if err != nil {
+		return nil, err
+	}
+
+	ds.c.Set(key, agentOptions, ds.scheduledQueriesExp)
+
+	return agentOptions, nil
+}
+
+func (ds *cachedMysql) SaveTeam(ctx context.Context, team *fleet.Team) (*fleet.Team, error) {
+	team, err := ds.Datastore.SaveTeam(ctx, team)
+	if err != nil {
+		return nil, err
+	}
+
+	key := fmt.Sprintf(teamAgentOptionsKey, team.ID)
+
+	ds.c.Set(key, team.AgentOptions, ds.teamAgentOptionsExp)
+
+	return team, nil
+}
+
+func (ds *cachedMysql) DeleteTeam(ctx context.Context, teamID uint) error {
+	err := ds.Datastore.DeleteTeam(ctx, teamID)
+	if err != nil {
+		return err
+	}
+
+	key := fmt.Sprintf(teamAgentOptionsKey, teamID)
+
+	ds.c.Delete(key)
+
+	return nil
 }
