3fe63d19b38db534594e1d6a6dd793847ec03052
Mike McNeil <mikermcneil@users.noreply.github.com>
2022-02-13 13:59:04 -0600

    run in parallel

--- website/scripts/get-estimation-report.js
+++ website/scripts/get-estimation-report.js
@@ -36,23 +36,20 @@ module.exports = {
     // FUTURE: only look at particular projects here instead of all of them
     projects = projects.filter((project)=> Number(project.id) === 13160610);// « TODO get rid of this hack
 
-    for (let project of projects) {// TODO: do this in parallel
+    await sails.helpers.flow.simultaneouslyForEach(projects, async(project)=>{
       estimationReport[project.name] = {};
       let columns = await sails.helpers.http.get(`https://api.github.com/projects/${project.id}/columns`, {}, baseHeaders);
-      for (let column of columns) {// TODO: do this in parallel
+      await sails.helpers.flow.simultaneouslyForEach(columns, async(column)=>{
         // console.log('------',project.name, column.name);
         estimationReport[project.name][column.name] = 0;
         let cards = await sails.helpers.http.get(`https://api.github.com/projects/columns/${column.id}/cards`, {}, baseHeaders);
-        for (let card of cards) {// TODO: do this in parallel
+        await sails.helpers.flow.simultaneouslyForEach(cards, async(card)=>{
 
           // Get the number of story points associated with this card.
           let numPoints = 0;
           if (!card.content_url) {
             // ignore "notes" (FUTURE: Maybe add some kind of sniffing for a prefix like "[5]")
           } else {
-            // let ownerAndRepo = 'fleetdm/fleet';
-            // let issueId = 4168;
-            // let issue = await sails.helpers.http.get(`https://api.github.com/repos/${ownerAndRepo}/issues/${issueId}`, {}, baseHeaders);
             let issue = await sails.helpers.http.get(card.content_url, {}, baseHeaders);
             let pointLabels = issue.labels.filter((label)=> Number(label.name) >= 1 && Number(label.name) < Infinity);
             if (pointLabels.length >= 2) { throw new Error(`Cannot have more than one story point label, but issue #${issue.id} seems to have more than one: ${_.pluck(pointLabels,'name')}`); }
@@ -64,10 +61,9 @@ module.exports = {
           }
           // console.log(`${column.name} :: ${card.id} ::   +${numPoints}`);
           estimationReport[project.name][column.name] += numPoints;
-        }//∞
-      }//∞
-    }//∞
-
+        });//∞
+      });//∞
+    });//∞
 
     return estimationReport;
 
