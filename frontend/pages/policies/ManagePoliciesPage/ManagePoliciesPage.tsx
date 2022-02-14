439d6f0623c8679ae1e4a508c7371df95a8b5431
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2022-02-14 16:09:55 -0600

    Fix flaky e2e policies (#4190)

--- frontend/pages/policies/ManagePoliciesPage/ManagePoliciesPage.tsx
+++ frontend/pages/policies/ManagePoliciesPage/ManagePoliciesPage.tsx
@@ -362,15 +362,18 @@ const ManagePolicyPage = ({
             </div>
           </div>
           <div className={`${baseClass} button-wrap`}>
-            {canAddOrRemovePolicy && teamId === 0 && (
-              <Button
-                onClick={() => onManageAutomationsClick()}
-                className={`${baseClass}__manage-automations button`}
-                variant="inverse"
-              >
-                <span>Manage automations</span>
-              </Button>
-            )}
+            {canAddOrRemovePolicy &&
+              teamId === 0 &&
+              !isLoadingFailingPoliciesWebhook &&
+              !isLoadingGlobalPolicies && (
+                <Button
+                  onClick={() => onManageAutomationsClick()}
+                  className={`${baseClass}__manage-automations button`}
+                  variant="inverse"
+                >
+                  <span>Manage automations</span>
+                </Button>
+              )}
             {canAddOrRemovePolicy && (
               <div className={`${baseClass}__action-button-container`}>
                 <Button
@@ -450,7 +453,7 @@ const ManagePolicyPage = ({
               />
             ))}
         </div>
-        {showInheritedPoliciesButton && (
+        {showInheritedPoliciesButton && globalPolicies && (
           <span>
             <Button
               variant="unstyled"
