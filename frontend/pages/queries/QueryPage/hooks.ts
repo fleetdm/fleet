905db8e8c67c726993140650e2435c5bac0b509b
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-02-21 17:03:28 -0600

    Catch targets API error

--- frontend/pages/queries/QueryPage/hooks.ts
+++ frontend/pages/queries/QueryPage/hooks.ts
@@ -31,57 +31,64 @@ const getTargets = async (
 ): Promise<ITargetsQueryResponse> => {
   const { query, queryId, selected, includeLabels } = queryKey;
 
-  const {
-    targets,
-    targets_count: targetsTotalCount,
-    targets_online: targetsOnline,
-  } = await targetsAPI.loadAll({
-    query,
-    queryId,
-    selected,
-  });
+  try {
+    const {
+      targets,
+      targets_count: targetsTotalCount,
+      targets_online: targetsOnline,
+    } = await targetsAPI.loadAll({
+      query,
+      queryId,
+      selected,
+    });
+    let response: Partial<ITargetsQueryResponse> = {};
 
-  let response: Partial<ITargetsQueryResponse> = {};
+    if (includeLabels) {
+      const { labels } = targets;
 
-  if (includeLabels) {
-    const { labels } = targets;
+      const all = filter(
+        labels,
+        ({ display_text: text }) => text === "All Hosts"
+      );
 
-    const all = filter(
-      labels,
-      ({ display_text: text }) => text === "All Hosts"
-    );
+      const platforms = filter(
+        labels,
+        ({ display_text: text }) =>
+          text === "macOS" || text === "MS Windows" || text === "All Linux"
+      );
 
-    const platforms = filter(
-      labels,
-      ({ display_text: text }) =>
-        text === "macOS" || text === "MS Windows" || text === "All Linux"
-    );
+      const other = filter(
+        labels,
+        ({ label_type: type }) => type === "regular"
+      );
 
-    const other = filter(labels, ({ label_type: type }) => type === "regular");
+      const labelCount =
+        all.length + platforms.length + other.length + targets.teams.length;
 
-    const labelCount =
-      all.length + platforms.length + other.length + targets.teams.length;
+      response = {
+        allHostsLabel: all,
+        platformLabels: platforms,
+        otherLabels: other,
+        teams: targets.teams,
+        labelCount,
+      };
+    }
 
-    response = {
-      allHostsLabel: all,
-      platformLabels: platforms,
-      otherLabels: other,
-      teams: targets.teams,
-      labelCount,
-    };
+    const targetsOnlinePercent =
+      targetsTotalCount > 0
+        ? Math.round((targetsOnline / targetsTotalCount) * 100)
+        : 0;
+
+    return Promise.resolve({
+      ...response,
+      relatedHosts: query ? [...targets.hosts] : [],
+      targetsTotalCount,
+      targetsOnlinePercent,
+    });
+  } catch (err) {
+    console.log("ERROR ", err);
+    return Promise.reject(err);
   }
-
-  const targetsOnlinePercent =
-    targetsTotalCount > 0
-      ? Math.round((targetsOnline / targetsTotalCount) * 100)
-      : 0;
-
-  return Promise.resolve({
-    ...response,
-    relatedHosts: query ? [...targets.hosts] : [],
-    targetsTotalCount,
-    targetsOnlinePercent,
-  });
 };
 
 export const useTargetsQuery = (
