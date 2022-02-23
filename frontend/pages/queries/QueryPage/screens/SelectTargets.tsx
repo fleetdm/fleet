2a34345d7f38640c1633ff0e3a3976da59e793e4
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-02-23 12:36:04 -0500

    Rebased onto S.G. SelectTargets refactor

--- frontend/pages/queries/QueryPage/screens/SelectTargets.tsx
+++ frontend/pages/queries/QueryPage/screens/SelectTargets.tsx
@@ -155,6 +155,10 @@ const SelectTargets = ({
     }
   );
 
+  useEffect(() => {
+    setTargetsTotalCount(targets?.targetsTotalCount || 0);
+  }, [targets]);
+
   const handleClickCancel = () => {
     setSelectedTargets([]);
     goToQueryEditor();
