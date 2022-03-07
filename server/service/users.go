f1b5c2994bd8eb60a1d027edc575376ccf17e053
Martin Angers <martin.n.angers@gmail.com>
2022-03-07 16:16:46 -0500

    Remove now-unused code

--- server/service/users.go
+++ server/service/users.go
@@ -921,3 +921,11 @@ func (svc *Service) RequestPasswordReset(ctx context.Context, email string) erro
 
 	return svc.mailService.SendEmail(resetEmail)
 }
+
+func (svc *Service) ListAvailableTeamsForUser(ctx context.Context, user *fleet.User) ([]*fleet.TeamSummary, error) {
+	// skipauth: No authorization check needed due to implementation returning
+	// only license error.
+	svc.authz.SkipAuthorization(ctx)
+
+	return nil, fleet.ErrMissingLicense
+}
