c0d979f46f700a6beab83bbf00f32c4edff45cb0
Mike McNeil <mikermcneil@users.noreply.github.com>
2022-03-08 15:57:05 -0600

    New handbook pages are approved by @mikermcneil

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -199,7 +199,7 @@ module.exports = {
           let isSenderDRIForAllChangedPaths = false;
           let DRI_BY_PATH = {
             'README.md': 'mike-j-thomas',// (github brandfront)
-            'handbook': 'eashaw',// (default for handbook)
+            'handbook': 'mikermcneil',// (default for handbook)
             'handbook/README.md': 'mikermcneil',
             'handbook/company.md': 'mikermcneil',
             'handbook/people.md': 'eashaw',
