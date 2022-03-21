f4d164ea122d9899bbf0514903fce6f42273be19
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-03-21 15:34:08 -0500

    Add macadmins tables to compatibility check

--- frontend/interfaces/platform.ts
+++ frontend/interfaces/platform.ts
@@ -20,10 +20,18 @@ export type IPlatformString =
 
 export const SUPPORTED_PLATFORMS = ["darwin", "windows", "linux"] as const;
 
-// TODO: How do we want to handle checking platform compatibility for extension tables? See template
-// "MDM enrolled" policy for example of how this can be an issue where tables are not included in
-// osquery_tables.json. One approach would be to maintain a separate constant that lists extension
-// tables as below.
-export const EXTENSION_TABLES: Record<string, IOsqueryPlatform[]> = {
+// TODO: revisit this approach pending resolution of https://github.com/fleetdm/fleet/issues/3555.
+export const MACADMINS_EXTENSION_TABLES: Record<string, IOsqueryPlatform[]> = {
+  file_lines: ["darwin", "linux", "windows"],
+  filevault_users: ["darwin"],
+  google_chrome_profiles: ["darwin", "linux", "windows"],
+  macos_profiles: ["darwin"],
   mdm: ["darwin"],
+  munki_info: ["darwin"],
+  munki_install: ["darwin"],
+  network_quality: ["darwin"],
+  puppet_info: ["darwin", "linux", "windows"],
+  puppet_logs: ["darwin", "linux", "windows"],
+  puppet_state: ["darwin", "linux", "windows"],
+  unified_log: ["darwin"],
 };
