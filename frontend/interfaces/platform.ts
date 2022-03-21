4754908ea415d06bba3c43f8ff48768706965310
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-03-21 17:45:13 -0500

    Remove unused extension table

--- frontend/interfaces/platform.ts
+++ frontend/interfaces/platform.ts
@@ -29,7 +29,7 @@ export const MACADMINS_EXTENSION_TABLES: Record<string, IOsqueryPlatform[]> = {
   mdm: ["darwin"],
   munki_info: ["darwin"],
   munki_install: ["darwin"],
-  network_quality: ["darwin"],
+  // network_quality: ["darwin"], // TODO: add this table if/when it is incorporated into orbit
   puppet_info: ["darwin", "linux", "windows"],
   puppet_logs: ["darwin", "linux", "windows"],
   puppet_state: ["darwin", "linux", "windows"],
