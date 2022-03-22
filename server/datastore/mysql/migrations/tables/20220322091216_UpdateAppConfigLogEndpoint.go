451afb3d22d01ada97fc9d2454f6dfddf8634bc1
Martin Angers <martin.n.angers@gmail.com>
2022-03-22 10:37:25 -0400

    Add migration to update appconfig agent log endpoint

---
+++ server/datastore/mysql/migrations/tables/20220322091216_UpdateAppConfigLogEndpoint.go
@@ -0,0 +1,56 @@
+package tables
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"errors"
+
+	"github.com/fleetdm/fleet/v4/server/fleet"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20220322091216, Down_20220322091216)
+}
+
+func Up_20220322091216(tx *sql.Tx) error {
+	const selectStmt = `SELECT json_value FROM app_config_json LIMIT 1`
+	var raw json.RawMessage
+	var config fleet.AppConfig
+
+	row := tx.QueryRow(selectStmt)
+	if err := row.Scan(&raw); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil
+		}
+		return err
+	}
+	if err := json.Unmarshal(raw, &config); err != nil {
+		return err
+	}
+
+	var (
+		oldPath = []byte(`"/api/v1/osquery/log"`)
+		newPath = []byte(`"/api/latest/osquery/log"`)
+	)
+	if config.AgentOptions != nil {
+		oldOpts := []byte(*config.AgentOptions)
+		newOpts := json.RawMessage(bytes.ReplaceAll(oldOpts, oldPath, newPath))
+		config.AgentOptions = &newOpts
+	}
+	b, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	const updateStmt = `UPDATE app_config_json SET json_value = ? WHERE id = 1`
+	if _, err := tx.Exec(updateStmt, b); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Down_20220322091216(tx *sql.Tx) error {
+	return nil
+}
---
+++ server/datastore/mysql/migrations/tables/20220322091216_UpdateAppConfigLogEndpoint_test.go
@@ -0,0 +1,21 @@
+
+package tables
+
+import "testing"
+
+func TestUp_20220322091216(t *testing.T) {
+	db := applyUpToPrev(t)
+
+	//
+	// Insert data to test the migration
+	//
+	// ...
+
+	// Apply current migration.
+	applyNext(t, db)
+
+	//
+	// Check data, insert new entries, e.g. to verify migration is safe.
+	//
+	// ...
+}
