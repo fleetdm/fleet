39def5cabe082cb4052c96b54878f56d8327dee0
eashaw <caglc@live.com>
2022-03-28 09:41:17 -0500

    Update bootstrap.js

--- website/config/bootstrap.js
+++ website/config/bootstrap.js
@@ -15,7 +15,7 @@ module.exports.bootstrap = async function() {
   var path = require('path');
 
   // This bootstrap version indicates what version of fake data we're dealing with here.
-  var HARD_CODED_DATA_VERSION = 1;
+  var HARD_CODED_DATA_VERSION = 0;
 
   // This path indicates where to store/look for the JSON file that tracks the "last run bootstrap info"
   // locally on this development computer (if we happen to be on a development computer).
