698e9276bf9a8fe65caf1893c1e3cb1ef86a5f3d
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-03-31 17:13:57 -0400

    Jira enabled work for vuln automations

--- frontend/pages/software/ManageSoftwarePage/components/ManageAutomationsModal/ManageAutomationsModal.tsx
+++ frontend/pages/software/ManageSoftwarePage/components/ManageAutomationsModal/ManageAutomationsModal.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 
+import { Link } from "react-router";
+import PATHS from "router/paths";
+
 import Modal from "components/Modal";
 import Button from "components/buttons/Button";
 import Slider from "components/forms/fields/Slider";
+import Radio from "components/forms/fields/Radio";
 // @ts-ignore
 import InputField from "components/forms/fields/InputField";
 
@@ -46,11 +50,11 @@ const ManageAutomationsModal = ({
     currentDestinationUrl || ""
   );
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
-
   const [
     softwareAutomationsEnabled,
     setSoftwareAutomationsEnabled,
   ] = useState<boolean>(softwareVulnerabilityWebhookEnabled || false);
+  const [jiraEnabled, setJiraEnabled] = useState<boolean>(false);
 
   useDeepEffect(() => {
     if (destination_url) {
@@ -82,6 +86,80 @@ const ManageAutomationsModal = ({
     }
   };
 
+  const onRadioChange = (jira: boolean): ((evt: string) => void) => {
+    console.log("onRadioChange formField", jira);
+    return (evt: string) => {
+      console.log("onRadioChange evt", evt);
+      setJiraEnabled(jira);
+      return;
+    };
+  };
+
+  const renderTicket = () => {
+    const integrations = false;
+    return (
+      <div className={`${baseClass}__ticket`}>
+        <div className={`${baseClass}__software-automation-description`}>
+          <p>
+            A ticket will be created in your <b>Integration</b> if a detected
+            vulnerability (CVE) was published in the last 2 days.
+          </p>
+        </div>
+        {integrations ? (
+          <>Integration dropdown here</>
+        ) : (
+          <div className={`${baseClass}__no-integrations`}>
+            <div>
+              <b>You have no integrations.</b>
+            </div>
+            <div className={`${baseClass}__no-integration--cta`}>
+              <Link
+                to={PATHS.ADMIN_INTEGRATIONS}
+                className={`${baseClass}__add-integration-link`}
+              >
+                <span>Add integration</span>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const renderWebhook = () => {
+    return (
+      <div className={`${baseClass}__webhook`}>
+        <div className={`${baseClass}__software-automation-description`}>
+          <p>
+            A request will be sent to your configured <b>Destination URL</b> if
+            a detected vulnerability (CVE) was published in the last 2 days.
+          </p>
+        </div>
+        <InputField
+          inputWrapperClass={`${baseClass}__url-input`}
+          name="webhook-url"
+          label={"Destination URL"}
+          type={"text"}
+          value={destination_url}
+          onChange={onURLChange}
+          error={errors.url}
+          hint={
+            "For each new vulnerability detected, Fleet will send a JSON payload to this URL with a list of the affected hosts."
+          }
+          placeholder={"https://server.com/example"}
+          tooltip="Provide a URL to deliver a webhook request to."
+        />
+        <Button
+          type="button"
+          variant="text-link"
+          onClick={togglePreviewPayloadModal}
+        >
+          Preview payload
+        </Button>
+      </div>
+    );
+  };
+
   if (showPreviewPayloadModal) {
     return <PreviewPayloadModal onCancel={togglePreviewPayloadModal} />;
   }
@@ -105,34 +183,28 @@ const ManageAutomationsModal = ({
         </div>
         <div className={`${baseClass}__overlay-container`}>
           <div className={`${baseClass}__software-automation-enabled`}>
-            <div className={`${baseClass}__software-automation-description`}>
-              <p>
-                A request will be sent to your configured <b>Destination URL</b>{" "}
-                if a detected vulnerability (CVE) was published in the last 30
-                days.
-              </p>
+            <div className={`${baseClass}__workflow`}>
+              Workflow
+              <Radio
+                className={`${baseClass}__radio-input`}
+                label={"Ticket"}
+                id={"ticket"}
+                checked={jiraEnabled}
+                value={"ticket"}
+                name={"ticket"}
+                onChange={onRadioChange(true)}
+              />
+              <Radio
+                className={`${baseClass}__radio-input`}
+                label={"Webhook"}
+                id={"webhook"}
+                checked={!jiraEnabled}
+                value={"webhook"}
+                name={"webhook"}
+                onChange={onRadioChange(false)}
+              />
             </div>
-            <InputField
-              inputWrapperClass={`${baseClass}__url-input`}
-              name="webhook-url"
-              label={"Destination URL"}
-              type={"text"}
-              value={destination_url}
-              onChange={onURLChange}
-              error={errors.url}
-              hint={
-                "For each new vulnerability detected, Fleet will send a JSON payload to this URL with a list of the affected hosts."
-              }
-              placeholder={"https://server.com/example"}
-              tooltip="Provide a URL to deliver a webhook request to."
-            />
-            <Button
-              type="button"
-              variant="text-link"
-              onClick={togglePreviewPayloadModal}
-            >
-              Preview payload
-            </Button>
+            {jiraEnabled ? renderTicket() : renderWebhook()}
           </div>
           {!softwareAutomationsEnabled && (
             <div className={`${baseClass}__overlay`} />
