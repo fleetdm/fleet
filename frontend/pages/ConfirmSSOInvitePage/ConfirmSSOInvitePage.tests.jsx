db60dbd76c6c0742dd5ae4c654762c1dc30f124a
tharun <rajendrantharun@live.com>
2022-04-05 19:35:09 +0530

    chore(test): rewritten confirmssoinvitepagetest using rtl

--- frontend/pages/ConfirmSSOInvitePage/ConfirmSSOInvitePage.tests.jsx
+++ frontend/pages/ConfirmSSOInvitePage/ConfirmSSOInvitePage.tests.jsx
@@ -1,4 +1,4 @@
-import { mount } from "enzyme";
+import { render } from "@testing-library/react";
 
 import ConfirmSSOInvitePage from "pages/ConfirmSSOInvitePage";
 import { connectedComponent, reduxMockStore } from "test/helpers";
@@ -12,23 +12,23 @@ describe("ConfirmSSOInvitePage - component", () => {
     props: { location, params },
     mockStore,
   });
-  const page = mount(component);
 
   it("renders", () => {
-    expect(page.length).toEqual(1);
-    expect(page.find("ConfirmSSOInvitePage").prop("inviteFormData")).toEqual({
-      email: "hi@gnar.dog",
-      invite_token: inviteToken,
-      name: "Gnar Dog",
-    });
+    const { container } = render(component);
+    expect(container).not.toBeEmptyDOMElement();
   });
 
   it("renders a ConfirmSSOInviteForm", () => {
-    expect(page.find("ConfirmSSOInviteForm").length).toEqual(1);
+    const { container } = render(component);
+    expect(
+      container.querySelectorAll(".confirm-invite-page__form").length
+    ).toEqual(1);
   });
 
   it("clears errors on unmount", () => {
-    page.unmount();
+    const { unmount } = render(component);
+
+    unmount();
 
     expect(mockStore.getActions()).toContainEqual({
       type: "users_CLEAR_ERRORS",
