c8671b5fa5a16ea7dd22aeb8face4bb5557d0cce
Martin Angers <martin.n.angers@gmail.com>
2022-04-20 09:10:21 -0400

    Add last opened at only if a specific host is requested

--- server/fleet/software.go
+++ server/fleet/software.go
@@ -37,6 +37,10 @@ type Software struct {
 	// CountsUpdatedAt is the timestamp when the hosts count was last updated
 	// for that software, filled only if hosts count is requested.
 	CountsUpdatedAt time.Time `json:"-" db:"counts_updated_at"`
+	// LastOpenedAt is the timestamp when that software was last opened on the
+	// corresponding host. Only filled when the software list is requested for
+	// a specific host (host_id is provided).
+	LastOpenedAt *time.Time `json:"last_opened_at,omitempty" db:"last_opened_at"`
 }
 
 func (Software) AuthzType() string {
