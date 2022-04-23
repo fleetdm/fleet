c2d98b0afdb3188591d29e0415aafbe2fa3458ea
Mike McNeil <mikermcneil@users.noreply.github.com>
2022-04-22 19:07:56 -0500

    Gather up the various API docs needed

--- website/scripts/prepare-sprint.js
+++ website/scripts/prepare-sprint.js
@@ -28,9 +28,14 @@ module.exports = {
   fn: async function () {
 
     sails.log('Running custom shell script... (`sails run prepare-sprint`)');
-    
+
     // Ensure milestone exists in both fleetdm/fleet and fleetdm/confidential
     // TODO
+
+    // List milestones · https://docs.github.com/en/rest/issues/milestones#list-milestones
+    // Create milestone · https://docs.github.com/en/rest/issues/milestones#create-a-milestone
+
+
 
     // For all issues in "Engineering" board's "✅ Ready for release":
     // • close them
@@ -38,10 +43,20 @@ module.exports = {
     // • set milestone for all issues (why? clean archive)
     // TODO
 
+    // Close issue · https://docs.github.com/en/rest/issues/issues#update-an-issue
+    // Remove card from project · https://docs.github.com/en/issues/trying-out-the-new-projects-experience/using-the-api-to-manage-projects#deleting-an-item-from-a-project
+    // Set milestone · https://docs.github.com/en/rest/issues/issues#update-an-issue
+
+
+
     // For all issues in the "Product" board's "✅ In development" column tagged with the next release (as opposed to the only other possible option: the release after that):
     // • move to "✅ In development"
     // • add to "Release" (engineering) board in "🥚 Ready" column
     // TODO
+
+    // Update select field ("status") · https://docs.github.com/en/issues/trying-out-the-new-projects-experience/using-the-api-to-manage-projects#updating-a-single-select-field
+    // Add card to project · https://docs.github.com/en/issues/trying-out-the-new-projects-experience/using-the-api-to-manage-projects#adding-an-item-to-a-project
+
 
     // Log the breakdown of estimations, column by column, on the release board from before this script was run versus now, after it has run successfully.
     // TODO
@@ -50,4 +65,3 @@ module.exports = {
 
 
 };
-
