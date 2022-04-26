da01de288da40c694525fc8034e5087c873577f0
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-04-26 16:48:43 -0400

    Fix empty states, changelog

--- frontend/pages/policies/ManagePoliciesPage/components/PoliciesListWrapper/PoliciesListWrapper.tsx
+++ frontend/pages/policies/ManagePoliciesPage/components/PoliciesListWrapper/PoliciesListWrapper.tsx
@@ -5,10 +5,13 @@ import paths from "router/paths";
 
 import { IPolicyStats } from "interfaces/policy";
 import { ITeamSummary } from "interfaces/team";
+
+import Button from "components/buttons/Button";
 import Spinner from "components/Spinner";
 import TableContainer from "components/TableContainer";
 import { generateTableHeaders, generateDataSet } from "./PoliciesTableConfig";
 import policySvg from "../../../../../../assets/images/no-policy-323x138@2x.png";
+import ExternalURLIcon from "../../../../../../assets/images/icon-external-url-12x12@2x.png";
 
 const baseClass = "policies-list-wrapper";
 const noPoliciesClass = "no-policies";
@@ -22,6 +25,7 @@ const TAGGED_TEMPLATES = {
 interface IPoliciesListWrapperProps {
   policiesList: IPolicyStats[];
   isLoading: boolean;
+  onAddPolicyClick?: () => void;
   onRemovePoliciesClick: (selectedTableIds: number[]) => void;
   resultsTitle?: string;
   canAddOrRemovePolicy?: boolean;
@@ -33,6 +37,7 @@ interface IPoliciesListWrapperProps {
 const PoliciesListWrapper = ({
   policiesList,
   isLoading,
+  onAddPolicyClick,
   onRemovePoliciesClick,
   resultsTitle,
   canAddOrRemovePolicy,
@@ -85,6 +90,17 @@ const PoliciesListWrapper = ({
                 changes.
               </p>
             </div>
+            {canAddOrRemovePolicy && (
+              <div className={`${baseClass}__action-button-container`}>
+                <Button
+                  variant="brand"
+                  className={`${baseClass}__select-policy-button`}
+                  onClick={onAddPolicyClick}
+                >
+                  Add a policy
+                </Button>
+              </div>
+            )}
           </div>
         </div>
       </div>
