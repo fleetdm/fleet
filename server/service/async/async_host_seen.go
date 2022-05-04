431e03097118a2f75334065e7a801cf84a9e91a6
Martin Angers <martin.n.angers@gmail.com>
2022-05-04 14:10:37 -0400

    Start working on the redis storage of async

--- server/service/async/async_host_seen.go
+++ server/service/async/async_host_seen.go
@@ -4,26 +4,69 @@ import (
 	"context"
 	"sync"
 	"time"
+
+	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
+	"github.com/fleetdm/fleet/v4/server/datastore/redis"
+	redigo "github.com/gomodule/redigo/redis"
 )
 
+const (
+	hostSeenRecordedHostIDsKey   = "{host_seen:host_ids}"            // the SET of current (pending) host ids
+	hostSeenProcessingHostIDsKey = "{host_seen:host_ids}:processing" // the SET of host ids in the process of being collected
+	hostSeenKeysMinTTL           = 7 * 24 * time.Hour                // 1 week
+)
+
+// RecordHostLastSeen records that the specified host ID was seen.
 func (t *Task) RecordHostLastSeen(ctx context.Context, hostID uint) error {
-	if t.AsyncEnabled {
-		// TODO: store in redis
-	}
-
-	t.seenHostSet.addHostID(hostID)
-	return nil
-}
-
-func (t *Task) FlushHostsLastSeen(ctx context.Context, now time.Time) error {
-	if t.AsyncEnabled {
-		// no-op, flushing the hosts' last seen is done via the cron that runs the
-		// Task's collectors.
+	if !t.AsyncEnabled {
+		t.seenHostSet.addHostID(hostID)
 		return nil
 	}
 
-	hostIDs := t.seenHostSet.getAndClearHostIDs()
-	return t.Datastore.MarkHostsSeen(ctx, hostIDs, now)
+	// set an expiration on the SET key, ensuring that if async processing is
+	// disabled, the set (eventually) does not use any redis space. Ensure that
+	// TTL is reasonably big to avoid deleting information that hasn't been
+	// collected yet - 1 week or 10 * the collector interval, whichever is
+	// biggest.
+	ttl := hostSeenKeysMinTTL
+	if maxTTL := 10 * t.CollectorInterval; maxTTL > ttl {
+		ttl = maxTTL
+	}
+
+	// keys and arguments passed to the script are:
+	// KEYS[1]: recorded set (hostSeenRecordedHostIDsKey)
+	// ARGV[1]: host id
+	// ARGV[2]: ttl for the key
+	script := redigo.NewScript(1, `
+    redis.call('SADD', KEYS[1], ARGV[1])
+    return redis.call('EXPIRE', KEYS[1], ARGV[2])
+  `)
+
+	conn := t.Pool.Get()
+	defer conn.Close()
+	if err := redis.BindConn(t.Pool, conn, hostSeenRecordedHostIDsKey); err != nil {
+		return ctxerr.Wrap(ctx, err, "bind redis connection")
+	}
+
+	if _, err := script.Do(conn, hostSeenRecordedHostIDsKey, int(ttl.Seconds())); err != nil {
+		return ctxerr.Wrap(ctx, err, "run redis script")
+	}
+	return nil
+}
+
+// FlushHostsLastSeen updates the last seen timestamp for the hosts that have
+// been recorded since the last time FlushHostsLastSeen was called. It is a
+// no-op if asychronous host processing is enabled, because then it is the
+// task collector that will process the writes to mysql.
+func (t *Task) FlushHostsLastSeen(ctx context.Context, now time.Time) error {
+	if !t.AsyncEnabled {
+		hostIDs := t.seenHostSet.getAndClearHostIDs()
+		return t.Datastore.MarkHostsSeen(ctx, hostIDs, now)
+	}
+
+	// no-op, flushing the hosts' last seen is done via the cron that runs the
+	// Task's collectors.
+	return nil
 }
 
 // seenHostSet implements synchronized storage for the set of seen hosts.
