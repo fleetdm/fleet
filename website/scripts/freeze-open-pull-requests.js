65497f0992f23436bf5d65fbd8600a50c0c44456
Mike McNeil <mikermcneil@users.noreply.github.com>
2022-05-05 23:42:26 -0500

    Use extrapolated logic + add 5 second wait time to prevent accidents + clean up

--- website/scripts/freeze-open-pull-requests.js
+++ website/scripts/freeze-open-pull-requests.js
@@ -29,8 +29,10 @@ module.exports = {
       state: 'open',
       per_page: 100,//eslint-disable-line camelcase
     }, baseHeaders);
-    // console.log(openPullRequests.length,'PRs returned!');
 
+    let SECONDS_TO_WAIT = 5;
+    sails.log(`Examining and potentially freezing ${openPullRequests.length} PRs in ${SECONDS_TO_WAIT} seconds…  (To cancel, press CTRL+C quickly!)`);
+    await sails.helpers.flow.pause(SECONDS_TO_WAIT*1000);
 
     // For all open pull requests…
     await sails.helpers.flow.simultaneouslyForEach(openPullRequests, async(pullRequest)=>{
@@ -45,48 +47,12 @@ module.exports = {
         githubUserToCheck: prAuthor,
         isGithubUserMaintainerOrDoesntMatter: true// « doesn't matter here because no auto-approval is happening.  Worst case, a community PR to an area with a "*" in the DRI mapping remains unfrozen.
       });
-      // let isAuthorPreapproved = await sails.helpers.flow.build(async()=>{
 
-      //   let isDRIForAllChangedPathsStill = false;
-
-      //   // [?] https://docs.github.com/en/rest/reference/pulls#list-pull-requests-files
-      //   let changedPaths = _.pluck(await sails.helpers.http.get(`https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/files`, {
-      //     per_page: 100,//eslint-disable-line camelcase
-      //   }, baseHeaders).retry(), 'filename');// (don't worry, it's the whole path, not the filename)
-
-      //   isDRIForAllChangedPathsStill = _.all(changedPaths, (changedPath)=>{
-      //     changedPath = changedPath.replace(/\/+$/,'');// « trim trailing slashes, just in case (b/c otherwise could loop forever)
-
-      //     require('assert')(githubUserToCheck !== undefined);
-      //     // sails.log.verbose(`…checking DRI of changed path "${changedPath}"`);
-
-      //     let selfMergers = DRI_BY_PATH[changedPath] ? [].concat(DRI_BY_PATH[changedPath]) : [];// « ensure array
-      //     if (selfMergers.includes(githubUserToCheck.toLowerCase()) || (isGithubUserMaintainerOrDoesntMatter && selfMergers.includes('*'))) {
-      //       return true;
-      //     }//•
-      //     let numRemainingPathsToCheck = changedPath.split('/').length;
-      //     while (numRemainingPathsToCheck > 0) {
-      //       let ancestralPath = changedPath.split('/').slice(0, -1 * numRemainingPathsToCheck).join('/');
-      //       // sails.log.verbose(`…checking DRI of ancestral path "${ancestralPath}" for changed path`);
-      //       let selfMergers = DRI_BY_PATH[ancestralPath] ? [].concat(DRI_BY_PATH[ancestralPath]) : [];// « ensure array
-      //       if (selfMergers.includes(githubUserToCheck.toLowerCase()) || (isGithubUserMaintainerOrDoesntMatter && selfMergers.includes('*'))) {
-      //         return true;
-      //       }//•
-      //       numRemainingPathsToCheck--;
-      //     }//∞
-      //   });//∞
-
-      //   if (isDRIForAllChangedPathsStill && changedPaths.length < 100) {
-      //     return true;
-      //   } else {
-      //     return false;
-      //   }
-      // });
-
-      sails.log(`#${prNumber} by @${prAuthor}:`, isAuthorPreapproved ? 'Skipping freeze…' : 'Freezing…');
-
-      if (!isDryRun) {
-        console.log('NOT A DRY RUN!');
+      if (isDryRun) {
+        sails.log(`#${prNumber} by @${prAuthor}:`, isAuthorPreapproved ? 'Would have skipped freeze…' : 'Would have frozen…');
+      } else {
+        sails.log(`#${prNumber} by @${prAuthor}:`, isAuthorPreapproved ? 'Skipping freeze…' : 'Freezing…');
+        // TODO: uncomment when ready
         // [?] https://docs.github.com/en/rest/reference/pulls#create-a-review-for-a-pull-request
         // await sails.helpers.http.post(`https://api.github.com/repos/${owner}/${repo}/pulls/${prNumber}/reviews`, {
         //   event: 'REQUEST_CHANGES',
