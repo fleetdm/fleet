8d219395827e41aafcabddbf4d2afe2d6cc78cc6
Juan Fernandez <juan@fleetdm.com>
2022-05-10 16:54:59 -0400

    Feature 5307: Fixed typo

--- server/vulnerabilities/oval/input/dpkg_object.go
+++ server/vulnerabilities/oval/input/dpkg_object.go
@@ -2,11 +2,11 @@ package oval_input
 
 // https://oval.mitre.org/language/version5.10.1/ovaldefinition/documentation/linux-definitions-schema.html#dpkginfo_object
 
-type dpkObjectNameXML struct {
+type dpkgObjectNameXML struct {
 	VarRef string `xml:"var_ref,attr"`
 }
 
 type DpkgObjectXML struct {
-	Id   string           `xml:"id,attr"`
-	Name dpkObjectNameXML `xml:"name"`
+	Id   string            `xml:"id,attr"`
+	Name dpkgObjectNameXML `xml:"name"`
 }
