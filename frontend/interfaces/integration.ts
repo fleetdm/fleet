9cfe2da6187dedbdf6e6b39266900aa0442d5dd5
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2022-05-10 22:33:30 -0400

    UI Zendesk integrations (#5356)

--- frontend/interfaces/integration.ts
+++ frontend/interfaces/integration.ts
@@ -4,30 +4,59 @@ export interface IJiraIntegration {
   api_token: string;
   project_key: string;
   enable_software_vulnerabilities?: boolean;
-  index?: number;
 }
 
-export interface IJiraIntegrationIndexed extends IJiraIntegration {
-  index: number;
-}
-
-export interface IJiraIntegrationFormData {
+export interface IZendeskIntegration {
   url: string;
-  username: string;
+  email: string;
+  api_token: string;
+  group_id: number;
+  enable_software_vulnerabilities?: boolean;
+}
+
+export interface IIntegration {
+  url: string;
+  username?: string;
+  email?: string;
+  api_token: string;
+  project_key?: string;
+  group_id?: number;
+  enable_software_vulnerabilities?: boolean;
+  originalIndex?: number;
+  type?: string;
+  tableIndex?: number;
+  dropdownIndex?: number;
+  name?: string;
+}
+
+export interface IIntegrationFormData {
+  url: string;
+  username?: string;
+  email?: string;
   apiToken: string;
-  projectKey: string;
+  projectKey?: string;
+  groupId?: number;
   enableSoftwareVulnerabilities?: boolean;
 }
 
-export interface IJiraIntegrationFormErrors {
+export interface IIntegrationTableData extends IIntegrationFormData {
+  originalIndex: number;
+  type: string;
+  tableIndex?: number;
+  name: string;
+}
+
+export interface IIntegrationFormErrors {
   url?: string | null;
+  email?: string | null;
   username?: string | null;
   apiToken?: string | null;
+  groupId?: number | null;
   projectKey?: string | null;
+  enableSoftwareVulnerabilities?: boolean;
 }
 
 export interface IIntegrations {
+  zendesk: IZendeskIntegration[];
   jira: IJiraIntegration[];
 }
-
-export type IIntegration = IJiraIntegration;
