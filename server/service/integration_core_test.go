3c4b8028417edd4133add541e66be6a8a0e270c8
Michal Nicpon <michal@fleetdm.com>
2022-05-13 09:21:51 -0600

    fix test

--- server/service/integration_core_test.go
+++ server/service/integration_core_test.go
@@ -489,10 +489,7 @@ func (s *integrationTestSuite) TestVulnerableSoftware() {
         "vulnerabilities": [
           {
             "cve": "cve-123-123-132",
-            "details_link": "https://nvd.nist.gov/vuln/detail/cve-123-123-132",
-            "cvss_score": null,
-            "epss_probability": null,
-            "cisa_known_exploit": null
+            "details_link": "https://nvd.nist.gov/vuln/detail/cve-123-123-132"
           }
         ]`
 	expectedJSONSoft1 := `"name": "foo",
