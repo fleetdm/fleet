e879b6391ec8adf7b65013022f68eba8f8b6830d
Michal Nicpon <michal@fleetdm.com>
2022-05-17 11:59:26 -0600

    add comment

--- ee/server/service/hosts.go
+++ ee/server/service/hosts.go
@@ -7,9 +7,11 @@ import (
 )
 
 func (svc *Service) GetHost(ctx context.Context, id uint, includeCVEScores bool) (*fleet.HostDetail, error) {
+	// reuse GetHost, but include cve scores in premium version
 	return svc.Service.GetHost(ctx, id, true)
 }
 
 func (svc *Service) HostByIdentifier(ctx context.Context, identifier string, includeCVEScores bool) (*fleet.HostDetail, error) {
+	// reuse HostByIdentifier, but include cve scores in premium version
 	return svc.Service.HostByIdentifier(ctx, identifier, true)
 }
--- ee/server/service/software.go
+++ ee/server/service/software.go
@@ -7,10 +7,12 @@ import (
 )
 
 func (svc Service) ListSoftware(ctx context.Context, opts fleet.SoftwareListOptions) ([]fleet.Software, error) {
+	// reuse ListSoftware, but include cve scores in premium version
 	opts.IncludeCVEScores = true
 	return svc.Service.ListSoftware(ctx, opts)
 }
 
 func (svc *Service) SoftwareByID(ctx context.Context, id uint, includeCVEScores bool) (*fleet.Software, error) {
+	// reuse SoftwareByID, but include cve scores in premium version
 	return svc.Service.SoftwareByID(ctx, id, true)
 }
