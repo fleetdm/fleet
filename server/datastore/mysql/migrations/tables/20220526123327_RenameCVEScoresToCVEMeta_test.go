da906ea75ea5b75256957ac213bad3a03bdff1fe
Michal Nicpon <michal@fleetdm.com>
2022-05-27 16:15:41 -0600

    add test

--- server/datastore/mysql/migrations/tables/20220526123327_RenameCVEScoresToCVEMeta_test.go
+++ server/datastore/mysql/migrations/tables/20220526123327_RenameCVEScoresToCVEMeta_test.go
@@ -1,21 +1,27 @@
-
 package tables
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestUp_20220526123327(t *testing.T) {
 	db := applyUpToPrev(t)
 
-	//
-	// Insert data to test the migration
-	//
-	// ...
-
-	// Apply current migration.
 	applyNext(t, db)
 
-	//
-	// Check data, insert new entries, e.g. to verify migration is safe.
-	//
-	// ...
+	query := `
+INSERT INTO cve_meta (
+    cve,
+    cvss_score,
+    epss_probability,
+    cisa_known_exploit,
+    published
+)
+VALUES (?, ?, ?, ?, ?)
+`
+	_, err := db.Exec(query, "CVE-2022-29464", 9.8, 0.63387, true, time.Now())
+	require.NoError(t, err)
 }
