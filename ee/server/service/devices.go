eb2334f5d1a5bcdbf82b5c5f5b51763e14ca6a63
Roberto Dip <dip.jesusr@gmail.com>
2022-05-31 11:07:07 -0300

    remove unused code

--- ee/server/service/devices.go
+++ ee/server/service/devices.go
@@ -3,23 +3,9 @@ package service
 import (
 	"context"
 
-	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	"github.com/fleetdm/fleet/v4/server/fleet"
 )
 
 func (svc *Service) ListDevicePolicies(ctx context.Context, host *fleet.Host) ([]*fleet.HostPolicy, error) {
-	if err := svc.authz.Authorize(ctx, &fleet.Host{}, fleet.ActionList); err != nil {
-		return nil, err
-	}
-
-	host, err := svc.ds.HostLite(ctx, host.ID)
-	if err != nil {
-		return nil, ctxerr.Wrap(ctx, err, "find host for device policies")
-	}
-
-	if err := svc.authz.Authorize(ctx, host, fleet.ActionRead); err != nil {
-		return nil, err
-	}
-
 	return svc.ds.ListPoliciesForHost(ctx, host)
 }
