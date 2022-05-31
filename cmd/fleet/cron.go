9a6ae352027c6b6f92ba5347be4cb6a28ce42a88
Juan Fernandez <juan-fdz-hawa@users.noreply.github.com>
2022-05-31 12:51:10 -0400

    Fixed typo
    Co-authored-by: Michal Nicpon <39177923+michalnicp@users.noreply.github.com>

--- cmd/fleet/cron.go
+++ cmd/fleet/cron.go
@@ -328,7 +328,7 @@ func checkOvalVulnerabilities(
 		start := time.Now()
 		_, err := oval.Analyze(ctx, ds, version, vulnPath)
 		elapsed := time.Since(start)
-		level.Debug(logger).Log("msg", "oval-analysis-done", "platfrom", version.Name, "elapsed", elapsed)
+		level.Debug(logger).Log("msg", "oval-analysis-done", "platform", version.Name, "elapsed", elapsed)
 
 		if err != nil {
 			level.Error(logger).Log("msg", "analyzing oval definitions", "err", err)
