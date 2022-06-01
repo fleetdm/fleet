538b06c281afafe2133225fc2655d1db9191e717
Roberto Dip <dip.jesusr@gmail.com>
2022-06-01 09:52:42 -0300

    add Go client to consume device endpoints

--- server/service/client.go
+++ server/service/client.go
@@ -3,36 +3,22 @@ package service
 import (
 	"bytes"
 	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
 	"time"
 
-	"github.com/fleetdm/fleet/v4/pkg/fleethttp"
 	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	"github.com/fleetdm/fleet/v4/server/fleet"
 )
 
-// httpClient interface allows the HTTP methods to be mocked.
-type httpClient interface {
-	Do(req *http.Request) (*http.Response, error)
-}
-
 type Client struct {
-	addr               string
-	baseURL            *url.URL
-	urlPrefix          string
-	token              string
-	http               httpClient
-	insecureSkipVerify bool
+	*baseClient
+	addr  string
+	token string
 
 	writer io.Writer
 }
@@ -42,46 +28,15 @@ type ClientOption func(*Client) error
 func NewClient(addr string, insecureSkipVerify bool, rootCA, urlPrefix string, options ...ClientOption) (*Client, error) {
 	// TODO #265 refactor all optional parameters to functional options
 	// API breaking change, needs a major version release
-	baseURL, err := url.Parse(addr)
+	baseClient, err := newBaseClient(addr, insecureSkipVerify, rootCA, urlPrefix)
+
 	if err != nil {
-		return nil, fmt.Errorf("parsing URL: %w", err)
+		return nil, err
 	}
-
-	if baseURL.Scheme != "https" && !strings.Contains(baseURL.Host, "localhost") && !strings.Contains(baseURL.Host, "127.0.0.1") {
-		return nil, errors.New("address must start with https:// for remote connections")
-	}
-
-	rootCAPool := x509.NewCertPool()
-	if rootCA != "" {
-		// read in the root cert file specified in the context
-		certs, err := ioutil.ReadFile(rootCA)
-		if err != nil {
-			return nil, fmt.Errorf("reading root CA: %w", err)
-		}
-
-		// add certs to pool
-		if ok := rootCAPool.AppendCertsFromPEM(certs); !ok {
-			return nil, errors.New("failed to add certificates to root CA pool")
-		}
-	} else if !insecureSkipVerify {
-		// Use only the system certs (doesn't work on Windows)
-		rootCAPool, err = x509.SystemCertPool()
-		if err != nil {
-			return nil, fmt.Errorf("loading system cert pool: %w", err)
-		}
-	}
-
-	httpClient := fleethttp.NewClient(fleethttp.WithTLSClientConfig(&tls.Config{
-		InsecureSkipVerify: insecureSkipVerify,
-		RootCAs:            rootCAPool,
-	}))
 
 	client := &Client{
-		addr:               addr,
-		baseURL:            baseURL,
-		http:               httpClient,
-		insecureSkipVerify: insecureSkipVerify,
-		urlPrefix:          urlPrefix,
+		baseClient: baseClient,
+		addr:       addr,
 	}
 
 	for _, option := range options {
@@ -179,13 +134,6 @@ func (c *Client) SetToken(t string) {
 	c.token = t
 }
 
-func (c *Client) url(path, rawQuery string) *url.URL {
-	u := *c.baseURL
-	u.Path = c.urlPrefix + path
-	u.RawQuery = rawQuery
-	return &u
-}
-
 // http.RoundTripper that will log debug information about the request and
 // response, including paths, timing, and body.
 //
@@ -240,34 +188,7 @@ func (c *Client) authenticatedRequestWithQuery(params interface{}, verb string, 
 	}
 	defer response.Body.Close()
 
-	switch response.StatusCode {
-	case http.StatusOK:
-		// ok
-	case http.StatusNotFound:
-		return notFoundErr{}
-	case http.StatusUnauthorized:
-		return ErrUnauthenticated
-	default:
-		return fmt.Errorf(
-			"%s %s received status %d %s",
-			verb, path,
-			response.StatusCode,
-			extractServerErrorText(response.Body),
-		)
-	}
-
-	err = json.NewDecoder(response.Body).Decode(&responseDest)
-	if err != nil {
-		return fmt.Errorf("decode %s %s response: %w", verb, path, err)
-	}
-
-	if e, ok := responseDest.(errorer); ok {
-		if e.error() != nil {
-			return fmt.Errorf("%s %s error: %s", verb, path, e.error())
-		}
-	}
-
-	return nil
+	return c.parseResponse(verb, path, response, responseDest)
 }
 
 func (c *Client) authenticatedRequest(params interface{}, verb string, path string, responseDest interface{}) error {
