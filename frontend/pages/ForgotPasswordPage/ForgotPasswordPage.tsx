5efee7c8f852a1d8583c844a8a0dbc3ceff0a79b
Luke Heath <luke@fleetdm.com>
2022-06-06 19:16:54 -0500

    Improving password recovery flow

--- frontend/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ frontend/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -9,6 +9,7 @@ import ForgotPasswordForm from "components/forms/ForgotPasswordForm";
 // @ts-ignore
 import StackedWhiteBoxes from "components/StackedWhiteBoxes";
 import AuthenticationFormWrapper from "components/AuthenticationFormWrapper";
+import ExternalURLIcon from "../../../assets/images/icon-external-url-12x12@2x.png";
 
 const ForgotPasswordPage = () => {
   const [email, setEmail] = useState<string>("");
@@ -37,14 +38,27 @@ const ForgotPasswordPage = () => {
 
     if (email) {
       return (
-        <div>
-          <div className={`${baseClass}__text-wrapper`}>
-            <p className={`${baseClass}__text`}>
-              An email was sent to
-              <span className={`${baseClass}__email`}> {email}</span>. Click the
-              link on the email to proceed with the password reset process.
-            </p>
-          </div>
+        <div className={`${baseClass}__text-wrapper`}>
+          <p className={`${baseClass}__text`}>
+            An email was sent to{" "}
+            <span className={`${baseClass}__email`}>{email}</span>. Click the
+            link in the email to proceed with the password reset process. If you
+            did not receive an email please contact your Fleet administrator.
+            <br />
+            You can find more information on resetting passwords at the{" "}
+            <a
+              href="https://fleetdm.com/docs/using-fleet/fleetctl-cli#using-fleetctl-with-an-api-only-user?utm_medium=fleetui&utm_campaign=get-api-token"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Password reset FAQ
+            </a>
+            <img
+              alt="external link icon"
+              className="icon-external"
+              src={ExternalURLIcon}
+            />
+          </p>
         </div>
       );
     }
@@ -59,11 +73,14 @@ const ForgotPasswordPage = () => {
   };
 
   const leadText =
-    "Enter your email below and we will email you a link so that you can reset your password.";
+    "Enter your email below to receive an email with instructions to reset your password.";
 
   return (
     <AuthenticationFormWrapper>
-      <StackedWhiteBoxes leadText={leadText} previousLocation={PATHS.LOGIN}>
+      <StackedWhiteBoxes
+        leadText={email ? "" : leadText}
+        previousLocation={PATHS.LOGIN}
+      >
         {renderContent()}
       </StackedWhiteBoxes>
     </AuthenticationFormWrapper>
