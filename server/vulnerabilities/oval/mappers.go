337734fce1474aab4ba2fcf61a5ab48355ccdb5d
Juan Fernandez <juan-fdz-hawa@users.noreply.github.com>
2022-06-08 14:29:58 -0400

    Bug: Issue while parsing dpkg objects with inline names (#6146)
    * Bug: Issue while parsing dpkg objects with inline names
    
    Dpkg objects can also define their names inline, not only using variable references.

--- server/vulnerabilities/oval/mappers.go
+++ server/vulnerabilities/oval/mappers.go
@@ -89,12 +89,31 @@ func mapPackageState(sta oval_input.DpkgStateXML) ([]oval_parsed.ObjectStateEvrS
 }
 
 func mapPackageObject(obj oval_input.DpkgObjectXML, vars map[string]oval_input.ConstantVariableXML) ([]string, error) {
+	// Test objects can define their 'name' in one of two ways:
+	// 1. Inline:
+	// <:object ...>
+	//      <:name>software name</:name>
+	// </:object>
+	//
+	// 2. As a variable reference:
+	// <:object ...>
+	// 		<:name var_ref="var:200224390000000" var_check="at least one" />
+	// </:object>
+
+	// Check whether the name was defined inline
+	if obj.Name.Value != "" {
+		return []string{obj.Name.Value}, nil
+	}
+
+	var r []string
+	// If not, the name should be defined as a variable
 	variable, ok := vars[obj.Name.VarRef]
 	if !ok {
 		return nil, fmt.Errorf("variable not found %s", obj.Name.VarRef)
 	}
 
-	var r []string
+	// Normally the variable for a test object contains a single value, but according to the specs,
+	// it can contain multiple values.
 	r = append(r, variable.Values...)
 
 	return r, nil
