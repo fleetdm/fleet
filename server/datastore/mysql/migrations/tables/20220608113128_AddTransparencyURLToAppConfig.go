562f37bf568095f4be64fbf70230a4256b159e2b
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-06-09 10:39:14 -0500

    Add updateAppConfigJSON method

--- server/datastore/mysql/migrations/tables/20220608113128_AddTransparencyURLToAppConfig.go
+++ server/datastore/mysql/migrations/tables/20220608113128_AddTransparencyURLToAppConfig.go
@@ -2,7 +2,6 @@ package tables
 
 import (
 	"database/sql"
-	"encoding/json"
 
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/pkg/errors"
@@ -13,32 +12,14 @@ func init() {
 }
 
 func Up_20220608113128(tx *sql.Tx) error {
-	var raw []byte
-	row := tx.QueryRow(`SELECT json_value FROM app_config_json LIMIT 1`)
-	if err := row.Scan(&raw); err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil
+	err := updateAppConfigJSON(tx, func(config *fleet.AppConfig) error {
+		if config.FleetDesktop.TransparencyURL != "" {
+			return errors.New("unexpected transparency_url value in app_config_json")
 		}
-		return errors.Wrap(err, "select app_config_json")
-	}
-
-	var config fleet.AppConfig
-	if err := json.Unmarshal(raw, &config); err != nil {
-		return errors.Wrap(err, "unmarshal appconfig")
-	}
-
-	if config.FleetDesktop.TransparencyURL != "" {
-		return errors.New("unexpected transparency_url value in app_config_json")
-	}
-
-	b, err := json.Marshal(config)
+		return nil
+	})
 	if err != nil {
-		return errors.Wrap(err, "marshal updated appconfig")
-	}
-
-	const updateStmt = `UPDATE app_config_json SET json_value = ? WHERE id = 1`
-	if _, err := tx.Exec(updateStmt, b); err != nil {
-		return errors.Wrap(err, "update app_config_json")
+		return err
 	}
 
 	return nil
--- server/datastore/mysql/migrations/tables/migration.go
+++ server/datastore/mysql/migrations/tables/migration.go
@@ -2,13 +2,14 @@ package tables
 
 import (
 	"database/sql"
+	"encoding/json"
 
+	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/fleetdm/goose"
+	"github.com/pkg/errors"
 )
 
-var (
-	MigrationClient = goose.New("migration_status_tables", goose.MySqlDialect{})
-)
+var MigrationClient = goose.New("migration_status_tables", goose.MySqlDialect{})
 
 func columnExists(tx *sql.Tx, table, column string) bool {
 	var count int
@@ -30,4 +31,36 @@ WHERE
 	}
 
 	return count > 0
+}
+
+func updateAppConfigJSON(tx *sql.Tx, fn func(config *fleet.AppConfig) error) error {
+	var raw []byte
+	row := tx.QueryRow(`SELECT json_value FROM app_config_json LIMIT 1`)
+	if err := row.Scan(&raw); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil
+		}
+		return errors.Wrap(err, "select app_config_json")
+	}
+
+	var config fleet.AppConfig
+	if err := json.Unmarshal(raw, &config); err != nil {
+		return errors.Wrap(err, "unmarshal appconfig")
+	}
+
+	if err := fn(&config); err != nil {
+		return err
+	}
+
+	b, err := json.Marshal(config)
+	if err != nil {
+		return errors.Wrap(err, "marshal updated appconfig")
+	}
+
+	const updateStmt = `UPDATE app_config_json SET json_value = ? WHERE id = 1`
+	if _, err := tx.Exec(updateStmt, b); err != nil {
+		return errors.Wrap(err, "update app_config_json")
+	}
+
+	return nil
 }
