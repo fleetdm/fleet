199658956d4d8aee22c3cc5275536061539f7a8d
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2022-06-15 11:27:58 -0400

    Forgot a file

---
+++ infrastructure/demo/SharedInfrastructure/alb.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "main" {
+  name                       = var.prefix
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.lb.id]
+  subnets                    = var.vpc.public_subnets
+  enable_deletion_protection = true
+}
+
+resource "aws_security_group" "lb" {
+  name   = "${var.prefix}-lb"
+  vpc_id = var.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "try a subdomain"
+      status_code  = "404"
+    }
+  }
+}
+
+output "alb_listener" {
+  value = aws_lb_listener.main
+}
