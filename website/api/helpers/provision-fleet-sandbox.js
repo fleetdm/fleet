f6f119fcd7861799ba060db572c4a2991dcd107b
Eric <eashaw@sailsjs.com>
2022-06-24 18:41:01 -0500

    update helper comments

--- website/api/helpers/provision-fleet-sandbox.js
+++ website/api/helpers/provision-fleet-sandbox.js
@@ -11,7 +11,7 @@ module.exports = {
 
     // userID: {}
 
-    // sandboxExpirationTimestamp: { }
+    // sandboxExpiresAt: {}
 
   },
 
@@ -34,18 +34,27 @@ module.exports = {
     // If(user.fleetSandboxURL) {
     // If the user has a fleetSandboxURL, throw an error
 
+    // If a sandboxExpiresAt was provided, we'll use that value, otherwise we'll create a new timestamp. Why?
+    // let expiresAt;
+    // if(!inputs.sandboxExpiresAt) {
+    //   expiresAt = New Date(Date.now() + (24*60*60*1000)).toISOString();
+    // } else {
+    //   expiresAt = inputs.sandboxExpiresAt;
+    // }
+    // Note: Both this helper and The cloud provisioner API expect an ISO 8601 string timestamp, but we store the timestamp in the website database as a JS timestamp
+
     // Send POST request to the cloud provisioner API
-    // let cloudProvisionerResponse = await sails.helpers.http.post('CLOUD_PROVISIONER_API_URL', {REQUEST_DATA}).timeout(5000).interpect(TODO)
-    //
+    // let cloudProvisionerResponse = await sails.helpers.http.post('CLOUD_PROVISIONER_API_URL', {
+      // API Request data
+    // }).timeout(5000).intercept(TODO);
 
-    // Attach the URL and demoKey in the response to the user record
 
-    // If the request was successful, update the user record with the fleetSandboxURL and sandboxExpirationTimestamp
+    // If the request was successful, update the user record with the fleetSandboxURL and sandboxExpiresAt
     // await User.updateOne({id: user.id}).set({
     //   fleetSandboxURL: cloudProvisionerResponse.url,
-    //   fleetSandboxExpiresAt: Date.parse(sandboxExpirationTimestamp), // Note: We store the expiration timestamp as a JS timestamp, instead of an ISO string.
+    //   fleetSandboxExpiresAt: Date.parse(sandboxExpiresAt), // Note: We store the expiration timestamp as a JS timestamp, instead of an ISO string.
     // });
-    // return the fleetSandboxURL
+
     // return fleetSandboxURL;
   }
 
