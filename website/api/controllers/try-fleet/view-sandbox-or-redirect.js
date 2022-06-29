073edd0832d655e972d3fd99b0de3f7c7b7aaacd
Eric <eashaw@sailsjs.com>
2022-06-29 17:13:32 -0500

    view-sandbox » view-sandbox-or-redirect

---
+++ website/api/controllers/try-fleet/view-sandbox-or-redirect.js
@@ -0,0 +1,56 @@
+module.exports = {
+
+
+  friendlyName: 'View sandbox',
+
+
+  description: 'Display "Sandbox" page or redirect users to their Fleet Sandbox instance.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/try-fleet/sandbox'
+    },
+
+    redirect: {
+      description: 'The User is being redirected to their Fleet sandbox instance',
+      responseType: 'redirect'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // If the user is not logged in, redirect them to the Fleet sandbox registration page.
+    if (!this.req.me) {
+      throw {redirect: '/try-fleet/register'};
+    }
+
+
+    // Check if the user has a fleetSandboxURL
+    if(this.req.me.fleetSandboxURL) {
+
+      // Check if this sandbox instance is expired.
+      if(this.req.me.fleetSandboxExpiresAt > Date.now()) {
+
+        // If this is a valid fleet sandbox instance, we'll check the /healthz endpoint before redirecting the user to their sandbox.
+        await sails.helpers.flow.until(async function () {
+          let serverResponse = await sails.helpers.http.sendHttpRequest('GET', this.req.me.fleetSandboxURL+'/healthz').timeout(5000).tolerate('non200Response').tolerate('requestFailed');
+          if(serverResponse) {
+            return serverResponse.statusCode === 200;
+          }
+        });
+        throw {redirect: this.req.me.fleetSandboxURL+'?demoKey='+this.req.me.fleetSandboxDemoKey};
+      }
+    }
+
+
+    // If the user doesn't have a fleetSandboxURL, or their Fleet Sandbox instance is expired, they will be taken to the sandbox page.
+    // Respond with view.
+    return;
+  }
+
+
+};
