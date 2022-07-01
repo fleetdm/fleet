0cb6aa63db6a4b1bd64384ddd18a0876eb053daa
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-07-01 15:56:05 -0400

    Test teams policy automation integration with Jira and Zendesk

--- cypress/integration/premium/admin.spec.ts
+++ cypress/integration/premium/admin.spec.ts
@@ -1,6 +1,312 @@
 import CONSTANTS from "../../support/constants";
 
-const { GOOD_PASSWORD } = CONSTANTS;
+const { GOOD_PASSWORD, CONFIG_INTEGRATIONS_AUTOMATIONS } = CONSTANTS;
+
+const patchTeamJiraPoliciesIntegration = {
+  integrations: {
+    jira: [
+      {
+        enable_failing_policies: false,
+        project_key: "PROJECT 1",
+        url: "https://fleetdm.atlassian.com",
+      },
+      {
+        enable_failing_policies: true,
+        project_key: "PROJECT 2",
+        url: "https://fleetdm.atlassian.com",
+      },
+    ],
+    zendesk: [
+      {
+        enable_failing_policies: false,
+        group_id: 12345678,
+        url: "https://fleetdm.zendesk.com",
+      },
+      {
+        enable_failing_policies: false,
+        group_id: 87654321,
+        url: "https://fleetdm.zendesk.com",
+      },
+    ],
+  },
+  webhook_settings: {
+    failing_policies_webhook: {
+      destination_url: "https://example.com/global_admin",
+      enable_failing_policies_webhook: false,
+      policy_ids: [1, 3, 2],
+    },
+  },
+};
+
+const getTeamJiraPoliciesIntegration = {
+  team: {
+    id: 1,
+    created_at: "2022-07-01T19:31:46Z",
+    name: "Apples",
+    description: "",
+    agent_options: {
+      config: {
+        options: {
+          logger_plugin: "tls",
+          pack_delimiter: "/",
+          logger_tls_period: 10,
+          distributed_plugin: "tls",
+          disable_distributed: false,
+          logger_tls_endpoint: "/api/osquery/log",
+          distributed_interval: 10,
+          distributed_tls_max_attempts: 3,
+        },
+        decorators: {
+          load: [
+            "SELECT uuid AS host_uuid FROM system_info;",
+            "SELECT hostname AS hostname FROM system_info;",
+          ],
+        },
+      },
+      overrides: {},
+    },
+    webhook_settings: {
+      failing_policies_webhook: {
+        enable_failing_policies_webhook: false,
+        destination_url: "https://example.com/global_admin",
+        policy_ids: [1, 3, 2],
+        host_batch_size: 0,
+      },
+    },
+    integrations: {
+      jira: [
+        {
+          enable_failing_policies: false,
+          project_key: "PROJECT 1",
+          url: "https://fleetdm.atlassian.com",
+        },
+        {
+          enable_failing_policies: true,
+          project_key: "PROJECT 2",
+          url: "https://fleetdm.atlassian.com",
+        },
+      ],
+      zendesk: [
+        {
+          enable_failing_policies: false,
+          group_id: 12345678,
+          url: "https://fleetdm.zendesk.com",
+        },
+        {
+          enable_failing_policies: false,
+          group_id: 87654321,
+          url: "https://fleetdm.zendesk.com",
+        },
+      ],
+    },
+    user_count: 0,
+    users: [
+      {
+        created_at: "0001-01-01T00:00:00Z",
+        updated_at: "0001-01-01T00:00:00Z",
+        id: 8,
+        name: "Marco",
+        email: "marco@organization.com",
+        force_password_reset: false,
+        gravatar_url: "",
+        sso_enabled: false,
+        global_role: null,
+        api_only: false,
+        teams: null,
+        role: "observer",
+      },
+      {
+        created_at: "0001-01-01T00:00:00Z",
+        updated_at: "0001-01-01T00:00:00Z",
+        id: 9,
+        name: "Anita T. Admin",
+        email: "anita@organization.com",
+        force_password_reset: false,
+        gravatar_url: "",
+        sso_enabled: false,
+        global_role: null,
+        api_only: false,
+        teams: null,
+        role: "admin",
+      },
+      {
+        created_at: "0001-01-01T00:00:00Z",
+        updated_at: "0001-01-01T00:00:00Z",
+        id: 10,
+        name: "Toni",
+        email: "toni@organization.com",
+        force_password_reset: false,
+        gravatar_url: "",
+        sso_enabled: false,
+        global_role: null,
+        api_only: false,
+        teams: null,
+        role: "observer",
+      },
+    ],
+    host_count: 0,
+    secrets: [
+      {
+        secret: "OgkyoX/SGsuvLXPaNHUVIJoYSx1PTV+S",
+        created_at: "2022-07-01T19:31:46Z",
+        team_id: 1,
+      },
+    ],
+  },
+};
+
+const patchTeamZendeskPoliciesIntegration = {
+  integrations: {
+    jira: [
+      {
+        enable_failing_policies: false,
+        project_key: "PROJECT 1",
+        url: "https://fleetdm.atlassian.com",
+      },
+      {
+        enable_failing_policies: false,
+        project_key: "PROJECT 2",
+        url: "https://fleetdm.atlassian.com",
+      },
+    ],
+    zendesk: [
+      {
+        enable_failing_policies: false,
+        group_id: 12345678,
+        url: "https://fleetdm.zendesk.com",
+      },
+      {
+        enable_failing_policies: true,
+        group_id: 87654321,
+        url: "https://fleetdm.zendesk.com",
+      },
+    ],
+  },
+  webhook_settings: {
+    failing_policies_webhook: {
+      destination_url: "https://example.com/global_admin",
+      enable_failing_policies_webhook: false,
+      policy_ids: [1, 3, 2],
+    },
+  },
+};
+
+const getTeamZendeskPoliciesIntegration = {
+  team: {
+    id: 1,
+    created_at: "2022-07-01T19:31:46Z",
+    name: "Apples",
+    description: "",
+    agent_options: {
+      config: {
+        options: {
+          logger_plugin: "tls",
+          pack_delimiter: "/",
+          logger_tls_period: 10,
+          distributed_plugin: "tls",
+          disable_distributed: false,
+          logger_tls_endpoint: "/api/osquery/log",
+          distributed_interval: 10,
+          distributed_tls_max_attempts: 3,
+        },
+        decorators: {
+          load: [
+            "SELECT uuid AS host_uuid FROM system_info;",
+            "SELECT hostname AS hostname FROM system_info;",
+          ],
+        },
+      },
+      overrides: {},
+    },
+    webhook_settings: {
+      failing_policies_webhook: {
+        enable_failing_policies_webhook: false,
+        destination_url: "https://example.com/global_admin",
+        policy_ids: [1, 3, 2],
+        host_batch_size: 0,
+      },
+    },
+    integrations: {
+      jira: [
+        {
+          enable_failing_policies: false,
+          project_key: "PROJECT 1",
+          url: "https://fleetdm.atlassian.com",
+        },
+        {
+          enable_failing_policies: false,
+          project_key: "PROJECT 2",
+          url: "https://fleetdm.atlassian.com",
+        },
+      ],
+      zendesk: [
+        {
+          enable_failing_policies: false,
+          group_id: 12345678,
+          url: "https://fleetdm.zendesk.com",
+        },
+        {
+          enable_failing_policies: true,
+          group_id: 87654321,
+          url: "https://fleetdm.zendesk.com",
+        },
+      ],
+    },
+    user_count: 0,
+    users: [
+      {
+        created_at: "0001-01-01T00:00:00Z",
+        updated_at: "0001-01-01T00:00:00Z",
+        id: 8,
+        name: "Marco",
+        email: "marco@organization.com",
+        force_password_reset: false,
+        gravatar_url: "",
+        sso_enabled: false,
+        global_role: null,
+        api_only: false,
+        teams: null,
+        role: "observer",
+      },
+      {
+        created_at: "0001-01-01T00:00:00Z",
+        updated_at: "0001-01-01T00:00:00Z",
+        id: 9,
+        name: "Anita T. Admin",
+        email: "anita@organization.com",
+        force_password_reset: false,
+        gravatar_url: "",
+        sso_enabled: false,
+        global_role: null,
+        api_only: false,
+        teams: null,
+        role: "admin",
+      },
+      {
+        created_at: "0001-01-01T00:00:00Z",
+        updated_at: "0001-01-01T00:00:00Z",
+        id: 10,
+        name: "Toni",
+        email: "toni@organization.com",
+        force_password_reset: false,
+        gravatar_url: "",
+        sso_enabled: false,
+        global_role: null,
+        api_only: false,
+        teams: null,
+        role: "observer",
+      },
+    ],
+    host_count: 0,
+    secrets: [
+      {
+        secret: "OgkyoX/SGsuvLXPaNHUVIJoYSx1PTV+S",
+        created_at: "2022-07-01T19:31:46Z",
+        team_id: 1,
+      },
+    ],
+  },
+};
 
 describe("Premium tier - Global Admin user", () => {
   before(() => {
@@ -193,7 +499,7 @@ describe("Premium tier - Global Admin user", () => {
       });
       cy.findByText(/successfully updated policy automations/i).should("exist");
     });
-    it("allows global admin to automate a team policy", () => {
+    it("allows global admin to automate a team policy webhook", () => {
       cy.visit("/policies/manage");
       cy.getAttached(".Select-control").within(() => {
         cy.findByText(/all teams/i).click();
@@ -214,7 +520,62 @@ describe("Premium tier - Global Admin user", () => {
       });
       cy.findByText(/successfully updated policy automations/i).should("exist");
     });
-    it("allows global admin to delete a team policy", () => {
+  });
+  it("allows global admin to delete a team policy", () => {
+    cy.visit("/policies/manage");
+    cy.getAttached(".Select-control").within(() => {
+      cy.findByText(/all teams/i).click();
+    });
+    cy.getAttached(".Select-menu")
+      .contains(/apples/i)
+      .click();
+    cy.getAttached("tbody").within(() => {
+      cy.getAttached("tr")
+        .first()
+        .within(() => {
+          cy.getAttached(".fleet-checkbox__input").check({
+            force: true,
+          });
+        });
+    });
+    cy.findByRole("button", { name: /delete/i }).click();
+    cy.getAttached(".remove-policies-modal").within(() => {
+      cy.findByRole("button", { name: /delete/i }).should("exist");
+      cy.findByRole("button", { name: /cancel/i }).click();
+    });
+  });
+  it("allows global admin to edit a team policy", () => {
+    cy.visit("policies/manage");
+    cy.findByText(/all teams/i).click();
+    cy.findByText(/apples/i).click();
+    cy.getAttached("tbody").within(() => {
+      cy.getAttached("tr")
+        .first()
+        .within(() => {
+          cy.getAttached(".fleet-checkbox__input").check({
+            force: true,
+          });
+        });
+    });
+    cy.findByText(/filevault enabled/i).click();
+    cy.getAttached(".policy-form__button-wrap").within(() => {
+      cy.findByRole("button", { name: /run/i }).should("exist");
+      cy.findByRole("button", { name: /save/i }).should("exist");
+    });
+  });
+  describe("Manage policies page (mock integrations)", () => {
+    beforeEach(() => {
+      cy.intercept(
+        "GET",
+        "/api/latest/fleet/config",
+        CONFIG_INTEGRATIONS_AUTOMATIONS
+      ).as("getIntegrations");
+      cy.visit("/policies/manage");
+      cy.wait("@getIntegrations").then((configStub) => {
+        console.log(JSON.stringify(configStub));
+      });
+    });
+    it("allows global admin to delete and automate a team policy jira integration", () => {
       cy.visit("/policies/manage");
       cy.getAttached(".Select-control").within(() => {
         cy.findByText(/all teams/i).click();
@@ -235,25 +596,68 @@ describe("Premium tier - Global Admin user", () => {
       cy.getAttached(".delete-policies-modal").within(() => {
         cy.findByRole("button", { name: /delete/i }).should("exist");
         cy.findByRole("button", { name: /cancel/i }).click();
+        cy.getAttached(".button-wrap")
+          .findByRole("button", { name: /manage automations/i })
+          .click();
+        cy.getAttached(".manage-automations-modal").within(() => {
+          cy.getAttached(".fleet-slider").click();
+          cy.getAttached(".fleet-slider").click();
+          cy.getAttached(".fleet-checkbox__input").check({ force: true });
+          cy.getAttached("#ticket-radio-btn").next().click();
+          cy.findByText(/select integration/i).click();
+          cy.findByText(/project 2/i).click();
+          cy.intercept(
+            "PATCH",
+            "/api/latest/fleet/teams/1",
+            patchTeamJiraPoliciesIntegration
+          ).as("enableJiraPoliciesIntegration");
+          cy.intercept(
+            "GET",
+            "/api/latest/fleet/teams/1",
+            getTeamJiraPoliciesIntegration
+          ).as("enabledJiraPoliciesIntegration");
+          cy.findByText(/save/i).click();
+        });
+        cy.findByText(/successfully updated policy automations/i).should(
+          "exist"
+        );
       });
-    });
-    it("allows global admin to edit a team policy", () => {
-      cy.visit("policies/manage");
-      cy.findByText(/all teams/i).click();
-      cy.findByText(/apples/i).click();
-      cy.getAttached("tbody").within(() => {
-        cy.getAttached("tr")
-          .first()
-          .within(() => {
-            cy.getAttached(".fleet-checkbox__input").check({
-              force: true,
-            });
+      it("allows global admin to automate a team policy zendesk integration", () => {
+        cy.visit("/policies/manage");
+        cy.getAttached(".Select-control").within(() => {
+          cy.findByText(/all teams/i).click();
+        });
+        cy.getAttached(".Select-menu")
+          .contains(/apples/i)
+          .click();
+        cy.getAttached(".button-wrap")
+          .findByRole("button", { name: /manage automations/i })
+          .click();
+        cy.getAttached(".manage-automations-modal").within(() => {
+          cy.getAttached(".fleet-slider").click();
+          cy.getAttached(".fleet-slider").click();
+          cy.getAttached(".fleet-checkbox__input").check({ force: true });
+          cy.getAttached("#ticket-radio-btn").next().click();
+          cy.findByText(/select integration/i).click();
+          cy.findByText(/87654321/i).click();
+          cy.intercept(
+            "PATCH",
+            "/api/latest/fleet/teams/1",
+            patchTeamZendeskPoliciesIntegration
+          ).as("enableZendeskPoliciesIntegration");
+          cy.intercept(
+            "GET",
+            "/api/latest/fleet/teams/1",
+            getTeamZendeskPoliciesIntegration
+          ).as("enabledZendeskPoliciesIntegration");
+          cy.findByText(/save/i).click();
+          cy.wait("@enableZendeskPoliciesIntegration").then((configStub) => {
+            console.log(JSON.stringify(configStub));
           });
-      });
-      cy.findByText(/filevault enabled/i).click();
-      cy.getAttached(".policy-form__button-wrap").within(() => {
-        cy.findByRole("button", { name: /run/i }).should("exist");
-        cy.findByRole("button", { name: /save/i }).should("exist");
+        });
+        cy.findByText(/successfully updated policy automations/i).should(
+          "exist"
+        );
       });
     });
   });
