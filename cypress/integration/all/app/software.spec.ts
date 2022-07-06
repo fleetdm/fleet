6d3e3fc11b09093cb956b112646e31c9b4c3b6d5
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-07-06 13:03:15 -0500

    Try fix for cypress test

--- cypress/integration/all/app/software.spec.ts
+++ cypress/integration/all/app/software.spec.ts
@@ -16,9 +16,11 @@ describe("Software", () => {
       cy.visit("/software/manage");
     });
     it("displays total software count", () => {
-      cy.getAttached(".table-container__header-left").within(() => {
-        cy.findByText(/902 software items/i).should("exist");
-      });
+      cy.getAttached(".manage-software-page__count")
+        .not(".count-loading")
+        .within(() => {
+          cy.findByText(/902 software items/i).should("exist");
+        });
     });
   });
 });
