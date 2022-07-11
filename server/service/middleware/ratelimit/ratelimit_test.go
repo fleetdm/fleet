af0cf9b70316cefcecdac740df73f27b758ee760
Tomas Touceda <chiiph@gmail.com>
2022-07-11 10:49:05 -0300

    Add rate limits for device authed endpoints (#6529)
    * Add rate limits for device authed endpoints
    
    * Fix lint
    
    * Add missing test
    
    * Fix test
    
    * Increase the quota for desktop endpoints
    
    * Add comment about quota

--- server/service/middleware/ratelimit/ratelimit_test.go
+++ server/service/middleware/ratelimit/ratelimit_test.go
@@ -33,3 +33,45 @@ func TestLimit(t *testing.T) {
 	var rle Error
 	assert.True(t, errors.As(err, &rle))
 }
+
+func TestNewErrorMiddlewarePanics(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+
+	NewErrorMiddleware(nil)
+}
+
+func TestLimitOnlyWhenError(t *testing.T) {
+	t.Parallel()
+
+	store, _ := memstore.New(1)
+	limiter := NewErrorMiddleware(store)
+	endpoint := func(context.Context, interface{}) (interface{}, error) { return struct{}{}, nil }
+	wrapped := limiter.Limit(
+		"test_limit", throttled.RateQuota{MaxRate: throttled.PerHour(1), MaxBurst: 0},
+	)(endpoint)
+
+	// Does NOT hit any rate limits because the endpoint doesn't fail
+	_, err := wrapped(context.Background(), struct{}{})
+	assert.NoError(t, err)
+	_, err = wrapped(context.Background(), struct{}{})
+	assert.NoError(t, err)
+
+	expectedError := errors.New("error")
+	failingEndpoint := func(context.Context, interface{}) (interface{}, error) { return nil, expectedError }
+	wrappedFailer := limiter.Limit(
+		"test_limit", throttled.RateQuota{MaxRate: throttled.PerHour(1), MaxBurst: 0},
+	)(failingEndpoint)
+
+	_, err = wrappedFailer(context.Background(), struct{}{})
+	assert.ErrorIs(t, err, expectedError)
+
+	// Hits rate limit now that it fails
+	_, err = wrappedFailer(context.Background(), struct{}{})
+	assert.Error(t, err)
+	var rle Error
+	assert.True(t, errors.As(err, &rle))
+}
