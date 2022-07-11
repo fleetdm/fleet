9b215eff91caefdf525baa8c6f067d2c56df639b
Juan Fernandez <juan@fleetdm.com>
2022-07-11 14:53:04 -0400

    Feature 6487: Remove any dups before applying unique constraint

--- server/datastore/mysql/migrations/tables/20220708095046_AddUniqconstraintSoftwareIDOnSoftwareCVE.go
+++ server/datastore/mysql/migrations/tables/20220708095046_AddUniqconstraintSoftwareIDOnSoftwareCVE.go
@@ -12,12 +12,51 @@ func init() {
 }
 
 func Up_20220708095046(tx *sql.Tx) error {
+	fmt.Println("Removing duplicates in the software_cve table")
+
+	const selectStmt = `
+SELECT software_id, cve, COUNT(1) FROM software_cve GROUP BY software_id, cve HAVING COUNT(1) > 1;
+`
+	rows, err := tx.Query(selectStmt)
+	if err != nil {
+		return errors.Wrap(err, "selecting duplicates")
+	}
+
+	type criteria struct {
+		softwareID uint
+		cve        string
+		count      uint
+	}
+
+	var criterias []criteria
+	for rows.Next() {
+		var softwareID uint
+		var cve string
+		var count uint
+
+		if err = rows.Scan(&softwareID, &cve, &count); err != nil {
+			return errors.Wrap(err, "scanning duplicate rows")
+		}
+
+		fmt.Printf("Found duplicated row software_id: %d, cve:%s\n", softwareID, cve)
+		criterias = append(criterias, criteria{softwareID: softwareID, cve: cve, count: count})
+	}
+
+	for _, c := range criterias {
+		if _, err := tx.Exec(
+			`DELETE FROM software_cve WHERE software_id = ? AND cve = ? LIMIT ?`,
+			c.softwareID, c.cve, c.count-1,
+		); err != nil {
+			return errors.Wrap(err, "removing duplicated row")
+		}
+	}
+
 	fmt.Println("Adding unique constraint on (cve, software_id) to software_cve table...")
-	_, err := tx.Exec(`
+	_, err = tx.Exec(`
 	ALTER TABLE software_cve ADD CONSTRAINT unq_software_id_cve UNIQUE (software_id, cve), ALGORITHM=INPLACE, LOCK=NONE;
 `)
 	if err != nil {
-		return errors.Wrapf(err, "adding uniq constraint to software_id on software_cve")
+		return errors.Wrapf(err, "adding unique constraint to software_id on software_cve")
 	}
 	fmt.Println("Done Adding unique constraint on (cve, software_id) to software_cve table...")
 
