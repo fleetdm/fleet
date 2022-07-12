7c15e117b0382e57c8c8534da0a338698b5778d8
Juan Fernandez <juan@fleetdm.com>
2022-07-12 10:50:36 -0400

    Feature 6487: Try to acquire lock

--- server/datastore/mysql/migrations/tables/20220708095046_AddUniqconstraintSoftwareIDOnSoftwareCVE.go
+++ server/datastore/mysql/migrations/tables/20220708095046_AddUniqconstraintSoftwareIDOnSoftwareCVE.go
@@ -3,7 +3,9 @@ package tables
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
+	"github.com/fleetdm/fleet/v4/server"
 	"github.com/pkg/errors"
 )
 
@@ -11,12 +13,14 @@ func init() {
 	MigrationClient.AddMigration(Up_20220708095046, Down_20220708095046)
 }
 
-func Up_20220708095046(tx *sql.Tx) error {
+func removeDups(tx *sql.Tx) error {
 	fmt.Println("Removing duplicates in the software_cve table")
 
 	const selectStmt = `
-SELECT software_id, cve, COUNT(1) FROM software_cve GROUP BY software_id, cve HAVING COUNT(1) > 1;
-`
+SELECT software_id, cve, COUNT(1) 
+FROM software_cve GROUP BY software_id, cve 
+HAVING COUNT(1) > 1;`
+
 	rows, err := tx.Query(selectStmt)
 	if err != nil {
 		return errors.Wrap(err, "selecting duplicates")
@@ -28,8 +32,8 @@ SELECT software_id, cve, COUNT(1) FROM software_cve GROUP BY software_id, cve HA
 		cve        string
 		count      uint
 	}
-
 	var criterias []criteria
+
 	for rows.Next() {
 		var softwareID uint
 		var cve string
@@ -57,14 +61,75 @@ SELECT software_id, cve, COUNT(1) FROM software_cve GROUP BY software_id, cve HA
 		}
 	}
 
+	return nil
+}
+
+func addUniqConstraint(tx *sql.Tx) error {
 	fmt.Println("Adding unique constraint on (cve, software_id) to software_cve table...")
-	_, err = tx.Exec(`
+	_, err := tx.Exec(`
 	ALTER TABLE software_cve ADD CONSTRAINT unq_software_id_cve UNIQUE (software_id, cve), ALGORITHM=INPLACE, LOCK=NONE;
 `)
 	if err != nil {
 		return errors.Wrapf(err, "adding unique constraint to software_id on software_cve")
 	}
 	fmt.Println("Done Adding unique constraint on (cve, software_id) to software_cve table...")
+
+	return nil
+}
+
+func acquireLock(tx *sql.Tx, identifier string) (bool, error) {
+	fmt.Println("Trying to acquire lock...")
+
+	r, err := tx.Exec(
+		`INSERT IGNORE INTO locks (name, owner, expires_at) VALUES (?, ?, ?)`,
+		"vulnerabilities", identifier, time.Now().Add(30*time.Minute),
+	)
+	if err != nil {
+		return false, errors.Wrapf(err, "trying to acquire lock")
+	}
+	rowsAffected, err := r.RowsAffected()
+	if err != nil {
+		return false, errors.Wrapf(err, "trying to acquire lock")
+	}
+	if rowsAffected > 0 {
+		fmt.Println("Lock acquired...")
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func releaseLock(tx *sql.Tx, identifier string) error {
+	if _, err := tx.Exec(`DELETE FROM locks WHERE name = ? and owner = ?`, "vulnerabilities", identifier); err != nil {
+		return errors.Wrapf(err, "trying to release lock")
+	}
+	return nil
+}
+
+func Up_20220708095046(tx *sql.Tx) error {
+	identifier, err := server.GenerateRandomText(64)
+	if err != nil {
+		fmt.Println("Could not generate identifier for lock, might not be able to remove duplicates in a reliable way...")
+	} else {
+		locked, err := acquireLock(tx, identifier)
+		if !locked || err != nil {
+			fmt.Println("Could not acquire lock, might not be able to remove duplicates in a reliable way...")
+		} else {
+			defer releaseLock(tx, identifier)
+		}
+	}
+
+	if err := removeDups(tx); err != nil {
+		return err
+	}
+
+	if err := addUniqConstraint(tx); err != nil {
+		return err
+	}
+
+	if err := releaseLock(tx, identifier); err != nil {
+		return err
+	}
 
 	return nil
 }
