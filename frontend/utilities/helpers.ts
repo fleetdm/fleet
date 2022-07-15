bcb1003b486ecf6633e9a6cb6a6421094b3d0961
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-07-15 11:01:31 -0400

    Convert to SSO provides new password of null

--- frontend/utilities/helpers.ts
+++ frontend/utilities/helpers.ts
@@ -689,9 +689,9 @@ export const secondsToHms = (d: number): string => {
   const m = Math.floor((d % 3600) / 60);
   const s = Math.floor((d % 3600) % 60);
 
-  const hDisplay = h > 0 ? h + (h === 1 ? " hr " : " hrs ") : "";
-  const mDisplay = m > 0 ? m + (m === 1 ? " min " : " mins ") : "";
-  const sDisplay = s > 0 ? s + (s === 1 ? " sec" : " secs") : "";
+  const hDisplay = h > 0 ? h + (h === 1 ? " hour " : " hours ") : "";
+  const mDisplay = m > 0 ? m + (m === 1 ? " minute " : " minutes ") : "";
+  const sDisplay = s > 0 ? s + (s === 1 ? " second" : " seconds") : "";
   return hDisplay + mDisplay + sDisplay;
 };
 
