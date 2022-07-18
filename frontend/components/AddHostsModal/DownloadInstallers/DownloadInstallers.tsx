8789e627926b03aab226a0447238c032b993f77e
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2022-07-18 10:12:44 -0500

    Replace icon; "Include Fleet Desktop"by default

--- frontend/components/AddHostsModal/DownloadInstallers/DownloadInstallers.tsx
+++ frontend/components/AddHostsModal/DownloadInstallers/DownloadInstallers.tsx
@@ -64,7 +64,7 @@ const DownloadInstallers = ({
   enrollSecret,
   onCancel,
 }: IDownloadInstallersProps): JSX.Element => {
-  const [includeDesktop, setIncludeDesktop] = useState(false); // TODO: confirm default false for includeDesktop
+  const [includeDesktop, setIncludeDesktop] = useState(true); // TODO: confirm default false for includeDesktop
   const [isDownloadError, setIsDownloadError] = useState(false);
   const [isDownloading, setIsDownloading] = useState(false);
   const [isDownloadSuccess, setIsDownloadSuccess] = useState(false);
