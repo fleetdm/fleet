acc178ea76ece637f7f6eab9f44ee51c44f59a00
Eric <eashaw@sailsjs.com>
2022-07-19 17:59:09 -0500

    Revert "Revert "Provision Fleet sandbox for users logging in""
    This reverts commit c2a2567b68325ea92e19f908226de2f52d8265f9.

--- website/api/controllers/entrance/login.js
+++ website/api/controllers/entrance/login.js
@@ -68,7 +68,11 @@ and exposed as \`req.me\`.)`
     noUser: {
       description: `The provided email does not match any user in the database.`,
       responseType: 'unauthorized'
-    }
+    },
+
+    couldNotProvisionSandbox: {
+      description: 'An error occurred while trying to provision the Fleet Sandbox Instance'
+    },
 
   },
 
@@ -108,6 +112,42 @@ and exposed as \`req.me\`.)`
       }
     }//ﬁ
 
+    // If this user does not have a Fleet Sandbox instance, we'll provision them one.
+    if(!userRecord.fleetSandboxURL) {
+      let fleetSandboxExpiresAt = Date.now() + (24*60*60*1000);
+
+      let cloudProvisionerResponse = await sails.helpers.http.post(
+        'https://sandbox.fleetdm.com/new',
+        {
+          'name': userRecord.firstName + ' ' + userRecord.lastName,
+          'email': userRecord.emailAddress,
+          'password': userRecord.password,
+          'sandbox_expiration': new Date(fleetSandboxExpiresAt).toISOString(), // sending expiration_timestamp as an ISO string.
+        },
+        {
+          'Authorization':sails.config.custom.cloudProvisionerSecret
+        }
+      )
+      .timeout(5000)
+      .intercept('non200Response', 'couldNotProvisionSandbox');
+
+      if(!cloudProvisionerResponse.URL) {
+        throw 'couldNotProvisionSandbox';
+      } else {
+        // Update this user's record with the fleetSandboxURL and fleetSandboxExpiresAt
+        await User.updateOne({id: userRecord.id}).set({
+          fleetSandboxURL: cloudProvisionerResponse.URL,
+          fleetSandboxExpiresAt: fleetSandboxExpiresAt,
+        });
+        // Poll the Fleet Sandbox Instance's /healthz endpoint until it returns a 200 response, if no response was recieved in 10 seconds, we'll throw an error.
+        await sails.helpers.flow.until( async()=>{
+          let serverResponse = await sails.helpers.http.sendHttpRequest('GET', cloudProvisionerResponse.URL+'/healthz').timeout(5000).tolerate('non200Response').tolerate('requestFailed');
+          if(serverResponse && serverResponse.statusCode) {
+            return serverResponse.statusCode === 200;
+          }
+        }, 10000);
+      }
+    }
     // Modify the active session instance.
     // (This will be persisted when the response is sent.)
     this.req.session.userId = userRecord.id;
