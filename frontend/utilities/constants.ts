47cf01c36b1a65d707e5bb9d3a6142ec106f99e5
Desmi-Dizney <99777687+Desmi-Dizney@users.noreply.github.com>
2022-07-20 12:44:52 -0500

    Update constants.ts

--- frontend/utilities/constants.ts
+++ frontend/utilities/constants.ts
@@ -178,7 +178,7 @@ export const DEFAULT_POLICIES = [
     description:
       "Checks that the password policy requires at least 10 characters. Requires osquery 5.4.0 or newer.",
     resolution:
-      "Contact your IT administrator to ensure your Mac is receiving configuration profiles for password length.",
+      "Contact your IT administrator to make sure your Mac is receiving configuration profiles for password length.",
     platform: "darwin",
   },
 ] as IPolicyNew[];
