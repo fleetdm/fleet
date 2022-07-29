93e561413a43b53aa4e5955efcb15ed06043dfdd
Scott Blake <945258+MScottBlake@users.noreply.github.com>
2022-07-29 15:21:06 -0400

    Update cleanup_macos.sh
    Remove the installer package receipt.

--- orbit/tools/cleanup/cleanup_macos.sh
+++ orbit/tools/cleanup/cleanup_macos.sh
@@ -5,3 +5,5 @@ sudo launchctl unload /Library/LaunchDaemons/com.fleetdm.orbit.plist
 
 sudo pkill fleet-desktop || true
 sudo rm -rf /Library/LaunchDaemons/com.fleetdm.orbit.plist /var/lib/orbit /usr/local/bin/orbit /var/log/orbit /opt/orbit/
+
+sudo pkgutil --forget com.fleetdm.orbit.base.pkg
