a1e921347fc84eec2ed61356376a19c2a8ee2c57
Martin Angers <martin.n.angers@gmail.com>
2022-08-02 14:05:46 -0400

    Test the migration

--- server/datastore/mysql/migrations/tables/20220802135510_AddMobileDeviceManagementTable_test.go
+++ server/datastore/mysql/migrations/tables/20220802135510_AddMobileDeviceManagementTable_test.go
@@ -1,20 +1,33 @@
 package tables
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestUp_20220802135510(t *testing.T) {
 	db := applyUpToPrev(t)
 
-	//
-	// Insert data to test the migration
-	//
-	// ...
-
-	// Apply current migration.
 	applyNext(t, db)
 
-	//
-	// Check data, insert new entries, e.g. to verify migration is safe.
-	//
-	// ...
+	query := `
+INSERT INTO mobile_device_management_solutions (
+	name, server_url
+)
+VALUES (?, ?)
+`
+	res, err := db.Exec(query, "test", "http://localhost:8080")
+	require.NoError(t, err)
+	id, _ := res.LastInsertId()
+
+	var (
+		name string
+		url  string
+	)
+	err = db.QueryRow(`SELECT name, server_url FROM mobile_device_management_solutions WHERE id = ?`, id).
+		Scan(&name, &url)
+	require.NoError(t, err)
+	require.Equal(t, "test", name)
+	require.Equal(t, "http://localhost:8080", url)
 }
