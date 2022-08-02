3107e623d73c8eb0b229d149b6216d6f97e2bd04
Scott Blake <945258+MScottBlake@users.noreply.github.com>
2022-08-02 11:44:35 -0400

    Update cleanup_macos.sh
    Continue on error

--- orbit/tools/cleanup/cleanup_macos.sh
+++ orbit/tools/cleanup/cleanup_macos.sh
@@ -6,4 +6,4 @@ sudo launchctl unload /Library/LaunchDaemons/com.fleetdm.orbit.plist
 sudo pkill fleet-desktop || true
 sudo rm -rf /Library/LaunchDaemons/com.fleetdm.orbit.plist /var/lib/orbit /usr/local/bin/orbit /var/log/orbit /opt/orbit/
 
-sudo pkgutil --forget com.fleetdm.orbit.base.pkg
+sudo pkgutil --forget com.fleetdm.orbit.base.pkg || true
