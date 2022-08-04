dccf9f507866ecab25353bbf769bbea220b5883d
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-08-04 15:31:44 -0400

    Fix merge conflicts

--- cypress/integration/premium/admin.spec.ts
+++ cypress/integration/premium/admin.spec.ts
@@ -539,7 +539,7 @@ describe("Premium tier - Global Admin user", () => {
         });
     });
     cy.findByRole("button", { name: /delete/i }).click();
-    cy.getAttached(".remove-policies-modal").within(() => {
+    cy.getAttached(".delete-policies-modal").within(() => {
       cy.findByRole("button", { name: /delete/i }).should("exist");
       cy.findByRole("button", { name: /cancel/i }).click();
     });
@@ -575,7 +575,7 @@ describe("Premium tier - Global Admin user", () => {
         console.log(JSON.stringify(configStub));
       });
     });
-    it("allows global admin to delete and automate a team policy jira integration", () => {
+    it("allows global admin to delete team policy", () => {
       cy.visit("/policies/manage");
       cy.getAttached(".Select-control").within(() => {
         cy.findByText(/all teams/i).click();
@@ -596,69 +596,75 @@ describe("Premium tier - Global Admin user", () => {
       cy.getAttached(".delete-policies-modal").within(() => {
         cy.findByRole("button", { name: /delete/i }).should("exist");
         cy.findByRole("button", { name: /cancel/i }).click();
-        cy.getAttached(".button-wrap")
-          .findByRole("button", { name: /manage automations/i })
-          .click();
-        cy.getAttached(".manage-automations-modal").within(() => {
-          cy.getAttached(".fleet-slider").click();
-          cy.getAttached(".fleet-slider").click();
-          cy.getAttached(".fleet-checkbox__input").check({ force: true });
-          cy.getAttached("#ticket-radio-btn").next().click();
-          cy.findByText(/select integration/i).click();
-          cy.findByText(/project 2/i).click();
-          cy.intercept(
-            "PATCH",
-            "/api/latest/fleet/teams/1",
-            patchTeamJiraPoliciesIntegration
-          ).as("enableJiraPoliciesIntegration");
-          cy.intercept(
-            "GET",
-            "/api/latest/fleet/teams/1",
-            getTeamJiraPoliciesIntegration
-          ).as("enabledJiraPoliciesIntegration");
-          cy.findByText(/save/i).click();
-        });
-        cy.findByText(/successfully updated policy automations/i).should(
-          "exist"
-        );
       });
-      it("allows global admin to automate a team policy zendesk integration", () => {
-        cy.visit("/policies/manage");
-        cy.getAttached(".Select-control").within(() => {
-          cy.findByText(/all teams/i).click();
-        });
-        cy.getAttached(".Select-menu")
-          .contains(/apples/i)
-          .click();
-        cy.getAttached(".button-wrap")
-          .findByRole("button", { name: /manage automations/i })
-          .click();
-        cy.getAttached(".manage-automations-modal").within(() => {
-          cy.getAttached(".fleet-slider").click();
-          cy.getAttached(".fleet-slider").click();
-          cy.getAttached(".fleet-checkbox__input").check({ force: true });
-          cy.getAttached("#ticket-radio-btn").next().click();
-          cy.findByText(/select integration/i).click();
-          cy.findByText(/87654321/i).click();
-          cy.intercept(
-            "PATCH",
-            "/api/latest/fleet/teams/1",
-            patchTeamZendeskPoliciesIntegration
-          ).as("enableZendeskPoliciesIntegration");
-          cy.intercept(
-            "GET",
-            "/api/latest/fleet/teams/1",
-            getTeamZendeskPoliciesIntegration
-          ).as("enabledZendeskPoliciesIntegration");
-          cy.findByText(/save/i).click();
-          cy.wait("@enableZendeskPoliciesIntegration").then((configStub) => {
-            console.log(JSON.stringify(configStub));
-          });
-        });
-        cy.findByText(/successfully updated policy automations/i).should(
-          "exist"
-        );
+    });
+    it("allows global admin to automate a team policy jira integration", () => {
+      cy.visit("/policies/manage");
+      cy.getAttached(".Select-control").within(() => {
+        cy.findByText(/all teams/i).click();
       });
+      cy.getAttached(".Select-menu")
+        .contains(/apples/i)
+        .click();
+      cy.getAttached(".button-wrap")
+        .findByRole("button", { name: /manage automations/i })
+        .click();
+      cy.getAttached(".manage-automations-modal").within(() => {
+        cy.getAttached(".fleet-slider").click();
+        cy.getAttached(".fleet-slider").click();
+        cy.getAttached(".fleet-checkbox__input").check({ force: true });
+        cy.getAttached("#ticket-radio-btn").next().click();
+        cy.findByText(/select integration/i).click();
+        cy.findByText(/project 2/i).click();
+        cy.intercept(
+          "PATCH",
+          "/api/latest/fleet/teams/1",
+          patchTeamJiraPoliciesIntegration
+        ).as("enableJiraPoliciesIntegration");
+        cy.intercept(
+          "GET",
+          "/api/latest/fleet/teams/1",
+          getTeamJiraPoliciesIntegration
+        ).as("enabledJiraPoliciesIntegration");
+        cy.findByText(/save/i).click();
+      });
+      cy.findByText(/successfully updated policy automations/i).should("exist");
+    });
+
+    it("allows global admin to automate a team policy zendesk integration", () => {
+      cy.visit("/policies/manage");
+      cy.getAttached(".Select-control").within(() => {
+        cy.findByText(/all teams/i).click();
+      });
+      cy.getAttached(".Select-menu")
+        .contains(/apples/i)
+        .click();
+      cy.getAttached(".button-wrap")
+        .findByRole("button", { name: /manage automations/i })
+        .click();
+      cy.getAttached(".manage-automations-modal").within(() => {
+        cy.getAttached(".fleet-slider").click();
+        cy.getAttached(".fleet-slider").click();
+        cy.getAttached(".fleet-checkbox__input").check({ force: true });
+        cy.getAttached("#ticket-radio-btn").next().click();
+        cy.findByText(/select integration/i).click();
+        cy.findByText(/87654321/i).click();
+        cy.intercept(
+          "PATCH",
+          "/api/latest/fleet/teams/1",
+          patchTeamZendeskPoliciesIntegration
+        ).as("enableZendeskPoliciesIntegration");
+        cy.intercept(
+          "GET",
+          "/api/latest/fleet/teams/1",
+          getTeamZendeskPoliciesIntegration
+        ).as("enabledZendeskPoliciesIntegration");
+        cy.findByText(/save/i).click();
+        cy.wait("@enableZendeskPoliciesIntegration").then((configStub) => {
+          console.log(JSON.stringify(configStub));
+        });
+      });
+      cy.findByText(/successfully updated policy automations/i).should("exist");
     });
   });
   describe("Admin settings page", () => {
