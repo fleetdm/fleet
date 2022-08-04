03a9776966f38bae79a4310ba25c85643f26ff5c
Juan Fernandez <juan@fleetdm.com>
2022-08-04 13:48:36 -0400

    Refactor delete statement

--- server/datastore/mysql/migrations/tables/20220711104651_RemoveCpeIDColumnFromSoftwareCve.go
+++ server/datastore/mysql/migrations/tables/20220711104651_RemoveCpeIDColumnFromSoftwareCve.go
@@ -12,52 +12,28 @@ func init() {
 
 func Up_20220711104651(tx *sql.Tx) error {
 	logger.Info.Println("Deleting dummy software_cpe entries...")
-
-	var min int
-	var max int
-
-	const selectStmt = `
-SELECT COALESCE(MIN(scpe.id), 0) AS min_id, COALESCE(MAX(scpe.id), 0) as max_id 
-FROM software_cpe AS scpe
-WHERE scpe.cpe LIKE 'none:%';`
-	if err := tx.QueryRow(selectStmt).Scan(&min, &max); err != nil {
-		return errors.Wrap(err, "selecting min,max id")
-	}
-
 	// Remove in batches
-	const batchSize = 500
-	const deleteStmt = `
-DELETE FROM software_cpe
-WHERE cpe LIKE 'none:%' AND id >= ? AND id < ?;`
+	const deleteStmt = `DELETE FROM software_cpe WHERE cpe LIKE 'none:%' LIMIT 500`
 
-	if min == 0 && max == 0 {
-		logger.Info.Println("Nothing to delete ...")
-	} else {
-		logger.Info.Printf("Deleting aprox %d records... \n", max-min)
-	}
-
-	start := min
 	for {
-		end := start + batchSize
-		if end >= max {
-			end = max + 1
-		}
 
-		_, err := tx.Exec(deleteStmt, start, end)
+		res, err := tx.Exec(deleteStmt)
 		if err != nil {
 			return errors.Wrapf(err, "deleting dummy software_cpe entries")
 		}
 
-		start += batchSize
-		if start >= max {
+		affected, err := res.RowsAffected()
+		if err != nil {
+			return errors.Wrapf(err, "deleting dummy software_cpe entries")
+		}
+
+		if affected == 0 {
 			break
 		}
 	}
-
 	logger.Info.Println("Done deleting dummy cpe_id entries...")
 
 	logger.Info.Println("Removing cpe_id from software_cve...")
-
 	const removeFkStmt = `
 ALTER TABLE software_cve DROP FOREIGN KEY software_cve_ibfk_1, ALGORITHM=INPLACE, LOCK=NONE; 
 `
