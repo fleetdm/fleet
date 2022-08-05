f746094290c5fa58e33371a47309e92a02f3e8db
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-08-05 11:17:22 -0400

    Client side pagination

--- frontend/pages/Homepage/cards/MDM/MDM.tsx
+++ frontend/pages/Homepage/cards/MDM/MDM.tsx
@@ -156,6 +156,7 @@ const MDM = ({
                   emptyComponent={EmptyMDMSolutions}
                   showMarkAllPages={false}
                   isAllPagesSelected={false}
+                  isClientSidePagination
                   disableCount
                   disableActionButton
                   pageSize={PAGE_SIZE}
