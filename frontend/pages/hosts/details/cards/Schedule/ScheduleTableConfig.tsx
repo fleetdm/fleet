73d0299af974ec15581e71e650264030bf9bfa30
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2022-08-09 10:00:09 -0400

    Fleet UI: Schedule frequency units, Convert seconds to units using date-fns (#7102)

--- frontend/pages/hosts/details/cards/Schedule/ScheduleTableConfig.tsx
+++ frontend/pages/hosts/details/cards/Schedule/ScheduleTableConfig.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { uniqueId } from "lodash";
 
 import { IQueryStats } from "interfaces/query_stats";
-import { performanceIndicator, secondsToHms } from "utilities/helpers";
+import { performanceIndicator, secondsToDhms } from "utilities/helpers";
 
 import TextCell from "components/TableContainer/DataTable/TextCell";
 import PillCell from "components/TableContainer/DataTable/PillCell";
@@ -102,7 +102,7 @@ const enhanceScheduleData = (query_stats: IQueryStats[]): IScheduleTable[] => {
     };
     return {
       query_name: query.query_name,
-      frequency: secondsToHms(query.interval),
+      frequency: secondsToDhms(query.interval),
       performance: [
         performanceIndicator(scheduledQueryPerformance),
         query.scheduled_query_id || uniqueId(),
