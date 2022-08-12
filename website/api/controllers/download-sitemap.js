7fe9a27590a2e3ded9f8e2da3de425ac72b89912
Eric <eashaw@sailsjs.com>
2022-08-11 21:54:01 -0500

    Website: update sitemap (#7107)
    * Website: update hand coded html pages in sitmap
    
    * add /podcasts

--- website/api/controllers/download-sitemap.js
+++ website/api/controllers/download-sitemap.js
@@ -50,7 +50,20 @@ module.exports = {
       '/platform',
       '/pricing',
       '/transparency',
-      '/docs'
+      '/docs',
+      '/logos',
+      '/platform',
+      '/reports/state-of-device-management',
+      '/overview',
+      '/releases',
+      '/device-management',
+      '/securing',
+      '/engineering',
+      '/guides',
+      '/announcements',
+      '/report',
+      '/deploy',
+      '/podcasts',
       // FUTURE: Do something smarter to get hand-coded HTML pages from routes.js, like how rebuild-cloud-sdk works, to avoid this manual duplication.
       // See also https://github.com/sailshq/sailsjs.com/blob/b53c6e6a90c9afdf89e5cae00b9c9dd3f391b0e7/api/helpers/get-pages-for-sitemap.js#L27
     ];
