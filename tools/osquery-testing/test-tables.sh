3407a909d91341c723db6a1e7c7bf682a3e51e54
Zach Wasserman <zach@fleetdm.com>
2022-08-17 11:10:36 -0700

    Update for separate table names

--- tools/osquery-testing/test-tables.sh
+++ tools/osquery-testing/test-tables.sh
@@ -4,14 +4,18 @@
 [ -z $1 ] && >&2 echo "Error: Input file must be provided" && exit 1
 
 # Read lines from input file.
-cat "$1" | while read -r query
+cat "$1" | while read -r line
 do
     # Ignore comments (lines starting with #) and empty lines in the input file.
-    if [ "${query:0:1}" = "#" ] || [ -z "$query" ]; then
+    if [ "${line:0:1}" = "#" ] || [ -z "$line" ]; then
         continue
     fi
 
+    IFS=': ' read -r table_name query <<< "$line"
+
     # Print the query to run.
+    echo "$table_name"
+    echo
     echo sudo osqueryi --line \""$query limit 3"\"
     echo
 
