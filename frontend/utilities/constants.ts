7c156241e34f914bdfd4c6526de161bc21c52937
Guillaume Ross <guillaume@fleetdm.com>
2022-08-22 14:55:46 -0400

    Updating constants from the changes in yaml
    Taking Noah's suggestions from the yaml and putting them in the `.ts`

--- frontend/utilities/constants.ts
+++ frontend/utilities/constants.ts
@@ -188,7 +188,7 @@ export const DEFAULT_POLICIES = [
     name: "Operating system up to date (macOS) [CIS 1.1]",
     description: "Checks that the operating system is up to date.",
     resolution:
-      "Go to the Software Update section of System Preferences and install the latest updates.",
+      "From the Apple menu () in the corner of your screen choose System Preferences. Then select Software Update and select Upgrade Now. You might be asked to restart or enter your password.",
     platform: "darwin",
   },
   {
@@ -197,7 +197,7 @@ export const DEFAULT_POLICIES = [
       "SELECT 1 FROM managed_policies WHERE domain='com.apple.SoftwareUpdate' AND name='AutomaticCheckEnabled' AND value=1 LIMIT 1;",
     name: "Automatic updates enabled (macOS) [CIS 1.2]",
     description:
-      "Checks that the operating system is configured to automatically checks for updates.",
+      "Checks that a mobile device management (MDM) solution configures the operating system to automatically check for updates.",
     resolution:
       "Contact your IT administrator to ensure your Mac is receiving a profile that enables automatic updates.",
     platform: "darwin",
@@ -208,7 +208,7 @@ export const DEFAULT_POLICIES = [
       "SELECT 1 FROM managed_policies WHERE domain='com.apple.SoftwareUpdate' AND name='AutomaticDownload' AND value=1 LIMIT 1;",
     name: "Automatic update downloads enabled (macOS) [CIS 1.3]",
     description:
-      "Checks that the operating system is configured to automatically download updates.",
+      "Checks that a mobile device management (MDM) solution configures the operating system to automatically download updates.",
     resolution:
       "Contact your IT administrator to ensure your Mac is receiving a profile that enables automatic update downloads.",
     platform: "darwin",
@@ -219,9 +219,9 @@ export const DEFAULT_POLICIES = [
       "SELECT 1 FROM managed_policies WHERE domain='com.apple.SoftwareUpdate' AND name='AutomaticallyInstallAppUpdates' AND value=1 LIMIT 1;",
     name: "Installation of application updates is enabled (macOS) [CIS 1.4]",
     description:
-      "Checks that the operating system is configured to automatically download updates to Apple applications.",
+      "Checks that a mobile device management (MDM) solution configures the operating system to automatically install updates to Apple applications.",
     resolution:
-      "Contact your IT administrator to ensure your Mac is receiving a profile that enables automatic application update downloads.",
+      "Contact your IT administrator to ensure your Mac is receiving a profile that enables installation of application updates.",
     platform: "darwin",
   },
 ] as IPolicyNew[];
