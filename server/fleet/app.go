d959ca4aad67cee1d1d4f0a34711a15a157a6455
Roberto Dip <me@roperzh.com>
2022-08-24 13:02:56 -0300

    Update server/fleet/app.go
    Co-authored-by: Martin Angers <martin.n.angers@gmail.com>

--- server/fleet/app.go
+++ server/fleet/app.go
@@ -161,7 +161,7 @@ type enrichedAppConfigFields struct {
 //
 // - If this function is not defined, AppConfig.UnmarshalJSON gets promoted and
 // will be called instead.
-// - If we try to unmarshal everyting in one go, AppConfig.UnmarshalJSON doesn't get
+// - If we try to unmarshal everything in one go, AppConfig.UnmarshalJSON doesn't get
 // called.
 func (e *EnrichedAppConfig) UnmarshalJSON(data []byte) error {
 	if err := json.Unmarshal(data, &e.AppConfig); err != nil {
