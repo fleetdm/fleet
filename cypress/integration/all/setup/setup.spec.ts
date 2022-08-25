986b49208b56b970532f3596f0ddc3f552af21eb
Gabriel Hernandez <ghernandez345@gmail.com>
2022-08-25 16:24:45 +0100

    add test for error

--- cypress/integration/all/setup/setup.spec.ts
+++ cypress/integration/all/setup/setup.spec.ts
@@ -2,12 +2,40 @@ import CONSTANTS from "../../../support/constants";
 
 const { GOOD_PASSWORD } = CONSTANTS;
 
+const fillOutForm = () => {
+  // Page 1
+  cy.findByPlaceholderText(/full name/i).type("Test name");
+
+  cy.findByPlaceholderText(/email/i).type("test@example.com");
+
+  cy.findByPlaceholderText(/^password/i)
+    .first()
+    .type(GOOD_PASSWORD);
+
+  cy.findByPlaceholderText(/confirm password/i)
+    .last()
+    .type(GOOD_PASSWORD);
+
+  cy.contains("button:enabled", /next/i).click();
+
+  // Page 2
+  cy.findByPlaceholderText(/organization name/i).type("Fleet Test");
+
+  cy.contains("button:enabled", /next/i).click();
+
+  // Page 3
+  cy.contains("button:enabled", /next/i).click();
+
+  // Page 4
+  cy.contains("button:enabled", /confirm/i).click();
+};
+
 describe("Setup", () => {
   // Different than normal beforeEach because we don't run the fleetctl setup.
   beforeEach(() => {
     const SHELL = Cypress.platform === "win32" ? "cmd" : "bash";
     cy.exec("make e2e-reset-db", {
-      timeout: 5000,
+      timeout: 20000,
       env: { SHELL },
     });
   });
@@ -17,33 +45,20 @@ describe("Setup", () => {
     cy.url().should("match", /\/setup$/);
     cy.contains(/setup/i);
 
-    // Page 1
-    cy.findByPlaceholderText(/full name/i).type("Test name");
-
-    cy.findByPlaceholderText(/email/i).type("test@example.com");
-
-    cy.findByPlaceholderText(/^password/i)
-      .first()
-      .type(GOOD_PASSWORD);
-
-    cy.findByPlaceholderText(/confirm password/i)
-      .last()
-      .type(GOOD_PASSWORD);
-
-    cy.contains("button:enabled", /next/i).click();
-
-    // Page 2
-    cy.findByPlaceholderText(/organization name/i).type("Fleet Test");
-
-    cy.contains("button:enabled", /next/i).click();
-
-    // Page 3
-    cy.contains("button:enabled", /next/i).click();
-
-    // Page 4
-    cy.contains("button:enabled", /confirm/i).click();
+    fillOutForm();
 
     cy.url().should("match", /\/hosts\/manage$/i);
     cy.contains(/all hosts/i);
+  });
+
+  it("shows messaging when there is an error during setup", () => {
+    cy.visit("/");
+    cy.intercept("POST", "/api/v1/setup", { forceNetworkError: true });
+
+    fillOutForm();
+
+    cy.findByText(
+      "We were unable to configure your Fleet instance. Please notify your system administrator."
+    ).should("exist");
   });
 });
--- frontend/pages/RegistrationPage/RegistrationPage.tsx
+++ frontend/pages/RegistrationPage/RegistrationPage.tsx
@@ -20,7 +20,7 @@ const ERROR_NOTIFICATION: INotification = {
   alertType: "error",
   isVisible: true,
   message:
-    "We were unable to configure your Fleet instance. Please notify your system administrator.",
+    "We were unable to configure your Fleet instance. Please ensure the Fleet server can be reached.",
 };
 
 interface IRegistrationPageProps {
