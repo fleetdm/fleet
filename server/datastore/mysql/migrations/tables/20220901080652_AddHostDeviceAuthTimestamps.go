f7b66a5680aad118d2a8e3bd9210ad66703a43b8
Roberto Dip <dip.jesusr@gmail.com>
2022-09-05 14:58:24 -0300

    use logger

--- server/datastore/mysql/migrations/tables/20220901080652_AddHostDeviceAuthTimestamps.go
+++ server/datastore/mysql/migrations/tables/20220901080652_AddHostDeviceAuthTimestamps.go
@@ -2,7 +2,6 @@ package tables
 
 import (
 	"database/sql"
-	"fmt"
 )
 
 func init() {
@@ -10,14 +9,14 @@ func init() {
 }
 
 func Up_20220901080652(tx *sql.Tx) error {
-	fmt.Println("Adding timestamps to 'host_device_auth'...")
+	logger.Info.Println("Adding timestamps to 'host_device_auth'...")
 	_, err := tx.Exec(`
 		ALTER TABLE host_device_auth
 			ADD COLUMN created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 			ADD COLUMN updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 `)
 	if err == nil {
-		fmt.Println("Done adding timestamps to 'host_device_auth'...")
+		logger.Info.Println("Done adding timestamps to 'host_device_auth'...")
 	}
 	return err
 }
