3168c6de5aeac8dc4cf9de073450cc9f453cc25d
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-09-07 10:05:10 -0400

    Update preview payload modal

--- frontend/pages/software/ManageSoftwarePage/components/PreviewPayloadModal/PreviewPayloadModal.tsx
+++ frontend/pages/software/ManageSoftwarePage/components/PreviewPayloadModal/PreviewPayloadModal.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { syntaxHighlight } from "utilities/helpers";
 
+import { AppContext } from "context/app";
 import Modal from "components/Modal";
 import Button from "components/buttons/Button";
 import ExternalLinkIcon from "../../../../../../assets/images/icon-external-link-12x12@2x.png";
@@ -11,17 +12,36 @@ interface IPreviewPayloadModalProps {
   onCancel: () => void;
 }
 
+interface IHostsAffectedProps {
+  id: number;
+  hostname: string;
+  url: string;
+}
+interface IJsonPayload {
+  timestamp: string;
+  vulnerability: {
+    cve: string;
+    details_link: string;
+    epss_probability?: number;
+    cvss_score?: number;
+    cisa_known_exploit?: boolean;
+    hosts_affected: IHostsAffectedProps[];
+  };
+}
+
 const PreviewPayloadModal = ({
   onCancel,
 }: IPreviewPayloadModalProps): JSX.Element => {
-  const json = {
+  const { isFreeTier } = useContext(AppContext);
+
+  let json: IJsonPayload = {
     timestamp: "0000-00-00T00:00:00Z",
     vulnerability: {
       cve: "CVE-2014-9471",
       details_link: "https://nvd.nist.gov/vuln/detail/CVE-2014-9471",
-      epss_probability: 0.7, // Premium feature only
-      cvss_score: 5.7, // Premium feature only
-      cisa_known_exploit: true, // Premium feature only
+      epss_probability: 0.7,
+      cvss_score: 5.7,
+      cisa_known_exploit: true,
       hosts_affected: [
         {
           id: 1,
@@ -36,6 +56,13 @@ const PreviewPayloadModal = ({
       ],
     },
   };
+
+  if (isFreeTier) {
+    // Premium only features
+    delete json.vulnerability.epss_probability;
+    delete json.vulnerability.cvss_score;
+    delete json.vulnerability.cisa_known_exploit;
+  }
 
   return (
     <Modal title={"Example payload"} onExit={onCancel} className={baseClass}>
