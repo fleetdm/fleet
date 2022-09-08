e67797eed05d09ee47fe744088f2e1adb3213397
Paulo Lieuthier <paulo.lieuthier@incognia.com>
2022-09-08 18:24:22 -0300

    Fix Content-Type used for login request

--- frontend/services/entities/sessions.ts
+++ frontend/services/entities/sessions.ts
@@ -11,7 +11,7 @@ export default {
   create: ({ email, password }: ICreateSessionProps) => {
     const { LOGIN } = endpoints;
 
-    return sendRequest("POST", LOGIN, JSON.stringify({ email, password })).then(
+    return sendRequest("POST", LOGIN, { email, password }).then(
       (response) => {
         const { user, available_teams } = response;
         const userWithGravatarUrl = helpers.addGravatarUrlToResource(user);
