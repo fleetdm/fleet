aa3195813d7a8297f248d3ac0f6e7a87af5b7378
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-09-09 10:27:44 -0400

    More progress towards refactoring software card

--- frontend/pages/Homepage/cards/Software/Software.tsx
+++ frontend/pages/Homepage/cards/Software/Software.tsx
@@ -1,138 +1,50 @@
-import React, { useContext, useState } from "react";
-import { useQuery } from "react-query";
+import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
-import { AppContext } from "context/app";
-import paths from "router/paths";
-import configAPI from "services/entities/config";
-import softwareAPI, { ISoftwareResponse } from "services/entities/software";
-
 import TabsWrapper from "components/TabsWrapper";
-import TableContainer, { ITableQueryData } from "components/TableContainer";
+import TableContainer from "components/TableContainer";
 import TableDataError from "components/DataError";
 import Spinner from "components/Spinner";
-import LastUpdatedText from "components/LastUpdatedText/LastUpdatedText";
 import generateTableHeaders from "./SoftwareTableConfig";
 import EmptySoftware from "../../../software/components/EmptySoftware";
 
 interface ISoftwareCardProps {
-  currentTeamId?: number;
+  errorSoftware: Error | null;
   showSoftwareUI: boolean;
-  setShowSoftwareUI: (showSoftwareTitle: boolean) => void;
+  isCollectingInventory: boolean;
+  isSoftwareFetching: boolean;
+  isSoftwareEnabled: boolean;
+  software: any;
+  pageIndex: number;
   setActionURL?: (url: string) => void;
-  setTitleDetail?: (content: JSX.Element | string | null) => void;
+  navTabIndex: any;
+  onTabChange: any;
+  onQueryChange: any;
 }
 
-const DEFAULT_SORT_DIRECTION = "desc";
-const DEFAULT_SORT_HEADER = "hosts_count";
-const DEFAULT_PAGE_SIZE = 8;
+const SOFTWARE_DEFAULT_SORT_DIRECTION = "desc";
+const SOFTWARE_DEFAULT_SORT_HEADER = "hosts_count";
+const SOFTWARE_DEFAULT_PAGE_SIZE = 8;
+
 const baseClass = "home-software";
 
 const Software = ({
-  currentTeamId,
+  errorSoftware,
   showSoftwareUI,
-  setShowSoftwareUI,
+  isCollectingInventory,
+  isSoftwareFetching,
+  isSoftwareEnabled,
+  pageIndex,
+  navTabIndex,
+  onTabChange,
+  onQueryChange,
   setActionURL,
-  setTitleDetail,
+  software,
 }: ISoftwareCardProps): JSX.Element => {
-  const [navTabIndex, setNavTabIndex] = useState(0);
-  const [pageIndex, setPageIndex] = useState(0);
-  const [isSoftwareEnabled, setIsSoftwareEnabled] = useState(false);
-
-  const { availableTeams, currentTeam, isOnGlobalTeam } = useContext(
-    AppContext
-  );
-
-  const { data: config } = useQuery(["config"], configAPI.loadAll, {
-    onSuccess: (data) => {
-      setIsSoftwareEnabled(data?.features?.enable_software_inventory);
-    },
-  });
-
-  const {
-    data: software,
-    isFetching: isSoftwareFetching,
-    error: errorSoftware,
-  } = useQuery<ISoftwareResponse, Error>(
-    [
-      "software",
-      {
-        pageIndex,
-        pageSize: DEFAULT_PAGE_SIZE,
-        sortDirection: DEFAULT_SORT_DIRECTION,
-        sortHeader: DEFAULT_SORT_HEADER,
-        teamId: currentTeamId,
-        vulnerable: !!navTabIndex, // we can take the tab index as a boolean to represent the vulnerable flag :)
-      },
-    ],
-    () =>
-      softwareAPI.load({
-        page: pageIndex,
-        perPage: DEFAULT_PAGE_SIZE,
-        orderKey: DEFAULT_SORT_HEADER,
-        orderDir: DEFAULT_SORT_DIRECTION,
-        vulnerable: !!navTabIndex, // we can take the tab index as a boolean to represent the vulnerable flag :)
-        teamId: currentTeamId,
-      }),
-    {
-      enabled:
-        isOnGlobalTeam ||
-        !!availableTeams?.find((t) => t.id === currentTeam?.id),
-      keepPreviousData: true,
-      staleTime: 30000, // stale time can be adjusted if fresher data is desired based on software inventory interval
-      onSuccess: (data) => {
-        setShowSoftwareUI(true);
-        if (isSoftwareEnabled && data.software?.length !== 0) {
-          setTitleDetail &&
-            setTitleDetail(
-              <LastUpdatedText
-                lastUpdatedAt={data.counts_updated_at}
-                whatToRetrieve={"software"}
-              />
-            );
-        }
-      },
-      onError: () => {
-        setShowSoftwareUI(true);
-      },
-    }
-  );
-
-  // TODO: Rework after backend is adjusted to differentiate empty search/filter results from
-  // collecting inventory
-  const isCollectingInventory =
-    !currentTeamId &&
-    !pageIndex &&
-    !software?.software &&
-    software?.counts_updated_at === null;
-
-  if (isCollectingInventory) {
-    setTitleDetail && setTitleDetail("");
-  }
-
-  // NOTE: this is called once on the initial rendering. The initial render of
-  // the TableContainer child component will call this handler.
-  const onQueryChange = async ({
-    pageIndex: newPageIndex,
-  }: ITableQueryData) => {
-    if (pageIndex !== newPageIndex) {
-      setPageIndex(newPageIndex);
-    }
-  };
-
-  const onTabChange = (index: number) => {
-    const { MANAGE_SOFTWARE } = paths;
-    setNavTabIndex(index);
-    setActionURL &&
-      setActionURL(
-        index === 1 ? `${MANAGE_SOFTWARE}?vulnerable=true` : MANAGE_SOFTWARE
-      );
-  };
-
   const tableHeaders = generateTableHeaders();
 
   // Renders opaque information as host information is loading
-  const opacity = showSoftwareUI ? { opacity: 1 } : { opacity: 0 };
+  const opacity = isSoftwareFetching ? { opacity: 0 } : { opacity: 1 };
 
   return (
     <div className={baseClass}>
@@ -156,8 +68,8 @@ const Software = ({
                   columns={tableHeaders}
                   data={software?.software || []}
                   isLoading={isSoftwareFetching}
-                  defaultSortHeader={DEFAULT_SORT_HEADER}
-                  defaultSortDirection={DEFAULT_SORT_DIRECTION}
+                  defaultSortHeader={"hosts_count"}
+                  defaultSortDirection={SOFTWARE_DEFAULT_SORT_DIRECTION}
                   hideActionButton
                   resultsTitle={"software"}
                   emptyComponent={() =>
@@ -171,7 +83,7 @@ const Software = ({
                   isAllPagesSelected={false}
                   disableCount
                   disableActionButton
-                  pageSize={DEFAULT_PAGE_SIZE}
+                  pageSize={SOFTWARE_DEFAULT_PAGE_SIZE}
                   onQueryChange={onQueryChange}
                 />
               )}
@@ -184,8 +96,8 @@ const Software = ({
                   columns={tableHeaders}
                   data={software?.software || []}
                   isLoading={isSoftwareFetching}
-                  defaultSortHeader={DEFAULT_SORT_HEADER}
-                  defaultSortDirection={DEFAULT_SORT_DIRECTION}
+                  defaultSortHeader={SOFTWARE_DEFAULT_SORT_HEADER}
+                  defaultSortDirection={SOFTWARE_DEFAULT_SORT_DIRECTION}
                   hideActionButton
                   resultsTitle={"software"}
                   emptyComponent={() =>
@@ -199,7 +111,7 @@ const Software = ({
                   isAllPagesSelected={false}
                   disableCount
                   disableActionButton
-                  pageSize={DEFAULT_PAGE_SIZE}
+                  pageSize={SOFTWARE_DEFAULT_PAGE_SIZE}
                   onQueryChange={onQueryChange}
                 />
               )}
