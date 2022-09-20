4f9c04f93a80da180fd39db1e9b40b739049cab1
Roberto Dip <dip.jesusr@gmail.com>
2022-09-20 15:59:56 -0300

    update comment

--- server/contexts/capabilities/capabilities.go
+++ server/contexts/capabilities/capabilities.go
@@ -11,13 +11,14 @@ type key int
 
 const capabilitiesKey key = 0
 
+// NewContext creates a new context with the given capabilities.
 func NewContext(ctx context.Context, r *http.Request) context.Context {
 	capabilities := fleet.CapabilityMap{}
 	capabilities.PopulateFromString(r.Header.Get(fleet.CapabilitiesHeader))
 	return context.WithValue(ctx, capabilitiesKey, capabilities)
 }
 
-// FromContext returns the current user information if present.
+// FromContext returns the capabilities in the request if present.
 func FromContext(ctx context.Context) (fleet.CapabilityMap, bool) {
 	v, ok := ctx.Value(capabilitiesKey).(fleet.CapabilityMap)
 	return v, ok
