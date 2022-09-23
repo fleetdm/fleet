33040a8bf2aa21f9c732f0c5460c03542dc2761c
Gabriel Hernandez <ghernandez345@gmail.com>
2022-09-23 15:48:46 +0100

    remove flaky e2e at the moment for acivity item editing agent options

--- cypress/integration/all/app/dashboard.spec.ts
+++ cypress/integration/all/app/dashboard.spec.ts
@@ -34,32 +34,4 @@ describe("Dashboard", () => {
       cy.getAttached(".operating-systems").should("exist");
     });
   });
-
-  // describe("Activity Card", () => {
-  //   beforeEach(() => {
-  //     cy.loginWithCySession();
-  //   });
-
-  //   it("displays activity when editing agent options", () => {
-  //     cy.intercept("GET", "/api/latest/fleet/activities?*").as("getActivities");
-  //     AppSettingsPage.visitAgentOptions();
-  //     AppSettingsPage.editAgentOptionsForm(
-  //       "{selectall}{backspace}test: null{enter}"
-  //     );
-
-  //     cy.getAttached(".flash-message").should("exist");
-
-  //     cy.visit("/dashboard");
-
-  //     cy.wait("@getActivities").then(() => {
-  //       // the edit agent options is split across multiple elements so we use a
-  //       // matcher function and assert the different parts individually.
-  //       cy.getAttached(".activity-feed__block").within(() => {
-  //         cy.getAttached(".activity-feed__details-topline")
-  //           .first()
-  //           .contains(/edited agent options/gi);
-  //       });
-  //     });
-  //   });
-  // });
 });
