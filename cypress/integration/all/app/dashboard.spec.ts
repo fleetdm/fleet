982fc70e93d8330479873da55d2669a904c21f4d
Gabriel Hernandez <ghernandez345@gmail.com>
2022-09-23 15:48:39 +0100

    remove flaky e2e at the moment for acivity item editing agent options

--- cypress/integration/all/app/dashboard.spec.ts
+++ cypress/integration/all/app/dashboard.spec.ts
@@ -35,31 +35,31 @@ describe("Dashboard", () => {
     });
   });
 
-  describe("Activity Card", () => {
-    beforeEach(() => {
-      cy.loginWithCySession();
-    });
+  // describe("Activity Card", () => {
+  //   beforeEach(() => {
+  //     cy.loginWithCySession();
+  //   });
 
-    it("displays activity when editing agent options", () => {
-      cy.intercept("GET", "/api/latest/fleet/activities?*").as("getActivities");
-      AppSettingsPage.visitAgentOptions();
-      AppSettingsPage.editAgentOptionsForm(
-        "{selectall}{backspace}test: null{enter}"
-      );
+  //   it("displays activity when editing agent options", () => {
+  //     cy.intercept("GET", "/api/latest/fleet/activities?*").as("getActivities");
+  //     AppSettingsPage.visitAgentOptions();
+  //     AppSettingsPage.editAgentOptionsForm(
+  //       "{selectall}{backspace}test: null{enter}"
+  //     );
 
-      cy.getAttached(".flash-message").should("exist");
+  //     cy.getAttached(".flash-message").should("exist");
 
-      cy.visit("/dashboard");
+  //     cy.visit("/dashboard");
 
-      cy.wait("@getActivities").then(() => {
-        // the edit agent options is split across multiple elements so we use a
-        // matcher function and assert the different parts individually.
-        cy.getAttached(".activity-feed__block").within(() => {
-          cy.getAttached(".activity-feed__details-topline")
-            .first()
-            .contains(/edited agent options/gi);
-        });
-      });
-    });
-  });
+  //     cy.wait("@getActivities").then(() => {
+  //       // the edit agent options is split across multiple elements so we use a
+  //       // matcher function and assert the different parts individually.
+  //       cy.getAttached(".activity-feed__block").within(() => {
+  //         cy.getAttached(".activity-feed__details-topline")
+  //           .first()
+  //           .contains(/edited agent options/gi);
+  //       });
+  //     });
+  //   });
+  // });
 });
