855cca9368a19bb4c230abe6c78dc11dfea283ba
Zach Wasserman <zach@fleetdm.com>
2022-09-27 08:25:42 -0700

    Update notarization to use notarytool (#7962)
    Updating from github.com/mitchellh/gon (old API) to the newer xcrun notarytool.
    
    See https://github.com/fleetdm/fleet/actions/runs/3132173324/jobs/5084249006 for example run.

--- orbit/tools/build/sign-macos.sh
+++ orbit/tools/build/sign-macos.sh
@@ -22,6 +22,6 @@ fi
 
 codesign -s "$CODESIGN_IDENTITY" -i com.fleetdm.orbit -f -v --timestamp --options runtime "$1"
 zip -r dist/orbit-macos_darwin_all dist/orbit-macos_darwin_all
-go run github.com/mitchellh/gon/cmd/gon orbit/.gon.hcl
+xcrun notarytool submit dist/orbit-macos_darwin_all.zip --apple-id "$AC_USERNAME" --password "$AC_PASSWORD" --team-id "$AC_TEAM_ID" --wait
 
 echo "Signed successfully"
