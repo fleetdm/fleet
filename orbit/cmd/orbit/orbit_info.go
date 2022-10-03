a4d7e81475fd47c7d0b67d5d77ffaee8e93429d6
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2022-10-03 17:28:19 -0300

    Orbit to support environments with revoked enroll secrets (#8056)
    * Support environments with revoked enroll secrets
    
    * Add instructions on how to fix Orbit enroll
    
    * Rename to last_recorded_error
    
    * Add alternative instructions

--- orbit/cmd/orbit/orbit_info.go
+++ orbit/cmd/orbit/orbit_info.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/fleetdm/fleet/v4/orbit/pkg/build"
 	orbit_table "github.com/fleetdm/fleet/v4/orbit/pkg/table"
+	"github.com/fleetdm/fleet/v4/server/service"
 	"github.com/osquery/osquery-go/plugin/table"
 )
 
 // orbitInfoExtension implements an extension table that provides info about Orbit.
 type orbitInfoExtension struct {
+	orbitClient     *service.OrbitClient
 	deviceAuthToken string
 }
 
@@ -25,6 +28,8 @@ func (o orbitInfoExtension) Columns() []table.ColumnDefinition {
 	return []table.ColumnDefinition{
 		table.TextColumn("version"),
 		table.TextColumn("device_auth_token"),
+		table.TextColumn("enrolled"),
+		table.TextColumn("last_recorded_error"),
 	}
 }
 
@@ -34,10 +39,17 @@ func (o orbitInfoExtension) GenerateFunc(_ context.Context, _ table.QueryContext
 	if v == "" {
 		v = "unknown"
 	}
+	lastRecordedError := ""
+	if err := o.orbitClient.LastRecordedError(); err != nil {
+		lastRecordedError = err.Error()
+	}
+
 	return []map[string]string{
 		{
-			"version":           v,
-			"device_auth_token": o.deviceAuthToken,
+			"version":             v,
+			"device_auth_token":   o.deviceAuthToken,
+			"enrolled":            strconv.FormatBool(o.orbitClient.Enrolled()),
+			"last_recorded_error": lastRecordedError,
 		},
 	}, nil
 }
