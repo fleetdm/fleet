bca8567e9e353d34d233804f7bd5535a8a4af33d
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2022-10-18 11:11:19 -0400

    Fleet UI: Host table defaults to 100 hosts per page (#8290)

--- frontend/pages/hosts/ManageHostsPage/ManageHostsPage.tsx
+++ frontend/pages/hosts/ManageHostsPage/ManageHostsPage.tsx
@@ -1676,6 +1676,7 @@ const ManageHostsPage = ({
         defaultSortDirection={
           (sortBy[0] && sortBy[0].direction) || DEFAULT_SORT_DIRECTION
         }
+        pageSize={100}
         actionButtonText={"Edit columns"}
         actionButtonIcon={EditColumnsIcon}
         actionButtonVariant={"text-icon"}
