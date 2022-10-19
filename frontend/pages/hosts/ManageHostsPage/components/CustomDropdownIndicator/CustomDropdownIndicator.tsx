55bf0cea6aef21daa9e45a091f4d38e19debb5bb
Gabriel Hernandez <ghernandez345@gmail.com>
2022-10-19 23:44:27 +0100

    change label filter dropdown icon match status dropdown and extend icon component (#8337)

---
+++ frontend/pages/hosts/ManageHostsPage/components/CustomDropdownIndicator/CustomDropdownIndicator.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { DropdownIndicatorProps, components } from "react-select-5";
+
+import { ILabel } from "interfaces/label";
+import Icon from "components/Icon";
+
+import { IEmptyOption, IGroupOption } from "../LabelFilterSelect/helpers";
+
+const baseClass = "custom-dropdown-indicator";
+
+const CustomDropdownIndicator = (
+  props: DropdownIndicatorProps<ILabel | IEmptyOption, false, IGroupOption>
+) => {
+  const { isFocused, selectProps } = props;
+  // no access to hover state here from react-select so that is done in the scss
+  // file of LabelFilterSelect.
+  const color = isFocused || selectProps.menuIsOpen ? "#6a67fe" : undefined;
+  return (
+    <components.DropdownIndicator {...props} className={baseClass}>
+      <Icon
+        name="chevron-down"
+        color={color}
+        className={`${baseClass}__icon`}
+      />
+    </components.DropdownIndicator>
+  );
+};
+
+export default CustomDropdownIndicator;
