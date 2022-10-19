5edf7e75fe54b8be6ff88c0de57c04edc493158c
Marcos Oviedo <marcos@fleetdm.com>
2022-10-19 16:05:22 -0300

    Orbit MSI installer now includes the necessary manifest file to use windows_event_log as a logger_plugin.

--- orbit/pkg/packaging/windows.go
+++ orbit/pkg/packaging/windows.go
@@ -87,6 +87,10 @@ func BuildMSI(opt Options) (string, error) {
 		}
 	}
 
+	if err := writeEventLogFile(opt, orbitRoot); err != nil {
+		return "", fmt.Errorf("write eventlog file: %w", err)
+	}
+
 	if err := writeWixFile(opt, tmpDir); err != nil {
 		return "", fmt.Errorf("write wix file: %w", err)
 	}
@@ -142,6 +146,25 @@ func writeWixFile(opt Options, rootPath string) error {
 
 	var contents bytes.Buffer
 	if err := windowsWixTemplate.Execute(&contents, opt); err != nil {
+		return fmt.Errorf("execute template: %w", err)
+	}
+
+	if err := ioutil.WriteFile(path, contents.Bytes(), 0o666); err != nil {
+		return fmt.Errorf("write file: %w", err)
+	}
+
+	return nil
+}
+
+func writeEventLogFile(opt Options, rootPath string) error {
+	// Eventlog manifest is going to be built and dumped into working directory
+	path := filepath.Join(rootPath, "osquery.man")
+	if err := secure.MkdirAll(filepath.Dir(path), constant.DefaultDirMode); err != nil {
+		return fmt.Errorf("mkdir: %w", err)
+	}
+
+	var contents bytes.Buffer
+	if err := windowsOsqueryEventLogTemplate.Execute(&contents, opt); err != nil {
 		return fmt.Errorf("execute template: %w", err)
 	}
 
