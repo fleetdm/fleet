f2d8f06f03e93a4890badd20e8b1056a7429b52c
Eric <eashaw@sailsjs.com>
2022-10-27 16:50:27 -0500

    Update view-generate-license.js

--- website/api/controllers/admin/view-generate-license.js
+++ website/api/controllers/admin/view-generate-license.js
@@ -4,7 +4,7 @@ module.exports = {
   friendlyName: 'View generate license',
 
 
-  description: 'Display "Generate license" page.',
+  description: 'Display "Generate license" page, an admin tool for generating Fleet Premium licenses',
 
 
   exits: {
@@ -18,10 +18,11 @@ module.exports = {
 
   fn: async function () {
 
-
-    if(!sails.config.custom.licenseKeyGeneratorPrivateKey ) {
+    // Throw an error if the licenseKeyGeneratorPrivateKey or licenseKeyGeneratorPassphrase are missing.
+    if(!sails.config.custom.licenseKeyGeneratorPrivateKey) {
       throw new Error('Missing config variable: The license key generator private key missing (sails.config.custom.licenseKeyGeneratorPrivateKey)! To use this tool, a license key generator private key is required.');
     }
+
     if(!sails.config.custom.licenseKeyGeneratorPassphrase) {
       throw new Error('Missing config variable: The license key generator passphrase missing(sails.config.custom.licenseKeyGeneratorPassphrase)! To use this tool, a license key generator passphrase is required.');
     }
