d56728c39f5de22725779f1c45e2854c450f72f6
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-11-03 16:15:08 -0400

    12x12 plus icon, remove x on chrome/ie

--- frontend/components/icons/Plus.tsx
+++ frontend/components/icons/Plus.tsx
@@ -7,8 +7,8 @@ interface IPlusProps {
 const Plus = ({ color = "#6a67fe" }: IPlusProps) => {
   return (
     <svg
-      width="16"
-      height="16"
+      width="12"
+      height="12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 16 16"
