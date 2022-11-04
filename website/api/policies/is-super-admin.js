c19135838f82d96924686152fef426990ef32cb4
Eric <eashaw@sailsjs.com>
2022-11-04 03:12:11 -0500

    Update redirects in is-super-admin policy

--- website/api/policies/is-super-admin.js
+++ website/api/policies/is-super-admin.js
@@ -14,7 +14,8 @@ module.exports = async function (req, res, proceed) {
   // > For more about where `req.me` comes from, check out this app's
   // > custom hook (`api/hooks/custom/index.js`).
   if (!req.me) {
-    return res.unauthorized();
+    // If the request did not come from a logged-in user, we'll redirect them to an generic version of the customer login page.
+    return res.redirect('/customers/login?admin');
   }//•
 
   // Then check that this user is a "super admin".
