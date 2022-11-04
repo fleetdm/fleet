973163a5c331debe2dfed3ba42f1682e6c2cc954
Frank Sievertsen <frank@fleetdm.com>
2022-11-04 18:33:13 +0100

    fix key in test

--- server/service/osquery_utils/queries_test.go
+++ server/service/osquery_utils/queries_test.go
@@ -439,6 +439,7 @@ func TestDirectIngestMDMWindows(t *testing.T) {
 	ds.SetOrUpdateMDMDataFunc = func(ctx context.Context, hostID uint, isServer, enrolled bool, serverURL string, installedFromDep bool, name string) error {
 		require.True(t, enrolled)
 		require.True(t, installedFromDep)
+		require.True(t, isServer)
 		require.NotEmpty(t, serverURL)
 		return nil
 	}
@@ -451,7 +452,7 @@ func TestDirectIngestMDMWindows(t *testing.T) {
 	err = directIngestMDMWindows(context.Background(), log.NewNopLogger(), &host, ds, []map[string]string{
 		{"key": "discovery_service_url", "value": "some url"},
 		{"key": "autopilot", "value": "true"},
-		{"key": "providerID", "value": "1337"},
+		{"key": "provider_id", "value": "1337"},
 		{"key": "installation_type", "value": "Windows SeRvEr 99.9"},
 	}, false)
 	require.NoError(t, err)
