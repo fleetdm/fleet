be352cc2fb8e5d8ef038fcf7ca695c207b58bb8c
Martin Angers <martin.n.angers@gmail.com>
2022-11-08 15:11:51 -0500

    Clean up rules implementation

--- tools/ci/rules.go
+++ tools/ci/rules.go
@@ -26,34 +26,46 @@ func txCheck(m dsl.Matcher) {
 	m.Import("github.com/fleetdm/fleet/v4/server/datastore/mysql")
 	m.Import("github.com/jmoiron/sqlx")
 
+	isDatastoreType := func(v dsl.Var) bool {
+		return v.Type.Is(`*mysql.Datastore`) || v.Type.Is(`mysql.Datastore`)
+	}
+
+	containsIllegal := func(v dsl.Var) bool {
+		return v.Contains(`$ds.writer`) || v.Contains(`$ds.reader`)
+	}
+
+	isSqlxIface := func(v dsl.Var) bool {
+		return (v.Type.Is(`sqlx.ExtContext`) || v.Type.Is(`sqlx.ExecContext`))
+	}
+
 	m.Match(`$ds.withTx($_, $fn)`, `$ds.withRetryTxx($_, $fn)`).
-		Where(m["ds"].Type.Is(`*mysql.Datastore`) && (m["fn"].Contains(`$ds.writer`) || m["fn"].Contains(`$ds.reader`))).
+		Where(isDatastoreType(m["ds"]) && containsIllegal(m["fn"])).
 		Report("improper use of ds.reader or ds.writer in a transaction")
 
 	// any Datastore method that receives a Tx (sqlx.ExtContext/sqlx.ExecContext)
 	// as the first argument must use it.
 	m.Match(`func ($ds *Datastore) $name($p1, $*_) $*_ { $*fn }`, `func ($ds Datastore) $name($p1, $*_) $*_ { $*fn }`).
 		Where(
-			(m["ds"].Type.Is(`*mysql.Datastore`) || m["ds"].Type.Is(`mysql.Datastore`)) &&
-				(m["p1"].Type.Is(`sqlx.ExtContext`) || m["p1"].Type.Is(`sqlx.ExecContext`)) &&
-				(m["fn"].Contains(`$ds.writer`) || m["fn"].Contains(`$ds.reader`))).
+			isDatastoreType(m["ds"]) &&
+				isSqlxIface(m["p1"]) &&
+				containsIllegal(m["fn"])).
 		Report("improper use of ds.reader or ds.writer in Datastore.$name")
 
 	// any Datastore method that receives a Tx (sqlx.ExtContext/sqlx.ExecerContext)
 	// as the second argument must use it (e.g. ds.method(ctx, tx, ...)).
 	m.Match(`func ($ds *Datastore) $name($_, $p2, $*_) $*_ { $*fn }`, `func ($ds Datastore) $name($_, $p2, $*_) $*_ { $*fn }`).
 		Where(
-			(m["ds"].Type.Is(`*mysql.Datastore`) || m["ds"].Type.Is(`mysql.Datastore`)) &&
-				(m["p2"].Type.Is(`sqlx.ExtContext`) || m["p2"].Type.Is(`sqlx.ExecerContext`)) &&
-				(m["fn"].Contains(`$ds.writer`) || m["fn"].Contains(`$ds.reader`))).
+			isDatastoreType(m["ds"]) &&
+				isSqlxIface(m["p2"]) &&
+				containsIllegal(m["fn"])).
 		Report("improper use of ds.reader or ds.writer in Datastore.$name")
 
 	// any func literal that receives a Tx (sqlx.ExtContext/sqlx.ExecerContext)
 	// as the first argument must use it.
 	m.Match(`func($p1, $*_) $*_ { $*fn }`).
 		Where(
-			(m["p1"].Type.Is(`sqlx.ExtContext`) || m["p1"].Type.Is(`sqlx.ExecerContext`)) &&
-				(m["fn"].Contains(`$ds.writer`) || m["fn"].Contains(`$ds.reader`))).
+			isSqlxIface(m["p1"]) &&
+				containsIllegal(m["fn"])).
 		Report("improper use of ds.reader or ds.writer in function literal")
 
 	// TODO: https://github.com/fleetdm/fleet/pull/8621#pullrequestreview-1172676063
