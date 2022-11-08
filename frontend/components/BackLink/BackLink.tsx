a45e7c06cde8001cd0d3886af82419e50b632f9f
Rachel Elysia Perkins <rachel@fleetdm.com>
2022-11-08 15:23:34 -0500

    Remove unused Link import

--- frontend/components/BackLink/BackLink.tsx
+++ frontend/components/BackLink/BackLink.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { browserHistory, Link } from "react-router";
+import { browserHistory } from "react-router";
 import Button from "components/buttons/Button";
 import Icon from "components/Icon";
 import classnames from "classnames";
