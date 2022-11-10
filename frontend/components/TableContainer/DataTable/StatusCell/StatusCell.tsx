d09fd8d870133deab2dde5308dedf9b1c27413cb
Jacob Shandling <jacob@fleetdm.com>
2022-11-10 15:18:05 -0800

    test commit 1

--- frontend/components/TableContainer/DataTable/StatusCell/StatusCell.tsx
+++ frontend/components/TableContainer/DataTable/StatusCell/StatusCell.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classnames from "classnames";
+import ReactTooltip from "react-tooltip";
 
 interface IStatusCellProps {
   value: string;
@@ -13,12 +14,42 @@ const generateClassTag = (rawValue: string): string => {
 };
 
 const StatusCell = ({ value }: IStatusCellProps): JSX.Element => {
+  console.log(`value: ${value}`);
   const statusClassName = classnames(
     "data-table__status",
     `data-table__status--${generateClassTag(value)}`
   );
+  const tipContent = (value: string): string | undefined => {
+    switch (value) {
+      case "online":
+        return "Online hosts will respond to a live query.";
+      case "offline":
+        return `Offline hosts won't respond to a live query because<br/>
+                they may be shut down, asleep, or not connected to<br/>
+                the internet.`;
+    }
+  };
 
-  return <span className={statusClassName}>{value}</span>;
+  // TODO: get unique id for each host
+  const id = "TESTID";
+  return (
+    <span className={statusClassName}>
+      <div data-tip data-for={id}>
+        {value}
+      </div>
+      <ReactTooltip
+        className="online-status-tooltip"
+        place="top"
+        type="dark"
+        effect="solid"
+        id={id}
+        backgroundColor="#3e4771"
+      >
+        {tipContent(value)}
+        'test-tip-val'
+      </ReactTooltip>
+    </span>
+  );
 };
 
 export default StatusCell;
