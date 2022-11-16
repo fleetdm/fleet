394e55e203d9c1acc6aa12025a44d3dfaa3443a7
Gabriel Hernandez <ghernandez345@gmail.com>
2022-11-16 17:00:10 +0000

    refactor activity items and add query name to live_query activity type

---
+++ frontend/__mocks__/queryMock.ts
@@ -0,0 +1,22 @@
+import { IQuery } from "interfaces/query";
+
+const DEFAULT_QUERY_MOCK: IQuery = {
+  created_at: "2022-11-03T17:22:14Z",
+  updated_at: "2022-11-03T17:22:14Z",
+  id: 1,
+  name: "Test Query",
+  description: "A test query",
+  query: "SELECT * FROM users",
+  saved: true,
+  author_id: 1,
+  author_name: "Rachel",
+  author_email: "rachel@fleetdm.com",
+  observer_can_run: false,
+  packs: [],
+};
+
+const createMockQuery = (overrides?: Partial<IQuery>): IQuery => {
+  return { ...DEFAULT_QUERY_MOCK, ...overrides };
+};
+
+export default createMockQuery;
