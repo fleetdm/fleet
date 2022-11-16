82d1c13b441858a3f63d07ce3b79cd1e5729505b
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2022-11-16 13:57:51 -0500

    Frontend integration tests: Create mock mac admins handler (#8728)

--- frontend/test/handlers/device-handler.ts
+++ frontend/test/handlers/device-handler.ts
@@ -3,6 +3,7 @@ import { rest } from "msw";
 import createMockDeviceUser from "__mocks__/deviceUserMock";
 import createMockHost from "__mocks__/hostMock";
 import createMockLicense from "__mocks__/licenseMock";
+import createMockMacAdmins from "__mocks__/macAdminsMock";
 import { baseUrl } from "test/test-utils";
 
 export const defaultDeviceHandler = rest.get(
@@ -25,6 +26,17 @@ export const defaultDeviceMappingHandler = rest.get(
       context.json({
         device_mapping: [createMockDeviceUser()],
         host_id: 1,
+      })
+    );
+  }
+);
+
+export const defaultMacAdminsHandler = rest.get(
+  baseUrl("/device/:token/macadmins"),
+  (req, res, context) => {
+    return res(
+      context.json({
+        macadmins: createMockMacAdmins(),
       })
     );
   }
