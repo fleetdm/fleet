5c9431e53304180a5b6f042ee9163a18d9f27cb1
Roberto Dip <dip.jesusr@gmail.com>
2022-12-01 10:01:17 -0300

    improve tests

--- server/vulnerabilities/msrc/io/msrc_test.go
+++ server/vulnerabilities/msrc/io/msrc_test.go
@@ -31,10 +31,17 @@ func TestMSRCClient(t *testing.T) {
 				require.Error(t, err)
 			})
 
-			t.Run("provided month and year is in the future", func(t *testing.T) {
-				future := now.AddDate(1, 1, 0)
-				_, err := sut.GetFeed(future.Month(), future.Year())
-				require.Error(t, err)
+			t.Run("provided arguments are in the future", func(t *testing.T) {
+				cases := []time.Time{
+					now.AddDate(0, 1, 0),
+					now.AddDate(1, 0, 0),
+					now.AddDate(1, 1, 0),
+				}
+
+				for _, c := range cases {
+					_, err := sut.GetFeed(c.Month(), c.Year())
+					require.Error(t, err)
+				}
 			})
 		})
 
