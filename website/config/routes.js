5bdd1c87e74ff631a89e17176116b3b938d6869e
Eric <eashaw@sailsjs.com>
2022-12-06 15:34:26 -0600

    update API name, move banned emails list inline, update api to send email and post in slack

--- website/config/routes.js
+++ website/config/routes.js
@@ -349,5 +349,5 @@ module.exports.routes = {
   'POST /api/v1/deliver-demo-signup':                   { action: 'deliver-demo-signup' },
   'POST /api/v1/create-or-update-one-newsletter-subscription': { action: 'create-or-update-one-newsletter-subscription' },
   '/api/v1/unsubscribe-from-all-newsletters': { action: 'unsubscribe-from-all-newsletters' },
-  'GET /api/v1/get-signed-apns-certificate': { action: 'get-signed-apns-certificate' },
+  'POST /api/v1/get_signed_apns_csr': { action: 'send-certificate-signing-request-email', csrf: false},
 };
