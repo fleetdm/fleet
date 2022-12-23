2c7410e0a7dd44e40b822b7bd874f24a760fbcf1
Roberto Dip <dip.jesusr@gmail.com>
2022-12-23 17:50:35 -0300

    update migration timestamp

--- server/datastore/mysql/migrations/tables/20221223174807_AlterHostsTablePendingMDMEnrollments.go
+++ server/datastore/mysql/migrations/tables/20221223174807_AlterHostsTablePendingMDMEnrollments.go
@@ -7,21 +7,21 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20221216115821, Down_20221216115821)
+	MigrationClient.AddMigration(Up_20221223174807, Down_20221223174807)
 }
 
-func Up_20221216115821(tx *sql.Tx) error {
+func Up_20221223174807(tx *sql.Tx) error {
 	_, err := tx.Exec(`
 		ALTER TABLE hosts MODIFY osquery_host_id VARCHAR(255) NULL;
 		ALTER TABLE hosts ADD INDEX idx_hosts_hardware_serial (hardware_serial)
 	`)
 	if err != nil {
-		return errors.Wrapf(err, "create table")
+		return errors.Wrapf(err, "altering hosts table")
 	}
 
 	return nil
 }
 
-func Down_20221216115821(tx *sql.Tx) error {
+func Down_20221223174807(tx *sql.Tx) error {
 	return nil
 }
--- server/datastore/mysql/migrations/tables/20221223174807_AlterHostsTablePendingMDMEnrollments_test.go
+++ server/datastore/mysql/migrations/tables/20221223174807_AlterHostsTablePendingMDMEnrollments_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUp_20221216115821(t *testing.T) {
+func TestUp_20221223174807(t *testing.T) {
 	db := applyUpToPrev(t)
 
 	someString := func() string {
