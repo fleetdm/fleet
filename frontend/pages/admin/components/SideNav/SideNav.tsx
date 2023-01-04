afed2e35e0982aca31edd4d9025f727e9f8d315a
Gabriel Hernandez <ghernandez345@gmail.com>
2023-01-04 18:39:39 +0000

    change integration page layout to side nav layout. (#9057)

---
+++ frontend/pages/admin/components/SideNav/SideNav.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import classnames from "classnames";
+
+import { IAppConfigFormProps } from "pages/admin/AppSettingsPage/cards/constants";
+
+import SideNavItem from "../SideNavItem";
+
+const baseClass = "side-nav";
+
+export interface ISideNavItem<T> {
+  title: string;
+  urlSection: string;
+  path: string;
+  Card: (props: T) => JSX.Element;
+}
+
+interface ISideNavProps {
+  navItems: ISideNavItem<IAppConfigFormProps>[];
+  activeItem: string;
+  CurrentCard: JSX.Element;
+  className?: string;
+}
+
+const SideNav = ({
+  navItems,
+  activeItem,
+  CurrentCard,
+  className,
+}: ISideNavProps) => {
+  const classes = classnames(baseClass, className);
+
+  return (
+    <div className={classes}>
+      <div className={`${baseClass}__container`}>
+        <nav aria-label="settings">
+          <ul className={`${baseClass}__nav-list`}>
+            {navItems.map((navItem) => (
+              <SideNavItem
+                key={navItem.title}
+                title={navItem.title}
+                path={navItem.path}
+                isActive={navItem.urlSection === activeItem}
+              />
+            ))}
+          </ul>
+        </nav>
+        <div className={`${baseClass}__card-container`}>{CurrentCard}</div>
+      </div>
+    </div>
+  );
+};
+
+export default SideNav;
