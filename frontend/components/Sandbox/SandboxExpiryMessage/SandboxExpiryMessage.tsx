73097a271b66e292ed008975d30462ddd6aee98b
Rachel Elysia Perkins <rachel@fleetdm.com>
2023-01-05 10:34:46 -0500

    Sandbox mode purple banner

--- frontend/components/Sandbox/SandboxExpiryMessage/SandboxExpiryMessage.tsx
+++ frontend/components/Sandbox/SandboxExpiryMessage/SandboxExpiryMessage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { browserHistory } from "react-router";
+import PATHS from "router/paths";
 
-import ExternalLinkIcon from "../../../../assets/images/icon-external-link-black-12x12@2x.png";
+import Button from "components/buttons/Button";
+import Icon from "components/Icon";
 
 const baseClass = "sandbox-expiry-message";
 
@@ -13,6 +16,33 @@ const SandboxExpiryMessage = ({
   expiry,
   noSandboxHosts,
 }: ISandboxExpiryMessageProps) => {
+  const openAddHostModal = () => {
+    browserHistory.push(PATHS.MANAGE_HOSTS_ADD_HOSTS);
+  };
+
+  if (noSandboxHosts) {
+    return (
+      <div className={baseClass}>
+        <p>Your Fleet Sandbox expires in {expiry}.</p>
+        <div className={`${baseClass}__tip`}>
+          <Icon name="lightbulb" />
+          <p>
+            <b>Quick tip: </b> Enroll a host to get started.
+          </p>
+          <form>
+            <Button
+              onClick={openAddHostModal}
+              className={`${baseClass}__add-hosts`}
+              variant="brand"
+            >
+              <span>Add hosts</span>
+            </Button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <a
       href="https://fleetdm.com/docs/using-fleet/learn-how-to-use-fleet#how-to-add-your-device-to-fleet"
@@ -21,17 +51,10 @@ const SandboxExpiryMessage = ({
       className={baseClass}
     >
       <p>Your Fleet Sandbox expires in {expiry}.</p>
-      {noSandboxHosts ? (
-        <p>
-          It&apos;s time to enroll your first host! Navigate to Hosts &gt;{" "}
-          <b>Add Hosts</b> to get started.
-        </p>
-      ) : (
-        <span>
-          Learn how to use Fleet{" "}
-          <img alt="Open external link" src={ExternalLinkIcon} />
-        </span>
-      )}
+      <p>
+        <b>Learn how to use Fleet</b>{" "}
+        <Icon name="external-link" color="core-fleet-black" />
+      </p>
     </a>
   );
 };
