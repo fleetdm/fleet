98b517f39276ddddc1b394ea51c936e73b60e70d
Jacob Shandling <jacob@fleetdm.com>
2023-01-09 10:00:20 -0800

    Integrate improvements from Gabe review that don't need input from Mike Thomas

--- frontend/components/icons/CircledCheck.tsx
+++ frontend/components/icons/CircledCheck.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const CircledCheck = () => {
   return (
     <svg
-      width="24"
-      height="24"
+      width="48"
+      height="48"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- frontend/pages/admin/IntegrationsPage/cards/Mdm/components/RequestCSRModal/RequestCSRModal.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/Mdm/components/RequestCSRModal/RequestCSRModal.tsx
@@ -7,7 +7,6 @@ import { IRequestCSRFormData } from "interfaces/request_csr";
 import Button from "components/buttons/Button";
 // @ts-ignore
 import InputField from "components/forms/fields/InputField";
-import Spinner from "components/Spinner";
 import DataError from "components/DataError";
 import Icon from "components/Icon";
 import Modal from "components/Modal";
@@ -15,7 +14,6 @@ import Modal from "components/Modal";
 const baseClass = "modal request-csr-modal";
 interface IRequestCSRModalProps {
   onCancel: () => void;
-  testingConnection?: boolean;
 }
 
 interface IFormField {
@@ -23,15 +21,12 @@ interface IFormField {
   value: string;
 }
 
-const RequestCSRModal = ({
-  onCancel,
-  testingConnection,
-}: IRequestCSRModalProps): JSX.Element => {
+const RequestCSRModal = ({ onCancel }: IRequestCSRModalProps): JSX.Element => {
   const { currentUser, config } = useContext(AppContext);
 
   const [formData, setFormData] = useState<IRequestCSRFormData>({
-    email: currentUser?.email || "",
-    orgName: config?.org_info?.org_name || "",
+    email: currentUser?.email ?? "",
+    orgName: config?.org_info?.org_name ?? "",
   });
 
   const [requestState, setRequestState] = useState<
@@ -47,7 +42,6 @@ const RequestCSRModal = ({
 
   const { email, orgName } = formData;
 
-  // destructure change event to its name and value
   const onInputChange = ({ name, value }: IFormField) => {
     setFormData({ ...formData, [name]: value });
   };
@@ -60,95 +54,84 @@ const RequestCSRModal = ({
     setTimeout(() => setRequestState("success"), 1000);
   };
 
+  const RequestCSRSuccess = () => {
+    return (
+      <div className="success">
+        <Icon name="circled-check" />
+        <h2>You&apos;re almost there</h2>
+        <p>
+          Go to your <strong>{email}</strong> email to download your CSR.
+          <br />
+          Your certificate and key for SCEP will be downloaded in the browser.
+          You&apos;ll need these later.
+        </p>
+        <Button onClick={onCancel}>Got it</Button>
+      </div>
+    );
+  };
+
   const renderRequestCSRForm = () => {
-    switch (requestState) {
-      case "success":
-        return (
-          <div className="success">
-            <Icon name="circled-check" />
-            <h2>You&apos;re almost there</h2>
-            <p>
-              Go to your <strong>{email}</strong> email to download your CSR.
-              <br />
-              Your certificate and key for SCEP will be downloaded in the
-              browser. You&apos;ll need these later.
-            </p>
-            <Button onClick={onCancel}>Got it</Button>
-          </div>
-        );
-        break;
-      case "error":
-        return <DataError />;
-        break;
-      default:
-        // requestState is either "undefined" (no request sent yet), "loading" (waiting for
-        // response), or "invalid" (invalid email was submitted)
-        return (
-          <>
-            <p>
-              A CSR and key for APNs and a certificate and key for SCEP are
-              required to connect Fleet to Apple Developer. Apple Inc. requires
-              the following information. <br />
-              <br />
-              fleetdm.com will send your CSR to the below email. Your
-              certificate and key for SCEP will be downloaded in the browser.
-            </p>
-            <form
-              className={`${baseClass}__form`}
-              onSubmit={onFormSubmit}
-              autoComplete="off"
-            >
-              <div className="bottom-label">
-                <InputField
-                  name="email"
-                  onChange={onInputChange}
-                  label="Email"
-                  parseTarget
-                  value={email}
-                  error={invalidMessage}
-                />
-                <p>
-                  Apple Inc. requires a work email (ex.
-                  name@your-organization.com).
-                </p>
-              </div>
-              <InputField
-                name="orgName"
-                onChange={onInputChange}
-                label="Organization name"
-                parseTarget
-                value={orgName}
-              />
-              <div className="modal-cta-wrap">
-                <Button
-                  type="submit"
-                  variant="brand"
-                  isLoading={requestState === "loading"}
-                >
-                  Request
-                </Button>
-                <Button onClick={onCancel} variant="inverse">
-                  Cancel
-                </Button>
-              </div>
-            </form>
-          </>
-        );
+    if (requestState === "success") {
+      return <RequestCSRSuccess />;
     }
+    if (requestState === "error") {
+      return <DataError />;
+    }
+    return (
+      <>
+        <p>
+          A CSR and key for APNs and a certificate and key for SCEP are required
+          to connect Fleet to Apple Developer. Apple Inc. requires the following
+          information. <br />
+          <br />
+          fleetdm.com will send your CSR to the below email. Your certificate
+          and key for SCEP will be downloaded in the browser.
+        </p>
+        <form
+          className={`${baseClass}__form`}
+          onSubmit={onFormSubmit}
+          autoComplete="off"
+        >
+          <div className="bottom-label">
+            <InputField
+              name="email"
+              onChange={onInputChange}
+              label="Email"
+              parseTarget
+              value={email}
+              error={invalidMessage}
+            />
+            <p>
+              Apple Inc. requires a work email (ex. name@your-organization.com).
+            </p>
+          </div>
+          <InputField
+            name="orgName"
+            onChange={onInputChange}
+            label="Organization name"
+            parseTarget
+            value={orgName}
+          />
+          <div className="modal-cta-wrap">
+            <Button
+              type="submit"
+              variant="brand"
+              isLoading={requestState === "loading"}
+            >
+              Request
+            </Button>
+            <Button onClick={onCancel} variant="inverse">
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </>
+    );
   };
 
   return (
     <Modal title="Request" onExit={onCancel} className={baseClass}>
-      <>
-        {testingConnection ? (
-          <div className={`${baseClass}__testing-connection`}>
-            <b>Testing connection</b>
-            <Spinner />
-          </div>
-        ) : (
-          renderRequestCSRForm()
-        )}
-      </>
+      {renderRequestCSRForm()}
     </Modal>
   );
 };
