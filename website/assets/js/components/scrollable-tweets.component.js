9f4290e2b328631957b66387869fa289d0c9e85f
Eric <eashaw@sailsjs.com>
2023-01-10 13:08:29 -0600

    update layout, move tweets into a new component

---
+++ website/assets/js/components/scrollable-tweets.component.js
@@ -0,0 +1,191 @@
+/**
+ * <scrollable-tweets>
+ * -----------------------------------------------------------------------------
+ * A button with a built-in loading spinner.
+ *
+ * @type {Component}
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+parasails.registerComponent('scrollableTweets', {
+  //  ╔═╗╦═╗╔═╗╔═╗╔═╗
+  //  ╠═╝╠╦╝║ ║╠═╝╚═╗
+  //  ╩  ╩╚═╚═╝╩  ╚═╝
+  props: [],
+
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: function () {
+    return {
+      currentTweetPage: 1,
+      numberOfTweetPages: 2,
+      howManyTweetsCanFitOnThisPage: 3,
+      tweetsPaddingWidth: 0,
+      tweetCardWidth: 0,
+    };
+  },
+
+  //  ╦ ╦╔╦╗╔╦╗╦
+  //  ╠═╣ ║ ║║║║
+  //  ╩ ╩ ╩ ╩ ╩╩═╝
+  template: `
+  <div class="d-flex flex-column">
+    <div purpose="tweets" class="d-flex flex-row flex-nowrap">
+      <div purpose="tweet-card" class="card">
+        <div class="mb-4">
+          <a href="https://twitter.com/Uber"><img width="87" height="38" alt="Uber logo" src="/images/social-proof-logo-uber-87x38@2x.png"/></a>
+        </div>
+        <p class="pb-2 mb-1">Exciting. This is a team that listens to feedback.</p>
+        <div class="row px-3 pt-2">
+          <div>
+            <p class="font-weight-bold m-0">Erik Gomez</p>
+            <p class="m-0">Staff Software Engineer <a href="https://twitter.com/Uber">@Uber</a></p>
+          </div>
+        </div>
+      </div>
+      <div purpose="tweet-card" class="card">
+        <div class="mb-4">
+          <a href="https://twitter.com/Square"><img width="131" height="38" alt="Square logo" src="/images/social-proof-logo-square-131x38@2x.png"/></a>
+        </div>
+        <p class="pb-2 mb-1">Mad props to how easy making a deploy pkg of Orbit was. I wish everyone made stuff that easy.</p>
+        <div class="row px-3 pt-2">
+          <div>
+            <p class="font-weight-bold m-0">Wesley Whetstone</p>
+            <p class="m-0">CPE <a href="https://twitter.com/Square">@Square</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div purpose="tweet-card" class="card">
+        <div class="mb-4">
+          <a href="https://twitter.com/atlassian"><img width="162" height="20" alt="Atlassian logo" src="/images/social-proof-logo-atlassian-162x20@2x.png"/></a>
+        </div>
+        <p class="pb-2 mb-1"><a href="https://twitter.com/hashtag/fleet">#Fleet</a>’s come a long way - to now being the top open-source <a href="https://twitter.com/hashtag/fleet">#osquery</a> manager. Just in the past 6 months.</p>
+        <div class="row px-3 pt-2">
+          <div>
+            <p class="font-weight-bold m-0">Brendan Shaklovitz</p>
+            <p class="m-0">Senior SRE <a href="https://twitter.com/atlassian">@Atlassian</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div purpose="tweet-card" class="card">
+        <div class="mb-4">
+          <a href="https://twitter.com/osquery"><img width="140" height="36" alt="osquery logo" src="/images/social-proof-logo-osquery-140x36@2x.png"/></a>
+        </div>
+        <p class="pb-2 mb-1">It’s great to see the new release of Fleet containing some really cool new features that make <a href="https://twitter.com/osquery">@osquery</a> much more usable in practical environments. I’m really impressed with the work that <a href="https://twitter.com/thezachw">@thezachw</a> and crew are doing at <a href="https://twitter.com/fleetctl">@fleetctl</a>.</p>
+        <div class="row px-3 pt-2">
+          <div>
+            <p class="font-weight-bold m-0">Mike Arpaia</p>
+            <p class="m-0">Creator of <a href="https://twitter.com/osquery">@osquery</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div purpose="tweet-card" class="card">
+        <div class="mb-4">
+          <a href="https://twitter.com/Wayfair"><img width="136" height="32" alt="Wayfair logo" src="/images/social-proof-logo-wayfair-136x32@2x.png"/></a>
+        </div>
+        <p class="pb-2 mb-1"><a href="https://twitter.com/hashtag/osquery">#osquery</a> is one of the best tools out there and <a href="https://twitter.com/hashtag/fleetdm">#fleetdm</a> makes it even better. Highly recommend it if you want to monitor, detect and investigate threats on a scale and also for infra/sys admin.</p>
+        <p>I have used it on 15k servers and it’s really scalable.</p>
+        <div class="row px-3 pt-2">
+          <div>
+            <p class="font-weight-bold m-0">Ahmed Elshaer</p>
+            <p class="m-0">DFIR, Blue Teaming, SecOps <a href="https://twitter.com/Wayfair">@wayfair</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div purpose="tweet-card" class="card">
+        <div class="mb-4">
+          <a href="https://twitter.com/comcast"><img width="107" height="38" alt="Comcast logo" src="/images/social-proof-logo-comcast-107x38.png"/></a>
+        </div>
+        <p class="pb-2 mb-1">With the power of osquery, you need a scalable & resilient platform to manage your workloads. Fleet is the "just right" open-source, enterprise grade solution.</p>
+        <div class="row px-3 pt-2">
+          <div>
+            <p class="font-weight-bold m-0">Abubakar Yousafzai</p>
+            <p class="m-0">Security Software Development & Engineering <a href="https://twitter.com/comcast">@Comcast</a></p>
+          </div>
+        </div>
+      </div>
+      <div purpose="tweet-cards-right-padding">
+      </div>
+    </div>
+    <div purpose="" class="mx-auto">
+      <nav aria-label="..." >
+        <ul purpose="tweets-page-indicator" class="pagination pagination-sm">
+          <li class="page-item" :class="[currentTweetPage === pageNumber ? 'selected' : '']" v-for="pageNumber in numberOfTweetPages" @click="scrollTweetDivHorizontally(pageNumber)"></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  `,
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function(){
+    await this.updateNumberOfTweetPages(); // Update the number of pages for the tweet page indicator.
+    window.addEventListener('mousewheel', this.updateTweetsPageIndicator); // Add a mouse wheel event listener to update the tweet page indicator when a user scrolls the div.
+    window.addEventListener('resize', this.updateNumberOfTweetPages); // Add an event listener to update the number of tweet pages based on how many tweet cards can fit on the screen.
+  },
+  beforeDestroy: function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+
+    updateNumberOfTweetPages: async function() {
+      // Get the first tweet card in the tweets div.
+      let firstTweetCardDiv = $('div[purpose="tweet-card"]')[0];
+      // Update the tweetCardWidth value to be the actual width of a tweet card.
+      this.tweetCardWidth = firstTweetCardDiv.clientWidth + 16;
+      let tweetsDiv = document.querySelector('div[purpose="tweets"]');
+      let tweetsPadding = window.getComputedStyle(tweetsDiv).getPropertyValue('padding-left');
+      this.tweetsPaddingWidth = parseInt(tweetsPadding);
+      let usersScreenWidth = window.innerWidth;
+      this.howManyTweetsCanFitOnThisPage = Math.floor(usersScreenWidth/this.tweetCardWidth);
+      this.numberOfTweetPages = Math.max(Math.floor(6 / this.howManyTweetsCanFitOnThisPage), 2);
+      await this.forceRender();
+    },
+    updateTweetsPageIndicator: function() {
+      // Get the tweets div.
+      let tweetsDiv = document.querySelector('div[purpose="tweets"]');
+      // Get the amount the tweets div has been scrolled to the left.
+      let currentTweetDivScrollAmount = tweetsDiv.scrollLeft;
+      // Divide the current amount scrolled by the width of a tweet card, and divide that value by how many tweet cards we can show on a page.
+      let pageThisShouldBe = ((currentTweetDivScrollAmount) / this.tweetCardWidth) / this.howManyTweetsCanFitOnThisPage;
+      // Round that value to the nearest whole number, if the number rounds to 0, then set it to be 1.
+      let pageToIndicate = Math.round(pageThisShouldBe) > 0 ? Math.round(pageThisShouldBe) + 1 : 1;
+      this.currentTweetPage = pageToIndicate;
+    },
+    scrollTweetDivHorizontally: function(page) {
+      let tweetsDiv = document.querySelector('div[purpose="tweets"]');
+      if(page === this.currentTweetPage){ // If the page it is currently on is selected, do nothing.
+        return;
+      } else if(page === 1){// If the first page was selected, scroll to the div to the starting position.
+        tweetsDiv.scroll(0, 9000);
+      } else if(page !== this.currentTweetPage) { // If any other page was selected, scroll the tweets div.
+        // Get the amount we need to scroll for a single page.
+        let amountToScrollBy = ((6 / this.numberOfTweetPages) * this.tweetCardWidth);
+        // Get the number of pages we're moving
+        let pageDifference = page - this.currentTweetPage;
+        // Multiply the amount to scroll by the number of pages we're scrolling
+        amountToScrollBy = pageDifference * amountToScrollBy;
+        // Scroll the Tweets div
+        tweetsDiv.scrollBy(amountToScrollBy, 0);
+      }
+      // Set the current page.
+      this.currentTweetPage = page;
+    },
+
+  }
+});
--- website/assets/js/pages/fleet-mdm.page.js
+++ website/assets/js/pages/fleet-mdm.page.js
@@ -22,18 +22,6 @@ parasails.registerPage('fleet-mdm', {
     // Modal
 
     modal: '',
-
-    // Page indicatior for the scrollable tweet cards
-    currentTweetPage: 1,
-    numberOfTweetPages: 2,
-    howManyTweetsCanFitOnThisPage: 3,
-    tweetDivPaddingWidth: 0,
-  },
-  computed: {
-    tweetCardWidth: function() {
-      let firstTweetCardDiv = $('div[purpose="tweet-card"]')[0];
-      return firstTweetCardDiv.clientWidth + 16;
-    },
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -43,55 +31,14 @@ parasails.registerPage('fleet-mdm', {
     //…
   },
   mounted: async function() {
-    await this.updateNumberOfTweetPages(); // Update the number of pages for the tweet page indicator.
-    window.addEventListener('mousewheel', this.updateCurrentTweetsPage); // Add a mouse wheel event listener to update the tweet page indicator when a user scrolls the div.
-    window.addEventListener('resize', this.updateNumberOfTweetPages); // Add an event listener to updat ethe number of tweet pages based on how many tweet cards can fit on the screen.
+
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
   //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
   //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
   methods: {
-    // TODO before merge: Clean these up.
-    updateCurrentTweetsPage: async function() {
-      let tweetsDiv = $('div[purpose="tweets"]')[0];
-      let currentTweetDivScrollAmount = tweetsDiv.scrollLeft;
-      let pageThisShouldBe = ((currentTweetDivScrollAmount) / this.tweetCardWidth) / this.howManyTweetsCanFitOnThisPage;
-      let pageToIndicate = Math.round(pageThisShouldBe) > 0 ? Math.round(pageThisShouldBe) + 1 : 1;
-      console.log(pageToIndicate);
-      if(pageToIndicate > this.numberOfTweetPages){
-        pageToIndicate = this.numberOfTweetPages;
-      }
-      this.currentTweetPage = pageToIndicate;
-    },
-    updateNumberOfTweetPages: async function() {
-      let tweetsPaddingAmount = $('div[purpose="tweets"]').css('padding-left');
-      this.tweetDivPaddingWidth = eval(tweetsPaddingAmount.split('px')[0]);
-      let usersScreenWidth = window.innerWidth;
-      this.howManyTweetsCanFitOnThisPage = Math.floor(usersScreenWidth/this.tweetCardWidth);
-      this.numberOfTweetPages = Math.floor(6/this.howManyTweetsCanFitOnThisPage);
-      if(this.numberOfTweetPages === Infinity) {
-        this.numberOfTweetPages = 6;
-      } else if (this.numberOfTweetPages === 1){
-        this.numberOfTweetPages = 2;
-      }
-      await this.forceRender();
-    },
-    scrollTweetDivHorizontally: function(page) {
-      let tweetsDiv = document.getElementById('tweets');
-      if(this.currentTweetPage === page){
-        return;
-      }
-      if(page === 1){
-        tweetsDiv.scroll(0, 9000);
-      } else if(page !== this.currentTweetPage) {
-        let amountToScrollBy = ((6 / this.numberOfTweetPages) * this.tweetCardWidth);
-        let pageDifference = page - this.currentTweetPage;
-        amountToScrollBy = pageDifference * amountToScrollBy;
-        tweetsDiv.scrollBy(amountToScrollBy, 0);
-      }
-      this.currentTweetPage = page;
-    },
+
     clickOpenSignupModal: function() {
       this.modal = 'beta-signup';
     },
