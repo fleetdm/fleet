b652ffd31570b486f3036b3f5c9006b4c5b4c6d8
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2023-01-17 14:00:21 -0500

    Add addon modules for s3 firehose logging and automatic migrations

---
+++ terraform/addons/README.md
@@ -0,0 +1,2 @@
+This directory contains addons to the core Fleet module that add additional features such as logging destinations, or other features that some customers might not want.
+These can be included and used as desired.
--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -1,13 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      Example = "This is a demo of the Fleet terraform module"
+    }
+  }
+}
+
+resource "random_pet" "main" {}
+
 module "main" {
   source          = "../"
   certificate_arn = module.acm.acm_certificate_arn
+  vpc = {
+    name = random_pet.main.id
+  }
+  fleet_config = {
+    extra_environment_variables = module.firehose-logging.fleet_extra_environment_variables
+    extra_iam_policies          = module.firehose-logging.fleet_extra_iam_policies
+  }
 }
 
 module "acm" {
   source  = "terraform-aws-modules/acm/aws"
   version = "4.3.1"
 
-  domain_name = "fleet.loadtest.fleetdm.com"
+  domain_name = "${random_pet.main.id}.loadtest.fleetdm.com"
   zone_id     = data.aws_route53_zone.main.id
 
   wait_for_validation = true
@@ -15,7 +41,7 @@ module "acm" {
 
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.id
-  name    = "fleet.loadtest.fleetdm.com"
+  name    = "${random_pet.main.id}.loadtest.fleetdm.com"
   type    = "A"
 
   alias {
@@ -28,4 +54,23 @@ resource "aws_route53_record" "main" {
 data "aws_route53_zone" "main" {
   name         = "loadtest.fleetdm.com."
   private_zone = false
+}
+
+module "firehose-logging" {
+  source = "../addons/logging-destination-firehose"
+  osquery_results_s3_bucket = {
+    name = "${random_pet.main.id}-results"
+  }
+  osquery_status_s3_bucket = {
+    name = "${random_pet.main.id}-status"
+  }
+}
+
+module "migrations" {
+  source                   = "../addons/migrations"
+  ecs_cluster              = module.main.byo-vpc.byo-db.byo-ecs.service.cluster
+  task_definition          = module.main.byo-vpc.byo-db.byo-ecs.task_definition.family
+  task_definition_revision = module.main.byo-vpc.byo-db.byo-ecs.task_definition.revision
+  subnets                  = module.main.byo-vpc.byo-db.byo-ecs.service.network_configuration[0].subnets
+  security_groups          = module.main.byo-vpc.byo-db.byo-ecs.service.network_configuration[0].security_groups
 }
