8601df74fb4a0261f290e45f4b47e018020d4090
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2023-01-18 14:59:51 -0500

    Add fix for http -> https redirect

--- terraform/byo-vpc/byo-db/main.tf
+++ terraform/byo-vpc/byo-db/main.tf
@@ -64,9 +64,14 @@ module "alb" {
 
   http_tcp_listeners = [
     {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+      port        = 80
+      protocol    = "HTTP"
+      action_type = "REDIRECT"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
   ]
 }
