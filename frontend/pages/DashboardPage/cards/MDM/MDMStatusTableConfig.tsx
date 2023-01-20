b6b073edce845b415a4537fa94f2f36b890bb629
Rachel Elysia Perkins <rachel@fleetdm.com>
2023-01-20 14:09:00 -0500

    Closest sort order for mdm status

--- frontend/pages/DashboardPage/cards/MDM/MDMStatusTableConfig.tsx
+++ frontend/pages/DashboardPage/cards/MDM/MDMStatusTableConfig.tsx
@@ -58,7 +58,7 @@ const statusTableHeaders = [
         {cellProps.cell.value}
       </TooltipWrapper>
     ),
-    sortType: "caseInsensitive",
+    sortType: "hasLength",
   },
   {
     title: "Hosts",
