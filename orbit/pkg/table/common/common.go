92bf996bcb7d911a2b9736dfcb3230d2966a9ee8
Sharon Katz <121527325+sharon-fdm@users.noreply.github.com>
2023-01-23 14:00:35 -0500

    Update common.go

--- orbit/pkg/table/common/common.go
+++ orbit/pkg/table/common/common.go
@@ -9,7 +9,7 @@ import (
 	"syscall"
 )
 
-// getActiveUserGroup gets the uid and gid of the current (or more accurately, most recently logged
+// GetActiveUserGroup gets the uid and gid of the current (or more accurately, most recently logged
 // in) *console* user. In most scenarios this should be the currently logged in user on the system.
 // Note that getting the current user of the Orbit process is typically going to return root and we
 // need the underlying user.
