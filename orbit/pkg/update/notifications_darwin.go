357c0484fc5acecfd97a14f3fcdbed1836b4d949
Martin Angers <martin.n.angers@gmail.com>
2023-01-24 09:23:58 -0500

    orbit: run the `profiles` command to renew the enrollment profile when signaled by fleet (#9409)

---
+++ orbit/pkg/update/notifications_darwin.go
@@ -0,0 +1,21 @@
+//go:build darwin
+
+package update
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func runRenewEnrollmentProfile() error {
+	cmd := exec.Command("/usr/bin/profiles", "renew", "--type", "enrollment")
+	out, err := cmd.CombinedOutput()
+	if err != nil && len(out) > 0 {
+		// just as a precaution, limit the length of the output
+		if len(out) > 512 {
+			out = out[:512]
+		}
+		err = fmt.Errorf("%w: %s", err, string(out))
+	}
+	return err
+}
