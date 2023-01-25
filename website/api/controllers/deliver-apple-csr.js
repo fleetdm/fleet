3f9b3ce228002000105d180c82bd67c0f8eab688
Martin Angers <martin.n.angers@gmail.com>
2023-01-25 12:00:22 -0500

    Set status code when email is invalid

--- website/api/controllers/deliver-apple-csr.js
+++ website/api/controllers/deliver-apple-csr.js
@@ -25,7 +25,8 @@ module.exports = {
     },
 
     invalidEmailDomain: {
-      description: 'This email address is on a denylist of domains and was not delivered.'
+      description: 'This email address is on a denylist of domains and was not delivered.',
+      statusCode: 400
     },
 
     badRequest: {
