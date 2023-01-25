cb45c62fd614a0ce16c1a4870d3fb8dbc79d14f6
Sharon Katz <sharon@fleetdm.com>
2023-01-25 10:34:02 -0500

    add tests

--- orbit/pkg/table/common/common_test.go
+++ orbit/pkg/table/common/common_test.go
@@ -47,3 +47,48 @@ func TestGetIntFromXMLWithTags(t *testing.T) {
 	}
 
 }
+
+func TestGetIntFromXMLWithTagsBadXML(t *testing.T) {
+	testXML := `<?xml veools.com</link>
+  <description>Free web build
+    <integer>11</integer>
+  </parentTag>
+  <item>://www.w3s_rss.asp</lin
+    <description>New RSS tutorial on W3Schools</description>
+  </itess>`
+
+	_, err := GetIntFromXMLWithTags(testXML, "parentTag", "tag", "tagValue")
+	if err == nil {
+		t.Fatalf("Err expected. Got nil")
+	}
+}
+
+func TestGetIntFromXMLWithTagsNoTag(t *testing.T) {
+	testXML := `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+<channel>
+  <title>W3Schools Home Page</title>
+  <link>https://www.w3schools.com</link>
+  <description>Free web building tutorials</description>
+  <parentTag>
+    <tag>tagValue</tag>
+    <integer>11</integer>
+  </parentTag>
+  <item>
+    <title>RSS Tutorial</title>
+    <link>https://www.w3schools.com/xml/xml_rss.asp</link>
+    <description>New RSS tutorial on W3Schools</description>
+  </item>
+  <item>
+    <title>XML Tutorial</title>
+    <link>https://www.w3schools.com/xml</link>
+    <description>New XML tutorial on W3Schools</description>
+  </item>
+</channel>
+</rss>`
+
+	_, err := GetIntFromXMLWithTags(testXML, "badTag", "BadTag", "BadValue")
+	if err == nil {
+		t.Fatalf("Err expected. Got nil")
+	}
+}
