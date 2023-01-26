3a3251306fca37e39f0135b8a1658c7825408b86
Sharon Katz <sharon@fleetdm.com>
2023-01-26 15:27:25 -0500

    fix

--- orbit/pkg/table/logging_and_auditing/logging_and_auditing.go
+++ orbit/pkg/table/logging_and_auditing/logging_and_auditing.go
@@ -13,7 +13,7 @@ import (
 // Columns is the schema of the table.
 func Columns() []table.ColumnDefinition {
 	return []table.ColumnDefinition{
-		table.IntegerColumn("security_auditing_flags"),
+		table.TextColumn("security_auditing_flags"),
 	}
 }
 
@@ -23,7 +23,7 @@ func Generate(ctx context.Context, queryContext table.QueryContext) ([]map[strin
 
 	flagsLine, err := tbl_common.FindLineInFileContainingString("/etc/security/audit_control", "flags")
 	if err != nil {
-		return []map[string]string{}, err
+		return nil, err
 	}
 	flags := strings.Replace(flagsLine, "flags:", "", 1)
 
