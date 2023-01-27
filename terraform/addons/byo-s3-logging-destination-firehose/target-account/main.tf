2234829a2811ff546dfae764cad1809cba5f93d6
Benjamin Edwards <edwards.benw@gmail.com>
2023-01-27 11:56:07 -0500

    add readme, refactor variables, add outputs

--- terraform/addons/byo-s3-logging-destination-firehose/target-account/main.tf
+++ terraform/addons/byo-s3-logging-destination-firehose/target-account/main.tf
@@ -1,18 +1,3 @@
-variable "osquery_results_bucket" {
-  type        = string
-  description = "name of the bucket to store osquery results logs"
-}
-
-variable "osquery_status_bucket" {
-  type        = string
-  description = "name of the bucket to store osquery status logs"
-}
-
-variable "fleet_iam_role_arn" {
-  type        = string
-  description = "the arn of the fleet role that firehose will assume to write data to your bucket"
-}
-
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "results" {
