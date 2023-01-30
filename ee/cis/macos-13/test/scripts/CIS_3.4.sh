a64ddad13710cc3cd45af6c4aaef9a516608ac15
Sharon Katz <sharon@fleetdm.com>
2023-01-30 14:55:24 -0500

    more tests

--- ee/cis/macos-13/test/scripts/CIS_3.4.sh
+++ ee/cis/macos-13/test/scripts/CIS_3.4.sh
@@ -6,7 +6,6 @@ sed "s/${origExpire}/expire-after:60d OR 1G/" ./tmp.txt > /etc/security/audit_co
 rm ./tmp.txt;
 
 
-
 # Explenation:
 # In your /etc/security/audit_control , look for a line starting at: expire-after
 # Cases to test:
@@ -14,6 +13,11 @@ rm ./tmp.txt;
 # SHOULD PASS:   expire-after:6G
 # SHOULD PASS:   expire-after:59d OR 6G
 # SHOULD PASS:   expire-after:60d OR 5G
+
 # SHOULD FAIL:   expire-after:59d
 # SHOULD FAIL:   expire-after:5G
 # SHOULD FAIL:   expire-after:59d OR 5G
+# SHOULD FAIL:   expire-after:60D
+# SHOULD FAIL:   expire-after:6g
+# SHOULD FAIL:   expire-after:59D OR 6g
+# SHOULD FAIL:   expire-after:60D OR 5g
