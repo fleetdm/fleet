cb11570f2ae69360e7f70c620f5b52ae38809ff3
Sharon Katz <sharon@fleetdm.com>
2023-01-31 10:50:38 -0500

    test script

--- ee/cis/macos-13/test/scripts/CIS_3.3.sh
+++ ee/cis/macos-13/test/scripts/CIS_3.3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-/usr/bin/sudo sed -E 's/all_max=[0-9]+M//g' /etc/asl/com.apple.install
-/usr/bin/sudo sed -E 's/all_max=[0-9]+G//g' /etc/asl/com.apple.install
+/usr/bin/sudo sed -E 's/all_max=[0-9]+M//g' /etc/asl/com.apple.install  > ./tmp.txt
+/usr/bin/sudo cp ./tmp.txt /etc/asl/com.apple.install
+/usr/bin/sudo rm ./tmp.txt
+
+/usr/bin/sudo sed -E 's/all_max=[0-9]+G//g' /etc/asl/com.apple.install  > ./tmp.txt
+/usr/bin/sudo cp ./tmp.txt /etc/asl/com.apple.install
+/usr/bin/sudo rm ./tmp.txt
+
 
