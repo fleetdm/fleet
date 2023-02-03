ffeec79e4fc9740bb0b9970fb38616db39060f66
Sharon Katz <sharon@fleetdm.com>
2023-02-03 16:11:13 -0500

    tests

--- orbit/pkg/table/file_system_permissions/file_system_permissions_test.go
+++ orbit/pkg/table/file_system_permissions/file_system_permissions_test.go
@@ -4,10 +4,28 @@
 package file_system_permissions
 
 import (
+	"github.com/osquery/osquery-go/plugin/table"
 	"golang.org/x/net/context"
 	"testing"
 	"time"
 )
+
+func TestGenerate(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	var tbl table.QueryContext
+
+	table, err := Generate(ctx, tbl)
+	if err != nil {
+		t.Fatalf(`Expected no error. got %s`, err)
+	}
+	if table[0]["amfi_enabled"] != "0" && table[0]["amfi_enabled"] != "1" {
+		t.Fatalf(`amfiEnabled expected 0 or 1. got %s`, table[0]["amfi_enabled"])
+	}
+	if table[0]["ssv_enabled"] != "0" && table[0]["ssv_enabled"] != "1" {
+		t.Fatalf(`ssvEnabled expected 0 or 1. got %s`, table[0]["ssvEnabled"])
+	}
+}
 
 func TestGetAMFIEnabled(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
