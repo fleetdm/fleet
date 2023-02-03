4cfb882d8fbeb17377bf1ebffe532e976358d060
Sharon Katz <sharon@fleetdm.com>
2023-02-03 15:02:50 -0500

    tests

--- orbit/pkg/table/file_system_permissions/file_system_permissions_test.go
+++ orbit/pkg/table/file_system_permissions/file_system_permissions_test.go
@@ -10,11 +10,23 @@ import (
 )
 
 func TestGetAMFIEnabled(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	amfiEnabled, err := getAMFIEnabled(ctx)
 	if amfiEnabled != "0" && amfiEnabled != "1" {
-		t.Fatalf(`amfiEnabled expected some answer. got %s`, amfiEnabled)
+		t.Fatalf(`amfiEnabled expected 0 or 1. got %s`, amfiEnabled)
+	}
+	if err != nil {
+		t.Fatalf(`Expected no error. got %s`, err)
+	}
+}
+
+func TestGetSSVEnabled(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	ssvEnabled, err := getSSVEnabled(ctx)
+	if ssvEnabled != "0" && ssvEnabled != "1" {
+		t.Fatalf(`ssvEnabled expected 0 or 1. got %s`, ssvEnabled)
 	}
 	if err != nil {
 		t.Fatalf(`Expected no error. got %s`, err)
