84ca2d277d87add20d13f37706d6e31f40dd60a9
Benjamin Edwards <edwards.benw@gmail.com>
2023-02-03 16:47:46 -0500

    refactor logging module to prefer logging directly to firehose in the customer account

--- terraform/addons/byo-firehose-logging-destination/target-account/firehose.tf
+++ terraform/addons/byo-firehose-logging-destination/target-account/firehose.tf
@@ -1,6 +1,3 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
 data "aws_iam_policy_document" "osquery_firehose_assume_role" {
   statement {
     effect  = "Allow"
@@ -25,31 +22,32 @@ data "aws_iam_policy_document" "firehose_policy" {
       "s3:PutObjectAcl" // required according to https://docs.aws.amazon.com/firehose/latest/dev/controlling-access.html#using-iam-s3
     ]
     resources = [
-      "arn:aws:s3:::${var.results_destination_s3_bucket}",
-      "arn:aws:s3:::${var.results_destination_s3_bucket}/*",
-      "arn:aws:s3:::${var.status_destination_s3_bucket}",
-      "arn:aws:s3:::${var.status_destination_s3_bucket}/*"
+      aws_s3_bucket.destination.arn,
+      "${aws_s3_bucket.destination.arn}/*",
     ]
   }
 
   statement {
-    effect    = "Allow"
-    actions   = ["kms:GenerateDataKey*"]
-    resources = [var.kms_key_arn]
-  }
-
-  statement {
-    effect    = "Allow"
-    actions   = ["logs:PutLogEvents"]
+    effect  = "Allow"
+    actions = ["logs:PutLogEvents"]
     resources = [
       "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/kinesisfirehose/${var.firehose_results_name}:*",
       "arn:aws:logs:${data.aws_region.current.id}:${data.aws_caller_identity.current.account_id}:log-group:/aws/kinesisfirehose/${var.firehose_status_name}:*"
     ]
   }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = [data.aws_kms_alias.s3.arn]
+  }
+
 }
 
 resource "aws_iam_role" "firehose" {
-  name = "${var.customer_prefix}-firehose-cross-account-role"
   assume_role_policy = data.aws_iam_policy_document.osquery_firehose_assume_role.json
 }
 
@@ -62,15 +60,22 @@ resource "aws_iam_role_policy_attachment" "firehose" {
   role       = aws_iam_role.firehose.name
 }
 
+resource "aws_kms_key" "firehose" {
+  enable_key_rotation = true
+}
+
 resource "aws_kinesis_firehose_delivery_stream" "osquery_results" {
   name        = var.firehose_results_name
   destination = "s3"
 
+  server_side_encryption {
+    key_arn = aws_kms_key.firehose.arn
+  }
+
   s3_configuration {
-    prefix      = var.results_object_prefix
-    role_arn    = aws_iam_role.firehose.arn
-    bucket_arn  = "arn:aws:s3:::${var.results_destination_s3_bucket}"
-    kms_key_arn = var.kms_key_arn
+    prefix     = var.results_prefix
+    role_arn   = aws_iam_role.firehose.arn
+    bucket_arn = aws_s3_bucket.destination.arn
   }
 }
 
@@ -78,27 +83,13 @@ resource "aws_kinesis_firehose_delivery_stream" "osquery_status" {
   name        = var.firehose_status_name
   destination = "s3"
 
+  server_side_encryption {
+    key_arn = aws_kms_key.firehose.arn
+  }
+
   s3_configuration {
-    prefix      = var.status_object_prefix
-    role_arn    = aws_iam_role.firehose
-    bucket_arn  = "arn:aws:s3:::${var.status_destination_s3_bucket}"
-    kms_key_arn = var.kms_key_arn
+    prefix     = var.status_prefix
+    role_arn   = aws_iam_role.firehose.arn
+    bucket_arn = aws_s3_bucket.destination.arn
   }
-}
-
-data "aws_iam_policy_document" "firehose-logging" {
-  statement {
-    actions = [
-      "firehose:DescribeDeliveryStream",
-      "firehose:PutRecord",
-      "firehose:PutRecordBatch",
-    ]
-    resources = [aws_kinesis_firehose_delivery_stream.osquery_results.arn, aws_kinesis_firehose_delivery_stream.osquery_status.arn]
-  }
-}
-
-resource "aws_iam_policy" "firehose-logging" {
-  name        = "fleet-firehose-logging"
-  description = "An IAM policy for fleet to log to Firehose destinations"
-  policy      = data.aws_iam_policy_document.firehose-logging.json
 }
