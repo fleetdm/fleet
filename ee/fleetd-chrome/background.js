56e91d71973c2966d625ee50c18a60fedfceb8b0
Zach Wasserman <zach@fleetdm.com>
2023-02-03 17:03:37 -0800

    refactoring

--- ee/fleetd-chrome/background.js
+++ ee/fleetd-chrome/background.js
@@ -6,17 +6,22 @@ import * as SQLite from "./node_modules/wa-sqlite/src/sqlite-api.js";
 import TableOSVersion from "./os_version.mjs";
 import TableGeolocation from "./geolocation.js";
 
-let globalDB;
+const register = (sqlite3, db, table) => {
+  sqlite3.create_module(db, table.name, table);
+};
+
 (async () => {
   const module = await SQLiteAsyncESMFactory();
   const sqlite3 = SQLite.Factory(module);
   const db = await sqlite3.open_v2(":memory:");
-  globalDB = db;
 
-  sqlite3.create_module(db, "os_version", new TableOSVersion(sqlite3, db));
-  sqlite3.create_module(db, "geolocation", new TableGeolocation(sqlite3, db));
+  register(sqlite3, db, new TableOSVersion(sqlite3, db));
+  register(sqlite3, db, new TableGeolocation(sqlite3, db));
 
   await sqlite3.exec(db, `SELECT * from geolocation`, (row, columns) => {
+    console.log(columns, row);
+  });
+  await sqlite3.exec(db, `SELECT * from os_version`, (row, columns) => {
     console.log(columns, row);
   });
   await sqlite3.close(db);
