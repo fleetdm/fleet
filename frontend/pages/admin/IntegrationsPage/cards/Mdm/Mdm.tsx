40c5cddf099e44af77e466cd9ef09540fecc7ead
Jacob Shandling <jacob@fleetdm.com>
2023-02-06 11:55:31 -0800

    fix

--- frontend/pages/admin/IntegrationsPage/cards/Mdm/Mdm.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/Mdm/Mdm.tsx
@@ -105,7 +105,7 @@ const Mdm = (): JSX.Element => {
 
       const privateFilename = "fleet-apple-mdm-bm-private.key";
       const privateFile = new global.window.File(
-        [keys.decodedPublic],
+        [keys.decodedPrivate],
         privateFilename,
         {
           type: "application/x-pem-file",
@@ -113,11 +113,13 @@ const Mdm = (): JSX.Element => {
       );
 
       FileSaver.saveAs(publicFile);
-      FileSaver.saveAs(privateFile);
+      setTimeout(() => {
+        FileSaver.saveAs(privateFile);
+      }, 100);
     } else {
       renderFlash(
         "error",
-        "Your MDM business manager keys could not be downloaded. Please TODO ACTION."
+        "Your MDM business manager keys could not be downloaded. Please try again."
       );
     }
     return false;
