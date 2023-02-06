416963de3670b30490c8385edcd4722552746cc9
Eric <eashaw@sailsjs.com>
2023-02-06 10:41:36 -0600

    Website: Update pricing page (#9621)
    Closes https://github.com/fleetdm/confidential/issues/1818
    
    Changes:
    - Updated the layout and content of the `/pricing` page
    - `pricing-features-table.yml`: Added an "Ultimate" tier with three
    features.
    - `build-static-content.js` - Updated the build script to add support
    for the "Ultimate" tier in `pricing-features-table.yml`
    
    ---------
    
    Co-authored-by: Mike Thomas <mthomas@fleetdm.com>

--- website/assets/js/pages/pricing.page.js
+++ website/assets/js/pages/pricing.page.js
@@ -3,7 +3,10 @@ parasails.registerPage('pricing', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    //…
+    formData: {},
+    estimatedCost: '', // For pricing calculator
+    estimatedUltimateCostPerHost: 7,
+    displaySecurityPricingMode: true, // For pricing mode switch
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -30,6 +33,20 @@ parasails.registerPage('pricing', {
       // > https://github.com/papercups-io/chat-widget/blob/master/src/index.tsx#L4-L6
       // > ~Dec 31, 2020
       window.dispatchEvent(new Event('papercups:open'));
-    }
+    },
+    updateEstimatedTotal: function() {
+      let total =
+      (7 * (this.formData.macos ? this.formData.macos : 0)) +
+      (7 * (this.formData.windows ? this.formData.windows : 0)) +
+      (7 * (this.formData.linux ? this.formData.linux : 0)) +
+      (1.50 * (this.formData.other ? this.formData.other : 0));
+      let totalNumberOfDevices =
+      (1 * (this.formData.macos ? this.formData.macos : 0)) +
+      (1 * (this.formData.windows ? this.formData.windows : 0)) +
+      (1 * (this.formData.linux ? this.formData.linux : 0)) +
+      (1 * (this.formData.other ? this.formData.other : 0));
+      this.estimatedCost = Number(total);
+      this.estimatedUltimateCostPerHost = Math.floor(this.estimatedCost / totalNumberOfDevices);
+    },
   }
 });
