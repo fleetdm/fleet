bd605e2b082d8137ac0a65a7c8b80d9acc9c1f86
Sharon Katz <sharon@fleetdm.com>
2023-02-06 12:40:43 -0500

    fix script

--- ee/cis/macos-13/test/scripts/CIS_3.4.sh
+++ ee/cis/macos-13/test/scripts/CIS_3.4.sh
@@ -2,7 +2,7 @@
 
 cp /etc/security/audit_control ./tmp.txt;
 origExpire=$(cat ./tmp.txt  | grep expire-after);
-sed "s/${origExpire}/expire-after:60d OR 1G/" ./tmp.txt > /etc/security/audit_control;
+sed "s/${origExpire}/expire-after:60d OR 5G/" ./tmp.txt > /etc/security/audit_control;
 rm ./tmp.txt;
 
 
