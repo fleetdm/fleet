84fcee9130a0ebff27ae9833ca981b6c38c77c83
Sharon Katz <121527325+sharon-fdm@users.noreply.github.com>
2023-02-07 13:26:05 -0500

    CIS 5.1.3+5.1.4 (#9642)

--- orbit/pkg/table/extension_darwin.go
+++ orbit/pkg/table/extension_darwin.go
@@ -3,6 +3,8 @@
 package table
 
 import (
+	"github.com/fleetdm/fleet/v4/orbit/pkg/table/csrutil_info"
+	"github.com/fleetdm/fleet/v4/orbit/pkg/table/nvram_info"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/privaterelay"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/pwd_policy"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/user_login_settings"
@@ -22,6 +24,8 @@ func platformTables() []osquery.OsqueryPlugin {
 		table.NewPlugin("icloud_private_relay", privaterelay.Columns(), privaterelay.Generate),
 		table.NewPlugin("user_login_settings", user_login_settings.Columns(), user_login_settings.Generate),
 		table.NewPlugin("pwd_policy", pwd_policy.Columns(), pwd_policy.Generate),
+		table.NewPlugin("csrutil_info", csrutil_info.Columns(), csrutil_info.Generate),
+		table.NewPlugin("nvram_info", nvram_info.Columns(), nvram_info.Generate),
 
 		// Macadmins extension tables
 		table.NewPlugin("filevault_users", filevaultusers.FileVaultUsersColumns(), filevaultusers.FileVaultUsersGenerate),
