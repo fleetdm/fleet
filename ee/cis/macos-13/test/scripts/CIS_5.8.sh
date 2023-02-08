e4e3512447c3389fedef62090960c859d9281835
Lucas Rodriguez <lucas@fleetdm.com>
2023-02-08 13:32:23 -0300

    Code review changes and update sql query

--- ee/cis/macos-13/test/scripts/CIS_5.8.sh
+++ ee/cis/macos-13/test/scripts/CIS_5.8.sh
@@ -2,4 +2,4 @@
 
 echo "Content of the banner" | sudo tee /Library/Security/PolicyBanner.txt
 /usr/bin/sudo /usr/sbin/chown root:wheel /Library/Security/PolicyBanner.txt
-/usr/bin/sudo /bin/chmod 0400 /Library/Security/PolicyBanner.txt
+/usr/bin/sudo /bin/chmod o+r /Library/Security/PolicyBanner.txt
