3a46a5adf17c93055458cb0c635670e7814e2046
Lucas Rodriguez <lucas@fleetdm.com>
2023-02-09 14:12:35 -0300

    Fix typo in table, integrity

--- orbit/pkg/table/extension_darwin.go
+++ orbit/pkg/table/extension_darwin.go
@@ -5,11 +5,8 @@ package table
 import (
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/authdb"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/csrutil_info"
-<<<<<<< HEAD
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/dscl"
-=======
-	firmware_eficheck_integity_check "github.com/fleetdm/fleet/v4/orbit/pkg/table/firmware_eficheck_integrity_check"
->>>>>>> main
+	"github.com/fleetdm/fleet/v4/orbit/pkg/table/firmware_eficheck_integrity_check"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/nvram_info"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/pmset"
 	"github.com/fleetdm/fleet/v4/orbit/pkg/table/privaterelay"
@@ -36,7 +33,7 @@ func platformTables() []osquery.OsqueryPlugin {
 		table.NewPlugin("authdb", authdb.Columns(), authdb.Generate),
 		table.NewPlugin("pmset", pmset.Columns(), pmset.Generate),
 		table.NewPlugin("sudo_info", sudo_info.Columns(), sudo_info.Generate),
-		table.NewPlugin("firmware_eficheck_integity_check", firmware_eficheck_integity_check.Columns(), firmware_eficheck_integity_check.Generate),
+		table.NewPlugin("firmware_eficheck_integrity_check", firmware_eficheck_integrity_check.Columns(), firmware_eficheck_integrity_check.Generate),
 		table.NewPlugin("dscl", dscl.Columns(), dscl.Generate),
 
 		// Macadmins extension tables
