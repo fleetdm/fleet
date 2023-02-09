6f2d0707903e1e4ba78b36a89c984d69008cda77
Sharon Katz <sharon@fleetdm.com>
2023-02-08 20:52:58 -0500

    unit tests

--- orbit/pkg/table/safari_options/safari_options_test.go
+++ orbit/pkg/table/safari_options/safari_options_test.go
@@ -4,21 +4,41 @@
 package safari_options
 
 import (
+	"github.com/osquery/osquery-go/plugin/table"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/net/context"
 	"testing"
+	"time"
 )
 
 func TestGenerate(t *testing.T) {
-	return
-	//ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-	//defer cancel()
-	//var tbl table.QueryContext
-	//tbl.Constraints.
-	//
-	//table, err := Generate(ctx, tbl)
-	//if err != nil {
-	//	t.Fatalf(`Expected no error. got %s`, err)
-	//}
-	//if table[0]["ssv_enabled"] != "0" && table[0]["ssv_enabled"] != "1" {
-	//	t.Fatalf(`ssvEnabled expected 0 or 1. got %s`, table[0]["ssvEnabled"])
-	//}
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	var myConstraintsList table.ConstraintList
+	var myConstraints []table.Constraint
+	var tbl table.QueryContext
+	tbl.Constraints = make(map[string]table.ConstraintList)
+
+	myConstraint := table.Constraint{2, "some-user"}
+	myConstraints = append(myConstraints, myConstraint)
+	myConstraintsList = table.ConstraintList{"TEXT", myConstraints}
+	tbl.Constraints["user_name"] = myConstraintsList
+
+	_, err := Generate(ctx, tbl)
+	require.Error(t, err)
+}
+
+func TestGetUserNameFromConstraints(t *testing.T) {
+	var myConstraintsList table.ConstraintList
+	var myConstraints []table.Constraint
+	var tbl table.QueryContext
+	tbl.Constraints = make(map[string]table.ConstraintList)
+
+	myConstraint := table.Constraint{2, "some-user"}
+	myConstraints = append(myConstraints, myConstraint)
+	myConstraintsList = table.ConstraintList{"TEXT", myConstraints}
+	tbl.Constraints["user_name"] = myConstraintsList
+
+	user := getUserNameFromConstraints(tbl)
+	require.Equal(t, "some-user", user)
 }
