500fcfed44211b1ff5888cd7a54dd2b1328c0f88
Martin Angers <martin.n.angers@gmail.com>
2023-02-14 16:15:29 -0500

    Add test for endpoint called without mdm enabled

--- server/service/integration_core_test.go
+++ server/service/integration_core_test.go
@@ -4510,6 +4510,11 @@ func (s *integrationTestSuite) TestPremiumEndpointsWithoutLicense() {
 	var appleBMResp getAppleBMResponse
 	s.DoJSON("GET", "/api/latest/fleet/mdm/apple_bm", nil, http.StatusPaymentRequired, &appleBMResp)
 	assert.Nil(t, appleBMResp.AppleBM)
+
+	// batch-apply a set of MDM profiles
+	res := s.Do("POST", "/api/latest/fleet/mdm/apple/profiles/batch", nil, http.StatusUnprocessableEntity)
+	errMsg := extractServerErrorText(res.Body)
+	require.Contains(t, errMsg, "Fleet MDM is not enabled")
 }
 
 // TestGlobalPoliciesBrowsing tests that team users can browse (read) global policies (see #3722).
