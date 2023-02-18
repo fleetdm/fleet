ec3c9b496eebcc6b5cb57878b2bd2f21ecb9a8b7
Eric <eashaw@sailsjs.com>
2023-02-17 19:06:23 -0600

    update error message

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -308,7 +308,7 @@ module.exports = {
         let mergeFreezeMainBranchStatusReport = await sails.helpers.http.get('https://www.mergefreeze.com/api/branches/fleetdm/fleet/main', { access_token: sails.config.custom.mergeFreezeAccessToken }) //eslint-disable-line camelcase
         .tolerate(['non200Response'], (err)=>{
           // If the MergeFreeze API returns a non 200 response, log a warning and continue under the assumption that the main branch is not frozen.
-          sails.log.warn('When sending a request to the MergeFreeze API to get the status of the main branch, the API returned a non-200 Response. If the main branch is frozen, it will need to be manually unfrozen for PR #'+prNumber+' to be merged. Raw error: '+err.raw);
+          sails.log.warn('When sending a request to the MergeFreeze API to get the status of the main branch, the API returned a non-200 Response. If the main branch is frozen, it will need to be manually unfrozen before PR #'+prNumber+' can be merged. Raw error: '+err.raw);
           return { frozen: false };
         });
         sails.log('#'+prNumber+' is under consideration...  The MergeFreeze API claims that it current main branch "frozen" status is:',mergeFreezeMainBranchStatusReport.frozen);
