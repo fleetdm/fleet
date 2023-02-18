fdae7306505295e7eed3ecb3040fc0e44ab6fd4e
Eric <eashaw@sailsjs.com>
2023-02-17 18:57:38 -0600

    update comment

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -307,7 +307,7 @@ module.exports = {
         // [?] https://docs.mergefreeze.com/web-api#get-freeze-status
         let mergeFreezeMainBranchStatusReport = await sails.helpers.http.get('https://www.mergefreeze.com/api/branches/fleetdm/fleet/main', { access_token: sails.config.custom.mergeFreezeAccessToken }) //eslint-disable-line camelcase
         .tolerate(['non200Response'], (err)=>{
-          // If the MergeFreeze API returns a non 200 response, log a warning pretend that the main branch is frozen.
+          // If the MergeFreeze API returns a non 200 response, log a warning and continue under the assumption that the main branch is frozen.
           sails.log.warn('When sending a request to the MergeFreeze API to get the status of the main branch, the API returned a non-200 Response. If the main branch is frozen, it will need to be manually unfrozen for PR #'+prNumber+' to be merged. Raw error: '+err.raw);
           return {frozen: false};
         });
