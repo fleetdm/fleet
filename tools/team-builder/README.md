2ba2e5c6c39b0e2dc8e6fd64a3b6986d8120e678
Kathy Satterlee <me@ksatter.com>
2023-02-23 16:58:11 -0600

    Add team builder script

---
+++ tools/team-builder/README.md
@@ -0,0 +1,89 @@
+
+# Fleet Team Builder
+
+Using a list of teams in a file as input, adds the listed teams to Fleet and generates installer processes. 
+
+For each team, an enroll secret will be created, the team added to Fleet using the team yaml template, and `.msi`,`.deb`, `.pkg` and `.rpm` installer packages will be created. 
+
+## Requirements
+
+fleetctl installed and logged in
+
+## Flags 
+
+Required flags:
+
+- -s: The source file containing teams to be added. 
+- -u: The url of the Fleet server.
+
+Optional flags:
+
+- -h: header - Default: false - Indicates that provided csv file has a header defining columns on the first line.
+- -c: columns - list the columns included in the CSV file. For use when header is not included.
+- -n: name_column - Default: "Name" - The column that contains the team name.
+- -p: packages - Default: "all" - The types of installer packages to create for each team.
+- -f: flags - Additional flags to apply to `fleetctl package`.
+- -o: output - Default: Current location - Directory in which to place the generated packages.
+- -x: dry_run - Test prossesing the file, creating the team in Fleet, and generating packages without applying any changes to the server.
+
+## Usage
+
+### Basic
+
+Create a file including a list of teams, one per line:
+
+```
+Workstation
+Canary
+Servers
+
+```
+Run the script and pass the Fleet Server URL and source file as arguments:
+
+```console
+$ ./build_teams.sh -s example.txt -u fleet.org.com
+```
+
+### Advanced 
+
+Describe the contents of an existing .csv file to import.  
+
+#### CSV With a header line
+ 
+Indicate that a header is present with `-h true` and indicate which column contains the team name using `-n`:
+
+```console
+$ ./build_teams.sh -s test.csv -u fleet.org.com -h true -n Name
+```
+
+The first line of the source file will be parsed to pull out the columns present and the team name will be extracted from the indicated column.
+
+#### CSV Without a header line
+
+Define the columns present in the file using `-c` and indicate the column that contains the team name with `-n`:
+
+```console
+$ ./build_teams.sh -s test.csv -u fleet.org.com -c "Name Code" -n Name    
+```
+
+## Team configuration
+
+By default, the template stored in `team_config.yml` will be applied. This file only creates the team and adds a randomly generated enroll secret. The team will inherit global agent options. You can modify the file to include addional options.
+
+If you modify the agent options, please note the placeholders for name and secret, these are necessary for the appropriate vales generated in the script to be applied
+
+```yml   
+    name: ${name}
+    secrets: 
+      - secret: ${secret}
+```
+
+> If you modify the configuration file, please test the script using `-x` before applying changes.
+
+## Testing
+
+To test team creation and package generation without applying the changes to Fleet, include the `-x` flag. This will add the `--dry_run` flag to `fleetctl apply`. All actions will be taken, but the generated team configuration YAML will be validated without creating the new team. 
+
+## TODO:
+
+- Add logic to check if a team with that name already exists
---
+++ tools/team-builder/build_teams.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+run(){
+	
+	local OPTIND
+
+  #default values
+	output="fleet_osquery_packages"
+  header=false
+	flags+="--disable-open-folder"
+
+  #Read flags
+	while getopts s:hc:n:p:u:f:d:o:x flag
+	 do
+		case "${flag}" in
+			s) #path to csv file containing team names. Must end with newline char.
+				source=($OPTARG);;
+			h) #indicates whether there is a header included in the file. default false;
+				header=true;;
+			c) #columns in csv file. if not provided, assumes a simple list of names separated by line breaks. not needed if header is provided.
+        set -f 
+        IFS=',' 
+        columns+=($OPTARG) ;; # use the split+glob operator
+			n) #column to use as team name
+				name_column=($OPTARG);;
+			p) #types of installers to create. Pass an individual flag for each type
+				types+=($OPTARG);;
+			u) #Fleet server url
+				url=($OPTARG);;
+			f) #Additional flags to apply to `fleetctl package`
+				flags+=($OPTARG);;
+			d) #include Fleet Desktop
+				flags+="--desktop";;
+			o) #Directory for created packages
+		    output=($OPTARG);;
+      x) #Dry run
+        dry_run="--dry-run";;
+		esac
+	 done
+
+
+	#Verify that passed file exists
+	if !(test -f $source)
+		then
+			echo "Source file not found"
+			return
+  fi
+
+  #Set up for simple file with single column and no header
+  if [[ (-z $columns ) && ($header == false)]]
+	 	then 
+    echo "here"
+		columns="name"
+    name_column="name"
+    echo ${columns[@]}
+    echo $name_column
+	fi
+
+  #Set up output directory
+	if !(test -d $output)
+		then
+				mkdir $output
+	fi
+
+  create_teams
+}
+
+create_teams(){
+   
+  #If a header is provided, parse the first line of the file to retreive columns and skip first line of file when creating teams
+  if ($header == true )
+	  then
+	    i=1
+		  columns=($(head -n1 $source | tr , '\n'))
+	  else
+	    i=0
+	fi
+
+  #Loop over file contents and generate a secret for each team, then create the team and generate packages
+	while IFS=",", read -r ${columns[@]}
+		do
+		  test $i -eq 1 && ((i=i+1)) && continue
+
+		  secret=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/random | head -c 24);
+		  name=${!name_column}
+
+		  create_team
+		  generate_packages
+		done < $source
+}
+
+create_team(){
+
+  #Generate yml based on template provided
+
+	( echo "cat <<EOF >final.yml";
+	cat team_config.yml;
+	) >temp.yml
+	. temp.yml
+
+  # Apply the new team to fleet
+	echo "Adding $name team to Fleet"
+	fleetctl apply -f final.yml $dry_run
+	rm -f final.yml temp.yml
+}
+
+generate_packages(){
+
+	echo "Generating installers for $name"  
+
+  #Set up directory to hold installers for this team
+  name_formatted=$(printf "$name" | tr '[:upper:]' '[:lower:]' | tr -s ' ' | tr ' ' '_')
+	team_dir=$output/$name_formatted
+  mkdir $team_dir
+
+  #set up variables for navigation into and back out of team directory
+  cwd=$(pwd)
+  cd $team_dir
+
+  #In the team directory, create a package for each specified type
+	for type in ${packages[@]}
+  do
+		(cd -- "$teamdir" && fleetctl package --type=$type --fleet-url=$url --enroll-secret=$secret ${flags[@]})
+	done
+  		find . -type f -name 'fleet-osquery*' -exec mv -f {} fleet_osquery_$name_formatted.$type ';'
+  
+  #Return to current location
+  cd $cwd
+}
+
+run "$@"
+
+
