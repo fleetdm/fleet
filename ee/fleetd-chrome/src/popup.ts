535fcc7e89e7fec3c58a1e75abdd02eab510738d
Zach Wasserman <zach@fleetdm.com>
2023-02-24 18:33:49 -0800

    Move all files to src directory

---
+++ ee/fleetd-chrome/src/popup.ts
@@ -0,0 +1,1 @@
+console.log("in popup");
--- ee/fleetd-chrome/webpack.common.js
+++ ee/fleetd-chrome/webpack.common.js
@@ -1,10 +1,13 @@
 import { resolve as _resolve, dirname } from "path";
 import { fileURLToPath } from "url";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const entry = {
   background: "./src/background.ts",
+  popup: "./src/popup.ts",
 };
 export const module = {
   rules: [
@@ -18,8 +21,23 @@ export const module = {
 export const resolve = {
   extensions: [".tsx", ".ts", ".js"],
 };
+export const plugins = [
+  new CopyWebpackPlugin({
+    patterns: [
+      { from: "./src/manifest.json" },
+      { from: "./src/schema.json" },
+      { from: "./src/icons" },
+    ],
+  }),
+  new HtmlWebpackPlugin({
+    template: "./src/popup.html",
+    filename: "popup.html",
+    chunks: ["popup"],
+  }),
+];
 export const output = {
   filename: "[name].bundle.js",
-  path: _resolve(__dirname, "dist", "js"),
+  path: _resolve(__dirname, "dist"),
   chunkFormat: "commonjs",
+  clean: true,
 };
