68457b85020942cf8d40252da0a5293c3ab30e22
Lucas Rodriguez <lucas@fleetdm.com>
2023-02-28 18:17:10 -0300

    Allow set user roles during JIT provisioning

--- ee/server/service/users.go
+++ ee/server/service/users.go
@@ -29,6 +29,39 @@ func (svc *Service) GetSSOUser(ctx context.Context, auth fleet.Auth) (*fleet.Use
 	var nfe fleet.NotFoundError
 	switch {
 	case err == nil:
+		// If the user exists, we want to update the user roles from the attributes received
+		// in the SAMLResponse.
+
+		// If JIT provisioning is disabled, then we don't attempt to change the role of the user.
+		if !config.SSOSettings.EnableJITProvisioning {
+			return user, nil
+		}
+
+		newGlobalRole, newTeamRoles, err := svc.userRolesFromSSOAttributes(ctx, auth)
+		if err != nil {
+			return nil, ctxerr.Wrap(ctx, err, "user roles from SSO attributes")
+		}
+		oldGlobalRole := user.GlobalRole
+		oldTeamRoles := user.Teams
+
+		// rolesChanged assumes that there cannot be multiple role entries for the same team,
+		// which is ok because the "old" values comes from the database and the "new" values
+		// come from fleet.RolesFromSSOAttributes which already checks for duplicates.
+		if !rolesChanged(oldGlobalRole, oldTeamRoles, newGlobalRole, newTeamRoles) {
+			// Roles haven't changed, so nothing to do.
+			return user, nil
+		}
+
+		user.GlobalRole = newGlobalRole
+		user.Teams = newTeamRoles
+
+		err = svc.ds.SaveUser(ctx, user)
+		if err != nil {
+			return nil, ctxerr.Wrap(ctx, err, "save user")
+		}
+		if err := fleet.LogRoleChangeActivities(ctx, svc.ds, user, oldGlobalRole, oldTeamRoles, user); err != nil {
+			return nil, ctxerr.Wrap(ctx, err, "log activities for role change")
+		}
 		return user, nil
 	case errors.As(err, &nfe):
 		if !config.SSOSettings.EnableJITProvisioning {
@@ -43,11 +76,18 @@ func (svc *Service) GetSSOUser(ctx context.Context, auth fleet.Auth) (*fleet.Use
 		displayName = auth.UserID()
 	}
 
+	// Retrieve (if set) user roles from SAML custom attributes.
+	globalRole, teamRoles, err := svc.userRolesFromSSOAttributes(ctx, auth)
+	if err != nil {
+		return nil, ctxerr.Wrap(ctx, err, "user roles from SSO attributes")
+	}
+
 	user, err = svc.Service.NewUser(ctx, fleet.UserPayload{
 		Name:       &displayName,
 		Email:      ptr.String(auth.UserID()),
 		SSOEnabled: ptr.Bool(true),
-		GlobalRole: ptr.String(fleet.RoleObserver),
+		GlobalRole: globalRole,
+		Teams:      &teamRoles,
 	})
 	if err != nil {
 		return nil, ctxerr.Wrap(ctx, err, "creating new SSO user")
@@ -60,4 +100,56 @@ func (svc *Service) GetSSOUser(ctx context.Context, auth fleet.Auth) (*fleet.Use
 		return nil, ctxerr.Wrap(ctx, err, "create activity for SSO user creation")
 	}
 	return user, nil
+}
+
+// rolesChanged checks whether there was any change between the old and new roles.
+//
+// rolesChanged assumes that there cannot be multiple role entries for the same team.
+func rolesChanged(oldGlobal *string, oldTeams []fleet.UserTeam, newGlobal *string, newTeams []fleet.UserTeam) bool {
+	if (newGlobal != nil && (oldGlobal == nil || *oldGlobal != *newGlobal)) || (newGlobal == nil && oldGlobal != nil) {
+		return true
+	}
+	if len(oldTeams) != len(newTeams) {
+		return true
+	}
+	oldTeamsMap := make(map[uint]fleet.UserTeam)
+	for _, oldTeam := range oldTeams {
+		oldTeamsMap[oldTeam.Team.ID] = oldTeam
+	}
+	for _, newTeam := range newTeams {
+		oldTeam, ok := oldTeamsMap[newTeam.Team.ID]
+		if !ok {
+			return true
+		}
+		if oldTeam.Role != newTeam.Role {
+			return true
+		}
+	}
+	return false
+}
+
+// userRolesFromSSOAttributes loads the global or team roles from custom SSO attributes.
+//
+// The returned `globalRole` and `teamRoles` are ready to be assigned to
+// `fleet.User` struct fields `GlobalRole` and `Teams` respectively.
+//
+// If the custom attributes are not found, then the default global observer ("observer", nil, nil) is returned.
+func (svc *Service) userRolesFromSSOAttributes(ctx context.Context, auth fleet.Auth) (globalRole *string, teamRoles []fleet.UserTeam, err error) {
+	ssoRoleInfo, err := fleet.RolesFromSSOAttributes(auth.AssertionAttributes())
+	if err != nil {
+		return nil, nil, ctxerr.Wrap(ctx, err, "invalid SSO attributes")
+	}
+
+	for _, teamRole := range ssoRoleInfo.Teams {
+		team, err := svc.ds.Team(ctx, teamRole.ID)
+		if err != nil {
+			return nil, nil, ctxerr.Wrap(ctx, err, "invalid team")
+		}
+		teamRoles = append(teamRoles, fleet.UserTeam{
+			Team: *team,
+			Role: teamRole.Role,
+		})
+	}
+
+	return ssoRoleInfo.Global, teamRoles, nil
 }
--- server/fleet/sessions.go
+++ server/fleet/sessions.go
@@ -1,7 +1,13 @@
 package fleet
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/fleetdm/fleet/v4/server/ptr"
 )
 
 // Auth contains methods to fetch information from a valid SSO auth response
@@ -19,6 +25,22 @@ type Auth interface {
 	UserDisplayName() string
 	// RequestID returns the request id associated with this SSO session
 	RequestID() string
+	// AssertionAttributes returns the attributes of the SAML response.
+	AssertionAttributes() []SAMLAttribute
+}
+
+// SAMLAttribute holds the name and values of a custom attribute.
+type SAMLAttribute struct {
+	Name   string
+	Values []SAMLAttributeValue
+}
+
+// SAMLAttributeValue holds the type and value of a custom attribute.
+type SAMLAttributeValue struct {
+	// Type is the type of attribute value.
+	Type string
+	// Value is the actual value of the attribute.
+	Value string
 }
 
 type SSOSession struct {
@@ -48,4 +70,105 @@ type Session struct {
 
 func (s Session) AuthzType() string {
 	return "session"
+}
+
+// SSORolesInfo holds the configuration parsed from SAML custom attributes.
+//
+// `Global` and `Teams` are never both set (by design, users must be either global
+// or member of teams).
+type SSORolesInfo struct {
+	// Global holds the role for the Global domain.
+	Global *string
+	// Teams holds the roles for teams.
+	Teams []TeamRole
+}
+
+// TeamRole holds a user's role on a Team.
+type TeamRole struct {
+	// ID is the unique identifier of the team.
+	ID uint
+	// Role is the role of the user in the team.
+	Role string
+}
+
+func (s SSORolesInfo) verify() error {
+	if s.Global != nil && len(s.Teams) > 0 {
+		return errors.New("cannot set both global and team roles")
+	}
+	// Check for duplicate entries for the same team.
+	// This is just in case some IdP allows duplicating attributes.
+	teamMap := make(map[uint]struct{})
+	for _, teamRole := range s.Teams {
+		if _, ok := teamMap[teamRole.ID]; ok {
+			return fmt.Errorf("duplicate team entry: %d", teamRole.ID)
+		}
+		teamMap[teamRole.ID] = struct{}{}
+	}
+	return nil
+}
+
+func (s SSORolesInfo) empty() bool {
+	return s.Global == nil && len(s.Teams) == 0
+}
+
+const (
+	globalUserRoleSSOAttrName     = "FLEET_JIT_USER_ROLE_GLOBAL"
+	teamUserRoleSSOAttrNamePrefix = "FLEET_JIT_USER_ROLE_TEAM_"
+)
+
+// RolesFromSSOAttributes loads Global and Team roles from SAML custom attributes.
+//   - Custom attribute `FLEET_JIT_USER_ROLE_GLOBAL` is used for setting global role.
+//   - Custom attributes of the form `FLEET_JIT_USER_ROLE_TEAM_<TEAM_ID>` are used
+//     for setting role for a team with ID <TEAM_ID>.
+//
+// For both attributes currently supported values are `admin`, `maintainer` and `observer`
+func RolesFromSSOAttributes(attributes []SAMLAttribute) (SSORolesInfo, error) {
+	ssoRoleInfo := SSORolesInfo{}
+	for _, attribute := range attributes {
+		switch {
+		case attribute.Name == globalUserRoleSSOAttrName:
+			role, err := parseRole(attribute.Values)
+			if err != nil {
+				return SSORolesInfo{}, fmt.Errorf("parse global role: %w", err)
+			}
+			ssoRoleInfo.Global = ptr.String(role)
+		case strings.HasPrefix(attribute.Name, teamUserRoleSSOAttrNamePrefix):
+			teamIDSuffix := strings.TrimPrefix(attribute.Name, teamUserRoleSSOAttrNamePrefix)
+			teamID, err := strconv.ParseUint(teamIDSuffix, 10, 64)
+			if err != nil {
+				return SSORolesInfo{}, fmt.Errorf("parse team ID: %w", err)
+			}
+			teamRole, err := parseRole(attribute.Values)
+			if err != nil {
+				return SSORolesInfo{}, fmt.Errorf("parse team role: %w", err)
+			}
+			ssoRoleInfo.Teams = append(ssoRoleInfo.Teams, TeamRole{
+				ID:   uint(teamID),
+				Role: teamRole,
+			})
+		default:
+			continue
+		}
+	}
+	if err := ssoRoleInfo.verify(); err != nil {
+		return SSORolesInfo{}, err
+	}
+	if ssoRoleInfo.empty() {
+		// When the configuration is not set, the default is to
+		// make the user a global observer.
+		return SSORolesInfo{Global: ptr.String(RoleObserver)}, nil
+	}
+	return ssoRoleInfo, nil
+}
+
+func parseRole(values []SAMLAttributeValue) (string, error) {
+	if len(values) == 0 {
+		return "", errors.New("empty role")
+	}
+	// Using last value by default.
+	value := values[len(values)-1].Value
+	if value != RoleAdmin && value != RoleMaintainer && value != RoleObserver {
+		return "", fmt.Errorf("invalid role: %s", value)
+	}
+	return value, nil
 }
