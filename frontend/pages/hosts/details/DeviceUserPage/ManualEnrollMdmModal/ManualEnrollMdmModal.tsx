d13308e6cc996490a499973e4203bb1b982a78d1
Jacob Shandling <jacob@fleetdm.com>
2023-03-01 15:04:20 -0800

    Update copy

--- frontend/pages/hosts/details/DeviceUserPage/ManualEnrollMdmModal/ManualEnrollMdmModal.tsx
+++ frontend/pages/hosts/details/DeviceUserPage/ManualEnrollMdmModal/ManualEnrollMdmModal.tsx
@@ -97,7 +97,7 @@ const ManualEnrollMdmModal = ({
               </Button>
             )}
           </li>
-          <li>Click on the profile you just downloaded.</li>
+          <li>Open the profile you just downloaded.</li>
           <li>
             From the Apple menu in the top left corner of your screen, select{" "}
             <b>System Settings</b> or <b>System Preferences</b>.
