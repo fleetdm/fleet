c29df2d682b99ea0e0ae06a2be92962fb04a5035
Eric <eashaw@sailsjs.com>
2023-03-01 16:04:37 -0600

    create /demo/okta-webflow

---
+++ website/api/controllers/demo/view-okta-webflow.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View okta webflow',
+
+
+  description: 'Display "Okta webflow" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/demo/okta-webflow'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
