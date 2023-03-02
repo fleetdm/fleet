abf8d5f22a6a1b258e761190a9907c9f4e0072f5
Eric <eashaw@sailsjs.com>
2023-03-01 18:23:16 -0600

    revert change from #10207

--- website/api/controllers/download-sitemap.js
+++ website/api/controllers/download-sitemap.js
@@ -65,7 +65,6 @@ module.exports = {
       '/deploy',
       '/podcasts',
       '/device-management',
-      '/osquery-management',
       // FUTURE: Do something smarter to get hand-coded HTML pages from routes.js, like how rebuild-cloud-sdk works, to avoid this manual duplication.
       // See also https://github.com/sailshq/sailsjs.com/blob/b53c6e6a90c9afdf89e5cae00b9c9dd3f391b0e7/api/helpers/get-pages-for-sitemap.js#L27
     ];
