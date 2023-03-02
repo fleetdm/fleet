6020b1ce38278d0371782d571f2a62460d981bdb
Zach Wasserman <zach@fleetdm.com>
2023-03-02 14:31:09 -0800

    Version work

--- ee/fleetd-chrome/src/tables/os_version.ts
+++ ee/fleetd-chrome/src/tables/os_version.ts
@@ -13,6 +13,14 @@ export default class TableOSVersion extends Table {
       "fullVersionList",
     ]);
 
+    let version = "";
+    for (let entry of data.fullVersionList) {
+      if (entry.brand === "Google Chrome") {
+        version = entry.version;
+        break;
+      }
+    }
+
     // Note we can actually get the platform of Chrome running on non-ChromeOS devices, but instead
     // we just hardcode to "chrome" so that Fleet always sees this Chrome extension as a Chrome
     // device even when we are doing local dev on a non-ChromeOS machine.
@@ -24,7 +32,7 @@ export default class TableOSVersion extends Table {
         name: data.platform,
         platform: "chrome",
         platform_like: "chrome",
-        version: data.platformVersion,
+        version,
         build: data.platformVersion,
         arch: arch,
       },
