7c3a281c23794a3c5cc79bfff0f8e91bc9a40327
Roberto Dip <me@roperzh.com>
2023-03-07 10:57:26 -0300

    add schema tables to support DEP Okta flow (#10290)
    For #10271 and #10273, this adds the underlying table that will support
    the Okta DEP flow.

---
+++ server/datastore/mysql/migrations/tables/20230303135738_AddMDMIdPAccountsTable.go
@@ -0,0 +1,27 @@
+package tables
+
+import (
+	"database/sql"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20230303135738, Down_20230303135738)
+}
+
+func Up_20230303135738(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+    CREATE TABLE mdm_idp_accounts (
+      uuid         varchar(255) NOT NULL,
+      username     varchar(255) NOT NULL,
+      salt         varchar(255) NOT NULL,
+      entropy      varchar(255) NOT NULL,
+      iterations   int unsigned NOT NULL,
+    
+      PRIMARY KEY (uuid)
+    )`)
+	return err
+}
+
+func Down_20230303135738(tx *sql.Tx) error {
+	return nil
+}
