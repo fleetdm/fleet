6538fa572a609da48c7ca7427f3f324d88138baa
Lucas Rodriguez <lucas@fleetdm.com>
2023-03-16 11:14:01 -0300

    Uninstalling software in a host also updates `software` table

--- server/datastore/mysql/software.go
+++ server/datastore/mysql/software.go
@@ -200,23 +200,39 @@ func deleteUninstalledHostSoftwareDB(
 	currentMap map[string]fleet.Software,
 	incomingMap map[string]fleet.Software,
 ) error {
-	var deletesHostSoftware []interface{}
-	deletesHostSoftware = append(deletesHostSoftware, hostID)
-
+	var deletesHostSoftware []uint
 	for currentKey, curSw := range currentMap {
 		if _, ok := incomingMap[currentKey]; !ok {
 			deletesHostSoftware = append(deletesHostSoftware, curSw.ID)
 		}
 	}
-	if len(deletesHostSoftware) <= 1 {
+	if len(deletesHostSoftware) == 0 {
 		return nil
 	}
-	sql := fmt.Sprintf(
-		`DELETE FROM host_software WHERE host_id = ? AND software_id IN (%s)`,
-		strings.TrimSuffix(strings.Repeat("?,", len(deletesHostSoftware)-1), ","),
-	)
-	if _, err := tx.ExecContext(ctx, sql, deletesHostSoftware...); err != nil {
+
+	stmt := `DELETE FROM host_software WHERE host_id = ? AND software_id IN (?);`
+	stmt, args, err := sqlx.In(stmt, hostID, deletesHostSoftware)
+	if err != nil {
+		return ctxerr.Wrap(ctx, err, "build delete host software query")
+	}
+	if _, err := tx.ExecContext(ctx, stmt, args...); err != nil {
 		return ctxerr.Wrap(ctx, err, "delete host software")
+	}
+
+	// Cleanup the software table when no more hosts have the deleted host_software
+	// table entries.
+	// Otherwise the software will be listed by ds.ListSoftware but ds.SoftwareByID,
+	// ds.CountHosts and ds.ListHosts will return a *notFoundError error for such
+	// software.
+	stmt = `DELETE FROM software WHERE id IN (?) AND id NOT IN (
+		SELECT DISTINCT software_id FROM host_software WHERE software_id IN (?)
+	);`
+	stmt, args, err = sqlx.In(stmt, deletesHostSoftware, deletesHostSoftware)
+	if err != nil {
+		return ctxerr.Wrap(ctx, err, "build delete software query")
+	}
+	if _, err := tx.ExecContext(ctx, stmt, args...); err != nil {
+		return ctxerr.Wrap(ctx, err, "delete software")
 	}
 
 	return nil
