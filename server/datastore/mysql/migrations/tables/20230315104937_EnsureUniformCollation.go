305392e7bb809bb04a340800e18b740ef133aaaf
Roberto Dip <me@roperzh.com>
2023-03-16 15:49:24 -0300

    enforce an uniform collation for all tables (#10515)
    related to #10441, inspired by the prior work done in
    https://github.com/kolide/fleet/pull/1360, this PR:
    
    1. Adds a migration to use `utf8mb4_general_ci` as the default collation
    for the database and all the tables. From [MySQL's documentation][1]:
    
    > To change the table default character set and all character columns
        > (CHAR, VARCHAR, TEXT) to a new character set, use a statement like
        > this:
        >
        > ```
        > ALTER TABLE tbl_name CONVERT TO CHARACTER SET charset_name;
        > ```
    > The statement also changes the collation of all character columns. If
    > you specify no COLLATE clause to indicate which collation to use, the
        > statement uses default collation for the character set.
    
    2. Changes the connection settings to use `utf8mb4_general_ci` as the
    default collation, from the [driver docs][2]:
    
       > Sets the collation used for client-server interaction on
    connection. In contrast to charset, collation does not issue additional
    queries. If the specified collation is unavailable on the target server,
    the connection will fail.
    
    [1]: https://dev.mysql.com/doc/refman/5.7/en/alter-table.html
    [2]: https://github.com/go-sql-driver/mysql
    
    
    **TODO:** discuss how we can enforce this, is setting the database
    default collation enough? should we add some kind of custom lint rule to
    all migrations?
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [x] Changes file added for user-visible changes in `changes/` or
    `orbit/changes/`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [x] Added/updated tests
    - [x] Manual QA for all new/changed functionality

---
+++ server/datastore/mysql/migrations/tables/20230315104937_EnsureUniformCollation.go
@@ -0,0 +1,116 @@
+package tables
+
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+	"text/template"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20230315104937, Down_20230315104937)
+}
+
+// changeCollation changes the default collation set of the database and all
+// table to the provided collation
+//
+// This is based on the changeCharacterSet function that's included in this
+// module and part of the 20170306075207_UseUTF8MB migration.
+func changeCollation(tx *sql.Tx, charset string, collation string) (err error) {
+	_, err = tx.Exec(fmt.Sprintf("ALTER DATABASE DEFAULT CHARACTER SET `%s` COLLATE `%s`", charset, collation))
+	if err != nil {
+		return fmt.Errorf("alter database: %w", err)
+	}
+
+	txx := sqlx.Tx{Tx: tx}
+
+	var names []string
+	err = txx.Select(&names, `
+          SELECT table_name
+          FROM information_schema.TABLES AS T, information_schema.COLLATION_CHARACTER_SET_APPLICABILITY AS C
+          WHERE C.collation_name = T.table_collation
+          AND T.table_schema = (SELECT database())
+          AND (C.CHARACTER_SET_NAME != ? OR C.COLLATION_NAME != ?)
+	  -- exclude tables that have columns with specific collations
+	  AND table_name NOT IN ('hosts', 'enroll_secrets')`, charset, collation)
+	if err != nil {
+		return fmt.Errorf("selecting tables: %w", err)
+	}
+
+	// disable foreign checks before changing the collations, otherwise the
+	// migration might fail. These are re-enabled after we're done.
+	defer func() {
+		if _, execErr := tx.Exec("SET FOREIGN_KEY_CHECKS = 1"); execErr != nil {
+			err = fmt.Errorf("re-enabling foreign key checks: %w", err)
+		}
+	}()
+	if _, err := tx.Exec("SET FOREIGN_KEY_CHECKS = 0"); err != nil {
+		return fmt.Errorf("disabling foreign key checks: %w", err)
+	}
+	for _, name := range names {
+		_, err = tx.Exec(fmt.Sprintf("ALTER TABLE `%s` CONVERT TO CHARACTER SET `%s` COLLATE `%s`", name, charset, collation))
+		if err != nil {
+			return fmt.Errorf("alter table %s: %w", name, err)
+		}
+	}
+
+	// enroll secrets was intentionally excluded because it contains only
+	// one "text" column, and that column contains a specific collation.
+	//
+	// note the use of DEFAULT below to indicate that new columns will
+	// contain the desired collation unless explicitly stated.
+	_, err = tx.Exec(fmt.Sprintf("ALTER TABLE enroll_secrets DEFAULT CHARACTER SET `%s` COLLATE `%s`", charset, collation))
+	if err != nil {
+		return fmt.Errorf("alter table enroll_secrets: %w", err)
+	}
+
+	// `hosts` was intentionally excluded, change the collation of all
+	// "text" columns except for `node_key` and `orbit_node_key`
+	//
+	// note the use of DEFAULT below to indicate that new columns will
+	// contain the desired collation unless explicitly stated.
+	tmpl := template.Must(template.New("").Parse(`
+	        ALTER TABLE hosts DEFAULT CHARACTER SET {{ .Cs }} COLLATE {{ .Co }},
+		MODIFY osquery_host_id   varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} DEFAULT NULL,
+		MODIFY hostname          varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY uuid              varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY platform          varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY osquery_version   varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY os_version        varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY build             varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY platform_like     varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY code_name         varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY cpu_type          varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY cpu_subtype       varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY cpu_brand         varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY hardware_vendor   varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY hardware_model    varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY hardware_version  varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY hardware_serial   varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY computer_name     varchar(255) CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY primary_ip        varchar(45)  CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY primary_mac       varchar(17)  CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT '',
+		MODIFY public_ip         varchar(45)  CHARACTER SET {{ .Cs }} COLLATE {{ .Co }} NOT NULL DEFAULT ''`))
+	var stmt bytes.Buffer
+	if err := tmpl.Execute(&stmt, map[string]string{"Cs": charset, "Co": collation}); err != nil {
+		return fmt.Errorf("executing template to alter hosts: %w", err)
+	}
+	if _, err = tx.Exec(stmt.String()); err != nil {
+		return fmt.Errorf("alter table hosts: %w", err)
+	}
+	return err
+}
+
+func Up_20230315104937(tx *sql.Tx) error {
+	// while newer versions of MySQL default to utf8mb4_0900_ai_ci, we
+	// still need to support 5.7, so we choose utf8mb4_unicode_ci for more
+	// details on the rationale, see:
+	// https://github.com/fleetdm/fleet/pull/10515#discussion_r1137611693
+	return changeCollation(tx, "utf8mb4", "utf8mb4_unicode_ci")
+}
+
+func Down_20230315104937(tx *sql.Tx) error {
+	return nil
+}
