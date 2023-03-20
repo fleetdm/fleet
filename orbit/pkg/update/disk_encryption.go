58276c51b46b9e8665632d067cf5ac1bc18cc73d
Roberto Dip <dip.jesusr@gmail.com>
2023-03-20 14:26:25 -0300

    fixes from @mna's code review

--- orbit/pkg/update/disk_encryption.go
+++ orbit/pkg/update/disk_encryption.go
@@ -1,6 +1,8 @@
 package update
 
 import (
+	"sync/atomic"
+
 	"github.com/fleetdm/fleet/v4/orbit/pkg/useraction"
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/rs/zerolog/log"
@@ -9,28 +11,26 @@ import (
 const maxRetries = 2
 
 type DiskEncryptionRunner struct {
-	Fetcher OrbitConfigFetcher
-
-	isIdle bool
+	fetcher   OrbitConfigFetcher
+	isRunning atomic.Bool
 }
 
-func ApplyDiskEncryptionRunnerMiddleware(f OrbitConfigFetcher) DiskEncryptionRunner {
-	return DiskEncryptionRunner{Fetcher: f}
+func ApplyDiskEncryptionRunnerMiddleware(f OrbitConfigFetcher) *DiskEncryptionRunner {
+	return &DiskEncryptionRunner{fetcher: f}
 }
 
-func (d DiskEncryptionRunner) GetConfig() (*fleet.OrbitConfig, error) {
-	cfg, err := d.Fetcher.GetConfig()
+func (d *DiskEncryptionRunner) GetConfig() (*fleet.OrbitConfig, error) {
+	cfg, err := d.fetcher.GetConfig()
 	if err != nil {
 		log.Info().Err(err).Msg("calling GetConfig from DiskEncryptionFetcher")
 		return nil, err
 	}
 
-	log.Debug().Msgf("running disk encryption fetcher middleware, notification: %v, isIdle: %v", cfg.Notifications.RotateDiskEncryptionKey, d.isIdle)
+	log.Debug().Msgf("running disk encryption fetcher middleware, notification: %v, isIdle: %v", cfg.Notifications.RotateDiskEncryptionKey, d.isRunning.Load())
 
-	if cfg.Notifications.RotateDiskEncryptionKey && !d.isIdle {
-		d.isIdle = true
+	if cfg.Notifications.RotateDiskEncryptionKey && !d.isRunning.Swap(true) {
 		go func() {
-			defer func() { d.isIdle = false }()
+			defer d.isRunning.Store(false)
 			if err := useraction.RotateDiskEncryptionKey(maxRetries); err != nil {
 				log.Error().Err(err).Msg("rotating encryption key")
 			}
