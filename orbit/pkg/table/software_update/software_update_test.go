4b5543953d6e1bf7c028ab12dd6193e96fb8871a
Sharon Katz <sharon@fleetdm.com>
2023-03-20 21:52:45 -0400

    test generate

--- orbit/pkg/table/software_update/software_update_test.go
+++ orbit/pkg/table/software_update/software_update_test.go
@@ -4,25 +4,25 @@
 package software_update
 
 import (
-	//	"github.com/osquery/osquery-go/plugin/table"
+	"github.com/osquery/osquery-go/plugin/table"
 	"golang.org/x/net/context"
 	"testing"
 	"time"
 )
 
-// func TestGenerate(t *testing.T) {
-// 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
-// 	defer cancel()
-// 	var tbl table.QueryContext
+func TestGenerate(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	var tbl table.QueryContext
 
-// 	table, err := Generate(ctx, tbl)
-// 	if err != nil {
-// 		t.Fatalf(`Expected no error. got %s`, err)
-// 	}
-// 	if table[0]["amfi_enabled"] != "0" && table[0]["amfi_enabled"] != "1" {
-// 		t.Fatalf(`amfiEnabled expected 0 or 1. got %s`, table[0]["amfi_enabled"])
-// 	}
-// }
+	table, err := Generate(ctx, tbl)
+	if err != nil {
+		t.Fatalf(`Expected no error. got %s`, err)
+	}
+	if table[0]["new_software_available"] != "0" && table[0]["new_software_available"] != "1" {
+		t.Fatalf(`new_software_available expected 0 or 1. got %s`, table[0]["new_software_available"])
+	}
+}
 
 func TestIsNewSoftwareAvailable(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
