d36d5a09a8b88ce15ced63cf075148a75f81ab85
Jacob Shandling <jacob@fleetdm.com>
2023-03-22 09:35:29 -0700

    Tighten up tests

--- frontend/pages/software/SoftwareDetailsPage/components/Vulnerabilities/Vulnerabilities.tests.tsx
+++ frontend/pages/software/SoftwareDetailsPage/components/Vulnerabilities/Vulnerabilities.tests.tsx
@@ -1,35 +1,26 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
+import createMockSoftware from "__mocks__/softwareMock";
+
 import Vulnerabilities from "./Vulnerabilities";
 
 describe("Vulnerabilities", () => {
-  const mockSoftwareWithVuln = {
-    id: 1,
-    name: "testSW",
-    version: "1.0",
-    source: "apps",
-    generated_cpe: "a:b:c",
-    vulnerabilities: [
-      {
-        cve: "CVE_333",
-        details_link: "https://its.really.bad",
-        cvss_score: 9.5,
-        epss_probability: 1,
-        cisa_known_exploit: false,
-        cve_published: "2023-02-14T20:15:00Z",
-      },
-    ],
-  };
-
-  const mockSoftwareNoVulns = {
-    id: 1,
-    name: "testSW",
-    version: "1.0",
-    source: "apps",
-    generated_cpe: "a:b:c",
-    vulnerabilities: [],
-  };
+  const [mockSoftwareWithVuln, mockSoftwareNoVulns] = [
+    createMockSoftware({
+      vulnerabilities: [
+        {
+          cve: "CVE_333",
+          details_link: "https://its.really.bad",
+          cvss_score: 9.5,
+          epss_probability: 1,
+          cisa_known_exploit: false,
+          cve_published: "2023-02-14T20:15:00Z",
+        },
+      ],
+    }),
+    createMockSoftware(),
+  ];
 
   it("renders the empty state when no vulnerabilities are provided", () => {
     render(
@@ -40,9 +31,6 @@ describe("Vulnerabilities", () => {
       />
     );
 
-    // Header
-    expect(screen.getByText("Vulnerabilities")).toBeInTheDocument();
-
     // Empty state
     expect(
       screen.getByText("No vulnerabilities detected for this software item.")
@@ -51,13 +39,6 @@ describe("Vulnerabilities", () => {
       screen.getByText("Expecting to see vulnerabilities?")
     ).toBeInTheDocument();
     expect(screen.getByText("File an issue on GitHub")).toBeInTheDocument();
-
-    // No rendered table
-    expect(screen.queryByText("Vulnerability")).toBeNull();
-    expect(screen.queryByText("Probability of exploit")).toBeNull();
-    expect(screen.queryByText("Severity")).toBeNull();
-    expect(screen.queryByText("Known exploit")).toBeNull();
-    expect(screen.queryByText("Published")).toBeNull();
   });
 
   it("correctly renders a table when 1 vulnerability is provided, Premium tier", () => {
@@ -68,16 +49,6 @@ describe("Vulnerabilities", () => {
         software={mockSoftwareWithVuln}
       />
     );
-
-    // Header
-    expect(screen.getByText("Vulnerabilities")).toBeInTheDocument();
-
-    // No empty state
-    expect(
-      screen.queryByText("No vulnerabilities detected for this software item.")
-    ).toBeNull();
-    expect(screen.queryByText("Expecting to see vulnerabilities?")).toBeNull();
-    expect(screen.queryByText("File an issue on GitHub")).toBeNull();
 
     // Rendered table
     expect(screen.getByText("Vulnerability")).toBeInTheDocument();
@@ -90,7 +61,8 @@ describe("Vulnerabilities", () => {
     expect(screen.getByText("Critical", { exact: false })).toBeInTheDocument();
     expect(screen.getByText("ago", { exact: false })).toBeInTheDocument();
   });
-  it("correctly renders a table when 1 vulnerability is provided, Free tier", () => {
+
+  it("Only renders the 'Vulnerability' column when 1 vulnerability is provided on Free tier", () => {
     render(
       <Vulnerabilities
         isLoading={false}
@@ -99,24 +71,16 @@ describe("Vulnerabilities", () => {
       />
     );
 
-    // Header
-    expect(screen.getByText("Vulnerabilities")).toBeInTheDocument();
-
-    // No empty state
-    expect(
-      screen.queryByText("No vulnerabilities detected for this software item.")
-    ).toBeNull();
-    expect(screen.queryByText("Expecting to see vulnerabilities?")).toBeNull();
-    expect(screen.queryByText("File an issue on GitHub")).toBeNull();
-
     // Rendered table
     expect(screen.getByText("Vulnerability")).toBeInTheDocument();
+
     // No premium-only columns
     expect(screen.queryByText("Probability of exploit")).toBeNull();
     expect(screen.queryByText("Severity")).toBeNull();
     expect(screen.queryByText("Known exploit")).toBeNull();
     expect(screen.queryByText("Published")).toBeNull();
 
+    // Row data
     expect(screen.getByText("CVE_333")).toBeInTheDocument();
     expect(screen.queryByText("100%")).toBeNull();
     expect(screen.queryByText("Critical", { exact: false })).toBeNull();
