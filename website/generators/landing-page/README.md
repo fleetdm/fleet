8247a89fa653c6d4031a1ed339cef3064311caa9
Eric <eashaw@sailsjs.com>
2023-03-22 15:27:16 -0500

    create basic landing page generator

---
+++ website/generators/landing-page/README.md
@@ -0,0 +1,64 @@
+# @eashaw/sails-generate-landing-page
+
+A `landing-page` generator for use with the Sails command-line interface.
+
+
+## Installation
+
+```sh
+$ npm install @eashaw/sails-generate-landing-page --save
+```
+
+Then merge the following into your `.sailsrc` file:
+
+```json
+{
+  "modules": {
+    "landing-page": "@eashaw/sails-generate-landing-page"
+  }
+}
+```
+
+> Note that instead of `"@eashaw/sails-generate-landing-page"`, you can also choose to provide the path to the generator locally (e.g. "./generators/landing-page").
+> This is useful if, for example, you have specific best practices for particular projects or teams within your organization, and you want to be able to check in generators to your code repository.
+>
+> Certain generators are installed by default in Sails, but they can be overridden.  Other generators add support for generating entirely new kinds of things.
+> Check out [Concepts > Extending Sails > Generators](https://sailsjs.com/docs/concepts/extending-sails/generators) for information on installing generator overrides / custom generators and information on building your own generators.
+
+
+
+## Usage
+
+```bash
+$ sails generate landing-page 
+```
+
+
+## Need help?
+
+See [Extending Sails > Generators > Custom Generators](https://sailsjs.com/docs/concepts/extending-sails/generators/custom-generators) in the [Sails documentation](https://sailsjs.com/documentation), or check out [recommended support options](https://sailsjs.com/support).
+
+<a href="https://sailsjs.com" target="_blank" title="Node.js framework for building realtime APIs."><img src="https://github-camo.global.ssl.fastly.net/9e49073459ed4e0e2687b80eaf515d87b0da4a6b/687474703a2f2f62616c64657264617368792e6769746875622e696f2f7361696c732f696d616765732f6c6f676f2e706e67" width=60 alt="Sails.js logo (small)"/></a>
+
+
+## Bugs &nbsp; [![NPM version](https://badge.fury.io/js/@eashaw/sails-generate-landing-page.svg)](http://npmjs.com/package/@eashaw/sails-generate-landing-page)
+
+To report a bug, [click here](https://sailsjs.com/bugs).
+
+
+## Contributing
+
+Please observe the guidelines and conventions laid out in the [Sails project contribution guide](https://sailsjs.com/documentation/contributing) when opening issues or submitting pull requests.
+
+[![NPM](https://nodei.co/npm/@eashaw/sails-generate-landing-page.png?downloads=true)](http://npmjs.com/package/@eashaw/sails-generate-landing-page)
+
+
+
+## License
+
+This landing-page generator is available under the **MIT license**.
+
+The [Sails framework](https://sailsjs.com) is free and open-source under the [MIT License](https://sailsjs.com/license).
+
+
+![image_squidhome@2x.png](http://i.imgur.com/RIvu9.png)
---
+++ website/generators/landing-page/index.js
@@ -0,0 +1,180 @@
+/**
+ * Module dependencies
+ */
+
+var util = require('util');
+var path = require('path');
+var _ = require('@sailshq/lodash');
+// var generateFile = require('./node_modules/sails-generate/builtins/file');
+
+/**
+ * @eashaw/sails-generate-landing-page
+ *
+ * Usage:
+ * `sails generate landing-page`
+ *
+ * @description Generates a landing-page.
+ * @docs https://sailsjs.com/docs/concepts/extending-sails/generators/custom-generators
+ */
+
+module.exports = {
+
+  templatesDirectory: path.resolve(__dirname,'./templates'),
+
+  /**
+   * Scope:
+   * ----------------------------------------------------
+   * @option {Array} args   [command-line arguments]
+   * ----------------------------------------------------
+   * @property {String} relPath
+   * @property {String} stem
+   * @property {String} newActionSlug
+   *
+   * @property {String} newViewRelPath
+   * @property {String} newActionRelPath
+   * @property {String} newStylesheetRelPath
+   * @property {String} newPageScriptRelPath
+   */
+
+  before: function (scope, exits) {
+    if (!scope.args[0]) {
+      return exits.error(
+        'Please specify the base name or path for the new page.\n'+
+        '(relative from the `views/pages/` folder;\n'+
+        ' e.g. `dashboard/activity-summary`)'
+      );
+    }
+
+    // e.g. `dashboard/activity-summary`
+    scope.relPath = scope.args[0];
+
+    // Check if it has a file extension and, if so, reject it.
+    if (path.extname(scope.relPath)) {
+      return exits.error('Please specify the path for the new page, excluding the filename suffix (i.e. no ".ejs")');
+    }
+
+    // Trim any whitespace from both sides.
+    scope.relPath = _.trim(scope.relPath);
+
+    // Replace backslashes with proper slashes.
+    // (This is crucial for Windows compatibility.)
+    scope.relPath = scope.relPath.replace(/\\/g, '/');
+
+    // Check that it does not have any trailing slashes.
+    if (scope.relPath.match(/\/$/)) {
+      return exits.error('Please specify the path for the new page. (No trailing slash please.)');
+    }
+
+    // Check that it does not begin with a slash or a dot dot slash.
+    // (a single dot+slash is ok, since you might be using tab-completion in the terminal)
+    if (scope.relPath.match(/^\.\.+\//) || scope.relPath.match(/^\//)) {
+      return exits.error('No need for dots and leading slashes and things. Please specify something like: `dashboard/activity-summary`');
+    }
+
+    // Make sure the relative path is not within "pages/", "views/", "controllers/",
+    // "assets/", "js/", "styles/", or anything else like that.  If it is, it's probably
+    // an accident.  And if it's not an accident, it's still super confusing.
+    if (scope.relPath.match(/^(pages\/|views\/|controllers\/|api\/|assets\/|js\/|styles\/)/i)) {
+      return exits.error('Please specify *just* the relative path for the new page, excluding prefixes like "pages/", "views/", or "controllers/".  Those will be attached for you automatically-- you just need to include the last bit; e.g. `dashboard/activity-summary` or  `internal/admin-activity-log`');
+    }
+
+    // Gracefully ignore double-slashes.
+    scope.relPath = scope.relPath.replace(/\/\/+/, '/');
+
+    // Gracefully ignore leading "./", if present.
+    scope.relPath = scope.relPath.replace(/^[\.\/]+/, '');
+
+    // Make sure all parent sub-folders are kebab-cased and don't contain any
+    // uppercase or non-alphanumeric characters (except dashes are ok, of course).
+    var parentSubFoldersString = path.dirname(scope.relPath);
+    var arrayOfParentSubFolders = parentSubFoldersString==='.' ? [] : parentSubFoldersString.split(/\//);
+    try {
+      _.each(arrayOfParentSubFolders, function(subFolderName){
+        if (subFolderName !== _.kebabCase(subFolderName)) {
+          throw new Error('Please make sure any parent sub-folders are written in kebab-case; e.g. "internal-site/admin-dashboard/foobar", and NOT "internalSite/admin_dashboard/dontDoTHIS/please"');
+        }
+        if (subFolderName.match(/[^a-z0-9\-]/) || subFolderName !== _.deburr(subFolderName)){
+          throw new Error('Please stick to alphanumeric characters and dashes.');
+        }
+      });//∞
+    } catch (err) { return exits.error(err.message); }
+
+    // Tease out the "stem".
+    // (e.g. `activity-summary`)
+    var stem = path.basename(scope.relPath);
+
+    // Then kebab-case it, if it isn't already.
+    // (e.g. `activitySummary` becomes `activity-summary`)
+    stem = _.kebabCase(stem);
+
+    // Make sure it doesn't start with `view`.
+    // (e.g. NOT `view-activity-summary`)
+    if (stem.match(/^view-/)) {
+      return exits.error('No need to prefix with "view-" when generating a page.  Instead, just leave that part out.  (It\'ll be added automatically where needed.)');
+    }
+
+    // Check that the stem doesn't still contain any uppercase or non-alphanumeric
+    // characters.  (Except dashes are ok, of course.)
+    if (stem.match(/[^a-z0-9\-]/) || stem !== _.deburr(stem)) {
+      return exits.error('Please stick to alphanumeric characters and dashes.');
+    }
+
+
+    // ◊  (Now then…)
+    scope.stem = stem;
+    scope.newActionSlug = path.join(arrayOfParentSubFolders.join('/landing-pages/'), 'view-'+stem);
+    scope.newActionRelPath = path.join('api/controllers/landing-pages/', scope.newActionSlug+'.js');
+    scope.newViewRelPath = path.join('views/pages/landing-pages/', scope.relPath+'.ejs');
+    scope.newStylesheetRelPath = path.join('assets/styles/pages/landing-pages/', scope.relPath+'.less');
+    scope.newPageScriptRelPath = path.join('assets/js/pages/landing-pages/', scope.relPath+'.page.js');
+
+    // Set up underlying "action" generator.
+    scope.actions2 = true;
+    scope.args = [ scope.newActionSlug ];
+
+    // Disable the "Created a new …!" output so we can use our own instead.
+    scope.suppressFinalLog = true;
+
+    return exits.success();
+  },
+
+  after: function (scope, done){
+    console.log();
+    console.log('Successfully generated:');
+    console.log(' •-',scope.newViewRelPath);
+    console.log(' •-',scope.newActionRelPath);
+    console.log(' •-',scope.newStylesheetRelPath);
+    console.log(' •-',scope.newPageScriptRelPath);
+    console.log();
+    console.log('A few reminders:');
+    console.log(' (1)  These files were generated assuming your Sails app is using');
+    console.log('      Vue.js as its front-end framework.  (If you\'re unsure,');
+    console.log('      head over to https://sailsjs.com/support)');
+    console.log();
+    console.log(' (2)  You\'ll need to manually add a route for this new page\'s');
+    console.log('      action in your `config/routes.js` file; e.g.');
+    console.log('          \'GET /'+scope.relPath+'\': { action: \'landing-pages/'+(
+      scope.newActionSlug.replace(/\\/g,'/')//« because Windows
+    )+'\' },');
+    console.log();
+    console.log(' (3)  You\'ll need to manually import the new LESS stylesheet');
+    console.log('      from your `assets/styles/importer.less` file; e.g.');
+    console.log('          @import \''+(
+      path.join('pages/landing-pages/', scope.relPath+'.less').replace(/\\/g,'/')//« because Windows
+    )+'\';');
+    console.log();
+    console.log(' (4)  Last but not least, since some of the above are backend changes,');
+    console.log('      don\'t forget to re-lift the server before testing!');
+    console.log();
+
+    return done();
+  },
+
+  targets: {
+    './': ['action'],// << Use underlying default generator
+    './:newViewRelPath': { template: 'page.ejs.template' },
+    './:newStylesheetRelPath': { template: 'stylesheet.less.template' },
+    './:newPageScriptRelPath': { template: 'page-script.page.js.template' }
+  }
+
+};
