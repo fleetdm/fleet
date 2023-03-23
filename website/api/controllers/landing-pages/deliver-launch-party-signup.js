c267c4b52ebb09cce67377e343603343fa57b307
Eric <eashaw@sailsjs.com>
2023-03-23 15:21:43 -0500

    Website: add action, rebuild cloud-sdk

---
+++ website/api/controllers/landing-pages/deliver-launch-party-signup.js
@@ -0,0 +1,78 @@
+module.exports = {
+
+
+  friendlyName: 'Deliver launch party signup',
+
+
+  description: 'Delivers a message to an internal slack channel to notify us when someone signs up for our RSA Happy hour',
+
+
+  inputs: {
+
+    emailAddress: {
+      required: true,
+      type: 'string',
+      description: 'The email address provided when a user submitted the launch party waitlist form.',
+      example: 'hermione@hogwarts.edu'
+    },
+
+    fullName: {
+      required: true,
+      type: 'string',
+      description: 'The name provided when a user submitted the launch party waitlist form',
+    },
+
+    jobTitle: {
+      type: 'string',
+      description: 'The job title provided when a user submitted the launch party waitlist form',
+    },
+
+    phoneNumber: {
+      type: 'number',
+      description: 'The phone number provided when a user submitted the launch party waitlist form',
+    },
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'The message was sent successfully.'
+    }
+
+  },
+
+
+  fn: async function({emailAddress, fullName, jobTitle, phoneNumber}) {
+
+    // if(!sails.config.custom.zapierSandboxWebhookSecret) {
+    //   throw new Error('Message not delivered: zapierSandboxWebhookSecret needs to be configured in sails.config.custom.');
+    // }
+    console.log({
+        'emailAddress': emailAddress,
+        'fullName': fullName,
+        'jobTitle': jobTitle,
+        'phoneNumber': phoneNumber,
+        'webhookSecret': sails.config.custom.zapierSandboxWebhookSecret
+      });
+    // Send a POST request to Zapier
+    // await sails.helpers.http.post(
+    //   'https://hooks.zapier.com/hooks/catch/3627242/bj5nh8y/',
+    //   {
+    //     'emailAddress': emailAddress,
+    //     'fullName': fullName,
+    //     'jobTitle': jobTitle,
+    //     'phoneNumber': phoneNumber,
+    //     'webhookSecret': sails.config.custom.zapierSandboxWebhookSecret
+    //   }
+    // )
+    // .timeout(5000)
+    // .tolerate(['non200Response', 'requestFailed', {name: 'TimeoutError'}], (err)=>{
+    //   // Note that Zapier responds with a 2xx status code even if something goes wrong, so just because this message is not logged doesn't mean everything is hunky dory.  More info: https://github.com/fleetdm/fleet/pull/6380#issuecomment-1204395762
+    //   sails.log.warn(`When a user submitted the launch party waitlist form, an error occurred while sending a request to Zapier. Raw error: ${require('util').inspect(err)}`);
+    //   return;
+    // });
+
+  }
+
+};
