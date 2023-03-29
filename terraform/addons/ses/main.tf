3254a6eab0c38f3b5a7273310f234112dd3446ef
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2023-03-29 12:06:57 -0400

    fixup

--- terraform/addons/ses/main.tf
+++ terraform/addons/ses/main.tf
@@ -2,18 +2,6 @@ data "aws_route53_zone" "main" {
   name = var.domain
 }
 
-module "labels" {
-  source  = "clouddrove/labels/aws"
-  version = "1.3.0"
-
-  name        = var.name
-  environment = var.environment
-  managedby   = var.managedby
-  label_order = var.label_order
-  repository  = var.repository
-}
-
-
 resource "aws_ses_domain_identity" "default" {
   domain = var.domain
 }
@@ -27,8 +15,8 @@ resource "aws_ses_domain_identity_verification" "default" {
 
 resource "aws_route53_record" "ses_verification" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = module.labels.id
-  type    = var.txt_type
+  name    = ""
+  type    = "TXT"
   ttl     = "600"
   records = [aws_ses_domain_identity.default.verification_token]
 }
@@ -43,15 +31,15 @@ resource "aws_route53_record" "dkim" {
   for_each = aws_ses_domain_dkim.default.dkim_tokens
   zone_id  = data.aws_route53_zone.main.zone_id
   name     = format("%s._domainkey.%s", each.key, var.domain)
-  type     = var.cname_type
+  type     = "CNAME"
   ttl      = 600
   records  = [format("%s.dkim.amazonses.com", each.key)]
 }
 
 resource "aws_route53_record" "spf_domain" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = module.labels.id
-  type    = var.txt_type
+  name    = ""
+  type    = "TXT"
   ttl     = "600"
   records = ["v=spf1 include:amazonses.com -all"]
 }
--- terraform/addons/ses/variables.tf
+++ terraform/addons/ses/variables.tf
@@ -1,46 +1,4 @@
-variable "name" {
-  type        = string
-  default     = ""
-  description = "Name  (e.g. `app` or `cluster`)."
-}
-
-variable "environment" {
-  type        = string
-  default     = ""
-  description = "Environment (e.g. `prod`, `dev`, `staging`)."
-}
-
-variable "repository" {
-  type        = string
-  default     = "https://github.com/clouddrove/terraform-aws-ses"
-  description = "Terraform current module repo"
-}
-
-variable "label_order" {
-  type        = list(any)
-  default     = []
-  description = "Label order, e.g. `name`,`application`."
-}
-
-variable "managedby" {
-  type        = string
-  default     = "hello@clouddrove.com"
-  description = "ManagedBy, eg 'CloudDrove'"
-}
-
 variable "domain" {
   type        = string
   description = "Domain to use for SES."
-}
-
-variable "txt_type" {
-  type        = string
-  default     = "TXT"
-  description = "Txt type for Record Set."
-}
-
-variable "cname_type" {
-  type        = string
-  default     = "CNAME"
-  description = "CNAME type for Record Set."
 }
