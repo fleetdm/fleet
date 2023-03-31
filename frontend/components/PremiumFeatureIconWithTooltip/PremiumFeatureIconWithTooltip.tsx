59c975fdab4df1271aa23667e7dd189d15d87bab
Jacob Shandling <jacob@fleetdm.com>
2023-03-31 15:08:34 -0700

    Add props to adjust position of tooltip

--- frontend/components/PremiumFeatureIconWithTooltip/PremiumFeatureIconWithTooltip.tsx
+++ frontend/components/PremiumFeatureIconWithTooltip/PremiumFeatureIconWithTooltip.tsx
@@ -1,9 +1,23 @@
 import Icon from "components/Icon";
 import { uniqueId } from "lodash";
 import React from "react";
-import ReactTooltip from "react-tooltip";
+import ReactTooltip, { Place } from "react-tooltip";
 
-const PremiumFeatureIconWithTooltip = () => {
+interface IPremiumFeatureIconWithTooltip {
+  tooltipPlace?: Place;
+  tooltipPositionOverrides?: {
+    leftAdj?: number;
+    topAdj?: number;
+  };
+}
+const PremiumFeatureIconWithTooltip = ({
+  tooltipPlace,
+  tooltipPositionOverrides,
+}: IPremiumFeatureIconWithTooltip) => {
+  const [leftAdj, topAdj] = [
+    tooltipPositionOverrides?.leftAdj ?? 0,
+    tooltipPositionOverrides?.topAdj ?? 0,
+  ];
   const tipId = uniqueId();
   return (
     <span className="premium-icon-tip">
@@ -11,13 +25,19 @@ const PremiumFeatureIconWithTooltip = () => {
         <Icon name="premium-feature" />
       </span>
       <ReactTooltip
-        place="top"
+        place={tooltipPlace ?? "top"}
         type="dark"
-        effect="solid"
+        effect="float"
         id={tipId}
         backgroundColor="#515774"
         delayHide={100}
         delayUpdate={500}
+        overridePosition={(pos: { left: number; top: number }) => {
+          return {
+            left: pos.left + leftAdj,
+            top: pos.top + topAdj,
+          };
+        }}
       >
         {`This is a Fleet Premium feature. `}
         <a href="https://fleetdm.com/upgrade" rel="noreferrer" target="_blank">
