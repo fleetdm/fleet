900d280dd2564e9a7cf8a2065216917a7b7a74c8
Rachel Elysia Perkins <rachel@fleetdm.com>
2023-04-04 10:47:58 -0400

    Add search to manage policies page

--- frontend/pages/policies/ManagePoliciesPage/components/PoliciesTable/PoliciesTable.tsx
+++ frontend/pages/policies/ManagePoliciesPage/components/PoliciesTable/PoliciesTable.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "context/app";
 import { noop } from "lodash";
 import paths from "router/paths";
@@ -9,7 +9,7 @@ import { IEmptyTableProps } from "interfaces/empty_table";
 
 import Button from "components/buttons/Button";
 import Spinner from "components/Spinner";
-import TableContainer from "components/TableContainer";
+import TableContainer, { ITableQueryData } from "components/TableContainer";
 import EmptyTable from "components/EmptyTable";
 import { generateTableHeaders, generateDataSet } from "./PoliciesTableConfig";
 
@@ -45,6 +45,12 @@ const PoliciesTable = ({
   const { MANAGE_HOSTS } = paths;
 
   const { config } = useContext(AppContext);
+
+  const [searchString, setSearchString] = useState("");
+
+  const handleSearchChange = ({ searchQuery }: ITableQueryData) => {
+    setSearchString(searchQuery);
+  };
 
   const emptyState = () => {
     const emptyPolicies: IEmptyTableProps = {
@@ -94,6 +100,8 @@ const PoliciesTable = ({
     return emptyPolicies;
   };
 
+  const searchable = !(policiesList?.length === 0 && searchString === "");
+
   return (
     <div
       className={`${baseClass} ${
@@ -134,9 +142,13 @@ const PoliciesTable = ({
               primaryButton: emptyState().primaryButton,
             })
           }
-          onQueryChange={noop}
           disableCount={tableType === "inheritedPolicies"}
           isClientSidePagination
+          isClientSideFilter
+          searchQueryColumn="name"
+          onQueryChange={handleSearchChange}
+          inputPlaceHolder="Search by name"
+          searchable={searchable}
         />
       )}
     </div>
