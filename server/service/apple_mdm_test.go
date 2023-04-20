f5d84e6d3aeed8fce3d3d4d7088d7cc0a0eea44a
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2023-04-19 21:37:09 -0500

    Fix test

--- server/service/apple_mdm_test.go
+++ server/service/apple_mdm_test.go
@@ -724,6 +724,9 @@ func TestHostDetailsMDMProfiles(t *testing.T) {
 	ds.ListPoliciesForHostFunc = func(ctx context.Context, host *fleet.Host) ([]*fleet.HostPolicy, error) {
 		return nil, nil
 	}
+	ds.GetHostMDMMacOSSetupFunc = func(ctx context.Context, hostID uint) (*fleet.HostMDMMacOSSetup, error) {
+		return nil, nil
+	}
 
 	expectedNilSlice := []fleet.HostMDMAppleProfile(nil)
 	expectedEmptySlice := []fleet.HostMDMAppleProfile{}
