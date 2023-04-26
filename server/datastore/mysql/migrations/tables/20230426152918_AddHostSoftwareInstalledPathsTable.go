e8aa5f56e53d90ec34dc509b8ffc4bb8a1f410f4
Juan Fernandez <juan@fleetdm.com>
2023-04-26 15:32:36 -0400

    Regen migration

---
+++ server/datastore/mysql/migrations/tables/20230426152918_AddHostSoftwareInstalledPathsTable.go
@@ -0,0 +1,30 @@
+package tables
+
+import (
+	"database/sql"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20230426152918, Down_20230426152918)
+}
+
+func Up_20230426152918(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+		CREATE TABLE host_software_installed_paths
+		(
+			host_id        INT    UNSIGNED NOT NULL,
+			software_id    BIGINT UNSIGNED NOT NULL,
+			installed_path TEXT            NOT NULL,
+			PRIMARY KEY (host_id, software_id)
+		)
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Down_20230426152918(tx *sql.Tx) error {
+	return nil
+}
