19bb6ad1bfd7fc6bfd4cf5c94215e661706eebec
Sharon Katz <121527325+sharon-fdm@users.noreply.github.com>
2023-05-02 18:42:23 -0400

    Update exec.go

--- orbit/pkg/table/user_exec/exec.go
+++ orbit/pkg/table/user_exec/exec.go
@@ -1,4 +1,29 @@
 // build +darwin
+
+// Copyright (c) 2020-present Fleet Device Management Inc
+// Adapted from Kolide Launcher:
+// Copyright (c) 2017 Kolide, Inc.
+
+// MIT License
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Package user_exec provides a generic way to run osquery as a user on macOS.
 // Some built-in osquery tables (e.g. screenlock) will only provide data for the
 // current user, which generally means "root" in fleet. This often isn't useful,
