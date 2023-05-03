97431cdcb22f0623b39dc7ad7fe7b691a1bbe5ae
Gabriel Hernandez <ghernandez345@gmail.com>
2023-05-03 11:35:01 +0100

    add missing componets to storybook

--- frontend/components/forms/fields/Checkbox/Checkbox.stories.tsx
+++ frontend/components/forms/fields/Checkbox/Checkbox.stories.tsx
@@ -1,27 +1,21 @@
 import React from "react";
-import { Meta, Story } from "@storybook/react";
-import { noop } from "lodash";
+import { Meta, StoryObj } from "@storybook/react";
 
 import Checkbox from ".";
 
-import { ICheckboxProps } from "./Checkbox";
-
-import "../../../../index.scss";
-
-export default {
+const meta: Meta<typeof Checkbox> = {
   component: Checkbox,
   title: "Components/FormFields/Checkbox",
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Basic: Story = {};
+
+export const WithLabel: Story = {
   args: {
-    value: false,
-    disabled: false,
-    indeterminate: false,
-    className: "",
-    name: "",
-    wrapperClassName: "",
-    onChange: noop,
+    children: <b>Label</b>,
   },
-} as Meta;
-
-const Template: Story<ICheckboxProps> = (props) => <Checkbox {...props} />;
-
-export const Default = Template.bind({});
+};
