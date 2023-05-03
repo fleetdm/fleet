3b86ed0156447d2ce7d291287502f091cfa6945f
Gabriel Hernandez <ghernandez345@gmail.com>
2023-05-03 17:51:33 +0100

    add components to storybook (#11500)

---
+++ frontend/components/FleetMarkdown/FleetMarkdown.stories.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+
+import FleetMarkdown from "./FleetMarkdown";
+
+const TestMarkdown = `
+# Test Markdown
+
+## This is a heading
+
+### This is a subheading
+
+#### This is a subsubheading
+
+
+---
+**bold**
+
+*italic*
+
+[test link](https://www.fleetdm.com)
+
+- test list item 1
+- test list item 2
+- test list item 3
+
+> test blockquote
+
+\`code text\`
+`;
+
+const meta: Meta<typeof FleetMarkdown> = {
+  title: "Components/FleetMarkdown",
+  component: FleetMarkdown,
+  args: { markdown: TestMarkdown },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FleetMarkdown>;
+
+export const Basic: Story = {};
