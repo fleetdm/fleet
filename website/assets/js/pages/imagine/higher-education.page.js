34feef4c585d3361b5b4a6ec71094365d87e197c
Andrew Baker <89049099+DrewBakerfdm@users.noreply.github.com>
2023-05-15 16:15:59 -0400

    Education page via landing page builder (#11698)
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [ ] Changes file added for user-visible changes in `changes/` or
    `orbit/changes/`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [ ] Documented any API changes (docs/Using-Fleet/REST-API.md or
    docs/Contributing/API-for-contributors.md)
    - [ ] Documented any permissions changes
    - [ ] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    - [ ] Added support on fleet's osquery simulator `cmd/osquery-perf` for
    new osquery data ingestion features.
    - [ ] Added/updated tests
    - [ ] Manual QA for all new/changed functionality
      - For Orbit and Fleet Desktop changes:
    - [ ] Manual QA must be performed in the three main OSs, macOS, Windows
    and Linux.
    - [ ] Auto-update manual QA, from released version of component to new
    version (see [tools/tuf/test](../tools/tuf/test/README.md)).

---
+++ website/assets/js/pages/imagine/higher-education.page.js
@@ -0,0 +1,29 @@
+parasails.registerPage('higher-education', {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    //…
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    clickOpenChatWidget: function() {
+      if(window.HubSpotConversations && window.HubSpotConversations.widget){
+        window.HubSpotConversations.widget.open();
+      }
+    },
+  }
+});
