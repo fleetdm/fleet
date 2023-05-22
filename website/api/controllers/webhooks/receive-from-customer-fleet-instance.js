8890317172f02efaeddc24e2934147f4f17b6954
Eric <eashaw@sailsjs.com>
2023-05-22 16:02:15 -0500

    lint fixes, update error messages, comments, and descriptions

--- website/api/controllers/webhooks/receive-from-customer-fleet-instance.js
+++ website/api/controllers/webhooks/receive-from-customer-fleet-instance.js
@@ -4,20 +4,28 @@ module.exports = {
   friendlyName: 'Receive from customer fleet instance',
 
 
-  description: 'Receive webhook requests and/or incoming auth redirects from Customer.',
+  description: 'Receive webhook requests from a customer\'s Fleet instance and sends a request to unenroll a specfied host from the customer\'s Workspace One instance.',
 
 
   inputs: {
+
     timestamp: {
       type: 'string',
       required: true,
-      description: 'An ISO timestamp representing when this request was sent',
+      description: 'An ISO timestamp representing when this request was sent.',
+      extendedDescription: 'This value is not used by the webhook.'
     },
+
     host: {
-      type: { id: 'number', uuid: 'string', hardware_serial: 'string' },
+      type: {
+        id: 'number',
+        uuid: 'string',
+        hardware_serial: 'string'//eslint-disable-line camelcase
+      },
       required: true,
-      description: 'An object containing information about a host that will be unenrolled from the customers old MDM instance.'
+      description: 'An object containing information about a host that will be unenrolled from the customers Workspace ONE instance.'
     }
+
   },
 
 
@@ -26,21 +34,21 @@ module.exports = {
   },
 
 
-  fn: async function ({timestamp, host}) {
+  fn: async function ({host}) {
 
     if(!sails.config.custom.customerWorkspaceOneUrl) {
-      throw new Error('No sails.config.custom.customerWorkspaceOneUrl configured! Please set this value to be the base url of the customers MDM instance.')
+      throw new Error('No sails.config.custom.customerWorkspaceOneUrl configured! Please set this value to be the base url of the customers Workspace One instance.');
     }
 
-    if(!sails.config.custom.customerWorkspaceOneTenentID){
-      throw new Error('No sails.config.custom.customerWorkspaceOneTenentID configured! Please set this value to be a the "AirWatch" API token from the Customer\'s MDM instance.')//FUTURE: better error
+    if(!sails.config.custom.customerWorkspaceOneTenentID) {
+      throw new Error('No sails.config.custom.customerWorkspaceOneTenentID configured! Please set this value to be a the "AirWatch" API token from the Customer\'s Workspace One instance.');
     }
 
     if(!sails.config.custom.customerWorkspaceOneAuthorizationHeader) {
-      throw new Error('No sails.config.custom.customerWorkspaceOneAuthorizationHeader configured! Please set this value to be the authorization header for requests to this customers MDM instance.')//FUTURE: better error
+      throw new Error('No sails.config.custom.customerWorkspaceOneAuthorizationHeader configured! Please set this value to be the authorization header for requests to the customer\'s Workspace One instance.');
     }
 
-    // Send a request to unenroll this host in the customer's old MDM instance.
+    // Send a request to unenroll this host in the customer's Workspace One instance.
     await sails.helpers.http.post.with({
       url: `/api/mdm/devices/commands?searchby=Serialnumber&id=${host.hardware_serial}&command=EnterpriseWipe`,
       headers: {
@@ -49,12 +57,17 @@ module.exports = {
       },
       baseUrl: sails.config.custom.customerWorkspaceOneUrl
     }).intercept((err)=>{
-      return new Error(`When sending a request to unenroll a host from an old MDM instance (Host information: Serial number: ${host.hardware_serial}, id: ${host.id}, uuid: ${host.uuid}), an error occured. Full error: ${err}`);// FUTURE: better error
-    })
+      if(err.raw.statusCode === 404){
+        return new Error(`When sending a request to unenroll a host from a Workspace One instance (Host information: Serial number: ${host.hardware_serial}, id: ${host.id}, uuid: ${host.uuid}), the specified host was not found on the customer's Workspace One instance. Full error: ${err}`);
+      } else if(err.raw.statusCode === 400) {
+        return new Error(`When sending a request to unenroll a host from a Workspace One instance (Host information: Serial number: ${host.hardware_serial}, id: ${host.id}, uuid: ${host.uuid}), the Workspace One instance could not unenroll the specified host. Full error: ${err}`);
+      } else {
+        return new Error(`When sending a request to unenroll a host from a Workspace One instance (Host information: Serial number: ${host.hardware_serial}, id: ${host.id}, uuid: ${host.uuid}), an error occured. Full error: ${err}`);
+      }
+    });
 
     // All done.
     return;
-
   }
 
 
