5972500df50177b80582d90f6f20fce21b72c090
Eric <eashaw@sailsjs.com>
2023-05-24 10:50:21 -0500

    add webhookSecret input and unauthorized exit

--- website/api/controllers/webhooks/receive-from-customer-fleet-instance.js
+++ website/api/controllers/webhooks/receive-from-customer-fleet-instance.js
@@ -25,17 +25,26 @@ module.exports = {
       },
       required: true,
       description: 'An dictionary containing information about a host that will be unenrolled from the customers Workspace ONE instance.'
+    },
+
+    webhookSecret: {
+      type: 'string',
+      required: true,
+      description: 'A shared secret used to confirm that this request came from a customer\'s Fleet instance',
     }
 
   },
 
 
   exits: {
-
+    unauthorized: {
+      responseType: 'unauthorized',
+      description: 'This webhook request could not be verified.',
+    }
   },
 
 
-  fn: async function ({host}) {
+  fn: async function ({host, webhookSecret}) {
 
     if(!sails.config.custom.customerWorkspaceOneBaseUrl) {
       throw new Error('No sails.config.custom.customerWorkspaceOneBaseUrl configured! Please set this value to be the base url of the customers Workspace One instance.');
@@ -49,6 +58,10 @@ module.exports = {
       throw new Error('No sails.config.custom.customerWorkspaceOneAuthorizationToken configured! Please set this value to be the authorization header for requests to the customer\'s Workspace One instance.');
     }
 
+    if(webhookSecret !== sails.config.custom.customerMigrationWebhookSecret) {
+      throw 'unauthorized';
+    }
+
     // Send a request to unenroll this host in the customer's Workspace One instance.
     await sails.helpers.http.post.with({
       // Contrary to what you what think the EnterpriseWipe command only unenrolls the host from a Workspace One instance.
@@ -59,7 +72,8 @@ module.exports = {
         'aw-tenant-code': sails.config.custom.customerWorkspaceOneTenantId,
       },
       baseUrl: sails.config.custom.customerWorkspaceOneBaseUrl
-    }).intercept('non200Response', (err)=>{
+    })
+    .intercept('non200Response', (err)=>{
       if(err.raw.statusCode === 404){
         return new Error(`When sending a request to unenroll a host from a Workspace One instance (Host information: Serial number: ${host.hardware_serial}, id: ${host.id}, uuid: ${host.uuid}), the specified host was not found on the customer's Workspace One instance. Full error: ${err}`);
       } else if(err.raw.statusCode === 400) {
