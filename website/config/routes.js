044af84c7133a2698000f7e82873f79252cb78ce
Mike Thomas <78363703+mike-j-thomas@users.noreply.github.com>
2023-05-30 09:42:21 +0900

    Add vulnerability management page (#11957)
    Changes:
    
    - Added new page `/vulnerability-management`.
    - Updated `routes.js` and `importer.less` for the new page.
    - Added nav item "Vulnerability management" under "Platform."
    
    Closes https://github.com/fleetdm/fleet/issues/11577
    
    ---------
    
    Co-authored-by: Eric <eashaw@sailsjs.com>

--- website/config/routes.js
+++ website/config/routes.js
@@ -273,6 +273,14 @@ module.exports.routes = {
     }
   },
 
+  'GET /vulnerability-management': {
+    action: 'view-vulnerability-management',
+    locals: {
+      pageTitleForMeta: 'Vulnerability management | Fleet for osquery',
+      pageDescriptionForMeta: 'Know what\’s going on with your computers. Measure and automate risk across your laptops and servers.',
+    }
+  },
+
 
   //  ╦╔╦╗╔═╗╔═╗╦╔╗╔╔═╗  ┌─┬  ┌─┐┌┐┌┌┬┐┬┌┐┌┌─┐  ┌─┐┌─┐┌─┐┌─┐┌─┐─┐
   //  ║║║║╠═╣║ ╦║║║║║╣   │ │  ├─┤│││ │││││││ ┬  ├─┘├─┤│ ┬├┤ └─┐ │
