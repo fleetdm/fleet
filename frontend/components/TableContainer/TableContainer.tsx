3d4aadba3edd0698ebd3bc42cf358715ab79cc61
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2023-05-30 17:16:47 -0400

    Fleet UI: Update icons (All icons related to action buttons/table container actions) (#11945)

--- frontend/components/TableContainer/TableContainer.tsx
+++ frontend/components/TableContainer/TableContainer.tsx
@@ -8,7 +8,7 @@ import SearchField from "components/forms/fields/SearchField";
 // @ts-ignore
 import Pagination from "components/Pagination";
 import Button from "components/buttons/Button";
-import { ButtonVariant } from "components/buttons/Button/Button";
+import Icon from "components/Icon/Icon";
 
 import DataTable from "./DataTable/DataTable";
 import TableContainerUtils from "./TableContainerUtils";
@@ -337,11 +337,8 @@ const TableContainer = ({
                 >
                   <>
                     {actionButton.buttonText}
-                    {actionButton.icon && (
-                      <img
-                        src={actionButton.icon}
-                        alt={`${actionButton.buttonText} icon`}
-                      />
+                    {actionButton.iconSvg && (
+                      <Icon name={actionButton.iconSvg} />
                     )}
                   </>
                 </Button>
