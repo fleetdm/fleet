2494ab9cf409843239e29c1f2420f166e3ead1d1
Rachel Elysia Perkins <rachel@fleetdm.com>
2023-05-30 16:41:07 -0400

    Progress towards tests, todo, certificate api call

--- frontend/components/AddHostsModal/AddHostsModal.tests.tsx
+++ frontend/components/AddHostsModal/AddHostsModal.tests.tsx
@@ -1,39 +1,120 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { renderWithSetup } from "test/test-utils";
+import { noop } from "lodash";
+import {
+  renderWithAppContext,
+  createCustomRenderer,
+  renderWithSetup,
+} from "test/test-utils";
 
 import AddHostsModal from "./AddHostsModal";
 
-describe("Add hosts modal", () => {
-  it("renders tabs", async () => {
-    render(<AddHostsModal />);
+const ENROLL_SECRET = "abcdefg12345678";
+
+describe("AddHostsModal", () => {
+  it("renders loading state", async () => {
+    render(
+      <AddHostsModal isAnyTeamSelected={false} isLoading onCancel={noop} />
+    );
+    const loadingSpinner = screen.getByTestId("spinner");
+    expect(loadingSpinner).toBeVisible();
+  });
+  it("renders platform tabs", async () => {
+    const { user } = renderWithSetup(
+      <AddHostsModal
+        isAnyTeamSelected
+        enrollSecret={ENROLL_SECRET}
+        isLoading={false}
+        onCancel={noop}
+      />
+    );
+
+    await user.click(screen.getByRole("tab", { name: "macOS" }));
+    const macOSText = screen.getByText("--type=pkg");
+    expect(macOSText).toBeInTheDocument();
+
+    await user.click(screen.getByRole("tab", { name: "Windows" }));
+    const windowsText = screen.getByText("--type=msi");
+    expect(windowsText).toBeInTheDocument();
+
+    await user.click(screen.getByRole("tab", { name: "Linux (RPM)" }));
+    const linuxRPMText = screen.getByText("--type=rpm");
+    expect(linuxRPMText).toBeInTheDocument();
+
+    await user.click(screen.getByRole("tab", { name: "Linux (deb)" }));
+    const linuxDebText = screen.getByText("--type=deb");
+    expect(linuxDebText).toBeInTheDocument();
+
+    // await user.click(screen.getByRole("tab", { name: "Advanced" }));
+    // const advancedText = screen.getByText("--type=YOUR_TYPE");
+    // expect(advancedText).toBeInTheDocument();
+  });
+  it("renders installer with secret", async () => {
+    render(<AddHostsModal isAnyTeamSelected isLoading onCancel={noop} />);
+    const text = screen.getByText("Something's gone wrong.");
+    const ctaButton = screen.getByRole("button", {
+      name: "Manage enroll secrets",
+    });
+    expect(text).toBeInTheDocument();
+    expect(ctaButton).toBeEnabled();
+  });
+  it("renders no enroll secret cta", async () => {
+    render(
+      <AddHostsModal
+        isAnyTeamSelected={false}
+        currentTeamName={"Apples"}
+        isLoading={false}
+        onCancel={noop}
+        openEnrollSecretModal={noop}
+      />
+    );
+
+    const text = screen.getByText("Something's gone wrong.");
+    const ctaButton = screen.getByRole("button", {
+      name: "Manage enroll secrets",
+    });
+
+    expect(text).toBeInTheDocument();
+    expect(ctaButton).toBeEnabled();
   });
 
-  it("renders tabs", async () => {
-    render(<AddHostsModal />);
-  });
+  describe("user is in sandbox mode", () => {
+    const contextValue = {
+      isSandboxMode: true,
+    };
 
-  it("renders tabs", async () => {
-    render(<AddHostsModal />);
-  });
+    it("download is disabled until a platform is selected", async () => {
+      // TODO:
+      // Need backend mock for certificate
+      // const render = createCustomRenderer({
+      //   withBackendMock: true,
+      // });
 
-  it("renders tabs", async () => {
-    render(<AddHostsModal />);
-  });
+      // Need app context for isSandboxMod
+      renderWithAppContext(
+        <AddHostsModal
+          isAnyTeamSelected={false}
+          isLoading={false}
+          enrollSecret={ENROLL_SECRET}
+          onCancel={noop}
+        />,
+        { contextValue }
+      );
 
-  it("renders tabs", async () => {
-    render(<AddHostsModal />);
-  });
+      // Need regular render (not with app context for user click)
+      const text = screen.getByText("Which platform");
+      const windowsText = screen.getByText("Windows");
+      const downloadButton = screen.getByRole("button", {
+        name: /Download installer/i,
+      });
 
-  it("renders global enroll secret", async () => {
-    render(<AddHostsModal />);
-  });
+      expect(text).toBeInTheDocument();
+      expect(screen.getByRole(downloadButton)).not.toBeEnabled();
 
-  it("renders team enroll secret if team selected", async () => {
-    render(<AddHostsModal />);
-  });
+      // TODO: Allow "user" click with app context render
+      // await user.click(windowsText);
 
-  it("renders sandbox mode", async () => {
-    render(<AddHostsModal />);
+      // expect(screen.getByRole(downloadButton)).toBeEnabled();
+    });
   });
 });
