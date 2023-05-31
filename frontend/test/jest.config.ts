c60641d9afa7068b77c516e4387cea66181bb345
Rachel Elysia Perkins <rachel@fleetdm.com>
2023-05-31 11:58:38 -0400

    Progress towards getting markdown to work with jest testing library

--- frontend/test/jest.config.ts
+++ frontend/test/jest.config.ts
@@ -1,16 +1,18 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  rootDir: "../",
+  rootDir: "../../",
   moduleDirectories: ["node_modules", "frontend"],
   testEnvironment: "jsdom",
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
+      "<rootDir>/frontend/__mocks__/fileMock.js",
     "\\.(css|scss|sass)$": "identity-obj-proxy",
+    "react-markdown":
+      "<rootDir>/node_modules/react-markdown/react-markdown.min.js",
   },
   testMatch: ["**/*tests.[jt]s?(x)"],
-  setupFilesAfterEnv: ["<rootDir>/test/test-setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/frontend/test/test-setup.ts"],
   clearMocks: true,
   testEnvironmentOptions: {
     url: "http://localhost:8080",
