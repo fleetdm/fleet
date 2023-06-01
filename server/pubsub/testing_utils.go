2d21b2be299e38fa83450c2006389c56a7d772b8
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2023-06-01 16:11:55 -0300

    Provide more feedback to the user when there's a Redis connection issue when running live queries (#11947)
    This could help future users to detect this issue: #10957
    It also adds an error log in Fleet that prints the actual error.
    
    The error is displayed if I kill Redis during a live session or if I set
    `client-output-buffer-limit` to something real low like `CONFIG SET
    "client-output-buffer-limit" "pubsub 100kb 50kb 60"`:
    ![Screenshot 2023-05-25 at 09 08
    08](https://github.com/fleetdm/fleet/assets/2073526/f021a77a-3a22-4b48-8073-bae9c6e21a11)
    
    - [X] Changes file added for user-visible changes in `changes/` or
    `orbit/changes/`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - ~[ ] Documented any API changes (docs/Using-Fleet/REST-API.md or
    docs/Contributing/API-for-contributors.md)~
    - ~[ ] Documented any permissions changes~
    - ~[ ] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)~
    - [X] Added support on fleet's osquery simulator `cmd/osquery-perf` for
    new osquery data ingestion features.
    - ~[ ] Added/updated tests~
    - [X] Manual QA for all new/changed functionality
      - ~For Orbit and Fleet Desktop changes:~
    - ~[ ] Manual QA must be performed in the three main OSs, macOS, Windows
    and Linux.~
    - ~[ ] Auto-update manual QA, from released version of component to new
    version (see [tools/tuf/test](../tools/tuf/test/README.md)).~

--- server/pubsub/testing_utils.go
+++ server/pubsub/testing_utils.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 
 	"github.com/fleetdm/fleet/v4/server/datastore/redis/redistest"
+	"github.com/go-kit/log"
 )
 
 func SetupRedisForTest(t *testing.T, cluster, readReplica bool) *redisQueryResults {
 	const dupResults = false
 	pool := redistest.SetupRedis(t, "zz", cluster, false, readReplica)
-	return NewRedisQueryResults(pool, dupResults)
+	return NewRedisQueryResults(pool, dupResults, log.NewNopLogger())
 }
