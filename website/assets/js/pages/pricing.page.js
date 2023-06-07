b6bb4797197be475f797336f6ff64ec745430586
Eric <eashaw@sailsjs.com>
2023-06-07 13:43:37 -0500

    update pricing page to render the pricing table on the server, update pricing table for security mode

--- website/assets/js/pages/pricing.page.js
+++ website/assets/js/pages/pricing.page.js
@@ -3,28 +3,7 @@ parasails.registerPage('pricing', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    displaySecurityPricingMode: false, // For pricing mode switch
-    // The order of categories in the pricing table for different pricing modes
-    itModeCategoryOrder: [
-      'Device management',
-      'Support',
-      'Inventory management',
-      'Collaboration',
-      'Security and compliance',
-      'Monitoring',
-      'Data outputs',
-      'Deployment'
-    ],
-    securityModeCategoryOrder: [
-      'Security and compliance',
-      'Monitoring',
-      'Inventory management',
-      'Collaboration',
-      'Support',
-      'Data outputs',
-      'Device management',
-      'Deployment'
-    ],
+    displaySecurityPricingMode: false,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -35,20 +14,6 @@ parasails.registerPage('pricing', {
   },
   mounted: async function(){
     //…
-  },
-  watch: {
-    displaySecurityPricingMode: function() {
-      // When the pricing mode changes, sort the pricing table based on the selected mode.
-      if(this.displaySecurityPricingMode){
-        this.pricingTable.sort((a, b)=>{
-          return this.securityModeCategoryOrder.indexOf(a.categoryName) - this.securityModeCategoryOrder.indexOf(b.categoryName);
-        });
-      } else {
-        this.pricingTable.sort((a, b)=>{
-          return this.itModeCategoryOrder.indexOf(a.categoryName) - this.itModeCategoryOrder.indexOf(b.categoryName);
-        });
-      }
-    }
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
