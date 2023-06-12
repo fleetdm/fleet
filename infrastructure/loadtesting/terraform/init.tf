09a8c07f630b99804ea6efea9563b13ca6e1542b
Luke Heath <luke@fleetdm.com>
2023-06-12 17:03:06 -0500

    Prepare v4.33.0

--- infrastructure/loadtesting/terraform/init.tf
+++ infrastructure/loadtesting/terraform/init.tf
@@ -14,7 +14,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.32.0"
+      version = "~> 4.33.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
