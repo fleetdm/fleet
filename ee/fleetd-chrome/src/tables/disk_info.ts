f2453f91b4514b388f995efbd55740d5e3920bd4
Sharon Katz <sharon@fleetdm.com>
2023-06-14 14:50:27 -0400

    add disk info

--- ee/fleetd-chrome/src/tables/disk_info.ts
+++ ee/fleetd-chrome/src/tables/disk_info.ts
@@ -2,13 +2,26 @@ import Table from "./Table";
 
 export default class TableDiskInfo extends Table {
   name = "disk_info";
-  columns = ["test1", "test2"];
+  columns = ["capacity", "id", "name", "type"];
 
   async generate() {
+    let capacity, id, name, type;
+    try {
+      const diskInfo = (await chrome.system.storage.getInfo()) as chrome.system.storage.StorageUnitInfo[];
+      capacity = diskInfo[0].capacity;
+      id = diskInfo[0].id
+      name = diskInfo[0].name
+      type = diskInfo[0].type
+    } catch (err) {
+      console.warn(`get disk info: ${err}`);
+    }
+
     return [
       {
-        test1: "test1 val",
-        test2: "test2 val",
+        capacity: capacity,
+        id: id,
+        name: name,
+        type: type,
       },
     ];
   }
