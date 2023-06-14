19559ca1c98a90e70c86ce3451a046882e80d1db
RachelElysia <rachel@fleetdm.com>
2023-06-14 14:51:55 -0400

    Add screnlock and system state

--- ee/fleetd-chrome/src/tables/screen_lock.ts
+++ ee/fleetd-chrome/src/tables/screen_lock.ts
@@ -7,18 +7,6 @@ export default class TableScreenLock extends Table {
   async generate() {
     let delay;
 
-    // This uses an old function callback so need to convert the callback style to a promise
-    // Consider updating the version so we don't have to use a function callback
-    // Use ts ignore with await and get rid of the promise
-    // try {
-    //   const delay = await new Promise((resolve) =>
-    //     chrome.idle.getAutoLockDelay(resolve)
-    //   );
-    //   return [{ delay }];
-    // } catch (err) {
-    //   console.warn("get screen lock info:", err);
-    // }
-
     try {
       // @ts-ignore
       const delay = await chrome.idle.getAutoLockDelay();
