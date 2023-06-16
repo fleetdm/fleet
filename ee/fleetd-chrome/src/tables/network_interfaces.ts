96edf391e89e39e127e111826658dcf42a063303
RachelElysia <rachel@fleetdm.com>
2023-06-16 11:21:13 -0400

    Remove exception capture handling so error will be sent to fleet via background.ts error handling

--- ee/fleetd-chrome/src/tables/network_interfaces.ts
+++ ee/fleetd-chrome/src/tables/network_interfaces.ts
@@ -5,16 +5,12 @@ export default class TableNetworkInterfaces extends Table {
   columns = ["mac", "ipv4", "ipv6"];
 
   async generate() {
-    let ipv4: string, ipv6: string, mac: string;
-    try {
-      // @ts-expect-error @types/chrome doesn't yet have the getNetworkDetails Promise API.
-      const networkDetails = (await chrome.enterprise.networkingAttributes.getNetworkDetails()) as chrome.enterprise.networkingAttributes.NetworkDetails;
-      ipv4 = networkDetails.ipv4;
-      ipv6 = networkDetails.ipv6;
-      mac = networkDetails.macAddress;
-    } catch (err) {
-      console.warn(`get network details: ${err}`);
-    }
+    // @ts-expect-error @types/chrome doesn't yet have the getNetworkDetails Promise API.
+    const networkDetails = (await chrome.enterprise.networkingAttributes.getNetworkDetails()) as chrome.enterprise.networkingAttributes.NetworkDetails;
+    const ipv4 = networkDetails.ipv4;
+    const ipv6 = networkDetails.ipv6;
+    const mac = networkDetails.macAddress;
+
     return [
       {
         mac,
