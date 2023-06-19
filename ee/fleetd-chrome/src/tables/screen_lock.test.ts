0af091fbb3b1e803b7bdbfbcdeb4dc9854a3053e
RachelElysia <rachel@fleetdm.com>
2023-06-19 10:31:52 -0400

    Work towards screenlock test

--- ee/fleetd-chrome/src/tables/screen_lock.test.ts
+++ ee/fleetd-chrome/src/tables/screen_lock.test.ts
@@ -2,9 +2,14 @@ import VirtualDatabase from "../db";
 
 describe("screen_lock", () => {
   test("success", async () => {
-    chrome.idle.getAutoLockDelay = jest.fn(() =>
-      Promise.resolve({ delay: 600 })
-    );
+    //   global.chrome.idle.getAutoLockDelay = jest.fn(() =>
+    //     Promise.resolve({ delay: 600 })
+    //   );
+
+    // @ts-expect-error Typescript doesn't include the userAgentData API yet.
+    global.chrome.idle = {
+      getAutoLockDelay: jest.fn(() => Promise.resolve({ delay: 600 })),
+    };
 
     const db = await VirtualDatabase.init();
     const res = await db.query("select * from screen_lock");
@@ -13,5 +18,5 @@ describe("screen_lock", () => {
         delay: 600,
       },
     ]);
-  });
+  }, 30000);
 });
