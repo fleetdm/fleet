bd4d8acc202f19f168f4c1905514b7c141b2d45d
Martin Angers <martin.n.angers@gmail.com>
2023-06-19 16:55:10 -0400

    Refactor registry win server check

--- orbit/pkg/update/execwinapi_windows.go
+++ orbit/pkg/update/execwinapi_windows.go
@@ -12,19 +12,27 @@ import (
 // Windows and tested on a Windows machine). Otherwise not meant to be called
 // from outside this package.
 func RunWindowsMDMEnrollment() error {
-	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows NT\CurrentVersion`, registry.QUERY_VALUE)
+	installType, err := readInstallationType()
 	if err != nil {
 		return err
+	}
+	if strings.ToLower(installType) == "server" {
+		// do not enroll, it is a server
+		return errIsWindowsServer
+	}
+	return nil
+}
+
+func readInstallationType() (string, error) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Windows NT\CurrentVersion`, registry.QUERY_VALUE)
+	if err != nil {
+		return "", err
 	}
 	defer k.Close()
 
 	s, _, err := k.GetStringValue("InstallationType")
 	if err != nil {
-		return err
+		return "", err
 	}
-	if strings.ToLower(s) == "server" {
-		// do not enroll, it is a server
-		return errIsWindowsServer
-	}
-	return nil
+	return s, nil
 }
