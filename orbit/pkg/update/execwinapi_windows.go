be24136f5755c5d81162f113876bcd2b5528fb25
Martin Angers <martin.n.angers@gmail.com>
2023-06-21 15:52:25 -0400

    Refactor improvement of windows API/code error message

--- orbit/pkg/update/execwinapi_windows.go
+++ orbit/pkg/update/execwinapi_windows.go
@@ -22,6 +22,10 @@ var (
 	// RegisterDeviceWithManagement registers a device with a MDM service:
 	// https://learn.microsoft.com/en-us/windows/win32/api/mdmregistration/nf-mdmregistration-registerdevicewithmanagement
 	procRegisterDeviceWithManagement *windows.LazyProc = dllMDMRegistration.NewProc("RegisterDeviceWithManagement")
+
+	// UnregisterDeviceWithManagement unregisters a device from a MDM service:
+	// https://learn.microsoft.com/en-us/windows/win32/api/mdmregistration/nf-mdmregistration-unregisterdevicewithmanagement
+	procUnregisterDeviceWithManagement *windows.LazyProc = dllMDMRegistration.NewProc("UnregisterDeviceWithManagement")
 )
 
 // Exported so that it can be used in tools/ (so that it can be built for
@@ -51,8 +55,7 @@ func RunWindowsMDMUnenrollment(args WindowsMDMEnrollmentArgs) error {
 		// do not unenroll, it is a server
 		return errIsWindowsServer
 	}
-	// TODO(mna): implement unenrollment call to UnregisterDeviceWithManagement
-	return nil
+	return unenrollHostFromMDM()
 }
 
 func readInstallationType() (string, error) {
@@ -97,7 +100,8 @@ func enrollHostToMDM(args MicrosoftMDMEnrollmentArgs) error {
 	}
 
 	// pre-load the DLL and pre-find the procedure, to return a more meaningful
-	// message if those steps fail and avoid a panic.
+	// message if those steps fail and avoid a panic (those are no-ops once
+	// loaded/found).
 	if err := dllMDMRegistration.Load(); err != nil {
 		return fmt.Errorf("load MDM dll: %w", err)
 	}
@@ -112,29 +116,64 @@ func enrollHostToMDM(args MicrosoftMDMEnrollmentArgs) error {
 	)
 	log.Debug().Msgf("RegisterDeviceWithManagement returned code: %#x ; message: %v", code, err)
 	if code != uintptr(windows.ERROR_SUCCESS) {
-		// hexadecimal error code can help identify error here:
-		//   https://learn.microsoft.com/en-us/windows/win32/mdmreg/mdm-registration-constants
-		// decimal error code can help identify error here (look for the ERROR_xxx constants):
-		//   https://pkg.go.dev/golang.org/x/sys/windows#pkg-constants
-		//
-		// Note that the error message may be "The operation completed
-		// successfully." even though there is an error (e.g. if the discovery URL
-		// results in a 404 not found, the error code will be 0x80190194 which
-		// means windows.HTTP_E_STATUS_NOT_FOUND). In this case, translate the
-		// message to something more useful.
-		if httpCode := code - uintptr(windows.HTTP_E_STATUS_BAD_REQUEST); httpCode >= 0 && httpCode < 200 {
-			// status bad request is 400, so if error code is between 400 and < 600.
-			err = fmt.Errorf("using discovery URL %q: HTTP error code %d", args.DiscoveryURL, http.StatusBadRequest+httpCode)
-		}
-		return fmt.Errorf("RegisterDeviceWithManagement failed: %s (%#x - %[2]d)", err, code)
+		return improveWindowsAPIError("RegisterDeviceWithManagement", args.DiscoveryURL, code, err)
 	}
 
 	return nil
 }
 
+// Perform the host MDM unenrollment process using MS-MDE protocol:
+// https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-mde/5c841535-042e-489e-913c-9d783d741267
+func unenrollHostFromMDM() error {
+	// we use an empty enrollment ID, it is not required
+	enrollID, err := syscall.UTF16PtrFromString("")
+	if err != nil {
+		return fmt.Errorf("enrollment ID to UTF16 pointer: %w", err)
+	}
+
+	// pre-load the DLL and pre-find the procedure, to return a more meaningful
+	// message if those steps fail and avoid a panic (those are no-ops once
+	// loaded/found).
+	if err := dllMDMRegistration.Load(); err != nil {
+		return fmt.Errorf("load MDM dll: %w", err)
+	}
+	if err := procUnregisterDeviceWithManagement.Find(); err != nil {
+		return fmt.Errorf("find MDM UnregisterDeviceWithManagement procedure: %w", err)
+	}
+
+	code, _, err := procUnregisterDeviceWithManagement.Call(uintptr(unsafe.Pointer(enrollID)))
+	log.Debug().Msgf("UnregisterDeviceWithManagement returned code: %#x ; message: %v", code, err)
+	if code != uintptr(windows.ERROR_SUCCESS) {
+		return improveWindowsAPIError("UnregisterDeviceWithManagement", "", code, err)
+	}
+
+	return nil
+}
+
+func improveWindowsAPIError(apiFunc, discoURL string, code uintptr, err error) error {
+	// hexadecimal error code can help identify error here:
+	//   https://learn.microsoft.com/en-us/windows/win32/mdmreg/mdm-registration-constants
+	// decimal error code can help identify error here (look for the ERROR_xxx constants):
+	//   https://pkg.go.dev/golang.org/x/sys/windows#pkg-constants
+	//
+	// Note that the error message may be "The operation completed
+	// successfully." even though there is an error (e.g. if the discovery URL
+	// results in a 404 not found, the error code will be 0x80190194 which
+	// means windows.HTTP_E_STATUS_NOT_FOUND). In this case, translate the
+	// message to something more useful.
+	if httpCode := code - uintptr(windows.HTTP_E_STATUS_BAD_REQUEST); httpCode >= 0 && httpCode < 200 {
+		// status bad request is 400, so if error code is between 400 and < 600.
+		if discoURL != "" {
+			err = fmt.Errorf("using discovery URL %q: HTTP error code %d", discoURL, http.StatusBadRequest+httpCode)
+		} else {
+			err = fmt.Errorf("HTTP error code %d", http.StatusBadRequest+httpCode)
+		}
+	}
+	return fmt.Errorf("%s failed: %s (%#x - %[3]d)", apiFunc, err, code)
+}
+
 func generateWindowsMDMAccessTokenPayload(args MicrosoftMDMEnrollmentArgs) ([]byte, error) {
 	var pld fleet.MicrosoftMDMAccessTokenPayload
-
 	pld.Type = fleet.MicrosoftMDMProgrammaticEnrollmentType // always programmatic for now
 	pld.Payload.HostUUID = args.HostUUID
 	return json.Marshal(pld)
--- tools/windows-mdm-enroll/main.go
+++ tools/windows-mdm-enroll/main.go
@@ -11,12 +11,19 @@ func main() {
 	var (
 		discoveryURL = flag.String("discovery-url", "", "The Windows MDM discovery URL")
 		hostUUID     = flag.String("host-uuid", "", "The Host UUID")
+		unenroll     = flag.Bool("unenroll", false, "Unenroll from MDM instead of enrolling")
 	)
 	flag.Parse()
+
+	if *unenroll {
+		err := update.RunMicrosoftMDMUnenrollment(update.MicrosoftMDMEnrollmentArgs{})
+		fmt.Println("unenrollment: ", err)
+		return
+	}
 
 	err := update.RunMicrosoftMDMEnrollment(update.MicrosoftMDMEnrollmentArgs{
 		DiscoveryURL: *discoveryURL,
 		HostUUID:     *hostUUID,
 	})
-	fmt.Println(err)
+	fmt.Println("enrollment: ", err)
 }
