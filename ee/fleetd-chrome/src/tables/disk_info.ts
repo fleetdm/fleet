682ecb28955be121f9c621c207a886cdad74dc8b
Sharon Katz <sharon@fleetdm.com>
2023-06-21 14:21:57 -0400

    remove try-catch

--- ee/fleetd-chrome/src/tables/disk_info.ts
+++ ee/fleetd-chrome/src/tables/disk_info.ts
@@ -6,18 +6,14 @@ export default class TableDiskInfo extends Table {
 
   async generate() {
     let rows = [];
-    try {
-      const disks = (await chrome.system.storage.getInfo()) as chrome.system.storage.StorageUnitInfo[];
-      for (let d of disks) {
-        rows.push({
-          capacity: d.capacity,
-          id: d.id,
-          name: d.name,
-          type: d.type,
-        });
-      }
-    } catch (err) {
-      console.warn(`get disk info: ${err}`);
+    const disks = (await chrome.system.storage.getInfo()) as chrome.system.storage.StorageUnitInfo[];
+    for (let d of disks) {
+      rows.push({
+        capacity: d.capacity,
+        id: d.id,
+        name: d.name,
+        type: d.type,
+      });
     }
     return rows;
   }
