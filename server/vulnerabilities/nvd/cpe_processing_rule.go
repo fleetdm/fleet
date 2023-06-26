ae04e828e34c19d0fd1bf40d728e474eab41b5c9
Juan Fernandez <juan@fleetdm.com>
2023-06-26 16:03:05 -0400

    WIP

--- server/vulnerabilities/nvd/cpe_processing_rule.go
+++ server/vulnerabilities/nvd/cpe_processing_rule.go
@@ -1,52 +1,80 @@
 package nvd
 
 import (
+	"github.com/Masterminds/semver"
 	"github.com/facebookincubator/nvdtools/wfn"
 )
 
-// CPEProcessingRuleAction what to do if the rules match
+// CPEProcessingRuleAction what to do if the rules matches
 type CPEProcessingRuleAction struct {
 	Skip bool `json:"skip"`
 }
 
-// CPEProcessingRuleAttributes CPE attributes
-type CPEProcessingRuleAttributes struct {
-	Vendor   string `json:"vendor"`
-	Product  string `json:"product"`
-	Version  string `json:"version"`
-	TargetSW string `json:"target_sw"`
+// versionRange represents an inclusive version range
+type versionRange struct {
+	lower *semver.Version
+	upper *semver.Version
 }
 
-// CPEProcessingRule specifies something we want to do post processing
+// CPEProcessingRule specifies an action that needs to take place if both
+// the CVE and the CPE attributes match
 type CPEProcessingRule struct {
-	Attributes CPEProcessingRuleAttributes `json:"cpe_attributes"`
-	CVE        string                      `json:"cve"`
-	Action     CPEProcessingRuleAction     `json:"action"`
+	Vendor      string                  `json:"vendor"`
+	Product     string                  `json:"product"`
+	TargetSW    string                  `json:"target_sw"`
+	SemVerLower string                  `json:"sem_ver_lower"`
+	SemVerUpper string                  `json:"sem_ver_upper"`
+	CVE         string                  `json:"cve"`
+	Action      CPEProcessingRuleAction `json:"action"`
 }
 
 type CPEProcessingRules []CPEProcessingRule
 
-// WfnAttrs Maps the processing rule CPE attributes to a wfn Attributes
-func (rule CPEProcessingRule) CPEAttrs() *wfn.Attributes {
+// getCPEAttrs maps the processing rule to its CPE attributes
+func (rule CPEProcessingRule) getCPEAttrs() *wfn.Attributes {
 	return &wfn.Attributes{
-		Vendor:   rule.Attributes.Vendor,
-		Product:  rule.Attributes.Product,
-		TargetSW: rule.Attributes.TargetSW,
+		Vendor:   rule.Vendor,
+		Product:  rule.Product,
+		TargetSW: rule.TargetSW,
 	}
 }
 
-// Matches Returns true if both the CPE and CVE match the rule
+// getVerRange returns a inclusive version range based
+func (rule CPEProcessingRule) getVerRange() (versionRange, error) {
+	lower, err := semver.NewVersion(rule.SemVerLower)
+	if err != nil {
+		return versionRange{}, err
+	}
+
+	upper, err := semver.NewVersion(rule.SemVerUpper)
+	if err != nil {
+		return versionRange{}, err
+	}
+
+	return versionRange{
+		lower: lower,
+		upper: upper,
+	}, nil
+}
+
+// Matches returns true if both the provided software and CVE match the rule.
 func (rule CPEProcessingRule) Matches(software softwareCPEWithNVDMeta, cve string) bool {
 	if cve != rule.CVE {
 		return false
 	}
 
-	if software.meta == nil || !software.meta.MatchWithoutVersion(rule.CPEAttrs()) {
+	if software.meta == nil || !software.meta.MatchWithoutVersion(rule.getCPEAttrs()) {
 		return false
 	}
 
 	// Check if versions match
-	return true
+	verRange, err := rule.getVerRange()
+	if err != nil {
+	}
+
+	softwareVer, _ := semver.NewVersion(software.meta.Version)
+	return (verRange.lower.GreaterThan(softwareVer) || verRange.lower.Equal(softwareVer)) &&
+		(verRange.upper.LessThan(softwareVer) || verRange.lower.Equal(softwareVer))
 }
 
 // FindMatch returns the first matching rule
