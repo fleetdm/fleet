f4ac2c815f81f6487f6ae21752de455b55694d99
Jacob Shandling <jacob@fleetdm.com>
2023-07-17 10:55:34 -0700

    Use TextCell formatted val as condition

--- frontend/components/TableContainer/DataTable/TextCell/TextCell.tsx
+++ frontend/components/TableContainer/DataTable/TextCell/TextCell.tsx
@@ -51,7 +51,7 @@ const TextCell = ({
 
   return (
     <span className={`text-cell ${classes} ${greyed && "grey-cell"}`}>
-      {val ? formatter(val) : renderEmptyCell()}
+      {formatter(val) || renderEmptyCell()}
     </span>
   );
 };
