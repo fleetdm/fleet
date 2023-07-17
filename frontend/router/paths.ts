c5a4fa60b7859f207ed937eb069be01e7f05bce5
Gabriel Hernandez <ghernandez345@gmail.com>
2023-07-17 15:51:40 +0100

    Feat UI update macos windows setup (#12744)
    relates to #12168
    
    Updates the fleet UI so that the macOS mdm setup flow is similar to the
    windows mdm setup. This includes:
    
    **Adding a new macos setup card:**
    
    
    ![image](https://github.com/fleetdm/fleet/assets/1153709/dc3371a6-864b-4af8-8e97-d716d5a51361)
    
    
    ![image](https://github.com/fleetdm/fleet/assets/1153709/5d7ebb54-6c80-45ac-86b2-93d452357b96)
    
    **Adding a new mac os mdm page on a new URL:**
    
    
    ![image](https://github.com/fleetdm/fleet/assets/1153709/30e42176-d4ab-4087-bde0-d74c81fde613)
    
    - [x] Changes file added for user-visible changes in `changes/` or
    `orbit/changes/`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [x] Manual QA for all new/changed functionality

--- frontend/router/paths.ts
+++ frontend/router/paths.ts
@@ -22,6 +22,7 @@ export default {
   ADMIN_INTEGRATIONS: `${URL_PREFIX}/settings/integrations`,
   ADMIN_INTEGRATIONS_TICKET_DESTINATIONS: `${URL_PREFIX}/settings/integrations/ticket-destinations`,
   ADMIN_INTEGRATIONS_MDM: `${URL_PREFIX}/settings/integrations/mdm`,
+  ADMIN_INTEGRATIONS_MDM_MAC: `${URL_PREFIX}/settings/integrations/mdm/apple`,
   ADMIN_INTEGRATIONS_MDM_WINDOWS: `${URL_PREFIX}/settings/integrations/mdm/windows`,
   ADMIN_INTEGRATIONS_AUTOMATIC_ENROLLMENT: `${URL_PREFIX}/settings/integrations/automatic-enrollment`,
   ADMIN_TEAMS: `${URL_PREFIX}/settings/teams`,
