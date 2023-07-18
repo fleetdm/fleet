f73b3c0a7c02a8e170729d7f998129a6b4fa47b8
RachelElysia <rachel@fleetdm.com>
2023-07-18 15:04:55 -0400

    Rename helpers.js to validate.js to match other adjacent directories

--- frontend/components/forms/ForgotPasswordForm/ForgotPasswordForm.jsx
+++ frontend/components/forms/ForgotPasswordForm/ForgotPasswordForm.jsx
@@ -4,12 +4,11 @@ import PropTypes from "prop-types";
 import Button from "components/buttons/Button";
 import Form from "components/forms/Form";
 import formFieldInterface from "interfaces/form_field";
-import helpers from "components/forms/ForgotPasswordForm/helpers";
 import InputFieldWithIcon from "components/forms/fields/InputFieldWithIcon";
+import validate from "./validate";
 
 const baseClass = "forgot-password-form";
 const fieldNames = ["email"];
-import validate from "./validate";
 
 class ForgotPasswordForm extends Component {
   static propTypes = {
