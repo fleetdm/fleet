cd098d8ca3c3e638397db4a16b0244e31901bf16
Gabriel Hernandez <ghernandez345@gmail.com>
2023-07-21 16:45:21 +0100

    add helper variables

--- frontend/utilities/roleBaseAccessControls/permissions.ts
+++ frontend/utilities/roleBaseAccessControls/permissions.ts
@@ -1,13 +1,12 @@
+// Helper variables to make writing frequently used roles more concise
+const allAdmins = ["global-admin", "team-admin"];
+const allMaintainers = ["global-maintainer", "team-maintainer"];
+
 // This is a mapping of the application actions to the roles that have
 // permission to perform them.
 export const permissions = {
   // host actions
-  "host.create": [
-    "global-admin",
-    "global-maintainer",
-    "team-admin",
-    "team-maintainer",
-  ],
+  "host.create": [...allAdmins, ...allMaintainers],
 
   // label actions
   "label.create": ["global-admin", "global-maintainer"],
