cd44e9914df4226ee2abb03840da035c80754382
Roberto Dip <dip.jesusr@gmail.com>
2023-07-24 20:10:51 -0300

    add missing line

--- ee/tools/puppet/fleetdm/lib/puppet/util/fleet_client.rb
+++ ee/tools/puppet/fleetdm/lib/puppet/util/fleet_client.rb
@@ -109,6 +109,7 @@ module Puppet::Util
     def req(method: :get, path: '', body: nil, headers: {}, cached: false)
       node_name = Puppet[:node_name_value]
       node = Puppet::Node.new(node_name)
+      node.environment = Puppet.lookup(:current_environment).name.to_s
       compiler = Puppet::Parser::Compiler.new(node)
       scope = Puppet::Parser::Scope.new(compiler)
       lookup_invocation = Puppet::Pops::Lookup::Invocation.new(scope, {}, {}, nil)
