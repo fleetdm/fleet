a8455906ec05a72ca9201779316c4cfa9fbf8566
Eric <eashaw@sailsjs.com>
2023-07-26 17:04:06 -0500

    Update self-service license dispenser

--- website/api/controllers/customers/save-billing-info-and-subscribe.js
+++ website/api/controllers/customers/save-billing-info-and-subscribe.js
@@ -60,6 +60,11 @@ module.exports = {
       responseType: 'badRequest'
     },
 
+    cardVerificationRequired: {
+      description: 'The billing card provided requires additional verfication before it can be used.',
+      responseType: 'badRequest'
+    },
+
   },
 
 
@@ -112,6 +117,18 @@ module.exports = {
         },
       ],
     });
+
+    // Get the Stripe ID of the invoice for this subscription.
+    let latestInvoiceIdForThisSubscription = subscription.latest_invoice;
+
+    // Get the invoice from Stripe.
+    const invoice = await stripe.invoices.retrieve(latestInvoiceIdForThisSubscription);
+
+    if(!invoice.paid) {
+      // If the invoice is not paid, we will throw an error, and ask the customer to contact support.
+      // FUTURE: Send an invoice to the customer and update the recieve-from-stripe webhook to handle off-website invoice payments.
+      throw 'cardVerificationRequired';
+    }
 
     // Generate the license key for this subscription
     let licenseKey = await sails.helpers.createLicenseKey.with({
