27b92d8ca1f70f8f8348d7fafd3478ce987d2df2
Eric <eashaw@sailsjs.com>
2023-08-02 15:15:37 -0500

    Update provision-sandbox-instance-and-deliver-email.js

--- website/api/controllers/admin/provision-sandbox-instance-and-deliver-email.js
+++ website/api/controllers/admin/provision-sandbox-instance-and-deliver-email.js
@@ -32,8 +32,8 @@ module.exports = {
 
     let userToRemoveFromSandboxWaitlist = await User.findOne({id: userId});
 
-    if(userToRemoveFromSandboxWaitlist.fleetSandboxURL) {
-      throw new Error('Consistency error! When attempting to provision a Fleet Sandbox instance for a user who is on the Sandbox Waitlist, the user associated with the provided ID has already been removed from the waitlist.');
+    if(!userToRemoveFromSandboxWaitlist.inSandboxWaitlist) {
+      throw new Error(`When attempting to provision a Fleet Sandbox instance for a user (id:${userId}) who is on the Sandbox Waitlist, the user account associated with the provided ID has already been removed from the waitlist.`);
     }
 
     const FIVE_DAYS_IN_MS = (5*24*60*60*1000);
@@ -43,77 +43,74 @@ module.exports = {
     // Creating a fleetSandboxDemoKey, this will be used for the user's password when we log them into their Sandbox instance.
     let fleetSandboxDemoKey = await sails.helpers.strings.uuid();
 
-    // // Send a POST request to the cloud provisioner API
-    // let cloudProvisionerResponseData = await sails.helpers.http.post(
-    //   'https://sandbox.fleetdm.com/new',
-    //   { // Request body
-    //     'name': userToRemoveFromSandboxWaitlist.firstName + ' ' + userToRemoveFromSandboxWaitlist.lastName,
-    //     'email': userToRemoveFromSandboxWaitlist.emailAddress,
-    //     'password': fleetSandboxDemoKey, //« this provisioner API was originally designed to accept passwords, but rather than specifying the real plaintext password, since users always access Fleet Sandbox from their fleetdm.com account anyway, this generated demo key is used instead to avoid any confusion
-    //     'sandbox_expiration': new Date(fleetSandboxExpiresAt).toISOString(), // sending expiration_timestamp as an ISO string.
-    //   },
-    //   { // Request headers
-    //     'Authorization':sails.config.custom.cloudProvisionerSecret
-    //   }
-    // )
-    // .timeout(10000)
-    // .intercept(['requestFailed', 'non200Response'], (err)=>{
-    //   // If we received a non-200 response from the cloud provisioner API, we'll throw a 500 error.
-    //   return new Error('When attempting to provision a Sandbox instance for a user on the Fleet Sandbox waitlist ('+emailAddress+'), the cloud provisioner gave a non 200 response. Raw response received from provisioner: '+err.stack);
-    // })
-    // .intercept({name: 'TimeoutError'},(err)=>{
-    //   // If the request timed out, log a warning and return a 'requestToSandboxTimedOut' response.
-    //   sails.log.warn('When attempting to provision a Sandbox instance for a user on the Fleet Sandbox waitlist ('+emailAddress+'), the request to the cloud provisioner took over timed out. Raw error: '+err.stack);
-    //   return 'requestToSandboxTimedOut';
-    // });
+    // Send a POST request to the cloud provisioner API
+    let cloudProvisionerResponseData = await sails.helpers.http.post(
+      'https://sandbox.fleetdm.com/new',
+      { // Request body
+        'name': userToRemoveFromSandboxWaitlist.firstName + ' ' + userToRemoveFromSandboxWaitlist.lastName,
+        'email': userToRemoveFromSandboxWaitlist.emailAddress,
+        'password': fleetSandboxDemoKey, //« this provisioner API was originally designed to accept passwords, but rather than specifying the real plaintext password, since users always access Fleet Sandbox from their fleetdm.com account anyway, this generated demo key is used instead to avoid any confusion
+        'sandbox_expiration': new Date(fleetSandboxExpiresAt).toISOString(), // sending expiration_timestamp as an ISO string.
+      },
+      { // Request headers
+        'Authorization':sails.config.custom.cloudProvisionerSecret
+      }
+    )
+    .timeout(10000)
+    .intercept(['requestFailed', 'non200Response'], (err)=>{
+      // If we received a non-200 response from the cloud provisioner API, we'll throw a 500 error.
+      return new Error('When attempting to provision a Sandbox instance for a user on the Fleet Sandbox waitlist ('+emailAddress+'), the cloud provisioner gave a non 200 response. Raw response received from provisioner: '+err.stack);
+    })
+    .intercept({name: 'TimeoutError'},(err)=>{
+      // If the request timed out, log a warning and return a 'requestToSandboxTimedOut' response.
+      sails.log.warn('When attempting to provision a Sandbox instance for a user on the Fleet Sandbox waitlist ('+emailAddress+'), the request to the cloud provisioner took over timed out. Raw error: '+err.stack);
+      return 'requestToSandboxTimedOut';
+    });
 
-    // if(!cloudProvisionerResponseData.URL) {
-    //   // If we didn't receive a URL in the response from the cloud provisioner API, we'll throwing an error before we save the new user record and the user will need to try to sign up again.
-    //   throw new Error(
-    //     `When provisioning a Fleet Sandbox instance for a user on the Fleet Sandbox waitlist (${emailAddress}), the response data from the cloud provisioner API was malformed. It did not contain a valid Fleet Sandbox instance URL in its expected "URL" property.
-    //     Here is the malformed response data (parsed response body) from the cloud provisioner API: ${cloudProvisionerResponseData}`
-    //   );
-    // }
+    if(!cloudProvisionerResponseData.URL) {
+      // If we didn't receive a URL in the response from the cloud provisioner API, we'll throwing an error before we save the new user record and the user will need to try to sign up again.
+      throw new Error(
+        `When provisioning a Fleet Sandbox instance for a user on the Fleet Sandbox waitlist (${emailAddress}), the response data from the cloud provisioner API was malformed. It did not contain a valid Fleet Sandbox instance URL in its expected "URL" property.
+        Here is the malformed response data (parsed response body) from the cloud provisioner API: ${cloudProvisionerResponseData}`
+      );
+    }
 
+    // Start polling the /healthz endpoint of the created Fleet Sandbox instance, once it returns a 200 response, we'll continue.
+    await sails.helpers.flow.until( async()=>{
+      let healthCheckResponse = await sails.helpers.http.sendHttpRequest('GET', cloudProvisionerResponseData.URL+'/healthz')
+      .timeout(5000)
+      .tolerate('non200Response')
+      .tolerate('requestFailed')
+      .tolerate({name: 'TimeoutError'});
+      if(healthCheckResponse) {
+        return true;
+      }
+    }, 10000).intercept('tookTooLong', ()=>{
+      return new Error('This newly provisioned Fleet Sandbox instance (for '+emailAddress+') is taking too long to respond with a 2xx status code, even after repeatedly polling the health check endpoint.  Note that failed requests and non-2xx responses from the health check endpoint were ignored during polling.  Search for a bit of non-dynamic text from this error message in the fleetdm.com source code for more info on exactly how this polling works.');
+    });
 
-    // // Start polling the /healthz endpoint of the created Fleet Sandbox instance, once it returns a 200 response, we'll continue.
-    // await sails.helpers.flow.until( async()=>{
-    //   let healthCheckResponse = await sails.helpers.http.sendHttpRequest('GET', cloudProvisionerResponseData.URL+'/healthz')
-    //   .timeout(5000)
-    //   .tolerate('non200Response')
-    //   .tolerate('requestFailed')
-    //   .tolerate({name: 'TimeoutError'});
-    //   if(healthCheckResponse) {
-    //     return true;
-    //   }
-    // }, 10000).intercept('tookTooLong', ()=>{
-    //   return new Error('This newly provisioned Fleet Sandbox instance (for '+emailAddress+') is taking too long to respond with a 2xx status code, even after repeatedly polling the health check endpoint.  Note that failed requests and non-2xx responses from the health check endpoint were ignored during polling.  Search for a bit of non-dynamic text from this error message in the fleetdm.com source code for more info on exactly how this polling works.');
-    // });
 
     await User.updateOne({id: userId}).set({
-      // fleetSandboxURL: cloudProvisionerResponseData.URL,
-      fleetSandboxURL: 'example.com',
+      fleetSandboxURL: cloudProvisionerResponseData.URL,
       fleetSandboxExpiresAt,
       fleetSandboxDemoKey,
       inSandboxWaitlist: false,
-    })
+    });
+
+
+    // Send the user an email to let them know that their Fleet sandbox instance is ready.
     await sails.helpers.sendTemplateEmail.with({
       to: userToRemoveFromSandboxWaitlist.emailAddress,
       from: sails.config.custom.fromEmailAddress,
       fromName: sails.config.custom.fromName,
-      subject: 'Your Fleet Sandbox instance is ready!', // TODO better subject
+      subject: 'Your Fleet Sandbox instance is ready!',
       template: 'email-sandbox-ready-approved',
       templateData: {},
     });
 
-    await setTimeout(async ()=>{
-      return;
-    }, 10000);
-
-    // Send the user an email to let them know that their Fleet sandbox instance is ready.
-
 
     // All done.
+    return;
 
   }
 
