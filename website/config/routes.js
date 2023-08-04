0efde9d974a516d199e29d0ea5e12f16774ef0cb
Sampfluger88 <108141731+Sampfluger88@users.noreply.github.com>
2023-08-04 10:29:14 -0500

    Update routes.js

--- website/config/routes.js
+++ website/config/routes.js
@@ -400,6 +400,7 @@ module.exports.routes = {
   'GET /platform': '/',
   'GET /handbook/company/senior-software-backend-engineer': 'https://www.linkedin.com/posts/mikermcneil_in-addition-to-our-product-quality-specialist-activity-7067711903166279680-6CMH',
   'GET /handbook/business-operations/ceo-handbook': '/handbook/company/ceo-handbook',
+  'GET /handbook/business-operations#people-operations': '/handbook/business-operations/people-operations',
 
   'GET /docs': '/docs/get-started/why-fleet',
   'GET /docs/get-started': '/docs/get-started/why-fleet',
