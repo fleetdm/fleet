8796316dc0540ea667818e7ae7dca021a7f93bd6
Luke Heath <luke@fleetdm.com>
2023-08-04 14:56:36 -0700

    Prepare v4.35.1

--- infrastructure/sandbox/JITProvisioner/jitprovisioner.tf
+++ infrastructure/sandbox/JITProvisioner/jitprovisioner.tf
@@ -206,7 +206,7 @@ resource "random_uuid" "jitprovisioner" {
 
 # Use the local to make the trigger work.
 locals {
-  fleet_tag = "v4.35.0"
+  fleet_tag = "v4.35.1"
 }
 
 resource "null_resource" "standard-query-library" {
