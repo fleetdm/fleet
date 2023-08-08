dce74f584fb76dd7934f8658b8904c875b385271
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2023-08-08 16:26:32 -0400

    fixup

--- terraform/addons/external-vuln-scans/main.tf
+++ terraform/addons/external-vuln-scans/main.tf
@@ -59,6 +59,18 @@ resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
       {
         name    = "fleet",
         command = ["fleet", "vuln_processing"]
+      },
+      {
+        resourceRequirements = [
+          {
+            type  = "VCPU",
+            value = "1"
+          },
+          {
+            type  = "MEMORY",
+            value = "4096"
+          }
+        ]
       }
     ]
   })
