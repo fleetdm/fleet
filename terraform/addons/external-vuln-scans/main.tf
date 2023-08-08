a2709487d9f9713aa8b2bb9dd2e67beea2ddf088
zwinnerman-fleetdm <zwinnerman@fleetdm.com>
2023-08-08 13:54:13 -0400

    Add external vuln scans addon

---
+++ terraform/addons/external-vuln-scans/main.tf
@@ -0,0 +1,65 @@
+data "aws_region" "current" {}
+
+resource "aws_cloudwatch_event_rule" "main" {
+  schedule_expression = "rate(1h)"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "ecs_events" {
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "ecs_events_run_task_with_any_role" {
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:PassRole"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["ecs:RunTask"]
+    resources = [replace(var.task_definition.arn, "/:\\d+$/", ":*")]
+  }
+}
+resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
+  role   = aws_iam_role.ecs_events.id
+  policy = data.aws_iam_policy_document.ecs_events_run_task_with_any_role.json
+}
+
+resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
+  arn      = var.ecs_cluster.cluster_arn
+  rule     = aws_cloudwatch_event_rule.main.name
+  role_arn = aws_iam_role.ecs_events.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = var.task_definition.arn
+    launch_type         = "FARGATE"
+    network_configuration {
+      subnets         = var.ecs_service.network_configuration.subnets
+      security_groups = var.ecs_service.network_configuration.security_groups
+    }
+  }
+
+  input = jsonencode({
+    containerOverrides = [
+      {
+        name    = "fleet",
+        command = ["vuln_processing"]
+      }
+    ]
+  })
+}
