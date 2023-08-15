087e2ed562bfbd1752c118f15f4dabd6b31f28f5
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2023-08-15 10:22:46 -0500

    Address comments

--- server/datastore/mysql/migrations/tables/20230807100822_AddIndexNanoEnrollment.go
+++ server/datastore/mysql/migrations/tables/20230807100822_AddIndexNanoEnrollment.go
@@ -8,6 +8,7 @@ func init() {
 	MigrationClient.AddMigration(Up_20230807100822, Down_20230807100822)
 }
 
+// This migration tracks the nanomdm repo. See https://github.com/micromdm/nanomdm/blob/main/storage/mysql/schema.00009.sql
 func Up_20230807100822(tx *sql.Tx) error {
 	_, err := tx.Exec(`ALTER TABLE nano_enrollment_queue ADD INDEX (priority DESC, created_at);`)
 
