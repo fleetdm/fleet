89d0dba2cc50324a5236a6fd1a3c9db198dfcdbe
Martin Angers <martin.n.angers@gmail.com>
2023-08-29 13:21:52 -0400

    Use -2 as exit code to signal that the host disabled script execution

--- orbit/pkg/scripts/scripts.go
+++ orbit/pkg/scripts/scripts.go
@@ -158,7 +158,7 @@ func (r *Runner) runOneDisabled(execID string) error {
 	err := r.Client.SaveHostScriptResult(&fleet.HostScriptResultPayload{
 		ExecutionID: execID,
 		Output:      "script execution disabled",
-		ExitCode:    -1,
+		ExitCode:    -2, // fleetctl knows that -2 means script was disabled on host
 	})
 	if err != nil {
 		return fmt.Errorf("save script result: %w", err)
