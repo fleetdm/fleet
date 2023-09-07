ac1444232ff764393a17c3b12420cb6af527ea7e
Gabriel Hernandez <ghernandez345@gmail.com>
2023-09-07 16:37:36 +0100

    change turn on mdm message so all users see the same message

--- frontend/pages/ManageControlsPage/components/TurnOnMdmMessage/TurnOnMdmMessage.tsx
+++ frontend/pages/ManageControlsPage/components/TurnOnMdmMessage/TurnOnMdmMessage.tsx
@@ -1,5 +1,4 @@
-import React, { useContext } from "react";
-import { AppContext } from "context/app";
+import React from "react";
 import PATHS from "router/paths";
 
 import EmptyTable from "components/EmptyTable";
@@ -13,38 +12,26 @@ interface ITurnOnMdmMessageProps {
 }
 
 const TurnOnMdmMessage = ({ router }: ITurnOnMdmMessageProps) => {
-  const { isGlobalAdmin } = useContext(AppContext);
-
-  const getInfoText = () => {
-    if (isGlobalAdmin) {
-      return "Turn on MDM to change settings on your hosts.";
-    }
-    return "Your Fleet administrator must turn on MDM to change settings on your hosts.";
-  };
-
   const onConnectClick = () => {
     router.push(PATHS.ADMIN_INTEGRATIONS_MDM);
   };
 
   const renderConnectButton = () => {
-    if (isGlobalAdmin) {
-      return (
-        <Button
-          variant="brand"
-          onClick={onConnectClick}
-          className={`${baseClass}__connectAPC-button`}
-        >
-          Turn on
-        </Button>
-      );
-    }
-    return <></>;
+    return (
+      <Button
+        variant="brand"
+        onClick={onConnectClick}
+        className={`${baseClass}__connectAPC-button`}
+      >
+        Turn on
+      </Button>
+    );
   };
 
   return (
     <EmptyTable
       header="Manage your macOS hosts"
-      info={getInfoText()}
+      info={"Turn on MDM to change settings on your hosts."}
       primaryButton={renderConnectButton()}
     />
   );
