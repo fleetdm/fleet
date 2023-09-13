db026b15b5c379abbd6e87136e637efa82c3877a
mostlikelee <tim@mostlikelee.com>
2023-09-13 09:54:57 -0600

    omit unused columns in select

--- server/datastore/mysql/migrations/tables/20230912101759_AddDescToCveMetaTable_test.go
+++ server/datastore/mysql/migrations/tables/20230912101759_AddDescToCveMetaTable_test.go
@@ -25,7 +25,7 @@ func TestUp_20230912101759(t *testing.T) {
 		Description *string `db:"description"`
 	}
 
-	err := db.Get(&cveMeta, "SELECT * FROM cve_meta WHERE cve = ?", cveVal)
+	err := db.Get(&cveMeta, "SELECT cve, description FROM cve_meta WHERE cve = ?", cveVal)
 	require.NoError(t, err)
 	require.Equal(t, cveVal, cveMeta.CVE)
 	require.Nil(t, cveMeta.Description)
@@ -39,7 +39,7 @@ func TestUp_20230912101759(t *testing.T) {
 	cveVal = "CVE-2010-3263"
 	descVal := "Cross-site scripting (XSS) vulnerability in setup/frames/index.inc.php in the setup script in phpMyAdmin 3.x before 3.3.7 allows remote attackers to inject arbitrary web script or HTML via a server name."
 	execNoErr(t, db, insertStmt, cveVal, descVal)
-	err = db.Get(&cveMeta, "SELECT * FROM cve_meta WHERE cve = ?", cveVal)
+	err = db.Get(&cveMeta, "SELECT cve, description FROM cve_meta WHERE cve = ?", cveVal)
 	require.NoError(t, err)
 	require.Equal(t, cveVal, cveMeta.CVE)
 	require.Equal(t, &descVal, cveMeta.Description)
