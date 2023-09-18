70dce088e206f1852368904ce02497d56b8f7474
mostlikelee <tim@mostlikelee.com>
2023-09-18 15:02:22 -0600

    don't update cve and swid on duplicate

--- server/datastore/mysql/software.go
+++ server/datastore/mysql/software.go
@@ -1436,9 +1436,7 @@ func (ds *Datastore) InsertSoftwareVulnerability(
 		INSERT INTO software_cve (cve, source, software_id, resolved_in_version) 
 		VALUES (?,?,?,?) 
 		ON DUPLICATE KEY UPDATE
-			cve = VALUES(cve),
 			source = VALUES(source),
-			software_id = VALUES(software_id),
 			resolved_in_version = VALUES(resolved_in_version),
 			updated_at=?
 	`
