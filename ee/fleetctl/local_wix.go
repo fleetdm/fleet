4e97d73b6b82a49d7d96fc4b98d064e3576dccbd
Jacob Shandling <jacob@fleetdm.com>
2023-09-22 16:25:41 -0700

    Adjust usage text; remove env var option

--- ee/fleetctl/local_wix.go
+++ ee/fleetctl/local_wix.go
@@ -4,11 +4,8 @@ import "github.com/urfave/cli/v2"
 
 func LocalWixDirFlag(dest *string) *cli.StringFlag {
 	return &cli.StringFlag{
-		Name: "local-wix-dir",
-		Usage: `Use local install of WiX instead of Docker Hub (only available on Windows w/ WiX v3).
-
-This functionality is licensed under the Fleet EE License. Usage requires a current Fleet EE subscription.`,
-		EnvVars:     []string{"FLEETCTL_LOCAL_WIX_DIR"},
+		Name:        "local-wix-dir",
+		Usage:       "Use local install of WiX instead of Docker Hub (only available on Windows w/ WiX v3). This functionality is licensed under the Fleet EE License. Usage requires a current Fleet EE subscription.",
 		Destination: dest,
 	}
 }
