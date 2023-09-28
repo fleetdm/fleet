88a3c52d72c43b27e2f9358328413bd0aecc576a
mostlikelee <tim@mostlikelee.com>
2023-09-28 06:54:09 -0700

    update test

--- server/datastore/mysql/migrations/tables/20230927155121_CreateTableQueryReports_test.go
+++ server/datastore/mysql/migrations/tables/20230927155121_CreateTableQueryReports_test.go
@@ -42,39 +42,48 @@ func TestUp_20230927155121(t *testing.T) {
 	_, err = db.Exec(insertStmt, queryID, hostID, osqueryVersion, errorMessage, lastFetched, nil)
 	require.NoError(t, err)
 
-	type QueryReport struct {
-		ID                uint      `db:"id"`
-		QueryID           uint      `db:"query_id"`
-		HostID            uint      `db:"host_id"`
-		OsqueryVersion    string    `db:"osquery_version"`
-		Error             string    `db:"error"`
-		LastFetched       time.Time `db:"last_fetched"`
-		OsqueryResultData string    `db:"data"`
+	type QueryResult struct {
+		ID                uint            `db:"id"`
+		QueryID           uint            `db:"query_id"`
+		HostID            uint            `db:"host_id"`
+		OsqueryVersion    string          `db:"osquery_version"`
+		Error             string          `db:"error"`
+		LastFetched       time.Time       `db:"last_fetched"`
+		OsqueryResultData *json.RawMessage `db:"data"`
 	}
 
-	// Load the report we just created
-	var report QueryReport
-	selectStmt := `SELECT id, query_id, host_id, osquery_version, error, last_fetched, data
-	FROM query_results
-	WHERE id = ?`
-	err = db.Get(&report, selectStmt, id)
+	// Load the 1st result
+	var queryReport []QueryResult
+	selectStmt := `
+		SELECT id, query_id, host_id, osquery_version, error, last_fetched, data
+		FROM query_results
+		WHERE query_id = ? AND host_id = ?
+		ORDER BY id ASC
+	`
+	err = db.Select(&queryReport, selectStmt, queryID, hostID)
 	require.NoError(t, err)
 
-	require.Equal(t, uint(id), report.ID)
-	require.Equal(t, queryID, report.QueryID)
-	require.Equal(t, hostID, report.HostID)
-	require.Equal(t, osqueryVersion, report.OsqueryVersion)
-	require.Empty(t, report.Error)
-	require.Equal(t, lastFetched.Truncate(time.Second), report.LastFetched.Truncate(time.Second))
-	require.JSONEq(t, string(jsonData), report.OsqueryResultData)
+	require.Equal(t, queryID, queryReport[0].QueryID)
+	require.Equal(t, hostID, queryReport[0].HostID)
+	require.Equal(t, osqueryVersion, queryReport[0].OsqueryVersion)
+	require.Empty(t, queryReport[0].Error)
+	require.True(t, lastFetched.Sub(queryReport[0].LastFetched) < time.Second)
+	require.JSONEq(t, string(jsonData), string(*queryReport[0].OsqueryResultData))
 
-	// Test cascading delete
-	// Delete the query we just created
+	// Error results should be loaded as well
+	require.Equal(t, queryID, queryReport[1].QueryID)
+	require.Equal(t, hostID, queryReport[1].HostID)
+	require.Equal(t, osqueryVersion, queryReport[1].OsqueryVersion)
+	require.Equal(t, errorMessage, queryReport[1].Error)
+	require.True(t, lastFetched.Sub(queryReport[1].LastFetched) < time.Second) // allow a 1 sec difference to account for time to run the query
+	require.Empty(t, queryReport[1].OsqueryResultData)
+
+	// Delete the query we just created to test the ON DELETE CASCADE
 	deleteQueryStmt := `DELETE FROM queries WHERE id = ?`
 	_, err = db.Exec(deleteQueryStmt, queryID)
 	require.NoError(t, err)
 
-	// Verify that the query_results record was deleted
+	// Verify that both query_result records were deleted
 	var count int
 	err = db.Get(&count, "SELECT COUNT(*) FROM query_results WHERE id = ?", id)
 	require.NoError(t, err)
