6116c16094751b8b5a4153aab578a7046c74aca4
Jacob Shandling <jacob@fleetdm.com>
2023-09-29 12:24:32 -0700

    mock now-necessary CSS.supports method

--- frontend/test/test-setup.ts
+++ frontend/test/test-setup.ts
@@ -2,6 +2,8 @@ import "@testing-library/jest-dom";
 
 import mockServer from "./mock-server";
 
+window.CSS.supports = jest.fn();
+
 // Mock server setup
 beforeAll(() => mockServer.listen());
 afterEach(() => mockServer.resetHandlers());
