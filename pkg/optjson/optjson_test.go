648dc853a4f2cd36f8a42ad8250374b8aa060f42
Roberto Dip <dip.jesusr@gmail.com>
2023-10-02 08:30:10 -0300

    add struct field names to literals

--- pkg/optjson/optjson_test.go
+++ pkg/optjson/optjson_test.go
@@ -97,11 +97,11 @@ func TestSlice(t *testing.T) {
 			wantRes   Slice[int]
 			marshalAs string
 		}{
-			{`[1,2,3]`, "", SetSlice([]int{1, 2, 3}), `[1,2,3]`},
-			{`[]`, "", SetSlice([]int{}), `[]`},
-			{`null`, "", Slice[int]{Set: true, Valid: false, Value: []int{}}, `null`},
-			{`[1,"2",3]`, "cannot unmarshal string", Slice[int]{Set: true, Valid: false, Value: nil}, `null`},
-			{`123`, "cannot unmarshal number", Slice[int]{Set: true, Valid: false, Value: []int(nil)}, `null`},
+			{data: `[1,2,3]`, wantErr: "", wantRes: SetSlice([]int{1, 2, 3}), marshalAs: `[1,2,3]`},
+			{data: `[]`, wantErr: "", wantRes: SetSlice([]int{}), marshalAs: `[]`},
+			{data: `null`, wantErr: "", wantRes: Slice[int]{Set: true, Valid: false, Value: []int{}}, marshalAs: `null`},
+			{data: `[1,"2",3]`, wantErr: "cannot unmarshal string", wantRes: Slice[int]{Set: true, Valid: false, Value: nil}, marshalAs: `null`},
+			{data: `123`, wantErr: "cannot unmarshal number", wantRes: Slice[int]{Set: true, Valid: false, Value: []int(nil)}, marshalAs: `null`},
 		}
 
 		for _, c := range cases {
@@ -131,10 +131,10 @@ func TestSlice(t *testing.T) {
 			wantRes   Slice[string]
 			marshalAs string
 		}{
-			{`["foo", "bar"]`, "", SetSlice([]string{"foo", "bar"}), `["foo","bar"]`},
-			{`[""]`, "", SetSlice([]string{""}), `[""]`},
-			{`null`, "", Slice[string]{Set: true, Valid: false, Value: []string{}}, `null`},
-			{`["foo", 123]`, "cannot unmarshal number", Slice[string]{Set: true, Valid: false, Value: []string(nil)}, `null`},
+			{data: `["foo", "bar"]`, wantErr: "", wantRes: SetSlice([]string{"foo", "bar"}), marshalAs: `["foo","bar"]`},
+			{data: `[""]`, wantErr: "", wantRes: SetSlice([]string{""}), marshalAs: `[""]`},
+			{data: `null`, wantErr: "", wantRes: Slice[string]{Set: true, Valid: false, Value: []string{}}, marshalAs: `null`},
+			{data: `["foo", 123]`, wantErr: "cannot unmarshal number", wantRes: Slice[string]{Set: true, Valid: false, Value: []string(nil)}, marshalAs: `null`},
 		}
 
 		for _, c := range cases {
@@ -164,9 +164,9 @@ func TestSlice(t *testing.T) {
 			wantRes   Slice[bool]
 			marshalAs string
 		}{
-			{`[true, false]`, "", SetSlice([]bool{true, false}), `[true,false]`},
-			{`[true, "false"]`, "cannot unmarshal string", Slice[bool]{Set: true, Valid: false, Value: []bool(nil)}, `null`},
-			{`null`, "", Slice[bool]{Set: true, Valid: false, Value: []bool{}}, `null`},
+			{data: `[true, false]`, wantErr: "", wantRes: SetSlice([]bool{true, false}), marshalAs: `[true,false]`},
+			{data: `[true, "false"]`, wantErr: "cannot unmarshal string", wantRes: Slice[bool]{Set: true, Valid: false, Value: []bool(nil)}, marshalAs: `null`},
+			{data: `null`, wantErr: "", wantRes: Slice[bool]{Set: true, Valid: false, Value: []bool{}}, marshalAs: `null`},
 		}
 
 		for _, c := range cases {
@@ -210,34 +210,49 @@ func TestSliceWithinStruct(t *testing.T) {
 			wantRes   Parent
 			marshalAs string
 		}{
-			{`{}`, "", Parent{}, `{"id": 0, "name": "", "nested": {"numbers": null, "words": null, "flags": null}}`},
-			{`{"id": 1, "name": "test", "nested": {"numbers": [1, 2, 3], "words": ["one", "two"], "flags": [true, false]}}`, "", Parent{
-				ID:   1,
-				Name: "test",
-				Nested: Nested{
-					Numbers: SetSlice([]int{1, 2, 3}),
-					Words:   SetSlice([]string{"one", "two"}),
-					Flags:   SetSlice([]bool{true, false}),
+			{data: `{}`, wantErr: "", wantRes: Parent{}, marshalAs: `{"id": 0, "name": "", "nested": {"numbers": null, "words": null, "flags": null}}`},
+			{
+				data:    `{"id": 1, "name": "test", "nested": {"numbers": [1, 2, 3], "words": ["one", "two"], "flags": [true, false]}}`,
+				wantErr: "",
+				wantRes: Parent{
+					ID:   1,
+					Name: "test",
+					Nested: Nested{
+						Numbers: SetSlice([]int{1, 2, 3}),
+						Words:   SetSlice([]string{"one", "two"}),
+						Flags:   SetSlice([]bool{true, false}),
+					},
 				},
-			}, `{"id": 1, "name": "test", "nested": {"numbers": [1,2,3], "words": ["one","two"], "flags": [true,false]}}`},
-			{`{"id": 1, "name": "test", "nested": {"numbers": null, "words": ["one", "two"], "flags": [true, false]}}`, "", Parent{
-				ID:   1,
-				Name: "test",
-				Nested: Nested{
-					Numbers: Slice[int]{Set: true, Valid: false, Value: []int{}},
-					Words:   SetSlice([]string{"one", "two"}),
-					Flags:   SetSlice([]bool{true, false}),
+				marshalAs: `{"id": 1, "name": "test", "nested": {"numbers": [1,2,3], "words": ["one","two"], "flags": [true,false]}}`,
+			},
+			{
+				data:    `{"id": 1, "name": "test", "nested": {"numbers": null, "words": ["one", "two"], "flags": [true, false]}}`,
+				wantErr: "",
+				wantRes: Parent{
+					ID:   1,
+					Name: "test",
+					Nested: Nested{
+						Numbers: Slice[int]{Set: true, Valid: false, Value: []int{}},
+						Words:   SetSlice([]string{"one", "two"}),
+						Flags:   SetSlice([]bool{true, false}),
+					},
 				},
-			}, `{"id": 1, "name": "test", "nested": {"numbers": null, "words": ["one","two"], "flags": [true,false]}}`},
-			{`{"id": 1, "name": "test", "nested": {"numbers": [1, 2, 3], "words": null, "flags": [true, false]}}`, "", Parent{
-				ID:   1,
-				Name: "test",
-				Nested: Nested{
-					Numbers: SetSlice([]int{1, 2, 3}),
-					Words:   Slice[string]{Set: true, Valid: false, Value: []string{}},
-					Flags:   SetSlice([]bool{true, false}),
+				marshalAs: `{"id": 1, "name": "test", "nested": {"numbers": null, "words": ["one","two"], "flags": [true,false]}}`,
+			},
+			{
+				data:    `{"id": 1, "name": "test", "nested": {"numbers": [1, 2, 3], "words": null, "flags": [true, false]}}`,
+				wantErr: "",
+				wantRes: Parent{
+					ID:   1,
+					Name: "test",
+					Nested: Nested{
+						Numbers: SetSlice([]int{1, 2, 3}),
+						Words:   Slice[string]{Set: true, Valid: false, Value: []string{}},
+						Flags:   SetSlice([]bool{true, false}),
+					},
 				},
-			}, `{"id": 1, "name": "test", "nested": {"numbers": [1,2,3], "words": null, "flags": [true,false]}}`},
+				marshalAs: `{"id": 1, "name": "test", "nested": {"numbers": [1,2,3], "words": null, "flags": [true,false]}}`,
+			},
 		}
 
 		for _, c := range cases {
