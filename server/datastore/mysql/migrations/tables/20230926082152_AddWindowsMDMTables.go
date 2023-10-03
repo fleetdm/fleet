f1c125243fbc8b0494fcc61037fd4f818a8011ff
Martin Angers <martin.n.angers@gmail.com>
2023-10-03 09:11:48 -0400

    Make device_id part of the primary key, cmd_verb a short string

--- server/datastore/mysql/migrations/tables/20230926082152_AddWindowsMDMTables.go
+++ server/datastore/mysql/migrations/tables/20230926082152_AddWindowsMDMTables.go
@@ -13,13 +13,13 @@ func Up_20230926082152(tx *sql.Tx) error {
 	_, err := tx.Exec(`
 CREATE TABLE windows_mdm_commands (
     -- command uuid is fleet-generated, so that a command has a unique id as
-    -- soon as it is enqueued. It is unique per device, unlike macOS commands
-    -- that may target multiple hosts. Note that a UUID is 36 chars, but for
+    -- soon as it is enqueued. Note that a UUID is 36 chars, but for
     -- consistency with nano_mdm_commands we use 127 here.
     command_uuid    VARCHAR(127) NOT NULL,
     device_id       VARCHAR(255) NOT NULL,
 
-    cmd_verb        SMALLINT UNSIGNED NOT NULL,
+    -- verb is e.g. Add, Exec, Get, etc.
+    cmd_verb        VARCHAR(20)  NOT NULL,
     setting_uri     VARCHAR(255) NOT NULL,
     setting_value   VARCHAR(255) NOT NULL,
     priority        SMALLINT UNSIGNED NOT NULL DEFAULT 0,
@@ -27,7 +27,7 @@ CREATE TABLE windows_mdm_commands (
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (command_uuid)
+    PRIMARY KEY (command_uuid, device_id)
 )`)
 	if err != nil {
 		return fmt.Errorf("failed to create windows_mdm_commands table: %w", err)
@@ -42,7 +42,7 @@ CREATE TABLE windows_mdm_command_results (
     -- this is the MS-MDM protocol-specific command ID, not the Fleet command UUID.
     command_id      VARCHAR(255) NOT NULL,
 
-    cmd_verb        SMALLINT UNSIGNED NOT NULL,
+    cmd_verb        VARCHAR(20)  NOT NULL,
     setting_uri     VARCHAR(255) NOT NULL,
     setting_value   VARCHAR(255) NOT NULL,
     priority        SMALLINT UNSIGNED NOT NULL DEFAULT 0,
@@ -54,8 +54,8 @@ CREATE TABLE windows_mdm_command_results (
     created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (command_uuid),
-    FOREIGN KEY fk_windows_mdm_command_results_command_uuid (command_uuid) REFERENCES windows_mdm_commands (command_uuid) ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY (command_uuid, device_id),
+    FOREIGN KEY fk_windows_mdm_command_results_command_uuid_device_id (command_uuid, device_id) REFERENCES windows_mdm_commands (command_uuid, device_id) ON DELETE CASCADE ON UPDATE CASCADE
 )`)
 	if err != nil {
 		return fmt.Errorf("failed to create windows_mdm_command_results table: %w", err)
