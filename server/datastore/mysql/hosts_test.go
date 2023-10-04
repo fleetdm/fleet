93e6c9e2a0e36e6e96211447c3079020fa7f772d
mostlikelee <tim@mostlikelee.com>
2023-10-04 13:27:18 -0600

    gofmt

--- server/datastore/mysql/hosts_test.go
+++ server/datastore/mysql/hosts_test.go
@@ -5713,7 +5713,7 @@ func testHostsDeleteHosts(t *testing.T, ds *Datastore) {
 		Query: "select * from time",
 	})
 	require.NoError(t, err)
-	
+
 	// update policy_results
 	_, err = ds.writer(context.Background()).Exec(`INSERT INTO query_results (host_id, query_id, last_fetched, data) VALUES (?, ?, ?, ?)`, host.ID, policy.ID, time.Now(), `{"foo": "bar"}`)
 	require.NoError(t, err)
