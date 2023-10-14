192de00290d6ee237a230c56eefda1f79f07042f
Mike McNeil <mikermcneil@users.noreply.github.com>
2023-10-14 16:32:11 -0500

    Update custom.js

--- website/config/custom.js
+++ website/config/custom.js
@@ -113,7 +113,7 @@ module.exports.custom = {
     'ee/tools/puppet': 'georgekarrv',//« Puppet integration (especially useful with macOS MDM turned on) -- FYI: Originally developed by request from "customer-eponym"
 
     // ⚗️ Reference, config surface, built-in queries, API, and other documentation
-     'docs': 'mikermcneil',                                      // « (specific paths covered in CODEOWNERS 2023-10-14)
+    'docs': 'mikermcneil',                              // « this is here because there's no way to have Rachael be codeowner of reference docs but mike codeowner of the other docs (to ensure we're deleting, not adding, surface area)
     // 'docs/Using-Fleet/REST-API.md': '',              // « Covered in CODEOWNERS (2023-07-22)
     // 'docs/Contributing/API-for-contributors.md': '', // « Covered in CODEOWNERS (2023-07-22)
     // 'schema': '',                                    // « Covered in CODEOWNERS (2023-07-22)
