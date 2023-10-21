e0e1e2700b75365ee4e79086ebf72f7dde72c699
Sampfluger88 <108141731+Sampfluger88@users.noreply.github.com>
2023-10-21 12:29:22 -0500

    Update routes.js

--- website/config/routes.js
+++ website/config/routes.js
@@ -413,7 +413,7 @@ module.exports.routes = {
   'GET /handbook/business-operations/ceo-handbook': '/handbook/ceo',
   'GET /handbook/business-operations/people-operations': '/handbook/company/communications#hiring',
   'GET /handbook/marketing': '/handbook/demand/',
-  
+
   'GET /docs': '/docs/get-started/why-fleet',
   'GET /docs/get-started': '/docs/get-started/why-fleet',
   'GET /docs/rest-api': '/docs/rest-api/rest-api',
