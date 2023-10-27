d7fabe5ff9ef9a6dfcf137f1aff39c6177d2c8c4
Victor Lyuboslavsky <victor@fleetdm.com>
2023-10-27 12:46:18 -0500

    Removing Kolide macos software update tables.

--- orbit/pkg/table/extension_darwin_arm64.go
+++ orbit/pkg/table/extension_darwin_arm64.go
@@ -5,7 +5,6 @@ package table
 import (
 	// ARM64 Kolide tables
 	appicons "github.com/kolide/launcher/pkg/osquery/tables/app-icons"
-	"github.com/kolide/launcher/pkg/osquery/tables/macos_software_update"
 
 	"github.com/osquery/osquery-go"
 )
@@ -14,8 +13,6 @@ func appendTables(plugins []osquery.OsqueryPlugin) []osquery.OsqueryPlugin {
 	plugins = append(plugins,
 		// ARM64 Kolide tables
 		appicons.AppIcons(),
-		macos_software_update.MacOSUpdate(),
-		macos_software_update.RecommendedUpdates(kolideLogger),
 	)
 	return plugins
 }
