82639c18f2a341e27be2b105327b349144c12e2b
Victor Lyuboslavsky <victor@fleetdm.com>
2023-10-31 12:59:56 -0500

    Removing unused logger methods.

--- orbit/pkg/table/extension_logger.go
+++ orbit/pkg/table/extension_logger.go
@@ -13,22 +13,10 @@ type Logger struct {
 	zerolog.Logger
 }
 
-// Fatal logs a fatal message, just like go-kit's logutil.Fatal.
-func (l *Logger) Fatal(args ...interface{}) {
-	l.Logger.Fatal().Msg(fmt.Sprint(args...))
-}
-
 // Log logs a message, implementing log.Logger interface
 func (l *Logger) Log(keyValuePairs ...interface{}) error {
 	log.Logger.Info().Msg(fmt.Sprint(keyValuePairs...))
 	return nil
-}
-
-// SetLevelKey sets the level key, just like go-kit's logutil.SetLevelKey.
-func (l *Logger) SetLevelKey(key interface{}) *Logger {
-	// Note: zerolog doesn't support changing the level key dynamically like logutil, so we just log it as a warning.
-	l.Logger.Warn().Msgf("Attempted to set level key to: %v", key)
-	return l
 }
 
 // NewOsqueryLogger returns the Logger struct.
