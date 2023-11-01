74c05dfa284f50867c2521593ee0e35671774b08
Gabriel Hernandez <ghernandez345@gmail.com>
2023-11-01 11:22:15 +0000

    create windows os update activity UI

--- frontend/interfaces/activity.ts
+++ frontend/interfaces/activity.ts
@@ -52,6 +52,7 @@ export enum ActivityType {
   AddedScript = "added_script",
   DeletedScript = "deleted_script",
   EditedScript = "edited_script",
+  EditedWindowsUpdates = "edited_windows_updates",
 }
 export interface IActivity {
   created_at: string;
@@ -95,4 +96,6 @@ export interface IActivityDetails {
   name?: string;
   script_execution_id?: string;
   script_name?: string;
+  deadline_days?: number;
+  grace_period_days?: number;
 }
