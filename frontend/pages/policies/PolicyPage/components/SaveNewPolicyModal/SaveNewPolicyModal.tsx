c4e70ecb5e374d9c7331baf5b8ae4c2e23b504c9
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2023-11-03 07:39:48 -0400

    Fleet UI: Update placeholder and hint text for creating a query and policy (#14908)

--- frontend/pages/policies/PolicyPage/components/SaveNewPolicyModal/SaveNewPolicyModal.tsx
+++ frontend/pages/policies/PolicyPage/components/SaveNewPolicyModal/SaveNewPolicyModal.tsx
@@ -120,7 +120,7 @@ const SaveNewPolicyModal = ({
             error={errors.name}
             inputClassName={`${baseClass}__policy-save-modal-name`}
             label="Name"
-            placeholder="What yes or no question does your policy ask about your devices?"
+            hint="What yes or no question does your policy ask about your hosts?"
             autofocus
             ignore1password
           />
@@ -131,7 +131,6 @@ const SaveNewPolicyModal = ({
             inputClassName={`${baseClass}__policy-save-modal-description`}
             label="Description"
             type="textarea"
-            placeholder="Add a description here (optional)"
           />
           <InputField
             name="resolution"
@@ -140,7 +139,7 @@ const SaveNewPolicyModal = ({
             inputClassName={`${baseClass}__policy-save-modal-resolution`}
             label="Resolution"
             type="textarea"
-            placeholder="What steps should a device owner take to resolve a host that fails this policy? (optional)"
+            hint="What steps should an end user take to resolve a host that fails this policy? (optional)"
           />
           {platformSelector.render()}
           {isPremiumTier && (
