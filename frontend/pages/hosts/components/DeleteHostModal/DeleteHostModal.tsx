a40ee0b2581228daeb0a97c9fa04a13c3f102d2d
Victor Lyuboslavsky <victor.lyuboslavsky@gmail.com>
2023-11-03 12:15:37 -0500

    Web UI no longer gives an error when deleting a large number of hosts. (#14896)
    After 30 seconds, the 'Delete host' modal closes and the delete
    operation continues in the background. The following text has been added
    to the modal when deleting 500 or more hosts: "When deleting a large
    volume of hosts, it may take some time for this change to be reflected
    in the UI."
    
    #14097 
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [x] Changes file added for user-visible changes in `changes/` or
    `orbit/changes/`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [x] Manual QA for all new/changed functionality
    - [x] Backend test added

--- frontend/pages/hosts/components/DeleteHostModal/DeleteHostModal.tsx
+++ frontend/pages/hosts/components/DeleteHostModal/DeleteHostModal.tsx
@@ -13,6 +13,8 @@ interface IDeleteHostModalProps {
   isAllMatchingHostsSelected?: boolean;
   /** Manage host page only */
   selectedHostIds?: number[];
+  /** Manage host page only */
+  hostsCount?: number;
   /** Host details page only */
   hostName?: string;
   isUpdating: boolean;
@@ -23,6 +25,7 @@ const DeleteHostModal = ({
   onCancel,
   isAllMatchingHostsSelected,
   selectedHostIds,
+  hostsCount,
   hostName,
   isUpdating,
 }: IDeleteHostModalProps): JSX.Element => {
@@ -34,6 +37,12 @@ const DeleteHostModal = ({
     }
     return hostName;
   };
+  const largeVolumeText = (): string => {
+    if (selectedHostIds && isAllMatchingHostsSelected && hostsCount && hostsCount >= 500) {
+      return " When deleting a large volume of hosts, it may take some time for this change to be reflected in the UI."
+    }
+    return ""
+  }
 
   return (
     <Modal
@@ -44,7 +53,7 @@ const DeleteHostModal = ({
     >
       <form className={`${baseClass}__form`}>
         <p>
-          This action will delete <b>{hostText()}</b> from your Fleet instance.
+          This action will delete <b>{hostText()}</b> from your Fleet instance.{largeVolumeText()}
         </p>
         <p>If the hosts come back online, they will automatically re-enroll.</p>
         <p>
