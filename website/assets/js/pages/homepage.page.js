60672c3d1216e15656c55029a3b7bc50881a36ce
Eric <eashaw@sailsjs.com>
2023-11-09 11:11:49 -0600

    Add animation to "ClickOps or DevOps" image

--- website/assets/js/pages/homepage.page.js
+++ website/assets/js/pages/homepage.page.js
@@ -14,7 +14,23 @@ parasails.registerPage('homepage', {
     //…
   },
   mounted: async function(){
-
+    // Add an event listener to the image in the "ClickOps or DevOps" section.
+    window.addEventListener('scroll', ()=>{
+      let animatedImage = document.querySelector('[purpose="platform-animated-image"]');
+      // Get the bounding box of the image.
+      let animatedImageBoundingBox = animatedImage.getBoundingClientRect();
+      if (animatedImageBoundingBox.top >= 0 &&
+          animatedImageBoundingBox.left >= 0 &&
+          animatedImageBoundingBox.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+          animatedImageBoundingBox.right <= (window.innerWidth || document.documentElement.clientWidth))
+      {
+        // When the image is completly in the user's viewport, add the 'animate' class to it.
+        animatedImage.classList.add('animate');
+      } else if(animatedImage.classList.contains('animate')) {
+        // When it is no longer in the user's viewport, remove the 'animate' class if it has it.
+        animatedImage.classList.remove('animate');
+      }
+    });
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
