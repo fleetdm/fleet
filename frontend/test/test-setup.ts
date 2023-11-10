75e36de902403a083babe62e940f84f8b32364a0
Luke Heath <luke@fleetdm.com>
2023-11-10 11:33:27 -0600

    Pass tests

--- frontend/test/test-setup.ts
+++ frontend/test/test-setup.ts
@@ -2,6 +2,17 @@ import "@testing-library/jest-dom";
 
 import mockServer from "./mock-server";
 
+window.matchMedia =
+  window.matchMedia ||
+  function matchMedia() {
+    return {
+      matches: false,
+      addEventListener() {
+        return null;
+      },
+    };
+  };
+
 // Mock server setup
 beforeAll(() => mockServer.listen());
 afterEach(() => mockServer.resetHandlers());
