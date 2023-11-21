96936809066507cb568b39c8a595a3b0a5b07ef0
Eric <eashaw@sailsjs.com>
2023-11-21 14:21:18 -0600

    Update script to create rituals

--- website/scripts/create-issues-for-todays-rituals.js
+++ website/scripts/create-issues-for-todays-rituals.js
@@ -11,6 +11,15 @@ module.exports = {
     dry: { type: 'boolean', defaultsTo: false, description: 'Whether to do a dry run instead.' },
   },
 
+  // Whats left 11-13:
+  // - Support for confidential repo in this script
+  // - Test that this script creates issues as expected
+  // - confirm that monthly behavior is correct/what we actually want
+  //
+  // Ideas for testing: Change the "task" of existing rituals to be the names of existing issues in the Fleet repo, and do a dry run of this script to confrim that it will create issues as expected.
+
+
+
 
   fn: async function ({ dry }) {
 
@@ -23,7 +32,6 @@ module.exports = {
     };
 
     let owner = 'fleetdm';
-    let repo = 'fleet';// TODO: support confidential and classified also
 
     // Find all the files in the top level /handbook folder and it's sub-folders
     let FILES_IN_HANDBOOK_FOLDER = await sails.helpers.fs.ls.with({
@@ -35,27 +43,44 @@ module.exports = {
       return _.endsWith(filePath, 'rituals.yml');
     });
     let pageNumberForPossiblePaginatedResults = 0;
-    let currentIssuesInGithubRepo = [];
-    const NUMBER_OF_RESULTS_REQUESTED = 100;
-    // Fetch all open issues in the fleetdm/fleet repo.
+    let currentIssuesInMainGithubRepo = [];
+    let currentIssuesInConfidentialGithubRepo = [];
+    let NUMBER_OF_RESULTS_REQUESTED = 100;
+
+    // Fetch all issues in the fleetdm/fleet and fleetdm/confidential repos.
     // Note: This will send requests to GitHub until the number of results is less than the number we requested.
     await sails.helpers.flow.until(async ()=>{
-    let githubIssues = await sails.helpers.http.get(
-        `https://api.github.com/repos/${owner}/${repo}/issues`,
+    let githubIssuesFromMainRepo = await sails.helpers.http.get(
+        `https://api.github.com/repos/${owner}/fleet/issues`,
         {
           'per_page': NUMBER_OF_RESULTS_REQUESTED,
           'page': pageNumberForPossiblePaginatedResults,
         },
         baseHeaders
       ).retry();
-      currentIssuesInGithubRepo = currentIssuesInGithubRepo.concat(githubIssues);
+      currentIssuesInMainGithubRepo = currentIssuesInMainGithubRepo.concat(githubIssuesFromMainRepo);
       // If we received less results than we requested, we've reached the last page of the results.
-      return currentIssuesInGithubRepo.length !== NUMBER_OF_RESULTS_REQUESTED;
+      return currentIssuesInMainGithubRepo.length !== NUMBER_OF_RESULTS_REQUESTED;
+    }, 10000);
+    await sails.helpers.flow.until(async ()=>{
+    let githubIssuesFromConfidentialRepo = await sails.helpers.http.get(
+        `https://api.github.com/repos/${owner}/confidential/issues`,// TODO: classified.
+        {
+          'per_page': NUMBER_OF_RESULTS_REQUESTED,
+          'page': pageNumberForPossiblePaginatedResults,
+        },
+        baseHeaders
+      ).retry();
+      currentIssuesInConfidentialGithubRepo = currentIssuesInConfidentialGithubRepo.concat(githubIssuesFromConfidentialRepo);
+      // If we received less results than we requested, we've reached the last page of the results.
+      return currentIssuesInConfidentialGithubRepo.length !== NUMBER_OF_RESULTS_REQUESTED;
     }, 10000);
 
+    // Get arrays of the unique gitHub issues returned in the API response.
+    let uniqueGitHubIssuesFromMainRepo = _.uniq(currentIssuesInMainGithubRepo, 'node_id');
+    let uniqueGitHubIssuesFromConfidentialRepo = _.uniq(currentIssuesInConfidentialGithubRepo, 'node_id');
 
     for (let ritualSource of ritualYamlPaths) {
-
       // Load rituals
       let pathToRituals = path.resolve(topLvlRepoPath, ritualSource);
       let rituals = [];
@@ -73,18 +98,7 @@ module.exports = {
         if (!ritual.autoIssue) {// « Skip to the next ritual if automations aren't enabled.
           continue;
         }
-        // An issue should be created for a ritual if:
-        // - ritual.autoissue contains a labels array (an array of strings of the GH labels that will be put on the issue)
-        // - the last issue created for a ritual is older than the rituals frequency.
-        // - No issue has been created for this ritual.
-        // - TODO: anything else?
-
-        // An issue should not be created if:
-        // - It has been less than the frequency since the last issue was created
-        // -
-
         let isItTimeToCreateANewIssue = false;// Default this value to false.
-
 
         let ritualsFrequencyInMs = 0;
 
@@ -96,9 +110,22 @@ module.exports = {
           ritualsFrequencyInMs = 1000 * 60 * 60 * 24 * 7 * 2;
         } else if(ritual.frequency === 'Triweekly'){
           ritualsFrequencyInMs = 1000 * 60 * 60 * 24 * 7 * 3;
-        }// TODO: Monthly
+        } else if (ritual.frequency === 'Monthly') {
+          // For monthly rituals, we will get the number of days in the previous month, and create a timestamp of the next time this ritual should be run.
+          let todaysDate = new Date();
+          let numberOfDaysInLastMonth = new Date(todaysDate.getFullYear(), todaysDate.getMonth() - 1, 0).getDate();
+          ritualsFrequencyInMs = 1000 * 60 * 60 * 24 * numberOfDaysInLastMonth;
+        }
 
-        let previousIssuesCreatedForThisRitual = _.filter(currentIssuesInGithubRepo, {title: ritual.task});
+        let previousIssuesCreatedForThisRitual = [];
+
+        if(ritual.autoIssue.repo === 'fleet'){
+          previousIssuesCreatedForThisRitual = _.uniq(_.filter(currentIssuesInMainGithubRepo, {title: ritual.task, user: {login: ritual.dri}}), 'node_id');
+        } else {
+          previousIssuesCreatedForThisRitual = _.uniq(_.filter(currentIssuesInConfidentialGithubRepo, {title: ritual.task, user: {login: ritual.dri}}), 'node_id');
+        }
+
+        let nextIssueShouldBeCreatedAt;
 
         // If we found issues previously created of this ritual, we'll check the created at timestamp of the most recent one.
         if(previousIssuesCreatedForThisRitual.length > 0){
@@ -106,8 +133,8 @@ module.exports = {
           let lastIssueCreatedForThisRitual = _.sortBy(previousIssuesCreatedForThisRitual, 'number')[previousIssuesCreatedForThisRitual.length - 1];
           // Create a JS timestamp of when the last issue for this ritual was created.
           let lastIssueWasCreatedAt = Date.parse(lastIssueCreatedForThisRitual.created_at);
-          // An a JS timestamp of when the next issue for this ritual should be created.
-          let nextIssueShouldBeCreatedAt = (lastIssueWasCreatedAt + ritualsFrequencyInMs);
+          // And a JS timestamp of when the next issue for this ritual should be created.
+          nextIssueShouldBeCreatedAt = (lastIssueWasCreatedAt + ritualsFrequencyInMs);
           // Set the flag to be true if it the issue's created at timestamp + the rituals frequency is after an hour ago.
           isItTimeToCreateANewIssue = (Date.now() - (1000 * 60 * 60)) >= nextIssueShouldBeCreatedAt;
         } else {
@@ -115,24 +142,25 @@ module.exports = {
           isItTimeToCreateANewIssue = true;
         }
 
+
         // Skip to the next ritual if it isn't time yet.
-        if (isItTimeToCreateANewIssue) {
+        if (!isItTimeToCreateANewIssue) {
+          sails.log(`Next issue for ${ritual.task} will be created on ${new Date(nextIssueShouldBeCreatedAt)}`)
           continue;
         }
 
-        // Create an issue with right labels and assignee, in the right repo.
+        // // Create an issue with right labels and assignee, in the right repo.
         if (!dry) {
           // [?] https://docs.github.com/en/rest/issues/issues#create-an-issue
-          await sails.helpers.http.post(`https://api.github.com/repos/${owner}/${repo}/issues`, {
-            title: ritual.task,
-            body: ritual.description,
-            labels: ritual.autoIssue.labels,
-            assignees: [ ritual.dri ]
-          }, baseHeaders);
+          // await sails.helpers.http.post(`https://api.github.com/repos/${owner}/${repo}/issues`, {
+          //   title: ritual.task,
+          //   body: ritual.description,
+          //   labels: ritual.autoIssue.labels,
+          //   assignees: [ ritual.dri ]
+          // }, baseHeaders);
         } else {
-          sails.log('Dry run: Would have created an issue for ritual:', ritual);
         }
-
+        sails.log('Dry run: Would have created an issue for ritual:', ritual);
       }//∞
     }//∞
 
