efa7cc5112c9401b027e9a6d19fce3d6f639b1fd
Martin Angers <martin.n.angers@gmail.com>
2023-12-04 15:58:06 -0500

    Adjust output of fleetctl get software

--- server/service/client_software.go
+++ server/service/client_software.go
@@ -4,9 +4,22 @@ import (
 	"github.com/fleetdm/fleet/v4/server/fleet"
 )
 
-// ListSoftware retrieves the software running across hosts.
-func (c *Client) ListSoftware(query string) ([]fleet.Software, error) {
-	verb, path := "GET", "/api/latest/fleet/software"
+// ListSoftwareVersions retrieves the software versions installed on hosts.
+func (c *Client) ListSoftwareVersions(query string) ([]fleet.Software, error) {
+	verb, path := "GET", "/api/latest/fleet/software" // TODO(mna): /versions
+	// TODO(mna): adjust if the response struct has changed, when this gets merged: https://github.com/fleetdm/fleet/issues/15229
+	var responseBody listSoftwareResponse
+	err := c.authenticatedRequestWithQuery(nil, verb, path, &responseBody, query)
+	if err != nil {
+		return nil, err
+	}
+	return responseBody.Software, nil
+}
+
+// ListSoftwareTitles retrieves the software titles installed on hosts.
+func (c *Client) ListSoftwareTitles(query string) ([]fleet.Software, error) {
+	verb, path := "GET", "/api/latest/fleet/software/titles"
+	// TODO(mna): adjust when this gets merged: https://github.com/fleetdm/fleet/issues/15228
 	var responseBody listSoftwareResponse
 	err := c.authenticatedRequestWithQuery(nil, verb, path, &responseBody, query)
 	if err != nil {
