87c4bfbd5b8995d0d8583474376f5215812aef1e
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2023-12-12 11:29:04 -0600

    Add a way to override hostnames on free.fleetdm.com

--- infrastructure/dogfood/terraform/aws-tf-module/free-ecs-hosts.tf
+++ infrastructure/dogfood/terraform/aws-tf-module/free-ecs-hosts.tf
@@ -1,17 +1,17 @@
 ## Linux hosts in ECS
 
 locals {
-  osquery_tags = [
-    "5.8.2-ubuntu22.04@sha256:b77c7b06c4d7f2a3c58cc3a34e51fffc480e97795fb3c75cb1dc1cf3709e3dc6",
-    "5.8.2-ubuntu20.04@sha256:3496ffd0ad570c88a9f405e6ef517079cfeed6ce405b9d22db4dc5ef6ed3faac",
-    "5.8.2-ubuntu18.04@sha256:372575e876c218dde3c5c0e24fd240d193800fca9b314e94b4ad4e6e22006c9b",
-    "5.8.2-ubuntu16.04@sha256:112655c42951960d8858c116529fb4c64951e4cf2e34cb7c08cd599a009025bb",
-    "5.8.2-debian10@sha256:de29337896aac89b2b03c7642805859d3fb6d52e5dc08230f987bbab4eeba9c5",
-    "5.8.2-debian9@sha256:47e46c19cebdf0dc704dd0061328856bda7e1e86b8c0fefdd6f78bd092c6200e",
-    "5.8.2-centos8@sha256:88a8adde80bd3b1b257e098bc6e41b6afea840f60033653dcb9fe984f36b0f97",
-    "5.8.2-centos7@sha256:ff251de4935b80a91c5fc1ac352aebdab9a6bbbf5bda1aaada8e26d22b50202d",
-    "5.8.2-centos6@sha256:b56736be8436288d3fbd2549ec6165e0588cd7197e91600de4a2f00f1df28617",
-  ]
+  osquery_hosts = {
+    "5.8.2-ubuntu22.04@sha256:b77c7b06c4d7f2a3c58cc3a34e51fffc480e97795fb3c75cb1dc1cf3709e3dc6" = "Skys-laptop"
+    "5.8.2-ubuntu20.04@sha256:3496ffd0ad570c88a9f405e6ef517079cfeed6ce405b9d22db4dc5ef6ed3faac" = "Cloud-City-server"
+    "5.8.2-ubuntu18.04@sha256:372575e876c218dde3c5c0e24fd240d193800fca9b314e94b4ad4e6e22006c9b" = "Mists-laptop"
+    "5.8.2-ubuntu16.04@sha256:112655c42951960d8858c116529fb4c64951e4cf2e34cb7c08cd599a009025bb" = "Ethers-laptop"
+    "5.8.2-debian10@sha256:de29337896aac89b2b03c7642805859d3fb6d52e5dc08230f987bbab4eeba9c5" = "Breezes-laptop"
+    "5.8.2-debian9@sha256:47e46c19cebdf0dc704dd0061328856bda7e1e86b8c0fefdd6f78bd092c6200e" = "Aero-server"
+    "5.8.2-centos8@sha256:88a8adde80bd3b1b257e098bc6e41b6afea840f60033653dcb9fe984f36b0f97" = "Stratuss-laptop"
+    "5.8.2-centos7@sha256:ff251de4935b80a91c5fc1ac352aebdab9a6bbbf5bda1aaada8e26d22b50202d" = "Zephyrs-Laptop"
+    "5.8.2-centos6@sha256:b56736be8436288d3fbd2549ec6165e0588cd7197e91600de4a2f00f1df28617" = "Halo-server"
+  }
 
 }
 
@@ -123,18 +123,18 @@ provider "docker" {
 }
 
 module "osquery_docker" {
-  for_each    = toset(local.osquery_tags)
+  for_each    = local.osquery_hosts
   source      = "./docker"
   ecr_repo    = aws_ecr_repository.osquery.repository_url
   osquery_tag = each.key
 }
 
 resource "random_uuid" "osquery" {
-  for_each = toset(local.osquery_tags)
+ for_each = local.osquery_hosts
 }
 
 resource "aws_ecs_task_definition" "osquery" {
-  for_each                 = toset(local.osquery_tags)
+  for_each                 = local.osquery_hosts
   // e.g. 5-8-2-ubuntu22-04 to match naming requirements 
   family                   = "osquery-${replace(split("@sha256", each.key)[0], ".", "-")}"
   network_mode             = "awsvpc"
@@ -142,6 +142,7 @@ resource "aws_ecs_task_definition" "osquery" {
   execution_role_arn       = aws_iam_role.osquery.arn
   cpu                      = 256
   memory                   = 512
+  # Needed to run hostname command
   container_definitions = jsonencode(
     [
       {
@@ -164,6 +165,12 @@ resource "aws_ecs_task_definition" "osquery" {
           logDriver = "awslogs"
           options   = module.free.byo-db.byo-ecs.logging_config
         }
+        environment = [
+          {
+            name = "FAKE_HOSTNAME"
+            value = each.value
+          }
+        ]
         secrets = [
           {
             name      = "ENROLL_SECRET"
@@ -207,11 +214,11 @@ resource "aws_ecs_task_definition" "osquery" {
 }
 
 resource "aws_ecs_service" "osquery" {
-  for_each                           = toset(local.osquery_tags)
+  for_each                           = local.osquery_hosts
   # Name must match ^[A-Za-z-_]+$ e.g. 5-8-2-ubuntu22-04
   name                               = "osquery_${replace(split("@sha256", each.key)[0], ".", "-")}"
   launch_type                        = "FARGATE"
-  cluster                            = module.main.byo-vpc.byo-db.byo-ecs.service.cluster
+  cluster                            = module.free.byo-db.byo-ecs.service.cluster
   task_definition                    = aws_ecs_task_definition.osquery[each.key].arn
   desired_count                      = 1
   # Spin down before spin up since we are specifying the host identifier manually
