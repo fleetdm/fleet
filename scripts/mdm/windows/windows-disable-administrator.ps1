23f3a7d482975e76f83623233f9af8b9a8134920
Luke Heath <luke@fleetdm.com>
2023-12-12 12:17:11 -0600

    Revert "Add Windows scripts and set scripts table width"
    This reverts commit 8c548afe3188007aa7485c04e4cd8b508b98c935.

--- scripts/mdm/windows/windows-disable-administrator.ps1
+++
@@ -1,8 +0,0 @@
-# PowerShell script to disable the Administrator account
-
-# Run this script as an administrator
-
-# Disable the Administrator account
-Disable-LocalUser -Name "Administrator"
-
-Write-Host "Administrator account has been disabled."
