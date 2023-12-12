0ea3189b0a5fbe1f6fa5246c15c39a77935144f8
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2023-12-12 09:42:46 -0500

    Add redirect

--- website/config/routes.js
+++ website/config/routes.js
@@ -511,6 +511,7 @@ module.exports.routes = {
   // These are external links not maintained by Fleet. We can point the Fleet UI to redirects here instead of the
   // original sources to help avoid broken links.
   'GET /learn-more-about/chromeos-updates': 'https://support.google.com/chrome/a/answer/6220366',
+  'GET /learn-more-about/just-in-tim-provisioning': 'https://fleetdm.com/docs/deploy/single-sign-on-sso#just-in-time-jit-user-provisioning'
 
   // Sitemap
   // =============================================================================================================
