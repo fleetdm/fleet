8456a4f3420ee3d40c8657dcdee1aa7345b59208
Jacob Shandling <jacob@fleetdm.com>
2023-12-15 12:03:27 -0800

    wip-titles

--- frontend/router/page_titles.ts
+++ frontend/router/page_titles.ts
@@ -5,18 +5,18 @@ import PATHS from "router/paths";
 
 // Note: Order matters for use of array.find() (specific subpaths must be listed before their parent path)
 export default [
-  { path: "/dashboard", title: `Dashboard | ${DOCUMENT_TITLE_SUFFIX}` },
+  { path: PATHS.DASHBOARD, title: `Dashboard | ${DOCUMENT_TITLE_SUFFIX}` },
   { path: "/hosts/manage", title: `Manage hosts | ${DOCUMENT_TITLE_SUFFIX}` },
   {
-    path: "/controls/os-updates",
+    path: PATHS.CONTROLS_OS_UPDATES,
     title: `Manage OS updates | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/controls/os-settings",
+    path: PATHS.CONTROLS_OS_SETTINGS,
     title: `Manage OS settings | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/controls/setup-experience",
+    path: PATHS.CONTROLS_SETUP_EXPERIENCE,
     title: `Manage setup experience | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
@@ -28,33 +28,33 @@ export default [
     title: `Software versions | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/queries/manage",
+    path: PATHS.MANAGE_QUERIES,
     title: `Manage queries | ${DOCUMENT_TITLE_SUFFIX}`,
   },
-  { path: "/queries/new", title: `New query | ${DOCUMENT_TITLE_SUFFIX}` },
+  { path: PATHS.NEW_QUERY(), title: `New query | ${DOCUMENT_TITLE_SUFFIX}` },
   {
-    path: "/policies/manage",
+    path: PATHS.MANAGE_POLICIES,
     title: `Manage policies | ${DOCUMENT_TITLE_SUFFIX}`,
   },
-  { path: "/policies/new", title: `New policy | ${DOCUMENT_TITLE_SUFFIX}` },
+  { path: PATHS.NEW_POLICY, title: `New policy | ${DOCUMENT_TITLE_SUFFIX}` },
   {
-    path: "/settings/organization",
+    path: PATHS.ADMIN_ORGANIZATION,
     title: `Manage organization settings | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/settings/integrations",
+    path: PATHS.ADMIN_INTEGRATIONS,
     title: `Manage integration settings | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/settings/users",
+    path: PATHS.ADMIN_USERS,
     title: `Manage user settings | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/settings/teams/members",
+    path: PATHS.TEAM_DETAILS_MEMBERS(),
     title: `Manage team members | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
-    path: "/settings/teams/options",
+    path: PATHS.TEAM_DETAILS_OPTIONS(),
     title: `Manage team options | ${DOCUMENT_TITLE_SUFFIX}`,
   },
   {
