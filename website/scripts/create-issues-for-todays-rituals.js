11c52ad671f717862255d4c67bdc6eee12e4e836
Eric <eashaw@sailsjs.com>
2023-12-19 14:23:44 -0600

    Update create-issues-for-todays-rituals.js

--- website/scripts/create-issues-for-todays-rituals.js
+++ website/scripts/create-issues-for-todays-rituals.js
@@ -11,16 +11,6 @@ module.exports = {
     dry: { type: 'boolean', defaultsTo: false, description: 'Whether to do a dry run instead.' },
   },
 
-  // Whats left 11-13:
-  // - Support for confidential repo in this script
-  // - Test that this script creates issues as expected
-  // - confirm that monthly behavior is correct/what we actually want
-  //
-  // Ideas for testing: Change the "task" of existing rituals to be the names of existing issues in the Fleet repo, and do a dry run of this script to confrim that it will create issues as expected.
-
-
-
-
   fn: async function ({ dry }) {
 
     let path = require('path');
@@ -42,44 +32,6 @@ module.exports = {
     let ritualYamlPaths = FILES_IN_HANDBOOK_FOLDER.filter((filePath)=>{
       return _.endsWith(filePath, 'rituals.yml');
     });
-    let pageNumberForPossiblePaginatedResults = 0;
-    let currentIssuesInMainGithubRepo = [];
-    let currentIssuesInConfidentialGithubRepo = [];
-    let NUMBER_OF_RESULTS_REQUESTED = 100;
-
-    // Fetch all issues in the fleetdm/fleet and fleetdm/confidential repos.
-    // Note: This will send requests to GitHub until the number of results is less than the number we requested.
-    await sails.helpers.flow.until(async ()=>{
-    let githubIssuesFromMainRepo = await sails.helpers.http.get(
-        `https://api.github.com/repos/${owner}/fleet/issues`,
-        {
-          'per_page': NUMBER_OF_RESULTS_REQUESTED,
-          'page': pageNumberForPossiblePaginatedResults,
-        },
-        baseHeaders
-      ).retry();
-      currentIssuesInMainGithubRepo = currentIssuesInMainGithubRepo.concat(githubIssuesFromMainRepo);
-      // If we received less results than we requested, we've reached the last page of the results.
-      return currentIssuesInMainGithubRepo.length !== NUMBER_OF_RESULTS_REQUESTED;
-    }, 10000);
-    await sails.helpers.flow.until(async ()=>{
-    let githubIssuesFromConfidentialRepo = await sails.helpers.http.get(
-        `https://api.github.com/repos/${owner}/confidential/issues`,// TODO: classified.
-        {
-          'per_page': NUMBER_OF_RESULTS_REQUESTED,
-          'page': pageNumberForPossiblePaginatedResults,
-        },
-        baseHeaders
-      ).retry();
-      currentIssuesInConfidentialGithubRepo = currentIssuesInConfidentialGithubRepo.concat(githubIssuesFromConfidentialRepo);
-      // If we received less results than we requested, we've reached the last page of the results.
-      return currentIssuesInConfidentialGithubRepo.length !== NUMBER_OF_RESULTS_REQUESTED;
-    }, 10000);
-
-    // Get arrays of the unique gitHub issues returned in the API response.
-    let uniqueGitHubIssuesFromMainRepo = _.uniq(currentIssuesInMainGithubRepo, 'node_id');
-    let uniqueGitHubIssuesFromConfidentialRepo = _.uniq(currentIssuesInConfidentialGithubRepo, 'node_id');
-
     for (let ritualSource of ritualYamlPaths) {
       // Load rituals
       let pathToRituals = path.resolve(topLvlRepoPath, ritualSource);
@@ -90,10 +42,11 @@ module.exports = {
       } catch (err) {
         throw new Error(`Could not parse the YAMl for rituals at ${pathToRituals} on line ${err.linePos.start.line}. To resolve, make sure the YAML is valid, and try again: ` + err.stack);
       }
-
-
-
       for (let ritual of rituals) {
+        // For each ritual, we'll:
+        //  - Convert the ritual's frequency into milliseconds.
+        //  - Find out when we will be creating the next issue for the ritual.
+        //  - Create an issue for the ritual if the ritual takes place in the next 24 hours.
 
         if (!ritual.autoIssue) {// « Skip to the next ritual if automations aren't enabled.
           continue;
@@ -113,46 +66,46 @@ module.exports = {
         } else if (ritual.frequency === 'Monthly') {
           // For monthly rituals, we will get the number of days in the previous month, and create a timestamp of the next time this ritual should be run.
           let todaysDate = new Date();
-          let numberOfDaysInLastMonth = new Date(todaysDate.getFullYear(), todaysDate.getMonth() - 1, 0).getDate();
+          let numberOfDaysInLastMonth = new Date(todaysDate.getFullYear(), todaysDate.getMonth(), 0).getDate();
           ritualsFrequencyInMs = 1000 * 60 * 60 * 24 * numberOfDaysInLastMonth;
         }
+        // Get a JS timestamp representing 12 PM UTC of the day this script is running.
+        let twelveHoursInMs = 1000 * 60 * 60 * 12;
+        let today = new Date();
+        let lastUTCNoonAt = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(), 12, 0, 0, 0)).getTime();
 
-        let previousIssuesCreatedForThisRitual = [];
-
-        if(ritual.autoIssue.repo === 'fleet'){
-          previousIssuesCreatedForThisRitual = _.uniq(_.filter(currentIssuesInMainGithubRepo, {title: ritual.task, assignee: {login: ritual.dri}}), 'node_id');
-        } else {
-          previousIssuesCreatedForThisRitual = _.uniq(_.filter(currentIssuesInConfidentialGithubRepo, {title: ritual.task, assignee: {login: ritual.dri}}), 'node_id');
-        }
-
-        let nextIssueShouldBeCreatedAt;
-
-        // If we found issues previously created of this ritual, we'll check the created at timestamp of the most recent one.
-        if(previousIssuesCreatedForThisRitual.length > 0){
-          // Sort the previous issues for the ritual by their issue number, and get the last (most recent) item in the array.
-          let lastIssueCreatedForThisRitual = _.sortBy(previousIssuesCreatedForThisRitual, 'number')[previousIssuesCreatedForThisRitual.length - 1];
-          // Create a JS timestamp of when the last issue for this ritual was created.
-          let lastIssueWasCreatedAt = Date.parse(lastIssueCreatedForThisRitual.created_at);
-          // And a JS timestamp of when the next issue for this ritual should be created.
-          nextIssueShouldBeCreatedAt = (lastIssueWasCreatedAt + ritualsFrequencyInMs);
-          // Set the flag to be true if it the issue's created at timestamp + the rituals frequency is after an hour ago.
-          isItTimeToCreateANewIssue = (Date.now() - (1000 * 60 * 60)) >= nextIssueShouldBeCreatedAt;
-        } else {
-          // If no GH issue exists that matches the "task" of the ritual, well set the isItTimeToCreateANewIssue flag to be true.
+        // Get a JS timestamp representing 12:00 PM UTC of the day this ritual started.
+        let ritualStartedAt = new Date(ritual.startedOn).getTime() + twelveHoursInMs;
+        // Find out how many times this ritual has occurred.
+        let howManyRitualsCycles = (lastUTCNoonAt - ritualStartedAt ) / ritualsFrequencyInMs;
+        // Find out when the next issue will be created at
+        let nextIssueShouldBeCreatedAt = ritualStartedAt + ((Math.floor(howManyRitualsCycles) + 1) * ritualsFrequencyInMs);
+        // Get the amount of this ritual's cycle remaining.
+        let amountOfCycleRemainingTillNextRitual = (Math.floor(howManyRitualsCycles) - howManyRitualsCycles) + 1;
+        // If amountOfCycleRemainingTillNextRitual is 0, then it is time to create a new issue for this ritual (Note: This will probably never happen)
+        if(amountOfCycleRemainingTillNextRitual === 0 || amountOfCycleRemainingTillNextRitual === -0){
           isItTimeToCreateANewIssue = true;
+        } else {
+          // Otherwise, get the number of milliseconds until the next issue for this ritual will be created.
+          let timeToNextRitualInMs = amountOfCycleRemainingTillNextRitual * ritualsFrequencyInMs;
+          // Since this script runs once a day at the same time, we'll create issues we'll create issues for
+          if(_.startsWith(ritual.frequency, 'Daily')) {// Using _.startsWith() to handle frequencies with emoji ("Daily ⏰") and with out ("Daily")
+            isItTimeToCreateANewIssue = true;
+          } else if(timeToNextRitualInMs < 86400000) {
+            // If the next occurance of this ritual is in less than 24 hours (before this script runs again), we'll create an issue for it.
+            isItTimeToCreateANewIssue = true;
+          }
         }
-
-
         // Skip to the next ritual if it isn't time yet.
         if (!isItTimeToCreateANewIssue) {
-          sails.log(`Next issue for ${ritual.task} will be created on ${new Date(nextIssueShouldBeCreatedAt)}`)
+          sails.log.verbose(`Next issue for ${ritual.task} (${ritual.autoIssue.labels.join(',')}) will be created on ${new Date(nextIssueShouldBeCreatedAt)} (Started on: ${ritual.startedOn}, frequency: ${ritual.frequency})`);
           continue;
         }
 
-        // // Create an issue with right labels and assignee, in the right repo.
+        // Create an issue with right labels and assignee, in the right repo.
         if (!dry) {
           // [?] https://docs.github.com/en/rest/issues/issues#create-an-issue
-          await sails.helpers.http.post(`https://api.github.com/repos/${owner}/${repo}/issues`, {
+          await sails.helpers.http.post(`https://api.github.com/repos/${owner}/${ritual.autoIssue.repo}/issues`, {
             title: ritual.task,
             body: ritual.description,
             labels: ritual.autoIssue.labels,
