7f81cd6fcb97336351809a597a9b154b5d7f126a
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2023-12-20 12:41:04 -0500

    fix: use hostSearchLike which has email searching capabilities

--- server/datastore/mysql/labels.go
+++ server/datastore/mysql/labels.go
@@ -595,7 +595,7 @@ func (ds *Datastore) applyHostLabelFilters(ctx context.Context, filter fleet.Tea
 	} else if opt.OSSettingsDiskEncryptionFilter.IsValid() {
 		query, params = ds.filterHostsByOSSettingsDiskEncryptionStatus(query, opt, params, enableDiskEncryption)
 	}
-	query, params = searchLike(query, params, opt.MatchQuery, hostSearchColumns...)
+	query, params, _ = hostSearchLike(query, params, opt.MatchQuery, hostSearchColumns...)
 
 	query, params = appendListOptionsWithCursorToSQL(query, params, &opt.ListOptions)
 	return query, params, nil
