7d4ca95bf55e0d051fcda98dd99e082f481a5c7b
mostlikelee <tim@mostlikelee.com>
2023-12-21 13:40:40 -0700

    remove resolved_in_version

--- server/datastore/mysql/migrations/tables/20231221130911_createOsCveTable_test.go
+++ server/datastore/mysql/migrations/tables/20231221130911_createOsCveTable_test.go
@@ -13,28 +13,27 @@ func TestUp_20231221130911(t *testing.T) {
 
 	insertStmt := `
 		INSERT INTO operating_system_cve (
-			cve, operating_system_id, resolved_in_version
-		) VALUES (?, ?, ?)
+			cve, operating_system_id
+		) VALUES (?, ?)
 		`
 
-	_, err := db.Exec(insertStmt, "CVE-2023-12345", 1, "14.2.1")
+	_, err := db.Exec(insertStmt, "CVE-2023-12345", 1)
 	require.NoError(t, err)
 
 	// unique constraint applies to cve+operating_system_id
-	_, err = db.Exec(insertStmt, "CVE-2023-12345", 1, "14.2.1")
+	_, err = db.Exec(insertStmt, "CVE-2023-12345", 1)
 	require.ErrorContains(t, err, "Duplicate entry")
 
-	_, err = db.Exec(insertStmt, "CVE-2023-12345", 2, "14.2.1")
+	_, err = db.Exec(insertStmt, "CVE-2023-12345", 2)
 	require.NoError(t, err)
 
 	selectStmt := `
-		SELECT cve, operating_system_id, resolved_in_version
+		SELECT cve, operating_system_id
 		FROM operating_system_cve
 		`
 	var rows []struct {
 		CVE                string `db:"cve"`
 		OperatingSystemID  uint   `db:"operating_system_id"`
-		ResolvedInVersion  string `db:"resolved_in_version"`
 	}
 	err = db.Select(&rows, selectStmt)
 	require.NoError(t, err)
