85f0148e2cb4816ef2249852d51abee1e826274c
Brock Walters <153771548+nonpunctual@users.noreply.github.com>
2023-12-21 14:42:12 -0500

    Added nonpunctual GitHub User to humans list (#15813)

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -88,6 +88,7 @@ module.exports = {
       '3kindsoffish',
       'phtardif1',
       'pintomi1989',
+      'nonpunctual',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
