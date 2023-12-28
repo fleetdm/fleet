23388aef1e6a258aaa97b09d77aa7e0f21cfb5f3
Gabriel Hernandez <ghernandez345@gmail.com>
2023-12-28 12:10:51 -0600

    start adding os details page

--- frontend/pages/SoftwarePage/SoftwareOSDetailsPage/SoftwareOSDetailsPage.tsx
+++ frontend/pages/SoftwarePage/SoftwareOSDetailsPage/SoftwareOSDetailsPage.tsx
@@ -1,11 +1,87 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { RouteComponentProps } from "react-router";
+
+import Spinner from "components/Spinner";
+import TableDataError from "components/DataError";
+import MainContent from "components/MainContent";
+
+import SoftwareDetailsSummary from "../components/SoftwareDetailsSummary";
+import SoftwareOSDetailsTable from "./SoftwareOSDetailsTable";
 
 const baseClass = "software-os-details-page";
 
-interface ISoftwareOSDetailsPageProps {}
+interface ISoftwareOSDetailsRouteParams {
+  id: string;
+}
 
-const SoftwareOSDetailsPage = ({}: ISoftwareOSDetailsPageProps) => {
-  return <div className={baseClass}>test</div>;
+type ISoftwareOSDetailsPageProps = RouteComponentProps<
+  undefined,
+  ISoftwareOSDetailsRouteParams
+>;
+
+const SoftwareOSDetailsPage = ({
+  router,
+  routeParams,
+}: ISoftwareOSDetailsPageProps) => {
+  // TODO: handle non integer values
+  const softwareId = parseInt(routeParams.id, 10);
+
+  const { data, isLoading, isError } = useQuery<
+    ISoftwareTitleResponse,
+    Error,
+    ISoftwareTitle
+  >(
+    ["softwareById", softwareId],
+    () => softwareAPI.getSoftwareTitle(softwareId),
+    {
+      select: (data) => data.software_title,
+    }
+  );
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <Spinner />;
+    }
+
+    if (isError) {
+      return <TableDataError className={`${baseClass}__table-error`} />;
+    }
+
+    if (!data) {
+      return null;
+    }
+
+    return (
+      <>
+        <SoftwareDetailsSummary
+          id={softwareId}
+          title={softwareTitle.name}
+          type={formatSoftwareType(softwareTitle)}
+          versions={softwareTitle.versions.length}
+          hosts={softwareTitle.hosts_count}
+          queryParam="software_title_id"
+          name={softwareTitle.name}
+          source={softwareTitle.source}
+        />
+        {/* TODO: can we use Card here for card styles */}
+        <div className={`${baseClass}__versions-section`}>
+          <h2>Vulnerabilities</h2>
+          {/* <SoftwareOSDetailsTable
+            router={router}
+            data={softwareTitle.versions}
+            isLoading={isSoftwareTitleLoading}
+          /> */}
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <MainContent className={baseClass}>
+      <>{renderContent()}</>
+    </MainContent>
+  );
 };
 
 export default SoftwareOSDetailsPage;
---
+++ frontend/pages/SoftwarePage/SoftwareOSDetailsPage/SoftwareOSDetailsTable/SoftwareOSDetailsTable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const baseClass = "software-os-details-table";
+
+interface ISoftwareOSDetailsTableProps {}
+
+const SoftwareOSDetailsTable = ({}: ISoftwareOSDetailsTableProps) => {
+  return <div className={baseClass}></div>;
+};
+
+export default SoftwareOSDetailsTable;
