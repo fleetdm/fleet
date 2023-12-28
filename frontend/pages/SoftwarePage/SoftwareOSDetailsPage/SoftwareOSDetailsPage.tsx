74b6e9567ed7486bc1b96130f7b0cdf67311ec73
Gabriel Hernandez <ghernandez345@gmail.com>
2023-12-28 11:57:16 -0600

    add software os details route and page

---
+++ frontend/pages/SoftwarePage/SoftwareOSDetailsPage/SoftwareOSDetailsPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const baseClass = "software-os-details-page";
+
+interface ISoftwareOSDetailsPageProps {}
+
+const SoftwareOSDetailsPage = ({}: ISoftwareOSDetailsPageProps) => {
+  return <div className={baseClass}>test</div>;
+};
+
+export default SoftwareOSDetailsPage;
