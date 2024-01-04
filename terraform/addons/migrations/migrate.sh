0e42959255e8607119ceab42d9c2eefb4a5f6bd9
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2024-01-04 13:51:05 -0600

    fix

--- terraform/addons/migrations/migrate.sh
+++ terraform/addons/migrations/migrate.sh
@@ -3,9 +3,7 @@ set -e
 set -x
 
 function scale_services(){
-	MIN_CAPACITY="${1:?}"
-	DESIRED_COUNT="${2:?}"
-	UP_DOWN="${3:?}"
+	UP_DOWN="${1:?}"
 	# Set the minimum capacity and desired count in the cluster to 0 to scale down or to the original size to scale back to normal.
 
 	# This is a bit hacky, but the update-service has to happen first when scaling up and second when scaling down.
@@ -33,7 +31,7 @@ do
    export "$KEY"="$VALUE"
 done
 
-scale_services 0 0 down
+scale_services down
 
 # Call aws ecs run-task
 TASK_ARN="$(aws ecs run-task --region "${REGION:?}" --cluster "${ECS_CLUSTER:?}" --task-definition "${TASK_DEFINITION:?}":"${TASK_DEFINITION_REVISION:?}" --launch-type FARGATE --network-configuration "awsvpcConfiguration={subnets="${SUBNETS:?}",securityGroups="${SECURITY_GROUPS:?}"}" --query 'tasks[].taskArn' --overrides '{"containerOverrides": [{"name": "fleet", "command": ["fleet", "prepare", "db"]}]}' --output text | rev | cut -d'/' -f1 | rev)"
@@ -41,7 +39,7 @@ TASK_ARN="$(aws ecs run-task --region "${REGION:?}" --cluster "${ECS_CLUSTER:?}"
 # Wait for completion
 aws ecs wait tasks-stopped --region "${REGION:?}" --cluster="${ECS_CLUSTER:?}" --tasks="${TASK_ARN:?}"
 
-scale_services "${MIN_CAPACITY:?}" "${DESIRED_COUNT:?}" up
+scale_services up
 
 # Exit with task's exit code
 TASK_EXIT_CODE=$(aws ecs describe-tasks --region "${REGION:?}" --cluster ${ECS_CLUSTER:?} --tasks ${TASK_ARN:?} --query "tasks[0].containers[?name=='fleet'].exitCode" --output text)
