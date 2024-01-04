840f7ad9c908dc9384aca3ac3ba352f36faf7730
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2024-01-04 00:36:09 -0600

    Update migrations to scale down services before migrating

--- terraform/addons/migrations/migrate.sh
+++ terraform/addons/migrations/migrate.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+function scale_services(){
+	MIN_CAPACITY="${1:?}"
+	DESIRED_COUNT="${2:?}"
+
+	# Set the minimum capacity and desired count in the cluster to 0 to scale down or to the original size to scale back to normal.
+	aws application-autoscaling register-scalable-target --region "${REGION:?}" --service-namespace ecs --resource-id "service/${ECS_CLUSTER:?}/${ECS_SERVICE:?}" --scalable-dimension "ecs:service:DesiredCount" --min-capacity "${MIN_CAPACITY:?}"
+	aws ecs update-service --region "${REGION:?}" --cluster "${ECS_CLUSTER:?}" --service "${ECS_SERVICE:?}" --desired-count "${DESIRED_COUNT:?}"
+
+	# The first task defintion might never get stable because it never had initial migrations so don't wait before continuing
+	if [ "${TASK_DEFINITION_REVISION}" != "1" ]; then
+		# Wait for scale-down to succeed
+		aws ecs wait services-stable --region "${REGION:?}" --cluster "${ECS_CLUSTER:?}" --service "${ECS_SERVICE:?}"
+	fi
+}
+
 for ARGUMENT in "$@"
 do
    KEY=$(echo $ARGUMENT | cut -f1 -d=)
@@ -11,12 +26,16 @@ do
    export "$KEY"="$VALUE"
 done
 
+scale_services 0 0
+
 # Call aws ecs run-task
-TASK_ARN="$(aws ecs run-task --region "${REGION}" --cluster "${ECS_CLUSTER}" --task-definition "${TASK_DEFINITION}":"${TASK_DEFINITION_REVISION}" --launch-type FARGATE --network-configuration "awsvpcConfiguration={subnets="${SUBNETS}",securityGroups="${SECURITY_GROUPS}"}" --query 'tasks[].taskArn' --overrides '{"containerOverrides": [{"name": "fleet", "command": ["fleet", "prepare", "db"]}]}' --output text | rev | cut -d'/' -f1 | rev)"
+TASK_ARN="$(aws ecs run-task --region "${REGION:?}" --cluster "${ECS_CLUSTER:?}" --task-definition "${TASK_DEFINITION:?}":"${TASK_DEFINITION_REVISION:?}" --launch-type FARGATE --network-configuration "awsvpcConfiguration={subnets="${SUBNETS:?}",securityGroups="${SECURITY_GROUPS:?}"}" --query 'tasks[].taskArn' --overrides '{"containerOverrides": [{"name": "fleet", "command": ["fleet", "prepare", "db"]}]}' --output text | rev | cut -d'/' -f1 | rev)"
 
 # Wait for completion
-aws ecs wait tasks-stopped --cluster="${ECS_CLUSTER}" --tasks="${TASK_ARN}"
+aws ecs wait tasks-stopped --region "${REGION:?}" --cluster="${ECS_CLUSTER:?}" --tasks="${TASK_ARN:?}"
+
+scale_services "${MIN_CAPACITY:?}" "${DESIRED_COUNT:?}"
 
 # Exit with task's exit code
-TASK_EXIT_CODE=$(aws ecs describe-tasks --cluster $ECS_CLUSTER --tasks $TASK_ARN --query "tasks[0].containers[?name=='fleet'].exitCode" --output text)
-exit $TASK_EXIT_CODE
+TASK_EXIT_CODE=$(aws ecs describe-tasks --region "${REGION:?}" --cluster ${ECS_CLUSTER:?} --tasks ${TASK_ARN:?} --query "tasks[0].containers[?name=='fleet'].exitCode" --output text)
+exit "${TASK_EXIT_CODE}"
