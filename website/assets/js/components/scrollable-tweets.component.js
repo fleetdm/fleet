81de9a17c12a87187fc4b12bc1d4f275cc8222d7
Eric <eashaw@sailsjs.com>
2024-01-11 16:13:02 -0600

    Update embedded video links

--- website/assets/js/components/scrollable-tweets.component.js
+++ website/assets/js/components/scrollable-tweets.component.js
@@ -66,7 +66,7 @@ parasails.registerComponent('scrollableTweets', {
     </div>
     <div v-for="video in quotesWithVideoLinks">
     <modal purpose="video-modal" v-if="modal === video.modalId" @close="closeModal()" >
-      <iframe width="560" height="315" :src="'https://www.youtube.com/embed/'+video.embedId+'?controls=0'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" :src="'https://www.youtube.com/embed/'+video.embedId+'?rel=0'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </modal>
     </div>
   </div>
