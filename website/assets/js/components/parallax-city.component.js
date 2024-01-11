2fc2e538d63b2bb1b3ae35def7de279db1dd6dc5
Eric <eashaw@sailsjs.com>
2024-01-11 13:50:07 -0600

    update component html, prevent parallax scroll on mobile devices

--- website/assets/js/components/parallax-city.component.js
+++ website/assets/js/components/parallax-city.component.js
@@ -13,7 +13,7 @@ parasails.registerComponent('parallaxCity', {
   //  ╔═╗╦═╗╔═╗╔═╗╔═╗
   //  ╠═╝╠╦╝║ ║╠═╝╚═╗
   //  ╩  ╩╚═╚═╝╩  ╚═╝
-  props: [],
+  props: ['isMobile'],
 
   //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
@@ -24,7 +24,6 @@ parasails.registerComponent('parallaxCity', {
       elementBottomPosition: undefined,
       elementHeight: undefined,
       distanceFromTopOfPage: undefined,
-      isFramePending: false,
     };
   },
 
@@ -33,16 +32,16 @@ parasails.registerComponent('parallaxCity', {
   //  ╩ ╩ ╩ ╩ ╩╩═╝
   template: `
   <div>
-    <div purpose="parallax-city">
-      <div class="layer layer-9" scroll-amount=12></div>
-      <div class="layer layer-8" scroll-amount=28></div>
-      <div class="layer layer-7" scroll-amount=20></div>
-      <div class="layer layer-6" scroll-amount=40></div>
-      <div class="layer layer-5" scroll-amount=40></div>
-      <div class="layer layer-4" scroll-amount=60></div>
-      <div class="layer layer-3" scroll-amount=60></div>
-      <div class="layer layer-2" scroll-amount=100></div>
-      <div class="layer layer-1" scroll-amount=120></div>
+    <div purpose="parallax-city-container">
+      <div purpose="background-cloud-3" scroll-amount=12></div>
+      <div purpose="background-cloud-2" scroll-amount=28></div>
+      <div purpose="small-island-2" scroll-amount=20></div>
+      <div purpose="small-island-1" scroll-amount=40></div>
+      <div purpose="background-cloud-1" scroll-amount=40></div>
+      <div purpose="large-island" scroll-amount=60></div>
+      <div purpose="swans" scroll-amount=60></div>
+      <div purpose="foreground-cloud-2" scroll-amount=100></div>
+      <div purpose="foreground-cloud-1" scroll-amount=120></div>
     </div>
   </div>
   `,
@@ -54,28 +53,32 @@ parasails.registerComponent('parallaxCity', {
 
   },
   mounted: async function(){
-    this.parallaxCityElement = document.querySelector('[purpose="parallax-city"]');
-    this.elementHeight = this.parallaxCityElement.clientHeight;
-    this.distanceFromTopOfPage = this.parallaxCityElement.offsetTop;
-    this.elementBottomPosition = this.elementHeight + this.distanceFromTopOfPage;
-    let parallaxCityElementPosition = this.parallaxCityElement.getBoundingClientRect();
-    if(parallaxCityElementPosition.bottom > this.distanceFromTopOfPage) {
-      this.handleParallaxScroll();
+    if(!this.isMobile){
+      this.parallaxCityElement = document.querySelector('[purpose="parallax-city-container"]');
+      this.elementHeight = this.parallaxCityElement.clientHeight;
+      this.distanceFromTopOfPage = this.parallaxCityElement.offsetTop;
+      this.elementBottomPosition = this.elementHeight + this.distanceFromTopOfPage;
+      let parallaxCityElementPosition = this.parallaxCityElement.getBoundingClientRect();
+      if(parallaxCityElementPosition.bottom > this.distanceFromTopOfPage) {
+        this.handleParallaxScroll();
+      }
+
+      this.parallaxCityElement.querySelectorAll('div').forEach((layer)=>{
+        let initialPosition = layer.getAttribute('scroll-amount');
+        layer.style.bottom = `-${Number(initialPosition) + 4}px`;
+      });
+
+      window.addEventListener('scroll', this.handleParallaxScroll);
+      window.addEventListener('resize', this.updateElementPositions);
+      window.addEventListener('orientationchange', this.updateElementPositions);
     }
-
-    document.querySelectorAll('div.layer').forEach((layer)=>{
-      let initialPosition = layer.getAttribute('scroll-amount');
-      layer.style.bottom = `-${Number(initialPosition) + 1}px`;
-    });
-
-    window.addEventListener('scroll', this.handleParallaxScroll);
-    window.addEventListener('resize', this.updateElementPositions);
-    window.addEventListener('orientationchange', this.updateElementPositions);
   },
   beforeDestroy: function() {
-    window.removeEventListener('scroll', this.handleParallaxScroll);
-    window.removeEventListener('resize', this.updateElementPositions);
-    window.removeEventListener('orientationchange', this.updateElementPositions);
+    if(!this.isMobile){
+      window.removeEventListener('scroll', this.handleParallaxScroll);
+      window.removeEventListener('resize', this.updateElementPositions);
+      window.removeEventListener('orientationchange', this.updateElementPositions);
+    }
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -100,13 +103,12 @@ parasails.registerComponent('parallaxCity', {
         percentageScrolled = 0;
       }
       if(percentageScrolled > 0){
-        this.parallaxCityElement.querySelectorAll('div.layer').forEach((layer) => {
+        this.parallaxCityElement.querySelectorAll('div').forEach((layer) => {
           let scrollAmount = layer.getAttribute('scroll-amount');
           let movement = (percentageScrolled * scrollAmount);
           layer.style.transform = 'translateY(-' + movement + 'px)';
         });
       }
-      this.isFramePending = false;
     },
   }
 });
