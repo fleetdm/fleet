02dc7de87766461118fe0bc2961cdf6c616fa9ce
Victor Lyuboslavsky <victor@fleetdm.com>
2024-01-12 14:12:00 -0600

    Initial untested Windows implementation.

--- orbit/pkg/keystore/keystore_windows.go
+++ orbit/pkg/keystore/keystore_windows.go
@@ -1,29 +1,41 @@
-//go:build (!darwin && !windows) || !cgo
+//go:build windows && cgo
 
 package keystore
 
-import "errors"
+import (
+	"github.com/danieljoos/wincred"
+)
+
+// Using a var instead of const so that it can be overridden in tests.
+var service = "com.fleetdm.fleetd.enroll.secret"
 
 func Exists() bool {
-	return false
+	return true
 }
 
 func Name() string {
-	return "not implemented"
+	return "Credential Manager"
 }
 
 // AddSecret will add a secret to the keychain. This secret can be retrieved by this application without any user authorization.
 func AddSecret(secret string) error {
-	return errors.New("not implemented")
+	cred := wincred.NewGenericCredential(service)
+	cred.CredentialBlob = []byte(secret)
+	err := cred.Write()
+	return err
 }
 
 // UpdateSecret will update a secret in the keychain. This secret can be retrieved by this application without any user authorization.
 func UpdateSecret(secret string) error {
-	return errors.New("not implemented")
+	return AddSecret(secret)
 }
 
 // GetSecret will retrieve a secret from the keychain. If the secret was added by user or another application,
 // then this application needs to be authorized to retrieve the secret.
 func GetSecret() (string, error) {
-	return "", errors.New("not implemented")
+	cred, err := wincred.GetGenericCredential(service)
+	if err != nil {
+		return "", err
+	}
+	return string(cred.CredentialBlob), nil
 }
