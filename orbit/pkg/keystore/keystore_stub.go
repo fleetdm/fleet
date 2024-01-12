6fc57bac3c7fdcab124f688cef29fe8eb8d23def
Victor Lyuboslavsky <victor@fleetdm.com>
2024-01-12 11:59:33 -0600

    Fixing compile issue.

--- orbit/pkg/keystore/keystore_stub.go
+++ orbit/pkg/keystore/keystore_stub.go
@@ -17,6 +17,11 @@ func AddSecret(secret string) error {
 	return errors.New("not implemented")
 }
 
+// UpdateSecret will update a secret in the keychain. This secret can be retrieved by this application without any user authorization.
+func UpdateSecret(secret string) error {
+	return errors.New("not implemented")
+}
+
 // GetSecret will retrieve a secret from the keychain. If the secret was added by user or another application,
 // then this application needs to be authorized to retrieve the secret.
 func GetSecret() (string, error) {
