1b9ba1b9f707ccf51b66c2dd1f60e2bdc3e4ed27
Eric <eashaw@sailsjs.com>
2024-01-12 18:03:02 -0600

    Update scrollable-tweets component styles

--- website/assets/js/components/scrollable-tweets.component.js
+++ website/assets/js/components/scrollable-tweets.component.js
@@ -42,31 +42,30 @@ parasails.registerComponent('scrollableTweets', {
   template: `
   <div class="d-flex flex-column">
     <div purpose="tweets" class="d-flex flex-row flex-nowrap">
-      <div purpose="tweet-card" class="card" v-for="testimonial in quotesToDisplay">
-        <div class="mb-4" v-if="testimonial.quoteImageFilename">
-          <a target="_blank" :href="testimonial.quoteLinkUrl"><img :height="testimonial.imageHeight" :src="'/images/'+testimonial.quoteImageFilename"/></a>
+      <a purpose="tweet-card" class="card" v-for="testimonial in quotesToDisplay" target="_blank" :href="testimonial.quoteLinkUrl">
+        <div purpose="logo" class="mb-4">
+          <img :height="testimonial.imageHeight" v-if="testimonial.quoteImageFilename" :src="'/images/'+testimonial.quoteImageFilename"/>
         </div>
-        <p class="pb-2 mb-1">
+        <p purpose="quote">
           {{testimonial.quote}}
-          <span purpose="video-link" v-if="testimonial.youtubeVideoUrl" @click="clickOpenVideoModal(testimonial.quoteAuthorName)">See the video.</span>
+          <a purpose="video-link" v-if="testimonial.youtubeVideoUrl" @click.prevent.self="clickOpenVideoModal(testimonial.quoteAuthorName)">See the video.</a>
         </p>
-        <div class="row px-3 pt-2">
-          <div>
-            <p class="font-weight-bold m-0">{{testimonial.quoteAuthorName}}</p>
-            <p class="m-0">
-              {{testimonial.quoteAuthorJobTitle}}
-              <a target="_blank" :href="testimonial.quoteLinkUrl">{{testimonial.quoteAuthorSocialHandle}}
-              </a>
-            </p>
+        <div purpose="quote-author-info" class="d-flex flex-row align-items-center">
+          <div purpose="profile-picture">
+            <img :src="'/images/'+testimonial.quoteAuthorProfileImageFilename">
+          </div>
+          <div class="d-flex flex-column align-self-top">
+            <p purpose="name" class="font-weight-bold m-0">{{testimonial.quoteAuthorName}}</p>
+            <p purpose="job-title" class="m-0">{{testimonial.quoteAuthorJobTitle}}</p>
           </div>
         </div>
-      </div>
+      </a>
     </div>
     <div purpose="page-indictator-container" class="mx-auto d-flex flex-row justify-content-center">
     </div>
     <div v-for="video in quotesWithVideoLinks">
     <modal purpose="video-modal" v-if="modal === video.modalId" @close="closeModal()" >
-      <iframe width="560" height="315" :src="'https://www.youtube.com/embed/'+video.embedId+'?controls=0'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe width="560" height="315" :src="'https://www.youtube.com/embed/'+video.embedId+'?rel=0'" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </modal>
     </div>
   </div>
