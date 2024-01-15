e68aa9a3a168126269f47ff91b4b23954f8ed70b
Jacob Shandling <jacob@fleetdm.com>
2024-01-14 20:03:43 -0800

    settings - usage statistics

--- frontend/pages/admin/OrgSettingsPage/cards/Statistics/Statistics.tsx
+++ frontend/pages/admin/OrgSettingsPage/cards/Statistics/Statistics.tsx
@@ -41,10 +41,10 @@ const Statistics = ({
   };
 
   return (
-    <>
-      <form className={baseClass} onSubmit={onFormSubmit} autoComplete="off">
-        <div className={`${baseClass}__section`}>
-          <h2>Usage statistics</h2>
+    <div className={baseClass}>
+      <div className={`${baseClass}__section`}>
+        <h2>Usage statistics</h2>
+        <form onSubmit={onFormSubmit} autoComplete="off">
           <p className={`${baseClass}__section-description`}>
             Help us improve Fleet by sending us anonymous usage statistics.
             <br />
@@ -61,32 +61,30 @@ const Statistics = ({
               newTab
             />
           </p>
-          <div className={`${baseClass}__inputs ${baseClass}__inputs--usage`}>
-            <Checkbox
-              onChange={handleInputChange}
-              name="enableUsageStatistics"
-              value={isPremiumTier ? true : enableUsageStatistics} // Set to true for all premium customers
-              parseTarget
-              wrapperClassName={
-                isPremiumTier
-                  ? `${baseClass}__disabled-usage-statistics-checkbox`
-                  : ""
-              }
-            >
-              Enable usage statistics
-            </Checkbox>
-          </div>
-        </div>
-        <Button
-          type="submit"
-          variant="brand"
-          className="save-loading"
-          isLoading={isUpdatingSettings}
-        >
-          Save
-        </Button>
-      </form>
-    </>
+          <Checkbox
+            onChange={handleInputChange}
+            name="enableUsageStatistics"
+            value={isPremiumTier ? true : enableUsageStatistics} // Set to true for all premium customers
+            parseTarget
+            wrapperClassName={
+              isPremiumTier
+                ? `${baseClass}__disabled-usage-statistics-checkbox`
+                : ""
+            }
+          >
+            Enable usage statistics
+          </Checkbox>
+          <Button
+            type="submit"
+            variant="brand"
+            className="button-wrap"
+            isLoading={isUpdatingSettings}
+          >
+            Save
+          </Button>
+        </form>
+      </div>
+    </div>
   );
 };
 
