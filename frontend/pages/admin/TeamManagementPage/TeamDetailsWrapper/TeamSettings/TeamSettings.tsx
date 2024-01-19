d481b4c002307f6b26c4836eba8bccaa90d39508
Jacob Shandling <jacob@fleetdm.com>
2024-01-19 20:18:38 +0000

    further scaffolding

--- frontend/pages/admin/TeamManagementPage/TeamDetailsWrapper/TeamSettings/TeamSettings.tsx
+++ frontend/pages/admin/TeamManagementPage/TeamDetailsWrapper/TeamSettings/TeamSettings.tsx
@@ -1,27 +1,71 @@
 import Button from "components/buttons/Button";
-import React from "react";
+import useTeamIdParam from "hooks/useTeamIdParam";
+import { ITeamConfig } from "interfaces/team";
+import { ITeamSubnavProps } from "interfaces/team_subnav";
+import React, { useState } from "react";
+import { useQuery } from "react-query";
 import { handleInputChange } from "react-select-5/dist/declarations/src/utils";
+import teamsAPI, { ILoadTeamResponse } from "services/entities/teams";
 
 const baseClass = "team-settings";
 
-interface ITeamSettings {}
+const TeamSettings = ({
+  location, router
+}: ITeamSubnavProps) => {
 
-const TeamSettings = ({}: ITeamSettings) => {
-  return (
+  const [formData, setFormData] = useState({}); // TODO type
+
+  // encompasses both when global setting is not enabled and
+  // when it is and the user has opted to override it with a local setting
+  const [UITeamExpiryEnabled, setUITeamExpiryEnabled] = useState(false);
+
+// TODO? - render flash on successful/failed update
+
+  const { isRouteOk, teamIdForApi } = useTeamIdParam(
+    {
+    location,
+    router,
+    includeAllTeams: false,
+    includeNoTeam: false,
+    permittedAccessByTeamRole: {
+      admin: true,
+      maintainer: false,
+      observer: false,
+      observer_plus: false,
+    },
+  });
+
+  // TODO
+  // const onSubmit = 
+
+  // TODO get globalExpiry from config
+
+  const {
+    data: teamData, isLoading, error
+  } = useQuery<ILoadTeamResponse, Error, ITeamConfig>(
+    ["team_details", teamIdForApi],
+    () => teamsAPI.load(teamIdForApi),
+    {
+      enabled: isRouteOk && !!teamIdForApi,
+      select: (data) => data.team,
+      onSuccess: (teamData) => {
+        setUITeamExpiryEnabled(teamData.host_expiry_settings.host_expiry_enabled);
+      }
+    }
+  );
+
     <section className={`${baseClass}`}>
       <div className="section-header">Settings</div>
       <form>
         <TeamHostExpiryOption
           globalExpiry={globalExpiry}
-          teamExpiryEnabled={teamExpiryEnabled}
-          setTeamExpiryEnabled={setTeamExpiryEnabled}
+          teamExpiryEnabled={UITeamExpiryEnabled}
+          setTeamExpiryEnabled={setUITeamExpiryEnabled}
         />
         <
         
         {
-        // encompasses both when global setting is not enabled and
-        // when it is and the user has opted to override it with a local setting
-        teamExpiryEnabled && 
+        UITeamExpiryEnabled && 
           <InputField
           label="Host expiry window"
           onChange={handleInputChange}
