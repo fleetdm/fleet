908b144f4f5357e4fbe6478fed5fc4d66a261e58
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2024-01-23 17:47:52 -0500

    Update website/config/routes.js
    Co-authored-by: Eric <eashaw@sailsjs.com>

--- website/config/routes.js
+++ website/config/routes.js
@@ -511,7 +511,7 @@ module.exports.routes = {
   'GET /learn-more-about/chromeos-updates': 'https://support.google.com/chrome/a/answer/6220366',
   'GET /learn-more-about/just-in-time-provisioning': 'https://fleetdm.com/docs/deploy/single-sign-on-sso#just-in-time-jit-user-provisioning',
   'GET /sign-in-to/microsoft-automatic-enrollment-tool': 'https://portal.azure.com',
-  'GET /learn-more-about/os-updates': 'https://fleetdm.com/docs/using-fleet/mdm-os-updates'
+  'GET /learn-more-about/os-updates': 'https://fleetdm.com/docs/using-fleet/mdm-os-updates',
 
   // Sitemap
   // =============================================================================================================
