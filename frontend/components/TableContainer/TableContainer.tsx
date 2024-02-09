9539b74fc0d1f464edfd7a7edb4a248d0cb5eaff
RachelElysia <rachel@fleetdm.com>
2024-02-09 15:14:57 -0500

    Remove misplaced type >.<

--- frontend/components/TableContainer/TableContainer.tsx
+++ frontend/components/TableContainer/TableContainer.tsx
@@ -25,8 +25,6 @@ export interface ITableQueryData {
   showInheritedTable?: boolean;
   /** Only used for sort/query changes to inherited policies table */
   editingInheritedTable?: boolean;
-  /** Only used for exploited vulnerabilities in software vulnerabilities table */
-  exploited?: boolean;
 }
 interface IRowProps extends Row {
   original: {
