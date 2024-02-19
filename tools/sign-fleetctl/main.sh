4b55e94a28249c0dbed966e422e4994bb77a75dc
Roberto Dip <dip.jesusr@gmail.com>
2024-02-19 16:52:36 -0300

    move to sh file as recommended in the docs

---
+++ tools/sign-fleetctl/main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo "signing binary..."
+sh -c 'echo "$APPLE_APPLICATION_CERTIFICATE" | base64 --decode > certificate.p12'
+rcodesign sign --p12-file certificate.p12 \
+       --p12-password "$APPLE_APPLICATION_CERTIFICATE_PASSWORD" \
+       --for-notarization "$FLEETCTL_BINARY_PATH"
+rm certificate.p12
+
+echo "notarizing binary..."
+mkdir private_keys
+sh -c 'echo "$APPLE_APP_STORE_CONNECT_KEY" > "private_keys/AuthKey_$APPLE_APP_STORE_CONNECT_KEY_ID.p8"'
+rcodesign notary-submit \
+       --api-issuer "$APPLE_APP_STORE_CONNECT_ISSUER_ID" \
+       --api-key "$APPLE_APP_STORE_CONNECT_KEY_ID" \
+       --staple "$FLEETCTL_BINARY_PATH"
+rm -rf private_keys
