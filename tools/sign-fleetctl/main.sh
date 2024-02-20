2d1f8f967272a0e5fc41b17d1b8baf4adf869e8f
Roberto Dip <dip.jesusr@gmail.com>
2024-02-20 11:24:17 -0300

    fix typo in script

--- tools/sign-fleetctl/main.sh
+++ tools/sign-fleetctl/main.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
+set -e
 
-echo "signing binary..."
-sh -c 'echo "$APPLE_APPLICATION_CERTIFICATE" | base64 --decode > certificate.p12'
+cleanup() {
+    echo "Cleaning up..."
+    rm -f certificate.p12
+    rm -rf private_keys
+    rm -f fleetctl.zip
+}
+
+# trap EXIT signal to call cleanup function
+trap cleanup EXIT
+
+echo "Signing binary..."
+printf "%s" "$APPLE_APPLICATION_CERTIFICATE" | base64 --decode > certificate.p12
 rcodesign sign --p12-file certificate.p12 \
-       --p12-password "$APPLE_APPLICATION_CERTIFICATE_PASSWORD" \
-       --for-notarization "$FLEETCTL_BINARY_PATH"
-rm certificate.p12
+               --p12-password "$APPLE_APPLICATION_CERTIFICATE_PASSWORD" \
+               --for-notarization "$FLEETCTL_BINARY_PATH"
 
-echo "notarizing binary..."
-mkdir private_keys
-sh -c 'echo "$APPLE_APP_STORE_CONNECT_KEY" > "private_keys/AuthKey_$APPLE_APP_STORE_CONNECT_KEY_ID.p8"'
-zip fletctl.zip "$FLEETCTL_BINARY_PATH"
+echo "Notarizing binary..."
+mkdir -p private_keys
+printf "%s" "$APPLE_APP_STORE_CONNECT_KEY" > "private_keys/AuthKey_$APPLE_APP_STORE_CONNECT_KEY_ID.p8"
+zip fleetctl.zip "$FLEETCTL_BINARY_PATH"
 rcodesign notary-submit \
-       --api-issuer "$APPLE_APP_STORE_CONNECT_ISSUER_ID" \
-       --api-key "$APPLE_APP_STORE_CONNECT_KEY_ID" \
-       --wait fleetctl.zip
-rm -rf private_keys
+          --api-issuer "$APPLE_APP_STORE_CONNECT_ISSUER_ID" \
+          --api-key "$APPLE_APP_STORE_CONNECT_KEY_ID" \
+          --wait fleetctl.zip
