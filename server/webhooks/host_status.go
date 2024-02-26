639048edeb164297c79e4674a17b0dcbfc3314b4
Victor Lyuboslavsky <victor@fleetdm.com>
2024-02-26 15:40:35 -0600

    Putting in webhook functionality.

--- server/webhooks/host_status.go
+++ server/webhooks/host_status.go
@@ -16,6 +16,14 @@ func TriggerHostStatusWebhook(
 	ds fleet.Datastore,
 	logger kitlog.Logger,
 ) error {
+	err := triggerGlobalHostStatusWebhook(ctx, ds, logger)
+	if err != nil {
+		return err
+	}
+	return triggerTeamHostStatusWebhook(ctx, ds, logger)
+}
+
+func triggerGlobalHostStatusWebhook(ctx context.Context, ds fleet.Datastore, logger kitlog.Logger) error {
 	appConfig, err := ds.AppConfig(ctx)
 	if err != nil {
 		return ctxerr.Wrap(ctx, err, "getting app config")
@@ -25,34 +33,67 @@ func TriggerHostStatusWebhook(
 		return nil
 	}
 
-	level.Debug(logger).Log("enabled", "true")
+	level.Debug(logger).Log("global", "true", "enable_host_status_webhook", "true")
 
-	total, unseen, err := ds.TotalAndUnseenHostsSince(ctx, appConfig.WebhookSettings.HostStatusWebhook.DaysCount)
+	return processWebhook(ctx, ds, nil, appConfig.WebhookSettings.HostStatusWebhook)
+}
+
+func processWebhook(ctx context.Context, ds fleet.Datastore, teamID *uint, settings fleet.HostStatusWebhookSettings) error {
+	total, unseen, err := ds.TotalAndUnseenHostsSince(ctx, teamID, settings.DaysCount)
 	if err != nil {
 		return ctxerr.Wrap(ctx, err, "getting total and unseen hosts")
 	}
 
-	percentUnseen := float64(unseen) * 100.0 / float64(total)
-	if percentUnseen >= appConfig.WebhookSettings.HostStatusWebhook.HostPercentage {
-		url := appConfig.WebhookSettings.HostStatusWebhook.DestinationURL
+	unseenCount := len(unseen)
+	percentUnseen := float64(unseenCount) * 100.0 / float64(total)
+	if percentUnseen >= settings.HostPercentage {
+		url := settings.DestinationURL
 
 		message := fmt.Sprintf(
 			"More than %.2f%% of your hosts have not checked into Fleet for more than %d days. "+
 				"You've been sent this message because the Host status webhook is enabled in your Fleet instance.",
-			percentUnseen, appConfig.WebhookSettings.HostStatusWebhook.DaysCount,
+			percentUnseen, settings.DaysCount,
 		)
 		payload := map[string]interface{}{
 			"text": message,
 			"data": map[string]interface{}{
-				"unseen_hosts": unseen,
+				"unseen_hosts": unseenCount,
 				"total_hosts":  total,
-				"days_unseen":  appConfig.WebhookSettings.HostStatusWebhook.DaysCount,
+				"days_unseen":  settings.DaysCount,
+				"host_ids":     unseen,
 			},
+		}
+		if teamID != nil {
+			payload["data"].(map[string]interface{})["team_id"] = *teamID
 		}
 
 		err = server.PostJSONWithTimeout(ctx, url, &payload)
 		if err != nil {
 			return ctxerr.Wrapf(ctx, err, "posting to %s", url)
+		}
+	}
+
+	return nil
+}
+
+func triggerTeamHostStatusWebhook(ctx context.Context, ds fleet.Datastore, logger kitlog.Logger) error {
+
+	teams, err := ds.TeamsSummary(ctx)
+	if err != nil {
+		return ctxerr.Wrap(ctx, err, "getting teams summary")
+	}
+	for _, teamSummary := range teams {
+		team, err := ds.Team(ctx, teamSummary.ID)
+		if err != nil {
+			return ctxerr.Wrap(ctx, err, "getting team")
+		}
+		if !team.Config.WebhookSettings.FailingPoliciesWebhook.Enable {
+			continue
+		}
+		level.Debug(logger).Log("team", teamSummary.ID, "enable_host_status_webhook", "true")
+		err = processWebhook(ctx, ds, &teamSummary.ID, team.Config.WebhookSettings.HostStatusWebhook)
+		if err != nil {
+			return err
 		}
 	}
 
--- server/webhooks/host_status_test.go
+++ server/webhooks/host_status_test.go
@@ -41,22 +41,26 @@ func TestTriggerHostStatusWebhook(t *testing.T) {
 		return ac, nil
 	}
 
-	ds.TotalAndUnseenHostsSinceFunc = func(ctx context.Context, daysCount int) (int, int, error) {
+	ds.TotalAndUnseenHostsSinceFunc = func(ctx context.Context, teamID *uint, daysCount int) (int, []uint, error) {
 		assert.Equal(t, 2, daysCount)
-		return 10, 6, nil
+		return 10, []uint{1, 2, 3, 4, 5, 6}, nil
+	}
+
+	ds.TeamsSummaryFunc = func(ctx context.Context) ([]*fleet.TeamSummary, error) {
+		return nil, nil
 	}
 
 	require.NoError(t, TriggerHostStatusWebhook(context.Background(), ds, kitlog.NewNopLogger()))
 	assert.Equal(
 		t,
-		`{"data":{"days_unseen":2,"total_hosts":10,"unseen_hosts":6},"text":"More than 60.00% of your hosts have not checked into Fleet for more than 2 days. You've been sent this message because the Host status webhook is enabled in your Fleet instance."}`,
+		`{"data":{"days_unseen":2,"host_ids":[1,2,3,4,5,6],"total_hosts":10,"unseen_hosts":6},"text":"More than 60.00% of your hosts have not checked into Fleet for more than 2 days. You've been sent this message because the Host status webhook is enabled in your Fleet instance."}`,
 		requestBody,
 	)
 	requestBody = ""
 
-	ds.TotalAndUnseenHostsSinceFunc = func(ctx context.Context, daysCount int) (int, int, error) {
+	ds.TotalAndUnseenHostsSinceFunc = func(ctx context.Context, teamID *uint, daysCount int) (int, []uint, error) {
 		assert.Equal(t, 2, daysCount)
-		return 10, 1, nil
+		return 10, []uint{1}, nil
 	}
 
 	require.NoError(t, TriggerHostStatusWebhook(context.Background(), ds, kitlog.NewNopLogger()))
