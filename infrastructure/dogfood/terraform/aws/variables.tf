397dbd5795481008e8b3dc6d4aa725b8b525e1c0
Sharon Katz <sharon@fleetdm.com>
2024-02-27 16:07:48 -0500

    change version in tf files

--- infrastructure/dogfood/terraform/aws/variables.tf
+++ infrastructure/dogfood/terraform/aws/variables.tf
@@ -56,7 +56,7 @@ variable "database_name" {
 
 variable "fleet_image" {
   description = "the name of the container image to run"
-  default     = "fleetdm/fleet:v4.45.0"
+  default     = "fleetdm/fleet:v4.46.1"
 }
 
 variable "software_inventory" {
