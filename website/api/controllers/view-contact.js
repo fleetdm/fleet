19586562ddffb5a18e85ef61a13f7f78f9db649d
Eric <eashaw@sailsjs.com>
2024-03-05 14:37:47 -0600

    update contact page

--- website/api/controllers/view-contact.js
+++ website/api/controllers/view-contact.js
@@ -6,7 +6,13 @@ module.exports = {
 
   description: 'Display "Contact" page.',
 
-
+  inputs: {
+    talkToUs: {
+      type: 'boolean',
+      description: 'A boolean that determines whether or not to display the talk to us form when the contact page loads.',
+      defaultsTo: false,
+    },
+  },
   exits: {
 
     success: {
@@ -16,10 +22,13 @@ module.exports = {
   },
 
 
-  fn: async function () {
-
+  fn: async function ({talkToUs}) {
+    let formToDisplay = 'contact';
+    if(talkToUs) {
+      formToDisplay = 'talk-to-us';
+    }
     // Respond with view.
-    return {};
+    return {formToDisplay};
 
   }
 
