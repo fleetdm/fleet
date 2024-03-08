26715e1df900865cc17891c0fc5b1f6795391683
Eric <eashaw@sailsjs.com>
2024-03-08 10:20:19 -0600

    update mobile tooltips to open in modals

--- website/assets/js/pages/pricing.page.js
+++ website/assets/js/pages/pricing.page.js
@@ -4,6 +4,8 @@ parasails.registerPage('pricing', {
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
     pricingMode: 'all',
+    modal: '',
+    selectedFeature: undefined,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -18,11 +20,6 @@ parasails.registerPage('pricing', {
       container: '#pricing',
       trigger: 'hover focus',
     });
-    // Tooltips for mobile users are opened by the clickToggleMobileTooltip function.
-    $('[data-toggle="mobile-tooltip"]').tooltip({
-      container: '#pricing',
-      trigger: 'manual',// This allows users to click the links inside tooltips.
-    });
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -32,8 +29,14 @@ parasails.registerPage('pricing', {
     clickChangePricingMode: async function(pricingMode){
       this.pricingMode = pricingMode;
     },
-    clickToggleMobileTooltip: function(element){
-      $(element).tooltip('toggle');
+    clickOpenMobileTooltip: function(feature){
+      console.log(feature);
+      this.selectedFeature = feature;
+      this.modal = 'mobileTooltip';
+    },
+    closeModal: function() {
+      this.selectedFeature = undefined;
+      this.modal = '';
     }
   }
 });
