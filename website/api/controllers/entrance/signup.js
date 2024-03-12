d0251527a2bbda5ef47e7a28936918115df5ee5f
Eric <eashaw@sailsjs.com>
2024-03-12 14:19:00 -0500

    Update signup.js

--- website/api/controllers/entrance/signup.js
+++ website/api/controllers/entrance/signup.js
@@ -65,6 +65,7 @@ the account verification message.)`,
 
     primaryBuyingSituation: {
       type: 'string',
+      description: 'What the user will be using Fleet for.',
       isIn: [
         'endpoint-ops-security',
         'endpoint-ops-it',
