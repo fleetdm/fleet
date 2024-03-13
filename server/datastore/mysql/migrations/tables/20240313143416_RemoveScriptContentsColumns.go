da27911d7f52a43d29d8f6a5908ab48fa83363a8
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2024-03-13 15:31:05 -0400

    chore: update timestamps

--- server/datastore/mysql/migrations/tables/20240313143416_RemoveScriptContentsColumns.go
+++ server/datastore/mysql/migrations/tables/20240313143416_RemoveScriptContentsColumns.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20240304143416, Down_20240304143416)
+	MigrationClient.AddMigration(Up_20240313143416, Down_20240313143416)
 }
 
-func Up_20240304143416(tx *sql.Tx) error {
+func Up_20240313143416(tx *sql.Tx) error {
 	stmt := `ALTER TABLE scripts DROP COLUMN script_contents`
 	if _, err := tx.Exec(stmt); err != nil {
 		return fmt.Errorf("remove scripts.script_contents column: %w", err)
@@ -24,6 +24,6 @@ func Up_20240304143416(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20240304143416(tx *sql.Tx) error {
+func Down_20240313143416(tx *sql.Tx) error {
 	return nil
 }
