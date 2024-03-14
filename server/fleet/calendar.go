09c00911f93096cc95c8912d6e6dc5af0911e6dd
Victor Lyuboslavsky <victor.lyuboslavsky@gmail.com>
2024-03-14 14:07:13 -0500

    Calendar interface (#17633)
    # Checklist for submitter
    - [ ] Changes file added for user-visible changes in `changes/` or
    `orbit/changes/`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [ ] Added support on fleet's osquery simulator `cmd/osquery-perf` for
    new osquery data ingestion features.
    - [ ] Added/updated tests
    - [x] Manual QA for all new/changed functionality

---
+++ server/fleet/calendar.go
@@ -0,0 +1,24 @@
+package fleet
+
+import "time"
+
+type DayEndedError struct {
+	Msg string
+}
+
+func (e DayEndedError) Error() string {
+	return e.Msg
+}
+
+type Calendar interface {
+	// Connect to calendar. This method must be called first. Currently, config must be a *GoogleCalendarConfig
+	Connect(config any) (Calendar, error)
+	// GetAndUpdateEvent retrieves the event from the calendar.
+	// If the event has been modified, it returns the updated event.
+	// If the event has been deleted, it schedules a new event with given body callback and returns the new event.
+	GetAndUpdateEvent(event *CalendarEvent, genBodyFn func() string) (updatedEvent *CalendarEvent, updated bool, err error)
+	// CreateEvent creates a new event on the calendar on the given date. DayEndedError is returned if there is no time left on the given date to schedule event.
+	CreateEvent(dateOfEvent time.Time, body string) (event *CalendarEvent, err error)
+	// DeleteEvent deletes the event with the given ID.
+	DeleteEvent(event *CalendarEvent) error
+}
