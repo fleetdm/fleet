d5953c7732c4f90cd3249994752b08c05b286b6e
Lucas Rodriguez <lucas@fleetdm.com>
2024-03-22 13:47:26 -0300

    Always create event on next Thursday

--- cmd/fleet/calendar_cron_test.go
+++ cmd/fleet/calendar_cron_test.go
@@ -12,72 +12,75 @@ func TestGetPreferredCalendarEventDate(t *testing.T) {
 		return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
 	}
 	for _, tc := range []struct {
-		name                  string
-		year                  int
-		month                 time.Month
-		daysStart             int
-		daysEnd               int
-		webhookFiredThisMonth bool
+		name      string
+		year      int
+		month     time.Month
+		daysStart int
+		daysEnd   int
 
 		expected time.Time
 	}{
 		{
-			name:                  "March 2024 (webhook hasn't fired)",
-			year:                  2024,
-			month:                 3,
-			daysStart:             1,
-			daysEnd:               31,
-			webhookFiredThisMonth: false,
+			name:      "March 2024 (before 3rd Tuesday)",
+			year:      2024,
+			month:     3,
+			daysStart: 1,
+			daysEnd:   19,
 
 			expected: date(2024, 3, 19),
 		},
 		{
-			name:                  "March 2024 (webhook has fired, days before 3rd Tuesday)",
-			year:                  2024,
-			month:                 3,
-			daysStart:             1,
-			daysEnd:               18,
-			webhookFiredThisMonth: true,
-
-			expected: date(2024, 3, 19),
-		},
-		{
-			name:                  "March 2024 (webhook has fired, days after 3rd Tuesday)",
-			year:                  2024,
-			month:                 3,
-			daysStart:             20,
-			daysEnd:               30,
-			webhookFiredThisMonth: true,
+			name:      "March 2024 (past 3rd Tuesday)",
+			year:      2024,
+			month:     3,
+			daysStart: 20,
+			daysEnd:   31,
 
 			expected: date(2024, 4, 16),
 		},
 		{
-			name:                  "April 2024 (webhook hasn't fired)",
-			year:                  2024,
-			month:                 4,
-			daysEnd:               30,
-			webhookFiredThisMonth: false,
+			name:      "April 2024 (before 3rd Tuesday)",
+			year:      2024,
+			month:     4,
+			daysStart: 1,
+			daysEnd:   16,
 
 			expected: date(2024, 4, 16),
+		},
+		{
+			name:      "April 2024 (after 3rd Tuesday)",
+			year:      2024,
+			month:     4,
+			daysStart: 17,
+			daysEnd:   30,
+
+			expected: date(2024, 5, 21),
+		},
+		{
+			name:      "May 2024 (before 3rd Tuesday)",
+			year:      2024,
+			month:     5,
+			daysStart: 1,
+			daysEnd:   21,
+
+			expected: date(2024, 5, 21),
+		},
+		{
+			name:      "May 2024 (after 3rd Tuesday)",
+			year:      2024,
+			month:     5,
+			daysStart: 22,
+			daysEnd:   31,
+
+			expected: date(2024, 6, 18),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			for day := tc.daysStart; day <= tc.daysEnd; day++ {
-				actual := getPreferredCalendarEventDate(tc.year, tc.month, day, tc.webhookFiredThisMonth)
+				actual := getPreferredCalendarEventDate(tc.year, tc.month, day)
 				require.NotEqual(t, actual.Weekday(), time.Saturday)
 				require.NotEqual(t, actual.Weekday(), time.Sunday)
-				if day <= tc.expected.Day() || tc.webhookFiredThisMonth {
-					require.Equal(t, tc.expected, actual)
-				} else {
-					today := date(tc.year, tc.month, day)
-					if weekday := today.Weekday(); weekday == time.Friday {
-						require.Equal(t, today.AddDate(0, 0, +3), actual)
-					} else if weekday == time.Saturday {
-						require.Equal(t, today.AddDate(0, 0, +2), actual)
-					} else {
-						require.Equal(t, today.AddDate(0, 0, +1), actual)
-					}
-				}
+				require.Equal(t, tc.expected, actual)
 			}
 		})
 	}
