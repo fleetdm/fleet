a65dbff70eb5e356384c9681917b4fe71aed1c99
Sharon Katz <121527325+sharon-fdm@users.noreply.github.com>
2024-03-25 22:41:32 -0400

    Update publish.go

--- tools/publish-automation/publish.go
+++ tools/publish-automation/publish.go
@@ -13,19 +13,20 @@ func main() {
 	version := getVersion()
 	makeSureNPMVersionIsCoherent(version)
 	if !openReleasesPage(version) {
-		fmt.Println("Script Failed.")
+		fmt.Println("Error - could not publish the fleat release.")
+		fmt.Println("Try manually at https://github.com/fleetdm/fleet/releases/edit/fleet-" + version)
 		return
 	}
 	if !npmPublish() {
-		fmt.Println("Script Failed.")
+		fmt.Println("Error - npm publish failed.")
 		return
 	}
 	if !checkDocker(version) {
-		fmt.Println("Script Failed.")
+		fmt.Println("Error - no docker image found.")
 		return
 	}
 	if !deployDogfood(version) {
-		fmt.Println("Script Failed.")
+		fmt.Println("Error -  could not deploy to dogfood.")
 		return
 	}
 	if !closeMilestoneTickets(version) {
