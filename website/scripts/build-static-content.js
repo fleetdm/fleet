a0e12ecea186d9aaa0f88229706af5f4bc7fa98d
Eric <eashaw@sailsjs.com>
2024-03-29 18:49:27 -0500

    Update build-static-content.js

--- website/scripts/build-static-content.js
+++ website/scripts/build-static-content.js
@@ -734,7 +734,7 @@ module.exports = {
                 let platformString = '<br> **Only available on ';// start building a string to add to the column's description
 
                 if(column.platforms.length > 3) {// FUTURE: add support for more than three platform values in columns.
-                  throw new Error('Support for more than three platforms has not been implemented yet.');
+                  throw new Error('Support for more than three platforms in columns has not been implemented yet. If this column is supported on all platforms, you can omit the platforms array entirely.');
                 }
 
                 if(column.platforms.length === 3) { // Because there are only four options for platform, we can safely assume that there will be at most 3 platforms, so we'll just handle this one of three ways
