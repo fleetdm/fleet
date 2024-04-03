726acc4efb9deaa5a1a1a1c643842ac49fc91306
Eric <eashaw@sailsjs.com>
2024-04-02 21:52:31 -0500

    update action

--- website/api/controllers/save-questionnaire-progress.js
+++ website/api/controllers/save-questionnaire-progress.js
@@ -10,10 +10,11 @@ module.exports = {
   inputs: {
     currentStep: {
       type: 'string',
-      // isIn: [TODO]
+      description: 'The step of the get started questionnaire that is being saved.'
     },
     formData: {
       type: {},
+      description: 'The formdata that will be saved for this step of the get started questionnaire'
     }
   },
 
@@ -24,17 +25,26 @@ module.exports = {
 
 
   fn: async function ({currentStep, formData}) {
+    // find this user's DB record.
     let userRecord = await User.findOne({id: this.req.me.id});
     let questionnaireProgress;
-    // console.log(userRecord.getStartedQuestionnarieAnswers);
+    // If this user doesn't have a currentGetStartedQuestionnarieStep or getStartedQuestionnarieAnswers
     if(!userRecord.currentGetStartedQuestionnarieStep || _.isEmpty(userRecord.getStartedQuestionnarieAnswers)) {
       questionnaireProgress = {};
-    } else {
+    } else {// other wise clone it from the user record.
       questionnaireProgress = _.clone(userRecord.getStartedQuestionnarieAnswers);
     }
+    if(currentStep === 'what-are-you-using-fleet-for') {
+      let primaryBuyingSituation = formData.primaryBuyingSituation;
+      await User.updateOne({id: this.req.me.id}).set({primaryBuyingSituation});
+    }
+    // Set the user's answer to the current step.
     questionnaireProgress[currentStep] = formData;
+    // Clone the questionnaireProgress to prevent any mutations from sending it through the updateOne Waterline method.
     let getStartedProgress = _.clone(questionnaireProgress);
+    // Update the user's database model.
     await User.updateOne({id: userRecord.id}).set({getStartedQuestionnarieAnswers: questionnaireProgress, currentGetStartedQuestionnarieStep: currentStep});
+    // Return the JSON dictionary of form data submitted by this user.
     return getStartedProgress;
   }
 
