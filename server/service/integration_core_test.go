44cc5b2ea6c8177bc6dc3dc51943c683321ddf39
Roberto Dip <me@roperzh.com>
2024-04-08 11:10:29 -0300

    Prevent MDM migration without assigned ADE profile (#17999)
    for #15929 this prevents us sending the notification to start the MDM
    migration if the device doesn't have the right JSON profile assigned.

--- server/service/integration_core_test.go
+++ server/service/integration_core_test.go
@@ -8253,11 +8253,8 @@ func (s *integrationTestSuite) TestOrbitConfigNotifications() {
 	require.False(t, resp.Notifications.RenewEnrollmentProfile)
 
 	// simulate ABM assignment
-	mysql.ExecAdhocSQL(t, s.ds, func(q sqlx.ExtContext) error {
-		insertAppConfigQuery := `INSERT INTO host_dep_assignments (host_id) VALUES (?)`
-		_, err = q.ExecContext(context.Background(), insertAppConfigQuery, hFleetMDM.ID)
-		return err
-	})
+	err = s.ds.UpsertMDMAppleHostDEPAssignments(ctx, []fleet.Host{*hFleetMDM})
+	require.NoError(t, err)
 	err = s.ds.SetOrUpdateMDMData(context.Background(), hSimpleMDM.ID, false, true, "https://simplemdm.com", false, fleet.WellKnownMDMSimpleMDM, "")
 	require.NoError(t, err)
 	resp = orbitGetConfigResponse{}
