c91fcad04f781f87191945c5807bc47bdcdbe161
mostlikelee <tim@mostlikelee.com>
2024-04-11 11:27:50 -0600

    defer file closings

--- server/vulnerabilities/nvd/sync/utils.go
+++ server/vulnerabilities/nvd/sync/utils.go
@@ -8,30 +8,34 @@ import (
 	"os"
 )
 
+// CompressFile compresses a file using gzip and writes it to a new file
+// with the given name and removes the old file.
 func CompressFile(fileName string, newFileName string) error {
 	// Read old file
 	file, err := os.Open(fileName)
 	if err != nil {
 		return err
 	}
+	defer file.Close()
+
 	read := bufio.NewReader(file)
 	data, err := io.ReadAll(read)
 	if err != nil {
 		return err
 	}
-	file.Close()
 
 	// Write new file
 	newFile, err := os.Create(newFileName)
 	if err != nil {
 		return err
 	}
+	defer newFile.Close()
+
 	writer := gzip.NewWriter(newFile)
+	defer writer.Close()
 	if _, err = writer.Write(data); err != nil {
 		return err
 	}
-	writer.Close()
-	newFile.Close()
 
 	// Remove old file
 	if err = os.Remove(fileName); err != nil {
