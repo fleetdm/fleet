0a45bfdc3798bc5d8cc15c69ca8e700fa659dd17
Gabriel Hernandez <ghernandez345@gmail.com>
2024-04-17 16:10:25 +0100

    test for editlabel page and adding get label mock handler

---
+++ frontend/test/handlers/label-handlers.ts
@@ -0,0 +1,16 @@
+import { rest } from "msw";
+
+import { baseUrl } from "test/test-utils";
+import { createMockLabel } from "__mocks__/labelsMock";
+import { ILabel } from "interfaces/label";
+
+// eslint-disable-next-line import/prefer-default-export
+export const getLabelHandler = (overrides: Partial<ILabel>) =>
+  rest.get(baseUrl("/labels/:id"), (req, res, context) => {
+    console.log("id", req.params);
+    return res(
+      context.json({
+        label: createMockLabel({ ...overrides }),
+      })
+    );
+  });
