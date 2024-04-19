cb035d873dcb1afc47286c3e40282a3d123c4017
Eric <eashaw@sailsjs.com>
2024-04-18 21:18:36 -0500

    Website: add `psychologicalStage` to User model (#18408)
    Related to: https://github.com/fleetdm/confidential/issues/6155
    
    Changes:
    - Added a new attribute to the `User` model: `psychologicalStage`
    - Updated `save-questionnaire-progress` to update a user's
    `psychologicalStage` based on their current answers/progress on the
    /start questionnaire
    - Updated the personalization on the /contact page to use the
    `psychologicalStage` from the user model
    - Updated the value of a unselectable form option on the /start page

--- website/assets/js/pages/contact.page.js
+++ website/assets/js/pages/contact.page.js
@@ -39,8 +39,8 @@ parasails.registerPage('contact', {
     // Success state when form has been submitted
     cloudSuccess: false,
 
-    // For personalizing the message at the top of the contact form.
-    buyingStage: undefined,
+    // For personalizing the message at the top of the contact form for logged-in users.
+    psychologicalStage: undefined,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -67,23 +67,7 @@ parasails.registerPage('contact', {
         this.formDataToPrefillForLoggedInUsers.primaryBuyingSituation = this.me.primaryBuyingSituation;
       }
       this.formData = _.clone(this.formDataToPrefillForLoggedInUsers);
-      // If this user has submitted the /start questionnaire, determine their buying stage based on the answers they provided
-      if(!_.isEmpty(this.me.getStartedQuestionnaireAnswers)) {
-        let getStartedQuestionnaireAnswers = _.clone(this.me.getStartedQuestionnaireAnswers);
-        if(getStartedQuestionnaireAnswers['have-you-ever-used-fleet']) {
-          // If the user has Fleet deployed, then we'll assume they're stage five.
-          if(getStartedQuestionnaireAnswers['have-you-ever-used-fleet'].fleetUseStatus === 'yes-deployed' ||
-            getStartedQuestionnaireAnswers['have-you-ever-used-fleet'].fleetUseStatus === 'yes-recently-deployed') {
-            this.buyingStage = 'five';
-          }
-        }
-        if(getStartedQuestionnaireAnswers['what-did-you-think']){
-          // If this user has completed the "What did you think" step and wants to self-host Fleet, we'll assume theyre stage four.
-          if(getStartedQuestionnaireAnswers['what-did-you-think'].whatDidYouThink === 'deploy-fleet-in-environment'){
-            this.buyingStage = 'four';
-          }
-        }
-      }
+      this.psychologicalStage = this.me.psychologicalStage;
     }
   },
   mounted: async function() {
