042237251dc529e615965a481d269d7c14c9ea72
Mike McNeil <mikermcneil@users.noreply.github.com>
2024-04-20 17:00:05 -0500

    Create get-human-interpretation-from-osquery-sql.js

---
+++ website/api/controllers/get-human-interpretation-from-osquery-sql.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Get human interpretation from osquery sql',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
