3dfdaafb7ac57eeb8dc275c761ebd748ec423fea
Martin Angers <martin.n.angers@gmail.com>
2024-04-22 15:49:30 -0400

    Skeleton of the storage abstraction

---
+++ server/fleet/software_installer.go
@@ -0,0 +1,33 @@
+package fleet
+
+import (
+	"context"
+	"io"
+)
+
+// SoftwareInstallerStore is the interface to store and retrieve software
+// installer files. Fleet supports storing to the local filesystem and to an
+// S3 bucket.
+type SoftwareInstallerStore interface {
+	// TODO: the idea here is that the flow would look like this:
+	// * User uploads software via API
+	// * Server hashes the contents, extracts name and version
+	// * Server saves metadata (hash, name, version, other stuff) to DB
+	// * Server stores contents to storage location under that hash
+	// And later on, to retrieve it:
+	// * Server reads metadata from DB, gets hash
+	// * Server retrieves contents from storage location using hash
+	//
+	// 'installerID' in the signature is really the hex-encoded hash.
+	Get(ctx context.Context, installerID string) (io.ReadCloser, int64, error)
+	Put(ctx context.Context, installerID string, content io.ReadSeeker) (string, error)
+	// not strictly required, but could save an upload if we already have this
+	// content.
+	Exists(ctx context.Context, installerID string) (bool, error)
+}
+
+// TODO: to be defined by the other sub-tasks, intended to be used to store
+// the metadata of an installer, e.g. name, version, package extension, etc.
+type SoftwareInstaller struct {
+	Filename string
+}
