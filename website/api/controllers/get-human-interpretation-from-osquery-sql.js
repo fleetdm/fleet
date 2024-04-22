1b00f90092df7fbcdefb00a1168ff61dd8840b5c
Mike McNeil <mikermcneil@users.noreply.github.com>
2024-04-22 11:03:34 -0500

    be sure and test this

--- website/api/controllers/get-human-interpretation-from-osquery-sql.js
+++ website/api/controllers/get-human-interpretation-from-osquery-sql.js
@@ -33,8 +33,53 @@ module.exports = {
 
   fn: async function ({sql}) {
 
-    let report;// TODO: call openai
-    // TODO: also do the .tolerate
+    if (!sails.config.custom.openAiSecret) {
+      throw new Error('sails.config.custom.openAiSecret not set.');
+    }//•
+
+    let prompt = `Given this osquery policy: aka a query which either passes (≥1 row) or fails (0 rows) for a given laptop, what risks might we anticipate from that laptop having failed the policy?
+
+Here is the query:
+\`\`\`
+${sql}
+\`\`\`
+
+Remember to minimize the number of words used!
+
+Please give me all of the above in JSON, with this data shape:
+
+\`\`\`
+{
+  risks: 'TODO',
+  whatWillProbablyHappenDuringMaintenance: 'TODO'
+}
+\`\`\``;
+
+    let BASE_MODEL = 'gpt-4';// The base model to use.  https://platform.openai.com/docs/models/gpt-4
+    // (Max tokens for gpt-3.5 ≈≈ 4000) (Max tokens for gpt-4 ≈≈ 8000)
+    // [?] API: https://platform.openai.com/docs/api-reference/chat/create
+    let report = await sails.helpers.http.post('https://api.openai.com/v1/chat/completions', {
+      model: BASE_MODEL,
+      messages: [// https://platform.openai.com/docs/guides/chat/introduction
+        {
+          role: 'user',
+          content: prompt
+        }
+      ],
+      temperature: 0.7,
+      max_tokens: 256//eslint-disable-line camelcase
+    }, {
+      Authorization: `Bearer ${sails.config.custom.openAiSecret}`
+    })
+    .tolerate((err)=>{
+      // FUTURE: Actual negotiate errors instead of just pretending it works but sending back garbage.
+      let failureMessage = 'Failed to generate human interpretation using generative AI.';
+      sails.log.warn(failureMessage+'  Error details from LLM: '+err.stack);
+      return {
+        risks: failureMessage,
+        whatWillProbablyHappenDuringMaintenance: failureMessage
+      };
+    });
 
     return report;
 
