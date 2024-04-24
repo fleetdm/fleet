845984e3e8d5e6775e1620899b334d9446d98b38
Sarah Gillespie <73313222+gillespi314@users.noreply.github.com>
2024-04-24 13:37:35 -0500

    Update backend `authz` for software installers feature (#18513)

--- server/fleet/software_installer.go
+++ server/fleet/software_installer.go
@@ -18,8 +18,7 @@ type SoftwareInstallerStore interface {
 // FailingSoftwareInstallerStore is an implementation of SoftwareInstallerStore
 // that fails all operations. It is used when S3 is not configured and the
 // local filesystem store could not be setup.
-type FailingSoftwareInstallerStore struct {
-}
+type FailingSoftwareInstallerStore struct{}
 
 func (FailingSoftwareInstallerStore) Get(ctx context.Context, installerID string) (io.ReadCloser, int64, error) {
 	return nil, 0, errors.New("software installer store not properly configured")
@@ -31,4 +30,17 @@ func (FailingSoftwareInstallerStore) Put(ctx context.Context, installerID string
 
 func (FailingSoftwareInstallerStore) Exists(ctx context.Context, installerID string) (bool, error) {
 	return false, errors.New("software installer store not properly configured")
+}
+
+// SoftwareInstaller represents a software installer package that can be used to install software on
+// hosts in Fleet.
+type SoftwareInstaller struct {
+	// TeamID is the ID of the team. A value of nil means it is scoped to hosts that are assigned to
+	// no team.
+	TeamID *uint `json:"team_id"`
+}
+
+// AuthzType implements authz.AuthzTyper.
+func (s *SoftwareInstaller) AuthzType() string {
+	return "software_installer"
 }
