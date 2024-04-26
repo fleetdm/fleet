08b5e1c7b5853deffd6e1237e3c106b1015a15a0
Roberto Dip <dip.jesusr@gmail.com>
2024-04-26 12:09:01 -0300

    fixed a bug causing servers with Windows MDM only to crash (#18568)

--- cmd/fleet/cron.go
+++ cmd/fleet/cron.go
@@ -1037,21 +1037,12 @@ func newMDMProfileManager(
 		defaultInterval = 30 * time.Second
 	)
 
-	if !cfg.IsAppleSCEPSet() {
-		return nil, ctxerr.New(ctx, "SCEP configuration is required")
-	}
-
-	cert, _, _, err := cfg.AppleSCEP()
-	if err != nil {
-		return nil, ctxerr.Wrap(ctx, err, "getting Apple SCEP keypair")
-	}
-
 	logger = kitlog.With(logger, "cron", name)
 	s := schedule.New(
 		ctx, name, instanceID, defaultInterval, ds, ds,
 		schedule.WithLogger(logger),
 		schedule.WithJob("manage_apple_profiles", func(ctx context.Context) error {
-			return service.ReconcileAppleProfiles(ctx, ds, commander, logger, cert)
+			return service.ReconcileAppleProfiles(ctx, ds, commander, logger, cfg)
 		}),
 		schedule.WithJob("manage_apple_declarations", func(ctx context.Context) error {
 			return service.ReconcileAppleDeclarations(ctx, ds, commander, logger)
