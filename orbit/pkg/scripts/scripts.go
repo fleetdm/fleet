79e584a28fb7b6f59665daa72a8a0a26dd475140
dantecatalfamo <dante.catalfamo@gmail.com>
2024-04-30 11:15:41 -0400

    Remove obsolete comment

--- orbit/pkg/scripts/scripts.go
+++ orbit/pkg/scripts/scripts.go
@@ -108,8 +108,6 @@ func (r *Runner) runOne(script *fleet.HostScriptResult) (finalErr error) {
 		ext = ".ps1"
 	}
 	scriptFile := filepath.Join(runDir, "script"+ext)
-	// the file does not need the executable bit set, it will be executed as
-	// argument to powershell, /bin/sh, or (/usr)/bin/zsh.
 	if err := os.WriteFile(scriptFile, []byte(script.ScriptContents), constant.DefaultFileMode); err != nil {
 		return fmt.Errorf("write script file: %w", err)
 	}
