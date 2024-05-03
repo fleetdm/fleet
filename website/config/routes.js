8f4c83295836d53abc05f64cb92b3ac28cb2bd4e
Zach Wasserman <zach@fleetdm.com>
2024-05-03 13:28:01 -0700

    Add fleetdm.com shortlinks for RSA event (#18746)

--- website/config/routes.js
+++ website/config/routes.js
@@ -525,6 +525,12 @@ module.exports.routes = {
   'GET /swag':                   'https://kqphpqst851.typeform.com/to/Y6NYxM5A',
   'GET /community':              'https://join.slack.com/t/osquery/shared_invite/zt-1wkw5fzba-lWEyke60sjV6C4cdinFA1w',
 
+  // Temporary redirects
+  // =============================================================================================================
+  // For events, etc. that can be removed after a certain date. Please leave a comment with a valid until date.
+  'GET /rsaparty':               'https://www.eventbrite.com/e/fleet-launch-party-at-rsac-tickets-877549332677?aff=fleetdm', // Valid until 2024-05-09
+  'GET /rsavip':                 'https://www.eventbrite.com/e/fleet-launch-party-at-rsac-tickets-877549332677?aff=fleetdm&discount=Fleet2024', // Valid until 2024-05-09
+
 
   //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
   //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
