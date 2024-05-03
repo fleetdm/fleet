977023a081a98e059572528d93c5939236f859e4
Luke Heath <luke@fleetdm.com>
2024-05-03 09:27:28 -0500

    Update heroku

--- infrastructure/heroku/main.tf
+++ infrastructure/heroku/main.tf
@@ -22,12 +22,12 @@ resource "heroku_app" "speedy" {
 
 # MySQL-compatible add-on
 resource "heroku_addon" "database" {
-  app_id = heroku_app.speedy.name
+  app_id = heroku_app.speedy.id
   plan   = "cleardb:punch8" # Change to the specific plan you need
 }
 
 # Redis add-on
 resource "heroku_addon" "redis" {
-  app_id = heroku_app.speedy.name
+  app_id = heroku_app.speedy.id
   plan   = "heroku-redis:mini" # Change to the specific plan you need
 }
