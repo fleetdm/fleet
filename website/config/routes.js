4a3c96125c265ee74bb20adbf225352bbdb20c22
Rachael Shaw <r@rachael.wtf>
2024-05-03 13:22:30 -0500

    Update routes.js

--- website/config/routes.js
+++ website/config/routes.js
@@ -494,6 +494,7 @@ module.exports.routes = {
   'GET /learn-more-about/downgrading': '/docs/using-fleet/downgrading-fleet',
   'GET /learn-more-about/fleetd': '/docs/get-started/anatomy#fleetd',
   'GET /learn-more-about/rotating-enroll-secrets': '/docs/configuration/configuration-files#rotating-enroll-secrets',
+  'GET /learn-more-about/calendar-events': '/announcements/fleet-in-your-calendar-introducing-maintenance-windows',
 
   // Sitemap
   // =============================================================================================================
