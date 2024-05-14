c7e2a2121f012476d2c9712c1b49d5ae9db0091f
Roberto Dip <rroperzh@gmail.com>
2024-05-14 18:43:27 -0300

    adjust mock with the right value

--- frontend/__mocks__/hostMock.ts
+++ frontend/__mocks__/hostMock.ts
@@ -149,7 +149,7 @@ export const createMockHostSoftware = (
 };
 
 const DEFAULT_GET_HOST_SOFTWARE_RESPONSE_MOCK: IGetHostSoftwareResponse = {
-  count: 0,
+  count: 1,
   software: [createMockHostSoftware()],
   meta: {
     has_next_results: false,
