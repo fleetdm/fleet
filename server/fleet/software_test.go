f0e390358595283874e8452738a93c7890294564
Roberto Dip <dip.jesusr@gmail.com>
2024-05-14 17:03:44 -0300

    add boilerplate to host installation result outputs (#18988)
    Specified by Marko, this should be returned by the API:
    
    <img width="642" alt="image"
    src="https://github.com/fleetdm/fleet/assets/4419992/945e899e-9527-43cb-ac42-88b02d4bc844">
    
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    <!-- Note that API documentation changes are now addressed by the
    product design team. -->
    
    - [x] Added/updated tests
    - [x] Manual QA for all new/changed functionality

--- server/fleet/software_test.go
+++ server/fleet/software_test.go
@@ -3,6 +3,7 @@ package fleet
 import (
 	"testing"
 
+	"github.com/fleetdm/fleet/v4/server/ptr"
 	"github.com/stretchr/testify/require"
 )
 
@@ -74,4 +75,84 @@ func TestParseSoftwareLastOpenedAtRowValue(t *testing.T) {
 	lastOpenedAt, err = ParseSoftwareLastOpenedAtRowValue("1694026958")
 	require.NoError(t, err)
 	require.NotZero(t, lastOpenedAt)
+}
+
+func TestEnhanceOutputDetails(t *testing.T) {
+	tests := []struct {
+		name                            string
+		hsr                             HostSoftwareInstallerResult
+		expectedPreInstallQueryOutput   string
+		expectedOutput                  string
+		expectedPostInstallScriptOutput string
+	}{
+		{
+			name: "pending status",
+			hsr: HostSoftwareInstallerResult{
+				Status: SoftwareInstallerPending,
+			},
+			expectedPreInstallQueryOutput:   "",
+			expectedOutput:                  "",
+			expectedPostInstallScriptOutput: "",
+		},
+		{
+			name: "non-pending status with empty PreInstallQueryOutput and successful install",
+			hsr: HostSoftwareInstallerResult{
+				Status:                SoftwareInstallerInstalled,
+				InstallScriptExitCode: ptr.Int(0),
+				PreInstallQueryOutput: "1",
+			},
+			expectedPreInstallQueryOutput:   "Query returned result\nProceeding to install...",
+			expectedOutput:                  "Installing software...\nSuccess\n",
+			expectedPostInstallScriptOutput: "",
+		},
+		{
+			name: "non-pending status with empty PreInstallQueryOutput and failed install",
+			hsr: HostSoftwareInstallerResult{
+				Status:                SoftwareInstallerFailed,
+				InstallScriptExitCode: ptr.Int(1),
+				PreInstallQueryOutput: "1",
+			},
+			expectedPreInstallQueryOutput:   "Query returned result\nProceeding to install...",
+			expectedOutput:                  "Installing software...\nFailed\n",
+			expectedPostInstallScriptOutput: "",
+		},
+		{
+			name: "non-pending status with non-empty PreInstallQueryOutput and disabled scripts",
+			hsr: HostSoftwareInstallerResult{
+				Status:                SoftwareInstallerFailed,
+				InstallScriptExitCode: ptr.Int(-2),
+				PreInstallQueryOutput: "1",
+			},
+			expectedPreInstallQueryOutput:   "Query returned result\nProceeding to install...",
+			expectedOutput:                  "Installing software...\nError: Scripts are disabled for this host. To run scripts, deploy the fleetd agent with --scripts-enabled.",
+			expectedPostInstallScriptOutput: "",
+		},
+		{
+			name: "non-pending status with non-empty PreInstallQueryOutput and failed post install script",
+			hsr: HostSoftwareInstallerResult{
+				Status:                    SoftwareInstallerInstalled,
+				InstallScriptExitCode:     ptr.Int(0),
+				PostInstallScriptExitCode: ptr.Int(1),
+				PreInstallQueryOutput:     "1",
+				PostInstallScriptOutput:   "output!",
+			},
+			expectedPreInstallQueryOutput: "Query returned result\nProceeding to install...",
+			expectedOutput:                "Installing software...\nSuccess\n",
+			expectedPostInstallScriptOutput: `Running script...
+Exit code: 1 (Failed)
+output!
+Rolling back software install...
+Rolled back successfully
+`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.hsr.EnhanceOutputDetails()
+			require.Equal(t, tt.expectedPreInstallQueryOutput, tt.hsr.PreInstallQueryOutput)
+			require.Equal(t, tt.expectedOutput, tt.hsr.Output)
+			require.Equal(t, tt.expectedPostInstallScriptOutput, tt.hsr.PostInstallScriptOutput)
+		})
+	}
 }
