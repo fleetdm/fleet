fleetdm/fleet

commit 6e8e1bd0b49f212455ac637531d7f6b6148ed049

Install script extension (#19012)
Because windows won't run powershell scripts without it 👎

orbit/pkg/installer/installer.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"sync"
 	"time"
 
@@ -212,7 +213,11 @@ func (r *Runner) installSoftware(ctx context.Context, installID string) (*fleet.
 		}
 	}()
 
-	installOutput, installExitCode, err := r.runInstallerScript(ctx, installer.InstallScript, installerPath, "install-script")
+	scriptExtension := ".sh"
+	if runtime.GOOS == "windows" {
+		scriptExtension = ".ps1"
+	}
+	installOutput, installExitCode, err := r.runInstallerScript(ctx, installer.InstallScript, installerPath, "install-script"+scriptExtension)
 	payload.InstallScriptOutput = &installOutput
 	payload.InstallScriptExitCode = &installExitCode
 	if err != nil {
@@ -220,7 +225,7 @@ func (r *Runner) installSoftware(ctx context.Context, installID string) (*fleet.
 	}
 
 	if installer.PostInstallScript != "" {
-		postOutput, postExitCode, postErr := r.runInstallerScript(ctx, installer.PostInstallScript, installerPath, "post-install-script")
+		postOutput, postExitCode, postErr := r.runInstallerScript(ctx, installer.PostInstallScript, installerPath, "post-install-script"+scriptExtension)
 		payload.PostInstallScriptOutput = &postOutput
 		payload.PostInstallScriptExitCode = &postExitCode
 

commit 5fb52f6baf20abb718347501bdd08d3ea5b1e5f5

Install script extension test (#19025)
Fixes a test I accidentally broke in #19012

orbit/pkg/installer/installer_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"strconv"
 	"testing"
 
@@ -285,8 +286,12 @@ func TestInstallerRun(t *testing.T) {
 		require.True(t, tmpDirFnCalled)
 
 		require.True(t, execCalled)
-		require.Contains(t, executedScripts, filepath.Join(tmpDir, "install-script"))
-		require.Contains(t, executedScripts, filepath.Join(tmpDir, "post-install-script"))
+		scriptExtension := ".sh"
+		if runtime.GOOS == "windows" {
+			scriptExtension = ".ps1"
+		}
+		require.Contains(t, executedScripts, filepath.Join(tmpDir, "install-script"+scriptExtension))
+		require.Contains(t, executedScripts, filepath.Join(tmpDir, "post-install-script"+scriptExtension))
 		require.Contains(t, execEnv, "INSTALLER_PATH="+filepath.Join(tmpDir, strconv.Itoa(int(installDetails.InstallerID))+".pkg"))
 
 		require.True(t, queryFnCalled)