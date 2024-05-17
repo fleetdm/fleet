54cca7b28a5c4f66f9994a42857346c688bbcb85
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2024-05-17 12:24:33 -0400

    Docs: Fix broken links & add redirects (#19066)

--- frontend/pages/admin/IntegrationsPage/cards/AutomaticEnrollment/components/AppleBusinessManagerSection/AppleBusinessManagerSection.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/AutomaticEnrollment/components/AppleBusinessManagerSection/AppleBusinessManagerSection.tsx
@@ -130,7 +130,7 @@ const AppleBusinessManagerSection = ({
           <span className={`${baseClass}__400-error-info`}>
             See our{" "}
             <CustomLink
-              url="https://fleetdm.com/docs/using-fleet/mdm-macos-setup#apple-business-manager-abm"
+              url="https://fleetdm.com/learn-more-about/setup-abm"
               text="ABM documentation"
               newTab
             />{" "}
