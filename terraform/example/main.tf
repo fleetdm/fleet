16967bc40aceb9616e0055ccb3b634ebef085202
Dave Herder <27025660+dherder@users.noreply.github.com>
2024-05-22 11:10:11 -0700

    reference 3 azs
    Co-authored-by: Robert Fairburn <8029478+rfairburn@users.noreply.github.com>

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -55,7 +55,7 @@ module "fleet" {
 
   vpc = {
     # By default, Availabililty zones for us-east-2 are configured. If an alternative region is desired,
-    # configure the azs variable below to the desired region. If you have an exported AWS-REGION or a
+    # configure the azs (3 required) variable below to the desired region.  If you have an exported AWS-REGION or a
     # region declared in ~/.aws/config, this value must match the region declared below.
     name = local.vpc_name
     # azs = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
