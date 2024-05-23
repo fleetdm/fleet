ab38e2dc2b9e81ff0d0f6dffde48760e2f4f6439
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2024-05-23 13:58:24 -0500

    Add todo

--- frontend/pages/DashboardPage/cards/ActivityFeed/ActivityItem/ActivityItem.tsx
+++ frontend/pages/DashboardPage/cards/ActivityFeed/ActivityItem/ActivityItem.tsx
@@ -1094,7 +1094,7 @@ interface IActivityItemProps {
     details: IActivityDetails
   ) => void;
 }
-
+// TODO: update global and host activities for self-service
 const ActivityItem = ({
   activity,
   isPremiumTier,
