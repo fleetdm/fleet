e5827e194f1f028cc03c2de4de157d82b66e5762
Sharon Katz <sharon@fleetdm.com>
2024-05-24 13:39:30 -0400

    fix sql

--- server/datastore/mysql/users.go
+++ server/datastore/mysql/users.go
@@ -278,7 +278,7 @@ func (ds *Datastore) DeleteUser(ctx context.Context, id uint) error {
 
 func tableRowsCount(ctx context.Context, db sqlx.QueryerContext, tableName string) (int, error) {
 	var count int
-	err := sqlx.GetContext(ctx, db, &count, `SELECT count(*) FROM ?`, tableName)
+	err := sqlx.GetContext(ctx, db, &count, fmt.Sprintf(`SELECT count(*) FROM %s`, tableName))
 	if err != nil {
 		return 0, err
 	}
