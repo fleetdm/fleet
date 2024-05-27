2af2c41572a56d429e39b4d514b9fb0bbc338d44
Eric <eashaw@sailsjs.com>
2024-05-27 16:43:40 -0500

    Website: Update round robin in salesforce helpers & move lead creation to save-questionnaire-progress. (#19209)
    Closes: #18932
    
    Changes:
    - Updated `update-or-create-contact-and-account.js` to not round robin
    new account records created.
    - Updated create-lead to reassign accounts owned by the integrations
    admin user when a new lead is created.
    - Updated logged warnings in signup, deliver-contact-form-message and
    deliver-talk-to-us-form-submission.
    - Updated signup.js to not create salesforce leads (only account and
    contact records)
    - Updated save-questionnaire-progress to create Salesforce leads when we
    know the user has a use case for Fleet.

--- website/api/controllers/deliver-talk-to-us-form-submission.js
+++ website/api/controllers/deliver-talk-to-us-form-submission.js
@@ -86,7 +86,7 @@ module.exports = {
         leadSource: 'Website - Contact forms',
         leadDescription: `Submitted the "Talk to us" form.`,
       }).tolerate((err)=>{
-        sails.log.warn(`Background task failed: When a user submitted the "Talk to us" form, a lead/contact could not be updated in the CRM for this email address: ${emailAddress}. Error:`, err.raw);
+        sails.log.warn(`Background task failed: When a user submitted the "Talk to us" form, a lead/contact could not be updated in the CRM for this email address: ${emailAddress}.`, err);
       });
     });//_∏_  (Meanwhile...)
 
