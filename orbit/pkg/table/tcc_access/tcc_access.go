896d481592779d3040886bd61e72d802a44043f2
Jacob Shandling <jacob@fleetdm.com>
2024-05-28 09:25:59 -0700

    col condition filtering

--- orbit/pkg/table/tcc_access/tcc_access.go
+++ orbit/pkg/table/tcc_access/tcc_access.go
@@ -5,6 +5,7 @@ package tcc_access
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os/exec"
 	"strings"
@@ -12,11 +13,22 @@ import (
 	"github.com/osquery/osquery-go/plugin/table"
 )
 
-// TODO - okay to use ~ for userPath?
+// TODO - QUESTION okay to use ~ for userPath? Seems to work.
+// ANSWER Lucas - no, get the actual absolute path
+// idea: list user's dir, use those paths.
+
+// QUESTION – case of MULTIPLE USERS include ALL user TCC dbs? How do we know which row belongs to
+// which user?
+// Lucas - solve by adding column "username", in place of/addition to(?) "source" column?
+// If user doesn't specify, include all?
+// product answer: add `username` column. QUESTION – what should be this column's value for the
+// system-sourced rows?
 var userPath, sysPath = "~/Library/Application Support/com.apple.TCC/TCC.db", "/Library/Application Support/com.apple.TCC/TCC.db"
 
-// TODO - instead of getting all rows from tcc.dbs, can we pass condition sent by user query into ?
+// TODO - QUESTION instead of getting all rows from tcc.dbs, can we pass condition sent by user query into ?
 // to only get desired rows? would elimnate need for filterByColEquality here
+// see tabl.go > QueryContext, seems like exactly this.
+// YES, looks like osquery via Constraints will do this filtering automatically in fact
 
 // var dbQuery = "SELECT service, client, client_type, auth_value, auth_reason, last_modified, policy_id, indirect_object_identifier, indirect_object_identifier_type FROM access WHERE ?;"
 
@@ -29,16 +41,15 @@ var dbColNames = []string{"service", "client", "client_type", "auth_value", "aut
 // Columns is the schema of the table.
 func Columns() []table.ColumnDefinition {
 	return []table.ColumnDefinition{
-		// TODO: how to add column descriptions?
-		table.TextColumn("source"),          // required
-		table.TextColumn("service"),         // required
-		table.TextColumn("client"),          // required
-		table.IntegerColumn("client_type"),  // required
-		table.IntegerColumn("auth_value"),   // required
-		table.IntegerColumn("auth_reason"),  // required
-		table.BigIntColumn("last_modified"), // required
+		table.TextColumn("source"),
+		// TODO - add a 'username' column that reports the username that a `user`-sourced row comes from
+		table.TextColumn("service"),
+		table.TextColumn("client"),
+		table.IntegerColumn("client_type"),
+		table.IntegerColumn("auth_value"),
+		table.IntegerColumn("auth_reason"),
+		table.BigIntColumn("last_modified"),
 		table.IntegerColumn("policy_id"),
-		// TODO - speced as "string" column - meaning text column?
 		table.TextColumn("indirect_object_identifier"),
 		table.IntegerColumn("indirect_object_identifier_type"),
 	}
@@ -46,9 +57,15 @@ func Columns() []table.ColumnDefinition {
 
 // Generate is called to return the results for the table at query time.
 // Constraints for generating can be retrieved from the queryContext.
+
 func Generate(ctx context.Context, queryContext table.QueryContext) ([]map[string]string, error) {
+	// TODO - check for invalid states:
+	// WHERE clause has operation other than equality
+	// SELECTed column is invalid
 	var uRs, sRs, rows []map[string]string
 	var err error
+	// TODO - update this to iterate over all existing users, assigning each respective value of
+	// "username" to that user's name.
 	uRs, err = getTCCAccessRows("user", userPath)
 	if err != nil {
 		return nil, err
@@ -57,11 +74,9 @@ func Generate(ctx context.Context, queryContext table.QueryContext) ([]map[strin
 	if err != nil {
 		return nil, err
 	}
+	rows = append(uRs, sRs...)
 
-	// TODO - how to get these from WHERE clauses of the query
-	var colToVals []colToVal
-
-	rows, err = filterByColEquality(colToVals, append(uRs, sRs...))
+	rows, err = filterByColEquality(queryContext.Constraints, rows)
 	if err != nil {
 		return nil, err
 	}
@@ -87,14 +102,22 @@ func getTCCAccessRows(source, dbPath string) ([]map[string]string, error) {
 	return rows, nil
 }
 
-type colToVal struct {
-	col string
-	val string
-}
+func filterByColEquality(constraints map[string]table.ConstraintList, rows []map[string]string) ([]map[string]string, error) {
+	// get a simple mapping of columns to the value a row must have for it, if any, as defined by the
+	// user-supplied query.
+	cVME, err := getColValsMustEqual(constraints)
+	if err != nil {
+		return nil, fmt.Errorf("generate failed: %w", err)
+	}
 
-func filterByColEquality(colToVals []colToVal, rows []map[string]string) ([]map[string]string, error) {
-	// TODO
-	return rows, nil
+	filteredRows := make([]map[string]string, 0, len(rows))
+	// for every row
+	for _, row := range rows {
+		if rowConstraintsSatisfied(row, cVME) {
+			filteredRows = append(filteredRows, row)
+		}
+	}
+	return filteredRows, nil
 }
 
 func parseTCCDbReadOutput(dbOut []byte) ([][]string, error) {
@@ -121,4 +144,41 @@ func buildTableRows(source string, parsedRows [][]string) []map[string]string {
 		rows = append(rows, row)
 	}
 	return rows
+}
+
+func getColValsMustEqual(constraints map[string]table.ConstraintList) (map[string]string, error) {
+	dbColNameSet := make(map[string]struct{}, len(dbColNames))
+	for _, name := range dbColNames {
+		dbColNameSet[name] = struct{}{}
+	}
+
+	cVME := make(map[string]string)
+	for col, constraintList := range constraints {
+		// TODO - QUESTION, should we check "Affinity" matches column typej–? LUCAs - no, all text
+
+		// check that col is valid column for this table
+		// TODO - move this check to top of Generate function
+		if _, ok := dbColNameSet[col]; !ok {
+			return nil, fmt.Errorf("generate failed: column '%w' not valid for tcc_access ", errors.New(col))
+		}
+		// TODO - move to top of Generate function
+		for _, constraint := range constraintList.Constraints {
+			if constraint.Operator != table.OperatorEquals {
+				// TODO - QUESTION can we add additional condition options in the future ? Lucas – wait to
+				// see how osquery handles these for us
+				return nil, errors.New("tcc_access only supports equality operation in WHERE clause")
+			}
+			cVME[col] = constraint.Expression
+		}
+	}
+	return cVME, nil
+}
+
+func rowConstraintsSatisfied(row map[string]string, constraints map[string]string) bool {
+	for col, targetVal := range constraints {
+		if row[col] != targetVal {
+			return false
+		}
+	}
+	return true
 }
