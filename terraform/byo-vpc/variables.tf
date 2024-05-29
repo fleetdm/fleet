4bfbe7ee10d4014d3cc8204f774187d97f69e37e
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2024-05-28 22:29:06 -0500

    Initial private key secret

--- terraform/byo-vpc/variables.tf
+++ terraform/byo-vpc/variables.tf
@@ -181,6 +181,7 @@ variable "fleet_config" {
     security_group_name          = optional(string, "fleet")
     iam_role_arn                 = optional(string, null)
     repository_credentials       = optional(string, "")
+    private_key_secret_name      = optional(string, "fleet-server-private-key")
     service = optional(object({
       name = optional(string, "fleet")
       }), {
@@ -278,6 +279,7 @@ variable "fleet_config" {
     security_group_name          = "fleet"
     iam_role_arn                 = null
     repository_credentials       = ""
+    private_key_secret_name      = "fleet-server-private-key"
     service = {
       name = "fleet"
     }
