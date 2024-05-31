e99b05757f6ac4423e5a7d946e4547f903fced0b
Eric <eashaw@sailsjs.com>
2024-05-31 17:28:54 -0500

    Update update-start-cta-visibility.js

--- website/api/controllers/account/update-start-cta-visibility.js
+++ website/api/controllers/account/update-start-cta-visibility.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 
-  friendlyName: 'Update start CTA visibility',// TODO is there a better name for this?
+  friendlyName: 'Update start CTA visibility',
 
 
   description: 'Sets a timestamp to determine when we should show the user the start CTA after they have dismissed it.',
@@ -21,9 +21,11 @@ module.exports = {
     if(!this.req.session){
       throw new Error('Consistency violation. When a user sent a request to the update start CTA visibility endpoint, a session is missing.');
     }
-    if(this.req.session.expandCtaAt){
+    if(this.req.session.expandCtaAt) {
+      // Expand the CTA.
       this.req.session.expandCtaAt = 0;
     } else {
+      // collapse CTA for 24 hours.
       let nowAt = Date.now();
       let tomorrowAt = nowAt + (24 * 60 * 60 * 1000);
       this.req.session.expandCtaAt = tomorrowAt;
