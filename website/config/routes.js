09a67e11e98db3dbfd3d809c32f350e4a05e18e9
Eric <eashaw@sailsjs.com>
2024-06-03 16:27:09 -0500

    Website: add `/better` page (#19394)
    Closes: https://github.com/fleetdm/confidential/issues/6159
    
    Changes:
    - Added a new route to the /transparency page (/better) that shows
    additional content.
    -
    
    ---------
    
    Co-authored-by: Mike Thomas <78363703+mike-j-thomas@users.noreply.github.com>

--- website/config/routes.js
+++ website/config/routes.js
@@ -105,6 +105,7 @@ module.exports.routes = {
     locals: {
       pageTitleForMeta: 'Transparency | Fleet',
       pageDescriptionForMeta: 'Learn what data osquery can see.',
+      showContactFormCta: false,
     }
   },
   'GET /new-license': {
@@ -296,6 +297,15 @@ module.exports.routes = {
       hideStartCTA: true,
       pageTitleForMeta: 'Start | Fleet',
       pageDescriptionForMeta: 'Get Started with Fleet. Spin up a local demo or get your Premium license key.',
+    }
+  },
+
+  'GET /better': {
+    action: 'view-transparency',
+    locals: {
+      showContactFormCta: true,
+      pageDescriptionForMeta: 'Discover how Fleet simplifies IT and security, prioritizing privacy, transparency, and trust for end users.',
+      pageTitleForMeta: 'Better with Fleet | Fleet'
     }
   },
 
