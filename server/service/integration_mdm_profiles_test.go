bd91f2d563f05b8946ba358d8cb638930f904e69
Lucas Rodriguez <lucas@fleetdm.com>
2024-06-05 09:14:49 -0300

    Fix tests

--- server/service/integration_mdm_profiles_test.go
+++ server/service/integration_mdm_profiles_test.go
@@ -1898,7 +1898,7 @@ func (s *integrationMDMTestSuite) TestHostMDMAppleProfilesStatus() {
 			SCEPChallenge: s.scepChallenge,
 			SCEPURL:       s.server.URL + apple_mdm.SCEPPath,
 			MDMURL:        s.server.URL + apple_mdm.MDMPath,
-		})
+		}, "MacBookPro16,1")
 
 		// enroll the device with orbit
 		var resp EnrollOrbitResponse
