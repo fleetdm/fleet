6a936a4158686f7a72bbe16110adad1b5f0af325
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2024-06-12 15:24:57 -0400

    fix: don't fail the MDM ingestion process if we can't find the idp uuid

--- server/datastore/mysql/hosts.go
+++ server/datastore/mysql/hosts.go
@@ -3678,7 +3678,12 @@ func (ds *Datastore) SetOrUpdateHostEmailsFromMdmIdpAccounts(
 	if fleetEnrollmentRef != "" {
 		idp, err := ds.GetMDMIdPAccountByUUID(ctx, fleetEnrollmentRef)
 		if err != nil {
-			return err
+			if !fleet.IsNotFound(err) {
+				return err
+			}
+
+			level.Debug(ds.logger).Log("msg", "mdm idp account not found", "uuid", fleetEnrollmentRef)
+			return nil
 		}
 		email = &idp.Email
 	}
