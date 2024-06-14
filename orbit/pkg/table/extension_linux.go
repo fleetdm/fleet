487e15875a0ed8e37f03a302e8a538b073ce632a
Dante Catalfamo <43040593+dantecatalfamo@users.noreply.github.com>
2024-06-14 16:56:58 -0400

    Disable `mdm_bridge` table on Windows Server (#19709)
    #19239

--- orbit/pkg/table/extension_linux.go
+++ orbit/pkg/table/extension_linux.go
@@ -10,10 +10,10 @@ import (
 	"github.com/osquery/osquery-go"
 )
 
-func PlatformTables(_ PluginOpts) []osquery.OsqueryPlugin {
+func PlatformTables(_ PluginOpts) ([]osquery.OsqueryPlugin, error) {
 	return []osquery.OsqueryPlugin{
 		cryptsetup.TablePlugin(osqueryLogger),            // table name is "cryptsetup_status"
 		falconctl.NewFalconctlOptionTable(osqueryLogger), // table name is "falconctl_option"
 		falcon_kernel_check.TablePlugin(osqueryLogger),   // table name is "falcon_kernel_check"
-	}
+	}, nil
 }
