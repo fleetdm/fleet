f56d2465542034030d6ef6bf34b1114d89574d7f
Zach Wasserman <zach@fleetdm.com>
2024-06-21 14:33:23 -0700

    Update DeleteHostModal.tsx

--- frontend/pages/hosts/components/DeleteHostModal/DeleteHostModal.tsx
+++ frontend/pages/hosts/components/DeleteHostModal/DeleteHostModal.tsx
@@ -76,7 +76,7 @@ const DeleteHostModal = ({
         <p>
           <CustomLink
             url={
-              "https://fleetdm.com/docs/using-fleet/faq#how-can-i-uninstall-fleetd"
+              "https://fleetdm.com/learn-more-about/uninstall-fleetd"
             }
             text="Uninstall Fleet's agent"
             newTab
