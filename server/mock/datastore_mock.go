0b22e41b0f6d369c10a64f651adb76dd04d94377
Victor Lyuboslavsky <victor@fleetdm.com>
2024-06-27 09:01:15 -0500

    Update mocks

--- server/mock/datastore_mock.go
+++ server/mock/datastore_mock.go
@@ -977,6 +977,8 @@ type CleanupUnusedSoftwareInstallersFunc func(ctx context.Context, softwareInsta
 
 type BatchSetSoftwareInstallersFunc func(ctx context.Context, tmID *uint, installers []*fleet.UploadSoftwareInstallerPayload) error
 
+type HasSelfServiceSoftwareInstallersFunc func(ctx context.Context, teamID *uint) (bool, error)
+
 type DataStore struct {
 	HealthCheckFunc        HealthCheckFunc
 	HealthCheckFuncInvoked bool
@@ -2414,6 +2416,9 @@ type DataStore struct {
 
 	BatchSetSoftwareInstallersFunc        BatchSetSoftwareInstallersFunc
 	BatchSetSoftwareInstallersFuncInvoked bool
+
+	HasSelfServiceSoftwareInstallersFunc        HasSelfServiceSoftwareInstallersFunc
+	HasSelfServiceSoftwareInstallersFuncInvoked bool
 
 	mu sync.Mutex
 }
@@ -5769,4 +5774,11 @@ func (s *DataStore) BatchSetSoftwareInstallers(ctx context.Context, tmID *uint, 
 	s.BatchSetSoftwareInstallersFuncInvoked = true
 	s.mu.Unlock()
 	return s.BatchSetSoftwareInstallersFunc(ctx, tmID, installers)
+}
+
+func (s *DataStore) HasSelfServiceSoftwareInstallers(ctx context.Context, teamID *uint) (bool, error) {
+	s.mu.Lock()
+	s.HasSelfServiceSoftwareInstallersFuncInvoked = true
+	s.mu.Unlock()
+	return s.HasSelfServiceSoftwareInstallersFunc(ctx, teamID)
 }
