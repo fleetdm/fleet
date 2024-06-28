de2b4ce13d0eaef3984e11804de1b9d7cff6fb14
dantecatalfamo <dante.catalfamo@gmail.com>
2024-06-28 15:39:01 -0400

    Detach child process before removing fleet

--- orbit/tools/cleanup/cleanup_macos.sh
+++ orbit/tools/cleanup/cleanup_macos.sh
@@ -1,9 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-sudo launchctl stop com.fleetdm.orbit
-sudo launchctl unload /Library/LaunchDaemons/com.fleetdm.orbit.plist
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
 
-sudo pkill fleet-desktop || true
-sudo rm -rf /Library/LaunchDaemons/com.fleetdm.orbit.plist /var/lib/orbit /usr/local/bin/orbit /var/log/orbit /opt/orbit/
 
-sudo pkgutil --forget com.fleetdm.orbit.base.pkg || true
+function remove_fleet {
+    launchctl stop com.fleetdm.orbit
+    launchctl unload /Library/LaunchDaemons/com.fleetdm.orbit.plist
+
+    pkill fleet-desktop || true
+    rm -rf /Library/LaunchDaemons/com.fleetdm.orbit.plist /var/lib/orbit /usr/local/bin/orbit /var/log/orbit /opt/orbit/
+
+    pkgutil --forget com.fleetdm.orbit.base.pkg || true
+}
+
+if [ $1 == "remove" ]; then
+    # We are in the detached child process
+    # Give the parent process time to report the success before removing
+    sleep 15
+    remove_fleet
+else
+    # We are in the parent shell, start the detached child and return success
+    echo "Removing fleet, system will be unenrolled"
+    echo "Executing detached child process"
+    nohup sh $0 remove >/dev/null 2>/dev/null </dev/null &
+fi
