501a8931cf7e3ab39ba799217f993cc5e57570ff
Sarah Gillespie <73313222+gillespi314@users.noreply.github.com>
2024-06-28 19:36:50 -0500

    Enable users to edit ABM settings via UI when ABM token is invalid (#20111)
    Issue #20090
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [ ] Added/updated tests
    - [x] Manual QA for all new/changed functionality

--- frontend/pages/admin/IntegrationsPage/cards/AutomaticEnrollment/components/MdmPlatformsSection/MdmPlatformsSection.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/AutomaticEnrollment/components/MdmPlatformsSection/MdmPlatformsSection.tsx
@@ -1,16 +1,7 @@
 import React, { useContext } from "react";
-import { useQuery } from "react-query";
 import { InjectedRouter } from "react-router";
-
-import { AxiosError } from "axios";
-
 import PATHS from "router/paths";
-import { IMdmAppleBm } from "interfaces/mdm";
-import mdmAppleBmAPI from "services/entities/mdm_apple_bm";
 import { AppContext } from "context/app";
-
-import DataError from "components/DataError";
-import Spinner from "components/Spinner/Spinner";
 import SectionHeader from "components/SectionHeader";
 
 import WindowsAutomaticEnrollmentCard from "./components/WindowsAutomaticEnrollmentCard";
@@ -25,19 +16,6 @@ interface IMdmPlatformsSectionProps {
 const MdmPlatformsSection = ({ router }: IMdmPlatformsSectionProps) => {
   const { config } = useContext(AppContext);
 
-  const {
-    data: mdmAppleBm,
-    isLoading: isLoadingMdmAppleBm,
-    error: errorMdmAppleBm,
-  } = useQuery<IMdmAppleBm, AxiosError, IMdmAppleBm>(
-    ["mdmAppleBmAPI"],
-    () => mdmAppleBmAPI.getAppleBMInfo(),
-    {
-      refetchOnWindowFocus: false,
-      retry: (tries, error) => error.status !== 404 && tries <= 3,
-    }
-  );
-
   const navigateToWindowsAutomaticEnrollment = () => {
     router.push(PATHS.ADMIN_INTEGRATIONS_AUTOMATIC_ENROLLMENT_WINDOWS);
   };
@@ -49,25 +27,6 @@ const MdmPlatformsSection = ({ router }: IMdmPlatformsSectionProps) => {
   const navigateToApplePushCertSetup = () => {
     router.push(PATHS.ADMIN_INTEGRATIONS_MDM);
   };
-
-  if (isLoadingMdmAppleBm) {
-    return (
-      <div className={baseClass}>
-        <Spinner />
-      </div>
-    );
-  }
-
-  const showMdmAppleBmError =
-    errorMdmAppleBm &&
-    // API returns a 404 error if ABM is not configured yet
-    errorMdmAppleBm.status !== 404 &&
-    // API returns a 400 error if ABM credentials are invalid
-    errorMdmAppleBm.status !== 400; // TODO: does this still signal expire/invalid credentials? do we need any special error handling? can anything else result in 400?
-
-  if (showMdmAppleBmError) {
-    return <DataError />;
-  }
 
   return (
     <div className={baseClass}>
