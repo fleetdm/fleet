4a159dd0cca3ab43ebd96699b76e6f5c3a9e7f03
Victor Lyuboslavsky <victor.lyuboslavsky@gmail.com>
2024-07-02 11:32:49 -0500

    Hide "Self-service" in Fleet Desktop and My device page (#20047)
    #19651 
    Hide "Self-service" in Fleet Desktop and My device page if there is no
    self-service software available
    
    # Checklist for submitter
    
    <!-- Note that API documentation changes are now addressed by the
    product design team. -->
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://fleetdm.com/docs/contributing/committing-changes#changes-files)
    for more information.
    - [x] Added/updated tests
    - [x] Manual QA for all new/changed functionality
      - For Orbit and Fleet Desktop changes:
    - [x] Manual QA must be performed in the three main OSs, macOS, Windows
    and Linux.
    - [x] Auto-update manual QA, from released version of component to new
    version (see [tools/tuf/test](../tools/tuf/test/README.md)).

--- orbit/cmd/desktop/desktop.go
+++ orbit/cmd/desktop/desktop.go
@@ -134,6 +134,7 @@ func main() {
 
 		selfServiceItem := systray.AddMenuItem("Self-service", "")
 		selfServiceItem.Disable()
+		selfServiceItem.Hide()
 
 		tokenReader := token.Reader{Path: identifierPath}
 		if _, err := tokenReader.Read(); err != nil {
@@ -180,6 +181,7 @@ func main() {
 			myDeviceItem.Disable()
 			transparencyItem.Disable()
 			selfServiceItem.Disable()
+			selfServiceItem.Hide()
 			migrateMDMItem.Disable()
 			migrateMDMItem.Hide()
 		}
@@ -203,7 +205,9 @@ func main() {
 						myDeviceItem.SetTitle("My device")
 						myDeviceItem.Enable()
 						transparencyItem.Enable()
-						selfServiceItem.Enable()
+						// Hide Self-Service for Free tier
+						selfServiceItem.Disable()
+						selfServiceItem.Hide()
 						return
 					}
 
@@ -300,6 +304,15 @@ func main() {
 				default:
 					log.Error().Err(err).Msg("get failing policies")
 					continue
+				}
+
+				// Check for null for backward compatibility with an old Fleet server
+				if sum.SelfService != nil && !*sum.SelfService {
+					selfServiceItem.Disable()
+					selfServiceItem.Hide()
+				} else {
+					selfServiceItem.Enable()
+					selfServiceItem.Show()
 				}
 
 				failingPolicies := 0
