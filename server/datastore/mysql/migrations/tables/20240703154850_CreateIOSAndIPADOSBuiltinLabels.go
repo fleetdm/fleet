258337ca9d776bc7a2c47d8425a12d92b4a0e484
Lucas Rodriguez <lucas@fleetdm.com>
2024-07-04 20:26:04 -0300

    Add migration tests

--- server/datastore/mysql/migrations/tables/20240703154850_CreateIOSAndIPADOSBuiltinLabels.go
+++ server/datastore/mysql/migrations/tables/20240703154850_CreateIOSAndIPADOSBuiltinLabels.go
@@ -15,13 +15,39 @@ func init() {
 }
 
 func Up_20240703154850(tx *sql.Tx) error {
+	// Create new builtin+manual labels for iOS/iPadOS
+	iOSLabelID, iPadOSLabelID, err := createBuiltinManualIOSAndIPadOSLabels(tx)
+	if err != nil {
+		return fmt.Errorf("failed to create iOS/iPadOS labels: %w", err)
+	}
+
+	// Add label membership to existing iOS/iPadOS devices.
+	if _, err := tx.Exec(`
+		INSERT INTO label_membership (host_id, label_id)
+		SELECT id AS host_id, IF(platform = 'ios', ?, ?) AS label_id
+		FROM hosts WHERE platform = 'ios' OR platform = 'ipados';`,
+		iOSLabelID, iPadOSLabelID,
+	); err != nil {
+		return fmt.Errorf("failed to insert label membership: %w", err)
+	}
+
+	// Move existing iOS/iPadOS profiles from "Verifying" to "Verified"
+	// (there's no osquery in these devices).
+	if _, err := tx.Exec(`
+		UPDATE host_mdm_apple_profiles hmap
+		JOIN hosts h ON hmap.host_uuid = h.uuid AND
+		(h.platform = 'ios' OR h.platform = 'ipados') AND hmap.status = 'verifying'
+		SET hmap.status = 'verified';`,
+	); err != nil {
+		return fmt.Errorf("failed to update host_mdm_apple_profiles: %w", err)
+	}
+
+	return nil
+}
+
+func createBuiltinManualIOSAndIPadOSLabels(tx *sql.Tx) (iOSLabelID uint, iPadOSLabelID uint, err error) {
 	// hard-coded timestamps are used so that schema.sql is stable
 	stableTS := time.Date(2024, 6, 28, 0, 0, 0, 0, time.UTC)
-
-	var (
-		iOSLabelID    uint
-		iPadOSLabelID uint
-	)
 	for _, label := range []struct {
 		name        string
 		description string
@@ -64,14 +90,14 @@ func Up_20240703154850(tx *sql.Tx) error {
 					// Thus we return an error and instruct the user how to solve the issue.
 					//
 					// NOTE(lucas): This is using the same approach we used when creating the Sonoma builtin label.
-					return fmt.Errorf(
+					return 0, 0, fmt.Errorf(
 						"label with the name %q already exists, please rename it before applying this migration: %w",
 						label.name,
 						err,
 					)
 				}
 			}
-			return fmt.Errorf("failed to insert label: %w", err)
+			return 0, 0, fmt.Errorf("failed to insert label: %w", err)
 		}
 		labelID, _ := res.LastInsertId()
 		if label.name == fleet.BuiltinLabelIOS {
@@ -80,29 +106,7 @@ func Up_20240703154850(tx *sql.Tx) error {
 			iPadOSLabelID = uint(labelID)
 		}
 	}
-
-	// Add label membership to existing iOS/iPadOS devices.
-	if _, err := tx.Exec(`
-		INSERT INTO label_membership (host_id, label_id)
-		SELECT id AS host_id, IF(platform = 'ios', ?, ?) AS label_id
-		FROM hosts WHERE platform = 'ios' OR platform = 'ipados';`,
-		iOSLabelID, iPadOSLabelID,
-	); err != nil {
-		return fmt.Errorf("failed to insert label membership: %w", err)
-	}
-
-	// Move existing iOS/iPadOS profiles from "Verifying" to "Verified"
-	// (there's no osquery in these devices).
-	if _, err := tx.Exec(`
-		UPDATE host_mdm_apple_profiles hmap
-		JOIN hosts h ON hmap.host_uuid = h.uuid AND
-		(h.platform = 'ios' OR h.platform = 'ipados') AND hmap.status = 'verifying'
-		SET hmap.status = 'verified';`,
-	); err != nil {
-		return fmt.Errorf("failed to update host_mdm_apple_profiles: %w", err)
-	}
-
-	return nil
+	return iOSLabelID, iPadOSLabelID, nil
 }
 
 func Down_20240703154850(tx *sql.Tx) error {
