59adb999ddbfc0e002218191d8e28f23a0ea0147
dantecatalfamo <dante.catalfamo@gmail.com>
2024-07-04 11:36:47 -0400

    Disable signing in with other microsoft accounts

--- scripts/mdm/windows/windows-lock.ps1
+++ scripts/mdm/windows/windows-lock.ps1
@@ -32,10 +32,14 @@ Get-LocalUser | Where-Object { $_.Enabled -eq $true -and $_.Name -ne "Administra
     Write-Host "Disabled account for $username"
 }
 
+# Disable additional AD logins
+New-ItemProperty -Path "HKLM:\Software\Microsoft\PolicyManager\default\Settings\AllowSignInOptions" -Name 'value' -Value 3 -PropertyType DWORD -Force
+
 # Disable cached logins for AD/Azure/Entra accounts
 New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\" -Name 'CachedLogonsCount' -Value 0 -PropertyType String -Force
 
 Write-Host "All local non-administrative users have been logged out and their accounts disabled."
+Write-Host "Logging in with other Microsoft accounts has been disabled"
 Write-Host "Cached Logins have been disabled, disable the Active Directory account to prevent further logins"
 
 # Shutdown computer in 10 seconds, after command has returned to fleet
