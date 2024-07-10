070277ffc17916deeb6fe99ba039e968791f026f
Victor Lyuboslavsky <victor.lyuboslavsky@gmail.com>
2024-07-10 08:49:05 -0500

    Distributed lock and store calendar_events UUID as binary in MySQL (#20277)
    #19352
    
    Fix for code review comment:
    https://github.com/fleetdm/fleet/pull/20156#discussion_r1668421504
    
    Also includes changes from https://github.com/fleetdm/fleet/pull/20252
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    <!-- Note that API documentation changes are now addressed by the
    product design team. -->
    
    - [x] Added/updated tests
    - [x] If database migrations are included, checked table schema to
    confirm autoupdate
    - For database migrations:
    - [x] Checked schema for all modified table for columns that will
    auto-update timestamps during migration.
    - [x] Confirmed that updating the timestamps is acceptable, and will not
    cause unwanted side effects.
    - [x] Ensured the correct collation is explicitly set for character
    columns (`COLLATE utf8mb4_unicode_ci`).
    - [x] Manual QA for all new/changed functionality
    
    (cherry picked from commit 7bcd61a8bddd5685a59d66e47a2aabc5ba2b4090)

--- server/fleet/calendar.go
+++ server/fleet/calendar.go
@@ -42,6 +42,25 @@ type UserCalendar interface {
 	Get(event *CalendarEvent, key string) (interface{}, error)
 }
 
+// Lock interface for managing distributed locks.
+type Lock interface {
+	// AcquireLock attempts to acquire a lock with the given key. value is the value to set for the key, which is used to release the lock.
+	// expireMs is the time in milliseconds after which the lock is automatically released. expireMs=0 means a default expiration time is used.
+	// Returns true if the lock was acquired, false otherwise.
+	AcquireLock(ctx context.Context, key string, value string, expireMs uint64) (ok bool, err error)
+	// ReleaseLock attempts to release a lock with the given key and value. If key does not exist or value does not match, the lock is not released.
+	// Returns true if the lock was released, false otherwise.
+	ReleaseLock(ctx context.Context, key string, value string) (ok bool, err error)
+	// Get retrieves the value of the given key. If the key does not exist, nil is returned.
+	Get(ctx context.Context, key string) (*string, error)
+	// AddToSet adds the value to the set identified by the given key.
+	AddToSet(ctx context.Context, key string, value string) error
+	// RemoveFromSet removes the value from the set identified by the given key.
+	RemoveFromSet(ctx context.Context, key string, value string) error
+	// GetSet retrieves a slice of string values from the set identified by the given key.
+	GetSet(ctx context.Context, key string) ([]string, error)
+}
+
 type CalendarWebhookPayload struct {
 	Timestamp        time.Time            `json:"timestamp"`
 	HostID           uint                 `json:"host_id"`
