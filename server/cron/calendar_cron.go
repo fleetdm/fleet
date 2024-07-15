72d52f17412e5e800f3ccf7e240857b1d156fcb5
Victor Lyuboslavsky <victor@fleetdm.com>
2024-07-15 07:30:12 -0500

    Fix potential race condition.

--- server/cron/calendar_cron.go
+++ server/cron/calendar_cron.go
@@ -385,13 +385,6 @@ func processFailingHostExistingCalendarEvent(
 		}
 	}()
 
-	// Remove event from the queue so that we don't process this event again.
-	// Note: This item can be added back to the queue while we are processing it.
-	err = distributedLock.RemoveFromSet(ctx, calendar.QueueKey, eventUUID)
-	if err != nil {
-		return fmt.Errorf("remove calendar event from queue: %w", err)
-	}
-
 	updatedEvent := calendarEvent
 	updated := false
 	now := time.Now()
@@ -421,6 +414,12 @@ func processFailingHostExistingCalendarEvent(
 			updatedEvent.TimeZone,
 		); err != nil {
 			return fmt.Errorf("updating event calendar on db: %w", err)
+		}
+
+		// Remove event from the queue so that we don't process this event again.
+		err = distributedLock.RemoveFromSet(ctx, calendar.QueueKey, eventUUID)
+		if err != nil {
+			return fmt.Errorf("remove calendar event from queue: %w", err)
 		}
 	}
 
