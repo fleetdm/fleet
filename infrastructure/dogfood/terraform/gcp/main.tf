6b868b83dbbfa6494a575b3202c440274f26beac
Benjamin Edwards <edwards.benw@gmail.com>
2024-07-18 14:57:45 -0400

    📝 (fleet-server-configuration.md): Update environment variable name for better clarity 🔧 (terraform/gcp): Remove unused artifact_registry.tf file ✨ (terraform/gcp): Add new resources for secret management and environment variables in cloud_run.tf ⬆️ (terraform/gcp): Upgrade google provider version in main.tf ✨ (terraform/gcp): Add new storage.tf file for managing Google Cloud Storage resources 🔧 (terraform/gcp): Update default image value and add new variables in variables.tf ✨ (terraform/gcp): Add new module for cloud router in vpc.tf

--- infrastructure/dogfood/terraform/gcp/main.tf
+++ infrastructure/dogfood/terraform/gcp/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.9.0"
+      version = "4.51.0"
     }
     google-beta = {
       source = "hashicorp/google-beta"
