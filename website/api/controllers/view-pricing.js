67b2d2f7dd22363ba092efb3f572c53b72bd4a6e
Eric <eashaw@sailsjs.com>
2024-07-23 17:22:29 -0500

    reorder categories and features on pricing page

--- website/api/controllers/view-pricing.js
+++ website/api/controllers/view-pricing.js
@@ -29,7 +29,7 @@ module.exports = {
 
     let pricingTable = [];
 
-    let pricingTableCategories = ['Support', 'Deployment', 'Integrations','Device management', 'Endpoint operations', 'Vulnerability management'];
+    let pricingTableCategories = ['Device management','Support', 'Deployment', 'Integrations','Endpoint operations','Vulnerability management'];
     for(let category of pricingTableCategories) {
       // Get all the features in that have a pricingTableFeatures array that contains this category.
       let featuresInThisCategory = _.filter(pricingTableFeatures, (feature)=>{
@@ -38,9 +38,7 @@ module.exports = {
       // Build a dictionary containing the category name, and all features in the category, sorting premium features to the bottom of the list.
       let allFeaturesInThisCategory = {
         categoryName: category,
-        features: _.sortBy(featuresInThisCategory, (feature)=>{
-          return feature.tier !== 'Free';
-        })
+        features: featuresInThisCategory,
       };
       // Add the dictionaries to the arrays that we'll use to build the features table.
       pricingTable.push(allFeaturesInThisCategory);
