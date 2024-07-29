881682bb21ce468cbb44990f327fb473a5b01ad4
dantecatalfamo <dante.catalfamo@gmail.com>
2024-07-29 13:46:27 -0400

    Remove bad indentation

--- server/datastore/mysql/migrations/tables/20240729120947_AddSoftwareInstallResultDeletedAt.go
+++ server/datastore/mysql/migrations/tables/20240729120947_AddSoftwareInstallResultDeletedAt.go
@@ -15,14 +15,17 @@ func Up_20240729120947(tx *sql.Tx) error {
 		return fmt.Errorf("failed to create host_deleted_at column on host_software_installs table: %w", err)
 	}
 	_, err = tx.Exec(`
-UPDATE host_software_installs i
-        LEFT JOIN
-    hosts h ON i.host_id = h.id
+UPDATE
+    host_software_installs i
+LEFT JOIN
+    hosts h
+    ON i.host_id = h.id
 SET
     i.host_deleted_at = NOW()
 WHERE
     i.host_deleted_at IS NULL
-        AND h.id IS NULL
+AND
+    h.id IS NULL
 `)
 	if err != nil {
 		return fmt.Errorf("failed to update host_software_installs.host_deleted_at for hosts that no longer exist")
