53ba4765166fe75c9df1d85a95bd6c65b56fbd38
Sarah Gillespie <73313222+gillespi314@users.noreply.github.com>
2024-07-29 15:46:27 -0500

    Fix unreleased bug: API error message for duplicate VPP apps (#20808)

--- server/datastore/mysql/vpp.go
+++ server/datastore/mysql/vpp.go
@@ -288,6 +288,13 @@ VALUES
 	}
 
 	_, err := tx.ExecContext(ctx, stmt, adamID, globalOrTmID, teamID)
+	if IsDuplicate(err) {
+		err = &existsError{
+			Identifier:   adamID,
+			TeamID:       teamID,
+			ResourceType: "VPPAppAdamID",
+		}
+	}
 
 	return ctxerr.Wrap(ctx, err, "writing vpp app team mapping to db")
 }
