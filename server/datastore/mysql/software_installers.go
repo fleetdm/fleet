bf3d849bd01434c090b2bc9ed973ffb2c45ff36b
Dante Catalfamo <43040593+dantecatalfamo@users.noreply.github.com>
2024-08-01 15:41:37 -0400

    Fix Deleted Host Software Installs Details (#20820)
    #20271
    
    This fix requires an accompanying front end fix to pass the
    `display_name` from the activity feed to the modal when displaying the
    install results.

--- server/datastore/mysql/software_installers.go
+++ server/datastore/mysql/software_installers.go
@@ -337,19 +337,17 @@ SELECT
 	hsi.post_install_script_output,
 	hsi.install_script_output,
 	hsi.host_id AS host_id,
-	h.computer_name AS host_display_name,
 	st.name AS software_title,
 	st.id AS software_title_id,
 	COALESCE(%s, '') AS status,
 	si.filename AS software_package,
-	h.team_id AS host_team_id,
 	hsi.user_id AS user_id,
 	hsi.post_install_script_exit_code,
 	hsi.install_script_exit_code,
-    hsi.self_service
+    hsi.self_service,
+    hsi.host_deleted_at
 FROM
 	host_software_installs hsi
-	JOIN hosts h ON h.id = hsi.host_id
 	JOIN software_installers si ON si.id = hsi.software_installer_id
 	JOIN software_titles st ON si.title_id = st.id
 WHERE
@@ -400,6 +398,7 @@ WHERE
 			FROM host_software_installs
 		WHERE
 			software_installer_id = :installer_id
+			AND host_deleted_at IS NULL
 		GROUP BY
 			host_id)) s`, softwareInstallerHostStatusNamedQuery("hsi", "status"))
 
