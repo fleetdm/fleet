e2fcb44d76473a8d97dc5c316af12d399ed71fd9
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2024-08-01 15:53:43 -0400

    Fleet UI: multiple unreleased and released bugs (#20951)

--- frontend/pages/SoftwarePage/SoftwareTitles/SoftwareTable/SoftwareTitlesTableConfig.tsx
+++ frontend/pages/SoftwarePage/SoftwareTitles/SoftwareTable/SoftwareTitlesTableConfig.tsx
@@ -76,11 +76,13 @@ const getSoftwareNameCellData = (
     iconUrl = app_store_app.icon_url;
   }
 
+  const isAllTeams = teamId === undefined;
+
   return {
     name: softwareTitle.name,
     source: softwareTitle.source,
     path: softwareTitleDetailsPath,
-    hasPackage: hasPackage && !!teamId,
+    hasPackage: hasPackage && !isAllTeams,
     isSelfService,
     iconUrl,
   };
