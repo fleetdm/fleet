4cb0d9ac5a3f83d2020c0a43ffeff016f25c0bb9
jacobshandling <61553566+jacobshandling@users.noreply.github.com>
2024-08-05 13:19:55 -0700

    to RC: UI – Render "vulns not supported" empty state for iphone/ipad host software filtered by vuln (#21061)
    #### This PR already merged to `main`, see
    https://github.com/fleetdm/fleet/pull/21029. This is against the release
    branch so it can be included in 4.55.0
    
    Co-authored-by: Jacob Shandling <jacob@fleetdm.com>

--- frontend/pages/hosts/details/cards/Software/HostSoftware.tsx
+++ frontend/pages/hosts/details/cards/Software/HostSoftware.tsx
@@ -12,6 +12,7 @@ import deviceAPI, {
   IGetDeviceSoftwareResponse,
 } from "services/entities/device_user";
 import { IHostSoftware, ISoftware } from "interfaces/software";
+import { Platform } from "interfaces/platform";
 import { DEFAULT_USE_QUERY_OPTIONS } from "utilities/constants";
 import { NotificationContext } from "context/notification";
 import { AppContext } from "context/app";
@@ -34,6 +35,8 @@ export interface ITableSoftware extends Omit<ISoftware, "vulnerabilities"> {
 interface IHostSoftwareProps {
   /** This is the host id or the device token */
   id: number | string;
+  /** The host's platform. Only used for the host details page, so can be omited on the Device User Page. */
+  platform?: Platform;
   softwareUpdatedAt?: string;
   hostCanInstallSoftware: boolean;
   router: InjectedRouter;
@@ -82,6 +85,7 @@ export const parseHostSoftwareQueryParams = (queryParams: {
 
 const HostSoftware = ({
   id,
+  platform,
   softwareUpdatedAt,
   hostCanInstallSoftware,
   router,
@@ -93,6 +97,8 @@ const HostSoftware = ({
   isMyDevicePage = false,
 }: IHostSoftwareProps) => {
   const { renderFlash } = useContext(NotificationContext);
+  const vulnFilterAndNotSupported =
+    ["ios", "ipados"].includes(platform ?? "") && queryParams.vulnerable;
   const {
     isGlobalAdmin,
     isGlobalMaintainer,
@@ -129,7 +135,8 @@ const HostSoftware = ({
     },
     {
       ...DEFAULT_USE_QUERY_OPTIONS,
-      enabled: isSoftwareEnabled && !isMyDevicePage, // if disabled, we'll always show a generic "No software detected" message
+      enabled:
+        isSoftwareEnabled && !isMyDevicePage && !vulnFilterAndNotSupported,
       keepPreviousData: true,
       staleTime: 7000,
     }
@@ -158,7 +165,7 @@ const HostSoftware = ({
     ({ queryKey }) => deviceAPI.getDeviceSoftware(queryKey[0]),
     {
       ...DEFAULT_USE_QUERY_OPTIONS,
-      enabled: isSoftwareEnabled && isMyDevicePage, // if disabled, we'll always show a generic "No software detected" message
+      enabled: isSoftwareEnabled && isMyDevicePage, // if disabled, we'll always show a generic "No software detected" message. No DUP for iPad/iPhone
       keepPreviousData: true,
       staleTime: 7000,
     }
@@ -251,7 +258,10 @@ const HostSoftware = ({
     if (isLoading) {
       return <Spinner />;
     }
-
+    // will never be the case - to handle `platform` typing discrepancy with DeviceUserPage
+    if (!platform) {
+      return null;
+    }
     return (
       <>
         {isError && <DataError />}
@@ -260,7 +270,20 @@ const HostSoftware = ({
             isLoading={
               isMyDevicePage ? deviceSoftwareFetching : hostSoftwareFetching
             }
-            data={data}
+            // this could be cleaner, however, we are going to revert this commit anyway once vulns are
+            // supported for iPad/iPhone, by the end of next sprint
+            data={
+              vulnFilterAndNotSupported
+                ? ({
+                    count: 0,
+                    meta: {
+                      has_next_results: false,
+                      has_previous_results: false,
+                    },
+                  } as IGetHostSoftwareResponse)
+                : data
+            } // eshould be mpty for iPad/iPhone since API call is disabled, but to be sure to trigger empty state
+            platform={platform}
             router={router}
             tableConfig={tableConfig}
             sortHeader={queryParams.order_key}
