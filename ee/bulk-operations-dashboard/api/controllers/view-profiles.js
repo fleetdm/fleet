7e14df48a0566db7c447dda84533f618511c790f
Eric <eashaw@sailsjs.com>
2024-08-05 17:39:10 -0500

    morre stubbing

--- ee/bulk-operations-dashboard/api/controllers/view-profiles.js
+++ ee/bulk-operations-dashboard/api/controllers/view-profiles.js
@@ -18,8 +18,78 @@ module.exports = {
 
   fn: async function () {
 
+
+    let teamsResponseData = await sails.helpers.http.get.with({
+      url: '/api/v1/fleet/teams',
+      baseUrl: sails.config.custom.fleetBaseUrl,
+      headers: {
+        Authorization: `Bearer ${sails.config.custom.fleetApiToken}`
+      }
+    })
+    .timeout(120000)
+    .retry(['requestFailed', {name: 'TimeoutError'}]);
+
+    let allTeams = teamsResponseData.teams;
+
+    let teamApids = _.pluck(allTeams, 'id');
+
+    // console.log(teamApids);
+    let allProfiles = [];
+
+    for(let teamApid of teamApids){
+      let configurationProfilesResponseData = await sails.helpers.http.get.with({
+        url: `/api/v1/fleet/configuration_profiles?team_id=${teamApid}`,
+        baseUrl: sails.config.custom.fleetBaseUrl,
+        headers: {
+          Authorization: `Bearer ${sails.config.custom.fleetApiToken}`
+        }
+      })
+      .timeout(120000)
+      .retry(['requestFailed', {name: 'TimeoutError'}]);
+      let profilesForThisTeam = configurationProfilesResponseData.profiles;
+      allProfiles = allProfiles.concat(profilesForThisTeam);
+    }
+
+    // Grab all of the configuration profiles on the Fleet instance.
+    let noTeamConfigurationProfilesResponseData = await sails.helpers.http.get.with({
+      url: '/api/v1/fleet/configuration_profiles',
+      baseUrl: sails.config.custom.fleetBaseUrl,
+      headers: {
+        Authorization: `Bearer ${sails.config.custom.fleetApiToken}`
+      }
+    })
+    .timeout(120000)
+    .retry(['requestFailed', {name: 'TimeoutError'}]);
+    let profilesForThisTeam = noTeamConfigurationProfilesResponseData.profiles;
+    allProfiles = allProfiles.concat(profilesForThisTeam);
+
+    // console.log(allProfiles);
+
+    let profileConfiguration = [];
+
+    let allProfilesByName = _.groupBy(allProfiles, 'name');
+    console.log(allProfilesByName)
+
+    for(let profileUuid in allProfilesByName) {
+      let teamsForThisProfile = _.pluck(allProfilesByName[profileUuid], 'team_id');
+      console.log(teamsForThisProfile);
+      let profile = allProfilesByName[profileUuid][0];// Grab the first profile returned in the api repsonse to build our profile configuration.
+      let profileInformation = {
+        name: profile.name,
+        platform: profile.platform,
+        createdAt: new Date(profile.created_at).getTime(),
+        uuid: profileUuid,
+        teams: teamsForThisProfile,
+      }
+      profileConfiguration.push(profileInformation)
+    }
+
+    // console.log(profileConfiguration);
+
+
+
     // Respond with view.
-    return {};
+    return {profileConfiguration};
 
   }
 
--- ee/bulk-operations-dashboard/config/policies.js
+++ ee/bulk-operations-dashboard/config/policies.js
@@ -10,16 +10,13 @@
 
 module.exports.policies = {
 
-  '*': 'is-logged-in',
+  // '*': 'is-logged-in',
+  '*': true,
 
   // Bypass the `is-logged-in` policy for:
   'entrance/*': true,
   'account/logout': true,
-  'view-homepage-or-redirect': true,
-  'view-faq': true,
-  'view-contact': true,
   'legal/view-terms': true,
   'legal/view-privacy': true,
-  'deliver-contact-form-message': true,
 
 };
