d9ec57af33d4ea183934fabe6bae1eb7f19d569e
dantecatalfamo <dante.catalfamo@gmail.com>
2024-08-06 11:13:40 -0400

    Add test to ensure empty gitops AdamIDs return an error to the user

--- cmd/fleetctl/gitops_test.go
+++ cmd/fleetctl/gitops_test.go
@@ -1310,6 +1310,7 @@ func TestTeamVPPAppsGitOps(t *testing.T) {
 		{"testdata/gitops/team_vpp_valid_empty.yml", "", time.Now().Add(-24 * time.Hour)},
 		{"testdata/gitops/team_vpp_valid_app.yml", "VPP token expired", time.Now().Add(-24 * time.Hour)},
 		{"testdata/gitops/team_vpp_invalid_app.yml", "app not available on vpp account", time.Now().Add(24 * time.Hour)},
+		{"testdata/gitops/team_vpp_empty_adamid.yml", "software app store id required", time.Now().Add(24 * time.Hour)},
 	}
 
 	for _, c := range cases {
