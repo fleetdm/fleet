4186784602b32a837d337d96cb29de4ed0969ca1
Sharon Katz <sharon@fleetdm.com>
2024-08-08 11:44:39 -0400

    fix test

--- server/datastore/mysql/statistics_test.go
+++ server/datastore/mysql/statistics_test.go
@@ -87,9 +87,9 @@ func testStatisticsShouldSend(t *testing.T, ds *Datastore) {
 	assert.Equal(t, false, stats.HostExpiryEnabled)
 	assert.Equal(t, false, stats.MDMWindowsEnabled)
 	assert.Equal(t, false, stats.LiveQueryDisabled)
-	assert.Equal(t, true, stats.AIFeaturesDisabled)
-	assert.Equal(t, true, stats.MaintenanceWindowsEnabled)
-	assert.Equal(t, true, stats.MaintenanceWindowsConfigured)
+	assert.Equal(t, false, stats.AIFeaturesDisabled)
+	assert.Equal(t, false, stats.MaintenanceWindowsEnabled)
+	assert.Equal(t, false, stats.MaintenanceWindowsConfigured)
 	assert.Equal(t, 0, stats.NumHostsFleetDesktopEnabled)
 
 	firstIdentifier := stats.AnonymousIdentifier
@@ -231,10 +231,10 @@ func testStatisticsShouldSend(t *testing.T, ds *Datastore) {
 	assert.Equal(t, `[{"count":10,"loc":["a","b","c"]}]`, string(stats.StoredErrors))
 	assert.Equal(t, []fleet.HostsCountByOsqueryVersion{{OsqueryVersion: "4.9.0", NumHosts: 1}}, stats.HostsEnrolledByOsqueryVersion)
 	assert.Equal(t, []fleet.HostsCountByOrbitVersion{{OrbitVersion: "1.1.0", NumHosts: 1}}, stats.HostsEnrolledByOrbitVersion)
-	assert.Equal(t, true, stats.AIFeaturesDisabled)
-	assert.Equal(t, true, stats.MaintenanceWindowsEnabled)
-	assert.Equal(t, true, stats.MaintenanceWindowsConfigured)
-	assert.Equal(t, 0, stats.NumHostsFleetDesktopEnabled)
+	assert.Equal(t, false, stats.AIFeaturesDisabled)
+	assert.Equal(t, false, stats.MaintenanceWindowsEnabled)
+	assert.Equal(t, false, stats.MaintenanceWindowsConfigured)
+	assert.Equal(t, 1, stats.NumHostsFleetDesktopEnabled)
 
 	err = ds.RecordStatisticsSent(ctx)
 	require.NoError(t, err)
@@ -340,10 +340,10 @@ func testStatisticsShouldSend(t *testing.T, ds *Datastore) {
 		{Version: "", NumEnrolled: 1},
 	}, stats.HostsEnrolledByOperatingSystem[""])
 	assert.Equal(t, `[{"count":10,"loc":["a","b","c"]}]`, string(stats.StoredErrors))
-	assert.Equal(t, true, stats.AIFeaturesDisabled)
-	assert.Equal(t, true, stats.MaintenanceWindowsEnabled)
-	assert.Equal(t, true, stats.MaintenanceWindowsConfigured)
-	assert.Equal(t, 0, stats.NumHostsFleetDesktopEnabled)
+	assert.Equal(t, false, stats.AIFeaturesDisabled)
+	assert.Equal(t, false, stats.MaintenanceWindowsEnabled)
+	assert.Equal(t, false, stats.MaintenanceWindowsConfigured)
+	assert.Equal(t, 1, stats.NumHostsFleetDesktopEnabled)
 
 	// Create multiple new sessions for a single user
 	_, err = ds.NewSession(ctx, u1.ID, "session_key2")
@@ -381,7 +381,7 @@ func testStatisticsShouldSend(t *testing.T, ds *Datastore) {
 	assert.Equal(t, false, stats.AIFeaturesDisabled)
 	assert.Equal(t, false, stats.MaintenanceWindowsEnabled)
 	assert.Equal(t, false, stats.MaintenanceWindowsConfigured)
-	assert.Equal(t, 0, stats.NumHostsFleetDesktopEnabled)
+	assert.Equal(t, 1, stats.NumHostsFleetDesktopEnabled)
 
 	// Add host to test hosts not responding stats
 	_, err = ds.NewHost(ctx, &fleet.Host{
