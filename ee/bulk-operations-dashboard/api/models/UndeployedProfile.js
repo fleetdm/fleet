0ce1cd6bf388cdbf9028d4353c623b38ab4e6c84
Eric <eashaw@sailsjs.com>
2024-08-08 17:04:19 -0500

    rename profile model

--- ee/bulk-operations-dashboard/api/models/UndeployedProfile.js
+++ ee/bulk-operations-dashboard/api/models/UndeployedProfile.js
@@ -1,5 +1,5 @@
 /**
- * Profile.js
+ * UndeployedProfile.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -18,33 +18,9 @@ module.exports = {
       description: 'The name of the profile on the Fleet instance',
     },
 
-    uuid: {
-      type: 'string',
-      required: true,
-      description: 'The uuid of the profile on the Fleet instance',
-    },
-
-    uploadedAt: {
-      type: 'string',
-      required: true,
-      description: 'A JS timestamp of when this profile was uploaded.'
-    },
-
-    teamApid: {
-      type: 'number',
-      description: 'The Fleet API ID of the team this profile is on.',
-      extendedDescription: 'Set to 0 if this script is on the "no team" team or undefined if the profile is not on a team'
-    },
-
-    teamDisplayName: {
-      type: 'string',
-      description: 'The name of the team this profile is on.'
-    },
-
     platform: {
       type: 'string',
       description: 'The type of operating system this profile is for.',
-
     },
 
     profileType: {// ∆: Do we need this attribute?
