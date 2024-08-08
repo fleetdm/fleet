08489fcc68411c66d7a0a7adb2cffbb02b4f3f87
Sharon Katz <sharon@fleetdm.com>
2024-08-08 10:55:19 -0400

    code from Lucas

--- server/datastore/mysql/statistics.go
+++ server/datastore/mysql/statistics.go
@@ -10,6 +10,7 @@ import (
 	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	"github.com/fleetdm/fleet/v4/server/contexts/license"
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/ptr"
 	"github.com/fleetdm/fleet/v4/server/version"
 	"github.com/go-kit/log/level"
 	"github.com/jmoiron/sqlx"
@@ -135,8 +136,21 @@ func (ds *Datastore) ShouldSendStatistics(ctx context.Context, frequency time.Du
 			stats.Organization = lic.Organization
 		}
 		stats.AIFeaturesDisabled = appConfig.ServerSettings.AIFeaturesDisabled
-		stats.MaintenanceWindowsEnabled = true // stats.MaintenanceWindowsEnabled = appConfig.Integrations.google_calendar.enable_calendar_events
-		stats.MaintenanceWindowsEnabled = true // stats.MaintenanceWindowsConfigured = appConfig.Integrations.google_calendar. ??
+		stats.MaintenanceWindowsConfigured = len(appConfig.Integrations.GoogleCalendar) > 0 && appConfig.Integrations.GoogleCalendar[0].Domain != "" && len(appConfig.Integrations.GoogleCalendar[0].ApiKey) > 0
+
+		stats.MaintenanceWindowsEnabled = false
+		teams, err := ds.ListTeams(ctx, fleet.TeamFilter{User: &fleet.User{
+			GlobalRole: ptr.String(fleet.RoleAdmin),
+		}}, fleet.ListOptions{})
+		if err != nil {
+			return ctxerr.Wrap(ctx, err, "list teams")
+		}
+		for _, team := range teams {
+			if team.Config.Integrations.GoogleCalendar != nil && team.Config.Integrations.GoogleCalendar.Enable {
+				stats.MaintenanceWindowsEnabled = true
+				break
+			}
+		}
 		stats.NumHostsFleetDesktopEnabled = numHostsFleetDesktopEnabled
 		return nil
 	}
