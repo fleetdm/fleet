37b010884b65e632802bd2884eca6a3887297abd
Martin Angers <martin.n.angers@gmail.com>
2024-08-20 10:14:34 -0400

    Add edge-case tests for abm migration

--- server/datastore/mysql/migrations/tables/20240813103800_SupportMultipleABMTokens_test.go
+++ server/datastore/mysql/migrations/tables/20240813103800_SupportMultipleABMTokens_test.go
@@ -174,4 +174,99 @@ LIMIT 1`)
 		require.NotNil(t, hostTokenID)
 		require.EqualValues(t, storedToken.ID, *hostTokenID)
 	})
+
+	t.Run("ExistingTokenNoMDMConfig", func(t *testing.T) {
+		db := applyUpToPrev(t)
+
+		// create an existing ABM token
+		existingToken, md5Checksum := createTokenAndHash()
+		execNoErr(t, db, `INSERT INTO mdm_config_assets (name, value, md5_checksum) VALUES ('abm_token', ?, ?)`, existingToken, md5Checksum)
+
+		// app config does not have the MDM object
+		execNoErr(t, db, `UPDATE app_config_json SET json_value = JSON_REMOVE(json_value, '$.mdm')`)
+
+		// create a host with a DEP assignment
+		hostID := insertHost(t, db, nil)
+		execNoErr(t, db, `INSERT INTO host_dep_assignments (host_id) VALUES (?)`, hostID)
+
+		// Apply current migration.
+		applyNext(t, db)
+
+		// ABM token is now soft-deleted in the old storage
+		var assetDeletedUUID string
+		err := db.Get(&assetDeletedUUID, `SELECT deletion_uuid FROM mdm_config_assets WHERE name = 'abm_token'`)
+		require.NoError(t, err)
+		require.NotEmpty(t, assetDeletedUUID)
+
+		// ABM token is stored in the new storage, with the default config
+		var storedToken abmToken
+		err = db.Get(&storedToken, `
+SELECT
+	id, organization_name, apple_id, terms_expired, renew_at, token, macos_default_team_id, ios_default_team_id, ipados_default_team_id
+FROM
+	abm_tokens
+LIMIT 1`)
+		require.NoError(t, err)
+
+		// we don't have those fields during DB migration
+		require.Empty(t, storedToken.OrganizationName)
+		require.Empty(t, storedToken.AppleID)
+		require.Equal(t, time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), storedToken.RenewAt)
+		require.False(t, storedToken.TermsExpired)
+		// token matches
+		require.Equal(t, existingToken, string(storedToken.Token))
+		// all platform default teams are set to nil
+		require.Nil(t, storedToken.MacOSDefaultTeamID)
+		require.Nil(t, storedToken.IOSDefaultTeamID)
+		require.Nil(t, storedToken.IPadOSDefaultTeamID)
+
+		// the existing host DEP assignment is linked to the token
+		var hostTokenID *uint
+		err = db.Get(&hostTokenID, `SELECT abm_token_id FROM host_dep_assignments WHERE host_id = ?`, hostID)
+		require.NoError(t, err)
+		require.NotNil(t, hostTokenID)
+		require.EqualValues(t, storedToken.ID, *hostTokenID)
+	})
+
+	t.Run("ExistingTokenCorruptedJSONConfig", func(t *testing.T) {
+		db := applyUpToPrev(t)
+
+		// create an existing ABM token
+		existingToken, md5Checksum := createTokenAndHash()
+		execNoErr(t, db, `INSERT INTO mdm_config_assets (name, value, md5_checksum) VALUES ('abm_token', ?, ?)`, existingToken, md5Checksum)
+
+		// set a corrupted JSON config for ABM
+		execNoErr(t, db, `UPDATE app_config_json SET json_value = JSON_SET(json_value, '$.mdm', JSON_OBJECT('apple_bm_default_team', 123, 'apple_bm_terms_expired', 'abc'))`)
+
+		// Apply current migration.
+		applyNext(t, db)
+
+		// ABM token is now soft-deleted in the old storage
+		var assetDeletedUUID string
+		err := db.Get(&assetDeletedUUID, `SELECT deletion_uuid FROM mdm_config_assets WHERE name = 'abm_token'`)
+		require.NoError(t, err)
+		require.NotEmpty(t, assetDeletedUUID)
+
+		// ABM token is stored in the new storage, with the default config (as the existing one was invalid)
+		var storedToken abmToken
+		err = db.Get(&storedToken, `
+SELECT
+	id, organization_name, apple_id, terms_expired, renew_at, token, macos_default_team_id, ios_default_team_id, ipados_default_team_id
+FROM
+	abm_tokens
+LIMIT 1`)
+		require.NoError(t, err)
+
+		// we don't have those fields during DB migration
+		require.Empty(t, storedToken.OrganizationName)
+		require.Empty(t, storedToken.AppleID)
+		require.Equal(t, time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC), storedToken.RenewAt)
+		require.False(t, storedToken.TermsExpired)
+		// token matches
+		require.Equal(t, existingToken, string(storedToken.Token))
+		// all platform default teams are set to nil
+		require.Nil(t, storedToken.MacOSDefaultTeamID)
+		require.Nil(t, storedToken.IOSDefaultTeamID)
+		require.Nil(t, storedToken.IPadOSDefaultTeamID)
+	})
 }
