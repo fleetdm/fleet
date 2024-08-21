064cad15de00de1b6ae6a6415b115e932cf1c10c
Gabriel Hernandez <ghernandez345@gmail.com>
2024-08-21 12:05:52 +0100

    first pass add new vpp page

---
+++ frontend/pages/admin/IntegrationsPage/cards/MdmSettings/VppPage/components/DeleteVppModal/DeleteVppModal.tsx
@@ -0,0 +1,85 @@
+import React, { useCallback, useContext, useState } from "react";
+
+import mdmAbmAPI from "services/entities/mdm_apple_bm";
+import { NotificationContext } from "context/notification";
+
+import Button from "components/buttons/Button";
+import Modal from "components/Modal";
+
+const baseClass = "delete-vpp-modal";
+
+interface IDeleteVppModalProps {
+  tokenOrgName: string;
+  tokenId: number;
+  onCancel: () => void;
+  onDeletedToken: () => void;
+}
+
+const DeleteVppModal = ({
+  tokenOrgName,
+  tokenId,
+  onCancel,
+  onDeletedToken,
+}: IDeleteVppModalProps) => {
+  const { renderFlash } = useContext(NotificationContext);
+
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const onDeleteToken = useCallback(async () => {
+    setIsDeleting(true);
+
+    try {
+      await mdmAbmAPI.deleteToken(tokenId);
+      renderFlash("success", "Deleted successfully.");
+      onDeletedToken();
+    } catch (e) {
+      // TODO: Check API sends back correct error messages
+      renderFlash(
+        "error",
+        "Couldn’t disable automatic enrollment. Please try again."
+      );
+      onCancel();
+    }
+  }, [onCancel, onDeletedToken, renderFlash, tokenId]);
+
+  return (
+    <Modal
+      title="Delete ABM"
+      className={baseClass}
+      onExit={onCancel}
+      isContentDisabled={isDeleting}
+    >
+      <>
+        <p>
+          New hosts purchased in the <b>{tokenOrgName}</b> won&apos;t
+          automatically enroll to Fleet.{" "}
+        </p>
+        <p>
+          If you want to re-enable automatic enrollment, you&apos;ll have to
+          upload a new ABM token.
+        </p>
+
+        <div className="modal-cta-wrap">
+          <Button
+            type="button"
+            variant="alert"
+            onClick={onDeleteToken}
+            disabled={isDeleting}
+            isLoading={isDeleting}
+          >
+            Delete
+          </Button>
+          <Button
+            onClick={onCancel}
+            disabled={isDeleting}
+            variant="inverse-alert"
+          >
+            Cancel
+          </Button>
+        </div>
+      </>
+    </Modal>
+  );
+};
+
+export default DeleteVppModal;
---
+++ frontend/pages/admin/IntegrationsPage/cards/MdmSettings/VppPage/components/RenewVppModal/RenewVppModal.tsx
@@ -0,0 +1,138 @@
+import React, { useState, useContext, useCallback } from "react";
+
+import { NotificationContext } from "context/notification";
+import { getErrorReason } from "interfaces/errors";
+import mdmAppleBmAPI from "services/entities/mdm_apple_bm";
+
+import Button from "components/buttons/Button";
+import CustomLink from "components/CustomLink";
+import {
+  FileUploader,
+  FileDetails,
+} from "components/FileUploader/FileUploader";
+import Modal from "components/Modal";
+
+const baseClass = "modal renew-vpp-modal";
+
+interface IRenewVppModalProps {
+  tokenId: number;
+  onCancel: () => void;
+  onRenewedToken: () => void;
+}
+
+const RenewVppModal = ({
+  tokenId,
+  onCancel,
+  onRenewedToken,
+}: IRenewVppModalProps) => {
+  const { renderFlash } = useContext(NotificationContext);
+
+  const [isUploading, setIsUploading] = useState(false);
+  const [tokenFile, setTokenFile] = useState<File | null>(null);
+
+  const onSelectFile = useCallback((files: FileList | null) => {
+    const file = files?.[0];
+    if (file) {
+      setTokenFile(file);
+    }
+  }, []);
+
+  const onRenewToken = useCallback(async () => {
+    if (!tokenFile) {
+      // this shouldn'r happen, but just in case
+      renderFlash("error", "Please provide a token file.");
+      return;
+    }
+    setIsUploading(true);
+    try {
+      await mdmAppleBmAPI.renewToken(tokenId, tokenFile);
+      renderFlash("success", "Renewed successfully.");
+      setIsUploading(false);
+      onRenewedToken();
+    } catch (e) {
+      // TODO: figure out what error message will be sent from API.
+      const msg = getErrorReason(e);
+      if (msg.toLowerCase().includes("valid token")) {
+        renderFlash("error", msg);
+      } else {
+        renderFlash("error", "Couldn’t renew. Please try again.");
+      }
+      onCancel();
+      setIsUploading(false);
+    }
+  }, [tokenFile, renderFlash, tokenId, onRenewedToken, onCancel]);
+
+  return (
+    <Modal
+      title="Renew token"
+      onExit={onCancel}
+      className={baseClass}
+      isContentDisabled={isUploading}
+    >
+      <div className={`${baseClass}__page-content ${baseClass}__setup-content`}>
+        <ol className={`${baseClass}__setup-instructions-list`}>
+          <li>
+            <p>
+              1. Sign in to{" "}
+              <CustomLink
+                url="https://business.apple.com/"
+                text="Apple Business Manager"
+                newTab
+              />
+            </p>
+          </li>
+          <li>
+            <p>
+              2. Select your <b>account name</b> at the bottom left of the
+              screen, then select <b>Preferences</b>.
+            </p>
+          </li>
+          <li>
+            <p>
+              3. In the <b>Your MDM Servers</b> section, select your Fleet
+              server, then select <b>Download Token</b> at the top.
+            </p>
+          </li>
+          <li>
+            <p>
+              4. Upload the downloaded token (.p7m file) below.
+              <FileUploader
+                className={`${baseClass}__file-uploader`}
+                accept=".p7m"
+                buttonMessage="Choose file"
+                buttonType="link"
+                graphicName="file-p7m"
+                message="ABM token (.p7m)"
+                onFileUpload={onSelectFile}
+                filePreview={
+                  tokenFile && (
+                    <FileDetails
+                      details={{ name: tokenFile.name }}
+                      graphicName="file-p7m"
+                    />
+                  )
+                }
+              />
+            </p>
+          </li>
+        </ol>
+        <div className={`${baseClass}__button-wrap`}>
+          <Button
+            className={`${baseClass}__submit-button ${
+              isUploading ? `uploading` : ""
+            }`}
+            variant="brand"
+            disabled={!tokenFile || isUploading}
+            isLoading={isUploading}
+            type="button"
+            onClick={onRenewToken}
+          >
+            Renew ABM
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default RenewVppModal;
