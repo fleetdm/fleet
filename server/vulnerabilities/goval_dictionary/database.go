f409ea1e4d0be3879a9924389b86f1c6f7b910db
Ian Littman <iansltx@gmail.com>
2024-08-24 21:00:50 -0500

    Include platform in database, drop interface

--- server/vulnerabilities/goval_dictionary/database.go
+++ server/vulnerabilities/goval_dictionary/database.go
@@ -5,27 +5,23 @@ import (
 	"errors"
 	"fmt"
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/vulnerabilities/oval"
 	"github.com/fleetdm/fleet/v4/server/vulnerabilities/utils"
 	"strings"
 )
 
-type VulnDataSource interface {
-	// Eval evaluates the current OVAL definition against an OS version and a list of installed software, returns all software
-	// vulnerabilities found.
-	Eval(fleet.OSVersion, []fleet.Software) ([]fleet.SoftwareVulnerability, error)
-}
-
-func NewDB(db *sql.DB) *Database {
-	return &Database{sqlite: db}
+func NewDB(db *sql.DB, platform oval.Platform) *Database {
+	return &Database{sqlite: db, platform: platform}
 }
 
 type Database struct {
-	sqlite *sql.DB
+	sqlite   *sql.DB
+	platform oval.Platform
 }
 
 // Eval evaluates the current goval_dictionary database against an OS version and a list of installed software,
 // returns all software vulnerabilities found.
-func (db Database) Eval(version fleet.OSVersion, software []fleet.Software) ([]fleet.SoftwareVulnerability, error) {
+func (db Database) Eval(software []fleet.Software) ([]fleet.SoftwareVulnerability, error) {
 	searchStmt := `SELECT packages.version, cves.cve_id 
 		FROM packages join definitions on definitions.id = packages.definition_id
 		JOIN advisories ON advisories.definition_id = definitions.id JOIN cves ON cves.advisory_id = advisories.id
