47084fb72759db599d7647f26baaae2e35c572b0
Harrison Ravazzolo <38767391+harrisonravazzolo@users.noreply.github.com>
2024-09-03 13:56:01 -0700

    add Harrison to humans list

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -90,6 +90,7 @@ module.exports = {
       'ddribeiro',
       'rebeccaui',
       'allenhouchins',
+      'harrisonravazzolo',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
