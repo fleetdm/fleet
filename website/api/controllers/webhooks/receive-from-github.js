be2d0a65567eb00ba5c89f6d455965e9a7bb8684
Allen Houchins <32207388+allenhouchins@users.noreply.github.com>
2024-09-03 11:38:27 -0700

    added allenhouchins to name list (#21781)

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -89,6 +89,7 @@ module.exports = {
       'SFriendLee',
       'ddribeiro',
       'rebeccaui',
+      'allenhouchins',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
