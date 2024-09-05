7f978ad8a5f0759e11f76f09f98b2070d010821f
dantecatalfamo <dante.catalfamo@gmail.com>
2024-09-05 14:13:21 -0400

    Wrap error in migration

--- server/datastore/mysql/migrations/tables/20240905140513_AddVPPTokenIDToVppAppsTeams.go
+++ server/datastore/mysql/migrations/tables/20240905140513_AddVPPTokenIDToVppAppsTeams.go
@@ -22,17 +22,17 @@ ALTER TABLE vpp_apps_teams
 		FOREIGN KEY (vpp_token_id) REFERENCES vpp_tokens(id) ON DELETE CASCADE`
 
 	if _, err := tx.Exec(stmtAddColumn); err != nil {
-		return fmt.Errorf("failed to add vpp_token_id column to table")
+		return fmt.Errorf("failed to add vpp_token_id column to table: %w", err)
 	}
 
 	// Associate apps with the first token available. If we're
 	// migrating from single-token VPP this should be correct.
 	if _, err := tx.Exec(stmtAssociate); err != nil {
-		return fmt.Errorf("failed to associate vpp apps with first token")
+		return fmt.Errorf("failed to associate vpp apps with first token: %w", err)
 	}
 
 	if _, err := tx.Exec(stmtAddConstraint); err != nil {
-		return fmt.Errorf("failed to add vpp token id constraint")
+		return fmt.Errorf("failed to add vpp token id constraint: %w", err)
 	}
 
 	return nil
