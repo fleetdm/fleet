795110962d7f2f4998e5e5641c9f2d0f8ceb0c3c
Victor Lyuboslavsky <victor@fleetdm.com>
2024-09-06 16:34:00 -0500

    Updated ListHostSoftware test and fixed issues.

--- server/datastore/mysql/software.go
+++ server/datastore/mysql/software.go
@@ -2179,7 +2179,7 @@ AND EXISTS (SELECT 1 FROM software s JOIN software_cve scve ON scve.software_id 
 					hs.host_id = :host_id AND
 					s.title_id = st.id
 			) OR `
-	status := fmt.Sprintf(`COALESCE(%s, %s)`, "hsi.status", vppAppHostStatusNamedQuery("hvsi", "ncr", ""))
+	status := fmt.Sprintf(`COALESCE(%s, %s)`, "hsi.last_status", vppAppHostStatusNamedQuery("hvsi", "ncr", ""))
 	if opts.OnlyAvailableForInstall {
 		// Get software that has a package/VPP installer but was not installed with Fleet
 		softwareIsInstalledOnHostClause = fmt.Sprintf(` %s IS NULL AND (si.id IS NOT NULL OR vat.adam_id IS NOT NULL) AND %s`, status,
@@ -2200,22 +2200,64 @@ AND EXISTS (SELECT 1 FROM software s JOIN software_cve scve ON scve.software_id 
 			vat.adam_id as vpp_app_adam_id,
 			vap.latest_version as vpp_app_version,
 			NULLIF(vap.icon_url, '') as vpp_app_icon_url,
-			COALESCE(hsi_install.created_at, hvsi.created_at) as last_install_installed_at,
-			COALESCE(hsi_install.execution_id, hvsi.command_uuid) as last_install_install_uuid,
-			hsi_uninstall.created_at as last_uninstall_uninstalled_at,
-			hsi_uninstall.execution_id as last_uninstall_script_execution_id,
+			COALESCE(hsi.last_installed_at, hvsi.created_at) as last_install_installed_at,
+			COALESCE(hsi.last_install_execution_id, hvsi.command_uuid) as last_install_install_uuid,
+			hsi.last_uninstalled_at as last_uninstall_uninstalled_at,
+			hsi.last_uninstall_execution_id as last_uninstall_script_execution_id,
 			-- get either the software installer status or the vpp app status
 			%s as status
 		FROM
 			software_titles st
 		LEFT OUTER JOIN
 			software_installers si ON st.id = si.title_id AND si.global_or_team_id = :global_or_team_id
-		LEFT OUTER JOIN -- get the latest install/uninstall attempt
-			host_software_installs hsi ON si.id = hsi.software_installer_id AND hsi.host_id = :host_id AND hsi.removed = 0
-		LEFT OUTER JOIN
-			host_software_installs hsi_install ON si.id = hsi_install.software_installer_id AND hsi_install.host_id = :host_id AND hsi_install.uninstall = 0 AND hsi_install.removed = 0
-		LEFT OUTER JOIN
-			host_software_installs hsi_uninstall ON si.id = hsi.software_installer_id AND hsi_uninstall.host_id = :host_id AND hsi_uninstall.uninstall = 1 AND hsi_uninstall.removed = 0
+		LEFT OUTER JOIN -- get the latest status and install/uninstall attempts (merge 3 host_software_installs rows into 1)
+			(
+				SELECT
+					hsi_group.host_id,
+					hsi_group.software_installer_id,
+					TIMESTAMP(GROUP_CONCAT(hsi_installed_at)) as last_installed_at,
+					GROUP_CONCAT(hsi_install_execution_id) as last_install_execution_id,
+					TIMESTAMP(GROUP_CONCAT(hsi_uninstalled_at)) as last_uninstalled_at,
+					GROUP_CONCAT(hsi_uninstall_execution_id) as last_uninstall_execution_id,
+					GROUP_CONCAT(hsi_status) as last_status
+				FROM (
+					-- get latest install/uninstall status
+					SELECT
+						host_id, software_installer_id,
+						NULL as hsi_installed_at, NULL as hsi_install_execution_id,
+						NULL as hsi_uninstalled_at, NULL as hsi_uninstall_execution_id,
+						-- get the status of the latest attempt; 27-1 is the length of the timestamp
+					    SUBSTRING(MAX(CONCAT(created_at, status)), 27) AS hsi_status
+					FROM host_software_installs
+					WHERE host_id = :host_id AND removed = 0 AND uninstall = 0
+					GROUP BY host_id, software_installer_id
+					UNION
+					-- get latest install attempt
+					SELECT
+						host_id, software_installer_id,
+						MAX(created_at) as hsi_installed_at,
+						-- get the execution_id of the latest attempt; 27-1 is the length of the timestamp
+					    SUBSTRING(MAX(CONCAT(created_at, execution_id)), 27) AS hsi_install_execution_id,
+						NULL as hsi_uninstalled_at, NULL as hsi_uninstall_execution_id,
+						NULL as hsi_status
+					FROM host_software_installs
+					WHERE host_id = :host_id AND removed = 0 AND uninstall = 0
+					GROUP BY host_id, software_installer_id
+					UNION
+					-- get latest uninstall attempt
+					SELECT
+						host_id, software_installer_id,
+						NULL as hsi_installed_at, NULL as hsi_install_execution_id,
+						MAX(created_at) as hsi_uninstalled_at,
+						-- get the execution_id of the latest attempt; 27-1 is the length of the timestamp
+					    SUBSTRING(MAX(CONCAT(created_at, execution_id)), 27) AS hsi_uninstall_execution_id,
+						NULL as hsi_status
+						FROM host_software_installs
+					WHERE host_id = :host_id AND removed = 0 AND uninstall = 1
+					GROUP BY host_id, software_installer_id
+				) as hsi_group
+				GROUP BY hsi_group.host_id, hsi_group.software_installer_id
+			) as hsi ON si.id = hsi.software_installer_id
 		LEFT OUTER JOIN
 			vpp_apps vap ON st.id = vap.title_id AND vap.platform = :host_platform
 		LEFT OUTER JOIN
@@ -2225,25 +2267,7 @@ AND EXISTS (SELECT 1 FROM software s JOIN software_cve scve ON scve.software_id 
 		LEFT OUTER JOIN
 			nano_command_results ncr ON ncr.command_uuid = hvsi.command_uuid
 		WHERE
-			-- use the latest install/uninstall attempt only
-			( hsi.id IS NULL OR hsi.id = (
-				SELECT hsi2.id
-				FROM host_software_installs hsi2
-				WHERE hsi2.host_id = hsi.host_id AND hsi2.software_installer_id = hsi.software_installer_id AND hsi2.removed = 0
-				ORDER BY hsi2.created_at DESC
-				LIMIT 1 ) ) AND
-			( hsi_install.id IS NULL OR hsi_install.id = (
-				SELECT hsi2.id
-				FROM host_software_installs hsi2
-				WHERE hsi2.host_id = hsi.host_id AND hsi2.software_installer_id = hsi.software_installer_id AND hsi2.removed = 0 AND uninstall = 0
-				ORDER BY hsi2.created_at DESC
-				LIMIT 1 ) ) AND
-			( hsi_uninstall.id IS NULL OR hsi_uninstall.id = (
-				SELECT hsi2.id
-				FROM host_software_installs hsi2
-				WHERE hsi2.host_id = hsi.host_id AND hsi2.software_installer_id = hsi.software_installer_id AND hsi2.removed = 0 AND uninstall = 1
-				ORDER BY hsi2.created_at DESC
-				LIMIT 1 ) ) AND
+			-- use the latest VPP install attempt only
 			( hvsi.id IS NULL OR hvsi.id = (
 				SELECT hvsi2.id
 				FROM host_vpp_software_installs hvsi2
