031e5b9c718a0a06b8de37c1756b67d3f8c26a79
Dante Catalfamo <43040593+dantecatalfamo@users.noreply.github.com>
2024-09-06 09:14:09 -0400

    Delete apps associated with VPP tokens when they're moved or deleted (#21852)
    #21804

---
+++ server/datastore/mysql/migrations/tables/20240905140513_AddVPPTokenIDToVppAppsTeams.go
@@ -0,0 +1,43 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20240905140513, Down_20240905140513)
+}
+
+func Up_20240905140513(tx *sql.Tx) error {
+	stmtAddColumn := `
+ALTER TABLE vpp_apps_teams
+	ADD COLUMN vpp_token_id int(10) UNSIGNED NOT NULL`
+
+	stmtAssociate := `UPDATE vpp_apps_teams SET vpp_token_id = (SELECT id FROM vpp_tokens LIMIT 1)`
+
+	stmtAddConstraint := `
+ALTER TABLE vpp_apps_teams
+	ADD CONSTRAINT fk_vpp_apps_teams_vpp_token_id
+		FOREIGN KEY (vpp_token_id) REFERENCES vpp_tokens(id) ON DELETE CASCADE`
+
+	if _, err := tx.Exec(stmtAddColumn); err != nil {
+		return fmt.Errorf("failed to add vpp_token_id column to table: %w", err)
+	}
+
+	// Associate apps with the first token available. If we're
+	// migrating from single-token VPP this should be correct.
+	if _, err := tx.Exec(stmtAssociate); err != nil {
+		return fmt.Errorf("failed to associate vpp apps with first token: %w", err)
+	}
+
+	if _, err := tx.Exec(stmtAddConstraint); err != nil {
+		return fmt.Errorf("failed to add vpp token id constraint: %w", err)
+	}
+
+	return nil
+}
+
+func Down_20240905140513(tx *sql.Tx) error {
+	return nil
+}
