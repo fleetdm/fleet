20a903387cc179418afc972b57e326cf4c4e6b4e
Martin Angers <martin.n.angers@gmail.com>
2024-09-10 10:56:38 -0400

    Add test for UpsertMaintainedApp

---
+++ server/datastore/mysql/maintained_apps_test.go
@@ -0,0 +1,67 @@
+package mysql
+
+import (
+	"context"
+	"testing"
+
+	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/mdm/maintainedapps"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMaintainedApps(t *testing.T) {
+	ds := CreateMySQLDS(t)
+
+	cases := []struct {
+		name string
+		fn   func(t *testing.T, ds *Datastore)
+	}{
+		{"UpsertMaintainedApps", testUpsertMaintainedApps},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			defer TruncateTables(t, ds)
+			c.fn(t, ds)
+		})
+	}
+}
+
+func testUpsertMaintainedApps(t *testing.T, ds *Datastore) {
+	ctx := context.Background()
+
+	listSavedApps := func() []*fleet.MaintainedApp {
+		var apps []*fleet.MaintainedApp
+		ExecAdhocSQL(t, ds, func(q sqlx.ExtContext) error {
+			return sqlx.SelectContext(ctx, q, &apps, "SELECT name, version, platform FROM fleet_library_apps ORDER BY token")
+		})
+		return apps
+	}
+
+	expectedApps := maintainedapps.IngestMaintainedApps(t, ds)
+	require.Equal(t, expectedApps, listSavedApps())
+
+	// ingesting again results in no changes
+	maintainedapps.IngestMaintainedApps(t, ds)
+	require.Equal(t, expectedApps, listSavedApps())
+
+	// upsert the figma app, changing the version
+	err := ds.UpsertMaintainedApp(ctx, &fleet.MaintainedApp{
+		Name:         "Figma",
+		Token:        "figma",
+		InstallerURL: "https://desktop.figma.com/mac-arm/Figma-999.9.9.zip",
+		Version:      "999.9.9",
+		Platform:     fleet.MacOSPlatform,
+	})
+	require.NoError(t, err)
+
+	// change the expected app data for figma
+	for _, app := range expectedApps {
+		if app.Name == "Figma" {
+			app.Version = "999.9.9"
+			break
+		}
+	}
+	require.Equal(t, expectedApps, listSavedApps())
+}
