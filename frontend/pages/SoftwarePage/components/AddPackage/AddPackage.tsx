5d4e1db08b0149c671efcead773ebd5a45a92adf
jacobshandling <61553566+jacobshandling@users.noreply.github.com>
2024-09-17 17:27:49 -0400

    to RC: UI – 2 small unreleased bug fixes for software install policy automat… (#22182)
    #### This PR already merged to `main`, see
    https://github.com/fleetdm/fleet/pull/22181. This is against the release
    branch so it can be included in 4.57.0 (issue #19551)
    
    Co-authored-by: Jacob Shandling <jacob@fleetdm.com>

--- frontend/pages/SoftwarePage/components/AddPackage/AddPackage.tsx
+++ frontend/pages/SoftwarePage/components/AddPackage/AddPackage.tsx
@@ -110,16 +110,19 @@ const AddPackage = ({
       ) {
         renderFlash(
           "error",
-          `${reason}. ${(
+          <>
+            {reason}{" "}
             <CustomLink
               newTab
               url={`${LEARN_MORE_ABOUT_BASE_LINK}/read-package-version`}
               text="Learn more"
+              iconColor="core-fleet-white"
             />
-          )} `
+          </>
         );
+      } else {
+        renderFlash("error", getErrorMessage(e));
       }
-      renderFlash("error", getErrorMessage(e));
     }
 
     onExit();
