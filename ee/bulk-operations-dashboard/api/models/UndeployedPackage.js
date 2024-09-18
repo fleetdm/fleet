ed99955e3a900f7623643842d087aea63c312cb1
Eric <eashaw@sailsjs.com>
2024-09-18 12:26:03 -0500

    Create UndeployedPackage.js

---
+++ ee/bulk-operations-dashboard/api/models/UndeployedPackage.js
@@ -0,0 +1,63 @@
+/**
+ * UndeployedPackage.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name: {
+      type: 'string',
+      required: true,
+      description: 'The name of the package on the Fleet instance.',
+    },
+
+    platform: {
+      type: 'string',
+      description: 'The type of operating system this package is for.',
+      required: true,
+      isIn: [
+        'macOS & Linux',
+        'Windows'
+      ],
+    },
+
+    packageType: {
+      type: 'string',
+      description: 'The file extension of the installer package.',
+      required: true,
+      isIn: [
+        '.exe',
+        '.pkg',
+        '.msi',
+        '.deb',
+      ],
+    },
+
+    packageContents: {
+      type: 'ref',
+      columnType: 'bytea',
+      required: true,
+      // description: 'The contents of the package stored as a string.',
+    },
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
