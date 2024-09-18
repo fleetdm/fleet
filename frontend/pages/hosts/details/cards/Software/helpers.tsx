39e53179e23110ae65a1eca5dc262cf9b9a3d12e
Ian Littman <iansltx@gmail.com>
2024-09-18 12:39:33 -0500

    Cherry-Pick: Pass through uninstall error messages from backend (#22211)
    Cherry-pick of #22208
    
    Command run:
    
    ```
    git checkout minor-fleet-v4.57.0 && git checkout -b uninstall-errors-passthrough-cherry-pick && git cherry-pick f7fab00 && git push -u origin uninstall-errors-passthrough-cherry-pick
    ```

--- frontend/pages/hosts/details/cards/Software/helpers.tsx
+++ frontend/pages/hosts/details/cards/Software/helpers.tsx
@@ -3,7 +3,10 @@ import { getErrorReason } from "interfaces/errors";
 import { trimEnd, upperFirst } from "lodash";
 
 const INSTALL_SOFTWARE_ERROR_PREFIX = "Couldn't install.";
-const DEFAULT_ERROR_MESSAGE = `${INSTALL_SOFTWARE_ERROR_PREFIX} Please try again.`;
+const DEFAULT_INSTALL_ERROR_MESSAGE = `${INSTALL_SOFTWARE_ERROR_PREFIX} Please try again.`;
+
+const UNINSTALL_SOFTWARE_ERROR_PREFIX = "Couldn't uninstall.";
+const DEFAULT_UNINSTALL_ERROR_MESSAGE = `${UNINSTALL_SOFTWARE_ERROR_PREFIX} Please try again.`;
 
 const createOnlyInstallableOnMacOSMessage = (reason: string) =>
   `Couldn't install. ${reason.replace("darwin", "macOS")}.`;
@@ -28,7 +31,7 @@ const showAPIMessage = (message: string) => {
 };
 
 // eslint-disable-next-line import/prefer-default-export
-export const getErrorMessage = (e: unknown) => {
+export const getInstallErrorMessage = (e: unknown) => {
   const reason = upperFirst(trimEnd(getErrorReason(e), "."));
 
   if (reason.includes("fleetd installed")) {
@@ -41,5 +44,28 @@ export const getErrorMessage = (e: unknown) => {
     return reason;
   }
 
-  return DEFAULT_ERROR_MESSAGE;
+  return DEFAULT_INSTALL_ERROR_MESSAGE;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const getUninstallErrorMessage = (e: unknown) => {
+  const reason = upperFirst(trimEnd(getErrorReason(e), "."));
+
+  if (
+    reason.includes("run script") ||
+    reason.includes("running script") ||
+    reason.includes("have fleetd") ||
+    reason.includes("only on")
+  ) {
+    return `${UNINSTALL_SOFTWARE_ERROR_PREFIX} ${reason}.`;
+  } else if (reason.startsWith("Couldn't uninstall software.")) {
+    return reason.replace(
+      "Couldn't uninstall software.",
+      "Couldn't uninstall."
+    );
+  } else if (reason.startsWith("No uninstall script exists")) {
+    return `${UNINSTALL_SOFTWARE_ERROR_PREFIX}. An uninstall script does not exist for this package.`;
+  }
+
+  return DEFAULT_UNINSTALL_ERROR_MESSAGE;
 };
