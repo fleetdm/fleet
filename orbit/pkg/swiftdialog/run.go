187b5d78fb1549f96fec12ee0bba0115d972e897
dantecatalfamo <dante.catalfamo@gmail.com>
2024-09-23 11:32:28 -0400

    Correctly handle different exit scenarios, get command file working

--- orbit/pkg/swiftdialog/run.go
+++ orbit/pkg/swiftdialog/run.go
@@ -4,18 +4,37 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/fs"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+// SwiftDialog really wants the command file to be mode 666 for some reason
+var CommandFilePerms = fs.FileMode(0666)
+
 type SwiftDialog struct {
 	cancel      context.CancelFunc
 	cmd         *exec.Cmd
 	commandFile *os.File
+	context     context.Context
 	output      *bytes.Buffer
 }
+
+// Exit codes
+const (
+	ExitButton1               = 0
+	ExitButton2               = 2
+	ExitInfoButton            = 3
+	ExitTimer                 = 4
+	ExitQuitCommand           = 5
+	ExitQuitKey               = 10
+	ExitKeyAuthFailed         = 30
+	ExitImageResourceNotFound = 201
+	ExitFileNotFound          = 202
+)
 
 func Run(ctx context.Context, swiftDialogBin string, options *SwiftDialogOptions) (*SwiftDialog, error) {
 	commandFile, err := os.CreateTemp("", "swiftDialogCommand")
@@ -30,11 +49,18 @@ func Run(ctx context.Context, swiftDialogBin string, options *SwiftDialogOptions
 
 	ctx, cancel := context.WithCancel(ctx)
 
+	if err := commandFile.Chmod(CommandFilePerms); err != nil {
+		commandFile.Close()
+		os.Remove(commandFile.Name())
+		cancel()
+		return nil, err
+	}
+
 	cmd := exec.CommandContext(
 		ctx,
 		swiftDialogBin,
 		"--jsonstring", string(jsonBytes),
-		"--comamndfile", commandFile.Name(),
+		"--commandfile", commandFile.Name(),
 		"--json",
 	)
 
@@ -51,6 +77,7 @@ func Run(ctx context.Context, swiftDialogBin string, options *SwiftDialogOptions
 		cancel:      cancel,
 		cmd:         cmd,
 		commandFile: commandFile,
+		context:     ctx,
 		output:      outBuf,
 	}, nil
 }
@@ -82,28 +109,63 @@ func (s *SwiftDialog) cleanup() error {
 	return nil
 }
 
-func (s *SwiftDialog) Wait() (map[string]any, error) {
+type SwiftDialogExit struct {
+	ExitCode int
+	Output   map[string]any
+}
+
+func (s *SwiftDialog) Wait() (*SwiftDialogExit, error) {
+	var exitCode int
+	errExit := &exec.ExitError{}
 	err := s.cmd.Wait()
 	if err != nil {
-		return nil, fmt.Errorf("waiting for swiftDialog: %w", err)
+		if errors.As(err, &errExit) {
+			exitCode = errExit.ExitCode()
+		} else {
+			return nil, fmt.Errorf("waiting for swiftDialog: %w", err)
+		}
 	}
 
 	parsed := map[string]any{}
-	err = json.Unmarshal(s.output.Bytes(), &parsed)
-	if err != nil {
-		return nil, fmt.Errorf("parsing swiftDialog output: %w", err)
+	if s.output.Len() != 0 {
+		err = json.Unmarshal(s.output.Bytes(), &parsed)
+		if err != nil {
+			return nil, fmt.Errorf("parsing swiftDialog output: %w", err)
+		}
 	}
 
 	if err := s.cleanup(); err != nil {
 		return nil, fmt.Errorf("Wait cleaning up after swiftDialog: %w", err)
 	}
 
-	return parsed, nil
+	return &SwiftDialogExit{
+		ExitCode: exitCode,
+		Output:   parsed,
+	}, nil
 }
 
 func (s *SwiftDialog) sendCommand(command, arg string) error {
-	_, err := fmt.Fprintf(s.commandFile, "%s: %s", command, arg)
-	return err
+	// For some reason swiftDialog needs us to open and close the file
+	// to detect a new command, just writing to the file doesn't cause
+	// a change
+	commandFile, err := os.OpenFile(s.commandFile.Name(), os.O_APPEND|os.O_WRONLY|os.O_CREATE, CommandFilePerms)
+	if err != nil {
+		return fmt.Errorf("opening command file for writing: %w", err)
+	}
+
+	fullCommand := fmt.Sprintf("%s: %s", command, arg)
+
+	_, err = fmt.Fprintf(commandFile, "%s\n", fullCommand)
+	if err != nil {
+		return fmt.Errorf("writing command to file: %w", err)
+	}
+
+	err = commandFile.Close()
+	if err != nil {
+		return fmt.Errorf("closing command file: %w", err)
+	}
+
+	return nil
 }
 
 // Title
