883f8bd200c49a08841ed28572bf4816e6309131
Noah Talerman <noahtal@umich.edu>
2024-09-23 15:25:59 -0400

    Add redirect

--- website/config/routes.js
+++ website/config/routes.js
@@ -559,6 +559,7 @@ module.exports.routes = {
   'GET /learn-more-about/host-identifiers': '/docs/rest-api/rest-api#get-host-by-identifier',
   'GET /learn-more-about/uninstall-fleetd': '/docs/using-fleet/faq#how-can-i-uninstall-fleetd',
   'GET /learn-more-about/vulnerability-processing': '/docs/using-fleet/vulnerability-processing',
+  'GET /learn-more-about/dep-profile': 'https://developer.apple.com/documentation/devicemanagement/define_a_profile',
 
   // Sitemap
   // =============================================================================================================
