585c3203f3e6ff6cf55c08aaf48d3accdf92f5a3
dantecatalfamo <dante.catalfamo@gmail.com>
2024-09-25 16:21:00 -0400

    Add comments that suggest enum

--- server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
+++ server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
@@ -14,8 +14,10 @@ func Up_20240925112748(tx *sql.Tx) error {
 CREATE TABLE setup_experience_status_results (
 	id				INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	host_uuid		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	-- Type can be one of 'bootstrap-package', 'software-install', 'post-install-script'
 	type			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 	name			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	-- Status can be one of 'pending', 'installing', 'installed', 'failed', 'ran', 'running'
 	status			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 	execution_id	VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 	error 			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
