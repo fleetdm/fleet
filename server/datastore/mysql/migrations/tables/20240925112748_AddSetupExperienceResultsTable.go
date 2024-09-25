f72c63c72d1a700868f306646adc84782f03f7a9
dantecatalfamo <dante.catalfamo@gmail.com>
2024-09-25 16:15:20 -0400

    Dump schema

---
+++ server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
@@ -0,0 +1,35 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20240925112748, Down_20240925112748)
+}
+
+func Up_20240925112748(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+CREATE TABLE setup_experience_status_results (
+	id				INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	host_uuid		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	type			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	name			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	status			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	execution_id	VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	error 			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+
+	PRIMARY KEY (id)
+)
+`)
+	if err != nil {
+		return fmt.Errorf("failed to create setup_experience_status_results table: %w", err)
+	}
+
+	return nil
+}
+
+func Down_20240925112748(tx *sql.Tx) error {
+	return nil
+}
