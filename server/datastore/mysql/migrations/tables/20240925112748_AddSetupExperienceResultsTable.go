41a947ebd02450e0e7af728f4e5c8488c156063d
dantecatalfamo <dante.catalfamo@gmail.com>
2024-09-25 16:36:20 -0400

    Adjust indentation for 8 space tabs

--- server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
+++ server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
@@ -12,15 +12,15 @@ func init() {
 func Up_20240925112748(tx *sql.Tx) error {
 	_, err := tx.Exec(`
 CREATE TABLE setup_experience_status_results (
-	id				INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	host_uuid		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	id		INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	host_uuid	VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 	-- Type can be one of 'bootstrap-package', 'software-install', 'post-install-script'
-	type			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-	name			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	type		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	name		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 	-- Status can be one of 'pending', 'installing', 'installed', 'failed', 'ran', 'running'
-	status			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	status		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 	execution_id	VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-	error 			VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+	error 		VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
 
 	PRIMARY KEY (id)
 )
