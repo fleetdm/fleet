bb3aa70d47d7574168707de99669916075b8b7a0
Gabriel Hernandez <ghernandez345@gmail.com>
2024-09-25 12:27:45 +0100

    stub out onSubmit

--- frontend/pages/SoftwarePage/SoftwareAddPage/SoftwareCustomPackage/SoftwareCustomPackage.tsx
+++ frontend/pages/SoftwarePage/SoftwareAddPage/SoftwareCustomPackage/SoftwareCustomPackage.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { InjectedRouter } from "react-router";
 import { Location } from "history";
 
 import PATHS from "router/paths";
-import { buildQueryStringFromParams } from "utilities/url";
+import { buildQueryStringFromParams, QueryParams } from "utilities/url";
+import softwareAPI from "services/entities/software";
+import { LEARN_MORE_ABOUT_BASE_LINK } from "utilities/constants";
+import { NotificationContext } from "context/notification";
+
+import CustomLink from "components/CustomLink";
 
 import { ISoftwareAddPageQueryParams } from "../SoftwareAddPage";
 import AddSoftwareCustomPackageForm from "./AddSoftwareCustomPackageForm";
 import { ICustomPackageAppFormData } from "./AddSoftwareCustomPackageForm/AddSoftwareCustomPackageForm";
+import { getErrorReason } from "interfaces/errors";
+import { getErrorMessage } from "./helpers";
 
 const baseClass = "software-custom-package";
+
+// 8 minutes + 15 seconds to account for extra roundtrip time.
+export const UPLOAD_TIMEOUT = (8 * 60 + 15) * 1000;
+export const MAX_FILE_SIZE_MB = 500;
+export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 
 interface ISoftwarePackageProps {
   currentTeamId: number;
@@ -26,6 +38,9 @@ const SoftwareCustomPackage = ({
   isSidePanelOpen,
   setSidePanelOpen,
 }: ISoftwarePackageProps) => {
+  const { renderFlash } = useContext(NotificationContext);
+  const [isUploading, setIsUploading] = React.useState(false);
+
   const onCancel = () => {
     router.push(
       `${PATHS.SOFTWARE_TITLES}?${buildQueryStringFromParams({
@@ -34,8 +49,68 @@ const SoftwareCustomPackage = ({
     );
   };
 
-  const onSubmit = (formData: ICustomPackageAppFormData) => {
+  const onSubmit = async (formData: ICustomPackageAppFormData) => {
     console.log("submit", formData);
+    setIsUploading(true);
+
+    if (formData.software && formData.software.size > MAX_FILE_SIZE_BYTES) {
+      renderFlash(
+        "error",
+        `Couldn't add. The maximum file size is ${MAX_FILE_SIZE_MB} MB.`
+      );
+      setIsUploading(false);
+      return;
+    }
+
+    // Note: This TODO is copied to onSaveSoftwareChanges in EditSoftwareModal
+    // TODO: confirm we are deleting the second sentence (not modifying it) for non-self-service installers
+    try {
+      await softwareAPI.addSoftwarePackage(
+        formData,
+        currentTeamId,
+        UPLOAD_TIMEOUT
+      );
+      renderFlash(
+        "success",
+        <>
+          <b>{formData.software?.name}</b> successfully added.
+          {formData.selfService
+            ? " The end user can install from Fleet Desktop."
+            : ""}
+        </>
+      );
+
+      const newQueryParams: QueryParams = { team_id: currentTeamId };
+      if (formData.selfService) {
+        newQueryParams.self_service = true;
+      } else {
+        newQueryParams.available_for_install = true;
+      }
+      router.push(
+        `${PATHS.SOFTWARE_TITLES}?${buildQueryStringFromParams(newQueryParams)}`
+      );
+    } catch (e) {
+      const reason = getErrorReason(e);
+      if (
+        reason.includes("Couldn't add. Fleet couldn't read the version from")
+      ) {
+        renderFlash(
+          "error",
+          <>
+            {reason}{" "}
+            <CustomLink
+              newTab
+              url={`${LEARN_MORE_ABOUT_BASE_LINK}/read-package-version`}
+              text="Learn more"
+              iconColor="core-fleet-white"
+            />
+          </>
+        );
+      } else {
+        renderFlash("error", getErrorMessage(e));
+      }
+    }
+    setIsUploading(false);
   };
 
   return (
