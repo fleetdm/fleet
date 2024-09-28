05a960e79cefe5c4d2898d30a02009a4cb864b64
Ian Littman <iansltx@gmail.com>
2024-09-27 21:40:44 -0500

    Fix JS lint error

--- frontend/pages/ManageControlsPage/Scripts/components/DeleteScriptModal/DeleteScriptModal.tsx
+++ frontend/pages/ManageControlsPage/Scripts/components/DeleteScriptModal/DeleteScriptModal.tsx
@@ -44,8 +44,8 @@ const DeleteScriptModal = ({
         <p>
           The script{" "}
           <span className={`${baseClass}__script-name`}>{scriptName}</span> will
-          run on pending hosts. After the script runs, its output and
-          exit code will appear in the activity feed.
+          run on pending hosts. After the script runs, its output and exit code
+          will appear in the activity feed.
         </p>
         <div className="modal-cta-wrap">
           <Button
