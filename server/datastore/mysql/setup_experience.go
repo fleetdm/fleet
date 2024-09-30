fc3e21f2a1ec8d2625d13e207b3d0e9fbebfb7dd
dantecatalfamo <dante.catalfamo@gmail.com>
2024-09-30 12:07:50 -0400

    Make queries, wrap in retry

--- server/datastore/mysql/setup_experience.go
+++ server/datastore/mysql/setup_experience.go
@@ -1,19 +1,19 @@
 package mysql
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"slices"
+	"strings"
+
+	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
+	"github.com/jmoiron/sqlx"
+)
 
 func (ds *Datastore) SetSetupExperienceSoftwareTitles(ctx context.Context, teamID uint, softwareTitleIDs []uint) error {
-	stmtUnselectInstallers := `
-UPDATE software_installers
-SET install_during_setup = false
-WHERE team_or_global_id = ?`
+	titleIDQuestionMarks := strings.Join(slices.Repeat([]string{"?"}, len(softwareTitleIDs)), ",")
 
-	stmtUnselectVPPAppsTeams := `
-UPDATE vpp_apps_teams vat
-SET install_during_setup = false
-WHERE team_or_global_id = ?`
-
-	stmtSelectInstallersIDs := `
+	stmtSelectInstallersIDs := fmt.Sprintf(`
 SELECT
 	si.id
 FROM
@@ -25,9 +25,9 @@ WHERE
 	global_or_team_id = ?
 AND
 	st.id IN (%s)
-`
+`, softwareTitleIDs)
 
-	stmtSelectVPPAppsTeamsID := `
+	stmtSelectVPPAppsTeamsID := fmt.Sprintf(`
 SELECT
 	vat.id
 FROM
@@ -42,17 +42,64 @@ WHERE
 	global_or_team_id = ?
 AND
 	st.id IN (%s)
-`
+`, titleIDQuestionMarks)
 
-	stmtUpdateInstallers := `
+	stmtUnsetInstallers := `
+UPDATE software_installers
+SET install_during_setup = false
+WHERE team_or_global_id = ?`
+
+	stmtUnsetVPPAppsTeams := `
+UPDATE vpp_apps_teams vat
+SET install_during_setup = false
+WHERE team_or_global_id = ?`
+
+	stmtSetInstallers := `
 UPDATE software_installers
 SET install_during_setup = true
-WHERE id IN `
+WHERE id IN (%s)`
 
-	stmtUpdateVPPApps := `
+	stmtSetVPPAppsTeams := `
 UPDATE vpp_apps_teams
 SET install_during_setup = true
-WHERE id IN`
+WHERE id IN (%s)`
+
+	if err := ds.withRetryTxx(ctx, func(tx sqlx.ExtContext) error {
+		var softwareIDs []uint
+		var vppAppTeamIDs []uint
+		titleIDs := make([]any, 0, len(softwareTitleIDs))
+		for _, id := range softwareTitleIDs {
+			titleIDs = append(titleIDs, id)
+		}
+
+		if err := sqlx.SelectContext(ctx, tx, &softwareIDs, stmtSelectInstallersIDs, titleIDs...); err != nil {
+			return ctxerr.Wrap(ctx, err, "selecting software IDs using title IDs")
+		}
+
+		if err := sqlx.SelectContext(ctx, tx, &vppAppTeamIDs, stmtSelectVPPAppsTeamsID, titleIDs...); err != nil {
+			return ctxerr.Wrap(ctx, err, "selecting vpp app team IDs using title IDs")
+		}
+
+		if _, err := tx.ExecContext(ctx, stmtUnsetInstallers); err != nil {
+			return ctxerr.Wrap(ctx, err, "unsetting software installers")
+		}
+
+		if _, err := tx.ExecContext(ctx, stmtUnsetVPPAppsTeams); err != nil {
+			return ctxerr.Wrap(ctx, err, "unsetting vpp app teams")
+		}
+
+		if _, err := tx.ExecContext(ctx, stmtSetInstallers); err != nil {
+			return ctxerr.Wrap(ctx, err, "setting software installers")
+		}
+
+		if _, err := tx.ExecContext(ctx, stmtSetVPPAppsTeams); err != nil {
+			return ctxerr.Wrap(ctx, err, "setting vpp app teams")
+		}
+
+		return nil
+	}); err != nil {
+		return ctxerr.Wrap(ctx, err, "setting setup experience software")
+	}
 
 	return nil
 }
