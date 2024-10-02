25e48a0dba7f77ec5f67ba8a37735be01c9aa05b
Gabriel Hernandez <ghernandez345@gmail.com>
2024-10-02 10:20:45 +0100

    set up routing for install software card

---
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/InstallSoftware/InstallSoftware.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const baseClass = "install-software";
+
+interface IInstallSoftwareProps {}
+
+const InstallSoftware = ({}: IInstallSoftwareProps) => {
+  return <div className={baseClass}>Install software</div>;
+};
+
+export default InstallSoftware;
