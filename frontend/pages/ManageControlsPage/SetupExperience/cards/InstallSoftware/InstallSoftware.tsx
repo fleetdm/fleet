6319dfea34bb0cca3fef7591470296520986bff9
Gabriel Hernandez <ghernandez345@gmail.com>
2024-10-02 11:40:52 +0100

    add source of truth for selected ids

--- frontend/pages/ManageControlsPage/SetupExperience/cards/InstallSoftware/InstallSoftware.tsx
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/InstallSoftware/InstallSoftware.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SectionHeader from "components/SectionHeader";
+
 import InstallSoftwarePreview from "./components/InstallSoftwarePreview";
+import AddInstallSoftware from "./components/AddInstallSoftware";
 
 const baseClass = "install-software";
 
 interface IInstallSoftwareProps {}
 
 const InstallSoftware = ({}: IInstallSoftwareProps) => {
+  const [showSelectSoftwareModal, setShowSelectSoftwareModal] = useState(false);
+  const [selectedSoftwareIds, setSelectedSoftwareIds] = useState([]);
+
   return (
     <div className={baseClass}>
-      <SectionHeader title="Bootstrap package" />
+      <SectionHeader title="Install software" />
       <div className={`${baseClass}__content`}>
-        <div>col 1</div>
+        <AddInstallSoftware
+          selectedSoftwareIds={selectedSoftwareIds}
+          onAddSoftware={() => setShowSelectSoftwareModal(true)}
+        />
         <InstallSoftwarePreview />
       </div>
     </div>
---
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/InstallSoftware/components/AddInstallSoftware/AddInstallSoftware.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import Button from "components/buttons/Button";
+import CustomLink from "components/CustomLink";
+
+const baseClass = "add-install-software";
+
+interface IAddInstallSoftwareProps {
+  selectedSoftwareIds: number[];
+  onAddSoftware: () => void;
+}
+
+const AddInstallSoftware = ({
+  selectedSoftwareIds,
+  onAddSoftware,
+}: IAddInstallSoftwareProps) => {
+  const addedText =
+    selectedSoftwareIds.length === 0
+      ? "No software added."
+      : `${selectedSoftwareIds.length} software will be installed during setup.`;
+  const buttonText = "Add software";
+
+  return (
+    <div className={baseClass}>
+      <div className={`${baseClass}__description-container`}>
+        <p className={`${baseClass}__description`}>
+          Install software on hosts that automatically enroll to Fleet.
+        </p>
+        <CustomLink newTab url="" text="Learn how" />
+      </div>
+      <span className={`${baseClass}__added-text`}>{addedText}</span>
+      <div>
+        <Button
+          className={`${baseClass}__button`}
+          variant="brand"
+          onClick={onAddSoftware}
+        >
+          {buttonText}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default AddInstallSoftware;
