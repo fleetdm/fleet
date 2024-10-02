6e45c9b488d87c47cb0c50a559029fd88ab14473
Gabriel Hernandez <ghernandez345@gmail.com>
2024-10-02 10:28:15 +0100

    two column grid for card content

--- frontend/pages/ManageControlsPage/SetupExperience/cards/InstallSoftware/InstallSoftware.tsx
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/InstallSoftware/InstallSoftware.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
+import SectionHeader from "components/SectionHeader";
+
 const baseClass = "install-software";
 
 interface IInstallSoftwareProps {}
 
 const InstallSoftware = ({}: IInstallSoftwareProps) => {
-  return <div className={baseClass}>Install software</div>;
+  return (
+    <div className={baseClass}>
+      <SectionHeader title="Bootstrap package" />
+      <div className={`${baseClass}__content`}>
+        <div>col 1</div>
+        <div>col 2</div>
+      </div>
+    </div>
+  );
 };
 
 export default InstallSoftware;
