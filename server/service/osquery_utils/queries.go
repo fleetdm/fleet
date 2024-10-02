05b517a21a408026e4acdbad6c52ce79724d1c66
Ian Littman <iansltx@gmail.com>
2024-10-02 16:37:18 -0500

    Fix apo-stray-phe

--- server/service/osquery_utils/queries.go
+++ server/service/osquery_utils/queries.go
@@ -559,7 +559,7 @@ var extraDetailQueries = map[string]DetailQuery{
 		// This query is used to determine battery health of macOS and Windows hosts
 		// based on the cycle count, designed capacity, and max capacity of the battery.
 		// The `health` column is ommitted due to a known osquery issue with M1 Macs
-		// (https://github.com/fleetdm/fleet/issues/6763) and it's absence on Windows.
+		// (https://github.com/fleetdm/fleet/issues/6763) and its absence on Windows.
 		Query:            `SELECT serial_number, cycle_count, designed_capacity, max_capacity FROM battery`,
 		Platforms:        []string{"windows", "darwin"},
 		DirectIngestFunc: directIngestBattery,
