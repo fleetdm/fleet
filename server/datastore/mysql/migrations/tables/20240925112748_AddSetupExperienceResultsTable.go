23df92224fcc0bc9c99c35175dd9251d5c4bf9e0
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2024-10-04 16:24:19 -0400

    fix: new cols

--- server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
+++ server/datastore/mysql/migrations/tables/20240925112748_AddSetupExperienceResultsTable.go
@@ -64,12 +64,12 @@ CREATE TABLE setup_experience_status_results (
 	host_software_installs_id INT(10) UNSIGNED,
 
 	-- VPP app reference
-	vpp_app_id INT(10) UNSIGNED,
+	vpp_app_team_id INT(10) UNSIGNED,
 	-- VPP app install reference
 	nano_command_uuid VARCHAR(255) COLLATE utf8mb4_unicode_ci,
 
-	-- Script reference
-	script_id INT(10) UNSIGNED,
+	-- Setup script reference
+	setup_experience_script_id INT(10) UNSIGNED,
 	-- Script execution reference
 	script_execution_id	VARCHAR(255) COLLATE utf8mb4_unicode_ci,
 	error 		VARCHAR(255) COLLATE utf8mb4_unicode_ci,
@@ -82,7 +82,10 @@ CREATE TABLE setup_experience_status_results (
 	KEY idx_setup_experience_scripts_nano_command_uuid (nano_command_uuid),
 	KEY idx_setup_experience_scripts_script_execution_id (script_execution_id),
 
-	CONSTRAINT fk_setup_experience_status_results_hsi_id FOREIGN KEY (host_software_installs_id) REFERENCES host_software_installs(id) ON DELETE CASCADE
+	CONSTRAINT fk_setup_experience_status_results_hsi_id FOREIGN KEY (host_software_installs_id) REFERENCES host_software_installs(id) ON DELETE CASCADE,
+	CONSTRAINT fk_setup_experience_status_results_si_id FOREIGN KEY (software_installer_id) REFERENCES software_installers(id) ON DELETE CASCADE,
+	CONSTRAINT fk_setup_experience_status_results_va_id FOREIGN KEY (vpp_app_team_id) REFERENCES vpp_apps_teams(id) ON DELETE CASCADE,
+	CONSTRAINT fk_setup_experience_status_results_ses_id FOREIGN KEY (setup_experience_script_id) REFERENCES setup_experience_scripts(id) ON DELETE CASCADE
 )
 `)
 	// Service layer state machine like SetupExperienceNestStep()?
