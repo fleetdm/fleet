f1418dc326665f733081edc9438255fa82622c21
Gabriel Hernandez <ghernandez345@gmail.com>
2024-10-07 12:04:04 +0100

    start adding script card

--- frontend/pages/ManageControlsPage/SetupExperience/cards/RunScript/RunScript.tsx
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/RunScript/RunScript.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 
 import SectionHeader from "components/SectionHeader";
+import DataError from "components/DataError";
+import Spinner from "components/Spinner";
+
+import AddRunScript from "./components/AddRunScript";
 
 const baseClass = "run-script";
 
 interface IRunScriptProps {}
 
 const RunScript = ({}: IRunScriptProps) => {
-  const renderContent = () => {};
+  const isLoading = false;
+  const isError = false;
+
+  const renderContent = () => {
+    if (isLoading) {
+      <Spinner />;
+    }
+
+    if (isError) {
+      return <DataError />;
+    }
+
+    return <AddRunScript />;
+  };
 
   return (
     <div className={baseClass}>
