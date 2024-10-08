e16076851464c4abd015271f9c2e32ded82c2328
Martin Angers <martin.n.angers@gmail.com>
2024-10-08 17:05:17 -0400

    WIP checking status of bootstrap package and post-DEP commands

--- ee/server/service/orbit.go
+++ ee/server/service/orbit.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/ptr"
 )
 
 func (svc *Service) GetOrbitSetupExperienceStatus(ctx context.Context, orbitNodeKey string) (*fleet.SetupExperienceStatusPayload, error) {
@@ -14,6 +15,59 @@ func (svc *Service) GetOrbitSetupExperienceStatus(ctx context.Context, orbitNode
 	if err != nil {
 		return nil, ctxerr.Wrap(ctx, err, "loading host by orbit node key")
 	}
+
+	// get the status of the bootstrap package deployment
+	bootstrapPkg, err := svc.ds.GetHostMDMMacOSSetup(ctx, host.ID)
+	if err != nil && !fleet.IsNotFound(err) {
+		return nil, ctxerr.Wrap(ctx, err, "get bootstrap package status")
+	}
+	// NOTE: bootstrapPkg can be nil if there was none to install.
+
+	// TODO(mna): list MDM commands for that host, check that all are in a final
+	// state (I believe it's safe to list all commands as any old ones from
+	// before the enrollment have been cleared on re-enroll).
+	adminTeamFilter := fleet.TeamFilter{
+		User: &fleet.User{GlobalRole: ptr.String(fleet.RoleAdmin)},
+	}
+
+	// InstallEnterpriseApplication covers the commands to install fleetd and the
+	// bootstrap package.
+	installCmds, err := svc.ds.ListMDMCommands(ctx, adminTeamFilter, &fleet.MDMCommandListOptions{
+		Filters: fleet.MDMCommandFilters{
+			HostIdentifier: host.UUID,
+			RequestType:    "InstallEnterpriseApplication",
+		},
+	})
+	if err != nil {
+		return nil, ctxerr.Wrap(ctx, err, "list InstallEnterpriseApplication commands")
+	}
+	// AccountConfiguration covers the (optional) command to setup SSO.
+	acctCmds, err := svc.ds.ListMDMCommands(ctx, adminTeamFilter, &fleet.MDMCommandListOptions{
+		Filters: fleet.MDMCommandFilters{
+			HostIdentifier: host.UUID,
+			RequestType:    "AccountConfiguration",
+		},
+	})
+	if err != nil {
+		return nil, ctxerr.Wrap(ctx, err, "list AccountConfiguration commands")
+	}
+
+	// TODO(mna): we keep track of the bootstrap package status in a specific
+	// table, and fleetd is necessarily installed when this endpoint gets called,
+	// so the only command to check is the optional AccountConfiguration one (for
+	// SSO).
+	cmdsCompleted := true
+	for _, cmd := range append(installCmds, acctCmds...) {
+		// succeeded or failed, it is done (final state)
+		if cmd.Status != fleet.MDMAppleStatusAcknowledged && cmd.Status != fleet.MDMAppleStatusError &&
+			cmd.Status != fleet.MDMAppleStatusCommandFormatError {
+			cmdsCompleted = false
+		}
+	}
+
+	// TODO(mna): list profiles for that host, check that all are in a final state.
+	// TODO(mna): how to unblock the caller after 15m waiting for profiles?
+	// TODO(mna): once all software/script are in final state, release device if it's not manually released.
 
 	res, err := svc.ds.ListSetupExperienceResultsByHostUUID(ctx, host.UUID)
 	if err != nil {
