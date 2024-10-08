40fa25a097d481c1c2258a364ac7e39a5541e230
Martin Angers <martin.n.angers@gmail.com>
2024-10-08 15:44:36 -0400

    Move /status endpoint to premium, require Apple MDM

--- server/service/orbit.go
+++ server/service/orbit.go
@@ -1106,28 +1106,9 @@ func getOrbitSetupExperienceStatusEndpoint(ctx context.Context, request interfac
 }
 
 func (svc *Service) GetOrbitSetupExperienceStatus(ctx context.Context, orbitNodeKey string) (*fleet.SetupExperienceStatusPayload, error) {
-	// this is not a user-authenticated endpoint
+	// skipauth: No authorization check needed due to implementation returning
+	// only license error.
 	svc.authz.SkipAuthorization(ctx)
-	host, err := svc.ds.LoadHostByOrbitNodeKey(ctx, orbitNodeKey)
-	if err != nil {
-		return nil, ctxerr.Wrap(ctx, err, "loading host by orbit node key")
-	}
 
-	res, err := svc.ds.ListSetupExperienceResultsByHostUUID(ctx, host.UUID)
-	if err != nil {
-		return nil, ctxerr.Wrap(ctx, err, "listing setup experience results")
-	}
-
-	payload := &fleet.SetupExperienceStatusPayload{Software: make([]*fleet.SetupExperienceStatusResult, 0)}
-	for _, r := range res {
-		if r.IsForScript() {
-			payload.Script = r
-		}
-
-		if r.IsForSoftware() {
-			payload.Software = append(payload.Software, r)
-		}
-	}
-
-	return payload, nil
+	return nil, fleet.ErrMissingLicense
 }
