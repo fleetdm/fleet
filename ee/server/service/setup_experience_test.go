0d4b28fe710455651f2d7ab392c2a13288359fac
dantecatalfamo <dante.catalfamo@gmail.com>
2024-10-17 11:19:40 -0400

    Pass initial tests

--- ee/server/service/setup_experience_test.go
+++ ee/server/service/setup_experience_test.go
@@ -15,12 +15,18 @@ func TestSetupExperienceNextStep(t *testing.T) {
 	ds := new(mock.Store)
 	svc := newTestService(t, ds)
 
-	resetInvoked := func() {
+	requestedInstalls := make(map[uint][]uint)
+	resetIndicators := func() {
 		ds.InsertSoftwareInstallRequestFuncInvoked = false
 		ds.InsertHostVPPSoftwareInstallFuncInvoked = false
 		ds.NewHostScriptExecutionRequestFuncInvoked = false
 		ds.UpdateSetupExperienceStatusResultFuncInvoked = false
+		clear(requestedInstalls)
 	}
+
+	host1UUID := "123"
+	host1ID := uint(1)
+	installerID1 := uint(1)
 
 	var mockListSetupExperience []*fleet.SetupExperienceStatusResult
 	ds.ListSetupExperienceResultsByHostUUIDFunc = func(ctx context.Context, hostUUID string) ([]*fleet.SetupExperienceStatusResult, error) {
@@ -32,40 +38,47 @@ func TestSetupExperienceNextStep(t *testing.T) {
 		return mockListHostsLite, nil
 	}
 
-	hostUUID1 := "123"
-	installerID1 := uint(1)
+	ds.InsertSoftwareInstallRequestFunc = func(ctx context.Context, hostID, softwareInstallerID uint, selfService bool, policyID *uint) (string, error) {
+		requestedInstalls[hostID] = append(requestedInstalls[hostID], softwareInstallerID)
+		return "install-uuid", nil
+	}
+
+	ds.UpdateSetupExperienceStatusResultFunc = func(ctx context.Context, status *fleet.SetupExperienceStatusResult) error {
+		return nil
+	}
 
 	// No host exists
-	_, err := svc.SetupExperienceNextStep(ctx, hostUUID1)
+	_, err := svc.SetupExperienceNextStep(ctx, host1UUID)
 	require.Error(t, err)
 
-	mockListHostsLite = append(mockListHostsLite, &fleet.Host{UUID: hostUUID1})
+	mockListHostsLite = append(mockListHostsLite, &fleet.Host{UUID: host1UUID, ID: host1ID})
 
 	// Host exists, nothing to do
-	finished, err := svc.SetupExperienceNextStep(ctx, hostUUID1)
+	finished, err := svc.SetupExperienceNextStep(ctx, host1UUID)
 	require.NoError(t, err)
 	assert.True(t, finished)
 	assert.False(t, ds.InsertSoftwareInstallRequestFuncInvoked)
 	assert.False(t, ds.InsertHostVPPSoftwareInstallFuncInvoked)
 	assert.False(t, ds.NewHostScriptExecutionRequestFuncInvoked)
 	assert.False(t, ds.UpdateSetupExperienceStatusResultFuncInvoked)
-	resetInvoked()
+	resetIndicators()
 
 	// Installer queued
 	mockListSetupExperience = []*fleet.SetupExperienceStatusResult{
 		{
-			HostUUID:            hostUUID1,
+			HostUUID:            host1UUID,
 			SoftwareInstallerID: &installerID1,
 			Status:              fleet.SetupExperienceStatusPending,
 		},
 	}
 
-	finished, err = svc.SetupExperienceNextStep(ctx, hostUUID1)
+	finished, err = svc.SetupExperienceNextStep(ctx, host1UUID)
 	require.NoError(t, err)
 	assert.False(t, finished)
 	assert.True(t, ds.InsertSoftwareInstallRequestFuncInvoked)
 	assert.False(t, ds.InsertHostVPPSoftwareInstallFuncInvoked)
 	assert.False(t, ds.NewHostScriptExecutionRequestFuncInvoked)
-	assert.False(t, ds.UpdateSetupExperienceStatusResultFuncInvoked)
-	resetInvoked()
+	assert.True(t, ds.UpdateSetupExperienceStatusResultFuncInvoked)
+	assert.Len(t, requestedInstalls, 1)
+	resetIndicators()
 }
