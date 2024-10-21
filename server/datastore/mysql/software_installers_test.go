bbac39f22a8694b1d4048f8f539e4304c7b528f0
Ian Littman <iansltx@gmail.com>
2024-10-21 17:46:50 -0500

    Ensure software installs aren't deleted, and have enough info to display, even if associated installer or title are deleted (#22996)
    #21654 #22087
    
    Also persists title/installer filename/version to the install record in
    case those are edited, though we'll continue showing the current title
    name when pulling the install record at this point, and don't expose
    installed version anywhere for now.
    
    # Checklist for submitter
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/Committing-Changes.md#changes-files)
    for more information.
    - [x] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    - [x] Added/updated tests
    - [x] If database migrations are included, checked table schema to
    confirm autoupdate
    - For database migrations:
    - [x] Checked schema for all modified table for columns that will
    auto-update timestamps during migration.
    - [x] Confirmed that updating the timestamps is acceptable, and will not
    cause unwanted side effects. (**force-set updated_at to avoid**)
    - [x] Ensured the correct collation is explicitly set for character
    columns (`COLLATE utf8mb4_unicode_ci`).
    - [x] Manual QA for all new/changed functionality

--- server/datastore/mysql/software_installers_test.go
+++ server/datastore/mysql/software_installers_test.go
@@ -495,6 +495,7 @@ func testGetSoftwareInstallResult(t *testing.T, ds *Datastore) {
 				Title:         "foo" + tc.name,
 				Source:        "bar" + tc.name,
 				InstallScript: "echo " + tc.name,
+				Version:       "1.11",
 				TeamID:        &teamID,
 				Filename:      swFilename,
 				UserID:        user1.ID,
@@ -534,7 +535,34 @@ func testGetSoftwareInstallResult(t *testing.T, ds *Datastore) {
 			})
 			require.NoError(t, err)
 
+			// edit installer to ensure host software install is unaffected
+			ExecAdhocSQL(t, ds, func(q sqlx.ExtContext) error {
+				_, err = q.ExecContext(ctx, `
+					UPDATE software_installers SET filename = 'something different', version = '1.23' WHERE id = ?`,
+					installerID)
+				require.NoError(t, err)
+				return nil
+			})
+
 			res, err = ds.GetSoftwareInstallResults(ctx, installUUID)
+			require.NoError(t, err)
+			require.Equal(t, swFilename, res.SoftwarePackage)
+
+			// delete installer to confirm that we can still access the install record
+			err = ds.DeleteSoftwareInstaller(ctx, installerID)
+			require.NoError(t, err)
+
+			res, err = ds.GetSoftwareInstallResults(ctx, installUUID)
+			require.NoError(t, err)
+
+			ExecAdhocSQL(t, ds, func(q sqlx.ExtContext) error { // ensure version is not changed, though we don't expose it yet
+				var version string
+				err := sqlx.GetContext(ctx, q, &version, `SELECT "version" FROM host_software_installs WHERE execution_id = ?`, installUUID)
+				require.NoError(t, err)
+				require.Equal(t, "1.11", version)
+
+				return nil
+			})
 			require.NoError(t, err)
 
 			require.Equal(t, installUUID, res.InstallUUID)
