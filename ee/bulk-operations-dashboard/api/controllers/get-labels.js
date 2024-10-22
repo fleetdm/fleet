1bf294d1b627ca408aece69b59f39befcd130691
Eric <eashaw@sailsjs.com>
2024-10-22 17:03:35 -0500

    rebuild cloud sdk

---
+++ ee/bulk-operations-dashboard/api/controllers/get-labels.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+
+  friendlyName: 'Get labels',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+
+    let labelsOnThisInstance = [];
+
+    let labelsResponseData = await sails.helpers.http.get.with({
+      url: '/api/v1/fleet/labels',
+      baseUrl: sails.config.custom.fleetBaseUrl,
+      headers: {
+        Authorization: `Bearer ${sails.config.custom.fleetApiToken}`
+      }
+    })
+    .timeout(120000)
+    .retry(['requestFailed', {name: 'TimeoutError'}]);
+
+    for(let label of labelsResponseData.labels) {
+      labelsOnThisInstance.push({
+        name: label.name,
+        value: label.id
+      });
+    }
+    // All done.
+    return labelsOnThisInstance;
+
+  }
+
+
+};
