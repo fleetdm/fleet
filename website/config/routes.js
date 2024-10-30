ca73acfc3ab1851c1efa475df98691e5622fe255
Eric <eashaw@sailsjs.com>
2024-10-30 01:14:08 -0500

    Website: add newsletter subscription form to homepage  (#23362)
    Related to: #23212
    
    Changes:
    - Added a form to the homepage for users to subscribe to the Fleet
    newsletter.
    - Updated the unsubscribe-from-all-newsletters action to redirect users
    to the fleet homepage once they are unsubscribed from the newsletter.
    - Updated the update-or-create-one-newsletter-subscription action to
    create/update CRM records when users sign up for the Fleet newsletter.

--- website/config/routes.js
+++ website/config/routes.js
@@ -518,6 +518,7 @@ module.exports.routes = {
   'GET /g':                       (req,res)=> { let originalQueryStringWithAmp = req.url.match(/\?(.+)$/) ? '&'+req.url.match(/\?(.+)$/)[1] : ''; return res.redirect(301, sails.config.custom.baseUrl+'/?meet-fleet'+originalQueryStringWithAmp); },
   'GET /test-fleet-sandbox':     '/register',
   'GET /unsubscribe':             (req,res)=> { let originalQueryString = req.url.match(/\?(.+)$/) ? req.url.match(/\?(.+)$/)[1] : ''; return res.redirect(301, sails.config.custom.baseUrl+'/api/v1/unsubscribe-from-marketing-emails?'+originalQueryString);},
+  'GET /unsubscribe-from-newsletter':             (req,res)=> { let originalQueryString = req.url.match(/\?(.+)$/) ? req.url.match(/\?(.+)$/)[1] : ''; return res.redirect(301, sails.config.custom.baseUrl+'/api/v1/unsubscribe-from-all-newsletters?'+originalQueryString);},
   'GET /tables':                 '/tables/account_policy_data',
   'GET /imagine/launch-party':  'https://www.eventbrite.com/e/601763519887',
   'GET /blackhat2023':   'https://github.com/fleetdm/fleet/tree/main/tools/blackhat-mdm', // Assets from @marcosd4h & @zwass Black Hat 2023 talk
