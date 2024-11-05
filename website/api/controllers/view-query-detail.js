8e54a2e5acba0664f728af31b281022e72f70930
Eric <eashaw@sailsjs.com>
2024-11-05 17:30:36 -0600

    Update syntax on query details pages highlighting to support osquery table and column names

--- website/api/controllers/view-query-detail.js
+++ website/api/controllers/view-query-detail.js
@@ -34,6 +34,27 @@ module.exports = {
       throw 'notFound';
     }
 
+    // Find the related osquery table documentation for tables used in this query, and grab the keywordsForSyntaxHighlighting from each table used.
+    let allTablesInformation = _.filter(sails.config.builtStaticContent.markdownPages, (pageInfo)=>{
+      return _.startsWith(pageInfo.url, '/tables/');
+    });
+    // Get all the osquery table names, we'll use this list to determine which tables are used.
+    let allTableNames = _.pluck(allTablesInformation, 'title');
+    // Create an array of words in the query.
+    let queryWords = _.words(query.query, /[^ ]+/g);
+    let keywordsForSyntaxHighlighting = [];
+    // Get all of the words that appear in both arrays
+    let intersectionBetweenQueryWordsAndTableNames = _.intersection(queryWords, allTableNames);
+    // For each matched osquery table, add the keywordsForSyntaxHighlighting (names of tables and columns) to the array we'll use for this page.
+    for(let tableName of intersectionBetweenQueryWordsAndTableNames) {
+      let tableMentionedInThisQuery = _.find(sails.config.builtStaticContent.markdownPages, {title: tableName});
+      let keyWordsForThisTable = tableMentionedInThisQuery.keywordsForSyntaxHighlighting;
+      keywordsForSyntaxHighlighting = keywordsForSyntaxHighlighting.concat(keyWordsForThisTable);
+    }
+
+
+
+
     // Setting the meta title and description of this page using the query object, and falling back to a generic title or description if query.name or query.description are missing.
     let pageTitleForMeta = query.name ? query.name + ' | Query details' : 'Query details';
     let pageDescriptionForMeta = query.description ? query.description : 'View more information about a query in Fleet\'s standard query library';
@@ -43,6 +64,7 @@ module.exports = {
       queryLibraryYmlRepoPath: sails.config.builtStaticContent.queryLibraryYmlRepoPath,
       pageTitleForMeta,
       pageDescriptionForMeta,
+      keywordsForSyntaxHighlighting,
       algoliaPublicKey: sails.config.custom.algoliaPublicKey,
     };
 
--- website/assets/js/pages/query-detail.page.js
+++ website/assets/js/pages/query-detail.page.js
@@ -27,10 +27,44 @@ parasails.registerPage('query-detail', {
         },
       });
     }
-
-    $('pre code').each((i, block) => {
-      window.hljs.highlightElement(block);
+    let keywordsForThisTable = [];
+    if(this.keywordsForSyntaxHighlighting){
+      keywordsForThisTable = this.keywordsForSyntaxHighlighting;
+    }
+    keywordsForThisTable = keywordsForThisTable.sort((a,b)=>{// Sorting the array of keywords by length to match larger keywords first.
+      return a.length < b.length ? 1 : -1;
     });
+    (()=>{
+      $('pre code').each((i, block) => {
+        let keywordsToHighlight = [];// Empty array to track the keywords that we will need to highlight
+        for(let keyword of keywordsForThisTable){// Going through the array of keywords for this table, if the entire word matches, we'll add it to the
+          for(let match of block.innerHTML.match(keyword+' ')||[]){
+            keywordsToHighlight.push(match);
+          }
+        }
+        // Now iterate through the keywordsToHighlight, replacing all matches in the elements innerHTML.
+        let replacementHMTL = block.innerHTML;
+        for(let keywordInExample of keywordsToHighlight) {
+          let regexForThisExample = new RegExp(keywordInExample, 'g');
+          replacementHMTL = replacementHMTL.replace(regexForThisExample, '<span class="hljs-attr">'+_.trim(keywordInExample)+'</span> ');
+        }
+        $(block).html(replacementHMTL);
+        window.hljs.highlightElement(block);
+        // After we've highlighted our keywords, we'll highlight the rest of the codeblock
+        // If this example is a single-line, we'll do some basic formatting to make it more human-readable.
+        if(!$(block)[0].innerText.match(/\n/gmi)){
+          // Adding [purpose="line-break"] to SQL keywords if they are one of: SELECT, WHERE, FROM, JOIN, AND, or OR. (case-insensitive)
+          $('.hljs-keyword').each((i, el)=>{
+            for(i in el.innerText.match(/select|where|from|join|and|or/gi)) {
+              $(el).attr({'purpose':'line-break'});
+            }
+          });
+        }
+      });
+    })();
+    // $('pre code').each((i, block) => {
+    //   window.hljs.highlightElement(block);
+    // });
     $('[purpose="copy-button"]').on('click', async function() {
       let code = $(this).siblings('pre').find('code').text();
       $(this).addClass('copied');
