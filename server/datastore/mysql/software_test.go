a802331b47d815499ad454450d091b95a15564cb
Martin Angers <martin.n.angers@gmail.com>
2024-11-11 14:46:26 -0500

    mysql tests pass

--- server/datastore/mysql/software_test.go
+++ server/datastore/mysql/software_test.go
@@ -1,7 +1,6 @@
 package mysql
 
 import (
-	"bytes"
 	"context"
 	"database/sql"
 	"encoding/hex"
@@ -4593,9 +4592,11 @@ func testListHostSoftwareInstallThenTransferTeam(t *testing.T, ds *Datastore) {
 	require.NoError(t, err)
 
 	// create a software installer for team 1
+	tfr1, err := fleet.NewTempFileReader(strings.NewReader("hello"), t.TempDir)
+	require.NoError(t, err)
 	installerTm1, err := ds.MatchOrCreateSoftwareInstaller(ctx, &fleet.UploadSoftwareInstallerPayload{
 		InstallScript: "hello",
-		InstallerFile: bytes.NewReader([]byte("hello")),
+		InstallerFile: tfr1,
 		StorageID:     "storage1",
 		Filename:      "file1",
 		Title:         "file1",
@@ -4703,9 +4704,11 @@ func testListHostSoftwareInstallThenDeleteInstallers(t *testing.T, ds *Datastore
 	require.NoError(t, err)
 
 	// create a software installer for team 1
+	tfr1, err := fleet.NewTempFileReader(strings.NewReader("hello"), t.TempDir)
+	require.NoError(t, err)
 	installerTm1, err := ds.MatchOrCreateSoftwareInstaller(ctx, &fleet.UploadSoftwareInstallerPayload{
 		InstallScript: "hello",
-		InstallerFile: bytes.NewReader([]byte("hello")),
+		InstallerFile: tfr1,
 		StorageID:     "storage1",
 		Filename:      "file1",
 		Title:         "file1",
