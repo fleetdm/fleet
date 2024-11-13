b951f889316090baa5447eb06ffd7dd70c7ca6e6
mostlikelee <tim@mostlikelee.com>
2024-11-13 08:32:38 -0700

    add keyslot retries

--- tools/luks/luks/main.go
+++ tools/luks/luks/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"syscall"
 
@@ -14,7 +15,7 @@ import (
 )
 
 func main() {
-	devicepath, err := lvm.FindRootDisk()
+	devicePath, err := lvm.FindRootDisk()
 	if err != nil {
 		fmt.Println("devicepath err:", err)
 		panic(err)
@@ -22,23 +23,39 @@ func main() {
 
 	// Prompt existing passphrase from the user.
 	fmt.Printf("Enter passphrase: ")
-	password, err := term.ReadPassword(int(syscall.Stdin))
+	currentPassphrase, err := term.ReadPassword(int(syscall.Stdin))
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println()
-	currentPassphrase := string(password)
 
 	const escrowPassPhrase = "fleet123"
 
-	userKey := encryption.NewKey(0, []byte(currentPassphrase))
-	escrowKey := encryption.NewKey(1, []byte(escrowPassPhrase))
-
 	device := luks.New(luks.AESXTSPlain64Cipher)
-	err = device.AddKey(context.Background(), devicepath, userKey, escrowKey)
-	if err != nil {
-		fmt.Println("AddKey err:", err)
-		panic(err)
+
+	keySlot := 1
+	for {
+		if keySlot == 8 {
+			panic(errors.New("all LUKS key slots are full"))
+		}
+
+		userKey := encryption.NewKey(0, currentPassphrase)
+		escrowKey := encryption.NewKey(keySlot, []byte(escrowPassPhrase))
+
+		if err := device.AddKey(context.Background(), devicePath, userKey, escrowKey); err != nil {
+			if errors.Is(err, encryption.ErrEncryptionKeyRejected) {
+				currentPassphrase, err = term.ReadPassword(int(syscall.Stdin))
+				if err != nil {
+					panic(err)
+				}
+				continue
+			}
+
+			keySlot++
+			continue
+		}
+
+		break
 	}
 
 	fmt.Println("Key escrowed successfully.")
