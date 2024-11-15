2fb0d5e068965d5e83a700f35c3b9fb75d67b28e
Jacob Shandling <jacob@fleetdm.com>
2024-11-14 18:03:11 -0800

    update os settings modal

--- frontend/pages/hosts/details/OSSettingsModal/OSSettingsModal.tsx
+++ frontend/pages/hosts/details/OSSettingsModal/OSSettingsModal.tsx
@@ -29,8 +29,7 @@ const OSSettingsModal = ({
   onClose,
   onProfileResent,
 }: IOSSettingsModalProps) => {
-  // the caller should ensure that hostMDMData is not undefined and that platform is "windows" or
-  // "darwin", otherwise we will allow an empty modal will be rendered.
+  // the caller should ensure that hostMDMData is not undefined and that platform is supported otherwise we will allow an empty modal will be rendered.
   // https://fleetdm.com/handbook/company/why-this-way#why-make-it-obvious-when-stuff-breaks
 
   const memoizedTableData = useMemo(
