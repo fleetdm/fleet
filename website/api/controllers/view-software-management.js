90af9997dbf3e4c1b5fd0c1b7c16ae1fb1f30307
Eric <eashaw@sailsjs.com>
2024-11-18 18:02:39 -0600

    generate empty page

---
+++ website/api/controllers/view-software-management.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+  friendlyName: 'View software management',
+
+
+  description: 'Display "Software management" page.',
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/software-management'
+    }
+
+  },
+
+
+  fn: async function () {
+
+    // Respond with view.
+    return {};
+
+  }
+
+
+};
