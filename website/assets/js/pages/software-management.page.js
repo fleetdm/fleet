ef66f2188d75f4538254359915a2c593284d1dce
Eric <eashaw@sailsjs.com>
2024-11-19 15:59:23 -0600

    rename vulnerability-management page

--- website/assets/js/pages/software-management.page.js
+++ website/assets/js/pages/software-management.page.js
@@ -1,4 +1,4 @@
-parasails.registerPage('vulnerability-management-page', {
+parasails.registerPage('software-management-page', {
   //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
