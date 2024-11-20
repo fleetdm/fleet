14282f977aa4f2ccfc4fbb923ec50b7cbf5af2bc
Ian Littman <iansltx@gmail.com>
2024-11-20 11:48:56 -0600

    Clean up list of MDM-enabled-only endpoints, removed MDM-required middleware from /disk_encryption endpoint for consistency
    The removed endpoints should be tested in their own right; will add requisite testing in the LUKS integration tests PR

--- server/service/handler.go
+++ server/service/handler.go
@@ -697,7 +697,7 @@ func attachFleetAPIRoutes(r *mux.Router, svc fleet.Service, config config.FleetC
 	// Deprecated: GET /mdm/disk_encryption/summary is now deprecated, replaced by the
 	// GET /disk_encryption endpoint.
 	ue.GET("/api/_version_/fleet/mdm/disk_encryption/summary", getMDMDiskEncryptionSummaryEndpoint, getMDMDiskEncryptionSummaryRequest{})
-	mdmAnyMW.GET("/api/_version_/fleet/disk_encryption", getMDMDiskEncryptionSummaryEndpoint, getMDMDiskEncryptionSummaryRequest{})
+	ue.GET("/api/_version_/fleet/disk_encryption", getMDMDiskEncryptionSummaryEndpoint, getMDMDiskEncryptionSummaryRequest{})
 
 	// Deprecated: GET /mdm/hosts/:id/encryption_key is now deprecated, replaced by
 	// GET /hosts/:id/encryption_key.
