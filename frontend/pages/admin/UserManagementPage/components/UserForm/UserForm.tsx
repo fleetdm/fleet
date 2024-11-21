4603199e7f0fb389498d669ca5993b95bf7927f0
RachelElysia <rachel@fleetdm.com>
2024-11-21 13:40:16 -0500

    Move renders

--- frontend/pages/admin/UserManagementPage/components/UserForm/UserForm.tsx
+++ frontend/pages/admin/UserManagementPage/components/UserForm/UserForm.tsx
@@ -420,233 +420,250 @@ const UserForm = ({
     );
   }
 
+  const renderAccountSection = () => {
+    <div className="form-field">
+      {isModifiedByGlobalAdmin ? (
+        <>
+          <div className="form-field__label">Account</div>
+          <Radio
+            className={`${baseClass}__radio-input`}
+            label="Create user"
+            id="create-user"
+            checked={formData.newUserType !== NewUserType.AdminInvited}
+            value={NewUserType.AdminCreated}
+            name="new-user-type"
+            onChange={onRadioChange("newUserType")}
+          />
+          <Radio
+            className={`${baseClass}__radio-input`}
+            label="Invite user"
+            id="invite-user"
+            disabled={!(smtpConfigured || sesConfigured)}
+            checked={formData.newUserType === NewUserType.AdminInvited}
+            value={NewUserType.AdminInvited}
+            name="new-user-type"
+            onChange={onRadioChange("newUserType")}
+            tooltip={
+              smtpConfigured || sesConfigured ? (
+                ""
+              ) : (
+                <>
+                  The &quot;Invite user&quot; feature requires that SMTP or SES
+                  is configured in order to send invitation emails.
+                  <br />
+                  <br />
+                  SMTP can be configured in Settings &gt; Organization settings.
+                </>
+              )
+            }
+          />
+        </>
+      ) : (
+        <input
+          type="hidden"
+          id="create-user"
+          value={NewUserType.AdminCreated}
+          name="new-user-type"
+        />
+      )}
+    </div>;
+  };
+
+  const renderNameAndEmailSection = () => {
+    return (
+      <>
+        <InputField
+          label="Full name"
+          autofocus
+          error={errors.name}
+          name="name"
+          onChange={onInputChange("name")}
+          placeholder="Full name"
+          value={formData.name || ""}
+          inputOptions={{
+            maxLength: "80",
+          }}
+        />
+        <InputField
+          label="Email"
+          error={errors.email || serverErrors?.email}
+          name="email"
+          onChange={onInputChange("email")}
+          placeholder="Email"
+          value={formData.email || ""}
+          readOnly={!isNewUser && !(smtpConfigured || sesConfigured)}
+          tooltip={
+            <>
+              Editing an email address requires that SMTP or SES is configured
+              in order to send a validation email.
+              <br />
+              <br />
+              Users with Admin role can configure SMTP in{" "}
+              <strong>Settings &gt; Organization settings</strong>.
+            </>
+          }
+        />
+      </>
+    );
+  };
+
+  const renderAuthenticationSection = () => {
+    <div className="form-field">
+      <div className="form-field__label">Authentication</div>
+      <Radio
+        className={`${baseClass}__radio-input`}
+        label="Enable single sign-on"
+        id="enable-single-sign-on"
+        checked={formData.sso_enabled}
+        value={formData.sso_enabled}
+        name="authentication-type"
+        onChange={onRadioChange("sso_enabled")}
+      />
+      <Radio
+        className={`${baseClass}__radio-input`}
+        label="Require password"
+        id="require-password"
+        disabled={!(smtpConfigured || sesConfigured)}
+        checked={!formData.sso_enabled}
+        value={formData.sso_enabled}
+        name="authentication-type"
+        onChange={onSsoDisable}
+        // helpText={
+        //   canUseSso ? (
+        //     <p className={`${baseClass}__sso-input sublabel`}>
+        //       Password authentication will be disabled for this user.
+        //     </p>
+        //   ) : (
+        //     <span className={`${baseClass}__sso-input sublabel-nosso`}>
+        //       This user previously signed in via SSO, which has been
+        //       globally disabled.{" "}
+        //       <button
+        //         className="button--text-link"
+        //         onClick={onSsoDisable}
+        //       >
+        //         Add password instead
+        //         <Icon
+        //           name="chevron-right"
+        //           color="core-fleet-blue"
+        //           size="small"
+        //         />
+        //       </button>
+        //     </span>
+        //   )
+        // }
+      />
+    </div>;
+  };
+
+  const renderPasswordSection = () => {
+    <div className={`${baseClass}__${isNewUser ? "" : "edit-"}password`}>
+      <InputField
+        label="Password"
+        error={errors.password}
+        name="password"
+        onChange={onInputChange("password")}
+        placeholder={isNewUser ? "Password" : "••••••••"}
+        value={formData.password || ""}
+        type="password"
+        helpText="12-48 characters, with at least 1 number (e.g. 0 - 9) and 1 symbol (e.g. &*#)."
+        blockAutoComplete
+        tooltip={
+          isNewUser ? (
+            <>
+              This password is temporary. This user will be asked to set a new
+              password after logging in to the Fleet UI.
+              <br />
+              <br />
+              This user will not be asked to set a new password after logging in
+              to fleetctl or the Fleet API.
+            </>
+          ) : undefined
+        }
+      />
+    </div>;
+  };
+
+  // 2fa option shows on premium tier or if previously set to true before downgrading to free
+  const renderTwoFactorAuthenticationOption = () => {
+    return (
+      <div className="form-field">
+        {/* Renders missing password heading when inviting a user */}
+        {formData.newUserType === NewUserType.AdminInvited && (
+          <div className="form-field__label">Password</div>
+        )}
+        <Checkbox
+          name="two_factor_authentication_enabled"
+          onChange={onCheckboxChange("two_factor_authentication_enabled")}
+          value={formData.two_factor_authentication_enabled}
+          wrapperClassName={`${baseClass}__2fa`}
+        >
+          Enable two-factor authentication (
+          <TooltipWrapper
+            tipContent={
+              <>
+                User will be asked to authenticate with a
+                <br />
+                magic link that will be sent to their email.
+              </>
+            }
+          >
+            email
+          </TooltipWrapper>
+          )
+        </Checkbox>
+      </div>
+    );
+  };
+
+  const renderPremiumRoleOptions = () => {
+    <>
+      <div className="form-field">
+        <div className="form-field__label">Team</div>
+        {isModifiedByGlobalAdmin ? (
+          <>
+            <Radio
+              className={`${baseClass}__radio-input`}
+              label="Global user"
+              id="global-user"
+              checked={isGlobalUser}
+              value={UserTeamType.GlobalUser}
+              name="user-team-type"
+              onChange={onIsGlobalUserChange}
+            />
+            <Radio
+              className={`${baseClass}__radio-input`}
+              label="Assign team(s)"
+              id="assign-teams"
+              checked={!isGlobalUser}
+              value={UserTeamType.AssignTeams}
+              name="user-team-type"
+              onChange={onIsGlobalUserChange}
+              disabled={!availableTeams.length}
+            />
+          </>
+        ) : (
+          <>{currentTeam ? currentTeam.name : ""}</>
+        )}
+      </div>
+      {isGlobalUser ? renderGlobalRoleForm() : renderTeamsForm()}
+    </>;
+  };
+
   const renderScrollableContent = () => {
     return (
       <div className={baseClass} ref={topDivRef}>
         <form autoComplete="off">
-          {isNewUser && (
-            <div className="form-field">
-              {isModifiedByGlobalAdmin ? (
-                <>
-                  <div className="form-field__label">Account</div>
-                  <Radio
-                    className={`${baseClass}__radio-input`}
-                    label="Create user"
-                    id="create-user"
-                    checked={formData.newUserType !== NewUserType.AdminInvited}
-                    value={NewUserType.AdminCreated}
-                    name="new-user-type"
-                    onChange={onRadioChange("newUserType")}
-                  />
-                  <Radio
-                    className={`${baseClass}__radio-input`}
-                    label="Invite user"
-                    id="invite-user"
-                    disabled={!(smtpConfigured || sesConfigured)}
-                    checked={formData.newUserType === NewUserType.AdminInvited}
-                    value={NewUserType.AdminInvited}
-                    name="new-user-type"
-                    onChange={onRadioChange("newUserType")}
-                    tooltip={
-                      smtpConfigured || sesConfigured ? (
-                        ""
-                      ) : (
-                        <>
-                          The &quot;Invite user&quot; feature requires that SMTP
-                          or SES is configured in order to send invitation
-                          emails.
-                          <br />
-                          <br />
-                          SMTP can be configured in Settings &gt; Organization
-                          settings.
-                        </>
-                      )
-                    }
-                  />
-                </>
-              ) : (
-                <input
-                  type="hidden"
-                  id="create-user"
-                  value={NewUserType.AdminCreated}
-                  name="new-user-type"
-                />
-              )}
-            </div>
-          )}
-          <InputField
-            label="Full name"
-            autofocus
-            error={errors.name}
-            name="name"
-            onChange={onInputChange("name")}
-            placeholder="Full name"
-            value={formData.name || ""}
-            inputOptions={{
-              maxLength: "80",
-            }}
-          />
-          <InputField
-            label="Email"
-            error={errors.email || serverErrors?.email}
-            name="email"
-            onChange={onInputChange("email")}
-            placeholder="Email"
-            value={formData.email || ""}
-            readOnly={!isNewUser && !(smtpConfigured || sesConfigured)}
-            tooltip={
-              <>
-                Editing an email address requires that SMTP or SES is configured
-                in order to send a validation email.
-                <br />
-                <br />
-                Users with Admin role can configure SMTP in{" "}
-                <strong>Settings &gt; Organization settings</strong>.
-              </>
-            }
-          />
-          {((!isNewUser && formData.sso_enabled) || canUseSso) && (
-            <div className="form-field">
-              <div className="form-field__label">Authentication</div>
-              <Radio
-                className={`${baseClass}__radio-input`}
-                label="Enable single sign-on"
-                id="enable-single-sign-on"
-                checked={formData.sso_enabled}
-                value={formData.sso_enabled}
-                name="authentication-type"
-                onChange={onRadioChange("sso_enabled")}
-              />
-              <Radio
-                className={`${baseClass}__radio-input`}
-                label="Require password"
-                id="require-password"
-                disabled={!(smtpConfigured || sesConfigured)}
-                checked={!formData.sso_enabled}
-                value={formData.sso_enabled}
-                name="authentication-type"
-                onChange={onSsoDisable}
-                // helpText={
-                //   canUseSso ? (
-                //     <p className={`${baseClass}__sso-input sublabel`}>
-                //       Password authentication will be disabled for this user.
-                //     </p>
-                //   ) : (
-                //     <span className={`${baseClass}__sso-input sublabel-nosso`}>
-                //       This user previously signed in via SSO, which has been
-                //       globally disabled.{" "}
-                //       <button
-                //         className="button--text-link"
-                //         onClick={onSsoDisable}
-                //       >
-                //         Add password instead
-                //         <Icon
-                //           name="chevron-right"
-                //           color="core-fleet-blue"
-                //           size="small"
-                //         />
-                //       </button>
-                //     </span>
-                //   )
-                // }
-              />
-            </div>
-          )}
+          {isNewUser && renderAccountSection()}
+          {renderNameAndEmailSection()}
+          {((!isNewUser && formData.sso_enabled) || canUseSso) &&
+            renderAuthenticationSection()}
           {((isNewUser && formData.newUserType !== NewUserType.AdminInvited) ||
             (!isNewUser && !isInvitePending && isModifiedByGlobalAdmin)) &&
-            !formData.sso_enabled && (
-              <div
-                className={`${baseClass}__${isNewUser ? "" : "edit-"}password`}
-              >
-                <InputField
-                  label="Password"
-                  error={errors.password}
-                  name="password"
-                  onChange={onInputChange("password")}
-                  placeholder={isNewUser ? "Password" : "••••••••"}
-                  value={formData.password || ""}
-                  type="password"
-                  helpText="12-48 characters, with at least 1 number (e.g. 0 - 9) and 1 symbol (e.g. &*#)."
-                  blockAutoComplete
-                  tooltip={
-                    isNewUser ? (
-                      <>
-                        This password is temporary. This user will be asked to
-                        set a new password after logging in to the Fleet UI.
-                        <br />
-                        <br />
-                        This user will not be asked to set a new password after
-                        logging in to fleetctl or the Fleet API.
-                      </>
-                    ) : undefined
-                  }
-                />
-              </div>
-            )}
-          {/* 2fa option shows on premium tier or if previously set to true and
-          downgraded to free tier */}
-          {(isPremiumTier || isTwoFactorAuthenticationEnabled) && (
-            <div className="form-field">
-              {/* Renders missing password heading when inviting a user */}
-              {formData.newUserType === NewUserType.AdminInvited && (
-                <div className="form-field__label">Password</div>
-              )}
-              <Checkbox
-                name="two_factor_authentication_enabled"
-                onChange={onCheckboxChange("two_factor_authentication_enabled")}
-                value={formData.two_factor_authentication_enabled}
-                wrapperClassName={`${baseClass}__2fa`}
-              >
-                Enable two-factor authentication (
-                <TooltipWrapper
-                  tipContent={
-                    <>
-                      User will be asked to authenticate with a
-                      <br />
-                      magic link that will be sent to their email.
-                    </>
-                  }
-                >
-                  email
-                </TooltipWrapper>
-                )
-              </Checkbox>
-            </div>
-          )}
-          {isPremiumTier && (
-            <>
-              <div className="form-field">
-                <div className="form-field__label">Team</div>
-                {isModifiedByGlobalAdmin ? (
-                  <>
-                    <Radio
-                      className={`${baseClass}__radio-input`}
-                      label="Global user"
-                      id="global-user"
-                      checked={isGlobalUser}
-                      value={UserTeamType.GlobalUser}
-                      name="user-team-type"
-                      onChange={onIsGlobalUserChange}
-                    />
-                    <Radio
-                      className={`${baseClass}__radio-input`}
-                      label="Assign team(s)"
-                      id="assign-teams"
-                      checked={!isGlobalUser}
-                      value={UserTeamType.AssignTeams}
-                      name="user-team-type"
-                      onChange={onIsGlobalUserChange}
-                      disabled={!availableTeams.length}
-                    />
-                  </>
-                ) : (
-                  <>{currentTeam ? currentTeam.name : ""}</>
-                )}
-              </div>
-              {isGlobalUser ? renderGlobalRoleForm() : renderTeamsForm()}
-            </>
-          )}
-          {!isPremiumTier && renderGlobalRoleForm()}
+            !formData.sso_enabled &&
+            renderPasswordSection()}
+          {(isPremiumTier || isTwoFactorAuthenticationEnabled) &&
+            renderTwoFactorAuthenticationOption()}
+          {isPremiumTier ? renderPremiumRoleOptions() : renderGlobalRoleForm()}
         </form>
       </div>
     );
@@ -677,7 +694,6 @@ const UserForm = ({
 
   return (
     <>
-      {/* {baseError && <div className="form__base-error">{baseError}</div>} */}
       {renderScrollableContent()}
       {renderFooter()}
     </>
