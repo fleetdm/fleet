cdfdbfa6293ad274b98444d3ae08cb98e68f4519
Scott Gress <scottmgress@gmail.com>
2024-11-25 15:17:26 -0600

    tabs to spaces

--- server/datastore/mysql/software.go
+++ server/datastore/mysql/software.go
@@ -1814,9 +1814,9 @@ FROM (
 ) as combined_results
 ON DUPLICATE KEY UPDATE
     software_titles.name = software_titles.name,
-		software_titles.source = software_titles.source,
-		software_titles.browser = software_titles.browser,
-		software_titles.bundle_identifier = software_titles.bundle_identifier
+    software_titles.source = software_titles.source,
+    software_titles.browser = software_titles.browser,
+    software_titles.bundle_identifier = software_titles.bundle_identifier
 `
 		res, err := tx.ExecContext(ctx, upsertTitlesStmt)
 		if err != nil {
