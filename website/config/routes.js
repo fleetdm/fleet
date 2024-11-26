b6e14c5148540cf08b484bafe45c9e6d93d8b830
Rachael Shaw <r@rachael.wtf>
2024-11-26 14:50:24 -0600

    Update routes.js

--- website/config/routes.js
+++ website/config/routes.js
@@ -599,7 +599,6 @@ module.exports.routes = {
   'GET /learn-more-about/installing-fleetctl': '/guides/fleetctl#installing-fleetctl',
   'GET /learn-more-about/mdm-disk-encryption': '/guides/enforce-disk-encryption',
   'GET /contribute-to/policies': 'https://github.com/fleetdm/fleet/edit/main/docs/01-Using-Fleet/standard-query-library/standard-query-library.yml',
-  'GET /learn-more-about/mdm-disk-encryption': '/guides/enforce-disk-encryption',
 
   // Sitemap
   // =============================================================================================================
