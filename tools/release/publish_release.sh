03c4c094c45c785e1033b73ca58407e7021a5c25
Luke Heath <luke@fleetdm.com>
2024-11-26 11:48:29 -0600

    Update publish_release.sh

--- tools/release/publish_release.sh
+++ tools/release/publish_release.sh
@@ -626,14 +626,14 @@ fi
 
 start_ver_tag=fleet-$start_version
 
-# Check if there are updates to fleetctl dependencies (only when doing security updates to base images).
-# if [[ $(git diff $start_ver_tag ./tools/wix-docker ./tools/bomutils-docker) ]]; then
-# 	echo "⚠️  Changes in fleetctl dependencies detected, please run the following before continuing the release:"
-# 	echo "1. git tag fleetctl-docker-deps-$next_ver && git push origin fleetctl-docker-deps-$next_ver"
-# 	echo "2. Wait for the triggered https://github.com/fleetdm/fleet/actions/workflows/release-fleetctl-docker-deps.yaml build to finish."
-# 	echo "3. Smoke test the pushed images by manually running the following action: https://github.com/fleetdm/fleet/actions/workflows/test-packaging.yml"
-# 	exit 1
-# fi
+Check if there are updates to fleetctl dependencies (only when doing security updates to base images).
+if [[ $(git diff $start_ver_tag ./tools/wix-docker ./tools/bomutils-docker) ]]; then
+	echo "⚠️  Changes in fleetctl dependencies detected, please run the following before continuing the release:"
+	echo "1. git tag fleetctl-docker-deps-$next_ver && git push origin fleetctl-docker-deps-$next_ver"
+	echo "2. Wait for the triggered https://github.com/fleetdm/fleet/actions/workflows/release-fleetctl-docker-deps.yaml build to finish."
+	echo "3. Smoke test the pushed images by manually running the following action: https://github.com/fleetdm/fleet/actions/workflows/test-packaging.yml"
+	exit 1
+fi
 
 if [[ "$minor" == "true" ]]; then
     echo "Minor release from $start_version to $next_ver"
