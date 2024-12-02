bd228d7a11cf876906d05c6d5bb55279dd3cd682
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2024-12-02 17:17:28 -0500

    chore: remove unused import

--- frontend/pages/hosts/details/cards/Activity/ActivityItems/UnlockedHostActivityItem/UnlockedHostActivityItem.tsx
+++ frontend/pages/hosts/details/cards/Activity/ActivityItems/UnlockedHostActivityItem/UnlockedHostActivityItem.tsx
@@ -1,4 +1,4 @@
-import React, { act } from "react";
+import React from "react";
 
 import { IHostActivityItemComponentProps } from "../../ActivityConfig";
 import HostActivityItem from "../../HostActivityItem";
