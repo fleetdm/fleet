a0b03b0c6eb4adbf38e1c86a06d47d9a242fe5fb
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2024-12-03 09:09:53 -0500

    Fleet UI: Add ModalFooter component to help handle sticky footers (#24169)

--- frontend/pages/ManageControlsPage/Scripts/components/ScriptDetailsModal/ScriptDetailsModal.tsx
+++ frontend/pages/ManageControlsPage/Scripts/components/ScriptDetailsModal/ScriptDetailsModal.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useContext, ReactNode } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useRef,
+  useState,
+  useEffect,
+} from "react";
 import { format } from "date-fns";
 import {
   useQuery,
@@ -15,6 +21,7 @@ import { IHostScript } from "interfaces/script";
 import { IApiError, getErrorReason } from "interfaces/errors";
 
 import Modal from "components/Modal";
+import ModalFooter from "components/ModalFooter";
 import Button from "components/buttons/Button";
 import Spinner from "components/Spinner";
 import Icon from "components/Icon";
@@ -68,6 +75,17 @@ const ScriptDetailsModal = ({
   isHidden = false,
   onClickRunDetails,
 }: IScriptDetailsModalProps) => {
+  // For scrollable modal
+  const [isTopScrolling, setIsTopScrolling] = useState(false);
+  const topDivRef = useRef<HTMLDivElement>(null);
+  const checkScroll = () => {
+    if (topDivRef.current) {
+      const isScrolling =
+        topDivRef.current.scrollHeight > topDivRef.current.clientHeight;
+      setIsTopScrolling(isScrolling);
+    }
+  };
+
   const { currentUser } = useContext(AppContext);
   const { renderFlash } = useContext(NotificationContext);
 
@@ -87,6 +105,14 @@ const ScriptDetailsModal = ({
       enabled: !selectedScriptContent && !!selectedScriptDetails?.script_id,
     }
   );
+
+  // For scrollable modal
+  useEffect(() => {
+    checkScroll();
+    window.addEventListener("resize", checkScroll);
+    return () => window.removeEventListener("resize", checkScroll);
+  }, [scriptContent]); // Re-run when data changes
+
   const getScriptContent = async () => {
     try {
       const content = selectedScriptContent || scriptContent;
@@ -157,60 +183,64 @@ const ScriptDetailsModal = ({
     ]
   );
 
-  const shouldShowFooter = () => {
-    return !isLoadingScriptContent && selectedScriptDetails !== undefined;
-  };
+  const shouldShowFooter =
+    !isLoadingScriptContent && selectedScriptDetails !== undefined;
 
   const renderFooter = () => {
     if (!shouldShowFooter) {
-      return <></>;
+      return null;
     }
 
     return (
-      <>
-        <div className={`secondary-actions ${baseClass}__script-actions`}>
-          <Button
-            className={`${baseClass}__action-button`}
-            variant="icon"
-            onClick={() => onClickDownload()}
-          >
-            <Icon name="download" />
-          </Button>
-          <Button
-            className={`${baseClass}__action-button`}
-            variant="icon"
-            onClick={onDelete}
-          >
-            <Icon name="trash" color="ui-fleet-black-75" />
-          </Button>
-        </div>
-        <div className={`primary-actions ${baseClass}__host-script-actions`}>
-          {showHostScriptActions && selectedScriptDetails && (
-            <div className={`${baseClass}__manage-automations-wrapper`}>
-              <ActionsDropdown
-                className={`${baseClass}__manage-automations-dropdown`}
-                onChange={(value) =>
-                  onSelectMoreActions(
-                    value,
+      <ModalFooter
+        isTopScrolling={isTopScrolling}
+        secondaryButtons={
+          <>
+            <Button
+              className={`${baseClass}__action-button`}
+              variant="icon"
+              onClick={() => onClickDownload()}
+            >
+              <Icon name="download" />
+            </Button>
+            <Button
+              className={`${baseClass}__action-button`}
+              variant="icon"
+              onClick={onDelete}
+            >
+              <Icon name="trash" color="ui-fleet-black-75" />
+            </Button>
+          </>
+        }
+        primaryButtons={
+          <>
+            {showHostScriptActions && selectedScriptDetails && (
+              <div className={`${baseClass}__manage-automations-wrapper`}>
+                <ActionsDropdown
+                  className={`${baseClass}__manage-automations-dropdown`}
+                  onChange={(value) =>
+                    onSelectMoreActions(
+                      value,
+                      selectedScriptDetails as IHostScript
+                    )
+                  }
+                  placeholder="More actions"
+                  isSearchable={false}
+                  options={generateActionDropdownOptions(
+                    currentUser,
+                    hostTeamId || null,
                     selectedScriptDetails as IHostScript
-                  )
-                }
-                placeholder="More actions"
-                isSearchable={false}
-                options={generateActionDropdownOptions(
-                  currentUser,
-                  hostTeamId || null,
-                  selectedScriptDetails as IHostScript
-                )}
-                menuPlacement="top"
-              />
-            </div>
-          )}
-          <Button onClick={onCancel} variant="brand">
-            Done
-          </Button>
-        </div>
-      </>
+                  )}
+                  menuPlacement="top"
+                />
+              </div>
+            )}
+            <Button onClick={onCancel} variant="brand">
+              Done
+            </Button>
+          </>
+        }
+      />
     );
   };
 
@@ -224,7 +254,10 @@ const ScriptDetailsModal = ({
     }
 
     return (
-      <div className={`${baseClass}__script-content`}>
+      <div
+        className={`${baseClass}__script-content  modal-scrollable-content`}
+        ref={topDivRef}
+      >
         <span>Script content:</span>
         <Textarea className={`${baseClass}__script-content-textarea`}>
           {scriptContent}
@@ -249,10 +282,12 @@ const ScriptDetailsModal = ({
       title={selectedScriptDetails?.name || "Script details"}
       width="large"
       onExit={onCancel}
-      actionsFooter={shouldShowFooter() ? renderFooter() : undefined}
       isHidden={isHidden}
     >
-      {renderContent()}
+      <>
+        {renderContent()}
+        {shouldShowFooter ? renderFooter() : undefined}
+      </>
     </Modal>
   );
 };
