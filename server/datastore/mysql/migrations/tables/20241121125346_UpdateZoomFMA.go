ea4fd5f26d5564c459ac458bad1fa7eaa0b5ac3f
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2024-12-03 09:27:30 -0500

    fix: update migration test

--- server/datastore/mysql/migrations/tables/20241121125346_UpdateZoomFMA.go
+++ server/datastore/mysql/migrations/tables/20241121125346_UpdateZoomFMA.go
@@ -13,13 +13,13 @@ func init() {
 
 func Up_20241121125346(tx *sql.Tx) error {
 	// Remove the existing Zoom fleet-maintained app
-	_, err := tx.Exec(`DELETE FROM fleet_library_apps WHERE token = 'zoom'`)
+	_, err := tx.Exec(`DELETE FROM fleet_library_apps WHERE token = 'zoom' AND platform = 'darwin'`)
 	if err != nil {
 		return fmt.Errorf("failed to remove existing zoom app from fleet_library_apps table: %w", err)
 	}
 
 	// Clear out scheduled runs for the maintained_apps cron. This will force the cron to run on
-	// next server start and pull in the updated Zoom FMA
+	// next server start and pull in the updated Zoom FMA.
 	_, err = tx.Exec(`DELETE FROM cron_stats WHERE name = ? AND stats_type = ?`, fleet.CronMaintainedApps, fleet.CronStatsTypeScheduled)
 	if err != nil {
 		return fmt.Errorf("failed to clear past scheduled runs of maintained_apps from cron_stats table: %w", err)
--- server/datastore/mysql/migrations/tables/20241121125346_UpdateZoomFMA_test.go
+++ server/datastore/mysql/migrations/tables/20241121125346_UpdateZoomFMA_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/jmoiron/sqlx"
+	"github.com/jmoiron/sqlx/reflectx"
+	"github.com/stretchr/testify/require"
 )
 
 func TestUp_20241121125346(t *testing.T) {
@@ -13,11 +16,63 @@ func TestUp_20241121125346(t *testing.T) {
 	execNoErr(t, db, `INSERT INTO cron_stats (name, instance, stats_type, status) VALUES (?, 'foo', ?, ?)`, fleet.CronMaintainedApps, fleet.CronStatsTypeScheduled, fleet.CronStatsStatusCompleted)
 	execNoErr(t, db, `INSERT INTO cron_stats (name, instance, stats_type, status) VALUES (?, 'foo', ?, ?)`, fleet.CronMaintainedApps, fleet.CronStatsTypeTriggered, fleet.CronStatsStatusCompleted)
 
+	// Add the old Zoom and Box Drive FMAs
+	tx, err := db.Begin()
+	require.NoError(t, err)
+	txx := sqlx.Tx{Tx: tx, Mapper: reflectx.NewMapperFunc("db", sqlx.NameMapper)}
+	installScriptID, err := getOrInsertScript(txx, "echo install")
+	require.NoError(t, err)
+	uninstallScriptID, err := getOrInsertScript(txx, "echo uninstall")
+	require.NoError(t, err)
+	err = tx.Commit()
+	require.NoError(t, err)
+
+	execNoErr(
+		t,
+		db,
+		`INSERT INTO fleet_library_apps (name, token, version, platform, installer_url, sha256, bundle_identifier, install_script_content_id, uninstall_script_content_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		"Zoom",
+		"zoom",
+		"6.2.11.43613",
+		"darwin",
+		"https://cdn.zoom.us/prod/6.2.11.43613/arm64/zoomusInstallerFull.pkg",
+		"dd6d28853eb6be7eaf7731aae1855c68cd6411ef6847158e6af18fffed5f8597",
+		"us.zoom.xos",
+		installScriptID,
+		uninstallScriptID,
+	)
+
+	execNoErr(
+		t,
+		db,
+		`INSERT INTO fleet_library_apps (name, token, version, platform, installer_url, sha256, bundle_identifier, install_script_content_id, uninstall_script_content_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		"Box Drive",
+		"box-drive",
+		"2.42.212",
+		"darwin",
+		"https://e3.boxcdn.net/desktop/releases/mac/BoxDrive-2.42.212.pkg",
+		"93550756150c434bc058c30b82352c294a21e978caf436ac99e0a5f431adfb6e",
+		"com.box.desktop",
+		installScriptID,
+		uninstallScriptID,
+	)
+
 	// Apply current migration.
 	applyNext(t, db)
 
-	//
-	// Check data, insert new entries, e.g. to verify migration is safe.
-	//
-	// ...
+	// Zoom should be deleted, only the Box Drive FMA should remain
+	var fmas []fleet.MaintainedApp
+	err = db.Select(&fmas, `SELECT name, token FROM fleet_library_apps`)
+	require.Len(t, fmas, 1)
+	require.Equal(t, "Box Drive", fmas[0].Name)
+	require.Equal(t, "box-drive", fmas[0].Token)
+
+	// Only the triggered job record should remain in the cron_stats table
+	var stats []fleet.CronStats
+	err = db.Select(&stats, `SELECT name, instance, stats_type, status FROM cron_stats`)
+	require.NoError(t, err)
+	require.Len(t, stats, 1)
+	require.Equal(t, string(fleet.CronMaintainedApps), stats[0].Name)
+	require.Equal(t, fleet.CronStatsTypeTriggered, stats[0].StatsType)
+	require.Equal(t, fleet.CronStatsStatusCompleted, stats[0].Status)
 }
