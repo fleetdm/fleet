117db6d3ea7337202d1a35c0c381b5f51d97b44e
Zach Wasserman <zach@fleetdm.com>
2024-12-04 09:49:01 -0700

    add /usr/sbin for Ubuntu 24

--- orbit/pkg/table/extension_linux.go
+++ orbit/pkg/table/extension_linux.go
@@ -17,6 +17,6 @@ func PlatformTables(_ PluginOpts) ([]osquery.OsqueryPlugin, error) {
 		cryptsetup.TablePlugin(log.Logger),            // table name is "cryptsetup_status"
 		falconctl.NewFalconctlOptionTable(log.Logger), // table name is "falconctl_option"
 		falcon_kernel_check.TablePlugin(log.Logger),   // table name is "falcon_kernel_check"
-		dataflattentable.TablePluginExec(log.Logger, "nftables", dataflattentable.JsonType, []string{"/usr/bin/nft", "-jat", "list", "ruleset"}), // -j (json) -a (show object handles) -t (terse, omit set contents)
+		dataflattentable.TablePluginExec(log.Logger, "nftables", dataflattentable.JsonType, []string{"nft", "-jat", "list", "ruleset"}, dataflattentable.WithBinDirs("/usr/bin", "/usr/sbin")), // -j (json) -a (show object handles) -t (terse, omit set contents)
 	}, nil
 }
