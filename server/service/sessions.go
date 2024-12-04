0a2bd509c720c42e10ed22ba4748b862f08ef5ca
Ian Littman <iansltx@gmail.com>
2024-12-04 11:59:20 -0600

    Add note on SupportsEmailVerification login endpoint flag

--- server/service/sessions.go
+++ server/service/sessions.go
@@ -112,9 +112,13 @@ func (svc *Service) DeleteSession(ctx context.Context, id uint) error {
 ////////////////////////////////////////////////////////////////////////////////
 
 type loginRequest struct {
-	Email                     string `json:"email"`
-	Password                  string `json:"password"`
-	SupportsEmailVerification bool   `json:"supports_email_verification"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	// If false/omitted, users that require email verification (Fleet MFA) to log in will fail to log in, rather than
+	// sending an MFA email, since the MFA email will land the user in a browser and complete the login there, rather
+	// than e.g. in the CLI that initiated the login. As with SSO, the expected behavior for users with MFA is to log
+	// in with MFA, then grab an API token for use elsewhere.
+	SupportsEmailVerification bool `json:"supports_email_verification"`
 }
 
 type loginResponse struct {
