ac383dd8db7d52f61dbf35edbe9879195dd116f2
Victor Lyuboslavsky <victor.lyuboslavsky@gmail.com>
2024-12-05 15:40:59 -0600

    Added cleanup job to delete stuck pending Apple profiles (#24437)
    #23816
    
    This fix may not completely fix the customer's issue. However, I'd like
    to see if there are improvements from this fix combined with the
    previous query optimization fix.
    
    # Checklist for submitter
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/Committing-Changes.md#changes-files)
    for more information.
    - [x] Added/updated tests
    - [x] If database migrations are included, checked table schema to
    confirm autoupdate
    - For database migrations:
    - [x] Checked schema for all modified table for columns that will
    auto-update timestamps during migration.
    - [x] Confirmed that updating the timestamps is acceptable, and will not
    cause unwanted side effects.
    - [x] Ensured the correct collation is explicitly set for character
    columns (`COLLATE utf8mb4_unicode_ci`).
    - [x] Manual QA for all new/changed functionality
    
    (cherry picked from commit 968f3297256997153199762a8eb55aa6bfccc87f)

--- server/datastore/mysql/apple_mdm.go
+++ server/datastore/mysql/apple_mdm.go
@@ -5712,6 +5712,21 @@ func (ds *Datastore) CleanupHostMDMCommands(ctx context.Context) error {
 	return nil
 }
 
+func (ds *Datastore) CleanupHostMDMAppleProfiles(ctx context.Context) error {
+	// Delete pending commands that don't have a corresponding entry in nano_enrollment_queue.
+	// This could occur due to errors (i.e., large server/DB load) or server being stopped while processing the profiles.
+	// After the entry is deleted, the mdm_apple_profile_manager job will try to requeue the profile.
+	stmt := fmt.Sprintf(`
+		DELETE hmap FROM host_mdm_apple_profiles AS hmap
+		LEFT JOIN nano_enrollment_queue neq ON hmap.host_uuid = neq.id AND hmap.command_uuid = neq.command_uuid
+		WHERE neq.id IS NULL AND (hmap.status IS NULL OR hmap.status = '%s') AND hmap.updated_at < NOW() - INTERVAL 1 HOUR`,
+		fleet.MDMDeliveryPending)
+	if _, err := ds.writer(ctx).ExecContext(ctx, stmt); err != nil {
+		return ctxerr.Wrap(ctx, err, "delete from host_mdm_apple_profiles")
+	}
+	return nil
+}
+
 func (ds *Datastore) GetMDMAppleOSUpdatesSettingsByHostSerial(ctx context.Context, serial string) (*fleet.AppleOSUpdateSettings, error) {
 	stmt := `
 SELECT
