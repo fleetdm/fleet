64507bed0b4f1f9e71ab58135beef27ddf5fb24a
dantecatalfamo <dante.catalfamo@gmail.com>
2024-12-10 14:27:19 -0500

    Add ContainsPrefixVars and MaybeExpand

---
+++ server/service/fleet_vars.go
@@ -0,0 +1,106 @@
+package service
+
+import (
+	"os"
+	"strings"
+)
+
+func ContainsPrefixVars(script, prefix string) []string {
+	secrets := []string{}
+	gather := func(variable string) string {
+		if strings.HasPrefix(variable, prefix) {
+			secrets = append(secrets, strings.TrimPrefix(variable, prefix))
+		}
+		return ""
+	}
+	os.Expand(script, gather)
+
+	return secrets
+}
+
+// MaybeExpand conditionally replaces ${var} or $var in the string based on the mapping function.
+// Based on os.Expand
+func MaybeExpand(s string, mapping func(string) (string, bool)) string {
+	var buf []byte
+	// ${} is all ASCII, so bytes are fine for this operation.
+	i := 0
+	for j := 0; j < len(s); j++ {
+		if s[j] == '$' && j+1 < len(s) {
+			if buf == nil {
+				buf = make([]byte, 0, 2*len(s))
+			}
+			buf = append(buf, s[i:j]...)
+			name, w := getShellName(s[j+1:])
+			if name == "" {
+				// Unlike the original Expand, don't
+				// eat invalid syntax, just leave it
+				// and pass over.
+				w = 0
+				// Valid syntax, but $ was not
+				// followed by a valid name. Leave
+				// untouched.
+				buf = append(buf, s[j])
+			} else {
+				replacement, shouldReplace := mapping(name)
+				if shouldReplace {
+					buf = append(buf, replacement...)
+				} else {
+					// We aren't replacing this
+					// reference, pass over it.
+					w = 0
+					buf = append(buf, s[j])
+				}
+			}
+			j += w
+			i = j + 1
+		}
+	}
+	if buf == nil {
+		return s
+	}
+	return string(buf) + s[i:]
+}
+
+// isShellSpecialVar reports whether the character identifies a special
+// shell variable such as $*.
+func isShellSpecialVar(c uint8) bool {
+	switch c {
+	case '*', '#', '$', '@', '!', '?', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return true
+	}
+	return false
+}
+
+// isAlphaNum reports whether the byte is an ASCII letter, number, or underscore.
+func isAlphaNum(c uint8) bool {
+	return c == '_' || '0' <= c && c <= '9' || 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z'
+}
+
+// getShellName returns the name that begins the string and the number of bytes
+// consumed to extract it. If the name is enclosed in {}, it's part of a ${}
+// expansion and two more bytes are needed than the length of the name.
+func getShellName(s string) (string, int) {
+	switch {
+	case s[0] == '{':
+		if len(s) > 2 && isShellSpecialVar(s[1]) && s[2] == '}' {
+			return s[1:2], 3
+		}
+		// Scan to closing brace
+		for i := 1; i < len(s); i++ {
+			if s[i] == '}' {
+				if i == 1 {
+					return "", 2 // Bad syntax; eat "${}"
+				}
+				return s[1:i], i + 1
+			}
+		}
+		return "", 1 // Bad syntax; eat "${"
+	case isShellSpecialVar(s[0]):
+		return s[0:1], 1
+	}
+	// Scan alphanumerics.
+	var i int
+	for i = 0; i < len(s) && isAlphaNum(s[i]); i++ {
+	}
+	return s[:i], i
+}
---
+++ server/service/fleet_vars_test.go
@@ -0,0 +1,52 @@
+package service
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/stretchr/testify/require"
+)
+
+func TestContainsPrefixVars(t *testing.T) {
+	script := `
+#!/bin/sh
+
+echo $FLEET_SECRET_FOO is the secret
+echo words${FLEET_SECRET_BAR}words
+$FLEET_SECRET_BAZ
+${FLEET_SECRET_QUX}
+`
+	secrets := ContainsPrefixVars(fleet.FLEET_SECRET_PREFIX, script)
+	require.Contains(t, secrets, "FOO")
+	require.Contains(t, secrets, "BAR")
+	require.Contains(t, secrets, "BAZ")
+	require.Contains(t, secrets, "QUX")
+}
+
+func TestMaybeExpand(t *testing.T) {
+	script := `
+This is $OTHER_VAR, $ $$ $* ${} in a sentence with${ALSO_OTHER_VAR}in the middle.
+We want to remember $FLEET_SECRET_BANANA and also${FLEET_SECRET_STRAWBERRY}are important.
+`
+	expected := `
+This is $OTHER_VAR, $ $$ $* ${} in a sentence with${ALSO_OTHER_VAR}in the middle.
+We want to remember BREAD and alsoSHORTCAKEare important.
+`
+
+	mapping := map[string]string{
+		"BANANA":     "BREAD",
+		"STRAWBERRY": "SHORTCAKE",
+	}
+
+	mapper := func(s string) (string, bool) {
+		if strings.HasPrefix(s, fleet.FLEET_SECRET_PREFIX) {
+			return mapping[strings.TrimPrefix(s, fleet.FLEET_SECRET_PREFIX)], true
+		}
+		return "", false
+	}
+
+	expanded := MaybeExpand(script, mapper)
+
+	require.Equal(t, expected, expanded)
+}
