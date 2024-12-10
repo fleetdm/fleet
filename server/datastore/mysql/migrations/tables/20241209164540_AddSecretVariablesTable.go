bfcb3ceed4f3b89234e8330c24a7934e924e0557
Victor Lyuboslavsky <victor@fleetdm.com>
2024-12-10 15:31:21 -0600

    Updated index name per code review comments.

--- server/datastore/mysql/migrations/tables/20241209164540_AddSecretVariablesTable.go
+++ server/datastore/mysql/migrations/tables/20241209164540_AddSecretVariablesTable.go
@@ -18,11 +18,11 @@ func Up_20241209164540(tx *sql.Tx) error {
 		CREATE TABLE secret_variables (
 		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
 		name VARCHAR(255) NOT NULL,
-		value BLOB NOT NULL,
+		value BLOB NOT NULL, -- 64KB max value size
 		created_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 		updated_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
 		PRIMARY KEY (id),
-		CONSTRAINT idx_name UNIQUE (name)
+		CONSTRAINT idx_secret_variables_name UNIQUE (name)
 	) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci`,
 	)
 	return err
