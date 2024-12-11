470e07fc8298249eb41e38582889b7aa920c366c
Ian Littman <iansltx@gmail.com>
2024-12-10 19:02:56 -0600

    Cherry-Pick: Include expiration info, drop "personalized", in MFA email (#24635)
    For #22078. Merged into `main` via #24630.

--- server/mail/mfa.go
+++ server/mail/mfa.go
@@ -2,6 +2,7 @@ package mail
 
 import (
 	"bytes"
+	"github.com/fleetdm/fleet/v4/server/fleet"
 	"html/template"
 	"time"
 
@@ -10,15 +11,17 @@ import (
 
 // MFAMailer is used to build an email template for the MFA email.
 type MFAMailer struct {
-	FullName    string
-	Token       string
-	BaseURL     template.URL
-	AssetURL    template.URL
-	CurrentYear int
+	FullName     string
+	Token        string
+	BaseURL      template.URL
+	AssetURL     template.URL
+	CurrentYear  int
+	TTLInMinutes float64 // due to rounding below, will always be a whole number
 }
 
 func (i *MFAMailer) Message() ([]byte, error) {
 	i.CurrentYear = time.Now().Year()
+	i.TTLInMinutes = fleet.MFALinkTTL.Truncate(time.Minute).Minutes() // better to show a whole, rounded-down number
 	t, err := server.GetTemplate("server/mail/templates/mfa.html", "email_template")
 	if err != nil {
 		return nil, err
