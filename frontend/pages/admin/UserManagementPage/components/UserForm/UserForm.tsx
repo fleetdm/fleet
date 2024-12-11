52651cd67e3e1bcc7ab8c66a2c10c3d14131a0e5
Jacob Shandling <jacob@fleetdm.com>
2024-12-10 16:35:25 -0800

    specify in tooltip that only global admins can update org settings

--- frontend/pages/admin/UserManagementPage/components/UserForm/UserForm.tsx
+++ frontend/pages/admin/UserManagementPage/components/UserForm/UserForm.tsx
@@ -437,7 +437,8 @@ const UserForm = ({
                   is configured in order to send invitation emails.
                   <br />
                   <br />
-                  SMTP can be configured in Settings &gt; Organization settings.
+                  SMTP can be configured by global admins in Settings &gt;
+                  Organization settings.
                 </>
               )
             }
@@ -586,7 +587,8 @@ const UserForm = ({
                 send authentication emails.
                 <br />
                 <br />
-                SMTP can be configured in Settings &gt; Organization settings.
+                SMTP can be configured by global admins in Settings &gt;
+                Organization settings.
               </>
             }
           >
