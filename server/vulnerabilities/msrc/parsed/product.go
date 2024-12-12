982064a3b086fc3ce195d83b08ac3dc036930378
Scott Gress <scottmgress@gmail.com>
2024-12-12 11:35:07 -0600

    update error

--- server/vulnerabilities/msrc/parsed/product.go
+++ server/vulnerabilities/msrc/parsed/product.go
@@ -17,8 +17,6 @@ type Product string
 
 type Products map[string]Product
 
-var ErrNoMatch = errors.New("no product matches")
-
 func (p Products) GetMatchForOS(ctx context.Context, os fleet.OperatingSystem) (string, error) {
 	var dvMatch, noDvMatch string
 
@@ -55,7 +53,7 @@ func (p Products) GetMatchForOS(ctx context.Context, os fleet.OperatingSystem) (
 	}
 
 	if dvMatch == "" && noDvMatch == "" {
-		return "", ctxerr.Wrap(ctx, ErrNoMatch)
+		return "", ctxerr.Wrap(ctx, errors.New("no product matches for "+os.Name))
 	}
 
 	if dvMatch == "" {
