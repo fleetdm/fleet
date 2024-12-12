23df4137dc9abe472f12922273fb69421dd3791e
Scott Gress <scottmgress@gmail.com>
2024-12-11 22:29:57 -0600

    update branch and test

--- server/service/integration_mdm_test.go
+++ server/service/integration_mdm_test.go
@@ -2922,16 +2922,26 @@ func (s *integrationMDMTestSuite) TestEnqueueMDMCommand() {
 	results, err := json.Marshal(listCmdResp.Results)
 	t.Logf("GET /api/latest/fleet/mdm/apple/commands response:\n%s", results)
 
-	require.Len(t, listCmdResp.Results, 1)
-	require.NotZero(t, listCmdResp.Results[0].UpdatedAt)
-	listCmdResp.Results[0].UpdatedAt = time.Time{}
+	// filter to the expected command.
+	// there may be other commands due to the bootstrap packages uploaded in prior tests.
+	// TODO: decouple these tests so we don't have to do this -- perhaps delete bootstrap packages?
+	var profileListCommands []*fleet.MDMAppleCommand
+	for _, result := range listCmdResp.Results {
+		if result.RequestType == "ProfileList" {
+			profileListCommands = append(profileListCommands, result)
+		}
+	}
+
+	require.Len(t, profileListCommands, 1)
+	require.NotZero(t, profileListCommands[0].UpdatedAt)
+	profileListCommands[0].UpdatedAt = time.Time{}
 	require.Equal(t, &fleet.MDMAppleCommand{
 		DeviceID:    mdmDevice.UUID,
 		CommandUUID: uuid2,
 		Status:      "Acknowledged",
 		RequestType: "ProfileList",
 		Hostname:    "test-host",
-	}, listCmdResp.Results[0])
+	}, profileListCommands[0])
 }
 
 func (s *integrationMDMTestSuite) TestMDMWindowsCommandResults() {
