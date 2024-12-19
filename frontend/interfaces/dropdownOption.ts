ae38de5e716b92044532039e36df8d5057ba1c19
RachelElysia <rachel@fleetdm.com>
2024-12-19 13:42:37 -0500

    Fix merge conflict

--- frontend/interfaces/dropdownOption.ts
+++ frontend/interfaces/dropdownOption.ts
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import PropTypes from "prop-types";
 
 export default PropTypes.shape({
@@ -7,12 +7,14 @@ export default PropTypes.shape({
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 });
 
+export type TooltipContent = string | JSX.Element | undefined;
+
 export interface IDropdownOption {
   disabled?: boolean;
   label: string | JSX.Element;
   value: string | number;
   helpText?: ReactNode;
   premiumOnly?: boolean;
-  tooltipContent?: string | JSX.Element;
+  tooltipContent?: TooltipContent;
   onClick?: (() => void) | void | null;
 }
