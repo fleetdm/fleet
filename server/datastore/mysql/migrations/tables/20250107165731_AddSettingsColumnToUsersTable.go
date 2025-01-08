394c89bb0961157258518472cf249c167c4f7c83
Jacob Shandling <jacob@fleetdm.com>
2025-01-07 16:58:34 -0800

    refresh migration

--- server/datastore/mysql/migrations/tables/20250107165731_AddSettingsColumnToUsersTable.go
+++ server/datastore/mysql/migrations/tables/20250107165731_AddSettingsColumnToUsersTable.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20250103112935, Down_20250103112935)
+	MigrationClient.AddMigration(Up_20250107165731, Down_20250107165731)
 }
 
-func Up_20250103112935(tx *sql.Tx) error {
+func Up_20250107165731(tx *sql.Tx) error {
 	_, err := tx.Exec(`ALTER TABLE users ADD COLUMN settings json NOT NULL DEFAULT (JSON_OBJECT())`)
 	if err != nil {
 		return fmt.Errorf("failed to add settings to users: %w", err)
@@ -17,6 +17,6 @@ func Up_20250103112935(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20250103112935(tx *sql.Tx) error {
+func Down_20250107165731(tx *sql.Tx) error {
 	return nil
 }
