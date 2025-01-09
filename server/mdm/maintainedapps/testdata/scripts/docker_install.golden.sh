863a37a3e5f11a214931959d19dfe0723448c006
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2025-01-09 14:22:21 -0500

    fix: update install script for FMAs to improve re-install process (#25238)
    > For #24148
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    <!-- Note that API documentation changes are now addressed by the
    product design team. -->
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/Committing-Changes.md#changes-files)
    for more information.
    - [x] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    - [x] If database migrations are included, checked table schema to
    confirm autoupdate
    - For database migrations:
    - [x] Checked schema for all modified table for columns that will
    auto-update timestamps during migration.
    - [x] Confirmed that updating the timestamps is acceptable, and will not
    cause unwanted side effects.
    - [x] Ensured the correct collation is explicitly set for character
    columns (`COLLATE utf8mb4_unicode_ci`).
    - [x] Added/updated automated tests
    - [x] A detailed QA plan exists on the associated ticket (if it isn't
    there, work with the product group's QA engineer to add it)
    - [x] Manual QA for all new/changed functionality

--- server/mdm/maintainedapps/testdata/scripts/docker_install.golden.sh
+++ server/mdm/maintainedapps/testdata/scripts/docker_install.golden.sh
@@ -3,6 +3,45 @@
 # variables
 APPDIR="/Applications/"
 TMPDIR=$(dirname "$(realpath $INSTALLER_PATH)")
+# functions
+
+quit_application() {
+  local bundle_id="$1"
+  local timeout_duration=10
+
+  # check if the application is running
+  if ! osascript -e "application id \"$bundle_id\" is running" 2>/dev/null; then
+    return
+  fi
+
+  local console_user
+  console_user=$(stat -f "%Su" /dev/console)
+  if [[ $EUID -eq 0 && "$console_user" == "root" ]]; then
+    echo "Not logged into a non-root GUI; skipping quitting application ID '$bundle_id'."
+    return
+  fi
+
+  echo "Quitting application '$bundle_id'..."
+
+  # try to quit the application within the timeout period
+  local quit_success=false
+  SECONDS=0
+  while (( SECONDS < timeout_duration )); do
+    if osascript -e "tell application id \"$bundle_id\" to quit" >/dev/null 2>&1; then
+      if ! pgrep -f "$bundle_id" >/dev/null 2>&1; then
+        echo "Application '$bundle_id' quit successfully."
+        quit_success=true
+        break
+      fi
+    fi
+    sleep 1
+  done
+
+  if [[ "$quit_success" = false ]]; then
+    echo "Application '$bundle_id' did not quit."
+  fi
+}
+
 
 # extract contents
 MOUNT_POINT=$(mktemp -d /tmp/dmg_mount_XXXXXX)
@@ -10,6 +49,8 @@ hdiutil attach -plist -nobrowse -readonly -mountpoint "$MOUNT_POINT" "$INSTALLER
 sudo cp -R "$MOUNT_POINT"/* "$TMPDIR"
 hdiutil detach "$MOUNT_POINT"
 # copy to the applications folder
+quit_application 'com.docker.docker'
+sudo [ -d "$APPDIR/Docker.app" ] && sudo mv "$APPDIR/Docker.app" "$TMPDIR/Docker.app.bkp"
 sudo cp -R "$TMPDIR/Docker.app" "$APPDIR"
 /bin/ln -h -f -s -- "$APPDIR/Docker.app/Contents/Resources/bin/docker" "/usr/local/bin/docker"
 /bin/ln -h -f -s -- "$APPDIR/Docker.app/Contents/Resources/bin/docker-credential-desktop" "/usr/local/bin/docker-credential-desktop"
