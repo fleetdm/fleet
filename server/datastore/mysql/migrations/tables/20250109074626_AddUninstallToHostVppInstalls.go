579275eb89242d29cc69f6c1eb5eb906e50faf4d
jspenc72 <jspenc72@gmail.com>
2025-01-09 08:00:19 -0700

    create migration to AddUninstallToHostVppInstalls

---
+++ server/datastore/mysql/migrations/tables/20250109074626_AddUninstallToHostVppInstalls.go
@@ -0,0 +1,25 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20250109074626, Down_20250109074626)
+}
+
+func Up_20250109074626(tx *sql.Tx) error {
+
+	if !columnExists(tx, "host_vpp_software_installs", "install") {
+		if _, err := tx.Exec("ALTER TABLE host_vpp_software_installs ADD COLUMN install TINYINT NOT NULL DEFAULT 0"); err != nil {
+			return fmt.Errorf("failed to add removed to host_vpp_software_installs: %w", err)
+		}
+	}	
+
+	return nil
+}
+
+func Down_20250109074626(tx *sql.Tx) error {
+	return nil
+}
