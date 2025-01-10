5ba8df224b23dcb11925ebc2f867bd000ff6ad3e
jspenc72 <jspenc72@gmail.com>
2025-01-10 02:56:45 -0700

    gofmt

--- server/datastore/mysql/migrations/tables/20250109150250_AddUninstallToHostVppInstalls.go
+++ server/datastore/mysql/migrations/tables/20250109150250_AddUninstallToHostVppInstalls.go
@@ -15,7 +15,7 @@ func Up_20250109074626(tx *sql.Tx) error {
 		if _, err := tx.Exec("ALTER TABLE host_vpp_software_installs ADD COLUMN uninstall TINYINT NOT NULL DEFAULT 0"); err != nil {
 			return fmt.Errorf("failed to add removed to host_vpp_software_installs: %w", err)
 		}
-	}	
+	}
 
 	return nil
 }
