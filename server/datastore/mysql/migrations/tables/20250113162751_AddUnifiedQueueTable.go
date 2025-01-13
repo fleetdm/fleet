854ecd07edc0f19aecec611111f950aef23681a1
Martin Angers <martin.n.angers@gmail.com>
2025-01-13 09:15:06 -0500

    Fix db migrations order, rebase on feature branch

--- server/datastore/mysql/migrations/tables/20250113162751_AddUnifiedQueueTable.go
+++ server/datastore/mysql/migrations/tables/20250113162751_AddUnifiedQueueTable.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20250106162751, Down_20250106162751)
+	MigrationClient.AddMigration(Up_20250113162751, Down_20250113162751)
 }
 
-func Up_20250106162751(tx *sql.Tx) error {
+func Up_20250113162751(tx *sql.Tx) error {
 	_, err := tx.Exec(`
 CREATE TABLE upcoming_activities (
 	id              BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -124,6 +124,6 @@ CREATE TABLE software_install_upcoming_activities (
 	return nil
 }
 
-func Down_20250106162751(tx *sql.Tx) error {
+func Down_20250113162751(tx *sql.Tx) error {
 	return nil
 }
