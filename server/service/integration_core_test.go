7820cac971a1eb93468c09c59cdcacde529c53bb
dantecatalfamo <dante.catalfamo@gmail.com>
2025-01-14 11:30:54 -0500

    Adjust tests again

--- server/service/integration_core_test.go
+++ server/service/integration_core_test.go
@@ -5941,14 +5941,6 @@ func (s *integrationTestSuite) TestExternalIntegrationsConfig() {
 				"group_id": 122,
 				"enable_software_vulnerabilities": false
 			}]
-		},
-		"webhook_settings": {
-			"vulnerabilities_webhook": {
-				"enable_vulnerabilities_webhook": true,
-				"destination_url": "http://some/url",
-				"host_batch_size": 1234
-			},
-			"interval": "1h"
 		}
 	}`, srvURL)), http.StatusOK)
 
@@ -5962,14 +5954,6 @@ func (s *integrationTestSuite) TestExternalIntegrationsConfig() {
 				"group_id": 122,
 				"enable_software_vulnerabilities": true
 			}]
-		},
-		"webhook_settings": {
-			"vulnerabilities_webhook": {
-				"enable_vulnerabilities_webhook": true,
-				"destination_url": "http://some/url",
-				"host_batch_size": 1234
-			},
-			"interval": "1h"
 		}
 	}`, srvURL)), http.StatusUnprocessableEntity)
 
