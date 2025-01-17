5cbf1fa3fcaf086cc5ccdcd7c6525bb237ed7fac
George Karr <georgekarrv@users.noreply.github.com>
2025-01-17 14:38:39 -0600

    Update publish_release.sh (#25435)
    Just updating publish script to fix new qa and teams

--- tools/release/publish_release.sh
+++ tools/release/publish_release.sh
@@ -294,7 +294,9 @@ create_qa_issue() {
         if [[ "$found" == "0" ]]; then
             cat .github/ISSUE_TEMPLATE/release-qa.md | awk 'BEGIN {count=0} /^---$/ {count++} count==2 && /^---$/ {getline; count++} count > 2 {print}' > temp_qa_issue_file
             gh issue create --title "Release QA: $target_milestone" -F temp_qa_issue_file \
-                --assignee "pezhub" --assignee "xpkoala" --label ":release" --label "#g-mdm" --label "#g-endpoint-ops"
+                --assignee "pezhub"  --label "#g-mdm" --label ":release" \
+                --assignee "jmwatts" --label "#g-software" \
+                --assignee "xpkoala" --label "#g-orchestration"
             rm -f temp_qa_issue_file
         fi
     else
