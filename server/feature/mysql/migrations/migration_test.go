81363093c0fbf6dea154ab98056c5a5c0b31380c
Victor Lyuboslavsky <victor@fleetdm.com>
2025-01-24 09:44:24 -0600

    Add feature schema.sql, and fix linting errors.

--- server/feature/mysql/migrations/migration_test.go
+++ server/feature/mysql/migrations/migration_test.go
@@ -26,7 +26,6 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -110,17 +109,6 @@ func applyUpToPrev(t *testing.T) *sqlx.DB {
 	}
 }
 
-func execNoErrLastID(t *testing.T, db *sqlx.DB, query string, args ...any) int64 {
-	res, err := db.Exec(query, args...)
-	require.NoError(t, err)
-	id, _ := res.LastInsertId()
-	return id
-}
-
-func execNoErr(t *testing.T, db *sqlx.DB, query string, args ...any) {
-	execNoErrLastID(t, db, query, args...)
-}
-
 // applyNext performs the next migration in the chain.
 func applyNext(t *testing.T, db *sqlx.DB) {
 	// gooseNoDir is the value to not parse local files and instead use
@@ -128,72 +116,4 @@ func applyNext(t *testing.T, db *sqlx.DB) {
 	const gooseNoDir = ""
 	err := MigrationClient.UpByOne(db.DB, gooseNoDir)
 	require.NoError(t, err)
-}
-
-func insertQuery(t *testing.T, db *sqlx.DB) uint {
-	// Insert a record into queries table
-	insertQueryStmt := `
-		INSERT INTO queries (
-			name, description, query, observer_can_run, platform, logging_type
-		) VALUES (?, ?, ?, ?, ?, ?)
-	`
-
-	queryName := "Test Query"
-	queryDescription := "A test query for the test suite"
-	queryValue := "SELECT * FROM apps;"
-	observerCanRun := 0
-	platform := "mac" // Just a placeholder, adjust as needed
-	loggingType := "snapshot"
-
-	res, err := db.Exec(insertQueryStmt, queryName, queryDescription, queryValue, observerCanRun, platform, loggingType)
-	require.NoError(t, err)
-
-	id, err := res.LastInsertId()
-	require.NoError(t, err)
-
-	return uint(id) //nolint:gosec // dismiss G115
-}
-
-func insertHost(t *testing.T, db *sqlx.DB, teamID *uint) uint {
-	// Insert a minimal record into hosts table
-	insertHostStmt := `
-		INSERT INTO hosts (
-			hostname, uuid, platform, osquery_version, os_version, build, platform_like, code_name,
-			cpu_type, cpu_subtype, cpu_brand, hardware_vendor, hardware_model, hardware_version,
-			hardware_serial, computer_name, team_id
-		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-	`
-
-	hostName := "Dummy Hostname"
-	hostUUID := "12345678-1234-1234-1234-123456789012"
-	hostPlatform := "windows"
-	osqueryVer := "5.9.1"
-	osVersion := "Windows 10"
-	buildVersion := "10.0.19042.1234"
-	platformLike := "windows"
-	codeName := "20H2"
-	cpuType := "x86_64"
-	cpuSubtype := "x86_64"
-	cpuBrand := "Intel"
-	hwVendor := "Dell Inc."
-	hwModel := "OptiPlex 7090"
-	hwVersion := "1.0"
-	hwSerial := "ABCDEFGHIJ"
-	computerName := "DESKTOP-TEST"
-
-	res, err := db.Exec(insertHostStmt, hostName, hostUUID, hostPlatform, osqueryVer, osVersion, buildVersion, platformLike, codeName,
-		cpuType, cpuSubtype, cpuBrand, hwVendor, hwModel, hwVersion, hwSerial, computerName, teamID)
-	require.NoError(t, err)
-
-	id, err := res.LastInsertId()
-	require.NoError(t, err)
-
-	return uint(id) //nolint:gosec // dismiss G115
-}
-
-func assertRowCount(t *testing.T, db *sqlx.DB, table string, count int) {
-	var n int
-	err := db.Get(&n, fmt.Sprintf("SELECT COUNT(*) FROM %s", table))
-	require.NoError(t, err)
-	assert.Equal(t, count, n)
 }
---
+++ tools/dbutils/feature/schema_generator.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"github.com/WatchBeam/clock"
+	"github.com/fleetdm/fleet/v4/server/config"
+	"github.com/fleetdm/fleet/v4/server/datastore/mysql"
+	"github.com/go-kit/log"
+)
+
+const (
+	testUsername = "root"
+	testPassword = "toor"
+	testAddress  = "localhost:3307"
+)
+
+func panicif(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		panic("not enough arguments")
+	}
+	fmt.Println("dumping schema to", os.Args[1])
+
+	// Create the database (must use raw MySQL client to do this)
+	db, err := sql.Open(
+		"mysql",
+		fmt.Sprintf("%s:%s@tcp(%s)/?multiStatements=true", testUsername, testPassword, testAddress),
+	)
+	panicif(err)
+	defer db.Close()
+	_, err = db.Exec("DROP DATABASE IF EXISTS schemadb; CREATE DATABASE schemadb;")
+	panicif(err)
+
+	// Create a datastore client in order to run migrations as usual
+	config := config.MysqlConfig{
+		Username: testUsername,
+		Password: testPassword,
+		Address:  testAddress,
+		Database: "schemadb",
+	}
+	ds, err := mysql.New(config, clock.NewMockClock(), mysql.Logger(log.NewNopLogger()), mysql.LimitAttempts(1))
+	panicif(err)
+	defer ds.Close()
+	panicif(ds.MigrateFeatureTables(context.Background()))
+
+	// Set created_at/updated_at for migrations and app_config_json to prevent the schema from being changed every time
+	// This schema is to test anyway
+	fixedDate := time.Date(2020, 01, 01, 01, 01, 01, 01, time.UTC)
+	_, err = db.Exec(`USE schemadb`)
+	panicif(err)
+	_, err = db.Exec(`UPDATE feature_migration_status SET tstamp = ?`, fixedDate)
+	panicif(err)
+
+	// Dump schema to dumpfile
+	cmd := exec.Command(
+		"docker", "compose", "exec", "-T", "mysql_test",
+		// Command run inside container
+		"mysqldump", "-u"+testUsername, "-p"+testPassword, "schemadb", "--compact", "--skip-comments",
+	)
+	var stdoutBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	panicif(cmd.Run())
+
+	panicif(os.WriteFile(os.Args[1], stdoutBuf.Bytes(), 0o644))
+}
