02c9a443cb4bc33cb22eaafd985018f76b6d66b0
Ian Littman <iansltx@gmail.com>
2025-01-25 18:18:41 -0600

    Add CPE translation mapping for IntelliJ CE for Windows
    Won't solve the false positive issues due to version number mismatches, but will fix the false negative where CE wasn't matching at all.
    
    For #25662.

--- server/vulnerabilities/nvd/cpe_test.go
+++ server/vulnerabilities/nvd/cpe_test.go
@@ -1334,6 +1334,15 @@ func TestCPEFromSoftwareIntegration(t *testing.T) {
 		},
 		{
 			software: fleet.Software{
+				Name:    "IntelliJ IDEA Community Edition 2022.3.2",
+				Source:  "programs",
+				Version: "223.8617.56",
+				Vendor:  "",
+			},
+			cpe: "cpe:2.3:a:jetbrains:intellij_idea:223.8617.56:*:*:*:*:windows:*:*",
+		},
+		{
+			software: fleet.Software{
 				Name:             "IntelliJ IDEA.app",
 				Source:           "apps",
 				Version:          "2022.3.3",
