98f0728ceec1655a793dcc780f246b2c50a35402
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2025-01-27 15:32:12 -0500

    Fleet UI: Update FMA API errors in UI (#25646)

--- frontend/pages/SoftwarePage/SoftwareAddPage/SoftwareFleetMaintained/FleetMaintainedAppDetailsPage/FleetMaintainedAppDetailsPage.tsx
+++ frontend/pages/SoftwarePage/SoftwareAddPage/SoftwareFleetMaintained/FleetMaintainedAppDetailsPage/FleetMaintainedAppDetailsPage.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { AxiosResponse } from "axios";
 import { Location } from "history";
 import { useQuery } from "react-query";
 import { InjectedRouter } from "react-router";
@@ -38,6 +39,12 @@ import {
   getFleetAppPolicyName,
   getFleetAppPolicyQuery,
 } from "./helpers";
+
+const DEFAULT_ERROR_MESSAGE = "Couldn't add. Please try again.";
+const REQUEST_TIMEOUT_ERROR_MESSAGE =
+  "Couldn't upload. Request timeout. Please make sure your server and load balancer timeout is long enough.";
+const AUTOMATIC_POLICY_ERROR_MESSAGE =
+  "Couldn't add automatic install policy. Software is successfully added. To retry, delete software and add it again.";
 
 const baseClass = "fleet-maintained-app-details-page";
 
@@ -196,7 +203,22 @@ const FleetMaintainedAppDetailsPage = ({
     } catch (error) {
       // quick exit if there was an error adding the software. Skip the policy
       // creation.
-      renderFlash("error", getErrorMessage(error));
+
+      const ae = (typeof error === "object" ? error : {}) as AxiosResponse;
+
+      const errorMessage = getErrorMessage(ae);
+
+      if (
+        ae.status === 408 ||
+        errorMessage.includes("json decoder error") // 400 bad request when really slow
+      ) {
+        renderFlash("error", REQUEST_TIMEOUT_ERROR_MESSAGE);
+      } else if (errorMessage) {
+        renderFlash("error", errorMessage);
+      } else {
+        renderFlash("error", DEFAULT_ERROR_MESSAGE);
+      }
+
       setShowAddFleetAppSoftwareModal(false);
       return;
     }
@@ -221,11 +243,9 @@ const FleetMaintainedAppDetailsPage = ({
           { persistOnPageChange: true }
         );
       } catch (e) {
-        renderFlash(
-          "error",
-          "Couldn't add automatic install policy. Software is successfully added. To retry, delete software and add it again.",
-          { persistOnPageChange: true }
-        );
+        renderFlash("error", AUTOMATIC_POLICY_ERROR_MESSAGE, {
+          persistOnPageChange: true,
+        });
       }
 
       // for automatic install we redirect on both a successful and error policy
