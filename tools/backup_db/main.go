86e2a437bf154b06a33171e7de647360cb6154e5
Scott Gress <scott@fleetdm.com>
2025-01-27 12:08:47 -0600

    continue snapshot work

--- tools/backup_db/main.go
+++ tools/backup_db/main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/manifoldco/promptui"
 )
 
 func main() {
+	// Determine the path to the top-level directory (where the Makefile resides).
+	repoRoot, err := getRepoRoot()
+	if err != nil {
+		fmt.Printf("Error determining repo root: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Change the working directory to the repo root.
+	if err := os.Chdir(repoRoot); err != nil {
+		fmt.Printf("Error changing directory to repo root: %v\n", err)
+		os.Exit(1)
+	}
+
 	// Get the home directory so we can get the backups dir.
 	homedir, err := os.UserHomeDir()
 	if err != nil {
@@ -29,13 +43,19 @@ func main() {
 	}
 
 	// Walk the ~/.fleet/db-backups directory if it exists.
+	files, err := os.ReadDir(backupsDir)
+	fileNames := make([]string, len(files))
+	for i, file := range files {
+		fileNames[i] = file.Name()
+		fileInfo, err := file.Info()
+		if err == nil {
+			fileNames[i] += " (" + fileInfo.ModTime().String() + ")"
+		}
+	}
 
 	prompt := promptui.Select{
-		Label: "Select Day",
-		Items: []string{
-			"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-			"Saturday", "Sunday",
-		},
+		Label: "Select snapshot to restore",
+		Items: fileNames,
 	}
 
 	_, result, err := prompt.Run()
@@ -44,5 +64,38 @@ func main() {
 		return
 	}
 
-	fmt.Printf("You choose %q\n", result)
+	cmd := exec.Command("/Users/scott/Development/fleet/tools/backup_db/restore.sh", result)
+
+	// Use the same stdin, stdout, and stderr as the parent process.
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Run the command.
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+// getRepoRoot determines the repo root (top-level directory) relative to this binary.
+func getRepoRoot() (string, error) {
+	// Get the path of the currently executing binary
+	executable, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	// Get the path of the binary, following symlinks.
+	execDir, err := filepath.EvalSymlinks(executable)
+	if err != nil {
+		return "", err
+	}
+	// Get the directory.
+	execDir = filepath.Dir(execDir)
+
+	// Compute the repo root relative to the binary's location.
+	repoRoot := filepath.Join(execDir, "../")
+	return filepath.Abs(repoRoot)
 }
--- tools/backup_db/restore.sh
+++ tools/backup_db/restore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-docker run --rm -i --network fleet_default ${FLEET_MYSQL_IMAGE:-mysql:8.0.36} bash -c 'gzip -dc - | mysql -hmysql -uroot -ptoor fleet' < backup.sql.gz
+BACKUP_NAME="${1:-backup.sql.gz}"
+docker run --rm -i --network fleet_default ${FLEET_MYSQL_IMAGE:-mysql:8.0.36} bash -c 'gzip -dc - | mysql -hmysql -uroot -ptoor fleet' < ${BACKUP_NAME}
