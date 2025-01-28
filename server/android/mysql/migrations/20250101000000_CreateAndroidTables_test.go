e0db298626bab6deb0b7c6183dcbbc299f234c8d
Victor Lyuboslavsky <victor@fleetdm.com>
2025-01-28 08:54:03 -0600

    Temporarily skipping failing test.

--- server/android/mysql/migrations/20250101000000_CreateAndroidTables_test.go
+++ server/android/mysql/migrations/20250101000000_CreateAndroidTables_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 )
 
+// TestUp_20250101000000 is a dummy test to ensure the test flow isn't broken.
+// Remove this test after adding a real test for a future migration.
 func TestUp_20250101000000(t *testing.T) {
 	db := applyUpToPrev(t)
 
