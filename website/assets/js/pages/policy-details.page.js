b637bfadc2aaf9c50e90a2482cbbe47b52cab908
Eric <eashaw@sailsjs.com>
2025-01-29 17:51:07 -0600

    update syntax highlighting on policy pages

--- website/assets/js/pages/policy-details.page.js
+++ website/assets/js/pages/policy-details.page.js
@@ -45,38 +45,42 @@ parasails.registerPage('policy-details', {
     });
     (()=>{
       $('pre code').each((i, block) => {
-        let tableNamesToHighlight = [];// Empty array to track the keywords that we will need to highlight
-        for(let tableName of tableNamesForThisQuery){// Going through the array of keywords for this table, if the entire word matches, we'll add it to the
-          for(let match of block.innerHTML.match(tableName)|| []){
-            tableNamesToHighlight.push(match);
-          }
-        }
-        // Now iterate through the tableNamesToHighlight, replacing all matches in the elements innerHTML.
-        let replacementHMTL = block.innerHTML;
-        for(let keywordInExample of tableNamesToHighlight) {
-          let regexForThisExample = new RegExp(keywordInExample, 'g');
-          replacementHMTL = replacementHMTL.replace(regexForThisExample, '<span class="hljs-attr">'+_.trim(keywordInExample)+'</span>');
-        }
-        $(block).html(replacementHMTL);
-        let columnNamesToHighlight = [];// Empty array to track the keywords that we will need to highlight
-        for(let columnName of columnNamesForThisQuery){// Going through the array of keywords for this table, if the entire word matches, we'll add it to the
-          for(let match of block.innerHTML.match(columnName)||[]){
-            columnNamesToHighlight.push(match);
-          }
-        }
-
-        for(let keywordInExample of columnNamesToHighlight) {
-          let regexForThisExample = new RegExp(keywordInExample, 'g');
-          replacementHMTL = replacementHMTL.replace(regexForThisExample, '<span class="hljs-string">'+_.trim(keywordInExample)+'</span>');
-        }
-        $(block).html(replacementHMTL);
-        window.hljs.highlightElement(block);
-        // After we've highlighted our keywords, we'll highlight the rest of the codeblock
-        // If this example is a single-line, we'll do some basic formatting to make it more human-readable.
-        if($(block)[0].innerText.match(/\n/gmi)){
-          $(block).addClass('has-linebreaks');
+        if(!block.classList.contains('sql')){
+          window.hljs.highlightElement(block);
         } else {
-          $(block).addClass('no-linebreaks');
+          let tableNamesToHighlight = [];// Empty array to track the keywords that we will need to highlight
+          for(let tableName of tableNamesForThisQuery){// Going through the array of keywords for this table, if the entire word matches, we'll add it to the
+            for(let match of block.innerHTML.match(tableName)|| []){
+              tableNamesToHighlight.push(match);
+            }
+          }
+          // Now iterate through the tableNamesToHighlight, replacing all matches in the elements innerHTML.
+          let replacementHMTL = block.innerHTML;
+          for(let keywordInExample of tableNamesToHighlight) {
+            let regexForThisExample = new RegExp(keywordInExample, 'g');
+            replacementHMTL = replacementHMTL.replace(regexForThisExample, '<span class="hljs-attr">'+_.trim(keywordInExample)+'</span>');
+          }
+          $(block).html(replacementHMTL);
+          let columnNamesToHighlight = [];// Empty array to track the keywords that we will need to highlight
+          for(let columnName of columnNamesForThisQuery){// Going through the array of keywords for this table, if the entire word matches, we'll add it to the
+            for(let match of block.innerHTML.match(columnName)||[]){
+              columnNamesToHighlight.push(match);
+            }
+          }
+
+          for(let keywordInExample of columnNamesToHighlight) {
+            let regexForThisExample = new RegExp(keywordInExample, 'g');
+            replacementHMTL = replacementHMTL.replace(regexForThisExample, '<span class="hljs-string">'+_.trim(keywordInExample)+'</span>');
+          }
+          $(block).html(replacementHMTL);
+          window.hljs.highlightElement(block);
+          // After we've highlighted our keywords, we'll highlight the rest of the codeblock
+          // If this example is a single-line, we'll do some basic formatting to make it more human-readable.
+          if($(block)[0].innerText.match(/\n/gmi)){
+            $(block).addClass('has-linebreaks');
+          } else {
+            $(block).addClass('no-linebreaks');
+          }
         }
       });
     })();
