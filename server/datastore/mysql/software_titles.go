0cea5790eab5b39a65115d059d5c2d50575873fa
Scott Gress <scott@fleetdm.com>
2025-01-29 11:10:31 -0600

    update comment

--- server/datastore/mysql/software_titles.go
+++ server/datastore/mysql/software_titles.go
@@ -338,7 +338,6 @@ GROUP BY st.id, package_self_service, package_name, package_version, package_url
 	}
 
 	additionalWhere := "TRUE"
-
 	match := opt.ListOptions.MatchQuery
 	softwareJoin := ""
 	if match != "" || opt.VulnerableOnly {
@@ -411,8 +410,8 @@ GROUP BY st.id, package_self_service, package_name, package_version, package_url
 		defaultFilter += ` AND ( si.self_service = 1 OR vat.self_service = 1 ) `
 	}
 
-	// if excluding fleet maintained apps, join on the fleet_library_apps table by bundle ID
-	// and filter out any row from software_titles that has a matching row in fleet_library_apps.
+	// if excluding fleet maintained apps, filter out any row from software_titles
+	// that has a matching row in fleet_library_apps.
 	if opt.ExcludeFleetMaintainedApps {
 		additionalWhere += " AND NOT EXISTS ( SELECT FALSE FROM fleet_library_apps AS fla WHERE fla.bundle_identifier = st.bundle_identifier )"
 	}
