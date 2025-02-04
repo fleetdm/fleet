7ff5800763c234b31977a33a75f9b755b26d457f
Benjamin Edwards <edwards.benw@gmail.com>
2025-02-04 18:04:19 -0500

    update vuln processing task to use volumes the same way the fleet module does

--- terraform/addons/external-vuln-scans/main.tf
+++ terraform/addons/external-vuln-scans/main.tf
@@ -58,6 +58,33 @@ resource "aws_ecs_task_definition" "vuln-processing" {
   pid_mode                 = var.fleet_config.pid_mode
   requires_compatibilities = ["FARGATE"]
 
+  dynamic "volume" {
+    for_each = var.fleet_config.volumes
+    content {
+      name      = volume.value.name
+      host_path = lookup(volume.value, "host_path", null)
+
+      dynamic "docker_volume_configuration" {
+        for_each = lookup(volume.value, "docker_volume_configuration", [])
+        content {
+          scope         = lookup(docker_volume_configuration.value, "scope", null)
+          autoprovision = lookup(docker_volume_configuration.value, "autoprovision", null)
+          driver        = lookup(docker_volume_configuration.value, "driver", null)
+          driver_opts   = lookup(docker_volume_configuration.value, "driver_opts", null)
+          labels        = lookup(docker_volume_configuration.value, "labels", null)
+        }
+      }
+
+      dynamic "efs_volume_configuration" {
+        for_each = lookup(volume.value, "efs_volume_configuration", [])
+        content {
+          file_system_id = lookup(efs_volume_configuration.value, "file_system_id", null)
+          root_directory = lookup(efs_volume_configuration.value, "root_directory", null)
+        }
+      }
+    }
+  }
+
   container_definitions = jsonencode(concat([
     {
       name                  = "fleet-vuln-processing"
