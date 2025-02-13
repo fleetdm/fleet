f3f028be2706f726f604b43ad3035dcd9271c20d
Jacob Shandling <jacob@fleetdm.com>
2025-02-13 14:11:13 -0800

    Implement `GitOpsModeTooltipWrapper` for use in remainder of UI work

---
+++ frontend/components/GitOpsModeTooltipWrapper/GitOpsModeTooltipWrapper.tsx
@@ -0,0 +1,57 @@
+import CustomLink from "components/CustomLink";
+import TooltipWrapper, {
+  ITooltipWrapper,
+} from "components/TooltipWrapper/TooltipWrapper";
+import { AppContext } from "context/app";
+import React, { useContext } from "react";
+
+interface IGitOpsModeTooltipWrapper {
+  position?: ITooltipWrapper["position"];
+  tipOffset?: ITooltipWrapper["tipOffset"];
+  renderChildren: (disableChildren?: boolean) => React.ReactNode;
+}
+
+const baseClass = "gitops-mode-tooltip-wrapper";
+
+const GitOpsModeTooltipWrapper = ({
+  position,
+  tipOffset,
+  renderChildren,
+}: IGitOpsModeTooltipWrapper) => {
+  const { config } = useContext(AppContext);
+  const gomEnabled = config?.change_management.gitops_mode_enabled;
+  const repoURL = config?.change_management.repository_url;
+
+  if (!gomEnabled) {
+    return <>{renderChildren()}</>;
+  }
+
+  const tipContent = (
+    // at this point repoURL will always be defined
+    <>
+      {repoURL && (
+        <>
+          Manage in{" "}
+          <CustomLink newTab text="YAML" variant="tooltip-link" url={repoURL} />
+          <br />
+        </>
+      )}
+      (GitOps mode enabled)
+    </>
+  );
+
+  return (
+    <TooltipWrapper
+      className={baseClass}
+      position={position}
+      tipOffset={tipOffset}
+      tipContent={tipContent}
+      underline={false}
+      showArrow
+    >
+      {renderChildren(true)}
+    </TooltipWrapper>
+  );
+};
+
+export default GitOpsModeTooltipWrapper;
