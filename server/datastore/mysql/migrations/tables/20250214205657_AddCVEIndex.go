dce256fc8f3b606eea487c1d5b80f1dc098e3a42
Jahziel Villasana-Espinoza <jahzielv@gmail.com>
2025-02-14 10:39:51 -0500

    chore: bump timestamp up

--- server/datastore/mysql/migrations/tables/20250214205657_AddCVEIndex.go
+++ server/datastore/mysql/migrations/tables/20250214205657_AddCVEIndex.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20250211205657, Down_20250211205657)
+	MigrationClient.AddMigration(Up_20250214205657, Down_20250214205657)
 }
 
-func Up_20250211205657(tx *sql.Tx) error {
+func Up_20250214205657(tx *sql.Tx) error {
 	_, err := tx.Exec(`ALTER TABLE software_cve ADD INDEX idx_software_cve_cve (cve);`)
 	if err != nil {
 		return fmt.Errorf("failed to add index to software_cve.cve: %w", err)
@@ -17,6 +17,6 @@ func Up_20250211205657(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20250211205657(tx *sql.Tx) error {
+func Down_20250214205657(tx *sql.Tx) error {
 	return nil
 }
