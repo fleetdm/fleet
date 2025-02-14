fb6fd0957caeff568e702e005f9ec08d94b19fd0
Victor Lyuboslavsky <victor@fleetdm.com>
2025-02-14 14:40:06 -0600

    Fix compile issue.

--- server/mdm/android/service/endpoint_utils.go
+++ server/mdm/android/service/endpoint_utils.go
@@ -23,47 +23,13 @@ import (
 type handlerFunc func(ctx context.Context, request interface{}, svc android.Service) fleet.Errorer
 
 func encodeResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
-	if e, ok := response.(fleet.Errorer); ok && e.Error() != nil {
-		endpoint_utils.EncodeError(ctx, e.Error(), w)
-		return nil
-	}
-
-	if e, ok := response.(statuser); ok {
-		w.WriteHeader(e.Status())
-		if e.Status() == http.StatusNoContent {
-			return nil
-		}
-	}
-
-	return json.MarshalWrite(w, response, jsontext.WithIndent("  "))
+	return endpoint_utils.EncodeCommonResponse(ctx, w, response,
+		func(w http.ResponseWriter, response interface{}) error {
+			return json.MarshalWrite(w, response, jsontext.WithIndent("  "))
+		},
+	)
 }
 
-// statuser allows response types to implement a custom
-// http success status - default is 200 OK
-type statuser interface {
-	Status() int
-}
-
-// makeDecoder creates a decoder for the type for the struct passed on. If the
-// struct has at least 1 json tag it'll unmarshall the body. If the struct has
-// a `url` tag with value list_options it'll gather fleet.ListOptions from the
-// URL (similarly for host_options, carve_options, user_options that derive
-// from the common list_options). Note that these behaviors do not work for embedded structs.
-//
-// Finally, any other `url` tag will be treated as a path variable (of the form
-// /path/{name} in the route's path) from the URL path pattern, and it'll be
-// decoded and set accordingly. Variables can be optional by setting the tag as
-// follows: `url:"some-id,optional"`.
-// The "list_options" are optional by default and it'll ignore the optional
-// portion of the tag.
-//
-// If iface implements the requestDecoder interface, it returns a function that
-// calls iface.DecodeRequest(ctx, r) - i.e. the value itself fully controls its
-// own decoding.
-//
-// If iface implements the bodyDecoder interface, it calls iface.DecodeBody
-// after having decoded any non-body fields (such as url and query parameters)
-// into the struct.
 func makeDecoder(iface interface{}) kithttp.DecodeRequestFunc {
 	return endpoint_utils.MakeDecoder(iface, func(body io.Reader, req any) error {
 		return json.UnmarshalRead(body, req)
