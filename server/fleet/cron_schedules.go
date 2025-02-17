cf076d35e224a58748fcee5003575c32104ca788
Jahziel Villasana-Espinoza <jahzielv@gmail.com>
2025-02-17 12:13:14 -0500

    fix: update comment

--- server/fleet/cron_schedules.go
+++ server/fleet/cron_schedules.go
@@ -29,7 +29,7 @@ const (
 	CronUninstallSoftwareMigration  CronScheduleName = "uninstall_software_migration"
 	CronMaintainedApps              CronScheduleName = "maintained_apps"
 	// CronRefreshVPPAppVersions updates the versions of VPP apps in Fleet to the latest value. Runs
-	// every 24h.
+	// every 1h.
 	CronRefreshVPPAppVersions CronScheduleName = "refresh_vpp_app_versions"
 )
 
