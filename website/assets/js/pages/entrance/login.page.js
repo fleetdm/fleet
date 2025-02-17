32e280359d71723ac02b2d097f68de0bcf850d62
Eric <eashaw@sailsjs.com>
2025-02-17 13:11:26 -0600

    Redirect users who login for support to contact form

--- website/assets/js/pages/entrance/login.page.js
+++ website/assets/js/pages/entrance/login.page.js
@@ -47,7 +47,7 @@ parasails.registerPage('login', {
     // If we're redirecting this user to the contact form after they log in, modify the link to the /register page and the pageToRedirectToAfterLogin.
     if(window.location.hash && window.location.hash === '#contact'){
       this.registerSlug = '/register';
-      this.pageToRedirectToAfterLogin = '/contact';
+      this.pageToRedirectToAfterLogin = '/contact?sendMessage';
       window.location.hash = '';
     }
   },
