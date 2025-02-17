ae354acaf3e74a468f6b7944d0db88b909c9bd7e
Martin Angers <martin.n.angers@gmail.com>
2025-02-17 12:56:55 -0500

    Work on existing upcoming activities migration

---
+++ server/datastore/mysql/migrations/tables/20250217093329_MigratePendingUpcomingActivities.go
@@ -0,0 +1,251 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20250217093329, Down_20250217093329)
+}
+
+func Up_20250217093329(tx *sql.Tx) error {
+	// this migration inserts pending software installs, software uninstalls,
+	// VPP app installs and script executions in the upcoming_activities table
+	// (inserts them already marked as "activated" since they are ready to be
+	// processed). There is no ordering guarantee for those already-pending
+	// activities, but any new upcoming activity will follow the unified queue
+	// order.
+	if err := migrateSoftwareInstalls(tx); err != nil {
+		return err
+	}
+	if err := migrateSoftwareUninstalls(tx); err != nil {
+		return err
+	}
+	if err := migrateVPPInstalls(tx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func migrateSoftwareInstalls(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+INSERT INTO upcoming_activities
+	(
+		host_id,
+		priority,
+		user_id,
+		fleet_initiated,
+		activity_type,
+		execution_id,
+		payload,
+		activated_at
+	)
+SELECT
+	hsi.host_id,
+	0,
+	hsi.user_id,
+	hsi.policy_id IS NOT NULL, -- true if fleet-initiated
+	'software_install',
+	hsi.execution_id,
+	JSON_OBJECT(
+		'self_service', hsi.self_service,
+		'installer_filename', hsi.installer_filename,
+		'version', hsi.version,
+		'software_title_name', hsi.software_title_name,
+		'user', (SELECT JSON_OBJECT('name', name, 'email', email, 'gravatar_url', gravatar_url) FROM users WHERE id = hsi.user_id)
+	),
+	hsi.created_at
+FROM
+	host_software_installs hsi
+	LEFT OUTER JOIN upcoming_activities ua
+		ON hsi.execution_id = ua.execution_id
+WHERE
+	ua.id IS NULL AND -- ensure no duplicates in case the migration needs to re-run after failure
+	hsi.status = 'pending_install'
+`)
+	if err != nil {
+		return fmt.Errorf("failed to insert pending software installs: %w", err)
+	}
+
+	_, err = tx.Exec(`
+INSERT INTO software_install_upcoming_activities
+	(
+		upcoming_activity_id,
+		software_installer_id,
+		policy_id,
+		software_title_id,
+		created_at
+	)
+SELECT
+	ua.id,
+	hsi.software_installer_id,
+	hsi.policy_id,
+	hsi.software_title_id,
+	hsi.created_at
+FROM
+	upcoming_activities ua
+	INNER JOIN host_software_installs hsi
+		ON hsi.execution_id = ua.execution_id
+	LEFT OUTER JOIN software_install_upcoming_activities sia
+		ON sia.upcoming_activity_id = ua.id
+WHERE
+	ua.activity_type = 'software_install' AND
+	hsi.status = 'pending_install' AND
+	sia.upcoming_activity_id IS NULL -- ensure no duplicates in case the migration needs to re-run after failure
+`)
+	if err != nil {
+		return fmt.Errorf("failed to insert pending software installs secondary table: %w", err)
+	}
+	return nil
+}
+
+func migrateSoftwareUninstalls(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+INSERT INTO upcoming_activities
+	(
+		host_id,
+		priority,
+		user_id,
+		fleet_initiated,
+		activity_type,
+		execution_id,
+		payload,
+		activated_at
+	)
+SELECT
+	hsi.host_id,
+	0,
+	hsi.user_id,
+	hsi.policy_id IS NOT NULL, -- true if fleet-initiated
+	'software_uninstall',
+	hsi.execution_id,
+	JSON_OBJECT(
+		'installer_filename', '',
+		'version', 'unknown',
+		'software_title_name', hsi.software_title_name,
+		'user', (SELECT JSON_OBJECT('name', name, 'email', email, 'gravatar_url', gravatar_url) FROM users WHERE id = hsi.user_id)
+	),
+	hsi.created_at
+FROM
+	host_software_installs hsi
+	LEFT OUTER JOIN upcoming_activities ua
+		ON hsi.execution_id = ua.execution_id
+WHERE
+	ua.id IS NULL AND -- ensure no duplicates in case the migration needs to re-run after failure
+	hsi.status = 'pending_uninstall'
+`)
+	if err != nil {
+		return fmt.Errorf("failed to insert pending software uninstalls: %w", err)
+	}
+
+	_, err = tx.Exec(`
+INSERT INTO software_install_upcoming_activities
+	(
+		upcoming_activity_id,
+		software_installer_id,
+		software_title_id,
+		created_at
+	)
+SELECT
+	ua.id,
+	hsi.software_installer_id,
+	hsi.software_title_id,
+	hsi.created_at
+FROM
+	upcoming_activities ua
+	INNER JOIN host_software_installs hsi
+		ON hsi.execution_id = ua.execution_id
+	LEFT OUTER JOIN software_install_upcoming_activities sia
+		ON sia.upcoming_activity_id = ua.id
+WHERE
+	ua.activity_type = 'software_uninstall' AND
+	hsi.status = 'pending_uninstall' AND
+	sia.upcoming_activity_id IS NULL -- ensure no duplicates in case the migration needs to re-run after failure
+`)
+	if err != nil {
+		return fmt.Errorf("failed to insert pending software uninstalls secondary table: %w", err)
+	}
+	return nil
+}
+
+func migrateVPPInstalls(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+INSERT INTO upcoming_activities
+	(
+		host_id,
+		priority,
+		user_id,
+		fleet_initiated,
+		activity_type,
+		execution_id,
+		payload,
+		activated_at
+	)
+SELECT
+	hvi.host_id,
+	0,
+	hvi.user_id,
+	hvi.policy_id IS NOT NULL, -- true if fleet-initiated
+	'vpp_app_install',
+	hvi.command_uuid,
+	JSON_OBJECT(
+		'self_service', hvi.self_service,
+		'associated_event_id', hvi.associated_event_id,
+		'user', (SELECT JSON_OBJECT('name', name, 'email', email, 'gravatar_url', gravatar_url) FROM users WHERE id = hvi.user_id)
+	),
+	COALESCE(hvi.created_at, NOW(6))
+FROM
+	host_vpp_software_installs hvi
+	INNER JOIN
+		nano_view_queue nvq ON nvq.command_uuid = hvi.command_uuid
+	LEFT OUTER JOIN upcoming_activities ua
+		ON hvi.command_uuid = ua.execution_id
+WHERE
+	ua.id IS NULL AND -- ensure no duplicates in case the migration needs to re-run after failure
+	nvq.status IS NULL AND
+	hvi.removed = 0
+`)
+	if err != nil {
+		return fmt.Errorf("failed to insert pending vpp app installs: %w", err)
+	}
+
+	_, err = tx.Exec(`
+INSERT INTO vpp_app_upcoming_activities
+	(
+		upcoming_activity_id,
+		adam_id,
+		platform,
+		policy_id,
+		created_at
+	)
+SELECT
+	ua.id,
+	hvi.adam_id,
+	hvi.platform,
+	hvi.policy_id,
+	hvi.created_at
+FROM
+	upcoming_activities ua
+	INNER JOIN host_vpp_software_installs hvi
+		ON hvi.command_uuid = ua.execution_id
+	INNER JOIN
+		nano_view_queue nvq ON nvq.command_uuid = hvi.command_uuid
+	LEFT OUTER JOIN vpp_app_upcoming_activities vaua
+		ON vaua.upcoming_activity_id = ua.id
+WHERE
+	ua.activity_type = 'vpp_app_install' AND
+	hvi.removed = 0 AND
+	nvq.status IS NULL AND
+	vaua.upcoming_activity_id IS NULL -- ensure no duplicates in case the migration needs to re-run after failure
+`)
+	if err != nil {
+		return fmt.Errorf("failed to insert pending vpp app installs secondary table: %w", err)
+	}
+	return nil
+}
+
+func Down_20250217093329(tx *sql.Tx) error {
+	return nil
+}
