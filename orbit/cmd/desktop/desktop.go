d37d1ee190101c5f1460ece03eeea2e1268fd1bd
Scott Gress <scott@fleetdm.com>
2025-02-21 12:18:47 -0600

    add conditional for linux

--- orbit/cmd/desktop/desktop.go
+++ orbit/cmd/desktop/desktop.go
@@ -480,20 +480,23 @@ func main() {
 	// Check for the system tray icon periodically and kill the process if it's missing,
 	// forcing the parent to restart it. This may happen if a Linux display manager
 	// is restarted.
-	go func() {
-		checkTrayIconTicker := time.NewTicker(5 * time.Second)
+	if runtime.GOOS == "linux" {
+		log.Debug().Msg("starting tray icon checker")
+		go func() {
+			checkTrayIconTicker := time.NewTicker(5 * time.Second)
 
-		for {
-			<-checkTrayIconTicker.C
-			if !trayIconExists() {
-				log.Warn().Msg("System tray icon missing, exiting...")
-				// Cleanly stop systray.
-				systray.Quit()
-				// Exit to trigger restart.
-				os.Exit(75)
+			for {
+				<-checkTrayIconTicker.C
+				if !trayIconExists() {
+					log.Warn().Msg("system tray icon missing, exiting")
+					// Cleanly stop systray.
+					systray.Quit()
+					// Exit to trigger restart.
+					os.Exit(75)
+				}
 			}
-		}
-	}()
+		}()
+	}
 
 	systray.Run(onReady, onExit)
 }
