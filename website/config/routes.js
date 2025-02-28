bf276cdfcd6842d8266d48c62fa821a064189bee
Rachael Shaw <r@rachael.wtf>
2025-02-28 13:08:16 -0600

    Fix GitOps mode 404 (#26718)
    Add redirect for https://fleetdm.com/learn-more-about/ui-gitops-mode

--- website/config/routes.js
+++ website/config/routes.js
@@ -871,6 +871,7 @@ module.exports.routes = {
   'GET /learn-more-about/end-user-authentication': '/guides/macos-setup-experience#end-user-authentication-and-end-user-license-agreement-eula',
   'GET /learn-more-about/policy-templates': '/policies',
   'GET /learn-more-about/windows-mdm': '/guides/windows-mdm-setup',
+  'GET /learn-more-about/ui-gitops-mode': 'https://github.com/fleetdm/fleet-gitops/?tab=readme-ov-file#fleet-ui',
 
   // Sitemap
   // =============================================================================================================
