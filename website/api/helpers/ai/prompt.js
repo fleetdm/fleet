9a187a3291db00a94a1f40ecb83b946f4a05d54a
Eric <eashaw@sailsjs.com>
2025-02-28 08:47:21 -0600

    update prompt helper to support systemPrompts

--- website/api/helpers/ai/prompt.js
+++ website/api/helpers/ai/prompt.js
@@ -9,9 +9,9 @@ module.exports = {
 
   inputs: {
     prompt: { type: 'string', required: true, example: 'Who is running macOS 15?' },
-    systemPrompt: { type: 'string', example: 'Who is running macOS 15?' },
     baseModel: { type: 'string', defaultsTo: 'gpt-3.5-turbo', isIn: ['gpt-3.5-turbo', 'gpt-4o', 'o1-preview', 'o3-mini-2025-01-31', 'gpt-4o-2024-08-06'] },
     expectJson: { type: 'boolean', defaultsTo: false },
+    systemPrompt: { type: 'string', example: 'Who is running macOS 15?' },
   },
 
 
@@ -39,9 +39,16 @@ module.exports = {
 
 Please do not add any text outside of the JSON report or wrap it in a code fence.  Never use newline characters within double quotes.`;
     let messages = [];
+    // If a systemPrompt was provided, add that as the first item in the messages array.
     if(systemPrompt){
-      messages.push({ role: 'system', content: systemPrompt });
+      // If the specified baseModel does not support system prompts, log a wanring and ignore the system prompt.
+      if(['o1-preview', 'o3-mini-2025-01-31'].includes(baseModel)){
+        sails.log.warn(`Warning: the prompt helper recieved a system prompt input, but the specified baseModel (${baseModel}) does not support a system prompt. This input will be ignored in this LLM generation, please remove the system prompt or use a different base model.`);
+      } else {
+        messages.push({ role: 'system', content: systemPrompt });
+      }
     }
+    // Add the user prompt to the messages.
     messages.push({role: 'user', content: prompt+(expectJson? JSON_PROMPT_SUFFIX : '')});
     // [?] API: https://platform.openai.com/docs/api-reference/chat/create
     let openAiResponse = await sails.helpers.http.post('https://api.openai.com/v1/chat/completions', {
@@ -53,7 +60,7 @@ Please do not add any text outside of the JSON report or wrap it in a code fence
     .intercept((err)=>{
       return new Error(failureMessage+'  Error details from LLM: '+err.stack);
     });
-    console.log(openAiResponse);
+
     let rawResult = openAiResponse.choices[0].message.content;
     if (!expectJson) {
       return rawResult;
