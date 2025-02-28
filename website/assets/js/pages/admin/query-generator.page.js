275f17716a9033b5b2e85f5e0cf6d06515ea599d
Eric <eashaw@sailsjs.com>
2025-02-28 17:56:50 -0600

    update query generator page

--- website/assets/js/pages/admin/query-generator.page.js
+++ website/assets/js/pages/admin/query-generator.page.js
@@ -36,27 +36,10 @@ parasails.registerPage('query-generator', {
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function() {
-    // this.generatedQueries = {
-    //   "macOSQuery": "SELECT name, uuid, version, path, publisher, publisher_id, installed_at, prerelease, uid FROM vscode_extensions;",
-    //   "windowsQuery": "SELECT name, uuid, version, path, publisher, publisher_id, installed_at, prerelease, uid FROM vscode_extensions;",
-    //   "linuxQuery": "SELECT name, uuid, version, path, publisher, publisher_id, installed_at, prerelease, uid FROM vscode_extensions;",
-    //   "chromeOSQuery": "",
-    //   "macOSCaveats": "",
-    //   "windowsCaveats": "",
-    //   "linuxCaveats": "",
-    //   "chromeOSCaveats": "The vscode_extensions table is not available on ChromeOS."
-    // }
-    // this.showGeneratedQuery = true;
+    //
   },
   mounted: async function() {
-    $('[purpose="copy-button"]').on('click', async function() {
-      let code = $(this).closest('[purpose="codeblock"]').find('pre:visible code').text();
-      $(this).addClass('copied');
-      await setTimeout(()=>{
-        $(this).removeClass('copied');
-      }, 2000);
-      navigator.clipboard.writeText(code);
-    });
+    //
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -68,18 +51,6 @@ parasails.registerPage('query-generator', {
       io.socket.get('/api/v1/query-generator/get-llm-generated-sql', {naturalLanguageQuestion: this.formData.naturalLanguageQuestion}, ()=>{});
       io.socket.on('queryGenerated', this._onQueryResultsReturned);
       io.socket.on('error', this._onQueryGenerationError);
-    },
-    _onQueryResultsReturned: function(response) {
-      this.generatedQueries = response.result;
-      this.syncing = false;
-      this.showGeneratedQuery = true;
-      // Disable the socket event listener after we display the results.
-      io.socket.off('queryGenerated', this._onQueryResultsReturned);
-    },
-    _onQueryGenerationError: function(response) {
-      this.cloudError = response.error;
-      this.syncing = false;
-      io.socket.off('error', this._onQueryGenerationError);
     },
     clickResetQueryGenerator: function() {
       this.showGeneratedQuery = false;
@@ -95,5 +66,40 @@ parasails.registerPage('query-generator', {
       };
       this.formData.naturalLanguageQuestion = '';
     },
+    _onQueryResultsReturned: function(response) {
+      this.generatedQueries = response.result;
+      if(response.result.macOSQuery) {
+        this.selectedTab = 'macos'
+      } else if(!response.result.macOSQuery){
+        this.selectedTab = 'windows';
+      } else if(!response.result.windowsQuery){
+        this.selectedTab = 'linux';
+      } else if(!response.result.linuxQuery) {
+        this.selectedTab = 'chromeos';
+      } else if(!response.result.chromeOSQuery) {
+        this.selectedTab = 'macos';
+      }
+      this.syncing = false;
+      this.showGeneratedQuery = true;
+      this._setupCopyButtonEventListener();
+      // Disable the socket event listener after we display the results.
+      io.socket.off('queryGenerated', this._onQueryResultsReturned);
+    },
+    _onQueryGenerationError: function(response) {
+      this.cloudError = response.error;
+      this.syncing = false;
+      io.socket.off('error', this._onQueryGenerationError);
+    },
+    _setupCopyButtonEventListener: function() {
+      $('[purpose="copy-button"]').on('click', async function() {
+        let code = $(this).closest('[purpose="codeblock"]').find('pre:visible code').text();
+        $(this).addClass('copied');
+        await setTimeout(()=>{
+          $(this).removeClass('copied');
+        }, 2000);
+        navigator.clipboard.writeText(code);
+      });
+    }
+
   }
 });
