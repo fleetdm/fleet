0c0757946bc7211dca194c772f95b83df1b0202f
Victor Lyuboslavsky <victor@fleetdm.com>
2025-03-03 16:20:53 -0600

    Removing skipped test.

--- server/vulnerabilities/nvd/cve_test.go
+++ server/vulnerabilities/nvd/cve_test.go
@@ -609,7 +609,6 @@ func TestTranslateCPEToCVE(t *testing.T) {
 	}
 
 	t.Run("find_vulns_on_cpes", func(t *testing.T) {
-		t.Skip("Skipping test due to flakiness. See https://github.com/fleetdm/fleet/issues/26748")
 		t.Parallel()
 
 		ds := new(mock.Store)
