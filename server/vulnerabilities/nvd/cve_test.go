3fa619fe1030a34aacc6a6a28885d758bb06aa91
Victor Lyuboslavsky <victor@fleetdm.com>
2025-03-03 09:07:51 -0600

    Undo skip failing test after merge

--- server/vulnerabilities/nvd/cve_test.go
+++ server/vulnerabilities/nvd/cve_test.go
@@ -592,7 +592,6 @@ func TestTranslateCPEToCVE(t *testing.T) {
 	}
 
 	t.Run("find_vulns_on_cpes", func(t *testing.T) {
-		t.Skip("Skipping failing test: https://github.com/fleetdm/fleet/issues/26748")
 		t.Parallel()
 
 		ds := new(mock.Store)
