1d69c6652e5db9d9dd0b0516ba64db03e35b489c
Jahziel Villasana-Espinoza <jahziel@fleetdm.com>
2025-03-03 16:50:25 -0500

    added new vulns and continuesToUpdate to python alpha version cve tests (#26785)
    > For #26748
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    <!-- Note that API documentation changes are now addressed by the
    product design team. -->
    
    - [x] Added/updated automated tests
    - [x] Manual QA for all new/changed functionality

--- server/vulnerabilities/nvd/cve_test.go
+++ server/vulnerabilities/nvd/cve_test.go
@@ -499,7 +499,12 @@ func TestTranslateCPEToCVE(t *testing.T) {
 					ID:                "CVE-2024-9287",
 					resolvedInVersion: "",
 				},
+				{
+					ID:                "CVE-2025-0938",
+					resolvedInVersion: "3.14.0a5",
+				},
 			},
+			continuesToUpdate: true,
 		},
 		"cpe:2.3:a:python:python:3.14.0:alpha2:*:*:*:macos:*:*": {
 			includedCVEs: []cve{
@@ -507,10 +512,22 @@ func TestTranslateCPEToCVE(t *testing.T) {
 					ID:                "CVE-2024-12254",
 					resolvedInVersion: "3.14.0a3",
 				},
+				{
+					ID:                "CVE-2025-0938",
+					resolvedInVersion: "3.14.0a5",
+				},
 			},
+			continuesToUpdate: true,
 		},
 		"cpe:2.3:a:python:python:3.14.0:alpha3:*:*:*:macos:*:*": {
 			excludedCVEs: []string{"CVE-2024-12254"},
+			includedCVEs: []cve{
+				{
+					ID:                "CVE-2025-0938",
+					resolvedInVersion: "3.14.0a5",
+				},
+			},
+			continuesToUpdate: true,
 		},
 	}
 
