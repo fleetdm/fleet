5ca94d61edc591a6f11f601fb82433abbc169d02
Gabriel Hernandez <ghernandez345@gmail.com>
2025-03-04 16:32:44 +0000

    add self closing successful Android enterprise connection page (#26767)
    For #26736
    
    adds a self closing page that is the final part of connecting with
    Android Enterprise. This allows the user to see the fleet page
    notification that android mdm is now enabled.

--- server/mdm/android/service/service.go
+++ server/mdm/android/service/service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
 	"net/http"
 	"strings"
@@ -137,10 +138,24 @@ type enterpriseSignupCallbackRequest struct {
 	EnterpriseToken string `query:"enterpriseToken"`
 }
 
+type enterpriseSignupCallbackResponse struct {
+	Err error `json:"error,omitempty"`
+}
+
+func (res enterpriseSignupCallbackResponse) Error() error { return res.Err }
+
+//go:embed enterpriseCallback.html
+var enterpriseCallbackHTML []byte
+
+func (res enterpriseSignupCallbackResponse) HijackRender(ctx context.Context, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	_, _ = w.Write(enterpriseCallbackHTML)
+}
+
 func enterpriseSignupCallbackEndpoint(ctx context.Context, request interface{}, svc android.Service) fleet.Errorer {
 	req := request.(*enterpriseSignupCallbackRequest)
 	err := svc.EnterpriseSignupCallback(ctx, req.ID, req.EnterpriseToken)
-	return android.DefaultResponse{Err: err}
+	return enterpriseSignupCallbackResponse{Err: err}
 }
 
 func (svc *Service) EnterpriseSignupCallback(ctx context.Context, id uint, enterpriseToken string) error {
