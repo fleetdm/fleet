72b3ffd104347d26f2a03ea64f1cf3bee11bc840
Gabriel Hernandez <ghernandez345@gmail.com>
2025-03-07 16:28:03 +0000

    get cert auth data from config from ui list item

--- frontend/pages/admin/IntegrationsPage/cards/Certificates/helpers.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/Certificates/helpers.tsx
@@ -5,6 +5,7 @@ import {
 } from "interfaces/integration";
 
 export interface ICertAuthority {
+  id: string;
   name: string;
   certificateAuthority: string;
 }
@@ -19,6 +20,7 @@ export const generateListData = (
   // these values for the certificateAuthority is meant to be a hard coded .
   if (ncspProxy) {
     certs.push({
+      id: "ndes", // only ever one  NDES so no need to make the id dynamic
       name: "NDES",
       certificateAuthority:
         "Microsoft Network Device Enrollment Service (NDES)",
@@ -28,6 +30,7 @@ export const generateListData = (
   if (digicertCerts?.length) {
     digicertCerts.forEach((cert) => {
       certs.push({
+        id: `digicert-${cert.id}`,
         name: cert.name,
         certificateAuthority: "DigiCert",
       });
@@ -37,6 +40,7 @@ export const generateListData = (
   if (customProxies?.length) {
     customProxies.forEach((cert) => {
       certs.push({
+        id: `custom-scep-proxy-${cert.id}`,
         name: cert.name,
         certificateAuthority:
           "Custom Simple Certificate Enrollment Protocol (SCEP)",
@@ -47,4 +51,29 @@ export const generateListData = (
   return certs.sort((a, b) =>
     a.name.toLowerCase().localeCompare(b.name.toLocaleLowerCase())
   );
+};
+
+export const getCertificateAuthority = (
+  id: string,
+  ncspProxy?: IScepIntegration | null,
+  digicertCerts?: IDigicertCertificate[],
+  customProxies?: ICustomScepProxy[]
+) => {
+  if (id === "ndes") {
+    return ncspProxy as IScepIntegration;
+  }
+
+  if (id.includes("digicert")) {
+    return digicertCerts?.find(
+      (cert) => id.split("digicert-")[1] === cert.id.toString()
+    );
+  }
+
+  if (id.includes("custom-scep-proxy")) {
+    return customProxies?.find(
+      (cert) => id.split("custom-scep-proxy-")[1] === cert.id.toString()
+    );
+  }
+
+  return null;
 };
