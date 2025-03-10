5beeddd4b6d3b7b3e11aeb9cac297f541fdb41b6
Noah Talerman <noahtal@umich.edu>
2025-03-10 09:30:45 -0400

    Add script to dogfood

---
+++ it-and-security/lib/windows/scripts/create-admin-user.ps1
@@ -0,0 +1,11 @@
+$Username = "IT admin"
+$Password = ConvertTo-SecureString "StrongPassword123!" -AsPlainText -Force
+
+# Create the local user account
+New-LocalUser -Name $Username -Password $Password -FullName "Fleet IT admin" 
+-Description "Admin account used to login when the end user forgets their 
+password or the host is returned to Fleet." 
+-AccountNeverExpires
+
+# Add the user to the Administrators group
+Add-LocalGroupMember -Group "Administrators" -Member $Username
