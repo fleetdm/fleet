f4db8ea30345c5e8f9a0bd2ed2795886aac684ce
Dan Tsekhanskiy <28414793+TsekNet@users.noreply.github.com>
2025-03-15 02:03:23 +0000

    Ignore comments at the top of XML files

--- server/fleet/windows_mdm_test.go
+++ server/fleet/windows_mdm_test.go
@@ -411,7 +411,9 @@ func TestValidateUserProvided(t *testing.T) {
 			profile: MDMWindowsConfigProfile{
 				SyncML: []byte(`
 				  <!-- this is a comment -->
+					<!-- this is another comment -->
 				  <Replace>
+					<!-- this is a comment inside replace -->
 				    <Target>
 				      <LocURI>Custom/URI</LocURI>
 				    </Target>
