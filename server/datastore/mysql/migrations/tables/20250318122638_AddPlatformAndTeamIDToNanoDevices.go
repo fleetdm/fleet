02180beaa261b2a64eb623b4336a128ae574c40c
Martin Angers <martin.n.angers@gmail.com>
2025-03-18 14:55:04 -0400

    Add platform and team id to nano devices table

---
+++ server/datastore/mysql/migrations/tables/20250318122638_AddPlatformAndTeamIDToNanoDevices.go
@@ -0,0 +1,27 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20250318122638, Down_20250318122638)
+}
+
+func Up_20250318122638(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+ALTER TABLE nano_devices
+	ADD COLUMN platform VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+	ADD COLUMN enroll_team_id INT UNSIGNED DEFAULT NULL,
+	ADD CONSTRAINT fk_nano_devices_team_id FOREIGN KEY (enroll_team_id) REFERENCES teams (id) ON DELETE SET NULL
+`)
+	if err != nil {
+		return fmt.Errorf("failed to alter nano_devices: %w", err)
+	}
+	return nil
+}
+
+func Down_20250318122638(tx *sql.Tx) error {
+	return nil
+}
