e6cb16453ec51d8fad52d10f90885c75ec75d67d
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2025-03-20 11:09:57 -0300

    Added more logging for troubleshooting of software package installation (#27291)
    For #27234.
    
    - Improved logging in orbit to help us during troubleshooting.
    - Added some documentation on how to grep for errors related to software
    package installation in orbit.
    - Added `took` to server request error logs (it was only present when
    the request succeeds).
    
    ---
    
    - [X] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/Committing-Changes.md#changes-files)
    for more information.
    - [x] A detailed QA plan exists on the associated ticket (if it isn't
    there, work with the product group's QA engineer to add it)
    - [X] Manual QA for all new/changed functionality
    - For Orbit and Fleet Desktop changes:
    - [x] Make sure fleetd is compatible with the latest released version of
    Fleet (see [Must
    rule](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/fleetd-development-and-release-strategy.md)).
    - [x] Orbit runs on macOS, Linux and Windows. Check if the orbit
    feature/bugfix should only apply to one platform (`runtime.GOOS`).
    - [x] Manual QA must be performed in the three main OSs, macOS, Windows
    and Linux.
    - [x] Auto-update manual QA, from released version of component to new
    version (see [tools/tuf/test](../tools/tuf/test/README.md)).

--- orbit/pkg/installer/installer_test.go
+++ orbit/pkg/installer/installer_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/fleetdm/fleet/v4/server/ptr"
 	osquery_gen "github.com/osquery/osquery-go/gen/osquery"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -27,11 +28,11 @@ type TestOrbitClient struct {
 	saveInstallerResultFn      func(*fleet.HostSoftwareInstallResultPayload) error
 }
 
-func (oc *TestOrbitClient) DownloadSoftwareInstallerFromURL(url string, filename string, downloadDir string) (string, error) {
+func (oc *TestOrbitClient) DownloadSoftwareInstallerFromURL(url string, filename string, downloadDir string, progressFunc func(int)) (string, error) {
 	return oc.downloadInstallerFromURLFn(url, filename, downloadDir)
 }
 
-func (oc *TestOrbitClient) DownloadSoftwareInstaller(installerID uint, downloadDir string) (string, error) {
+func (oc *TestOrbitClient) DownloadSoftwareInstaller(installerID uint, downloadDir string, progressFunc func(int)) (string, error) {
 	return oc.downloadInstallerFn(installerID, downloadDir)
 }
 
@@ -730,7 +731,7 @@ func TestScriptsDisabled(t *testing.T) {
 	}
 	oc.getInstallerDetailsFn = getInstallerDetailsDefaultFn
 
-	out, err := r.installSoftware(context.Background(), "1")
+	out, err := r.installSoftware(context.Background(), "1", log.With().Logger())
 	require.NoError(t, err)
 	require.EqualValues(t, &fleet.HostSoftwareInstallResultPayload{
 		InstallUUID:               "1",
