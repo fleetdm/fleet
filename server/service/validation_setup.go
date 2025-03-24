66567e1c3a8cc922bede4c05f6caa442b3f37cc9
Jacob Shandling <jacob@fleetdm.com>
2025-03-24 10:31:21 -0700

    clean up server check

--- server/service/validation_setup.go
+++ server/service/validation_setup.go
@@ -9,30 +9,11 @@ import (
 
 func (mw validationMiddleware) NewAppConfig(ctx context.Context, payload fleet.AppConfig) (*fleet.AppConfig, error) {
 	invalid := &fleet.InvalidArgumentError{}
-	var serverURLString string
 	if payload.ServerSettings.ServerURL == "" {
 		invalid.Append("server_url", "missing required argument")
-	} else {
-		serverURLString = cleanupURL(payload.ServerSettings.ServerURL)
-	}
-	if err := validateServerURL(serverURLString); err != nil {
-		invalid.Append("server_url", err.Error())
 	}
 	if invalid.HasErrors() {
 		return nil, ctxerr.Wrap(ctx, invalid)
 	}
 	return mw.Service.NewAppConfig(ctx, payload)
-}
-
-func validateServerURL(urlString string) error {
-	// serverURL, err := url.Parse(urlString)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// if serverURL.Scheme != "https" && !strings.Contains(serverURL.Host, "localhost") {
-	// 	return errors.New("url scheme must be https")
-	// }
-
-	return nil
 }
