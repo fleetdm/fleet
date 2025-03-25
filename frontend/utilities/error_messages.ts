312e7b2e69dabb3a9f31ba0f37f0d99381dc07db
Jacob Shandling <jacob@fleetdm.com>
2025-03-25 14:23:42 -0700

    Update frontend error message to reflect actual validation conditions

--- frontend/utilities/error_messages.ts
+++ frontend/utilities/error_messages.ts
@@ -1,3 +1,3 @@
-const INVALID_SERVER_URL_MESSAGE = `Fleet server URL must use “https” or “http”.`;
+const INVALID_SERVER_URL_MESSAGE = `Fleet server address must be a valid “https” or “http” URL.`;
 
 export default INVALID_SERVER_URL_MESSAGE;
