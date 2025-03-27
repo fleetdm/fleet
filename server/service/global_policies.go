d72c3394b2d6f5bbfe2acbefc45b17168ecadaab
dantecatalfamo <dante.catalfamo@gmail.com>
2025-03-27 11:35:35 -0400

    Add parameters to endpoints

--- server/service/global_policies.go
+++ server/service/global_policies.go
@@ -25,13 +25,15 @@ import (
 /////////////////////////////////////////////////////////////////////////////////
 
 type globalPolicyRequest struct {
-	QueryID     *uint  `json:"query_id"`
-	Query       string `json:"query"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Resolution  string `json:"resolution"`
-	Platform    string `json:"platform"`
-	Critical    bool   `json:"critical" premium:"true"`
+	QueryID          *uint    `json:"query_id"`
+	Query            string   `json:"query"`
+	Name             string   `json:"name"`
+	Description      string   `json:"description"`
+	Resolution       string   `json:"resolution"`
+	Platform         string   `json:"platform"`
+	Critical         bool     `json:"critical" premium:"true"`
+	LabelsIncludeAny []string `json:"labels_include_any"`
+	LabelsExcludeAny []string `json:"labels_exclude_any"`
 }
 
 type globalPolicyResponse struct {
