96be402518cf24d8c2e40e992c2abd13d6664e73
Jacob Shandling <jacob@fleetdm.com>
2025-03-27 13:15:38 -0700

    updates

--- frontend/components/forms/validators/valid_url/valid_url.ts
+++ frontend/components/forms/validators/valid_url/valid_url.ts
@@ -9,19 +9,10 @@ interface IValidUrl {
   allowLocalHost?: boolean;
 }
 
-export default ({ url, protocols, allowLocalHost }: IValidUrl) => {
-  // this function also has a `require_valid_protocol` option, though as called it seems to already validate
-  // that the URL's protocol is one of those specified
-  if (allowLocalHost) {
-    if (
-      url.startsWith("http://localhost") ||
-      url.startsWith("https://localhost")
-    ) {
-      return true;
-    }
-  }
+export default ({ url, protocols, allowLocalHost = false }: IValidUrl) => {
   return isURL(url, {
     protocols,
     require_protocol: !!protocols?.length,
+    require_tld: !allowLocalHost,
   });
 };
