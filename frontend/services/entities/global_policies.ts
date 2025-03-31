44817d99f8caeed1925841a21e44ef9dd88df7e6
Scott Gress <scott@fleetdm.com>
2025-03-31 16:37:09 -0500

    back out stub

--- frontend/services/entities/global_policies.ts
+++ frontend/services/entities/global_policies.ts
@@ -55,15 +55,7 @@ export default {
     const { GLOBAL_POLICIES } = endpoints;
     const path = `${GLOBAL_POLICIES}/${id}`;
 
-    return sendRequest("GET", path).then((response) => {
-      return {
-        policy: {
-          ...response.policy,
-          labels_include_any: [{ id: 1, name: "Label Uno" }],
-          labels_exclude_any: [],
-        },
-      };
-    });
+    return sendRequest("GET", path);
   },
   loadAll: (): Promise<ILoadAllPoliciesResponse> => {
     const { GLOBAL_POLICIES } = endpoints;
