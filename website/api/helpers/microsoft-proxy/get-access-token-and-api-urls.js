2db8123fec64b8bb2c731678466c1589840c1247
Eric <eashaw@sailsjs.com>
2025-04-01 17:07:01 -0500

    Update get-access-token-and-api-urls.js

--- website/api/helpers/microsoft-proxy/get-access-token-and-api-urls.js
+++ website/api/helpers/microsoft-proxy/get-access-token-and-api-urls.js
@@ -31,7 +31,7 @@ module.exports = {
       return new Error(`No matching tenant record could be found with the specified ID. (${complianceTenantRecordId}`);
     }
 
-    // Get an access token for this
+    // Get an access token for this tenant
     let accessTokenResponse = await sails.helpers.http.sendHttpRequest.with({
       method: 'POST',
       url: `https://login.microsoftonline.com/${informationAboutThisTenant.entraTenantId}/oauth2/v2.0/token`,
@@ -43,18 +43,27 @@ module.exports = {
         grant_type: 'client_credentials'// eslint-disable-line camelcase
       }
     });
-    // TODO: return error if no access token.
-    let accessToken = accessTokenResponse.body.access_token;
+
+    // Parse the json response body to get the access token.
+    let accessToken;
+    try {
+      accessToken = JSON.parse(accessTokenResponse.body).access_token;
+    } catch(err){
+      throw new Error(`When sending a request to get an access token for a Microsoft compliance tenant, an error occured. full error: ${require('util').inspect(err)}`)
+    }
+
+    // (2025-04-01) TODO: test the code below once the test tenant has been configured to be able to use the Fleet compliance partner application.
+
     // [?]: https://learn.microsoft.com/en-us/graph/api/resources/serviceprincipal
     let servicePrincipalResponse = await sails.helpers.http.get.with({
-      url: `https://graph.microsoft.com/v1.0/servicePrincipals?$filter=${encodeURIComponent(`appId eq ${informationAboutThisTenant.entraTenantId}`)}`,
+      url: `https://graph.microsoft.com/v1.0/servicePrincipals?$filter=${encodeURIComponent(`appId eq '${sails.config.custom.compliancePartnerClientId}'`)}`,
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
     });
 
     // TODO: verify that the servicePrincipal object is not nested in the response from the Microsoft graph API.
-    let servicePrincipalObjectId = servicePrincipalResponse.id;
+    let servicePrincipalObjectId = servicePrincipalResponse.value[0].id;
 
     // [?]: https://learn.microsoft.com/en-us/graph/api/group-list-endpoints
     let servicePrincipalEndpointResponse = await sails.helpers.http.get.with({
@@ -63,11 +72,10 @@ module.exports = {
         'Authorization': `Bearer ${accessToken}`
       },
     });
-
     let endpointsInResponse = servicePrincipalEndpointResponse.value;
+    // TODO: throw errors if these endpoints are missing.
     let tenantDataSyncUrl = _.find(endpointsInResponse, {providerName: 'PartnerTenantDataSyncService'}).uri;
     let partnerCompliancePoliciesUrl = _.find(endpointsInResponse, {providerName: 'PartnerCompliancePolicies'}).uri;
-
 
     return {
       accessToken,
