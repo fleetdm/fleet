08aecb5f432508b5334d409fc398481119b51a4c
Jacob Shandling <jacob@fleetdm.com>
2025-04-01 13:22:47 -0700

    add ui phase-based infrastructure

--- frontend/pages/admin/IntegrationsPage/cards/ConditionalAccess/ConditionalAccess.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/ConditionalAccess/ConditionalAccess.tsx
@@ -1,5 +1,4 @@
-import React, { useContext, useState } from "react";
-import { InjectedRouter } from "react-router";
+import React, { useContext, useEffect, useState } from "react";
 
 import { size } from "lodash";
 
@@ -14,17 +13,24 @@ import InputField from "components/forms/fields/InputField";
 import CustomLink from "components/CustomLink";
 import SectionHeader from "components/SectionHeader";
 
-import { AppContext } from "context/app";
 import { LEARN_MORE_ABOUT_BASE_LINK } from "utilities/constants";
 import Button from "components/buttons/Button";
-import Spinner from "components/Spinner";
 import { IFormField } from "interfaces/form_field";
-import { IConfig } from "interfaces/config";
+import { AppContext } from "context/app";
+import Spinner from "components/Spinner";
+import PremiumFeatureMessage from "components/PremiumFeatureMessage";
 // import { getErrorReason } from "interfaces/errors";
 
 const baseClass = "conditional-access";
 
 const msetid = "microsoft_entra_tenant_id";
+
+// States –> UI phases:
+// 	- not premium –> -1
+// 	- no tenant id –> 0
+// 	- tenant id & form submitted –> 1
+// 	- tenant id & no consent –> 2
+//   - tenant id & consent –> 3
 
 interface IFormData {
   [msetid]: string;
@@ -45,21 +51,31 @@ const validate = (formData: IFormData) => {
 const ConditionalAccess = () => {
   const { renderFlash } = useContext(NotificationContext);
 
+  // UI phases:
+  // 	0: form (valid, loading, error)
+  // 	1: form submitted (aka, “continue in other tab”)
+  // 	2: checking integration
+  // 	3: integration confirmed
+  const [phase, setPhase] = useState(0);
+
   const [formData, setFormData] = useState<IFormData>({
+    // [msetid]: "12345",
     [msetid]: "",
   });
   const [formErrors, setFormErrors] = useState<IFormErrors>({});
   const [isUpdating, setIsUpdating] = useState(false);
 
-  // Redirect to /settings if not a cloud-managed Fleet instance. Must do this down at this level
-  // since it depends on config context
-  // if (!config.license?.managed_cloud) {
-  //   // return <>OOPS</>;
-  //   router.push(paths.ADMIN_SETTINGS);
-  // }
+  const { isPremiumTier, config } = useContext(AppContext);
 
-  // TODO - actually call API
-  setFormData({ [msetid]: "12345" });
+  // watch curMsetid coming from `config`, populate initial form state once present
+  const curMsetid = config?.conditional_access?.microsoft_entra_tenant_id;
+  useEffect(() => {
+    setFormData({ [msetid]: curMsetid || "" });
+  }, [curMsetid]);
+
+  if (!isPremiumTier) {
+    return <PremiumFeatureMessage />;
+  }
 
   const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
@@ -75,7 +91,7 @@ const ConditionalAccess = () => {
         formData[msetid]
       );
       setIsUpdating(false);
-      // TODO go to next step
+      setPhase(1);
     } catch (e) {
       // const message = getErrorReason(e);
       // renderFlash("error", message || "Failed to update settings");
@@ -109,54 +125,72 @@ const ConditionalAccess = () => {
     setFormErrors(validate(formData));
   };
 
-  const renderContent = () => {
-    return <>OOPS</>;
-    if (!formData[msetid]) {
-      return (
-        <form onSubmit={handleSubmit} autoComplete="off">
-          <InputField
-            label="Microsoft Entra tenant ID"
-            helpText={
-              <>
-                You can find this in your Microsoft Entra admin center.{" "}
-                <CustomLink
-                  url={`${LEARN_MORE_ABOUT_BASE_LINK}/microsoft-entra-setup`}
-                  text="Learn more"
-                  newTab
-                />
-              </>
-            }
-            onChange={onInputChange}
-            name={msetid}
-            value={formData[msetid]}
-            parseTarget
-            onBlur={onInputBlur}
-            error={formErrors[msetid]}
-          />
-          <Button
-            type="submit"
-            variant="brand"
-            disabled={!!size(formErrors)}
-            className="button-wrap"
-            isLoading={isUpdating}
-          >
-            Save
-          </Button>
-        </form>
-      );
-    }
-    return <div>TODO</div>;
-  };
+  const renderForm = () => (
+    <form onSubmit={handleSubmit} autoComplete="off">
+      <InputField
+        label="Microsoft Entra tenant ID"
+        helpText={
+          <>
+            You can find this in your Microsoft Entra admin center.{" "}
+            <CustomLink
+              url={`${LEARN_MORE_ABOUT_BASE_LINK}/microsoft-entra-setup`}
+              text="Learn more"
+              newTab
+            />
+          </>
+        }
+        onChange={onInputChange}
+        name={msetid}
+        value={formData[msetid]}
+        parseTarget
+        onBlur={onInputBlur}
+        error={formErrors[msetid]}
+      />
+      <Button
+        type="submit"
+        variant="brand"
+        disabled={!!size(formErrors)}
+        className="button-wrap"
+        isLoading={isUpdating}
+      >
+        Save
+      </Button>
+    </form>
+  );
 
-  return <>OOPs2</>;
+  const renderFormSubmitted = () => (
+    // TODO
+    <></>
+  );
+
+  const renderIntegrationConfirmed = () => (
+    // TODO
+    <></>
+  );
+
+  const renderContent = () => {
+    switch (phase) {
+      case 0:
+        return renderForm();
+      case 1:
+        return renderFormSubmitted();
+      case 2:
+        // checking integration
+        return <Spinner />;
+      case 3:
+        // TODO
+        return renderIntegrationConfirmed();
+      default:
+        return <Spinner />;
+    }
+  };
   return (
     <div className={baseClass}>
       <SectionHeader title="Conditional access" />
       <p className={`${baseClass}__page-description`}>
         Block hosts failing any policies from logging into third party apps.
-        Enable or disable on the
-        <CustomLink url={paths.MANAGE_POLICIES} text="Policies" />
-        page.
+        Enable or disable on the{" "}
+        <CustomLink url={paths.MANAGE_POLICIES} text="Policies" /> page.
       </p>
       {renderContent()}
     </div>
