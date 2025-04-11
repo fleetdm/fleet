51baaff9a79dd58947f13d4c8ef5a6b783ec909d
Eric <eashaw@sailsjs.com>
2025-04-11 12:36:54 -0500

    Create get-access-token.js

---
+++ website/api/helpers/android-enterprise/get-access-token.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+
+  friendlyName: 'Get access token',
+
+
+  description: '',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+    success: {
+      outputFriendlyName: 'Access token',
+    },
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // Get access token.
+    var accessToken;
+    // TODO
+
+    // Send back the result through the success exit.
+    return accessToken;
+
+  }
+
+
+};
+
