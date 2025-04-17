4bce683282f7e7dbf299fc5559550bc591ead9e5
Scott Gress <scott@fleetdm.com>
2025-04-16 22:06:10 -0500

    progress

--- cmd/fleetctl/generate_gitops.go
+++ cmd/fleetctl/generate_gitops.go
@@ -3,8 +3,24 @@ package main
 import (
 	"fmt"
 
+	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/urfave/cli/v2"
 )
+
+type SecretWarning struct {
+	Filename string
+	Path     string
+}
+
+type Note struct {
+	Filename string
+	Note     string
+}
+
+type Messages struct {
+	SecretWarnings []SecretWarning
+	Notes          []Note
+}
 
 func generateGitopsCommand() *cli.Command {
 	return &cli.Command{
@@ -12,10 +28,95 @@ func generateGitopsCommand() *cli.Command {
 		Usage:       "Generate GitOps configuration files for Fleet.",
 		Description: "This command generates GitOps configuration files for Fleet.",
 		Action:      generateGitopsAction,
+		Flags: []cli.Flag{
+			configFlag(),
+			contextFlag(),
+			debugFlag(),
+		},
 	}
 }
 
 func generateGitopsAction(c *cli.Context) error {
 	fmt.Println("Generating GitOps configuration files...")
+	// Check license
+	fleetClient, err := clientFromCLI(c)
+	if err != nil {
+		return err
+	}
+	appConfig, err := fleetClient.GetAppConfig()
+	if err != nil {
+		return err
+	}
+
+	messages := &Messages{}
+
+	orgSettings := generateOrgSettings(appConfig, messages)
+
+	fmt.Printf("App Config: %+v\n", (*orgSettings)["org_info"])
 	return nil
+}
+
+func generateOrgSettings(appConfig *fleet.EnrichedAppConfig, messages *Messages) *map[string]interface{} {
+	orgSettings := &map[string]interface{}{
+		"features":             appConfig.Features,
+		"fleet_desktop":        appConfig.FleetDesktop,
+		"host_expiry_settings": appConfig.HostExpirySettings,
+		"org_info":             appConfig.OrgInfo,
+		"secrets": []map[string]interface{}{
+			{
+				"secret": "# TODO: Add your secret here",
+			},
+		},
+		"server_settings":  appConfig.ServerSettings,
+		"sso_settings":     generateSSOSettings(appConfig.SSOSettings, messages),
+		"integrations":     generateIntegrations(&appConfig.Integrations, messages),
+		"webhook_settings": appConfig.WebhookSettings,
+		"mdm":              generateMDM(&appConfig.MDM, messages),
+		"yara_rules":       generateYaraRules(appConfig.YaraRules, messages),
+	}
+	return orgSettings
+}
+
+func generateSSOSettings(ssoSettings *fleet.SSOSettings, messages *Messages) map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func generateIntegrations(ssoSettings *fleet.Integrations, messages *Messages) map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func generateMDM(mdm *fleet.MDM, messages *Messages) map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func generateYaraRules(yaraRules []fleet.YaraRule, messages *Messages) map[string]interface{} {
+	return map[string]interface{}{}
+}
+
+func generateTeamSettings(teamID int) string {
+	return fmt.Sprintf("team_settings_%d.yaml", teamID)
+}
+
+func generateAgentOptions() string {
+	return "agent_options.yaml"
+}
+
+func generateControls() string {
+	return "controls.yaml"
+}
+
+func generatePolicies() string {
+	return "policies.yaml"
+}
+
+func generateQueries() string {
+	return "queries.yaml"
+}
+
+func generateSoftware() string {
+	return "software.yaml"
+}
+
+func generateLabels() string {
+	return "labels.yaml"
 }
