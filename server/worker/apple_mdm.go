89e62907c0b533f0aea1f42d0f16e27d8fb74593
Victor Lyuboslavsky <victor@fleetdm.com>
2025-04-21 17:09:01 -0500

    Hooked up config to fleetd install.

--- server/worker/apple_mdm.go
+++ server/worker/apple_mdm.go
@@ -111,14 +111,36 @@ func (a *AppleMDM) runPostManualEnrollment(ctx context.Context, args appleMDMArg
 }
 
 func (a *AppleMDM) runPostDEPEnrollment(ctx context.Context, args appleMDMArgs) error {
-	var awaitCmdUUIDs []string
+	var (
+		awaitCmdUUIDs []string
+		appCfg        *fleet.AppConfig
+		team          *fleet.Team
+	)
 
 	if isMacOS(args.Platform) {
-		fleetdCmdUUID, err := a.installFleetd(ctx, args.HostUUID)
-		if err != nil {
-			return ctxerr.Wrap(ctx, err, "installing post-enrollment packages")
+		var manualAgentInstall bool
+		var err error
+		if args.TeamID == nil {
+			appCfg, err = a.Datastore.AppConfig(ctx)
+			if err != nil {
+				return ctxerr.Wrap(ctx, err, "getting app config")
+			}
+			manualAgentInstall = appCfg.MDM.MacOSSetup.ManualAgentInstall.Value
+		} else {
+			team, err = a.Datastore.Team(ctx, *args.TeamID)
+			if err != nil {
+				return ctxerr.Wrap(ctx, err, "fetch team to send AccountConfiguration")
+			}
+			manualAgentInstall = team.Config.MDM.MacOSSetup.ManualAgentInstall.Value
 		}
-		awaitCmdUUIDs = append(awaitCmdUUIDs, fleetdCmdUUID)
+
+		if !manualAgentInstall {
+			fleetdCmdUUID, err := a.installFleetd(ctx, args.HostUUID)
+			if err != nil {
+				return ctxerr.Wrap(ctx, err, "installing post-enrollment packages")
+			}
+			awaitCmdUUIDs = append(awaitCmdUUIDs, fleetdCmdUUID)
+		}
 
 		bootstrapCmdUUID, err := a.installBootstrapPackage(ctx, args.HostUUID, args.TeamID)
 		if err != nil {
@@ -131,9 +153,12 @@ func (a *AppleMDM) runPostDEPEnrollment(ctx context.Context, args appleMDMArgs) 
 
 	if ref := args.EnrollReference; ref != "" {
 		a.Log.Log("info", "got an enroll_reference", "host_uuid", args.HostUUID, "ref", ref)
-		appCfg, err := a.Datastore.AppConfig(ctx)
-		if err != nil {
-			return ctxerr.Wrap(ctx, err, "getting app config")
+		if appCfg == nil {
+			var err error
+			appCfg, err = a.Datastore.AppConfig(ctx)
+			if err != nil {
+				return ctxerr.Wrap(ctx, err, "getting app config")
+			}
 		}
 
 		acct, err := a.Datastore.GetMDMIdPAccountByUUID(ctx, ref)
@@ -143,9 +168,11 @@ func (a *AppleMDM) runPostDEPEnrollment(ctx context.Context, args appleMDMArgs) 
 
 		ssoEnabled := appCfg.MDM.MacOSSetup.EnableEndUserAuthentication
 		if args.TeamID != nil {
-			team, err := a.Datastore.Team(ctx, *args.TeamID)
-			if err != nil {
-				return ctxerr.Wrap(ctx, err, "fetch team to send AccountConfiguration")
+			if team == nil {
+				team, err = a.Datastore.Team(ctx, *args.TeamID)
+				if err != nil {
+					return ctxerr.Wrap(ctx, err, "fetch team to send AccountConfiguration")
+				}
 			}
 			ssoEnabled = team.Config.MDM.MacOSSetup.EnableEndUserAuthentication
 		}
@@ -175,18 +202,23 @@ func (a *AppleMDM) runPostDEPEnrollment(ctx context.Context, args appleMDMArgs) 
 	// release.
 	if !isMacOS(args.Platform) || args.UseWorkerDeviceRelease {
 		var manualRelease bool
+		var err error
 		if args.TeamID == nil {
-			ac, err := a.Datastore.AppConfig(ctx)
-			if err != nil {
-				return ctxerr.Wrap(ctx, err, "get AppConfig to read enable_release_device_manually")
+			if appCfg == nil {
+				appCfg, err = a.Datastore.AppConfig(ctx)
+				if err != nil {
+					return ctxerr.Wrap(ctx, err, "get AppConfig to read enable_release_device_manually")
+				}
 			}
-			manualRelease = ac.MDM.MacOSSetup.EnableReleaseDeviceManually.Value
+			manualRelease = appCfg.MDM.MacOSSetup.EnableReleaseDeviceManually.Value
 		} else {
-			tm, err := a.Datastore.Team(ctx, *args.TeamID)
-			if err != nil {
-				return ctxerr.Wrap(ctx, err, "get Team to read enable_release_device_manually")
+			if team == nil {
+				team, err = a.Datastore.Team(ctx, *args.TeamID)
+				if err != nil {
+					return ctxerr.Wrap(ctx, err, "get Team to read enable_release_device_manually")
+				}
 			}
-			manualRelease = tm.Config.MDM.MacOSSetup.EnableReleaseDeviceManually.Value
+			manualRelease = team.Config.MDM.MacOSSetup.EnableReleaseDeviceManually.Value
 		}
 
 		if !manualRelease {
