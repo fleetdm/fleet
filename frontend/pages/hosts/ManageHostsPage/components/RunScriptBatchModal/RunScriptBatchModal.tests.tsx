eeda4ab5a0b51637467509f7f948ea6cdff5378b
Jacob Shandling <jacob@fleetdm.com>
2025-04-28 10:45:58 -0700

    temporarily remove only new tests

--- frontend/pages/hosts/ManageHostsPage/components/RunScriptBatchModal/RunScriptBatchModal.tests.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { render, screen } from "@testing-library/react";
-
-import RunScriptBatchModal from "./RunScriptBatchModal";
-
-describe("RunScriptBatchModal component", () => {
-  it("", () => {});
-});
