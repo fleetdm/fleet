4e470c51ee91e54937f4aaad743a99eec9df5846
Scott Gress <scott@fleetdm.com>
2025-04-29 08:16:39 -0500

    fix disk_info table for chromeos

--- ee/fleetd-chrome/src/tables/disk_info.test.ts
+++ ee/fleetd-chrome/src/tables/disk_info.test.ts
@@ -1,30 +1,57 @@
 import VirtualDatabase from "../db";
 
-const DISK_INFO_MOCK = [
+const INTERNAL_DISK_INFO_MOCK = {  
+    estimate: {
+      quota: 54321,
+    }  
+};
+const INTERNAL_DISK_INFO_EXPECTED_RESULT = {  
+    capacity: "54321",
+    id: "internal",
+    name: "Internal Storage",
+    type: "fixed",
+};
+
+const REMOVABLE_DISK_INFO_MOCK = [
   {
     capacity: "1234",
     id: "123",
-    name: "Cell phone (internal storage",
-    type: "Removable",
+    name: "Cell phone (internal storage)",
+    type: "removable",
   },
   {
     capacity: "0",
     id: "12",
     name: "Thumbdrive",
-    type: "Removable",
+    type: "removable",
   },
 ];
 
 describe("disk_info", () => {
+  let orgNavigator = window.navigator;
+  beforeAll(() => {
+    // Mock the Navigator API.
+    Object.defineProperty(window, 'navigator', {
+      value: {storage: {
+        estimate: () => Promise.resolve({ quota: 54321 })        
+      }},
+      writable: true
+    })
+  });
+  afterAll(() => {
+    // Restore the original Navigator API.
+    window.navigator = orgNavigator;
+  });
+
   test("success", async () => {
     // @ts-ignore
     chrome.system.storage.getInfo = jest.fn(() =>
-      Promise.resolve(DISK_INFO_MOCK)
+      Promise.resolve(REMOVABLE_DISK_INFO_MOCK)
     );
 
     const db = await VirtualDatabase.init();
 
     const res = await db.query("select * from disk_info");
-    expect(res).toEqual({"data":DISK_INFO_MOCK, "warnings": null});
+    expect(res).toEqual({"data":[INTERNAL_DISK_INFO_EXPECTED_RESULT, ...REMOVABLE_DISK_INFO_MOCK], "warnings": null});
   });
 });
--- ee/fleetd-chrome/src/tables/disk_info.ts
+++ ee/fleetd-chrome/src/tables/disk_info.ts
@@ -6,6 +6,18 @@ export default class TableDiskInfo extends Table {
 
   async generate() {
     let rows = [];
+    // Try to get internal storage first.
+    const estimate = await navigator.storage.estimate();
+    if (estimate && estimate.quota) {
+        const internalStorageUnit:chrome.system.storage.StorageUnitInfo = {
+          id: 'internal',
+          name: 'Internal Storage',
+          type: 'fixed',
+          capacity: estimate.quota, 
+        };
+        rows.push(internalStorageUnit);
+    }
+    // Add any removable storage.
     const disks = (await chrome.system.storage.getInfo()) as chrome.system.storage.StorageUnitInfo[];
     for (let d of disks) {
       rows.push({
