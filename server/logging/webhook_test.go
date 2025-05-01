4392bb60713826b48c44f005735c01668d1890bc
dantecatalfamo <dante.catalfamo@gmail.com>
2025-05-01 12:17:17 -0400

    Make sure things work when webhook url broken

--- server/logging/webhook_test.go
+++ server/logging/webhook_test.go
@@ -39,3 +39,24 @@ func TestWebhookSubmission(t *testing.T) {
 	require.Len(t, body.Details, 3)
 	require.Equal(t, logs, body.Details)
 }
+
+func TestWebhookFailure(t *testing.T) {
+	ctx := context.Background()
+	logger := log.NewNopLogger()
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "bad", http.StatusBadRequest)
+	}))
+	writer, err := NewWebhookLogWriter(server.URL, logger)
+	require.NoError(t, err)
+
+	// Should always return no error and not stall, even if bad things happen
+
+	// Bad return code
+	err = writer.Write(ctx, []json.RawMessage{json.RawMessage("{}")})
+	require.NoError(t, err)
+
+	// No server
+	server.Close()
+	err = writer.Write(ctx, []json.RawMessage{json.RawMessage("{}")})
+	require.NoError(t, err)
+}
