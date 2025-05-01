faa95818e4e313ec8082970cd816b0a347591f75
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2025-05-01 11:00:53 -0400

    Update custom.js

--- website/config/custom.js
+++ website/config/custom.js
@@ -183,6 +183,10 @@ module.exports.custom = {
     // 🌐 GitHub issue templates
     '.github/ISSUE_TEMPLATE': 'sampfluger88',
 
+    // 💝 Fleet-maintained apps
+    'ee/maintained-apps/inputs': 'mostlikelee',
+    'ee/maintained-apps/outputs': 'eugkuo',
+    
   },
 
   // FUTURE: Support DRIs for confidential and other repos (except see other note above about a consolidated way to do it, to reduce these 4-6 config keys into one)
