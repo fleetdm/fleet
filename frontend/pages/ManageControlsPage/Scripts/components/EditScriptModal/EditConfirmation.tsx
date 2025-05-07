5154d5b1ec5a6314cb3b05d7cb84b175439f6080
dantecatalfamo <dante.catalfamo@gmail.com>
2025-05-07 13:46:28 -0400

    Save confirmation modal works

--- frontend/pages/ManageControlsPage/Scripts/components/EditScriptModal/EditConfirmation.tsx
+++ frontend/pages/ManageControlsPage/Scripts/components/EditScriptModal/EditConfirmation.tsx
@@ -4,18 +4,18 @@ import Modal from "components/Modal";
 import Button from "components/buttons/Button";
 
 interface IEditScriptConfirmationModalProps {
-  onSubmit: () => void;
+  onSave: () => void;
   onCancel: () => void;
   scriptName: string;
 }
 
 const EditScriptConfirmationModal = ({
-  onSubmit,
+  onSave,
   onCancel,
   scriptName,
 }: IEditScriptConfirmationModalProps): JSX.Element => {
   return (
-    <Modal title="Save changes?" onExit={onCancel} onEnter={onSubmit}>
+    <Modal title="Save changes?" onExit={onCancel} onEnter={onSave}>
       <>
         <p>
           The changes you are making will cancel any pending script runs for{" "}
@@ -24,7 +24,7 @@ const EditScriptConfirmationModal = ({
         <p>If this script is currently running on a host, it will complete.</p>
         <p>You cannot undo this action.</p>
         <div className="modal-cta-wrap">
-          <Button onClick={onSubmit}>Save</Button>
+          <Button onClick={onSave}>Save</Button>
           <Button onClick={onCancel} variant="inverse-alert">
             Cancel
           </Button>
