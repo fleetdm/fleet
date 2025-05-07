48de857dca7367b56bcdd603122df1332a8eae3b
Martin Angers <martin.n.angers@gmail.com>
2025-05-07 16:48:18 -0400

    BRP: add batch-resend profile to hosts endpoint based on status (#28871)

--- server/fleet/datastore.go
+++ server/fleet/datastore.go
@@ -1586,6 +1586,11 @@ type Datastore interface {
 	// to be resent upon the next cron run.
 	ResendHostMDMProfile(ctx context.Context, hostUUID string, profileUUID string) error
 
+	// BatchResendMDMProfileToHosts updates the profile status to NULL for the
+	// matching hosts that satisfy the filter, thereby triggering the profile to
+	// be resent upon the next cron run.
+	BatchResendMDMProfileToHosts(ctx context.Context, profileUUID string, filters BatchResendMDMProfileFilters) (int64, error)
+
 	// GetHostMDMProfileInstallStatus returns the status of the profile for the host.
 	GetHostMDMProfileInstallStatus(ctx context.Context, hostUUID string, profileUUID string) (MDMDeliveryStatus, error)
 
