6ab966af5424a6c10d1abf42e61f3fd5e16eaf04
Raiven Williams <142276177+bettapizza@users.noreply.github.com>
2025-05-15 15:23:06 -0500

    add bettapizza to list of humans
    added bettapizza to list of humans

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -98,7 +98,8 @@ module.exports = {
       'sgress454',
       'BCTBB',
       'kc9wwh',
-      'JordanMontgomery',
+      'JordanMontgomery'
+      'bettapizza',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
