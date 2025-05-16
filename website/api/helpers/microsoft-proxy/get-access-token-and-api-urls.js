5ec15840961bed38ade6decb54ebf33b2e33a962
Eric <eashaw@sailsjs.com>
2025-05-16 09:44:08 -0500

    Update get-access-token-and-api-urls.js

--- website/api/helpers/microsoft-proxy/get-access-token-and-api-urls.js
+++ website/api/helpers/microsoft-proxy/get-access-token-and-api-urls.js
@@ -86,13 +86,13 @@ module.exports = {
 
     let endpointsInResponse = servicePrincipalEndpointResponse.value;
 
-    let tenantDataSyncService = _.find(endpointsInResponse, {providerName: 'PartnerTenantDataSyncService'}).uri;
+    let tenantDataSyncService = _.find(endpointsInResponse, {providerName: 'PartnerTenantDataSyncService'});
     if(!tenantDataSyncService) {
       throw new Error(`When sending a request to get the PartnerTenantDataSyncService service principal of a Microsoft compliance tenant, no PartnerTenantDataSyncService service principal was found.`)
     }
     let tenantDataSyncUrl = tenantDataSyncService.uri;
 
-    let deviceDataSyncService = _.find(endpointsInResponse, {providerName: 'PartnerDeviceDataSyncService'}).uri;
+    let deviceDataSyncService = _.find(endpointsInResponse, {providerName: 'PartnerDeviceDataSyncService'});
     if(!deviceDataSyncService) {
       throw new Error(`When sending a request to get the PartnerDeviceDataSyncService service principal of a Microsoft compliance tenant, no PartnerDeviceDataSyncService service principal was found.`)
     }
