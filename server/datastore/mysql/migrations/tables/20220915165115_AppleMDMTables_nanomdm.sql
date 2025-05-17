acd5f5afb1dfbf2411b70ef871795f987ed8a0a6
Jahziel Villasana-Espinoza <jahzielv@gmail.com>
2025-05-16 20:41:37 -0400

    minimum COLLATE calls

--- server/datastore/mysql/migrations/tables/20220915165115_AppleMDMTables_nanomdm.sql
+++ server/datastore/mysql/migrations/tables/20220915165115_AppleMDMTables_nanomdm.sql
@@ -3,7 +3,7 @@
  */
 
 CREATE TABLE nano_devices (
-    id VARCHAR(255) NOT NULL,
+    id VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
 
     identity_cert TEXT NULL,
 
--- server/datastore/mysql/migrations/tables/20250326161931_AddPlatformAndTeamIDToNanoDevices.go
+++ server/datastore/mysql/migrations/tables/20250326161931_AddPlatformAndTeamIDToNanoDevices.go
@@ -34,10 +34,10 @@ SELECT
 	d.authenticate
 FROM
 	nano_devices d
-	JOIN nano_enrollments e ON d.id = e.device_id COLLATE utf8mb4_unicode_ci
-	LEFT OUTER JOIN hosts h ON h.uuid COLLATE utf8mb4_unicode_ci = d.id COLLATE utf8mb4_unicode_ci
+	JOIN nano_enrollments e ON d.id = e.device_id
+	LEFT OUTER JOIN hosts h ON h.uuid = d.id COLLATE utf8mb4_unicode_ci
 WHERE
-	e.type = _utf8mb4'Device' COLLATE utf8mb4_unicode_ci AND
+	e.type = 'Device' AND
 	e.enabled = 1 AND
 	h.id IS NULL
 `
