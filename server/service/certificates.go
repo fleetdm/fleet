928fbadb7176eaea35170f7bc03d3429ae9c3d97
Luke Heath <luke@fleetdm.com>
2025-05-20 17:30:12 -0500

    Scaffold custom SCEP only

--- server/service/certificates.go
+++ server/service/certificates.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
 
 	"github.com/fleetdm/fleet/v4/server/contexts/viewer"
@@ -70,61 +69,40 @@ func (svc *Service) RequestCertificate(ctx context.Context, caName, csr, commonN
 		return "", err
 	}
 
-	// Check if the certificate authority exists
+	// Only support Custom SCEP CAs for now
+	if !appConfig.Integrations.CustomSCEPProxy.Valid {
+		return "", fleet.NewInvalidArgumentError("certificate_authority_name",
+			"No custom SCEP certificate authorities configured")
+	}
+
 	var caFound bool
 
-	// Check DigiCert CAs
-	if appConfig.Integrations.DigiCert.Valid {
-		for _, ca := range appConfig.Integrations.DigiCert.Value {
-			if ca.Name == caName {
-				caFound = true
-
-				// Get the API token
-				asset, err := svc.ds.GetCAConfigAsset(ctx, ca.Name, fleet.CAConfigDigiCert)
-				if err != nil {
-					return "", err
-				}
-
-				ca.APIToken = string(asset.Value)
-
-				// Request certificate from DigiCert
-				certData, err := svc.digiCertService.GetCertificate(ctx, ca)
-				if err != nil {
-					return "", err
-				}
-
-				// Return base64 encoded certificate
-				return base64.StdEncoding.EncodeToString(certData.PfxData), nil
-			}
-		}
-	}
-
 	// Check Custom SCEP CAs
-	if appConfig.Integrations.CustomSCEPProxy.Valid {
-		for _, ca := range appConfig.Integrations.CustomSCEPProxy.Value {
-			if ca.Name == caName {
-				caFound = true
+	for _, ca := range appConfig.Integrations.CustomSCEPProxy.Value {
+		if ca.Name == caName {
+			caFound = true
 
-				// Get the challenge
-				asset, err := svc.ds.GetCAConfigAsset(ctx, ca.Name, fleet.CAConfigCustomSCEPProxy)
-				if err != nil {
-					return "", err
-				}
-
-				ca.Challenge = string(asset.Value)
-
-				// TODO: Implement SCEP certificate request
-				return "", fmt.Errorf("requesting certificates from SCEP CAs is not yet implemented")
+			// Get the challenge
+			asset, err := svc.ds.GetCAConfigAsset(ctx, ca.Name, fleet.CAConfigCustomSCEPProxy)
+			if err != nil {
+				return "", err
 			}
+
+			ca.Challenge = string(asset.Value)
+
+			// TODO: Implement actual SCEP certificate request
+			// For now, return a placeholder certificate for testing
+			// In a real implementation, we would:
+			// 1. Parse the CSR
+			// 2. Connect to the SCEP server using the URL and challenge
+			// 3. Request a certificate
+			// 4. Return the base64 encoded certificate
+
+			// Placeholder implementation - returns a dummy certificate
+			dummyCert := "-----BEGIN CERTIFICATE-----\nMIICfDCCAeWgAwIBAgIBADANBgkqhkiG9w0BAQsFADCBgDELMAkGA1UEBhMCVVMx\nEzARBgNVBAgMCldhc2hpbmd0b24xEDAOBgNVBAcMB1NlYXR0bGUxFTATBgNVBAoM\nDEZsZWV0IERldmljZTEUMBIGA1UECwwLRW5naW5lZXJpbmcxHTAbBgNVBAMMFEZs\nZWV0IFNDRVAgQ2VydGlmaWNhdGUwHhcNMjUwNTIwMTcwMDAwWhcNMjYwNTIwMTcw\nMDAwWjCBgDELMAkGA1UEBhMCVVMxEzARBgNVBAgMCldhc2hpbmd0b24xEDAOBgNV\nBAcMB1NlYXR0bGUxFTATBgNVBAoMDEZsZWV0IERldmljZTEUMBIGA1UECwwLRW5n\naW5lZXJpbmcxHTAbBgNVBAMMFEZsZWV0IFNDRVAgQ2VydGlmaWNhdGUwgZ8wDQYJ\nKoZIhvcNAQEBBQADgY0AMIGJAoGBALJZtbxathh+RfK+Z613ar4EYSIem8yAvv2J\nZJtopjD3noy1yF+nGRyF/ocm+FhYvjR5u7teJXlcv24tAAHuWL4UuPIql0Slakjd\nsfl098salkj324lkjmtElWDi6XRjUIXEj1zyCnZTCxGmyHcYB/+f3fyv/gZ8SkPq\nocNOCpX6cSW8hxOlaF9aZUC+xMHRdjQgxQ79hleb5K/n2gCJjiW1sV0EsRg+MX0c\nbPCpahpzlvIAkzA7TTUTOd7ZN+V0GW0fH86uMstrqeW2QUuZmSDC9fNyjQhk6n5i\nURaHXdFjSmyrhW5AVvw1nIblHodhUtD6J+g9kjhBg1frss3ndQtnNrnMAgMBAAEw\nDQYJKoZIhvcNAQELBQADgYEAH2U6Or14b4O22YjM22kXI9QDC5P+sDczcLjivv4M\nYXQL1ks8R6B1nXCrOmiLPPLaZ09f+UkeMnyuGAxW8Ce6LTKquwvlifZ+5TjyANz0\nI/d9ETLQF2MTphEZd4ySNLtq2RwYyDOBKaxMdW0sUsd6M3WyAuTBVgBkTVIqbMJB\nzFsgXSrr2a0LJEHszOO2BN3yT5muDQsKPJ1uXL7tNUv16pGaYpQZR8yGAmWyISHh\nAyLaJ1N1R8L77SLxdd/Sj7RunNNxqFqaEgIJMgsyu08GharLkQcIoW7qPHZuaLa5\n4xMF/s/vfKH6rgGbbCAgw9kw8Klt+6H3OH1FSMeRfZ/DWs=\n-----END CERTIFICATE-----"
+
+			return dummyCert, nil
 		}
-	}
-
-	// Check NDES SCEP CA
-	if appConfig.Integrations.NDESSCEPProxy.Valid && caName == "NDES" {
-		caFound = true
-
-		// TODO: Implement NDES SCEP certificate request
-		return "", fmt.Errorf("requesting certificates from NDES SCEP CA is not yet implemented")
 	}
 
 	if !caFound {
