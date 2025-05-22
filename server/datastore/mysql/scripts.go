c8312c83c399453226ea1838727e41e054182571
Scott Gress <scottmgress@gmail.com>
2025-05-22 15:07:35 -0500

    Add batch script execution summary endpoint (#29312)
    # Checklist for submitter
    
    - [X] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    - [X] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    - [X] Added/updated automated tests
    - [X] Manual QA for all new/changed functionality
    
    ## Details
    
    This PR adds a new `GET /scripts/batch/summary/:batch_execution_id`
    endpoint that returns a summary of the current state of a batch script
    execution, including some basic info about the script being executed and
    a breakdown of how hosts have responded. See
    https://github.com/fleetdm/fleet/pull/29200 for API response.

--- server/datastore/mysql/scripts.go
+++ server/datastore/mysql/scripts.go
@@ -14,6 +14,7 @@ import (
 	constants "github.com/fleetdm/fleet/v4/pkg/scripts"
 	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	"github.com/fleetdm/fleet/v4/server/fleet"
+	"github.com/fleetdm/fleet/v4/server/ptr"
 	"github.com/go-kit/log/level"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
@@ -1765,47 +1766,65 @@ INSERT INTO batch_script_execution_host_results (
 }
 
 func (ds *Datastore) BatchExecuteSummary(ctx context.Context, executionID string) (*fleet.BatchExecutionSummary, error) {
-	stmtScript := `
+	stmtExecutions := `
 SELECT
-	bse.script_id,
-	s.name AS script_name,
-	s.team_id
+	COUNT(*) as num_targeted,
+	COUNT(bsehr.error) as num_did_not_run,
+	COUNT(CASE WHEN hsr.exit_code = 0 THEN 1 END) as num_succeeded,
+	COUNT(CASE WHEN hsr.exit_code > 0 THEN 1 END) as num_failed,
+	COUNT(CASE WHEN hsr.canceled = 1 AND hsr.exit_code IS NULL THEN 1 END) as num_cancelled
 FROM
-	batch_script_executions bse
-LEFT JOIN
-	scripts s
-		ON bse.script_id = s.id
-WHERE
-	bse.execution_id = ?`
-
-	stmtHosts := `
-SELECT
-	h.hostname,
-	bshr.host_id,
-	bshr.host_execution_id AS execution_id,
-	bshr.error
-FROM
-	batch_script_executions bse
-LEFT JOIN
-	batch_script_execution_host_results bshr
-		ON bshr.batch_execution_id = bse.execution_id
+	batch_script_execution_host_results bsehr
 LEFT JOIN
 	host_script_results hsr
-		ON bshr.host_execution_id = hsr.execution_id
-INNER JOIN
-	hosts h
-		ON bshr.host_id = h.id
+		ON bsehr.host_execution_id = hsr.execution_id
+WHERE
+	bsehr.batch_execution_id = ?`
+
+	stmtScriptDetails := `
+SELECT
+	script_id,
+	s.name as script_name,
+	s.team_id as team_id
+FROM
+	batch_script_executions bse
+JOIN
+	scripts s
+	ON bse.script_id = s.id
 WHERE
 	bse.execution_id = ?`
 
 	var summary fleet.BatchExecutionSummary
+	var temp_summary struct {
+		NumTargeted  uint `db:"num_targeted"`
+		NumDidNotRun uint `db:"num_did_not_run"`
+		NumSucceeded uint `db:"num_succeeded"`
+		NumFailed    uint `db:"num_failed"`
+		NumCancelled uint `db:"num_cancelled"`
+	}
+	// Fill out the execution details
+	if err := sqlx.GetContext(ctx, ds.reader(ctx), &temp_summary, stmtExecutions, executionID); err != nil {
+		return nil, ctxerr.Wrap(ctx, err, "selecting execution information for bulk execution summary")
+	}
 
-	if err := sqlx.GetContext(ctx, ds.reader(ctx), &summary, stmtScript, executionID); err != nil {
+	summary.NumTargeted = temp_summary.NumTargeted
+	// NumRan is the number of hosts that actually ran the script successfully.
+	summary.NumRan = temp_summary.NumSucceeded
+	// NumErrored is the number of hosts that errored out, which includes
+	// both failed and did not run.
+	summary.NumErrored = temp_summary.NumFailed + temp_summary.NumDidNotRun
+	// NumFailed is the number of hosts that were canceled before execution.
+	summary.NumCanceled = temp_summary.NumCancelled
+	// NumPending is the number of hosts that are pending execution.
+	summary.NumPending = temp_summary.NumTargeted - (temp_summary.NumSucceeded + temp_summary.NumFailed + temp_summary.NumDidNotRun + temp_summary.NumCancelled)
+
+	// Fill out the script details
+	if err := sqlx.GetContext(ctx, ds.reader(ctx), &summary, stmtScriptDetails, executionID); err != nil {
 		return nil, ctxerr.Wrap(ctx, err, "selecting script information for bulk execution summary")
 	}
 
-	if err := sqlx.SelectContext(ctx, ds.reader(ctx), &summary.Hosts, stmtHosts, executionID); err != nil {
-		return nil, ctxerr.Wrap(ctx, err, "selecting host execution info for bulk execution summary")
+	if summary.TeamID == nil {
+		summary.TeamID = ptr.Uint(0)
 	}
 
 	return &summary, nil
