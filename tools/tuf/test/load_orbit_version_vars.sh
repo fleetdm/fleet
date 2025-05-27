31d39911b04abcac003bcdd938dab300c342a01b
Lucas Rodriguez <lucas@fleetdm.com>
2025-05-27 14:41:35 -0300

    Pack day hour and minute to 16-bit patch number

--- tools/tuf/test/load_orbit_version_vars.sh
+++ tools/tuf/test/load_orbit_version_vars.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-# Orbit (test) version number are currently constrained by this error from WiX:
-# "Z:\wix\main.wxs(3): error CNDL0242 : Invalid product version '10.250525.0913'. Product version
-# must have a major version less than 256, a minor version less than 256, and a build version less than 65536."
-ORBIT_MAJOR=$(date +"%y")
-ORBIT_MINOR=$(date +"%m")
-ORBIT_PATCH=$(date +"%d")
-ORBIT_BUILD=$(date +"%H%M")
-ORBIT_VERSION="$ORBIT_MAJOR.$ORBIT_MINOR.$ORBIT_PATCH.$ORBIT_BUILD"
+# Constraints for orbit versioning:
+# - WiX fails with "Z:\wix\main.wxs(3): error CNDL0242 : Invalid product version '10.250525.0913'. Product version
+#   must have a major version less than 256, a minor version less than 256, and a build version less than 65536."
+# - Cannot use "-build", WiX/Windows prefers three dots X.Y.Z.P format.
+# - Must be with three parts X.Y.Z (otherwise breaks orbit semantic versioning)
+ORBIT_MAJOR=$(date +"%-y") # year
+ORBIT_MINOR=$(date +"%-m") # month
+day=$(date +"%-d")
+hour=$(date +"%-H")
+minute=$(date +"%-M")
+ORBIT_PATCH=$(( (day << 11) | (hour << 6) | minute )) # must fit into 16-bit number
+ORBIT_VERSION="$ORBIT_MAJOR.$ORBIT_MINOR.$ORBIT_PATCH"
 ORBIT_COMMIT=$(git rev-parse HEAD)
 
 export ORBIT_MAJOR
 export ORBIT_MINOR
 export ORBIT_PATCH
-export ORBIT_BUILD
 export ORBIT_VERSION
 export ORBIT_COMMIT
