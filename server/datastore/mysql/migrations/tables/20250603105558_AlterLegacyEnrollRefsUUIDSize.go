784bdfc8a01b853ecab57a4115092c041cea4d0d
Martin Angers <martin.n.angers@gmail.com>
2025-06-03 11:31:34 -0400

    Update db migration to add charset

--- server/datastore/mysql/migrations/tables/20250603105558_AlterLegacyEnrollRefsUUIDSize.go
+++ server/datastore/mysql/migrations/tables/20250603105558_AlterLegacyEnrollRefsUUIDSize.go
@@ -12,7 +12,7 @@ func init() {
 func Up_20250603105558(tx *sql.Tx) error {
 	_, err := tx.Exec(`
 	ALTER TABLE legacy_host_mdm_enroll_refs
-		CHANGE COLUMN host_uuid host_uuid VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
+		CHANGE COLUMN host_uuid host_uuid VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 `)
 	if err != nil {
 		return fmt.Errorf("failed to alter column host_uuid of legacy_host_mdm_enroll_refs: %w", err)
@@ -20,7 +20,7 @@ func Up_20250603105558(tx *sql.Tx) error {
 
 	_, err = tx.Exec(`
 	ALTER TABLE legacy_host_mdm_idp_accounts
-		CHANGE COLUMN host_uuid host_uuid VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
+		CHANGE COLUMN host_uuid host_uuid VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 `)
 	if err != nil {
 		return fmt.Errorf("failed to alter column host_uuid of legacy_host_mdm_idp_accounts: %w", err)
@@ -28,7 +28,7 @@ func Up_20250603105558(tx *sql.Tx) error {
 
 	_, err = tx.Exec(`
 	ALTER TABLE host_mdm_idp_accounts
-		CHANGE COLUMN host_uuid host_uuid VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL;
+		CHANGE COLUMN host_uuid host_uuid VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL;
 `)
 	if err != nil {
 		return fmt.Errorf("failed to alter column host_uuid of host_mdm_idp_accounts: %w", err)
