cf1e7a6ece7960a4299e44edb5a40b12df1b21ba
Jake Stenger <myselfjake@gmail.com>
2025-06-03 16:43:37 -0700

    Adding myself to list of humans in receive-from-github.js

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -100,6 +100,7 @@ module.exports = {
       'JordanMontgomery',
       'bettapizza',
       'irenareedy',
+      'jstenger',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
