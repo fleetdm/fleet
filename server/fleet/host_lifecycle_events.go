96668c5b62598223de2067815535757b759b2502
Jordan Montgomery <elijah.jordan.montgomery@gmail.com>
2025-06-03 13:46:02 -0600

    Change eventtype to enum

--- server/fleet/host_lifecycle_events.go
+++ server/fleet/host_lifecycle_events.go
@@ -1,11 +1,20 @@
 package fleet
 
+type HostLifecycleEventType string
+
+const (
+	HostLifecycleEventStartedMDMSetup       HostLifecycleEventType = "started_mdm_setup"
+	HostLifecycleEventCompletedMDMSetup     HostLifecycleEventType = "completed_mdm_setup"
+	HostLifecycleEventStartedMDMMigration   HostLifecycleEventType = "started_mdm_migration"
+	HostLifecycleEventCompletedMDMMigration HostLifecycleEventType = "completed_mdm_migration"
+)
+
 type HostLifecycleEvent struct {
-	ID         uint   `db:"id"`
-	HostSerial string `db:"host_serial"`
-	HostUUID   string `db:"host_uuid"`
-	HostID     uint   `db:"host_id"`
-	EventType  string `db:"event_type"` // TODO enum
+	ID         uint                   `db:"id"`
+	HostSerial string                 `db:"host_serial"`
+	HostUUID   string                 `db:"host_uuid"`
+	HostID     uint                   `db:"host_id"`
+	EventType  HostLifecycleEventType `db:"event_type"`
 	CreateTimestamp
 	ActivityID *uint `db:"activity_id"`
 }
