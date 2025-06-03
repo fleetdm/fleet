8572259c3b06155148b71436520b6d9337ba533d
Ian Littman <iansltx@gmail.com>
2025-06-03 13:49:42 -0600

    Replace [here] in receive-from-github Sails support link

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -194,7 +194,7 @@ module.exports = {
       //     `\n`+
       //     `Please remember: never post in a public forum if you believe you've found a genuine security vulnerability.  Instead, [disclose it responsibly](https://sailsjs.com/security).\n`+
       //     `\n`+
-      //     `For help with questions about Sails, [click here](http://sailsjs.com/support).\n`;
+      //     `For help with questions about Sails, see the [Sails support page](http://sailsjs.com/support).\n`;
       //   }
       // }
       // // Now that we know what to say, add our comment.
