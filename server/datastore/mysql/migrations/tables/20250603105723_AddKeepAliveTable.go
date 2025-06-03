2c02df86be37cd15d2699151ba6e592ab339299f
Jacob Shandling <jacob@fleetdm.com>
2025-06-03 11:41:38 -0600

    add table

---
+++ server/datastore/mysql/migrations/tables/20250603105723_AddKeepAliveTable.go
@@ -0,0 +1,26 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20250603105723, Down_20250603105723)
+}
+
+func Up_20250603105723(tx *sql.Tx) error {
+	stmt := `CREATE TABLE keep_alive (
+		last_server_instance_checkin DATETIME(6) NOT NULL DEFAULT NOW(6) ON UPDATE NOW(6)
+	);`
+
+	_, err := tx.Exec(stmt)
+	if err != nil {
+		return fmt.Errorf("creating keep_alive table: %w", err)
+	}
+	return nil
+}
+
+func Down_20250603105723(tx *sql.Tx) error {
+	return nil
+}
