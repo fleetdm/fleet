d847ec8ed40283a578bfc412491c3d6fac5d7fcf
Juan Fernandez <juan-fdz-hawa@users.noreply.github.com>
2025-06-11 13:56:07 -0400

    21979: Extended wipe end-point to allow for doWipe Win CMD (#29770)
    For #21979
    
    Extended POST /api/v1/fleet/hosts/:id/wipe end-point to allow users to
    specify an optional payload for specifying what type of remote wipe to
    perform on Win hosts.
    
    ---------
    
    Co-authored-by: Rachael Shaw <r@rachael.wtf>

--- server/fleet/mdm.go
+++ server/fleet/mdm.go
@@ -1005,3 +1005,8 @@ type MDMConfigProfileStatus struct {
 	Pending   uint `json:"pending" db:"pending"`
 	Failed    uint `json:"failed" db:"failed"`
 }
+
+// MDMWipeMetadata specifies optional metadata for the remote wipe command
+type MDMWipeMetadata struct {
+	Windows *MDMWindowsWipeMetadata
+}
