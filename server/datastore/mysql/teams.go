7e79c1dc2069d4640656216ee7fce4dab30dfe88
Ian Littman <iansltx@gmail.com>
2025-06-20 14:17:47 -0500

    Avoid unnecessary prepared statements in TeamAgentOptions uncached query
    For #30199. This is one of a few approaches to mitigate the issue the customer is seeing.
    
    This is SQLi-safe because we're dealing with an unsigned int parameter, sprintf'd %d.

--- server/datastore/mysql/teams.go
+++ server/datastore/mysql/teams.go
@@ -394,9 +394,9 @@ func amountTeamsDB(ctx context.Context, db sqlx.QueryerContext) (int, error) {
 
 // TeamAgentOptions loads the agents options of a team.
 func (ds *Datastore) TeamAgentOptions(ctx context.Context, tid uint) (*json.RawMessage, error) {
-	sql := `SELECT config->'$.agent_options' FROM teams WHERE id = ?`
+	stmt := fmt.Sprintf(`SELECT config->'$.agent_options' FROM teams WHERE id = %d`, tid) // safe because uint
 	var agentOptions *json.RawMessage
-	if err := sqlx.GetContext(ctx, ds.reader(ctx), &agentOptions, sql, tid); err != nil {
+	if err := sqlx.GetContext(ctx, ds.reader(ctx), &agentOptions, stmt); err != nil {
 		return nil, ctxerr.Wrap(ctx, err, "select team")
 	}
 	return agentOptions, nil
