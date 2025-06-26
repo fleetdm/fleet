b7f9cb5f34812971b4bf5ef4d3a5f5ccbd0275d4
Gabriel Hernandez <ghernandez345@gmail.com>
2025-06-26 09:42:01 +0100

    remove config php settings

--- tools/saml/config.php
+++ tools/saml/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$metadata['https://cloudsquid.co.uk'] = array(
+$metadata['https://localhost:8080'] = array(
     'AssertionConsumerService' => [
-        'https://cloudsquid.co.uk/api/v1/fleet/sso/callback',
-        'https://cloudsquid.co.uk/api/v1/fleet/mdm/sso/callback',
+        'https://localhost:8080/api/v1/fleet/sso/callback',
+        'https://localhost:8080/api/v1/fleet/mdm/sso/callback',
     ],
     'NameIDFormat' => 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddres',
     'simplesaml.nameidattribute' => 'email',
