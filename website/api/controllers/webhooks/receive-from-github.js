6cc50d8e9d0a0aeb4f5ac18a901f7af4a7b5786c
Magnus Jensen <magnus@fleetdm.com>
2025-07-01 16:53:17 +0200

    add self to humans list

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -101,6 +101,7 @@ module.exports = {
       'irenareedy',
       'jakestenger',
       'AndreyKizimenko',
+      'MagnusHJensen',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
