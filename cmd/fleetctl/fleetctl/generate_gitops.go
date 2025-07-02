c1c078795e0af525c68e4c7d859d7b9e23d17382
Scott Gress <scottmgress@gmail.com>
2025-07-02 07:07:58 -0500

    Fix macos_setup not always being exported correctly by generate-gitops (#30504)
    for #30502
    
    # Details
    
    This PR fixes an issue where `fleetctl generate-gitops` would not always
    add a `macos_setup` setting to a .yml file even if the team had a setup
    experience configured. This was due to relying on the `MacOSSetup`
    config returned by app/team config APIs to have this data populated,
    which turned out to be an incorrect assumption. Instead, we now utilize
    various APIs to check for the presence of setup software, scripts,
    bootstrap packages and profiles.
    
    Note that for now, `generate-gitops` will only output a `TODO` line if
    setup experience is detected;
    https://github.com/fleetdm/fleet/issues/30210 is open to flesh this out.
    In the meantime `fleetctl gitops` will fail if this TODO is inserted, so
    that the user must go and fix it manually.
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    <!-- Note that API documentation changes are now addressed by the
    product design team. -->
    
    - [X] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    - [X] Added/updated automated tests
    - [X] Manual QA for all new/changed functionality
    
    # Testing
    
    I set up MDM on a local instance and tried the following both on No Team
    and a regular team:
    
    * Turned "End user authentication on", verified that `fleetctl
    generate-gitops` output a `macos_setup` setting for the team. Turned it
    back off and verified that `macos_setup` was no longer exported by
    `fleetctl generate-gitops`.
    * Did the same for bootstrap package.
    * Did the same for install software, and additionally verified that
    having software available but _not_ selected did not cause `macos_setup`
    to be exported. Same for teams with no software available at all.
    * Did the same for setup assistant.
    
    I also tested that changes to No Team didn't affect the output when
    exporting a regular team.
    
    ---------
    
    Co-authored-by: Lucas Rodriguez <lucas@fleetdm.com>

--- cmd/fleetctl/fleetctl/generate_gitops.go
+++ cmd/fleetctl/fleetctl/generate_gitops.go
@@ -72,6 +72,10 @@ type generateGitopsClient interface {
 	GetQueries(teamID *uint, name *string) ([]fleet.Query, error)
 	GetLabels() ([]*fleet.LabelSpec, error)
 	Me() (*fleet.User, error)
+	GetSetupExperienceSoftware(teamID uint) ([]fleet.SoftwareTitleListResult, error)
+	GetBootstrapPackageMetadata(teamID uint, forUpdate bool) (*fleet.MDMAppleBootstrapPackage, error)
+	GetSetupExperienceScript(teamID uint) (*fleet.Script, error)
+	GetAppleMDMEnrollmentProfile(teamID uint) (*fleet.MDMAppleSetupAssistant, error)
 }
 
 // Given a struct type and a field name, return the JSON field name.
@@ -893,14 +897,16 @@ func (cmd *GenerateGitopsCommand) generateControls(teamId *uint, teamName string
 		}
 	}
 
-	if teamMdm != nil && cmd.AppConfig.License.IsPremium() {
+	if cmd.AppConfig.License.IsPremium() {
 		mdmT := reflect.TypeOf(fleet.TeamMDM{})
 
-		result[jsonFieldName(mdmT, "EnableDiskEncryption")] = teamMdm.EnableDiskEncryption
-		result[jsonFieldName(mdmT, "MacOSUpdates")] = teamMdm.MacOSUpdates
-		result[jsonFieldName(mdmT, "IOSUpdates")] = teamMdm.IOSUpdates
-		result[jsonFieldName(mdmT, "IPadOSUpdates")] = teamMdm.IPadOSUpdates
-		result[jsonFieldName(mdmT, "WindowsUpdates")] = teamMdm.WindowsUpdates
+		if teamMdm != nil {
+			result[jsonFieldName(mdmT, "EnableDiskEncryption")] = teamMdm.EnableDiskEncryption
+			result[jsonFieldName(mdmT, "MacOSUpdates")] = teamMdm.MacOSUpdates
+			result[jsonFieldName(mdmT, "IOSUpdates")] = teamMdm.IOSUpdates
+			result[jsonFieldName(mdmT, "IPadOSUpdates")] = teamMdm.IPadOSUpdates
+			result[jsonFieldName(mdmT, "WindowsUpdates")] = teamMdm.WindowsUpdates
+		}
 
 		if teamId == nil || *teamId == 0 {
 			mdmT := reflect.TypeOf(fleet.MDM{})
@@ -911,13 +917,53 @@ func (cmd *GenerateGitopsCommand) generateControls(teamId *uint, teamName string
 			result["windows_enabled_and_configured"] = cmd.AppConfig.MDM.WindowsEnabledAndConfigured
 		}
 
-		// TODO -- add an IsSet() method to MacOSSSetup to encapsulate this logic.
-		if teamMdm.MacOSSetup.BootstrapPackage.Value != "" || teamMdm.MacOSSetup.EnableEndUserAuthentication || teamMdm.MacOSSetup.MacOSSetupAssistant.Value != "" || teamMdm.MacOSSetup.Script.Value != "" || (teamMdm.MacOSSetup.Software.Valid && len(teamMdm.MacOSSetup.Software.Value) > 0) {
-			result[jsonFieldName(mdmT, "MacOSSetup")] = "TODO: update with your macos_setup configuration"
-			cmd.Messages.Notes = append(cmd.Messages.Notes, Note{
-				Filename: teamName,
-				Note:     "The macos_setup configuration is not supported by this tool yet.  To configure it, please follow the Fleet documentation at https://fleetdm.com/docs/configuration/yaml-files#macos-setup",
-			})
+		if teamId != nil {
+			// See if the team has macOS setup software configured.
+			setupSoftware, err := cmd.Client.GetSetupExperienceSoftware(*teamId)
+			if err != nil {
+				fmt.Fprintf(cmd.CLI.App.ErrWriter, "Error getting setup software: %s\n", err)
+				return nil, err
+			}
+			hasSetupSoftware := false
+			for _, software := range setupSoftware {
+				if software.SoftwarePackage.InstallDuringSetup != nil && *software.SoftwarePackage.InstallDuringSetup {
+					hasSetupSoftware = true
+					break
+				}
+			}
+
+			// See if the team has macOS bootstrap package configured.
+			bootstrapPackage, err := cmd.Client.GetBootstrapPackageMetadata(*teamId, false)
+			if err != nil && !strings.Contains(err.Error(), "bootstrap package for this team does not exist") {
+				fmt.Fprintf(cmd.CLI.App.ErrWriter, "Error getting bootstrap package metadata: %s\n", err)
+				return nil, err
+			}
+			hasBootstrapPackage := bootstrapPackage != nil
+
+			// See if the team has a macOS setup scripts configured.
+			setupScript, err := cmd.Client.GetSetupExperienceScript(*teamId)
+			if err != nil {
+				fmt.Fprintf(cmd.CLI.App.ErrWriter, "Error getting setup script: %s\n", err)
+				return nil, err
+			}
+			hasSetupScript := setupScript != nil
+
+			// See if the team has a macOS enrollment profile configured.
+			enrollmentProfile, err := cmd.Client.GetAppleMDMEnrollmentProfile(*teamId)
+			if err != nil {
+				fmt.Fprintf(cmd.CLI.App.ErrWriter, "Error getting enrollment profile: %s\n", err)
+				return nil, err
+			}
+			hasEnrollmentProfile := enrollmentProfile != nil
+
+			// If the team has any of these configured, we need to generate the macos_setup section.
+			if hasSetupSoftware || hasBootstrapPackage || hasSetupScript || hasEnrollmentProfile || (teamMdm != nil && teamMdm.MacOSSetup.EnableEndUserAuthentication) {
+				result[jsonFieldName(mdmT, "MacOSSetup")] = "TODO: update with your macos_setup configuration"
+				cmd.Messages.Notes = append(cmd.Messages.Notes, Note{
+					Filename: teamName,
+					Note:     "The macos_setup configuration is not supported by this tool yet.  To configure it, please follow the Fleet documentation at https://fleetdm.com/docs/configuration/yaml-files#macos-setup",
+				})
+			}
 		}
 	}
 
