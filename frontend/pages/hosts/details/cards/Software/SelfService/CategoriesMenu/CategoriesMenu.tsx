87d3aab2ae994cd65ed9b3a4a9902e67edbc691f
Jacob Shandling <jacob@fleetdm.com>
2025-07-03 17:14:01 -0700

    pull nav list styles into a mixin, apply to SiteNav and subcomponent and to CategoriesMenu and subcomponent

--- frontend/pages/hosts/details/cards/Software/SelfService/CategoriesMenu/CategoriesMenu.tsx
+++ frontend/pages/hosts/details/cards/Software/SelfService/CategoriesMenu/CategoriesMenu.tsx
@@ -21,35 +21,37 @@ const CategoriesMenu = ({
   const wrapperClasses = classNames(baseClass, className);
 
   return (
-    <div className={wrapperClasses}>
+    <ul className={wrapperClasses}>
       {categories.map((cat: ICategory) => {
         const isActive =
           cat.id === queryParams.category_id ||
           (cat.id === 0 && !queryParams.category_id);
 
         return (
-          <LinkWithContext
-            key={cat.value ?? "all"}
-            withParams={{
-              type: "query",
-              names: ["query", "page", "category_id"],
-            }}
-            currentQueryParams={{
-              ...queryParams,
-              page: 0,
-              category_id: cat.id !== 0 ? cat.id : undefined,
-            }}
-            to={location.pathname}
-            className={classNames({
-              [`${baseClass}__category-link`]: true,
+          <li
+            className={classNames(`${baseClass}__category-link`, {
               [`${baseClass}__category-link--active`]: isActive,
             })}
           >
-            <span data-text="">{cat.label}</span>
-          </LinkWithContext>
+            <LinkWithContext
+              key={cat.value ?? "all"}
+              withParams={{
+                type: "query",
+                names: ["query", "page", "category_id"],
+              }}
+              currentQueryParams={{
+                ...queryParams,
+                page: 0,
+                category_id: cat.id !== 0 ? cat.id : undefined,
+              }}
+              to={location.pathname}
+            >
+              <span data-text="">{cat.label}</span>
+            </LinkWithContext>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 };
 export default CategoriesMenu;
