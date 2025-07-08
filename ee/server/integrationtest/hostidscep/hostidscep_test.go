18dd3b36a0d7a4f3b72e54f924ed093e589ec6c5
Victor Lyuboslavsky <2685025+getvictor@users.noreply.github.com>
2025-07-08 08:55:12 -0500

    Added failure integration tests.

--- ee/server/integrationtest/hostidscep/hostidscep_test.go
+++ ee/server/integrationtest/hostidscep/hostidscep_test.go
@@ -8,6 +8,7 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"fmt"
+	"strings"
 	"testing"
 	"time"
 
@@ -20,6 +21,8 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const enrollmentSecret = "test_secret"
+
 func TestHostIdentitySCEP(t *testing.T) {
 	s := SetUpSuite(t, "integrationtest.HostIdentitySCEP")
 
@@ -28,6 +31,7 @@ func TestHostIdentitySCEP(t *testing.T) {
 		fn   func(t *testing.T, s *Suite)
 	}{
 		{"GetCert", testGetCert},
+		{"GetCertFailures", testGetCertFailures},
 	}
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
@@ -51,9 +55,7 @@ func testGetCert(t *testing.T, s *Suite) {
 
 func testGetCertWithCurve(t *testing.T, s *Suite, curve elliptic.Curve) {
 	ctx := t.Context()
-
 	// Create an enrollment secret
-	const enrollmentSecret = "test_secret"
 	err := s.DS.ApplyEnrollSecrets(ctx, nil, []*fleet.EnrollSecret{
 		{
 			Secret: enrollmentSecret,
@@ -179,4 +181,164 @@ func testGetCertWithCurve(t *testing.T, s *Suite, curve elliptic.Curve) {
 	require.NotNil(t, storedPubKey)
 	assert.True(t, certPubKey.Equal(storedPubKey), "Stored public key should match certificate public key")
 	assert.Equal(t, curve, storedPubKey.Curve, "Stored public key should use the expected elliptic curve")
+}
+
+func testGetCertFailures(t *testing.T, s *Suite) {
+	cases := []struct {
+		name   string
+		config SCEPFailureConfig
+	}{
+		{
+			name: "empty challenge password",
+			config: SCEPFailureConfig{
+				ChallengePassword: "",
+				CommonName:        "test-host-identity",
+				UseECC:            true,
+			},
+		},
+		{
+			name: "wrong challenge password",
+			config: SCEPFailureConfig{
+				ChallengePassword: "wrong-secret",
+				CommonName:        "test-host-identity",
+				UseECC:            true,
+			},
+		},
+		{
+			name: "CN longer than 255 characters",
+			config: SCEPFailureConfig{
+				ChallengePassword: enrollmentSecret,
+				CommonName:        strings.Repeat("a", 256),
+				UseECC:            true,
+			},
+		},
+		{
+			name: "non-ECC algorithm used",
+			config: SCEPFailureConfig{
+				ChallengePassword: enrollmentSecret,
+				CommonName:        "test-host-identity",
+				UseECC:            false,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			testSCEPFailure(t, s, c.config)
+		})
+	}
+}
+
+type SCEPFailureConfig struct {
+	ChallengePassword string
+	CommonName        string
+	UseECC            bool
+}
+
+func testSCEPFailure(t *testing.T, s *Suite, config SCEPFailureConfig) {
+	ctx := t.Context()
+
+	// Create an enrollment secret
+	err := s.DS.ApplyEnrollSecrets(ctx, nil, []*fleet.EnrollSecret{
+		{
+			Secret: enrollmentSecret,
+		},
+	})
+	require.NoError(t, err)
+
+	// Create SCEP client
+	scepURL := fmt.Sprintf("%s/api/fleet/orbit/host_identity/scep", s.Server.URL)
+	scepClient, err := scepclient.New(scepURL, s.Logger, nil)
+	require.NoError(t, err)
+
+	// Get CA certificate
+	resp, _, err := scepClient.GetCACert(ctx, "")
+	require.NoError(t, err)
+	caCerts, err := x509.ParseCertificates(resp)
+	require.NoError(t, err)
+	require.NotEmpty(t, caCerts)
+
+	var privateKey interface{}
+	var sigAlg x509.SignatureAlgorithm
+
+	if config.UseECC {
+		// Create ECC private key
+		eccKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		require.NoError(t, err)
+		privateKey = eccKey
+		sigAlg = x509.ECDSAWithSHA256
+	} else {
+		// Create RSA private key (should fail)
+		rsaKey, err := rsa.GenerateKey(rand.Reader, 2048)
+		require.NoError(t, err)
+		privateKey = rsaKey
+		sigAlg = x509.SHA256WithRSA
+	}
+
+	// Create CSR
+	csrTemplate := x509util.CertificateRequest{
+		CertificateRequest: x509.CertificateRequest{
+			Subject: pkix.Name{
+				CommonName: config.CommonName,
+			},
+			SignatureAlgorithm: sigAlg,
+		},
+		ChallengePassword: config.ChallengePassword,
+	}
+
+	csrDerBytes, err := x509util.CreateCertificateRequest(rand.Reader, &csrTemplate, privateKey)
+	require.NoError(t, err)
+	csr, err := x509.ParseCertificateRequest(csrDerBytes)
+	require.NoError(t, err)
+
+	// Create temporary RSA key for SCEP envelope
+	tempRSAKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	// Create self-signed certificate for SCEP protocol
+	deviceCertTemplate := x509.Certificate{
+		Subject: pkix.Name{
+			CommonName: config.CommonName,
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(365 * 24 * time.Hour),
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+	}
+
+	deviceCertDerBytes, err := x509.CreateCertificate(
+		rand.Reader,
+		&deviceCertTemplate,
+		&deviceCertTemplate,
+		&tempRSAKey.PublicKey,
+		tempRSAKey,
+	)
+	require.NoError(t, err)
+
+	deviceCert, err := x509.ParseCertificate(deviceCertDerBytes)
+	require.NoError(t, err)
+
+	// Create SCEP PKI message
+	pkiMsgReq := &scep.PKIMessage{
+		MessageType: scep.PKCSReq,
+		Recipients:  caCerts,
+		SignerKey:   tempRSAKey,
+		SignerCert:  deviceCert,
+	}
+
+	msg, err := scep.NewCSRRequest(csr, pkiMsgReq, scep.WithLogger(s.Logger))
+	require.NoError(t, err)
+
+	// Send PKI operation request
+	respBytes, err := scepClient.PKIOperation(ctx, msg.Raw)
+	require.NoError(t, err)
+
+	// Parse response
+	pkiMsgResp, err := scep.ParsePKIMessage(respBytes, scep.WithLogger(s.Logger), scep.WithCACerts(msg.Recipients))
+	require.NoError(t, err)
+
+	// Verify failure response
+	assert.Equal(t, scep.FAILURE, pkiMsgResp.PKIStatus, "SCEP request should fail")
+	assert.NotEqual(t, scep.SUCCESS, pkiMsgResp.PKIStatus, "SCEP request should not succeed")
 }
