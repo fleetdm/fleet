47d2d487aa7a0bb64e0048a0bde35525e89d9372
Robert Fairburn <8029478+rfairburn@users.noreply.github.com>
2025-07-11 13:22:02 -0500

    Fix maintenance window and rds engine version dogfood

--- infrastructure/dogfood/terraform/aws-tf-module/entra.tf
+++ infrastructure/dogfood/terraform/aws-tf-module/entra.tf
@@ -3,16 +3,16 @@ variable "entra_api_key" {}
 resource "aws_iam_policy" "entra_conditional_access" {
   name   = "fleet-entra-conditional-access"
   policy = data.aws_iam_policy_document.entra_conditional_access.json
-}     
-    
+}
+
 data "aws_iam_policy_document" "entra_conditional_access" {
   statement {
     actions = [
       "secretsmanager:GetSecretValue",
-    ] 
+    ]
     resources = [aws_secretsmanager_secret.entra_conditional_access.arn]
-  } 
-} 
+  }
+}
 
 resource "aws_secretsmanager_secret" "entra_conditional_access" {
   name = "dogfood-entra-conditional-access"
