adcabc745ec89a39a2e860f548251aa2d7e98775
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2025-07-15 10:49:08 -0500

    Update modal

--- frontend/pages/hosts/components/TransferHostModal/TransferHostModal.tests.tsx
+++ frontend/pages/hosts/components/TransferHostModal/TransferHostModal.tests.tsx
@@ -5,7 +5,7 @@ import { render, screen } from "@testing-library/react";
 import TransferHostModal from "./TransferHostModal";
 
 describe("TransferHostModal", () => {
-  it("renders the correct message when more than one host is being transfered", () => {
+  it("does not render the disk encryption message", () => {
     render(
       <TransferHostModal
         multipleHosts
@@ -21,25 +21,6 @@ describe("TransferHostModal", () => {
       screen.getByText(
         "The hosts' disk encryption keys are deleted if they're transferred to a team with disk encryption turned off."
       )
-    ).toBeVisible();
-  });
-
-  it("render the correct message when one host is being transfered", () => {
-    render(
-      <TransferHostModal
-        multipleHosts={false}
-        teams={[]}
-        onSubmit={noop}
-        onCancel={noop}
-        isUpdating={false}
-        isGlobalAdmin={false}
-      />
-    );
-
-    expect(
-      screen.getByText(
-        "The host's disk encryption key is deleted if it's transferred to a team with disk encryption turned off."
-      )
-    ).toBeVisible();
+    ).not.toBeVisible();
   });
 });
