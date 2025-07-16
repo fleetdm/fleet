86a3a029ea7678e14b177231f8b0e2e19063e1c0
Janis Watts <184028114+jmwatts@users.noreply.github.com>
2025-07-16 12:53:35 -0500

    Adjusting osquery perf cpu memory and host config

--- infrastructure/loadtesting/terraform/loadtesting.tf
+++ infrastructure/loadtesting/terraform/loadtesting.tf
@@ -19,8 +19,8 @@ resource "aws_ecs_task_definition" "loadtest" {
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.main.arn
   task_role_arn            = aws_iam_role.main.arn
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 2048
+  memory                   = 4096
   container_definitions = jsonencode(
     [
       {
@@ -55,7 +55,8 @@ resource "aws_ecs_task_definition" "loadtest" {
           "-server_url", "http://${aws_lb.internal.dns_name}",
           "--policy_pass_prob", "0.5",
           "--start_period", "5m",
-          "--orbit_prob", "0.0"
+          "--orbit_prob", "1",
+          "--os_templates", "macos_14.1.2:300,windows_11:100,ubuntu_22.04:100"
         ]
       }
   ])
