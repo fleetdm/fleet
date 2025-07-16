236dd5c771118338afc823a21c24658a64f810f4
Jahziel Villasana-Espinoza <jahzielv@gmail.com>
2025-07-16 13:33:43 -0400

    fix test

--- frontend/pages/hosts/details/cards/Software/HostSoftwareTable/HostSoftwareTable.tests.tsx
+++ frontend/pages/hosts/details/cards/Software/HostSoftwareTable/HostSoftwareTable.tests.tsx
@@ -54,7 +54,9 @@ describe("HostSoftwareTable", () => {
       screen.getByText(/software is not supported for this host/i)
     ).toBeInTheDocument();
     expect(screen.getByText(/let us know/i)).toBeInTheDocument();
-    expect("Software installed on this host").not.toBeInTheDocument();
+    expect(
+      screen.getByText(/Software installed on this host/i)
+    ).not.toBeInTheDocument();
   });
 
   it("renders custom filter button when filters are applied", () => {
