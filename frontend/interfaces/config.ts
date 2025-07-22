d6395387678023faa18981708b4ebf8aa39f30b4
Scott Gress <scott@fleetdm.com>
2025-07-22 09:08:33 -0500

    Add "advanced" UI to disk encryption page

--- frontend/interfaces/config.ts
+++ frontend/interfaces/config.ts
@@ -47,6 +47,7 @@ export interface IMdmConfig {
   /** Update this URL if you're self-hosting Fleet and you want your hosts to talk to a different URL for MDM features. (If not configured, hosts will use the base URL of the Fleet instance.) */
   apple_server_url: string;
   enable_disk_encryption: boolean;
+  windows_require_bitlocker_pin: boolean;
   /** `enabled_and_configured` only tells us if Apples MDM has been enabled and
   configured correctly. The naming is slightly confusing but at one point we
   only supported apple mdm, so thats why it's name the way it is. */
