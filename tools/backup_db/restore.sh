bc40dcc82fb5bc3a89d1b14d49dd9557a242eb93
dantecatalfamo <dante.catalfamo@gmail.com>
2025-07-23 16:28:39 -0400

    Add script that backs up and restores DB when switching branches

--- tools/backup_db/restore.sh
+++ tools/backup_db/restore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 BACKUP_NAME="${1:-backup.sql.gz}"
-docker run --rm -i --network fleet_default ${FLEET_MYSQL_IMAGE:-mysql:8.0.36} bash -c 'gzip -dc - | MYSQL_PWD=toor mysql -hmysql -uroot fleet' < ${BACKUP_NAME}
+docker run --rm -i --network fleet_default ${FLEET_MYSQL_IMAGE:-mysql:8.0.36} bash -c 'gzip -dc - | MYSQL_PWD=toor mysql -hmysql -uroot fleet' < "${BACKUP_NAME}"
