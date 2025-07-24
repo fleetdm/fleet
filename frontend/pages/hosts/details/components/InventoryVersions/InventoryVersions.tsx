1883099c43fbac45da75708a19d5e9dfca455202
Jacob Shandling <jacob@fleetdm.com>
2025-07-24 14:46:04 -0700

    `InventoryVersions`

---
+++ frontend/pages/hosts/details/components/InventoryVersions/InventoryVersions.tsx
@@ -0,0 +1,134 @@
+import Card from "components/Card";
+import DataSet from "components/DataSet";
+import {
+  formatSoftwareType,
+  IHostSoftware,
+  ISoftwareInstallVersion,
+  SoftwareSource,
+} from "interfaces/software";
+import React from "react";
+import { dateAgo } from "utilities/date_format";
+
+const generateVulnerabilitiesValue = (vulnerabilities: string[]) => {
+  const first3 = vulnerabilities.slice(0, 3);
+  const rest = vulnerabilities.slice(3);
+
+  const first3Text = first3.join(", ");
+  const restText = `, +${rest.length} more`;
+
+  return (
+    <>
+      <span>{`${first3Text}${rest.length > 0 ? restText : ""}`}</span>
+    </>
+  );
+};
+
+const baseClass = "inventory-versions";
+
+interface IInventoryVersionProps {
+  version: ISoftwareInstallVersion;
+  source: SoftwareSource;
+  bundleIdentifier?: string;
+}
+
+const InventoryVersion = ({
+  version,
+  source,
+  bundleIdentifier,
+}: IInventoryVersionProps) => {
+  const {
+    vulnerabilities,
+    installed_paths: installedPaths,
+    signature_information: signatureInformation,
+  } = version;
+
+  return (
+    <Card
+      className={`${baseClass}__version`}
+      color="grey"
+      borderRadiusSize="medium"
+    >
+      <div className={`${baseClass}__row`}>
+        <DataSet title="Version" value={version.version} />
+        <DataSet title="Type" value={formatSoftwareType({ source })} />
+        {bundleIdentifier && (
+          <DataSet title="Bundle identifier" value={bundleIdentifier} />
+        )}
+        {version.last_opened_at && (
+          <DataSet title="Last used" value={dateAgo(version.last_opened_at)} />
+        )}
+      </div>
+      {vulnerabilities && vulnerabilities.length !== 0 && (
+        <div className={`${baseClass}__row`}>
+          <DataSet
+            title="Vulnerabilities"
+            value={generateVulnerabilitiesValue(vulnerabilities)}
+          />
+        </div>
+      )}
+      {!!installedPaths?.length &&
+        installedPaths.map((path) => {
+          // Find the signature info for this path
+          const sigInfo = signatureInformation?.find(
+            (info) => info.installed_path === path
+          );
+
+          return (
+            <div className={`${baseClass}__sig-info`}>
+              <DataSet orientation="horizontal" title="Path" value={path} />
+              {sigInfo?.hash_sha256 && (
+                <DataSet
+                  orientation="horizontal"
+                  title="Hash"
+                  value={sigInfo.hash_sha256}
+                />
+              )}
+            </div>
+          );
+        })}
+    </Card>
+  );
+};
+
+interface IInventoryVersionsProps {
+  hostSoftware: IHostSoftware;
+}
+const InventoryVersions = ({ hostSoftware }: IInventoryVersionsProps) => {
+  const installedVersions = hostSoftware.installed_versions;
+
+  if (!installedVersions || installedVersions.length === 0) {
+    return (
+      <div className={`${baseClass}__software-details`}>
+        <Card
+          className={`${baseClass}__version-details`}
+          color="grey"
+          borderRadiusSize="medium"
+        >
+          <div className={`${baseClass}__row`}>
+            <DataSet
+              title="Type"
+              value={formatSoftwareType({ source: hostSoftware.source })}
+            />
+          </div>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <div className={baseClass}>
+      {installedVersions.map((installedVersion) => {
+        return (
+          <InventoryVersion
+            key={installedVersion.version}
+            version={installedVersion}
+            source={hostSoftware.source}
+            bundleIdentifier={hostSoftware.bundle_identifier}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default InventoryVersions;
