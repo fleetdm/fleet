adb707b3a8bb76be101704754d5fdf5889300668
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2025-07-30 14:59:29 -0400

    UI copy tweak: New label page
    - Address the confusion from Fleeties and customers/users:
      - https://github.com/fleetdm/fleet/issues/31285
      - https://fleetdm.slack.com/archives/C02A8BRABB5/p1753886293561879

--- frontend/pages/labels/NewLabelPage/NewLabelPage.tsx
+++ frontend/pages/labels/NewLabelPage/NewLabelPage.tsx
@@ -433,7 +433,7 @@ const NewLabelPage = ({
               options={availableCriteria}
               classname={`${baseClass}__criteria-dropdown`}
               wrapperClassName={`${baseClass}__form-field ${baseClass}__form-field--criteria`}
-              helpText="Currently, label criteria can be IdP group or department."
+              helpText="Currently supported on macOS hosts."
             />
             <p>is equal to</p>
             <InputField
