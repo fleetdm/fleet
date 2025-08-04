b9094f483346d25e522735fffda279cc732b822f
Gabriel Hernandez <ghernandez345@gmail.com>
2025-08-04 13:24:31 +0100

    adding new service entities and moving the interfaces for cert authorities in UI

--- frontend/services/entities/certificates.ts
+++ frontend/services/entities/certificates.ts
@@ -1,15 +1,34 @@
-import configAPI from "./config";
+import endpoints from "utilities/endpoints";
+import sendRequest from "services";
 
 export default {
+  getCertificateAuthoritiesList: (): Promise<any> => {
+    const { CERTIFICATE_AUTHORITIES } = endpoints;
+    return sendRequest("GET", CERTIFICATE_AUTHORITIES);
+  },
+
+  getCertificateAuthority: (id: number): Promise<any> => {
+    const { CERTIFICATE_AUTHORITY } = endpoints;
+    return sendRequest("GET", CERTIFICATE_AUTHORITY(id));
+  },
+
   addCertificateAuthority: (updateData: any): Promise<any> => {
-    return configAPI.update(updateData);
+    const { CERTIFICATE_AUTHORITIES } = endpoints;
+    return sendRequest("POST", CERTIFICATE_AUTHORITIES, updateData);
   },
 
-  editCertAuthorityModal: (updateData: any): Promise<any> => {
-    return configAPI.update(updateData);
+  editCertAuthorityModal: (id: number, updateData: any): Promise<any> => {
+    const { CERTIFICATE_AUTHORITY } = endpoints;
+    return sendRequest("PATCH", CERTIFICATE_AUTHORITY(id), updateData);
   },
 
-  deleteCertificateAuthority: (updateData: any): Promise<any> => {
-    return configAPI.update(updateData);
+  deleteCertificateAuthority: (id: number): Promise<any> => {
+    const { CERTIFICATE_AUTHORITY } = endpoints;
+    return sendRequest("DELETE", CERTIFICATE_AUTHORITY(id));
+  },
+
+  requestCertificate: (id: number): Promise<any> => {
+    const { CERTIFICATE_AUTHORITY_REQUEST_CERT } = endpoints;
+    return sendRequest("GET", CERTIFICATE_AUTHORITY_REQUEST_CERT(id));
   },
 };
