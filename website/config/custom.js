257a64cc54aca2cc96b74171863b84786369495f
Sam Pfluger <108141731+Sampfluger88@users.noreply.github.com>
2025-08-07 18:08:33 -0500

    Make Mike-T reviewer of articles

--- website/config/custom.js
+++ website/config/custom.js
@@ -135,7 +135,7 @@ module.exports.custom = {
     'it-and-security': 'allenhouchins',
 
     // 🫧 Articles and release notes
-    'articles': 'rachaelshaw',
+    'articles': 'mike-j-thomas',
     'CHANGELOG.md': 'lukeheath',
 
     // 🫧 Website (fleetdm.com)
