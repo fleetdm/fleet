1f364a9913ed626da1d714c02c4cbebc80497a95
dantecatalfamo <dante.catalfamo@gmail.com>
2025-08-07 10:21:49 -0400

    Fix test

--- server/datastore/mysql/scripts_test.go
+++ server/datastore/mysql/scripts_test.go
@@ -1966,7 +1966,7 @@ func testBatchScriptSchedule(t *testing.T, ds *Datastore) {
 	require.NoError(t, err)
 
 	scheduledTime := time.Now().Add(10 * time.Hour).UTC()
-	execID, err := ds.BatchScheduleScript(ctx, script.ID, &user.ID, []uint{host1.ID, host2.ID, host3.ID}, scheduledTime)
+	execID, err := ds.BatchScheduleScript(ctx, &user.ID, script.ID, []uint{host1.ID, host2.ID, host3.ID}, scheduledTime)
 	require.NoError(t, err)
 	require.NotEmpty(t, execID)
 
