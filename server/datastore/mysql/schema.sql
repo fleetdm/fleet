8fc463ab3c8f667a5d615d838618a5b7984fb35d
Jahziel Villasana-Espinoza <jahzielv@gmail.com>
2025-08-08 11:54:55 -0400

    dump schema

--- server/datastore/mysql/schema.sql
+++ server/datastore/mysql/schema.sql
@@ -1029,12 +1029,13 @@ INSERT INTO `jobs` VALUES (1,'2024-03-20 00:00:00','2024-03-20 00:00:00','macos_
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `kernels` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `software_title_id` int DEFAULT NULL,
-  `software_id` int DEFAULT NULL,
-  `os_version_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `software_title_id` int unsigned DEFAULT NULL,
+  `software_id` int unsigned DEFAULT NULL,
+  `os_version_id` int unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_kernels_unique_mapping` (`software_title_id`,`software_id`,`os_version_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
