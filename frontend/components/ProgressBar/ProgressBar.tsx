2ead2202acdb180428e99b28557792b494f4d5d9
Jacob Shandling <jacob@fleetdm.com>
2025-08-08 11:51:42 -0700

    progress bar

--- frontend/components/ProgressBar/ProgressBar.tsx
+++ frontend/components/ProgressBar/ProgressBar.tsx
@@ -1,15 +1,39 @@
 import React from "react";
+import { COLORS } from "styles/var/colors";
 
 const baseClass = "progress-bar";
 
-interface IProgressBar {
-
+export interface IProgressBarSection {
+  color: string;
+  portion: number; // Value between 0 and 1
 }
 
-const ProgressBar = ({}: IProgressBar) => {
-  return (
-    <div className={`${baseClass}`}>
+export interface IProgressBar {
+  sections: IProgressBarSection[];
+  backgroundColor?: string;
+}
 
+const ProgressBar = ({
+  sections,
+  backgroundColor = COLORS["ui-fleet-black-10"],
+}: IProgressBar) => {
+  return (
+    <div
+      className={`${baseClass}`}
+      style={{ backgroundColor }}
+      role="progressbar"
+    >
+      {sections.map((section, index) => (
+        <div
+          key={`${section.color}-${section.portion}`}
+          data-testid={`section-${index}`}
+          className={`${baseClass}__section`}
+          style={{
+            backgroundColor: section.color,
+            width: `${section.portion * 100}%`,
+          }}
+        />
+      ))}
     </div>
   );
 };
