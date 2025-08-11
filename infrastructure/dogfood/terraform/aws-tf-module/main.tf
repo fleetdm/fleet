6a06c8adb28ad568267d34eff0fe6f643c32839b
Jorge Falcon <22119513+BCTBB@users.noreply.github.com>
2025-08-11 16:32:22 -0400

    Updated provider versions to allow for v6 aws provider. Updated .terraform.lock.hcl.

--- infrastructure/dogfood/terraform/aws-tf-module/main.tf
+++ infrastructure/dogfood/terraform/aws-tf-module/main.tf
@@ -19,7 +19,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.68.0"
     }
     docker = {
       source  = "kreuzwerker/docker"
