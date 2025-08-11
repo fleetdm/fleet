850fcff73d7e9e56702e72e25fea2032a1773028
Jorge Falcon <22119513+BCTBB@users.noreply.github.com>
2025-08-11 15:21:54 -0400

    Adding vpc module tag to 1.18.3 for dogfood free

--- infrastructure/dogfood/terraform/aws-tf-module/free.tf
+++ infrastructure/dogfood/terraform/aws-tf-module/free.tf
@@ -58,7 +58,7 @@ locals {
 }
 
 module "free" {
-  source = "github.com/fleetdm/fleet-terraform//byo-vpc?ref=tf-mod-byo-vpc-v1.18.2"
+  source = "github.com/fleetdm/fleet-terraform//byo-vpc?ref=tf-mod-byo-vpc-v1.18.3"
   vpc_config = {
     name   = local.customer_free
     vpc_id = module.main.vpc.vpc_id
