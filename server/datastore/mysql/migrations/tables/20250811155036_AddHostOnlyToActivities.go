d2060b106bbe12a3ccffe582e06c71f2dc90ce42
dantecatalfamo <dante.catalfamo@gmail.com>
2025-08-11 15:53:24 -0400

    Redo migration

--- server/datastore/mysql/migrations/tables/20250811155036_AddHostOnlyToActivities.go
+++ server/datastore/mysql/migrations/tables/20250811155036_AddHostOnlyToActivities.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20250805140452, Down_20250805140452)
+	MigrationClient.AddMigration(Up_20250811155036, Down_20250811155036)
 }
 
-func Up_20250805140452(tx *sql.Tx) error {
+func Up_20250811155036(tx *sql.Tx) error {
 	stmt := `ALTER TABLE activities ADD COLUMN host_only BOOLEAN NOT NULL DEFAULT false`
 	if _, err := tx.Exec(stmt); err != nil {
 		return fmt.Errorf("adding host_only column to activities table: %w", err)
@@ -17,6 +17,6 @@ func Up_20250805140452(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20250805140452(tx *sql.Tx) error {
+func Down_20250811155036(tx *sql.Tx) error {
 	return nil
 }
