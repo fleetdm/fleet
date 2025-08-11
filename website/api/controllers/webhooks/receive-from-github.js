c5f390f42296e557c44b58cd6bde410ebc31b447
Carlo D <1778532+cdcme@users.noreply.github.com>
2025-08-11 15:59:54 -0400

    Update product-groups.md, receive-from-github.js (#31800)
    Update `product-groups.md` and `receive-from-github.js`.

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -105,6 +105,7 @@ module.exports = {
       'MunkiMind',
       'spalmesano0',
       'escomeau',
+      'cdcme',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
