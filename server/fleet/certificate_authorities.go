66650e323bc173a58b78b6fca4af894a0017ad37
Magnus Jensen <magnus@fleetdm.com>
2025-08-11 17:54:57 +0200

    Add certificate authority type to permission policy (#31783)
    Piece of work to avoid duplicate work being made in parallel.
    
    Adds CA specific permission entries, for admins and gitops which can
    both read and write [as per the permission
    change](https://github.com/fleetdm/fleet/pull/30930/files#diff-61a0f8c7cd442fab953eefa601995b52cce1b360efacec358968ef6f2c6a195c)
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    ## Testing
    
    - [x] Added/updated automated tests

--- server/fleet/certificate_authorities.go
+++ server/fleet/certificate_authorities.go
@@ -57,3 +57,7 @@ type CertificateAuthority struct {
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
 }
+
+func (c *CertificateAuthority) AuthzType() string {
+	return "certificate_authority"
+}
