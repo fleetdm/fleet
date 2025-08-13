8c3b7c80abc5db2984f1d3d7839fc5737c5f1a83
dantecatalfamo <dante.catalfamo@gmail.com>
2025-08-13 13:47:23 -0400

    Missing formatter

--- server/datastore/mysql/migrations/tables/20250812143902_AddBatchHostResultsUnique.go
+++ server/datastore/mysql/migrations/tables/20250812143902_AddBatchHostResultsUnique.go
@@ -11,7 +11,7 @@ func init() {
 
 func Up_20250812143902(tx *sql.Tx) error {
 	if _, err := tx.Exec(`ALTER TABLE batch_activity_host_results ADD CONSTRAINT unique_batch_host_results_execution_hostid UNIQUE (batch_execution_id, host_id)`); err != nil {
-		return fmt.Errorf("adding unique index to batch_activity_host_results: err", err)
+		return fmt.Errorf("adding unique index to batch_activity_host_results: %w", err)
 	}
 	return nil
 }
