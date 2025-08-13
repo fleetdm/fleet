2a9b3b20e5b7909ca1b3d3c56bb84122ba281c3e
Gabriel Hernandez <ghernandez345@gmail.com>
2025-08-13 13:37:01 +0100

    update link

--- frontend/pages/ManageControlsPage/SetupExperience/cards/EndUserAuthentication/components/EndUserAuthForm/EndUserAuthForm.tsx
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/EndUserAuthentication/components/EndUserAuthForm/EndUserAuthForm.tsx
@@ -65,7 +65,9 @@ const EndUserAuthForm = ({
           Require end users to authenticate with your identity provider (IdP)
           and agree to an end user license agreement (EULA) when they setup
           their new macOS, iOS, iPadOS and Android hosts.{" "}
-          <Link to={PATHS.ADMIN_INTEGRATIONS_MDM}>View IdP and EULA</Link>
+          <Link to={PATHS.ADMIN_INTEGRATIONS_IDENTITY_PROVIDER}>
+            View IdP and EULA
+          </Link>
         </p>
         <GitOpsModeTooltipWrapper
           renderChildren={(disableChildren) => (
