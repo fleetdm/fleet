a0b8a19f7e5f73aafaf7fd8ee8064b837ea59b16
dantecatalfamo <dante.catalfamo@gmail.com>
2025-08-14 12:35:56 -0400

    Bump migrations

--- server/datastore/mysql/migrations/tables/20250814123333_AddBatchHostResultsUnique.go
+++ server/datastore/mysql/migrations/tables/20250814123333_AddBatchHostResultsUnique.go
@@ -6,16 +6,16 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20250812143902, Down_20250812143902)
+	MigrationClient.AddMigration(Up_20250814123333, Down_20250814123333)
 }
 
-func Up_20250812143902(tx *sql.Tx) error {
+func Up_20250814123333(tx *sql.Tx) error {
 	if _, err := tx.Exec(`ALTER TABLE batch_activity_host_results ADD CONSTRAINT unique_batch_host_results_execution_hostid UNIQUE (batch_execution_id, host_id)`); err != nil {
 		return fmt.Errorf("adding unique index to batch_activity_host_results: %w", err)
 	}
 	return nil
 }
 
-func Down_20250812143902(tx *sql.Tx) error {
+func Down_20250814123333(tx *sql.Tx) error {
 	return nil
 }
