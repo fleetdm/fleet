8de953194d48edcdbf8db00164d4fdfa287d25ed
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2025-08-15 17:33:00 -0400

    Create PIN modal: Update copy
    Copy updates for the following user story.
    
    Figma wireframes:

--- frontend/pages/hosts/details/DeviceUserPage/DeviceUserPage.tsx
+++ frontend/pages/hosts/details/DeviceUserPage/DeviceUserPage.tsx
@@ -637,8 +637,10 @@ const DeviceUserPage = ({
                       </li>
                       <li>
                         <p>
-                          <b>Choose Enter a PIN (recommended)</b> and follow the
-                          prompts to create a PIN.
+                          Choose <b>Enter a PIN (recommended)</b> and follow the
+                          prompts to create a PIN.  If you don't see this option, 
+                          wait 1 minute and refresh the <b>BitLocker Drive Encrypion</b> 
+                          window.
                         </p>
                       </li>
                       <li>
