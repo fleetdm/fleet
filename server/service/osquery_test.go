9c6e1685753bda65d740aa17977b544d46e1291c
Victor Lyuboslavsky <2685025+getvictor@users.noreply.github.com>
2025-08-29 11:38:54 -0500

    Fix test

--- server/service/osquery_test.go
+++ server/service/osquery_test.go
@@ -3490,6 +3490,24 @@ func TestPolicyWebhooks(t *testing.T) {
 		}, nil
 	}
 
+	// Since the host doesn't have a team, DefaultTeamConfig will be called
+	ds.TeamWithoutExtrasFunc = func(ctx context.Context, id uint) (*fleet.Team, error) {
+		if id == 0 {
+			return &fleet.Team{
+				ID: 0,
+				Config: fleet.TeamConfig{
+					WebhookSettings: fleet.TeamWebhookSettings{
+						FailingPoliciesWebhook: fleet.FailingPoliciesWebhookSettings{
+							Enable:    true,
+							PolicyIDs: []uint{1, 2, 3},
+						},
+					},
+				},
+			}, nil
+		}
+		return nil, nil
+	}
+
 	ds.PolicyQueriesForHostFunc = func(ctx context.Context, host *fleet.Host) (map[string]string, error) {
 		return map[string]string{
 			"1": "select 1;",                       // passing policy
