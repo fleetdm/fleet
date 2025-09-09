e430ac28bc58cb6d15adef200e497402d76f12d5
Martin Angers <martin.n.angers@gmail.com>
2025-09-09 12:03:50 -0400

    Fix lint issue

--- server/mdm/android/arch_test.go
+++ server/mdm/android/arch_test.go
@@ -17,7 +17,7 @@ func TestAllAndroidPackageDependencies(t *testing.T) {
 		// Ignoring tests, as they may import the mysql datastore package (we decided not to
 		// isolate the datastore layer):
 		// https://github.com/fleetdm/fleet/pull/29853#pullrequestreview-2936461467
-		//WithTests().
+		// WithTests().
 		IgnoreXTests("github.com/fleetdm/fleet/v4/server/fleet"). // ignore fleet_test package
 		IgnorePackages(
 			"github.com/fleetdm/fleet/v4/server/datastore/mysql/common_mysql...",
