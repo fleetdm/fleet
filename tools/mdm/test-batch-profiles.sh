591a4e1c1595792b520d717cf1bb39e55e9a5478
Gabriel Hernandez <ghernandez345@gmail.com>
2025-09-09 18:33:36 +0100

    docs

--- tools/mdm/test-batch-profiles.sh
+++ tools/mdm/test-batch-profiles.sh
@@ -6,12 +6,17 @@
 # to generate different test cases.
 #--------------------------------------------------------------
 
+# generate request payload
+payload="$(
 $FLEET_PATH/tools/mdm/make_cfg_profiles.sh \
 --file $FLEET_PATH/it-and-security/lib/macos/configuration-profiles/1password-managed-settings.mobileconfig --name "1Password Managed Settings" \
 --labels-type include_all --label "test label 2" --next \
 --file $FLEET_PATH/it-and-security/lib/windows/configuration-profiles/Enable\ Firewall.xml --name "Windows Enable Firewall" \
 --labels-type include_any --label "test label 1" --next \
-| curl -X POST "$FLEET_SERVER_URL/api/latest/fleet/configuration_profiles/batch" \
+)"
+
+# make request to Fleet API
+curl -X POST "$FLEET_SERVER_URL/api/latest/fleet/configuration_profiles/batch" \
 -H "Content-Type: application/json" \
 -H "Authorization: Bearer $FLEET_AUTH_TOKEN" \
--d @-
+-d "$payload"
