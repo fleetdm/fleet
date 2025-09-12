2674288d7bbecef14f5489867db2a5d79eeed6ba
gillespi314 <73313222+gillespi314@users.noreply.github.com>
2025-09-12 13:32:01 -0500

    Add validations; support smallstep cert renewal

--- server/datastore/mysql/host_certificates.go
+++ server/datastore/mysql/host_certificates.go
@@ -115,7 +115,7 @@ func (ds *Datastore) UpdateHostCertificates(ctx context.Context, hostID uint, ho
 		for _, hostMDMManagedCert := range hostMDMManagedCerts {
 			// Note that we only care about proxied SCEP certificates because DigiCert are requested
 			// by Fleet and stored in the DB directly, so we need not fetch them via osquery/MDM
-			if hostMDMManagedCert.Type != fleet.CAConfigCustomSCEPProxy && hostMDMManagedCert.Type != fleet.CAConfigNDES {
+			if !hostMDMManagedCert.Type.SupportsRenewalID() {
 				continue
 			}
 			for _, certToInsert := range toInsert {
