e940c902235e54d9225daffaf550acc7fb57854b
Eric <eashaw@sailsjs.com>
2025-09-17 11:04:29 -0500

    Update scripts page

--- website/assets/js/pages/scripts.page.js
+++ website/assets/js/pages/scripts.page.js
@@ -3,7 +3,8 @@ parasails.registerPage('scripts', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-    selectedPlatform: 'apple', // Initially set to 'macos'
+    selectedPlatform: 'apple',
+    modal: undefined,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -26,6 +27,8 @@ parasails.registerPage('scripts', {
       }
       window.location.hash = '';
     }
+    this.handleScrollingPlatformFilters();
+    window.addEventListener('scroll', this.handleScrollingPlatformFilters);
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -34,6 +37,34 @@ parasails.registerPage('scripts', {
   methods: {
     clickSelectPlatform: function(platform) {
       this.selectedPlatform = platform;
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
     },
+    handleScrollingPlatformFilters: function () {
+      let platformFilters = document.querySelector('div[purpose="platform-filters"]');
+      let scrollTop = window.pageYOffset;
+      let windowHeight = window.innerHeight;
+      // If the right nav bar exists, add and remove a class based on the current scroll position.
+      if (platformFilters) {
+        if (scrollTop > this.scrollDistance && scrollTop > windowHeight * 1.5) {
+          platformFilters.classList.add('header-hidden');
+          this.lastScrollTop = scrollTop;
+        } else if(scrollTop < this.lastScrollTop - 60) {
+          platformFilters.classList.remove('header-hidden');
+          this.lastScrollTop = scrollTop;
+        }
+      }
+      this.scrollDistance = scrollTop;
+    },
+    clickOpenTableOfContents: function () {
+      this.modal = 'table-of-contents';
+    },
+    closeModal: async function() {
+      this.modal = '';
+      await this.forceRender();
+    }
   }
 });
