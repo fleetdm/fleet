74da5f56776d1bf65d58cc58f85fac11006dd62f
Magnus Jensen <magnus@fleetdm.com>
2025-09-17 20:04:45 +0300

    validate smallstep ca on creation

--- server/mock/scep/config.go
+++ server/mock/scep/config.go
@@ -17,6 +17,10 @@ type GetNDESSCEPChallengeFunc func(ctx context.Context, proxy fleet.NDESSCEPProx
 
 type ValidateSCEPURLFunc func(ctx context.Context, url string) error
 
+type ValidateSmallstepChallengeURLFunc func(ctx context.Context, ca fleet.SmallstepSCEPProxyCA) error
+
+type GetSmallstepSCEPChallengeFunc func(ctx context.Context, ca fleet.SmallstepSCEPProxyCA) (string, error)
+
 type SCEPConfigService struct {
 	ValidateNDESSCEPAdminURLFunc        ValidateNDESSCEPAdminURLFunc
 	ValidateNDESSCEPAdminURLFuncInvoked bool
@@ -27,7 +31,10 @@ type SCEPConfigService struct {
 	ValidateSCEPURLFunc        ValidateSCEPURLFunc
 	ValidateSCEPURLFuncInvoked bool
 
-	GetSmallstepSCEPChallengeFunc        func(ctx context.Context, ca fleet.SmallstepSCEPProxyCA) (string, error)
+	ValidateSmallstepChallengeURLFunc        ValidateSmallstepChallengeURLFunc
+	ValidateSmallstepChallengeURLFuncInvoked bool
+
+	GetSmallstepSCEPChallengeFunc        GetSmallstepSCEPChallengeFunc
 	GetSmallstepSCEPChallengeFuncInvoked bool
 
 	mu sync.Mutex
@@ -52,6 +59,13 @@ func (s *SCEPConfigService) ValidateSCEPURL(ctx context.Context, url string) err
 	s.ValidateSCEPURLFuncInvoked = true
 	s.mu.Unlock()
 	return s.ValidateSCEPURLFunc(ctx, url)
+}
+
+func (s *SCEPConfigService) ValidateSmallstepChallengeURL(ctx context.Context, ca fleet.SmallstepSCEPProxyCA) error {
+	s.mu.Lock()
+	s.ValidateSmallstepChallengeURLFuncInvoked = true
+	s.mu.Unlock()
+	return s.ValidateSmallstepChallengeURLFunc(ctx, ca)
 }
 
 func (s *SCEPConfigService) GetSmallstepSCEPChallenge(ctx context.Context, ca fleet.SmallstepSCEPProxyCA) (string, error) {
