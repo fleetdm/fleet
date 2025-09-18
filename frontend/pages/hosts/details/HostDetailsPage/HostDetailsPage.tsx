a335a6fa253e8253116a65b3ab7a0b053a7213ce
RachelElysia <rachel@fleetdm.com>
2025-09-18 10:37:37 -0400

    TabNav iterations

--- frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
@@ -979,8 +979,12 @@ const HostDetailsPage = ({
         {isSoftwareLibrarySupported ? (
           <>
             <TabList>
-              <Tab>Inventory</Tab>
-              <Tab>Library</Tab>
+              <Tab>
+                <TabText>Inventory</TabText>
+              </Tab>
+              <Tab>
+                <TabText>Library</TabText>
+              </Tab>
             </TabList>
             <TabPanel>
               <SoftwareInventoryCard
