2b6708f738b73fffc2b0f70e83fac778996fa75e
Scott Gress <scott@fleetdm.com>
2025-09-22 14:10:08 -0500

    count negative exit codes as errored

--- pkg/scripts/scripts.go
+++ pkg/scripts/scripts.go
@@ -7,7 +7,8 @@ import "time"
 const (
 	// MaxHostExecutionTime is the maximum time allowed for a script to run in a
 	// host before is terminated.
-	MaxHostExecutionTime = 5 * time.Minute
+	// MaxHostExecutionTime = 5 * time.Minute
+	MaxHostExecutionTime = 1 * time.Second
 	// MaxServerWaitTime is the maximum time allowed for the server to wait for
 	// hosts to run a script during syncronous execution. We add an extra buffer
 	// to account for the notification system used to deliver scripts to the
--- server/datastore/mysql/hosts.go
+++ server/datastore/mysql/hosts.go
@@ -1395,7 +1395,7 @@ func (*Datastore) getBatchExecutionFilters(whereParams []interface{}, opt fleet.
 			//                        with this script run.
 			batchScriptExecutionFilter += ` AND ((hsr.host_id AND (hsr.exit_code IS NULL AND (hsr.canceled IS NULL OR hsr.canceled = 0) AND bsehr.error IS NULL)) OR (hsr.host_id is NULL AND ba.canceled = 0 AND bsehr.error IS NULL))`
 		case fleet.BatchScriptExecutionErrored:
-			batchScriptExecutionFilter += ` AND hsr.exit_code > 0 AND hsr.canceled = 0`
+			batchScriptExecutionFilter += ` AND hsr.exit_code <> 0 AND hsr.canceled = 0`
 		case fleet.BatchScriptExecutionIncompatible:
 			batchScriptExecutionFilter += ` AND bsehr.error IS NOT NULL`
 		case fleet.BatchScriptExecutionCanceled:
