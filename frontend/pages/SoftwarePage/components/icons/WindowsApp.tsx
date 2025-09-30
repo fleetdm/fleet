b4ef94cf1fb6663954355148e381485794e8b59b
Carlo DiCelico <carlo@fleetdm.com>
2025-09-30 15:44:46 -0400

    lint cleanup

--- frontend/pages/SoftwarePage/components/icons/WindowsApp.tsx
+++ frontend/pages/SoftwarePage/components/icons/WindowsApp.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import type { SVGProps } from "react";
 
-
 const WindowsApp = (props: SVGProps<SVGSVGElement>) => (
   <svg fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path fill="#F9FAFC" d="M0 0h32v32H0z" />
