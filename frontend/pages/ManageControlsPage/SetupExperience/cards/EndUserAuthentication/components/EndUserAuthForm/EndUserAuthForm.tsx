84da74e043754727ecd88ca7578ba5c2504150c6
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2025-10-01 18:58:59 -0400

    Fleet product: Copy typo
    - Missing space

--- frontend/pages/ManageControlsPage/SetupExperience/cards/EndUserAuthentication/components/EndUserAuthForm/EndUserAuthForm.tsx
+++ frontend/pages/ManageControlsPage/SetupExperience/cards/EndUserAuthentication/components/EndUserAuthForm/EndUserAuthForm.tsx
@@ -18,7 +18,7 @@ const getTooltipCopy = (android = false) => {
   return (
     <>
       {android ? "Android" : "Apple"} MDM must be turned on in <b>Settings</b>{" "}
-      &gt; <b>Integrations</b> &gt; <b>Mobile Device Management(MDM)</b> to turn
+      &gt; <b>Integrations</b> &gt; <b>Mobile Device Management (MDM)</b> to turn
       on end user authentication.
     </>
   );
