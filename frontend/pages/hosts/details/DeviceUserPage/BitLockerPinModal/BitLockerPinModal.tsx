71c84b1a4a0fbad9f776f1d0095eefd34b8d404f
Rachael Shaw <r@rachael.wtf>
2025-10-02 12:11:59 -0500

    Update BitLockerPinModal.tsx

--- frontend/pages/hosts/details/DeviceUserPage/BitLockerPinModal/BitLockerPinModal.tsx
+++ frontend/pages/hosts/details/DeviceUserPage/BitLockerPinModal/BitLockerPinModal.tsx
@@ -37,7 +37,7 @@ const BitLockerPinModal = ({
               <li>
                 <p>
                   Click <b>Change how the drive is unlocked at startup</b>. If 
-                  this option doesn&apos;t show up, wait a minute and relaunch{" "}
+                  this option doesn&apos;t show up, wait a minute and relaunch{" "}
                   <b>Manage BitLocker</b>.
                 </p>
               </li>
