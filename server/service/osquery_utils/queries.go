bbc36bbc83203ffbdbc49758a2de91b1d5c1da3c
Ian Littman <iansltx@gmail.com>
2025-10-13 17:33:20 -0500

    Fall back to app filename when ingesting macOS apps that have no display name/bundle name and run.sh as the bundle executable (#34176)
    Fixes #34157. Seen on Steam games, which also don't have a bundle ID.
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    - [x] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    
    ## Testing
    
    - [x] QA'd all new/changed functionality manually

--- server/service/osquery_utils/queries.go
+++ server/service/osquery_utils/queries.go
@@ -909,7 +909,7 @@ var softwareMacOS = DetailQuery{
 	// which is used in vulnerability scanning.
 	Query: withCachedUsers(`WITH cached_users AS (%s)
 SELECT
-  COALESCE(NULLIF(display_name, ''), NULLIF(bundle_name, ''), NULLIF(bundle_executable, ''), TRIM(name, '.app') ) AS name,
+  COALESCE(NULLIF(display_name, ''), NULLIF(bundle_name, ''), NULLIF(NULLIF(bundle_executable, ''), 'run.sh'), TRIM(name, '.app') ) AS name,
   COALESCE(NULLIF(bundle_short_version, ''), bundle_version) AS version,
   bundle_identifier AS bundle_identifier,
   '' AS extension_id,
