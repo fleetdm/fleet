a5e239f7aa43da6d7739696e8b76c2de14e4177b
Ian Littman <iansltx@gmail.com>
2025-10-15 11:16:05 -0500

    Tweak Poly Lens Desktop software name on ingestion (#34287)
    To match revision in #34159 spec spreadsheet.
    
    - [x] Added/updated automated tests

--- server/service/osquery_utils/queries.go
+++ server/service/osquery_utils/queries.go
@@ -1980,7 +1980,7 @@ var (
 			matchBundleIdentifier: "com.poly.lens.legacyhost.app",
 			matchName:             "legacyhost",
 			mutate: func(s *fleet.Software, logger log.Logger) {
-				s.Name = "Poly Lens Desktop"
+				s.Name = "Poly Lens Desktop (Legacy)"
 			},
 		},
 		{
