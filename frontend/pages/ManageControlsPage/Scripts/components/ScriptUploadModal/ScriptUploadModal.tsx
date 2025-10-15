2e1ebf6f3b1b0b50f1f239c5b8f39d947b5df9f0
Scott Gress <scott@fleetdm.com>
2025-10-15 09:19:16 -0500

    updates to modal

--- frontend/pages/ManageControlsPage/Scripts/components/ScriptUploadModal/ScriptUploadModal.tsx
+++ frontend/pages/ManageControlsPage/Scripts/components/ScriptUploadModal/ScriptUploadModal.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { NotificationContext } from "context/notification";
+import scriptAPI from "services/entities/scripts";
 
+import Button from "components/buttons/Button";
 import Modal from "components/Modal";
+import { getErrorMessage } from "./helpers";
 import ScriptUploader from "../ScriptUploader";
 
 const baseClass = "script-upload-modal";
@@ -16,6 +20,31 @@ const ScriptUploadModal = ({
   onExit,
   currentTeamId,
 }: IScriptUploadModal) => {
+  const { renderFlash } = useContext(NotificationContext);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [showLoading, setShowLoading] = useState(false);
+
+  const onUploadFile = async () => {
+    if (!selectedFile) {
+      return;
+    }
+    setShowLoading(true);
+    try {
+      await scriptAPI.uploadScript(selectedFile, currentTeamId);
+      renderFlash("success", "Successfully uploaded!");
+      onSubmit();
+    } catch (e) {
+      renderFlash("error", getErrorMessage(e));
+    } finally {
+      setShowLoading(false);
+    }
+  };
+
+  const additionalInfo =
+    selectedFile && selectedFile.name.match(/\.sh$/)
+      ? 'On macOS and Linux, script will run according to the interpreter specified in the first line: "#!/bin/sh", "#!/bin/zsh", or "#!/bin/bash"'
+      : undefined;
+
   return (
     <Modal
       title="Add script"
@@ -27,9 +56,22 @@ const ScriptUploadModal = ({
         <div className={`${baseClass}__content`}>
           <ScriptUploader
             currentTeamId={currentTeamId}
-            onUpload={onSubmit}
+            onFileSelected={(file) => setSelectedFile(file)}
+            selectedFile={selectedFile}
             forModal
           />
+        </div>
+        {additionalInfo && (
+          <p className={`${baseClass}__additional-info`}>{additionalInfo}</p>
+        )}
+        <div className="modal-cta-wrap">
+          <Button
+            onClick={onUploadFile}
+            disabled={!selectedFile || showLoading}
+            isLoading={showLoading}
+          >
+            Add Script
+          </Button>
         </div>
       </>
     </Modal>
--- frontend/pages/ManageControlsPage/Scripts/components/ScriptUploader/ScriptUploader.tsx
+++ frontend/pages/ManageControlsPage/Scripts/components/ScriptUploader/ScriptUploader.tsx
@@ -1,44 +1,25 @@
 import React, { useContext, useState } from "react";
 
-import { NotificationContext } from "context/notification";
-import scriptAPI from "services/entities/scripts";
-
 import FileUploader from "components/FileUploader";
-
-import { getErrorMessage } from "./helpers";
+import { getFileDetails } from "utilities/file/fileUtils";
 
 const baseClass = "script-uploader";
 
 interface IScriptPackageUploaderProps {
   currentTeamId: number;
-  onUpload: () => void;
+  onFileSelected: (file: File) => void;
+  selectedFile: File | null;
   forModal?: boolean;
 }
 
 const ScriptPackageUploader = ({
-  currentTeamId,
-  onUpload,
   forModal,
+  onFileSelected,
+  selectedFile,
 }: IScriptPackageUploaderProps) => {
-  const { renderFlash } = useContext(NotificationContext);
-  const [showLoading, setShowLoading] = useState(false);
-
-  const onUploadFile = async (files: FileList | null) => {
-    if (!files || files.length === 0) {
-      return;
-    }
-
-    const file = files[0];
-
-    setShowLoading(true);
-    try {
-      await scriptAPI.uploadScript(file, currentTeamId);
-      renderFlash("success", "Successfully uploaded!");
-      onUpload();
-    } catch (e) {
-      renderFlash("error", getErrorMessage(e));
-    } finally {
-      setShowLoading(false);
+  const onFileSelect = (files: FileList | null) => {
+    if (files && files.length > 0) {
+      onFileSelected(files[0]);
     }
   };
 
@@ -51,8 +32,8 @@ const ScriptPackageUploader = ({
       graphicName={["file-sh", "file-ps1"]}
       message="Shell (.sh) for macOS and Linux or PowerShell (.ps1) for Windows"
       accept=".sh,.ps1"
-      onFileUpload={onUploadFile}
-      isLoading={showLoading}
+      onFileUpload={onFileSelect}
+      fileDetails={selectedFile ? getFileDetails(selectedFile) : undefined}
       buttonType={buttonType}
       buttonMessage={buttonMessage}
       gitopsCompatible
