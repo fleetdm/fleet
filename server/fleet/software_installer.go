c69df43dccdef1d2950891aca4a5cfe0eab7b5bd
Carlo DiCelico <carlo@fleetdm.com>
2025-10-16 08:29:21 -0400

    add source field to installed_software activities

--- server/fleet/software_installer.go
+++ server/fleet/software_installer.go
@@ -378,6 +378,8 @@ type HostSoftwareInstallerResult struct {
 	SoftwareInstallerID *uint `json:"-" db:"software_installer_id"`
 	// SoftwarePackage is the name of the software installer package.
 	SoftwarePackage string `json:"software_package" db:"software_package"`
+	// Source is the osquery source for this software (e.g., "sh_packages", "ps1_packages").
+	Source *string `json:"source" db:"source"`
 	// HostID is the ID of the host.
 	HostID uint `json:"host_id" db:"host_id"`
 	// Status is the status of the software installer package on the host.
