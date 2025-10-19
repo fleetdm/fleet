e2bfd4b7524f2b4da94f7a9eabdce59f5123f265
Mason Buettner <mason.buettner@gmail.com>
2025-10-19 15:32:47 -0700

    Clarify script steps
    Add comments to clarify the actions taken by this script.

--- docs/solutions/windows/scripts/disable-insider-ui-page.ps1
+++ docs/solutions/windows/scripts/disable-insider-ui-page.ps1
@@ -1,9 +1,9 @@
 # Disable Windows Insider opt-in UI page
 
+# Verify that the registry path exists, and creates it if not.
 $regPath = "HKLM:\SOFTWARE\Microsoft\WindowsSelfHost\UI\Visibility"
 if (!(Test-Path $regPath)) {
     New-Item -Path $regPath -Force | Out-Null
 }
-
-# Turn off preview builds and prevent joining the Insider Program
+#Set DWord value to hide the Windows insider page under Windows Update
 Set-ItemProperty -Path $regPath -Name "HideInsiderPage" -Value 1 -Type DWord
