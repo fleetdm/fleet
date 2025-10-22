2f9083d52372ad571bee07b3abcbe00a246b889a
Eric <eashaw@sailsjs.com>
2025-10-22 16:46:42 -0500

    Update RenderProofOfValue.js

--- website/api/models/RenderProofOfValue.js
+++ website/api/models/RenderProofOfValue.js
@@ -19,8 +19,8 @@ module.exports = {
         'record-created',
         'provisioning',
         'ready-for-assignment',
+        'deployed',
         'in-use',
-        'expiring-soon',
         'expired',
       ],
       defaultsTo: 'record-created',
@@ -34,10 +34,16 @@ module.exports = {
       required: true
     },
 
-    url: {
+    instanceUrl: {
       type: 'string',
       description: 'The full URL of this Fleet instance',
     },
+
+    renderProjectId: {
+      type: 'string',
+      description: 'The ID of the Render project this Fleet instance belongs to.'
+    },
+
     renderMySqlServiceId: {
       type: 'string',
       description: 'The ID of the MySQL service this Render POV is configured to use'
@@ -53,9 +59,14 @@ module.exports = {
       description: 'The ID of the Fleet service this Render POV is configured to use'
     },
 
-    renderFleetStorageId: {
+    renderBeforeFirstUseDeployId: {
       type: 'string',
-      description: 'The ID of the disk storage this Render POV is configured to use'
+      description: 'The ID of the deploy that was creaed when a user was assigned to this instance. Used to check the status of the instance before redirecting the user to it.'
+    },
+
+    renderTrialEndsAt: {
+      type: 'string',
+      description: 'A JS timestamp representing when the Render isntance associated with this record will be deleted.',
     },
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
@@ -66,6 +77,12 @@ module.exports = {
     //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    user: {
+      model: 'User',
+      description: 'The ID of the render POV\'s user.',
+      // required: true
+    }
 
   },
 
