deb51dfcb7c9f8ba0000008f3d6d6394bf04196e
Konstantin Sykulev <konst@sykulev.com>
2025-10-23 12:59:26 -0500

    replacing mysql_test mysql_replica_test with database_*

--- tools/dbutils/schema_generator.go
+++ tools/dbutils/schema_generator.go
@@ -79,7 +79,7 @@ func main() {
 
 	// Dump schema to dumpfile
 	cmd := exec.Command(
-		"docker", "compose", "exec", "-T", "mysql_test",
+		"docker", "compose", "exec", "-T", "database_test",
 		// Command run inside container
 		"mysqldump", "-u"+testUsername, "-p"+testPassword, "schemadb", "--compact", "--skip-comments",
 	)
