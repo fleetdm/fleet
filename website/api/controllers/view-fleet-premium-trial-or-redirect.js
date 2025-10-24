d071f02cbb93ff8ef5b08e18f12947ab9d76857a
Eric <eashaw@sailsjs.com>
2025-10-24 16:07:33 -0500

    Update Fleet Premium trial page and view action

--- website/api/controllers/view-fleet-premium-trial-or-redirect.js
+++ website/api/controllers/view-fleet-premium-trial-or-redirect.js
@@ -14,7 +14,7 @@ module.exports = {
     },
 
     redirectToLiveFleetInstance: {
-      description: 'The user was redirected to their Premium Fleet isntance trial.',
+      description: 'The user was redirected to their Fleet Premium trial instance.',
       responseType: 'redirect',
     }
 
@@ -22,37 +22,32 @@ module.exports = {
 
 
   fn: async function () {
-
-    let trialType;
     let trialIsExpired = false;
-    // If this user does not have a Fleet premium trial license, show the expired trial state.
-    if(!this.req.me.fleetPremiumTrialLicenseKey || !this.req.me.fleetPremiumTrialLicenseKeyExpiresAt){
+    // If this user does not have a Fleet Premium trial, show the expired state for local trials.
+    if(!this.req.me.fleetPremiumTrialLicenseKey || !this.req.me.fleetPremiumTrialLicenseKeyExpiresAt) {
       return {
         trialIsExpired: true,
+        trialExpiredAt: 0,
         trialType: 'local-trial'
       };
     }
+
     let trialExpiresAt = this.req.me.fleetPremiumTrialLicenseKeyExpiresAt;
     if(trialExpiresAt < Date.now()) {
-
+      trialIsExpired = true;
     }
 
-    if(this.req.me.fleetPremiumTrialType === 'render-trial') {
+    // If this user has an active Fleet premium trial instance on Render, redirect them to it.
+    if(this.req.me.fleetPremiumTrialType === 'render-trial' && !trialIsExpired) {
       // Find the associated database record for this user's Render trial.
       let renderTrialRecord = await RenderProofOfValue.findOne({user: this.req.me.id});
-      // Check to see if the trial has ended.
-      if(renderTrialRecord.renderTrialEndsAt > Date.now()){
-        // If the trial is still active, redirect the user to their trial instance.
-        return { redirectToLiveFleetInstance: renderTrialRecord.instanceUrl }
-      }
-    } else {
-      let trialLicenseKey = this.req.me.fleetPremiumTrialLicenseKey;
+      throw { redirectToLiveFleetInstance: renderTrialRecord.instanceUrl };
     }
-
 
     // Respond with view.
     return {
-      trialIsExpired,
+      trialIsExpired: trialIsExpired,
+      trialExpiredAt: this.req.me.fleetPremiumTrialLicenseKeyExpiresAt,
       trialType: this.req.me.fleetPremiumTrialType,
     };
 
