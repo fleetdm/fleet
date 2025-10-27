9c4788b3e1d2331cc8a65abdf6d027d7f338edd4
Scott Gress <scott@fleetdm.com>
2025-10-27 15:42:58 -0500

    revert orbit service changes

--- server/service/orbit.go
+++ server/service/orbit.go
@@ -35,10 +35,6 @@ type EnrollOrbitResponse struct {
 	Err          error  `json:"error,omitempty"`
 }
 
-type SSOURLResponse struct {
-	URL string `json:"url"`
-}
-
 type orbitGetConfigRequest struct {
 	OrbitNodeKey string `json:"orbit_node_key"`
 }
@@ -180,26 +176,6 @@ func (svc *Service) EnrollOrbit(ctx context.Context, hostInfo fleet.OrbitHostInf
 	appConfig, err := svc.ds.AppConfig(ctx)
 	if err != nil {
 		return "", fleet.OrbitError{Message: "app config load failed: " + err.Error()}
-	}
-	isEndUserAuthRequired := appConfig.MDM.MacOSSetup.EnableEndUserAuthentication
-	// If the secret is for a team, get the team config as well.
-	if secret.TeamID != nil {
-		team, err := svc.ds.Team(ctx, *secret.TeamID)
-		if err != nil {
-			return "", fleet.OrbitError{Message: "failed to get team config: " + err.Error()}
-		}
-		isEndUserAuthRequired = team.Config.MDM.MacOSSetup.EnableEndUserAuthentication
-	}
-
-	if isEndUserAuthRequired {
-		// Try to find an IdP account for this host.
-		idpAccount, err := svc.ds.GetMDMIdPAccountByHostUUID(ctx, hostInfo.HardwareUUID)
-		if err != nil {
-			return "", fleet.OrbitError{Message: "failed to get IdP account: " + err.Error()}
-		}
-		if idpAccount == nil {
-			return "", fleet.NewOrbitIDPAuthRequiredError()
-		}
 	}
 
 	var stickyEnrollment *string
