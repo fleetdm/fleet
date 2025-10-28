63f42cc1be2c1fd713b4edc091a0d307f36dd1d2
dantecatalfamo <dante.catalfamo@gmail.com>
2025-10-28 11:38:25 -0400

    Add error check

--- server/datastore/mysql/migrations/tables/20251027132858_AddESTEnumValueToCATable.go
+++ server/datastore/mysql/migrations/tables/20251027132858_AddESTEnumValueToCATable.go
@@ -2,6 +2,7 @@ package tables
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 func init() {
@@ -9,11 +10,13 @@ func init() {
 }
 
 func Up_20251027132858(tx *sql.Tx) error {
-	tx.Exec(`
-ALTER TABLE certificate_authorities
-MODIFY COLUMN type
-ENUM('digicert','ndes_scep_proxy','custom_scep_proxy','hydrant','smallstep','custom_est_proxy') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
-`)
+	if _, err := tx.Exec(`
+		ALTER TABLE certificate_authorities
+		MODIFY COLUMN type
+		ENUM('digicert','ndes_scep_proxy','custom_scep_proxy','hydrant','smallstep','custom_est_proxy') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+    `); err != nil {
+		return fmt.Errorf("adding custom_est_proxy to certificate_authorities.types enum: %w", err)
+	}
 	return nil
 }
 
