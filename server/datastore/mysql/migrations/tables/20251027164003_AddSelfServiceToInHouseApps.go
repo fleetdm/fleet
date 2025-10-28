74058fa65c917b02f813e3aff4bdb2cd8390a643
Martin Angers <martin.n.angers@gmail.com>
2025-10-28 10:18:05 -0400

    Add self-service to host_in_house_software_installs

--- server/datastore/mysql/migrations/tables/20251027164003_AddSelfServiceToInHouseApps.go
+++ server/datastore/mysql/migrations/tables/20251027164003_AddSelfServiceToInHouseApps.go
@@ -17,6 +17,15 @@ ALTER TABLE in_house_apps
 	if err != nil {
 		return fmt.Errorf("failed to alter in_house_apps self_service: %w", err)
 	}
+
+	_, err = tx.Exec(`
+ALTER TABLE host_in_house_software_installs
+	ADD COLUMN self_service tinyint(1) NOT NULL DEFAULT '0'
+`)
+	if err != nil {
+		return fmt.Errorf("failed to alter host_in_house_software_installs self_service: %w", err)
+	}
+
 	return nil
 }
 
