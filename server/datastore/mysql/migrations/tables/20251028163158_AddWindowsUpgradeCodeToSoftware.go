c5190197b217ceddcd67a7506e4d74d1e62a5265
Jacob Shandling <jacob@shandling.dev>
2025-10-28 20:39:53 -0700

    fix collation

--- server/datastore/mysql/migrations/tables/20251028163158_AddWindowsUpgradeCodeToSoftware.go
+++ server/datastore/mysql/migrations/tables/20251028163158_AddWindowsUpgradeCodeToSoftware.go
@@ -11,7 +11,7 @@ func init() {
 
 func Up_20251028163158(tx *sql.Tx) error {
 	// CHAR(38) to account for 32 hex chars + 4 hyphens + open/close curly braces
-	_, err := tx.Exec(`ALTER TABLE software_titles ADD COLUMN upgrade_code CHAR(38) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL`)
+	_, err := tx.Exec(`ALTER TABLE software_titles ADD COLUMN upgrade_code CHAR(38) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL`)
 	if err != nil {
 		return fmt.Errorf("failed to add software_titles.upgrade_code column: %w", err)
 	}
@@ -20,7 +20,7 @@ func Up_20251028163158(tx *sql.Tx) error {
 		return fmt.Errorf("failed to add default empty string value to software_titles.upgrade_code column for rows where source = 'programs': %w", err)
 	}
 
-	_, err = tx.Exec(`ALTER TABLE software ADD COLUMN upgrade_code CHAR(38) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL`)
+	_, err = tx.Exec(`ALTER TABLE software ADD COLUMN upgrade_code CHAR(38) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL`)
 	if err != nil {
 		return fmt.Errorf("failed to add software.upgrade_code column: %w", err)
 	}
