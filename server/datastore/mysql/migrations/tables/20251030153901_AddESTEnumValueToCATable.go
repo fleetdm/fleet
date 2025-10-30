e38049b6d3e7ee58dce21ee766a13767bc15b966
dantecatalfamo <dante.catalfamo@gmail.com>
2025-10-30 15:41:03 -0400

    Redo migration

--- server/datastore/mysql/migrations/tables/20251030153901_AddESTEnumValueToCATable.go
+++ server/datastore/mysql/migrations/tables/20251030153901_AddESTEnumValueToCATable.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20251027132858, Down_20251027132858)
+	MigrationClient.AddMigration(Up_20251030153901, Down_20251030153901)
 }
 
-func Up_20251027132858(tx *sql.Tx) error {
+func Up_20251030153901(tx *sql.Tx) error {
 	if _, err := tx.Exec(`
 		ALTER TABLE certificate_authorities
 		MODIFY COLUMN type
@@ -20,6 +20,6 @@ func Up_20251027132858(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20251027132858(tx *sql.Tx) error {
+func Down_20251030153901(tx *sql.Tx) error {
 	return nil
 }
