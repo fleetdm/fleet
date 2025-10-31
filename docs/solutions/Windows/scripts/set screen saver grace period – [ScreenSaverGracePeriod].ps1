2419dce55ecc6c942e91ca690d31eec7ac9511c0
Noah Talerman <noahtal@umich.edu>
2025-10-31 11:37:33 -0400

    Update naming

--- docs/solutions/Windows/scripts/set screen saver grace period – [ScreenSaverGracePeriod].ps1
+++
@@ -1,9 +0,0 @@
-# script: Set_ScreenSaverGracePeriod.ps1 
-# author: brock@fleetdm.com © 2025 Fleet Device Management
-# description: PowerShell command to set MSS:(ScreenSaverGracePeriod) - the time in seconds before the screen saver grace period expires
-
-# Value to be configured for grace period as an interger in seconds
-$NumberOfSeconds = "5"
-
-# Command to set value in Windows Registry
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name ScreenSaverGracePeriod -Value $NumberOfSeconds -Force
