567ad26c0d659e13c8bc9c7e0096f400c2396c4c
RachelElysia <RachelElysia@users.noreply.github.com>
2025-10-31 00:11:52 +0000

    Update MIN_OSQUERY_VERSION_OPTIONS

--- frontend/utilities/constants.tsx
+++ frontend/utilities/constants.tsx
@@ -86,6 +86,7 @@ export const MAX_OSQUERY_SCHEDULED_QUERY_INTERVAL = 604800;
 
 export const MIN_OSQUERY_VERSION_OPTIONS = [
   { label: "All", value: "" },
+  { label: "5.20.0 +", value: "5.20.0" },
   { label: "5.19.0 +", value: "5.19.0" },
   { label: "5.18.1 +", value: "5.18.1" },
   { label: "5.18.0 +", value: "5.18.0" },
