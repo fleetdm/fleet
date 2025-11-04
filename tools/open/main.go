55ecf7fadf579c40696444b62b1416db6ac61520
Scott Gress <scott@fleetdm.com>
2025-11-03 19:37:45 -0600

    add open pkg testing tool

---
+++ tools/open/main.go
@@ -0,0 +1,27 @@
+package main
+
+// This is a tool to test the zenity package on Linux
+// It will show an entry dialog, a progress dialog, and an info dialog
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/fleetdm/fleet/v4/pkg/open"
+)
+
+func main() {
+	openTool := flag.String("url", "", "URL to open")
+	flag.Parse()
+
+	if *openTool == "" {
+		fmt.Println("Please provide a URL to open using the -url flag")
+		return
+	}
+
+	err := open.Browser(*openTool)
+	if err != nil {
+		fmt.Println("Err opening URL")
+		panic(err)
+	}
+}
