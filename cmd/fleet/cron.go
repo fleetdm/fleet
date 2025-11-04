ac69cb7bcca060b065c4c786a5f28951646a93eb
Jordan Montgomery <elijah.jordan.montgomery@gmail.com>
2025-11-04 14:58:31 -0500

    reduce android device reconciler frequency (#35121)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** Resolves #35117 
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    
    ## Testing
    
    - [x] QA'd all new/changed functionality manually

--- cmd/fleet/cron.go
+++ cmd/fleet/cron.go
@@ -1832,7 +1832,7 @@ func newAndroidMDMDeviceReconcilerSchedule(
 ) (*schedule.Schedule, error) {
 	const (
 		name            = string(fleet.CronMDMAndroidDeviceReconciler)
-		defaultInterval = 30 * time.Second
+		defaultInterval = 1 * time.Hour
 	)
 
 	logger = kitlog.With(logger, "cron", name)
