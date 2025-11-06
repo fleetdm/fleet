68060c82426fd8843b3b0b1558abcf7b572f5df2
Scott Gress <scott@fleetdm.com>
2025-11-06 13:30:35 -0600

    update errors and comments

--- server/service/orbit.go
+++ server/service/orbit.go
@@ -199,9 +199,12 @@ func (svc *Service) EnrollOrbit(ctx context.Context, hostInfo fleet.OrbitHostInf
 		if idpAccount == nil {
 			// If the Orbit client doesn't support end user auth, complain loudly and let the host enroll.
 			mp, ok := capabilities.FromContext(ctx)
-			if !ok || !mp.Has(fleet.CapabilityEndUserAuth) {
+			if !ok {
+				level.Error(svc.logger).Log("msg", "!!! ERR_ALLOWING_UNAUTHENTICATED: host is not authenticated, but fleet could not determine whether orbit supports end-user authentication. proceeding with enrollment. !!! ", "host_uuid", hostInfo.HardwareUUID)
+			} else if !mp.Has(fleet.CapabilityEndUserAuth) {
 				level.Error(svc.logger).Log("msg", "!!! ERR_ALLOWING_UNAUTHENTICATED: host is not authenticated, but connected with an orbit version that does not support end user authentication. proceeding with enrollment. !!! ", "host_uuid", hostInfo.HardwareUUID)
 			} else {
+				// Otherwise report the unauthenticated host and let Orbit handle it (e.g. by prompting the user to authenticate).
 				return "", fleet.NewOrbitIDPAuthRequiredError()
 			}
 		}
