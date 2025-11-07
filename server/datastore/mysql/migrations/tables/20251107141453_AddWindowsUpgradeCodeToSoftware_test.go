10c07eee0a32fc1d42929d22cc3b5fcf8173edf4
Jacob Shandling <jacob@shandling.dev>
2025-11-07 14:24:55 -0800

    bump migration

--- server/datastore/mysql/migrations/tables/20251107141453_AddWindowsUpgradeCodeToSoftware_test.go
+++ server/datastore/mysql/migrations/tables/20251107141453_AddWindowsUpgradeCodeToSoftware_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUp_20251104160024(t *testing.T) {
+func TestUp_20251107141453(t *testing.T) {
 	db := applyUpToPrev(t)
 
 	ms := fleet.SoftwareTitle{
