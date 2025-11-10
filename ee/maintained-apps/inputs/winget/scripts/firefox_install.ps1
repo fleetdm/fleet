cf1ab9c5a73b19059f9439109471dc1761090487
Mitch Francese <2227948+tux234@users.noreply.github.com>
2025-11-10 15:36:47 -0500

    Creating Windows FMA for Firefox

--- ee/maintained-apps/inputs/winget/scripts/firefox_install.ps1
+++ ee/maintained-apps/inputs/winget/scripts/firefox_install.ps1
@@ -27,3 +27,30 @@ Exit $exitCode
   Write-Host "Error: $_"
   Exit 1
 }
+# Learn more about .exe install scripts:
+# http://fleetdm.com/learn-more-about/exe-install-scripts
+
+$exeFilePath = "${env:INSTALLER_PATH}"
+
+try {
+
+# Add arguments to install silently (Firefox uses an Inno Setup-based installer)
+$processOptions = @{
+  FilePath = "$exeFilePath"
+  ArgumentList = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NORESTART /CLOSEAPPLICATIONS /MERGETASKS=!runcode"
+  PassThru = $true
+  Wait = $true
+}
+    
+# Start process and track exit code
+$process = Start-Process @processOptions
+$exitCode = $process.ExitCode
+
+# Prints the exit code
+Write-Host "Install exit code: $exitCode"
+Exit $exitCode
+
+} catch {
+  Write-Host "Error: $_"
+  Exit 1
+}
