9b36abc608c5b181925919e83124b5de009a896e
Jonathan Katz <44128041+jkatz01@users.noreply.github.com>
2025-11-11 15:13:24 -0500

    Support categories for in-house apps (#35542)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** Resolves #35464 
    Also allows #35531 to be merged
    Adds self-service categories for #32247
    
    Changes:
    - Move category insertion up in update installer so in house apps can
    use it
    - Add migration for in_house_app_software_categories table
    - Fix some overly wrapped errors
    - Add new tests for categories
    - Fix unused param in another test
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [ ] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    - [ ] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    - [ ] If paths of existing endpoints are modified without backwards
    compatibility, checked the frontend/CLI for any necessary changes
    
    ## Testing
    
    - [x] Added/updated automated tests
    - [ ] Where appropriate, [automated tests simulate multiple hosts and
    test for host
    isolation](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/reference/patterns-backend.md#unit-testing)
    (updates to one hosts's records do not affect another)
    
    - [x] QA'd all new/changed functionality manually
    
    ## Database migrations
    
    - [ ] Checked schema for all modified table for columns that will
    auto-update timestamps during migration.
    - [ ] Confirmed that updating the timestamps is acceptable, and will not
    cause unwanted side effects.
    - [ ] Ensured the correct collation is explicitly set for character
    columns (`COLLATE utf8mb4_unicode_ci`).

--- server/datastore/mysql/in_house_apps.go
+++ server/datastore/mysql/in_house_apps.go
@@ -43,11 +43,11 @@ func (ds *Datastore) insertInHouseApp(ctx context.Context, payload *fleet.InHous
 	err = ds.withRetryTxx(ctx, func(tx sqlx.ExtContext) error {
 		row := tx.QueryRowxContext(ctx, selectStmt, globalOrTeamID, payload.BundleID, payload.Filename)
 		if err := row.Scan(&count); err != nil {
-			return ctxerr.Wrap(ctx, err, "insertInHouseApp")
+			return err
 		}
 		if count > 0 {
 			// ios or ipados version of this installer exists
-			err = alreadyExists("insertInHouseApp", payload.Filename)
+			err = alreadyExists("In-house app", payload.Filename)
 		}
 
 		argsIos := []any{
@@ -75,12 +75,12 @@ func (ds *Datastore) insertInHouseApp(ctx context.Context, payload *fleet.InHous
 
 		_, err := ds.insertInHouseAppDB(ctx, tx, payload, argsIpad)
 		if err != nil {
-			return ctxerr.Wrap(ctx, err, "insertInHouseApp")
+			return err
 		}
 
 		installerID, err = ds.insertInHouseAppDB(ctx, tx, payload, argsIos)
 		if err != nil {
-			return ctxerr.Wrap(ctx, err, "insertInHouseApp")
+			return err
 		}
 
 		return nil
@@ -129,7 +129,7 @@ func (ds *Datastore) insertInHouseAppDB(ctx context.Context, tx sqlx.ExtContext,
 	res, err := tx.ExecContext(ctx, stmt, args...)
 	if err != nil {
 		if IsDuplicate(err) {
-			err = alreadyExists("insertInHouseAppDB", payload.Filename)
+			err = alreadyExists("In-house app", payload.Filename)
 		}
 		return 0, ctxerr.Wrap(ctx, err, "insertInHouseAppDB")
 	}
@@ -142,6 +142,13 @@ func (ds *Datastore) insertInHouseAppDB(ctx context.Context, tx sqlx.ExtContext,
 	if err := setOrUpdateSoftwareInstallerLabelsDB(ctx, tx, installerID, *payload.ValidatedLabels, softwareTypeInHouseApp); err != nil {
 		return 0, ctxerr.Wrap(ctx, err, "insertInHouseAppDB")
 	}
+
+	if payload.CategoryIDs != nil {
+		if err := setOrUpdateSoftwareInstallerCategoriesDB(ctx, tx, installerID, payload.CategoryIDs, softwareTypeInHouseApp); err != nil {
+			return 0, ctxerr.Wrap(ctx, err, "upsert in house apps categories")
+		}
+	}
+
 	return installerID, nil
 }
 
@@ -230,6 +237,15 @@ WHERE
 	dest.LabelsExcludeAny = exclAny
 	dest.LabelsIncludeAny = inclAny
 
+	categoryMap, err := ds.GetCategoriesForSoftwareTitles(ctx, []uint{titleID}, teamID)
+	if err != nil {
+		return nil, ctxerr.Wrap(ctx, err, "getting categories for in house app metadata")
+	}
+
+	if categories, ok := categoryMap[titleID]; ok {
+		dest.Categories = categories
+	}
+
 	return &dest, nil
 }
 
@@ -258,6 +274,12 @@ func (ds *Datastore) SaveInHouseAppUpdates(ctx context.Context, payload *fleet.U
 		if payload.ValidatedLabels != nil {
 			if err := setOrUpdateSoftwareInstallerLabelsDB(ctx, tx, payload.InstallerID, *payload.ValidatedLabels, softwareTypeInHouseApp); err != nil {
 				return ctxerr.Wrap(ctx, err, "upsert in house app labels")
+			}
+		}
+
+		if payload.CategoryIDs != nil {
+			if err := setOrUpdateSoftwareInstallerCategoriesDB(ctx, tx, payload.InstallerID, payload.CategoryIDs, softwareTypeInHouseApp); err != nil {
+				return ctxerr.Wrap(ctx, err, "upsert in house app categories")
 			}
 		}
 
