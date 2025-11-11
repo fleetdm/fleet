df9edf223bb9b225c87d2e2a66a049ecb7d1f4ba
jkatz01 <yehonatankatz@gmail.com>
2025-11-11 14:44:36 -0500

    Remove unnecessary sql line

--- server/datastore/mysql/schema.sql
+++ server/datastore/mysql/schema.sql
@@ -1142,7 +1142,7 @@ CREATE TABLE `in_house_app_software_categories` (
   `created_at` datetime(6) DEFAULT CURRENT_TIMESTAMP(6),
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_unique_in_house_app_id_software_category_id` (`in_house_app_id`,`software_category_id`),
-  KEY `software_category_id` (`software_category_id`),
+  KEY `in_house_app_software_categories_ibfk_2` (`software_category_id`),
   CONSTRAINT `in_house_app_software_categories_ibfk_1` FOREIGN KEY (`in_house_app_id`) REFERENCES `in_house_apps` (`id`) ON DELETE CASCADE,
   CONSTRAINT `in_house_app_software_categories_ibfk_2` FOREIGN KEY (`software_category_id`) REFERENCES `software_categories` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
