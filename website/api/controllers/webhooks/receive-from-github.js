2915c4b28f64afc1dae777549f854936ffa0776d
melpike <79950145+melpike@users.noreply.github.com>
2025-11-12 15:09:28 -0700

    Added melpike to list of humans (#35665)

--- website/api/controllers/webhooks/receive-from-github.js
+++ website/api/controllers/webhooks/receive-from-github.js
@@ -118,6 +118,7 @@ module.exports = {
       'maribell-fleetdm',
       'jkatz01',
       'johnjeremiah',
+      'melpike',
     ];
 
     let GREEN_LABEL_COLOR = 'C2E0C6';// « Used in multiple places below.  (FUTURE: Use the "+" prefix for this instead of color.  2022-05-05)
