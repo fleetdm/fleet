3e3003db8d6f42e338dae29a54b9c56780969aae
Jordan Montgomery <elijah.jordan.montgomery@gmail.com>
2025-11-14 10:22:36 -0500

    Add PUT endpoint for setup experience script (#35651)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** Resolves #35309
    
    Related doc update #35736 
    
    Adds a PUT endpoint for setting setup experience scripts, as opposed to
    the current POST implementation(which errors if the script is already
    set, which is why gitops calls DELETE first every time). If the contents
    change, the new endpoint has the same effect as DELETE then POST today,
    however if the contents are unchanged no changes occur, allowing gitops
    runs to avoid cancelling script executions.
    
    Also switched gitops over to the new PUT endpoint and removed the DELETE
    in the "set" path.
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    - [x] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    - [x] If paths of existing endpoints are modified without backwards
    compatibility, checked the frontend/CLI for any necessary changes
    
    ## Testing
    
    - [x] Added/updated automated tests
    - [x] Where appropriate, [automated tests simulate multiple hosts and
    test for host
    isolation](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/reference/patterns-backend.md#unit-testing)
    (updates to one hosts's records do not affect another)
    - [ ] QA'd all new/changed functionality manually

--- server/datastore/mysql/setup_experience.go
+++ server/datastore/mysql/setup_experience.go
@@ -526,6 +526,10 @@ WHERE id = ?
 }
 
 func (ds *Datastore) GetSetupExperienceScript(ctx context.Context, teamID *uint) (*fleet.Script, error) {
+	return ds.getSetupExperienceScript(ctx, ds.reader(ctx), teamID)
+}
+
+func (ds *Datastore) getSetupExperienceScript(ctx context.Context, q sqlx.QueryerContext, teamID *uint) (*fleet.Script, error) {
 	query := `
 SELECT
   id,
@@ -545,7 +549,7 @@ WHERE
 	}
 
 	var script fleet.Script
-	if err := sqlx.GetContext(ctx, ds.reader(ctx), &script, query, globalOrTeamID); err != nil {
+	if err := sqlx.GetContext(ctx, q, &script, query, globalOrTeamID); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, ctxerr.Wrap(ctx, notFound("SetupExperienceScript"), "get setup experience script")
 		}
@@ -581,7 +585,7 @@ WHERE
 	return &script, nil
 }
 
-func (ds *Datastore) SetSetupExperienceScript(ctx context.Context, script *fleet.Script) error {
+func (ds *Datastore) SetSetupExperienceScript(ctx context.Context, script *fleet.Script, allowUpdate bool) error {
 	err := ds.withRetryTxx(ctx, func(tx sqlx.ExtContext) error {
 		var err error
 
@@ -591,6 +595,29 @@ func (ds *Datastore) SetSetupExperienceScript(ctx context.Context, script *fleet
 			return err
 		}
 		id, _ := scRes.LastInsertId()
+
+		// This clause allows for PUT semantics in some cases. The basic idea is:
+		// - no existing setup script -> go through the usual insert logic
+		// - existing setup script with different content -> delete(with all side effects) and re-insert
+		// - existing setup script with same content -> no-op
+		if allowUpdate {
+			gotSetupExperienceScript, err := ds.getSetupExperienceScript(ctx, tx, script.TeamID)
+			if err != nil && !fleet.IsNotFound(err) {
+				return err
+			}
+			// We will fall through on a notFound err - nothing to do here
+			if err == nil {
+				if gotSetupExperienceScript.ScriptContentID != uint(id) { // nolint:gosec // dismiss G115 - low risk here
+					err = ds.deleteSetupExperienceScript(ctx, tx, script.TeamID)
+					if err != nil {
+						return err
+					}
+				} else {
+					// no change
+					return nil
+				}
+			}
+		}
 
 		// then create the script entity
 		_, err = insertSetupExperienceScript(ctx, tx, script, uint(id)) // nolint: gosec
@@ -631,12 +658,16 @@ VALUES
 }
 
 func (ds *Datastore) DeleteSetupExperienceScript(ctx context.Context, teamID *uint) error {
+	return ds.deleteSetupExperienceScript(ctx, ds.writer(ctx), teamID)
+}
+
+func (ds *Datastore) deleteSetupExperienceScript(ctx context.Context, tx sqlx.ExtContext, teamID *uint) error {
 	var globalOrTeamID uint
 	if teamID != nil {
 		globalOrTeamID = *teamID
 	}
 
-	_, err := ds.writer(ctx).ExecContext(ctx, `DELETE FROM setup_experience_scripts WHERE global_or_team_id = ?`, globalOrTeamID)
+	_, err := tx.ExecContext(ctx, `DELETE FROM setup_experience_scripts WHERE global_or_team_id = ?`, globalOrTeamID)
 	if err != nil {
 		return ctxerr.Wrap(ctx, err, "delete setup experience script")
 	}
