fbb37de0eb9be676ce4d4572f9561f6d9074ed62
Ian Littman <iansltx@gmail.com>
2025-11-17 17:25:45 -0600

    Use lighter Team call when it's obviously safe to do so, comment potential areas for further improvement (#35587)
    **Related issue:** Resolves #35357
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [x] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    - [x] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements)
    ## Testing
    
    - [x] Added/updated automated tests
    
    - [ ] QA'd all new/changed functionality manually

--- server/service/osquery.go
+++ server/service/osquery.go
@@ -1268,7 +1268,7 @@ func processCalendarPolicies(
 		return nil
 	}
 
-	team, err := ds.TeamWithExtras(ctx, *host.TeamID)
+	team, err := ds.TeamLite(ctx, *host.TeamID)
 	if err != nil {
 		return ctxerr.Wrap(ctx, err, "load host team")
 	}
