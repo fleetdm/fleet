fee670241247f43204737dd8dfb61705a46c4a9c
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2025-11-17 20:41:04 -0500

    Update ConditionalAccess.tsx

--- frontend/pages/admin/IntegrationsPage/cards/ConditionalAccess/ConditionalAccess.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/ConditionalAccess/ConditionalAccess.tsx
@@ -80,9 +80,7 @@ const DeleteConditionalAccessModal = ({
             newTab
           />
         </p>
-        <p>
-          If you don't, end users will stay blocked even after deleting Entra.
-        </p>
+        <p>If you don't, end users will stay blocked even after deleting Entra.</p>
         <div className="modal-cta-wrap">
           <Button
             type="button"
