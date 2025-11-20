35cfe84c0c47ce4fd76bb73a883cbd76afba4975
Allen Houchins <32207388+allenhouchins@users.noreply.github.com>
2025-11-19 23:45:06 -0600

    Add BBEdit icon component

--- frontend/pages/SoftwarePage/components/icons/BBEdit.tsx
+++ frontend/pages/SoftwarePage/components/icons/BBEdit.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import type { SVGProps } from "react";
+
 const SvgBbEdit = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} {...props}>
     <image
