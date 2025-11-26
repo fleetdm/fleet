4d9eccfabf90a2592b72b1fef5cd0c654a2c4699
Scott Gress <scottmgress@gmail.com>
2025-11-26 11:44:55 -0600

    Allow deleting all certificate templates from GitOps (#36264)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** For #35460 
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    ## Testing
    
    - [X] Added/updated automated tests
    - [X] QA'd all new/changed functionality manually

--- server/service/client.go
+++ server/service/client.go
@@ -2870,30 +2870,30 @@ func (c *Client) doGitOpsQueries(config *spec.GitOps, logFn func(format string, 
 }
 
 func (c *Client) doGitOpsAndroidCertificates(config *spec.GitOps, logFn func(format string, args ...interface{}), dryRun bool) error {
-	if config.Controls.AndroidSettings == nil {
-		return nil
+	certificates := make([]fleet.CertificateTemplateSpec, 0)
+
+	// Extract Android certificates from config if there are any.
+	if config.Controls.AndroidSettings != nil {
+		androidSettings, ok := config.Controls.AndroidSettings.(fleet.AndroidSettings)
+		if ok && androidSettings.Certificates.Valid && len(androidSettings.Certificates.Value) > 0 {
+			certificates = androidSettings.Certificates.Value
+		}
 	}
 
-	androidSettings, ok := config.Controls.AndroidSettings.(fleet.AndroidSettings)
-	if !ok {
-		return nil
-	}
-
-	if !androidSettings.Certificates.Valid || len(androidSettings.Certificates.Value) == 0 {
-		return nil
-	}
-
-	certificates := androidSettings.Certificates.Value
 	numCerts := len(certificates)
 
 	teamID := ""
 	if config.TeamID != nil {
 		teamID = fmt.Sprintf("%d", *config.TeamID)
 	} else {
-		return errors.New("applying Android certificates: Team ID is required")
+		// TODO -- implement "no team" certs
+		return nil
+		// return errors.New("applying Android certificates: Team ID is required")
 	}
 
-	logFn("[+] attempting to apply %s\n", numberWithPluralization(numCerts, "Android certificate", "Android certificates"))
+	if numCerts > 0 {
+		logFn("[+] attempting to apply %s\n", numberWithPluralization(numCerts, "Android certificate", "Android certificates"))
+	}
 
 	// existing certificate templates
 	existingCertificates, err := c.GetCertificateTemplates(teamID)
@@ -2968,13 +2968,15 @@ func (c *Client) doGitOpsAndroidCertificates(config *spec.GitOps, logFn func(for
 		}
 	}
 
-	if dryRun {
-		logFn("[+] would've applied %s\n", numberWithPluralization(numCerts, "Android certificate", "Android certificates"))
-	} else {
-		if err := c.ApplyCertificateSpecs(certRequests); err != nil {
-			return fmt.Errorf("applying Android certificates: %w", err)
+	if numCerts > 0 {
+		if dryRun {
+			logFn("[+] would've applied %s\n", numberWithPluralization(numCerts, "Android certificate", "Android certificates"))
+		} else {
+			if err := c.ApplyCertificateSpecs(certRequests); err != nil {
+				return fmt.Errorf("applying Android certificates: %w", err)
+			}
+			logFn("[+] applied %s\n", numberWithPluralization(numCerts, "Android certificate", "Android certificates"))
 		}
-		logFn("[+] applied %s\n", numberWithPluralization(numCerts, "Android certificate", "Android certificates"))
 	}
 
 	return nil
