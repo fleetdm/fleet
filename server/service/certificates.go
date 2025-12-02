f0494961d5f83c756682aa68e02a31bbb83b92cb
Konstantin Sykulev <konst@sykulev.com>
2025-12-01 19:10:55 -0600

    validating certificate template subjectname fleet vars (#36377)
    **Related issue:** Resolves #36289
    
    ## Testing
    
    - [x] Added/updated automated tests
    - [x] QA'd all new/changed functionality manually

--- server/service/certificates.go
+++ server/service/certificates.go
@@ -3,12 +3,12 @@ package service
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strconv"
 
 	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	hostctx "github.com/fleetdm/fleet/v4/server/contexts/host"
 	"github.com/fleetdm/fleet/v4/server/fleet"
-	"github.com/fleetdm/fleet/v4/server/variables"
 )
 
 type createCertificateTemplateRequest struct {
@@ -44,6 +44,10 @@ func createCertificateTemplateEndpoint(ctx context.Context, request interface{},
 func (svc *Service) CreateCertificateTemplate(ctx context.Context, name string, teamID uint, certificateAuthorityID uint, subjectName string) (*fleet.CertificateTemplateResponseFull, error) {
 	if err := svc.authz.Authorize(ctx, &fleet.CertificateTemplate{TeamID: teamID}, fleet.ActionWrite); err != nil {
 		return nil, err
+	}
+
+	if err := validateCertificateTemplateFleetVariables(subjectName); err != nil {
+		return nil, &fleet.BadRequestError{Message: err.Error()}
 	}
 
 	certTemplate := &fleet.CertificateTemplate{
@@ -285,6 +289,11 @@ func (svc *Service) ApplyCertificateTemplateSpecs(ctx context.Context, specs []*
 			teamID = uint(parsed)
 		}
 
+		// Validate Fleet variables in subject name
+		if err := validateCertificateTemplateFleetVariables(spec.SubjectName); err != nil {
+			return &fleet.BadRequestError{Message: fmt.Sprintf("%s (certificate %s)", err.Error(), spec.Name)}
+		}
+
 		cert := &fleet.CertificateTemplate{
 			Name:                   spec.Name,
 			CertificateAuthorityID: spec.CertificateAuthorityId,
@@ -323,35 +332,6 @@ func (svc *Service) DeleteCertificateTemplateSpecs(ctx context.Context, certific
 		return err
 	}
 	return svc.ds.BatchDeleteCertificateTemplates(ctx, certificateTemplateIDs)
-}
-
-// replaceCertificateVariables replaces FLEET_VAR_* variables in the subject name with actual host values
-func (svc *Service) replaceCertificateVariables(ctx context.Context, subjectName string, host *fleet.Host) (string, error) {
-	fleetVars := variables.Find(subjectName)
-	if len(fleetVars) == 0 {
-		return subjectName, nil
-	}
-
-	result := subjectName
-	for _, fleetVar := range fleetVars {
-		switch fleetVar {
-		case "HOST_UUID":
-			result = fleet.FleetVarHostUUIDRegexp.ReplaceAllString(result, host.UUID)
-		case "HOST_HARDWARE_SERIAL":
-			result = fleet.FleetVarHostHardwareSerialRegexp.ReplaceAllString(result, host.HardwareSerial)
-		case "HOST_END_USER_IDP_USERNAME":
-			users, err := fleet.GetEndUsers(ctx, svc.ds, host.ID)
-			if err != nil {
-				return "", ctxerr.Wrapf(ctx, err, "getting host end users for variable %s", fleetVar)
-			}
-			if len(users) == 0 || users[0].IdpUserName == "" {
-				return "", ctxerr.Errorf(ctx, "host %s does not have an IDP username for variable %s", host.UUID, fleetVar)
-			}
-			result = fleet.FleetVarHostEndUserIDPUsernameRegexp.ReplaceAllString(result, users[0].IdpUserName)
-		}
-	}
-
-	return result, nil
 }
 
 type updateCertificateStatusRequest struct {
