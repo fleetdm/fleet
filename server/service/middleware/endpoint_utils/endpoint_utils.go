386c72221bed5a58a6d4efec166c652ba73ace68
Victor Lyuboslavsky <2685025+getvictor@users.noreply.github.com>
2025-12-04 10:06:39 -0600

    Added panic on misconfiguration.

--- server/service/middleware/endpoint_utils/endpoint_utils.go
+++ server/service/middleware/endpoint_utils/endpoint_utils.go
@@ -576,6 +576,10 @@ func (e *CommonEndpointer[H]) makeEndpoint(f H, v interface{}) http.Handler {
 			endp = mw(endp)
 		}
 	}
+	if e.AuthMiddleware == nil {
+		// This panic catches potential security issues during development.
+		panic("AuthMiddleware must be set on CommonEndpointer")
+	}
 	endp = e.AuthMiddleware(endp)
 
 	// Apply "before auth" middleware (in reverse order so that the first wraps
