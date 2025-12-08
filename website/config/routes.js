7fb0ab105ad01d0efac281a18998c17b8d3fe322
Steven Palmesano <3100993+spalmesano0@users.noreply.github.com>
2025-12-08 14:10:46 -0600

    Windows MDM migration notification (#36525)
    Related to https://github.com/fleetdm/confidential/issues/12925
    
    ---------
    
    Co-authored-by: Noah Talerman <47070608+noahtalerman@users.noreply.github.com>

--- website/config/routes.js
+++ website/config/routes.js
@@ -1149,6 +1149,7 @@ module.exports.routes = {
   'GET /learn-more-about/setup-experience/bootstrap-package': '/guides/setup-experience#bootstrap-package',
   'GET /learn-more-about/built-in-variables': '/docs/configuration/yaml-files#variables',
   'GET /learn-more-about/disable-entra-conditional-access': '/guides/entra-conditional-access-integration#disable',
+  'GET /learn-more-about/connect-microsoft-entra': '/guides/windows-mdm-setup#automatic-enrollment',
   'GET /learn-more-about/macos-configuration-profiles-same-scope': '/guides/custom-os-settings#upgrading-to-4-71-0',
 
   // Sitemap
