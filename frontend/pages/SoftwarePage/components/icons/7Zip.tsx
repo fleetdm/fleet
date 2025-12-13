c64630d3e60b4874840a7e9ac76639b0aab95a65
Allen Houchins <allenhouchins@mac.com>
2025-12-12 21:04:53 -0600

    linting fixes

--- frontend/pages/SoftwarePage/components/icons/7Zip.tsx
+++ frontend/pages/SoftwarePage/components/icons/7Zip.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import type { SVGProps } from "react";
 
-const 7Zip = (props: SVGProps<SVGSVGElement>) => (
+const SevenZip = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} {...props}>
     <image
       width={32}
@@ -11,4 +11,4 @@ const 7Zip = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
-export default 7Zip;
+export default SevenZip;
