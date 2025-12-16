1ede4d53c4771d25f31bc9bd64360cce25833bf9
dantecatalfamo <dante.catalfamo@gmail.com>
2025-12-16 15:03:35 -0500

    Spot

--- android/app/src/main/java/com/fleetdm/agent/ui/theme/Theme.kt
+++ android/app/src/main/java/com/fleetdm/agent/ui/theme/Theme.kt
@@ -11,11 +11,11 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-//private val DarkColorScheme = darkColorScheme(
+// private val DarkColorScheme = darkColorScheme(
 //    primary = Purple80,
 //    secondary = PurpleGrey80,
 //    tertiary = Pink80,
-//)
+// )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
