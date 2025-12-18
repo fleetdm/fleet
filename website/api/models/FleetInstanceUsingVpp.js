a01371fc4c88137dcdb8e19b36aff4359ffd9e36
Eric <eashaw@sailsjs.com>
2025-12-18 11:23:03 -0600

    Add database model

---
+++ website/api/models/FleetInstanceUsingVpp.js
@@ -0,0 +1,47 @@
+/**
+ * InstanceUsingVpp.js// TODO: better name.
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    fleetInstanceUrl: {
+      type: 'string',
+      required: true,
+      unique: true,
+      description: 'The URL of a Fleet instance that is using the fleet website VPP proxy.'
+    },
+
+    fleetServerSecret: {
+      type: 'string',
+      required: true,
+      unique: true,
+      description: 'A generated secret used to autheticate & identify requests from a particular Fleet instance.'
+    },
+
+    vppToken: {
+      type: 'string',
+      required: true,
+      description: 'A VPP token used to autheticate requests to an organization\'s VPP apps.'// TODO: is that acurate?
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
