aafc607f0a5c909e91da0c2e605d62d8d43aca9b
Jordan Montgomery <elijah.jordan.montgomery@gmail.com>
2025-12-22 16:18:24 -0500

    Skip bootstrap package on non-AwaitingConfiguration hosts

--- server/service/apple_mdm.go
+++ server/service/apple_mdm.go
@@ -3520,7 +3520,7 @@ func (svc *MDMAppleCheckinAndCommandService) TokenUpdate(r *mdm.Request, m *mdm.
 		EnrollReference:         acctUUID,
 		HasSetupExperienceItems: hasSetupExpItems,
 		UserEnrollmentID:        m.EnrollmentID,
-		FromMDMMigration:        info.MigrationInProgress,
+		FromMDMMigration:        info.MigrationInProgress || (info.DEPAssignedToFleet && !m.AwaitingConfiguration),
 	})
 }
 
