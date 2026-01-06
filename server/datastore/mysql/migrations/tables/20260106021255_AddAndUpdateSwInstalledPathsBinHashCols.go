40646a4257177b22939ed90964e07632ecfb3769
Jacob Shandling <jacob@shandling.dev>
2026-01-06 14:05:13 -0800

    add new migration files

---
+++ server/datastore/mysql/migrations/tables/20260106021255_AddAndUpdateSwInstalledPathsBinHashCols.go
@@ -0,0 +1,34 @@
+package tables
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20260106021255, Down_20260106021255)
+}
+
+func Up_20260106021255(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+	ALTER TABLE host_software_installed_paths
+	ADD COLUMN binary_sha256 CHAR(64) COLLATE utf8mb4_unicode_ci NULL
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to add column 'binary_sha256' to 'host_software_installed_paths' table: %w", err)
+	}
+
+	_, err = tx.Exec(`
+	ALTER TABLE host_software_installed_paths
+	CHANGE executable_sha256 cdhash_sha256 CHAR(64) COLLATE utf8mb4_unicode_ci NULL
+	`)
+	if err != nil {
+		return fmt.Errorf("failed to update name of 'host_software_installed_paths' column 'binary_sha256' to `cdhash_sha256`: %w", err)
+	}
+
+	return nil
+}
+
+func Down_20260106021255(tx *sql.Tx) error {
+	return nil
+}
