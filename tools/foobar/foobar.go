faaac51fec4bb835ee3e708a22f395f217274058
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2026-01-06 14:26:06 -0300

    Add dependency check for some libraries

---
+++ tools/foobar/foobar.go
@@ -0,0 +1,12 @@
+package foobar
+
+import (
+	"fmt"
+
+	_ "github.com/valyala/fastjson"
+	_ "github.com/valyala/fasttemplate"
+)
+
+func main() {
+	fmt.Println("Hey there, I'm only here for testing. I will be removed before merging to main")
+}
