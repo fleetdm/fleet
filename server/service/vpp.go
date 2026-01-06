e35684268d3148df9f14b0aa6a474f309829e431
Scott Gress <scottmgress@gmail.com>
2026-01-06 10:52:26 -0600

    Add auto-update schedule to software activity updates (#37715)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** Resolves #35458
    
    # Details
    
    This PR adds new metadata to the `ActivityEditedAppStoreApp` activity
    relating to the app's auto-update schedule. The data will be included
    with every `ActivityEditedAppStoreApp` activity regardless of whether
    the values changed. I have an open question about this on the [activity
    docs
    PR](https://github.com/fleetdm/fleet/pull/36534/changes#r2648884183).
    
    One functional change to note here is that the act of recording the
    activity has been moved up a level into the endpoint code, because the
    activity now contains metadata from two different service methods (one
    that updates the VPP app, and one that creates the auto-update
    schedule).
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [X] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    ## Testing
    
    - [X] Added/updated automated tests
    - [X] QA'd all new/changed functionality manually

--- server/service/vpp.go
+++ server/service/vpp.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/docker/go-units"
+	"github.com/fleetdm/fleet/v4/server/authz"
 	"github.com/fleetdm/fleet/v4/server/contexts/ctxerr"
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/fleetdm/fleet/v4/server/service/middleware/endpoint_utils"
@@ -128,7 +129,7 @@ func (r updateAppStoreAppResponse) Error() error { return r.Err }
 func updateAppStoreAppEndpoint(ctx context.Context, request interface{}, svc fleet.Service) (fleet.Errorer, error) {
 	req := request.(*updateAppStoreAppRequest)
 
-	updatedApp, err := svc.UpdateAppStoreApp(ctx, req.TitleID, req.TeamID, fleet.AppStoreAppUpdatePayload{
+	updatedApp, activity, err := svc.UpdateAppStoreApp(ctx, req.TitleID, req.TeamID, fleet.AppStoreAppUpdatePayload{
 		SelfService:      req.SelfService,
 		LabelsIncludeAny: req.LabelsIncludeAny,
 		LabelsExcludeAny: req.LabelsExcludeAny,
@@ -152,15 +153,32 @@ func updateAppStoreAppEndpoint(ctx context.Context, request interface{}, svc fle
 		}
 	}
 
+	// Re-fetch the software title to get the updated auto-update config.
+	updatedTitle, err := svc.SoftwareTitleByID(ctx, req.TitleID, req.TeamID)
+	if err != nil {
+		return updateAppStoreAppResponse{Err: err}, nil
+	}
+	if updatedTitle.AutoUpdateEnabled != nil {
+		activity.AutoUpdateEnabled = updatedTitle.AutoUpdateEnabled
+		if *updatedTitle.AutoUpdateEnabled {
+			activity.AutoUpdateStartTime = updatedTitle.AutoUpdateStartTime
+			activity.AutoUpdateEndTime = updatedTitle.AutoUpdateEndTime
+		}
+	}
+
+	if err := svc.NewActivity(ctx, authz.UserFromContext(ctx), activity); err != nil {
+		return updateAppStoreAppResponse{Err: err}, nil
+	}
+
 	return updateAppStoreAppResponse{AppStoreApp: updatedApp}, nil
 }
 
-func (svc *Service) UpdateAppStoreApp(ctx context.Context, titleID uint, teamID *uint, payload fleet.AppStoreAppUpdatePayload) (*fleet.VPPAppStoreApp, error) {
+func (svc *Service) UpdateAppStoreApp(ctx context.Context, titleID uint, teamID *uint, payload fleet.AppStoreAppUpdatePayload) (*fleet.VPPAppStoreApp, *fleet.ActivityEditedAppStoreApp, error) {
 	// skipauth: No authorization check needed due to implementation returning
 	// only license error.
 	svc.authz.SkipAuthorization(ctx)
 
-	return nil, fleet.ErrMissingLicense
+	return nil, nil, fleet.ErrMissingLicense
 }
 
 ////////////////////////////////////////////////////////////////////////////////
