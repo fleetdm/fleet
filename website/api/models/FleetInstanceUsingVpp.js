5ebb2292d37a6757624b5e65fb24d700c8be02d8
Eric <eashaw@sailsjs.com>
2026-01-07 12:03:41 -0600

    Update database model

--- website/api/models/FleetInstanceUsingVpp.js
+++ website/api/models/FleetInstanceUsingVpp.js
@@ -15,22 +15,14 @@ module.exports = {
     fleetInstanceUrl: {
       type: 'string',
       required: true,
-      unique: true,
       description: 'The URL of a Fleet instance that is using the fleet website VPP proxy.'
     },
 
     fleetServerSecret: {
       type: 'string',
       required: true,
-      unique: true,
       description: 'A generated secret used to autheticate & identify requests from a particular Fleet instance.'
     },
-
-    vppToken: {
-      type: 'string',
-      required: true,
-      description: 'A VPP token used to autheticate requests to an organization\'s VPP apps.'// TODO: is that acurate?
-    }
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
