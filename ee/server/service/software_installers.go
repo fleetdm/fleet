83013b683c7aebcf55d764949f3960d84685c804
Magnus Jensen <magnus@fleetdm.com>
2026-01-08 12:40:11 -0500

    Revert "switch the software install apple VPP association to use the old v1 API"
    This reverts commit 86ec95ecb553d55474dae9232121dbbe29b1c468.

--- ee/server/service/software_installers.go
+++ ee/server/service/software_installers.go
@@ -1363,7 +1363,7 @@ func (svc *Service) InstallVPPAppPostValidation(ctx context.Context, host *fleet
 			}
 		}
 
-		err = vpp.ManageVPPLicenses(token, &vpp.ManageVPPLicensesRequest{AdamID: vppApp.AdamID, PricingParam: assets[0].PricingParam, AssociateSerialNumbers: []string{host.HardwareSerial}})
+		eventID, err = vpp.AssociateAssets(token, &vpp.AssociateAssetsRequest{Assets: assets, SerialNumbers: []string{host.HardwareSerial}})
 		if err != nil {
 			return "", ctxerr.Wrapf(ctx, err, "associating asset with adamID %s to host %s", vppApp.AdamID, host.HardwareSerial)
 		}
