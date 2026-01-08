d5566c539f275d9987ad5d5af650119450d2f2b7
Ian Littman <iansltx@gmail.com>
2026-01-08 17:45:09 -0600

    Fix showing all labels when we shouldn't in new query targets UI

--- frontend/pages/queries/edit/components/SaveNewQueryModal/SaveNewQueryModal.tsx
+++ frontend/pages/queries/edit/components/SaveNewQueryModal/SaveNewQueryModal.tsx
@@ -77,7 +77,7 @@ const SaveNewQueryModal = ({
   queryReportsDisabled,
   platformSelector,
 }: ISaveNewQueryModalProps): JSX.Element => {
-  const { config, isPremiumTier } = useContext(AppContext);
+  const { config, isPremiumTier, currentTeam } = useContext(AppContext);
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
@@ -107,7 +107,16 @@ const SaveNewQueryModal = ({
     isFetching: isFetchingLabels,
   } = useQuery<ILabelsSummaryResponse, Error>(
     ["custom_labels"],
-    () => labelsAPI.summary(apiTeamIdForQuery, true),
+    () => {
+      // Wait for the current team to load from context before pulling labels, otherwise on a page load
+      // directly on the page this gets called with currentTeam not set, then again
+      // with the correct team value. If we don't trigger on currentTeam changes we'll just start with a
+      // null team ID here and never populate with the correct team unless we navigate from another page
+      // where team context is already set prior to navigation.
+      return !currentTeam
+        ? ({ labels: [] } as ILabelsSummaryResponse)
+        : labelsAPI.summary(currentTeam?.id, true);
+    },
     {
       ...DEFAULT_USE_QUERY_OPTIONS,
       enabled: isPremiumTier,
