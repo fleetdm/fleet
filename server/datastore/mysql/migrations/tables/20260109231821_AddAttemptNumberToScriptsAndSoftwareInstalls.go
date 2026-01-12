992274780dd862357fb75047a0c4197d979df89f
Konstantin Sykulev <konst@sykulev.com>
2026-01-12 11:08:29 -0600

    adding indexes

--- server/datastore/mysql/migrations/tables/20260109231821_AddAttemptNumberToScriptsAndSoftwareInstalls.go
+++ server/datastore/mysql/migrations/tables/20260109231821_AddAttemptNumberToScriptsAndSoftwareInstalls.go
@@ -26,6 +26,21 @@ func Up_20260109231821(tx *sql.Tx) error {
 		return errors.Wrap(err, "adding attempt_number column to host_software_installs")
 	}
 
+	_, err = tx.Exec(`
+		ALTER TABLE host_script_results
+		ADD INDEX idx_host_script_results_host_policy_attempt (host_id, policy_id, attempt_number);
+	`)
+	if err != nil {
+		return errors.Wrap(err, "adding index to host_script_results")
+	}
+	_, err = tx.Exec(`
+		ALTER TABLE host_software_installs
+		ADD INDEX idx_host_software_installs_host_policy_attempt (host_id, policy_id, attempt_number);
+	`)
+	if err != nil {
+		return errors.Wrap(err, "adding index to host_software_installs")
+	}
+
 	return nil
 }
 
