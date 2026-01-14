66fa9e46e19dad83ffbeccf60496bb69aaa4cf1c
RachelElysia <rachel@fleetdm.com>
2026-01-14 15:41:33 -0500

    use common fleet.IsNotFound for conditional

--- ee/server/service/software_installers.go
+++ ee/server/service/software_installers.go
@@ -2002,7 +2002,7 @@ func (svc *Service) softwareInstallerPayloadFromSlug(ctx context.Context, payloa
 	app, err := svc.ds.GetMaintainedAppBySlug(ctx, *slug, teamID)
 	if err != nil {
 		// Return user-friendly message for generic not found error
-		if strings.Contains(err.Error(), "MaintainedApp was not found in the datastore") {
+		if fleet.IsNotFound(err) {
 			// Must return low-level error in order to be properly handled upstream
 			return fleet.NewUserMessageError(
 				fmt.Errorf("%s isn't a supported Fleet-maintained app. See supported apps: https://fleetdm.com/learn-more-about/supported-fleet-maintained-app-slugs", *slug),
