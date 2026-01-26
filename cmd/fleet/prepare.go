45cef8baac3f6ccc025d45c78dc2428e6add2308
Ian Littman <iansltx@gmail.com>
2026-01-26 12:39:42 -0600

    Only show row counts after migration

--- cmd/fleet/prepare.go
+++ cmd/fleet/prepare.go
@@ -50,27 +50,10 @@ To setup Fleet infrastructure, use one of the available commands.
 				initFatal(err, "creating db connection")
 			}
 
-			showTableStatsFn := func() {}
-
-			if showTableStats {
-				showTableStatsFn = func() {
-					stats, err := ds.GetTableRowCounts(cmd.Context())
-					if err != nil {
-						initFatal(err, "getting table stats")
-					}
-					statsAsJSON, err := json.Marshal(stats)
-					if err != nil {
-						initFatal(err, "encoding table row counts to JSON")
-					}
-					fmt.Printf("Table Row Counts: %s\n", statsAsJSON)
-				}
-			}
-
 			status, err := ds.MigrationStatus(cmd.Context())
 			if err != nil {
 				initFatal(err, "retrieving migration status")
 			}
-			showTableStatsFn()
 			if status.StatusCode == fleet.NeedsFleetv4732Fix {
 				if !noPrompt {
 					printFleetv4732FixMessage()
@@ -117,13 +100,24 @@ To setup Fleet infrastructure, use one of the available commands.
 			}
 
 			fmt.Println("Migrations completed.")
-			showTableStatsFn()
+
+			if showTableStats {
+				stats, err := ds.GetTableRowCounts(cmd.Context())
+				if err != nil {
+					initFatal(err, "getting table stats")
+				}
+				statsAsJSON, err := json.Marshal(stats)
+				if err != nil {
+					initFatal(err, "encoding table row counts to JSON")
+				}
+				fmt.Printf("Table Row Counts: %s\n", statsAsJSON)
+			}
 		},
 	}
 
 	dbCmd.PersistentFlags().BoolVar(&noPrompt, "no-prompt", false, "disable prompting before migrations (for use in scripts)")
 	dbCmd.PersistentFlags().BoolVar(&dev, "dev", false, "Enable developer options")
-	dbCmd.PersistentFlags().BoolVar(&showTableStats, "with-table-stats", false, "Show approximate table row counts before and after migrations")
+	dbCmd.PersistentFlags().BoolVar(&showTableStats, "with-table-stats", false, "Show approximate table row counts after migrations")
 
 	prepareCmd.AddCommand(dbCmd)
 	return prepareCmd
