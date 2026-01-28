c8cc4d088ce0eed2b3c8a12ca18e3fa8775bc85f
Jacob Shandling <jacob@shandling.dev>
2026-01-28 10:07:32 -0800

    use full names first for labels and list

--- frontend/pages/admin/IntegrationsPage/cards/CertificateAuthorities/components/helpers.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/CertificateAuthorities/components/helpers.tsx
@@ -1,12 +1,12 @@
 import { ICertificateAuthorityType } from "interfaces/certificates";
 
 const CA_LABEL_BY_TYPE: Record<ICertificateAuthorityType, string> = {
-  custom_est_proxy: "Custom EST (Enrollment Over Secure Transport)",
-  custom_scep_proxy: "Custom SCEP (Simple Certificate Enrollment Protocol)",
+  custom_est_proxy: "Custom Enrollment Over Secure Transport (EST)",
+  custom_scep_proxy: "Custom Simple Certificate Enrollment Protocol (SCEP)",
   digicert: "DigiCert",
-  hydrant: "Hydrant EST (Enrollment Over Secure Transport)",
+  hydrant: "Hydrant Enrollment Over Secure Transport (EST)",
   ndes_scep_proxy:
-    "Okta CA or Microsoft NDES (Network Device Enrollment Service)",
+    "Okta CA or Microsoft Network Device Enrollment Service (NDES)",
   smallstep: "Smallstep",
 };
 
