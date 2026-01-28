d415d92a3161f61dbe045ce2e3d67dc334c556a0
dantecatalfamo <dante.catalfamo@gmail.com>
2026-01-28 13:25:31 -0500

    Add missing bypass modal

---
+++ frontend/pages/hosts/details/DeviceUserPage/BypassModal/BypassModal.tsx
@@ -0,0 +1,38 @@
+import Button from "components/buttons/Button";
+import Modal from "components/Modal";
+import React from "react";
+
+const baseClass = "device-bypass-modal";
+
+interface IBypassModal {
+  onCancel: () => void;
+  onResolveLater: () => void;
+  isLoading: boolean;
+}
+
+const BypassModal = ({ onCancel, onResolveLater, isLoading }: IBypassModal) => {
+  return (
+    <Modal onExit={onCancel} title="Resolve later">
+      <>
+        <p>
+          This will allow you to log in with Okta once.
+          <br />
+          <br />
+          {`Please resolve all policies marked "Action required" to restore access for subsequent logins.`}
+        </p>
+        <div className="modal-cta-wrap">
+          <Button
+            type="button"
+            onClick={onResolveLater}
+            isLoading={isLoading}
+            disabled={isLoading}
+          >
+            Resolve later
+          </Button>
+        </div>
+      </>
+    </Modal>
+  );
+};
+
+export default BypassModal;
