952f34eaaadc7e29a7aa010a20632a578fb71cad
dantecatalfamo <dante.catalfamo@gmail.com>
2026-02-02 13:54:35 -0500

    Make sure the conditional access bypass only shows up for okta

--- frontend/pages/admin/IntegrationsPage/cards/ConditionalAccess/ConditionalAccess.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/ConditionalAccess/ConditionalAccess.tsx
@@ -500,7 +500,7 @@ const ConditionalAccess = () => {
           />
         )}
       </div>
-      {(oktaConfigured || entraPhase === EntraPhase.Configured) && (
+      {oktaConfigured && (
         <div className={`${baseClass}__end-user-experience`}>
           <SectionHeader title="End user experience" />
           <form onSubmit={handleSaveBypassSettings}>
