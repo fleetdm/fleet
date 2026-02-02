38870d92f9e2b61e8a7842abc290b103c07757ec
Magnus Jensen <magnus@fleetdm.com>
2026-02-02 14:47:03 -0500

    bump limits

--- server/platform/http/request.go
+++ server/platform/http/request.go
@@ -11,11 +11,11 @@ const (
 	// anything above that limit (primarily files) will be written to temp disk files
 	MaxMultipartFormSize     int64 = 1 * units.MiB
 	MaxScriptSize            int64 = 1 * units.MiB
-	MaxBatchScriptSize       int64 = 10 * units.MiB
+	MaxBatchScriptSize       int64 = 25 * units.MiB
 	MaxProfileSize           int64 = 1 * units.MiB
-	MaxBatchProfileSize      int64 = 10 * units.MiB
+	MaxBatchProfileSize      int64 = 25 * units.MiB
 	MaxEULASize              int64 = 500 * units.MiB
-	MaxMDMCommandSize        int64 = 1 * units.MiB
+	MaxMDMCommandSize        int64 = 2 * units.MiB
 	MaxSoftwareInstallerSize int64 = 10 * units.GiB
 	// MaxMultiScriptQuerySize, sets a max size for payloads that take multiple scripts and SQL queries.
 	MaxMultiScriptQuerySize int64 = 5 * units.MiB
