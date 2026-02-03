4cc4445ece77c6f40f3a7eec5c057c6dbd08d325
Eric <eashaw@sailsjs.com>
2026-02-03 17:24:10 -0600

    Update login.page.js

--- website/assets/js/pages/entrance/login.page.js
+++ website/assets/js/pages/entrance/login.page.js
@@ -3,15 +3,20 @@ parasails.registerPage('login', {
   //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
   //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
   data: {
-
-
     formToDisplay: 'signup',
     // Shared by forms
+    // Main syncing/loading state for this page.
     syncing: false,
+    // Server error state for the form
     cloudError: undefined,
-    // Signup form
+
+    // Signup form data
     signupFormData: {},
+    // For tracking client-side validation errors in our form.
+    // > Has property set to `true` for each invalid property in `formData`.
     signupFormErrors: {},
+    // A set of validation rules for our form.
+    // > The form will not be submitted if these are invalid.
     signupFormRules: {
       firstName: {required: true},
       lastName: {required: true},
@@ -25,64 +30,37 @@ parasails.registerPage('login', {
         }
       },
     },
-    // Login form
+
+    // Login form data
     loginFormData: {},
+    // For tracking client-side validation errors in our form.
+    // > Has property set to `true` for each invalid property in `formData`.
     loginFormErrors: {},
+    // A set of validation rules for our form.
+    // > The form will not be submitted if these are invalid.
     loginFormRules: {
       emailAddress: {required: true, isEmail: true},
       password: {required: true},
     },
-
-
-    // Main syncing/loading state for this page.
-    // syncing: false,
-
-    // Form data
-    formData: {
-      rememberMe: true,
-    },
-
-    // For tracking client-side validation errors in our form.
-    // > Has property set to `true` for each invalid property in `formData`.
-    formErrors: { /* … */ },
-
-    // A set of validation rules for our form.
-    // > The form will not be submitted if these are invalid.
-    formRules: {
-      emailAddress: { required: true, isEmail: true },
-      password: { required: true },
-    },
-
-    // Server error state for the form
-    // cloudError: '',
-    showCustomerLogin: true,
     // For redirecting users coming from the "Get your license" link to the license dispenser.
-    registerSlug: '/register',
-    pageToRedirectToAfterLogin: '/try',
+    pageToRedirectToAfterFormSubmission: '/try',
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
   //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
   //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
   beforeMount: function() {
-    if(window.location.search === '?admin') {
-      this.showCustomerLogin = false;
-    }
-
     if(window.location.hash){
       // If we're redirecting this user to the license dispenser after they log in, modify the link to the /register page and the pageToRedirectToAfterLogin.
       if(window.location.hash === '#purchaseLicense'){
-        this.registerSlug = '/register#purchaseLicense';
-        this.pageToRedirectToAfterLogin = '/new-license';
+        this.pageToRedirectToAfterFormSubmission = '/new-license';
         window.location.hash = '';
-      // If we're redirecting this user to the contact form after they log in, modify the link to the /register page and the pageToRedirectToAfterLogin.
+      // If we're redirecting this user to the contact form after they log in, modify the link to the /register page and the pageToRedirectToAfterFormSubmission.
       } else if(window.location.hash === '#contact'){
-        this.registerSlug = '/register';
-        this.pageToRedirectToAfterLogin = '/contact?sendMessage';
+        this.pageToRedirectToAfterFormSubmission = '/contact?sendMessage';
         window.location.hash = '';
       } else if(window.location.hash === '#tryfleet'){
-        this.registerSlug = '/register#tryfleet';
-        this.pageToRedirectToAfterLogin = '/try-fleet';
+        this.pageToRedirectToAfterFormSubmission = '/try-fleet';
         window.location.hash = '';
       }
     }
@@ -111,6 +89,7 @@ parasails.registerPage('login', {
         this.signupFormErrors = _.omit(this.signupFormErrors, field);
       }
     },
+
     submittedSignupForm: async function(){
       this.syncing = true;
       // Track a "key event" in Google Analytics.
@@ -124,18 +103,11 @@ parasails.registerPage('login', {
       if(window.lintrk !== undefined) {
         window.lintrk('track', { conversion_id: 18587097 });// eslint-disable-line camelcase
       }
-      this.goto('/try');
+      this.goto(this.pageToRedirectToAfterFormSubmission);
     },
     submittedLoginForm: async function() {
       this.syncing = true;
-      this.goto('/try');
-    },
-    submittedForm: async function() {
-      // Redirect to the /start page on success.
-      // > (Note that we re-enable the syncing state here.  This is on purpose--
-      // > to make sure the spinner stays there until the page navigation finishes.)
-      this.syncing = true;
-      this.goto(this.pageToRedirectToAfterLogin);
+      this.goto(this.pageToRedirectToAfterFormSubmission);
     },
 
     clickGoBack: function () {
