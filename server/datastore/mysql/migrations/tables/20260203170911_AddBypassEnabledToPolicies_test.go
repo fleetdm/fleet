54f5b9f962974fab87327d7333f21673a4aba740
dantecatalfamo <dante.catalfamo@gmail.com>
2026-02-04 16:11:12 -0500

    Linting error

--- server/datastore/mysql/migrations/tables/20260203170911_AddBypassEnabledToPolicies_test.go
+++ server/datastore/mysql/migrations/tables/20260203170911_AddBypassEnabledToPolicies_test.go
@@ -10,34 +10,37 @@ import (
 func TestUp_20260203170911(t *testing.T) {
 	db := applyUpToPrev(t)
 
-	db.Exec(`
+	_, err := db.Exec(`
 		INSERT INTO policies (name, query) VALUES
 			('test1', 'SELECT 1'),
 			('test2', 'SELECT 1')
 	`)
+	require.NoError(t, err)
 
 	// Apply current migration.
 	applyNext(t, db)
 
 	// Existing policies should have the new column set to true after migration
 	// New policies should default to true when no value defined
-	db.Exec(`
+	_, err = db.Exec(`
 		INSERT INTO policies (name, query) VALUES
 			('test3', 'SELECT 1'),
 	`)
+	require.NoError(t, err)
 
 	// Only new policies with explicit false should be set to false
-	db.Exec(`
+	_, err = db.Exec(`
 		INSERT INTO policies (name, query, conditional_access_bypass_enabled) VALUES
 			('test4', 'SELECT 1', false),
 	`)
+	require.NoError(t, err)
 
 	var policies []struct {
 		Name          string `db:"name"`
 		BypassEnabled bool   `db:"conditional_access_bypass_enabled"`
 	}
 
-	err := sqlx.Select(db, &policies, `SELECT name, conditional_access_bypass_enabled FROM policies`)
+	err = sqlx.Select(db, &policies, `SELECT name, conditional_access_bypass_enabled FROM policies`)
 	require.NoError(t, err)
 
 	for _, p := range policies {
