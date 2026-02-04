cddefbf835ac9ab9f654709db611384a6864afa5
Savannah Friend <157323611+SFriendLee@users.noreply.github.com>
2026-02-04 17:21:14 -0600

    Update comment to reflect website context (#38590)

--- website/config/routes.js
+++ website/config/routes.js
@@ -2,7 +2,7 @@
  * Route Mappings
  * (sails.config.routes)
  *
- * Your routes tell Sails what to do each time it receives a request.
+ * Your routes tell the website what to do each time it receives a request.
  *
  * For more information on configuring custom routes, check out:
  * https://sailsjs.com/anatomy/config/routes-js
