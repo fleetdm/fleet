e94d99e422fd2a79622c63f25d217ef4c7d486dd
jkatz01 <yehonatankatz@gmail.com>
2026-02-10 16:03:43 -0500

    small formatting change

--- server/datastore/mysql/android.go
+++ server/datastore/mysql/android.go
@@ -1229,7 +1229,7 @@ func (ds *Datastore) ListHostMDMAndroidProfilesPendingOrFailedInstallWithVersion
 		SELECT profile_uuid, host_uuid, status, operation_type, detail, profile_name, policy_request_uuid, device_request_uuid, request_fail_count, included_in_policy_version
 		FROM host_mdm_android_profiles
 		WHERE host_uuid = ? AND included_in_policy_version <= ? AND operation_type = ? 
-		AND (status = 'pending' OR (status = 'failed' AND can_reverify = true) )
+		AND (status = 'pending' OR (status = 'failed' AND can_reverify = true))
 	`
 
 	var profiles []*fleet.MDMAndroidProfilePayload
