f15f62cdd8e647ddf69b22bb138bd852162d9a89
Magnus Jensen <magnus@fleetdm.com>
2026-02-10 11:18:28 -0500

    add migration steps

--- server/datastore/mysql/migrations/tables/20260210151544_AddAPNSQueryIndexes.go
+++ server/datastore/mysql/migrations/tables/20260210151544_AddAPNSQueryIndexes.go
@@ -9,17 +9,15 @@ func init() {
 }
 
 func Up_20260210151544(tx *sql.Tx) error {
-	_, err := tx.Exec(`
-CREATE INDEX idx_neq_filter ON nano_enrollment_queue (
+	return withSteps([]migrationStep{
+		basicMigrationStep(`CREATE INDEX idx_neq_filter ON nano_enrollment_queue (
     active,
     priority,
     created_at,
     id
-);
-
-CREATE INDEX idx_ncr_lookup ON nano_command_results (id, command_uuid, status);`)
-
-	return err
+);`, "creating index idx_neq_filter on nano_enrollment_queue"),
+		basicMigrationStep(`CREATE INDEX idx_ncr_lookup ON nano_command_results (id, command_uuid, status);`, "creating index idx_ncr_lookup on nano_command_results"),
+	}, tx)
 }
 
 func Down_20260210151544(tx *sql.Tx) error {
