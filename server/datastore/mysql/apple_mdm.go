06803dc486f1a3d65cc4aef1d37c3ba70e03a9bf
Magnus Jensen <magnus@fleetdm.com>
2026-02-10 10:32:50 -0500

    remove join on nano_enrollments

--- server/datastore/mysql/apple_mdm.go
+++ server/datastore/mysql/apple_mdm.go
@@ -6304,12 +6304,10 @@ SELECT DISTINCT
     neq.id
 FROM
     nano_enrollment_queue neq
-    INNER JOIN nano_enrollments ne ON ne.id = neq.id
     LEFT JOIN nano_command_results ncr ON ncr.command_uuid = neq.command_uuid
     AND ncr.id = neq.id
 WHERE
     neq.active = 1
-    AND ne.enabled = 1
     AND ncr.status IS NULL
     AND neq.created_at >= NOW() - INTERVAL 7 DAY
     AND neq.priority IN (0, 1)
