9336cbe56426c854d6890c8fa34a5d1c678b2ad5
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2026-02-11 09:54:33 -0500

    Apply suggestion from @noahtalerman

--- frontend/pages/queries/edit/EditQueryPage.tsx
+++ frontend/pages/queries/edit/EditQueryPage.tsx
@@ -316,7 +316,7 @@ const EditQueryPage = ({
 
     try {
       await queryAPI.update(queryId, updatedQuery);
-      renderFlash("success", "Query saved.");
+      renderFlash("success", "Query updated.");
       refetchStoredQuery(); // Required to compare recently saved query to a subsequent save to the query
     } catch (updateError: any) {
       console.error(updateError);
