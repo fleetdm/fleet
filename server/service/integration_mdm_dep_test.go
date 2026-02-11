0ea86bc18fb239f95a987e96a04bcad8c7b2b3f0
Scott Gress <scott@fleetdm.com>
2026-02-11 16:20:04 -0600

    more test fixes

--- server/service/integration_mdm_dep_test.go
+++ server/service/integration_mdm_dep_test.go
@@ -954,7 +954,7 @@ func (s *integrationMDMTestSuite) TestDEPProfileAssignment() {
 		require.NotNil(t, pending[0].Args)
 		var gotArgs struct {
 			Task              string   `json:"task"`
-			TeamID            *uint    `json:"fleet_id,omitempty" renamedfrom:"team_id"`
+			TeamID            *uint    `json:"team_id,omitempty"`
 			HostSerialNumbers []string `json:"host_serial_numbers,omitempty"`
 		}
 		require.NoError(t, json.Unmarshal(*pending[0].Args, &gotArgs))
