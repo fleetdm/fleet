28b6c8c43d7b5a8eb6b2e5af8523ac2b945532b0
Gabriel Hernandez <ghernandez345@gmail.com>
2026-02-12 12:40:47 +0000

    update windows enrollment card to handle all three cases

--- frontend/pages/admin/IntegrationsPage/cards/MdmSettings/components/MicrosoftEntraSection/WindowsAutomaticEnrollmentCard/WindowsEnrollmentCard.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/MdmSettings/components/MicrosoftEntraSection/WindowsAutomaticEnrollmentCard/WindowsEnrollmentCard.tsx
@@ -6,35 +6,67 @@ import SectionCard from "../../SectionCard";
 
 interface IWindowsAutomaticEnrollmentCardProps {
   windowsMdmEnabled: boolean;
+  tenantAdded: boolean;
   viewDetails: () => void;
 }
 
+const WindowsMdmDisabledCard = (
+  <SectionCard header="Windows enrollment">
+    To enable end users to enroll to Fleet via Microsoft Entra (e.g. Autopilot),
+    first turn on Windows MDM.
+  </SectionCard>
+);
+
+interface IWindowsTenantAddedCardProps {
+  editTenants: () => void;
+}
+
+const WindowsTenantAddedCard = ({
+  editTenants,
+}: IWindowsTenantAddedCardProps) => (
+  <SectionCard
+    iconName="success"
+    cta={
+      <Button onClick={editTenants} variant="inverse" iconStroke>
+        <Icon name="pencil" />
+        Edit
+      </Button>
+    }
+  >
+    Microsoft Entra tenant ID added.
+  </SectionCard>
+);
+
+interface IWindowsTenantNotAddedCardProps {
+  addTenant: () => void;
+}
+
+const WindowsTenantNotAddedCard = ({
+  addTenant,
+}: IWindowsTenantNotAddedCardProps) => (
+  <SectionCard
+    header="Windows enrollment"
+    cta={<Button onClick={addTenant}>Connect</Button>}
+  >
+    To enable end users to enroll to Fleet via Microsoft Entra (e.g. Autopilot),
+    you need to add Entra tenant ID first.
+  </SectionCard>
+);
+
 const WindowsAutomaticEnrollmentCard = ({
   windowsMdmEnabled,
+  tenantAdded,
   viewDetails,
 }: IWindowsAutomaticEnrollmentCardProps) => {
-  const contentText = (
-    <>
-      To enable end users to enroll to Fleet via Microsoft Entra (e.g.
-      Autopilot),{" "}
-      {windowsMdmEnabled
-        ? "you need to add Entra tenant ID first."
-        : "first turn on Windows MDM."}
-    </>
-  );
+  if (!windowsMdmEnabled) {
+    return WindowsMdmDisabledCard;
+  }
 
-  return (
-    <SectionCard
-      header="Windows enrollment"
-      cta={
-        windowsMdmEnabled ? (
-          <Button onClick={viewDetails}>Connect</Button>
-        ) : undefined
-      }
-    >
-      {contentText}
-    </SectionCard>
-  );
+  if (tenantAdded) {
+    return <WindowsTenantAddedCard editTenants={viewDetails} />;
+  }
+
+  return <WindowsTenantNotAddedCard addTenant={viewDetails} />;
 };
 
 export default WindowsAutomaticEnrollmentCard;
