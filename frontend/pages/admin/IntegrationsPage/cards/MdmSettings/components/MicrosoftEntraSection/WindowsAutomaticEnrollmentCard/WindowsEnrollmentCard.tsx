5fcbc691a8a744a6a497fa95dddd4931f6157d45
Gabriel Hernandez <ghernandez345@gmail.com>
2026-02-12 12:01:27 +0000

    update windows enrollment card copy

--- frontend/pages/admin/IntegrationsPage/cards/MdmSettings/components/MicrosoftEntraSection/WindowsAutomaticEnrollmentCard/WindowsEnrollmentCard.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/MdmSettings/components/MicrosoftEntraSection/WindowsAutomaticEnrollmentCard/WindowsEnrollmentCard.tsx
@@ -5,23 +5,36 @@ import Icon from "components/Icon/Icon";
 import SectionCard from "../../SectionCard";
 
 interface IWindowsAutomaticEnrollmentCardProps {
+  windowsMdmEnabled: boolean;
   viewDetails: () => void;
 }
 
 const WindowsAutomaticEnrollmentCard = ({
+  windowsMdmEnabled,
   viewDetails,
 }: IWindowsAutomaticEnrollmentCardProps) => {
+  const contentText = (
+    <>
+      To enable end users to enroll to Fleet via Microsoft Entra (e.g.
+      Autopilot),{" "}
+      {windowsMdmEnabled
+        ? "you need to connect Fleet to Entra first."
+        : "first turn on Windows MDM."}{" "}
+    </>
+  );
+
   return (
     <SectionCard
       header="Windows enrollment"
       cta={
-        <Button onClick={viewDetails} variant="inverse" iconStroke>
-          Details <Icon name="chevron-right" color="ui-fleet-black-75" />
-        </Button>
+        windowsMdmEnabled ? (
+          <Button onClick={viewDetails} variant="inverse" iconStroke>
+            Details <Icon name="chevron-right" color="ui-fleet-black-75" />
+          </Button>
+        ) : undefined
       }
     >
-      To enable end users to enroll to Fleet via Microsoft Entra (e.g.
-      Autopilot), you need to connect Fleet to Entra first.
+      {contentText}
     </SectionCard>
   );
 };
