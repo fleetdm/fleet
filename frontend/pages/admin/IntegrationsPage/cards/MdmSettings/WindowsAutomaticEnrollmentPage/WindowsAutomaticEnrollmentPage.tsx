28e88943642a72d2654b7c26fcdf57ad37e4c483
Gabriel Hernandez <ghernandez345@gmail.com>
2026-02-13 11:03:48 +0000

    redo windows automatic enrollment page layout

--- frontend/pages/admin/IntegrationsPage/cards/MdmSettings/WindowsAutomaticEnrollmentPage/WindowsAutomaticEnrollmentPage.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/MdmSettings/WindowsAutomaticEnrollmentPage/WindowsAutomaticEnrollmentPage.tsx
@@ -8,10 +8,7 @@ import InputField from "components/forms/fields/InputField";
 import BackButton from "components/BackButton";
 import MainContent from "components/MainContent";
 import CustomLink from "components/CustomLink/CustomLink";
-import InfoBanner from "components/InfoBanner";
-import Icon from "components/Icon";
 import PageDescription from "components/PageDescription";
-import Card from "components/Card";
 
 const generateMdmTermsOfUseUrl = (domain: string) => {
   return `${domain}/api/mdm/microsoft/tos`;
@@ -37,49 +34,57 @@ const WindowsAutomaticEnrollmentPage = () => {
           />
         </div>
         <h1>Microsoft Entra</h1>
-        <PageDescription
-          content={
-            <>
-              To connect Fleet to Microsoft Entra, follow the instructions in
-              the{" "}
-              <CustomLink
-                newTab
-                text="guide"
-                url="https://fleetdm.com/learn-more-about/connect-microsoft-entra"
-              />
-            </>
-          }
-        />
-        <Card className={`${baseClass}__card`}>
-          <p>
-            You will need to copy and paste these values to create the
-            application in Microsoft Entra.
-          </p>
-          <div className={`${baseClass}__url-inputs-wrapper`}>
-            <InputField
-              inputWrapperClass={`${baseClass}__url-input`}
-              label="MDM terms of use URL"
-              name="mdmTermsOfUseUrl"
-              tooltip="The terms of use URL is used to display the terms of service to end users
+        <div className={`${baseClass}__content-container`}>
+          <PageDescription
+            content={
+              <>
+                To connect Fleet to Microsoft Entra, follow the instructions in
+                the{" "}
+                <CustomLink
+                  newTab
+                  text="guide"
+                  url="https://fleetdm.com/learn-more-about/connect-microsoft-entra"
+                />
+              </>
+            }
+          />
+          <section className={`${baseClass}__mdm-urls-container`}>
+            <h2>MDM URLs</h2>
+            <div>
+              <p>
+                You will need to copy and paste these values to create the
+                application in Microsoft Entra.
+              </p>
+              <div className={`${baseClass}__url-inputs-wrapper`}>
+                <InputField
+                  inputWrapperClass={`${baseClass}__url-input`}
+                  label="MDM terms of use URL"
+                  name="mdmTermsOfUseUrl"
+                  tooltip="The terms of use URL is used to display the terms of service to end users
               before turning on MDM for their host. The terms of use text informs users about
               policies that will be enforced on the host."
-              value={generateMdmTermsOfUseUrl(
-                config?.server_settings.server_url || ""
-              )}
-              enableCopy
-            />
-            <InputField
-              inputWrapperClass={`${baseClass}__url-input`}
-              label="MDM discovery URL"
-              name="mdmDiscoveryUrl"
-              tooltip="The enrollment URL is used to connect hosts with the MDM service."
-              value={generateMdmDiscoveryUrl(
-                config?.server_settings.server_url || ""
-              )}
-              enableCopy
-            />
-          </div>
-        </Card>
+                  value={generateMdmTermsOfUseUrl(
+                    config?.server_settings.server_url || ""
+                  )}
+                  enableCopy
+                />
+                <InputField
+                  inputWrapperClass={`${baseClass}__url-input`}
+                  label="MDM discovery URL"
+                  name="mdmDiscoveryUrl"
+                  tooltip="The enrollment URL is used to connect hosts with the MDM service."
+                  value={generateMdmDiscoveryUrl(
+                    config?.server_settings.server_url || ""
+                  )}
+                  enableCopy
+                />
+              </div>
+            </div>
+          </section>
+          <section className={`${baseClass}__tenants-container`}>
+            <h2>Entra tenants</h2>
+          </section>
+        </div>
       </>
     </MainContent>
   );
