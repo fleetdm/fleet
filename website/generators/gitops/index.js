f546ce3c47da41e669819360c0e44d7b791fac52
Mike McNeil <mikermcneil@users.noreply.github.com>
2026-02-13 06:57:59 -0600

    add placeholder generator that simulates `fleetctl new`

---
+++ website/generators/gitops/index.js
@@ -0,0 +1,54 @@
+module.exports = {
+
+  targets: {
+
+    './it-and-security/.gitignore': { file: { contents: '# todo' } },
+    './it-and-security/README.md': { file: { contents: 'todo' } },
+    './it-and-security/default.yml': { file: { contents: '# todo' } },
+    './it-and-security/fleets/': { folder: {} },
+    './it-and-security/fleets/workstations.yml': { file: { contents: '# todo' } },
+    './it-and-security/fleets/personal-mobile-devices.yml': { file: { contents: '# todo' } },
+    './it-and-security/fleets/company-owned-mobile-devices.yml': { file: { contents: '# todo' } },
+    './it-and-security/labels/': { folder: {} },
+    './it-and-security/icons/': { folder: {} },
+    './it-and-security/platforms/': { folder: {} },
+    './it-and-security/platforms/linux': { folder: {} },
+    './it-and-security/platforms/windows': { folder: {} },
+    './it-and-security/platforms/macos': { folder: {} },
+    './it-and-security/platforms/ios': { folder: {} },
+    './it-and-security/platforms/ipados': { folder: {} },
+    './it-and-security/platforms/tvos': { folder: {} },
+    './it-and-security/platforms/android': { folder: {} },
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // • e.g. create a folder:
+    // ```
+    // './hey_look_a_folder': { folder: {} }
+    // ```
+    //
+    // • e.g. create a dynamically-named file relative to `scope.rootPath`
+    // (defined by the `filename` scope variable).
+    //
+    // The `template` helper reads the specified template, making the
+    // entire scope available to it (uses underscore/JST/ejs syntax).
+    // Then the file is copied into the specified destination (on the left).
+    // ```
+    // './:filename': { template: 'example.template.js' },
+    // ```
+    //
+    // • See https://sailsjs.com/docs/concepts/extending-sails/generators for more documentation.
+    // (Or visit https://sailsjs.com/support and talk to a maintainer of a core or community generator.)
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  },
+
+
+  /**
+   * The absolute path to the `templates` for this generator
+   * (for use with the `template` and `copy` builtins)
+   *
+   * @type {String}
+   */
+  templatesDirectory: path.resolve(__dirname, './templates')
+
+};
