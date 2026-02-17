b83aed25373cc8960d20a7b9bc39d2199bea6654
Eric <eashaw@sailsjs.com>
2026-02-17 11:59:30 -0600

    Website: Create admin page for resetting Fleet Premium trials (#39980)
    Changes:
    - Added /admin/reset-trial, a page where website admins can reset local
    Fleet Premium trials.
    - Added a link to the reset trial page to the navigation on admin pages.

--- website/config/routes.js
+++ website/config/routes.js
@@ -389,6 +389,14 @@ module.exports.routes = {
     }
   },
 
+  'GET /admin/reset-trial': {
+    action: 'admin/view-reset-trial',
+    locals: {
+      hideFooterLinks: true,
+      showAdminLinks: true,
+    }
+  },
+
 
   'GET /connect-vanta': {
     action: 'view-connect-vanta',
@@ -1257,6 +1265,7 @@ module.exports.routes = {
   'POST /api/v1/get-llm-generated-configuration-profile': { action: 'get-llm-generated-configuration-profile', hasSocketFeatures: true },
   'POST /api/v1/deliver-application-submission': { action: 'deliver-application-submission' },
   'POST /api/v1/deliver-gitops-request': { action: 'deliver-gitops-workshop-request' },
+  'POST /api/v1/admin/reset-one-fleet-premium-local-trial': { action: 'admin/reset-one-fleet-premium-local-trial' },
 
 
   //  ╔╦╗╦╔═╗╦═╗╔═╗╔═╗╔═╗╔═╗╔╦╗  ╔═╗╦═╗╔═╗═╗ ╦╦ ╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
