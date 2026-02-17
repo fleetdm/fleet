c4c39259b499b4698df3d7895cddfbdf750e2fd6
Marko Lisica <83164494+marko-lisica@users.noreply.github.com>
2026-02-17 17:12:15 +0100

    Apply suggestion from @kiloconnect[bot]
    Co-authored-by: kiloconnect[bot] <240665456+kiloconnect[bot]@users.noreply.github.com>

--- frontend/pages/hosts/details/HostDetailsPage/modals/LockModal/LockModal.tests.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/modals/LockModal/LockModal.tests.tsx
@@ -61,7 +61,7 @@ describe("LockModal", () => {
     expect(
       screen.getByText(
         /won't be able to receive MDM commands necessary to unlock the host/i
-      )
+        /won't be able to unlock the host remotely/i
     ).toBeInTheDocument();
   });
 
