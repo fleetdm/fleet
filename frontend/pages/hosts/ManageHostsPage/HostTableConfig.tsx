fa616047d3085e42ef46570109788f7d1daa9cf5
Luke Heath <luke@fleetdm.com>
2026-02-16 18:33:12 -0600

    Update frontend/pages/hosts/ManageHostsPage/HostTableConfig.tsx
    Co-authored-by: kiloconnect[bot] <240665456+kiloconnect[bot]@users.noreply.github.com>

--- frontend/pages/hosts/ManageHostsPage/HostTableConfig.tsx
+++ frontend/pages/hosts/ManageHostsPage/HostTableConfig.tsx
@@ -2,7 +2,6 @@
 // disable this rule as it was throwing an error in Header and Cell component
 // definitions for the selection row for some reason when we dont really need it.
 import React from "react";
-import { upperFirst } from "lodash";
 import { CellProps, Column } from "react-table";
 import ReactTooltip from "react-tooltip";
 
