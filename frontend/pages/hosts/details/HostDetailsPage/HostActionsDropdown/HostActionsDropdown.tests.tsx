9b27a365ad01441ea711f2f8df2ca2de98ca91e1
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2026-02-19 14:20:53 -0500

    Update HostActionsDropdown.tests.tsx

--- frontend/pages/hosts/details/HostDetailsPage/HostActionsDropdown/HostActionsDropdown.tests.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/HostActionsDropdown/HostActionsDropdown.tests.tsx
@@ -88,7 +88,7 @@ describe("Host Actions Dropdown", () => {
 
       await user.click(screen.getByText("Actions"));
 
-      expect(screen.getByText("Report")).toBeInTheDocument();
+      expect(screen.getByText("Live report")).toBeInTheDocument();
     });
 
     it("renders the Query action as disabled with a tooltip when a host is offline", async () => {
@@ -115,16 +115,16 @@ describe("Host Actions Dropdown", () => {
       await user.click(screen.getByText("Actions"));
 
       expect(
-        screen.getByText("Report").parentElement?.parentElement?.parentElement
+        screen.getByText("Live report").parentElement?.parentElement?.parentElement
       ).toHaveClass("actions-dropdown-select__option--is-disabled");
 
       await waitFor(() => {
         waitFor(() => {
-          user.hover(screen.getByText("Report"));
+          user.hover(screen.getByText("Live report"));
         });
 
         expect(
-          screen.getByText(/You can't run a report on an offline host./i)
+          screen.getByText(/You can't run a live report on an offline host./i)
         ).toBeInTheDocument();
       });
     });
@@ -152,7 +152,7 @@ describe("Host Actions Dropdown", () => {
 
       await user.click(screen.getByText("Actions"));
       expect(
-        screen.getByText("Report").parentElement?.parentElement?.parentElement
+        screen.getByText("Live report").parentElement?.parentElement?.parentElement
       ).toHaveClass("actions-dropdown-select__option--is-disabled");
     });
 
@@ -179,7 +179,7 @@ describe("Host Actions Dropdown", () => {
 
       await user.click(screen.getByText("Actions"));
 
-      expect(screen.getByText("Report").parentElement).toHaveClass(
+      expect(screen.getByText("Live report").parentElement).toHaveClass(
         "actions-dropdown-select__option--is-disabled"
       );
     });
@@ -1433,7 +1433,7 @@ describe("Host Actions Dropdown", () => {
       expect(screen.queryByText("Wipe")).toBeInTheDocument();
       expect(screen.queryByText("Delete")).toBeInTheDocument();
 
-      expect(screen.queryByText("Report")).not.toBeInTheDocument();
+      expect(screen.queryByText("Live report")).not.toBeInTheDocument();
       expect(screen.queryByText("Run script")).not.toBeInTheDocument();
       expect(
         screen.queryByText("Show disk encryption key")
@@ -1471,7 +1471,7 @@ describe("Host Actions Dropdown", () => {
       expect(screen.queryByText("Wipe")).toBeInTheDocument();
       expect(screen.queryByText("Delete")).toBeInTheDocument();
 
-      expect(screen.queryByText("Report")).not.toBeInTheDocument();
+      expect(screen.queryByText("Live report")).not.toBeInTheDocument();
       expect(screen.queryByText("Run script")).not.toBeInTheDocument();
       expect(
         screen.queryByText("Show disk encryption key")
@@ -1509,7 +1509,7 @@ describe("Host Actions Dropdown", () => {
 
       expect(screen.getByText("Transfer")).toBeInTheDocument();
       expect(screen.getByText("Delete")).toBeInTheDocument();
-      expect(screen.queryByText("Report")).not.toBeInTheDocument();
+      expect(screen.queryByText("Live report")).not.toBeInTheDocument();
       expect(screen.queryByText("Run script")).not.toBeInTheDocument();
       expect(screen.queryByText("Wipe")).not.toBeInTheDocument();
       expect(screen.queryByText("Lock")).not.toBeInTheDocument();
@@ -1548,7 +1548,7 @@ describe("Host Actions Dropdown", () => {
 
       expect(screen.getByText("Transfer")).toBeInTheDocument();
       expect(screen.getByText("Delete")).toBeInTheDocument();
-      expect(screen.queryByText("Report")).not.toBeInTheDocument();
+      expect(screen.queryByText("Live report")).not.toBeInTheDocument();
       expect(screen.queryByText("Run script")).not.toBeInTheDocument();
       expect(screen.queryByText("Wipe")).not.toBeInTheDocument();
       expect(screen.queryByText("Lock")).not.toBeInTheDocument();
