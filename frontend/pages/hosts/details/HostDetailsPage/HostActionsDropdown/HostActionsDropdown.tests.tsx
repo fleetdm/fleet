4be4da61207420108e66b70be208bf3139a3b153
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2026-02-19 14:30:34 -0500

    Apply suggestion from @noahtalerman

--- frontend/pages/hosts/details/HostDetailsPage/HostActionsDropdown/HostActionsDropdown.tests.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/HostActionsDropdown/HostActionsDropdown.tests.tsx
@@ -115,8 +115,9 @@ describe("Host Actions Dropdown", () => {
       await user.click(screen.getByText("Actions"));
 
       expect(
-        screen.getByText("Live report").parentElement?.parentElement?.parentElement
-      ).toHaveClass("actions-dropdown-select__option--is-disabled");
+        screen.getByText("Live report").parentElement?.parentElement?
+        .parentElement).toHaveClass(
+        "actions-dropdown-select__option--is-disabled");
 
       await waitFor(() => {
         waitFor(() => {
