90d2dba64c55bc2d81e2afe6738dde6afe020ed8
Jacob Shandling <jacob@shandling.dev>
2026-02-20 08:31:35 -0800

    Revert "wip - todo"
    This reverts commit 3f9c3b81e800f3794dcfcede3ae9c14b976d5ed3.

--- frontend/pages/hosts/details/DeviceUserPage/DeviceUserPage.tsx
+++ frontend/pages/hosts/details/DeviceUserPage/DeviceUserPage.tsx
@@ -840,7 +840,6 @@ const DeviceUserPage = ({
             onResolveLater={
               globalConfig?.features?.enable_conditional_access &&
               globalConfig.features?.enable_conditional_access_bypass &&
-              // this should be if ANY of the host's policies are failing and this is true(?)
               selectedPolicy?.conditional_access_bypass_enabled
                 ? () => {
                     onCancelPolicyDetailsModal();
