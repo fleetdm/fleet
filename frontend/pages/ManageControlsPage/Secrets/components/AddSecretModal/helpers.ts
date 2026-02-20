afb31254f7b2c60f6872de2a9066dba548369ada
nulmete <nicoulmete1@gmail.com>
2026-02-20 11:30:15 -0300

    add useFormValidation hook and sample usage

--- frontend/pages/ManageControlsPage/Secrets/components/AddSecretModal/helpers.ts
+++ frontend/pages/ManageControlsPage/Secrets/components/AddSecretModal/helpers.ts
@@ -1,47 +1,19 @@
-import { IAddSecretModalScheduleFormData } from "./AddSecretModal";
+import { IValidationConfig } from "hooks/useFormValidation";
+import { IAddSecretFormData } from "./AddSecretModal";
 
-// TODO: create a validator abstraction for this and the other form validation files
-
-export interface IAddSecretModalFormValidation {
-  isValid: boolean;
-  name?: { isValid: boolean; message?: string };
-  value?: { isValid: boolean; message?: string };
-}
-
-type IMessageFunc = (formData: IAddSecretModalScheduleFormData) => string;
-type IValidationMessage = string | IMessageFunc;
-type IFormValidationKey = keyof Omit<
-  IAddSecretModalScheduleFormData,
-  "isValid"
->;
-
-interface IValidation {
-  name: string;
-  isValid: (
-    formData: IAddSecretModalScheduleFormData,
-    validations?: IAddSecretModalFormValidation
-  ) => boolean;
-  message?: IValidationMessage;
-}
-
-type IFormValidations = Record<
-  IFormValidationKey,
-  { validations: IValidation[] }
->;
-
-const FORM_VALIDATIONS: IFormValidations = {
+const ADD_SECRET_VALIDATIONS: IValidationConfig<IAddSecretFormData> = {
   name: {
     validations: [
       {
         name: "required",
-        isValid: (formData: IAddSecretModalScheduleFormData) => {
+        isValid: (formData) => {
           return formData.name.length > 0;
         },
-        message: `Name is required`,
+        message: "Name is required",
       },
       {
         name: "validName",
-        isValid: (formData: IAddSecretModalScheduleFormData) => {
+        isValid: (formData) => {
           if (formData.name.length === 0) {
             return true; // Skip this validation if name is empty
           }
@@ -52,17 +24,17 @@ const FORM_VALIDATIONS: IFormValidations = {
       },
       {
         name: "notTooLong",
-        isValid: (formData: IAddSecretModalScheduleFormData) => {
+        isValid: (formData) => {
           return formData.name.length <= 255;
         },
         message: "Name may not exceed 255 characters",
       },
       {
         name: "doesNotIncludePrefix",
-        isValid: (formData: IAddSecretModalScheduleFormData) => {
+        isValid: (formData) => {
           return !formData.name.match(/^FLEET_SECRET_/);
         },
-        message: `Name should not include variable prefix`,
+        message: "Name should not include variable prefix",
       },
     ],
   },
@@ -70,55 +42,13 @@ const FORM_VALIDATIONS: IFormValidations = {
     validations: [
       {
         name: "required",
-        isValid: (formData: IAddSecretModalScheduleFormData) => {
+        isValid: (formData) => {
           return formData.value.length > 0;
         },
-        message: `Value is required`,
+        message: "Value is required",
       },
     ],
   },
 };
 
-const getErrorMessage = (
-  formData: IAddSecretModalScheduleFormData,
-  message?: IValidationMessage
-) => {
-  if (message === undefined || typeof message === "string") {
-    return message;
-  }
-  return message(formData);
-};
-
-export const validateFormData = (
-  formData: IAddSecretModalScheduleFormData,
-  isSaving = false
-) => {
-  const formValidation: IAddSecretModalFormValidation = {
-    isValid: true,
-  };
-  Object.keys(FORM_VALIDATIONS).forEach((key) => {
-    const objKey = key as keyof typeof FORM_VALIDATIONS;
-    const failedValidation = FORM_VALIDATIONS[objKey].validations.find(
-      (validation) => {
-        if (!isSaving && validation.name === "required") {
-          return false; // Skip this validation if not saving
-        }
-        return !validation.isValid(formData, formValidation);
-      }
-    );
-
-    if (!failedValidation) {
-      formValidation[objKey] = {
-        isValid: true,
-      };
-    } else {
-      formValidation.isValid = false;
-      formValidation[objKey] = {
-        isValid: false,
-        message: getErrorMessage(formData, failedValidation.message),
-      };
-    }
-  });
-
-  return formValidation;
-};
+export default ADD_SECRET_VALIDATIONS;
