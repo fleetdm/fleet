96d27334ec3164a505de59beff16144c93001ce6
jacobshandling <61553566+jacobshandling@users.noreply.github.com>
2026-02-20 13:08:04 -0800

    UI: Fix policies resolvable condition (#40208)

--- frontend/pages/hosts/details/DeviceUserPage/DeviceUserPage.tsx
+++ frontend/pages/hosts/details/DeviceUserPage/DeviceUserPage.tsx
@@ -673,6 +673,10 @@ const DeviceUserPage = ({
       );
     }
 
+    const hasAnyBypassDisabledFailingCAPolicy = host?.policies.some(
+      (p) => p.response === "fail" && !p.conditional_access_bypass_enabled
+    );
+
     return (
       <>
         <div className={`${baseClass} main-content`}>
@@ -840,6 +844,7 @@ const DeviceUserPage = ({
             onResolveLater={
               globalConfig?.features?.enable_conditional_access &&
               globalConfig.features?.enable_conditional_access_bypass &&
+              !hasAnyBypassDisabledFailingCAPolicy &&
               selectedPolicy?.conditional_access_bypass_enabled
                 ? () => {
                     onCancelPolicyDetailsModal();
