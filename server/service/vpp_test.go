c3f6e8b83288daa07e185c2c5de0924d279f27c3
Martin Angers <martin.n.angers@gmail.com>
2026-02-23 15:16:29 -0500

    Add authorization and mdm not configured tests for the new endpoint

--- server/service/vpp_test.go
+++ server/service/vpp_test.go
@@ -35,39 +35,40 @@ func TestVPPAuth(t *testing.T) {
 	}
 
 	testCases := []struct {
-		name            string
-		user            *fleet.User
-		teamID          *uint
-		shouldFailRead  bool
-		shouldFailWrite bool
+		name                   string
+		user                   *fleet.User
+		teamID                 *uint
+		shouldFailRead         bool
+		shouldFailWrite        bool
+		shouldFailCreateWebApp bool
 	}{
-		{"no role no team", test.UserNoRoles, nil, true, true},
-		{"no role team", test.UserNoRoles, ptr.Uint(1), true, true},
-		{"global admin no team", test.UserAdmin, nil, false, false},
-		{"global admin team", test.UserAdmin, ptr.Uint(1), false, false},
-		{"global maintainer no team", test.UserMaintainer, nil, false, false},
-		{"global maintainer team", test.UserMaintainer, ptr.Uint(1), false, false},
-		{"global observer no team", test.UserObserver, nil, true, true},
-		{"global observer team", test.UserObserver, ptr.Uint(1), true, true},
-		{"global observer+ no team", test.UserObserverPlus, nil, true, true},
-		{"global observer+ team", test.UserObserverPlus, ptr.Uint(1), true, true},
-		{"global gitops no team", test.UserGitOps, nil, true, false},
-		{"global gitops team", test.UserGitOps, ptr.Uint(1), true, false},
-		{"team admin no team", test.UserTeamAdminTeam1, nil, true, true},
-		{"team admin team", test.UserTeamAdminTeam1, ptr.Uint(1), false, false},
-		{"team admin other team", test.UserTeamAdminTeam2, ptr.Uint(1), true, true},
-		{"team maintainer no team", test.UserTeamMaintainerTeam1, nil, true, true},
-		{"team maintainer team", test.UserTeamMaintainerTeam1, ptr.Uint(1), false, false},
-		{"team maintainer other team", test.UserTeamMaintainerTeam2, ptr.Uint(1), true, true},
-		{"team observer no team", test.UserTeamObserverTeam1, nil, true, true},
-		{"team observer team", test.UserTeamObserverTeam1, ptr.Uint(1), true, true},
-		{"team observer other team", test.UserTeamObserverTeam2, ptr.Uint(1), true, true},
-		{"team observer+ no team", test.UserTeamObserverPlusTeam1, nil, true, true},
-		{"team observer+ team", test.UserTeamObserverPlusTeam1, ptr.Uint(1), true, true},
-		{"team observer+ other team", test.UserTeamObserverPlusTeam2, ptr.Uint(1), true, true},
-		{"team gitops no team", test.UserTeamGitOpsTeam1, nil, true, true},
-		{"team gitops team", test.UserTeamGitOpsTeam1, ptr.Uint(1), true, false},
-		{"team gitops other team", test.UserTeamGitOpsTeam2, ptr.Uint(1), true, true},
+		{"no role no team", test.UserNoRoles, nil, true, true, true},
+		{"no role team", test.UserNoRoles, ptr.Uint(1), true, true, true},
+		{"global admin no team", test.UserAdmin, nil, false, false, false},
+		{"global admin team", test.UserAdmin, ptr.Uint(1), false, false, false},
+		{"global maintainer no team", test.UserMaintainer, nil, false, false, false},
+		{"global maintainer team", test.UserMaintainer, ptr.Uint(1), false, false, false},
+		{"global observer no team", test.UserObserver, nil, true, true, true},
+		{"global observer team", test.UserObserver, ptr.Uint(1), true, true, true},
+		{"global observer+ no team", test.UserObserverPlus, nil, true, true, true},
+		{"global observer+ team", test.UserObserverPlus, ptr.Uint(1), true, true, true},
+		{"global gitops no team", test.UserGitOps, nil, true, false, false},
+		{"global gitops team", test.UserGitOps, ptr.Uint(1), true, false, false},
+		{"team admin no team", test.UserTeamAdminTeam1, nil, true, true, false},
+		{"team admin team", test.UserTeamAdminTeam1, ptr.Uint(1), false, false, false},
+		{"team admin other team", test.UserTeamAdminTeam2, ptr.Uint(1), true, true, false},
+		{"team maintainer no team", test.UserTeamMaintainerTeam1, nil, true, true, false},
+		{"team maintainer team", test.UserTeamMaintainerTeam1, ptr.Uint(1), false, false, false},
+		{"team maintainer other team", test.UserTeamMaintainerTeam2, ptr.Uint(1), true, true, false},
+		{"team observer no team", test.UserTeamObserverTeam1, nil, true, true, true},
+		{"team observer team", test.UserTeamObserverTeam1, ptr.Uint(1), true, true, true},
+		{"team observer other team", test.UserTeamObserverTeam2, ptr.Uint(1), true, true, true},
+		{"team observer+ no team", test.UserTeamObserverPlusTeam1, nil, true, true, true},
+		{"team observer+ team", test.UserTeamObserverPlusTeam1, ptr.Uint(1), true, true, true},
+		{"team observer+ other team", test.UserTeamObserverPlusTeam2, ptr.Uint(1), true, true, true},
+		{"team gitops no team", test.UserTeamGitOpsTeam1, nil, true, true, false},
+		{"team gitops team", test.UserTeamGitOpsTeam1, ptr.Uint(1), true, false, false},
+		{"team gitops other team", test.UserTeamGitOpsTeam2, ptr.Uint(1), true, true, false},
 	}
 
 	for _, tt := range testCases {
@@ -104,6 +105,9 @@ func TestVPPAuth(t *testing.T) {
 			} else {
 				checkAuthErr(t, tt.shouldFailWrite, err)
 			}
+
+			_, err = svc.CreateAndroidWebApp(ctx, "test", "http://example.com", nil)
+			checkAuthErr(t, tt.shouldFailCreateWebApp, err)
 		})
 	}
 }
