3793101f640ba671aaa29536808ae9ca72860a52
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2026-02-27 12:20:29 -0500

    Update gitops.go (#38863)
    - Moved this information out of the YAML reference:
    https://github.com/fleetdm/fleet/pull/38862/files#diff-b71104232d8fbaaf4bd537065533cbf39ec5f9bf028ec1d8083346953ed5178dL5

--- cmd/fleetctl/fleetctl/gitops.go
+++ cmd/fleetctl/fleetctl/gitops.go
@@ -36,7 +36,7 @@ func gitopsCommand() *cli.Command {
 	)
 	return &cli.Command{
 		Name:      "gitops",
-		Usage:     "Synchronize Fleet configuration with provided file. This command is intended to be used in a GitOps workflow.",
+		Usage:     "This command is used by Fleet's best practice GitOps workflow.",
 		UsageText: `fleetctl gitops [options]`,
 		Flags: []cli.Flag{
 			&cli.StringSliceFlag{
