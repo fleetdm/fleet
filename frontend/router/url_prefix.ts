5531d1f86aee76a0ccabd749191ce25f652922de
Jordan Montgomery <elijah.jordan.montgomery@gmail.com>
2026-03-02 13:29:38 -0500

    Fix inline scripts

--- frontend/router/url_prefix.ts
+++ frontend/router/url_prefix.ts
@@ -1,12 +1,4 @@
 // Encapsulate the URL Prefix so that this is the only module that
 // needs to access the global. All other modules should use this one.
 
-// extending window interface to include urlPrefix.
-// https://stackoverflow.com/questions/12709074/how-do-you-explicitly-set-a-new-property-on-window-in-typescript
-declare global {
-  interface Window {
-    urlPrefix: string;
-  }
-}
-
-export default window.urlPrefix || "";
+export default document.querySelector<HTMLMetaElement>('meta[name="url-prefix"]')?.content ?? "";
