8eb1f15d3b7434f9934314fc4e6ba692680b537a
Jordan Montgomery <elijah.jordan.montgomery@gmail.com>
2026-03-11 12:16:03 -0400

    Fix default value in team creation from spec

--- ee/server/service/teams.go
+++ ee/server/service/teams.go
@@ -1241,6 +1241,12 @@ func (svc *Service) createTeamFromSpec(
 		return nil, ctxerr.Wrap(ctx, fleet.NewInvalidArgumentError("macos_setup.lock_end_user_info", `"enable_end_user_authentication" must be set to "true" in order to enable "lock_end_user_info"`))
 	}
 
+	// Default the value of "lock_end_user_info" to the value of "enable_end_user_authentication" if not explicitly set in the spec to keep prior
+	// behavior.
+	if !macOSSetup.LockEndUserInfo.Valid {
+		macOSSetup.LockEndUserInfo = optjson.SetBool(macOSSetup.EnableEndUserAuthentication)
+	}
+
 	enableDiskEncryption := spec.MDM.EnableDiskEncryption.Value
 	if !spec.MDM.EnableDiskEncryption.Valid {
 		if de := macOSSettings.DeprecatedEnableDiskEncryption; de != nil {
