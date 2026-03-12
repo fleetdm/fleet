8d8f4ed7ff8b7df2172217d9698392c0dc18c2a3
Mitch Francese <2227948+tux234@users.noreply.github.com>
2026-03-12 12:00:31 -0400

    Remove spurious TorBrowser import from Charles Proxy branch

--- frontend/pages/SoftwarePage/components/icons/index.ts
+++ frontend/pages/SoftwarePage/components/icons/index.ts
@@ -211,7 +211,6 @@ import TextExpander from "./TextExpander";
 import TheUnarchiver from "./TheUnarchiver";
 import Thunderbird from "./Thunderbird";
 import Todoist from "./Todoist";
-import TorBrowser from "./TorBrowser";
 import Tower from "./Tower";
 import Transmit from "./Transmit";
 import Tunnelblick from "./Tunnelblick";
@@ -466,7 +465,6 @@ export const SOFTWARE_NAME_TO_ICON_MAP = {
   "the unarchiver": TheUnarchiver,
   thunderbird: Thunderbird,
   todoist: Todoist,
-  "tor browser": TorBrowser,
   tower: Tower,
   transmit: Transmit,
   tunnelblick: Tunnelblick,
