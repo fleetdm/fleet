0ab1dfb063493ee2c2cb041c163570c9c2db0341
Gabriel Hernandez <ghernandez345@gmail.com>
2026-03-12 14:59:25 +0000

    update TableContainer to use TooltipWrapper

--- frontend/components/TableContainer/TableContainer.tsx
+++ frontend/components/TableContainer/TableContainer.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useCallback, useRef, useEffect } from "react";
 import classnames from "classnames";
 import { Row } from "react-table";
-import ReactTooltip from "react-tooltip";
 import useDeepEffect from "hooks/useDeepEffect";
 import { noop } from "lodash";
 
@@ -10,8 +9,6 @@ import Pagination from "components/Pagination";
 import Button from "components/buttons/Button";
 import Icon from "components/Icon/Icon";
 import TooltipWrapper from "components/TooltipWrapper";
-
-import { COLORS } from "styles/var/colors";
 
 import DataTable from "./DataTable/DataTable";
 import { IActionButtonProps } from "./DataTable/ActionButton/ActionButton";
@@ -384,28 +381,24 @@ const TableContainer = <T,>({
             {customControl && customControl()}
             {searchable && !wideSearch && (
               <div className={`${baseClass}__search`}>
-                <div
+                <TooltipWrapper
+                  tipContent={
+                    <span className={`tooltip ${baseClass}__tooltip-text`}>
+                      {searchToolTipText}
+                    </span>
+                  }
+                  position="top"
+                  showArrow
+                  disableTooltip={!searchToolTipText}
+                  underline={false}
                   className={`${baseClass}__search-input`}
-                  data-tip
-                  data-for="search-tooltip"
-                  data-tip-disable={!searchToolTipText}
                 >
                   <SearchField
                     placeholder={inputPlaceHolder}
                     defaultValue={searchQuery}
                     onChange={onSearchQueryChange}
                   />
-                </div>
-                <ReactTooltip
-                  effect="solid"
-                  backgroundColor={COLORS["tooltip-bg"]}
-                  id="search-tooltip"
-                  data-html
-                >
-                  <span className={`tooltip ${baseClass}__tooltip-text`}>
-                    {searchToolTipText}
-                  </span>
-                </ReactTooltip>
+                </TooltipWrapper>
               </div>
             )}
             {customFiltersButton && customFiltersButton()}
@@ -457,30 +450,26 @@ const TableContainer = <T,>({
             {/* Render search bar only if not empty component */}
             {searchable && !wideSearch && (
               <div className={`${baseClass}__search`}>
-                <div
+                <TooltipWrapper
+                  tipContent={
+                    <span className={`tooltip ${baseClass}__tooltip-text`}>
+                      {searchToolTipText}
+                    </span>
+                  }
+                  position="top"
+                  showArrow
+                  disableTooltip={!searchToolTipText}
+                  underline={false}
                   className={`${baseClass}__search-input ${
                     stackControls ? "stack-table-controls" : ""
                   }`}
-                  data-tip
-                  data-for="search-tooltip"
-                  data-tip-disable={!searchToolTipText}
                 >
                   <SearchField
                     placeholder={inputPlaceHolder}
                     defaultValue={searchQuery}
                     onChange={onSearchQueryChange}
                   />
-                </div>
-                <ReactTooltip
-                  effect="solid"
-                  backgroundColor={COLORS["tooltip-bg"]}
-                  id="search-tooltip"
-                  data-html
-                >
-                  <span className={`tooltip ${baseClass}__tooltip-text`}>
-                    {searchToolTipText}
-                  </span>
-                </ReactTooltip>
+                </TooltipWrapper>
               </div>
             )}
           </div>
