878fb1af273d998a9c9b8c94f9f852a14a274355
Gabriel Hernandez <ghernandez345@gmail.com>
2026-03-12 15:21:01 +0000

    update HostMdMStatusCell and LiveQueryIssueCell. Fix Dropdown emtionCache

--- frontend/components/TableContainer/DataTable/HostMdmStatusCell/HostMdmStatusCell.tsx
+++ frontend/components/TableContainer/DataTable/HostMdmStatusCell/HostMdmStatusCell.tsx
@@ -1,22 +1,23 @@
 import React from "react";
-import ReactTooltip from "react-tooltip";
-import { COLORS } from "styles/var/colors";
+
 import { DEFAULT_EMPTY_CELL_VALUE } from "utilities/constants";
+import { IHost } from "interfaces/host";
+
 import Icon from "components/Icon";
 import NotSupported from "components/NotSupported";
-import { IHost } from "interfaces/host";
+import TooltipWrapper from "components/TooltipWrapper";
 
 const baseClass = "host-mdm-status-cell";
 
 const HostMdmStatusCell = ({
   row: {
-    original: { id, mdm, platform },
+    original: { mdm, platform },
   },
   cell: { value },
 }: {
   row: { original: IHost };
   cell: { value: string };
-}): JSX.Element => {
+}) => {
   if (platform === "chrome") {
     return NotSupported;
   }
@@ -29,28 +30,19 @@ const HostMdmStatusCell = ({
     <span className={`${baseClass}`}>
       {value}
       {mdm?.dep_profile_error && (
-        <>
-          <span
-            className={`tooltip tooltip__tooltip-icon`}
-            data-tip
-            data-for={`host-mdm-status__${id}`}
-            data-tip-disable={false}
-          >
-            <Icon name="error-outline" color="status-error" size="medium" />
-          </span>
-          <ReactTooltip
-            place="top"
-            effect="solid"
-            backgroundColor={COLORS["tooltip-bg"]}
-            id={`host-mdm-status__${id}`}
-            data-html
-          >
-            <span className={`tooltip__tooltip-text`}>
-              Fleet hit Apple’s API rate limit when preparing the macOS Setup
-              Assistant for this host. Fleet will try again every hour.
+        <TooltipWrapper
+          tipContent={
+            <span className="tooltip__tooltip-text">
+              Fleet hit Apple&apos;s API rate limit when preparing the macOS
+              Setup Assistant for this host. Fleet will try again every hour.
             </span>
-          </ReactTooltip>
-        </>
+          }
+          position="top"
+          underline={false}
+          className="tooltip tooltip__tooltip-icon"
+        >
+          <Icon name="error-outline" color="status-error" size="medium" />
+        </TooltipWrapper>
       )}
     </span>
   );
