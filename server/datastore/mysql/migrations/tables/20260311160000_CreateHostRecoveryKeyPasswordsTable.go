1625dfdc3e4140916409db6c2984e37171b0678b
mostlikelee <tim@mostlikelee.com>
2026-03-11 21:12:52 -0600

    add soft deletes update migration timestamp

--- server/datastore/mysql/migrations/tables/20260311160000_CreateHostRecoveryKeyPasswordsTable.go
+++ server/datastore/mysql/migrations/tables/20260311160000_CreateHostRecoveryKeyPasswordsTable.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260309160000, Down_20260309160000)
+	MigrationClient.AddMigration(Up_20260311160000, Down_20260311160000)
 }
 
-func Up_20260309160000(tx *sql.Tx) error {
+func Up_20260311160000(tx *sql.Tx) error {
 	if _, err := tx.Exec(`
 		CREATE TABLE host_recovery_key_passwords (
 			host_uuid varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -17,11 +17,13 @@ func Up_20260309160000(tx *sql.Tx) error {
 			status VARCHAR(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 			operation_type VARCHAR(20) COLLATE utf8mb4_unicode_ci NOT NULL,
 			error_message TEXT COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+			deleted TINYINT(1) NOT NULL DEFAULT 0,
 			created_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 			updated_at TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
 			PRIMARY KEY (host_uuid),
 			KEY status (status),
 			KEY operation_type (operation_type),
+			KEY deleted (deleted),
 			CONSTRAINT host_recovery_key_passwords_ibfk_1 FOREIGN KEY (status) REFERENCES mdm_delivery_status (status) ON UPDATE CASCADE,
 			CONSTRAINT host_recovery_key_passwords_ibfk_2 FOREIGN KEY (operation_type) REFERENCES mdm_operation_types (operation_type) ON UPDATE CASCADE
 		)
@@ -31,6 +33,6 @@ func Up_20260309160000(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20260309160000(tx *sql.Tx) error {
+func Down_20260311160000(tx *sql.Tx) error {
 	return nil
 }
