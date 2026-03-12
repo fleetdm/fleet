583ad26e067743efd97d6e10223c181553169446
Mitch Francese <2227948+tux234@users.noreply.github.com>
2026-03-12 13:50:36 -0400

    Add RustDesk as Fleet maintained app (macOS, Windows)

---
+++ ee/maintained-apps/inputs/winget/scripts/rustdesk_install.ps1
@@ -0,0 +1,26 @@
+# Learn more about .exe install scripts:
+# http://fleetdm.com/learn-more-about/exe-install-scripts
+
+$exeFilePath = "${env:INSTALLER_PATH}"
+
+try {
+
+$processOptions = @{
+  FilePath = "$exeFilePath"
+  ArgumentList = "--silent-install"
+  PassThru = $true
+  Wait = $true
+}
+
+# Start process and track exit code
+$process = Start-Process @processOptions
+$exitCode = $process.ExitCode
+
+# Prints the exit code
+Write-Host "Install exit code: $exitCode"
+Exit $exitCode
+
+} catch {
+  Write-Host "Error: $_"
+  Exit 1
+}
