f74a953281fdaaa98814a3e05bacc80716c48859
Gabriel Hernandez <ghernandez345@gmail.com>
2026-03-13 13:25:00 +0000

    updat eossettingstatuscell

--- frontend/pages/hosts/details/OSSettingsModal/OSSettingsTable/OSSettingStatusCell/OSSettingStatusCell.tsx
+++ frontend/pages/hosts/details/OSSettingsModal/OSSettingsTable/OSSettingStatusCell/OSSettingStatusCell.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import ReactTooltip from "react-tooltip";
-import { uniqueId } from "lodash";
 
 import Icon from "components/Icon";
 import TextCell from "components/TableContainer/DataTable/TextCell";
+import TooltipWrapper from "components/TooltipWrapper";
 import {
   FLEET_ANDROID_CERTIFICATE_TEMPLATE_PROFILE_ID,
   LinuxDiskEncryptionStatus,
   ProfileOperationType,
   ProfilePlatform,
 } from "interfaces/mdm";
-import { COLORS } from "styles/var/colors";
 
 import { OsSettingsTableStatusValue } from "../OSSettingsTableConfig";
 import TooltipContent from "./components/Tooltip/TooltipContent";
@@ -113,27 +111,14 @@ const OSSettingStatusCell = ({
 
   if (displayOption) {
     const { statusText, iconName, tooltip } = displayOption;
-    const tooltipId = uniqueId();
     return (
       <span className={baseClass}>
         <Icon name={iconName} />
         {tooltip ? (
-          <>
-            <span
-              className={`${baseClass}__status-text`}
-              data-tip
-              data-for={tooltipId}
-              data-tip-disable={false}
-            >
-              {statusText}
-            </span>
-            <ReactTooltip
-              place="top"
-              effect="solid"
-              backgroundColor={COLORS["tooltip-bg"]}
-              id={tooltipId}
-              data-html
-            >
+          <TooltipWrapper
+            showArrow
+            className={`${baseClass}__status-text`}
+            tipContent={
               <span className="tooltip__tooltip-text">
                 {status !== "action_required" ? (
                   <TooltipContent
@@ -151,8 +136,12 @@ const OSSettingStatusCell = ({
                   />
                 )}
               </span>
-            </ReactTooltip>
-          </>
+            }
+            position="top"
+            underline={false}
+          >
+            {statusText}
+          </TooltipWrapper>
         ) : (
           statusText
         )}
