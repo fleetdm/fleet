3c39d3881df6ebda396e1a39858a75cc44bcf3ae
Victor Lyuboslavsky <2685025+getvictor@users.noreply.github.com>
2026-03-14 07:15:44 -0500

    timestamp

--- server/datastore/mysql/migrations/tables/20260314120000_AddHostSoftwareSoftwareIDIndex.go
+++ server/datastore/mysql/migrations/tables/20260314120000_AddHostSoftwareSoftwareIDIndex.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260313120000, Down_20260313120000)
+	MigrationClient.AddMigration(Up_20260314120000, Down_20260314120000)
 }
 
-func Up_20260313120000(tx *sql.Tx) error {
+func Up_20260314120000(tx *sql.Tx) error {
 	// Add a secondary index on software_id to speed up queries that join or filter host_software by software_id.
 	if _, err := tx.Exec(`ALTER TABLE host_software ADD INDEX idx_host_software_software_id (software_id)`); err != nil {
 		return fmt.Errorf("adding software_id index to host_software: %w", err)
@@ -17,6 +17,6 @@ func Up_20260313120000(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20260313120000(_ *sql.Tx) error {
+func Down_20260314120000(_ *sql.Tx) error {
 	return nil
 }
