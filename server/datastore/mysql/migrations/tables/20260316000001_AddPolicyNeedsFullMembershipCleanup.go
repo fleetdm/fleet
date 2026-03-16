7f7facfba10ae7155b9ced3df982d0117512cb95
Juan Fernandez <juan@fleetdm.com>
2026-03-16 12:27:02 -0400

    Bump migration

--- server/datastore/mysql/migrations/tables/20260316000001_AddPolicyNeedsFullMembershipCleanup.go
+++ server/datastore/mysql/migrations/tables/20260316000001_AddPolicyNeedsFullMembershipCleanup.go
@@ -3,10 +3,10 @@ package tables
 import "database/sql"
 
 func init() {
-	MigrationClient.AddMigration(Up_20260302000001, Down_20260302000001)
+	MigrationClient.AddMigration(Up_20260316000001, Down_20260316000001)
 }
 
-func Up_20260302000001(tx *sql.Tx) error {
+func Up_20260316000001(tx *sql.Tx) error {
 	_, err := tx.Exec(`
         ALTER TABLE policies
         ADD COLUMN needs_full_membership_cleanup TINYINT(1) NOT NULL DEFAULT 0,
@@ -15,6 +15,6 @@ func Up_20260302000001(tx *sql.Tx) error {
 	return err
 }
 
-func Down_20260302000001(tx *sql.Tx) error {
+func Down_20260316000001(tx *sql.Tx) error {
 	return nil
 }
