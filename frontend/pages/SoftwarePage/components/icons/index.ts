bb17751cbd4155dee36028f76d251b10aeed5962
Allen Houchins <allenhouchins@mac.com>
2026-03-17 11:12:09 -0500

    Add RustDesk as a Windows FMA

--- frontend/pages/SoftwarePage/components/icons/index.ts
+++ frontend/pages/SoftwarePage/components/icons/index.ts
@@ -19,6 +19,7 @@ import OktaVerify from "./OktaVerify";
 import Ollama from "./Ollama";
 import Proxifier from "./Proxifier";
 import Putty from "./Putty";
+import RustDesk from "./RustDesk";
 import SequelAce from "./SequelAce";
 import SevenZip from "./7Zip";
 import Abstract from "./Abstract";
@@ -438,6 +439,7 @@ export const SOFTWARE_NAME_TO_ICON_MAP = {
   rider: Rider,
   "royal tsx": RoyalTsx,
   rubymine: RubyMine,
+  rustdesk: RustDesk,
   rustrover: RustRover,
   safari: Safari,
   santa: Santa,
