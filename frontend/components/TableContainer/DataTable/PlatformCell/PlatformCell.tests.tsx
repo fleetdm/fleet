860156b99d3c0152b33937800a343dd956102613
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2026-03-17 11:42:24 -0300

    Fix missing import

--- frontend/components/TableContainer/DataTable/PlatformCell/PlatformCell.tests.tsx
+++ frontend/components/TableContainer/DataTable/PlatformCell/PlatformCell.tests.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import { ScheduledQueryablePlatform } from "interfaces/platform";
+import { QueryablePlatform } from "interfaces/platform";
 import PlatformCell from "./PlatformCell";
 
 const QUERYABLE_PLATFORMS: QueryablePlatform[] = [
