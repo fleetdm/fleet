0a61e7e19b3575bee6c0921e39ec5455bbbf03a8
melpike <79950145+melpike@users.noreply.github.com>
2026-03-19 15:27:28 -0600

    Update EditTeamsAbmModal.tsx

--- frontend/pages/admin/IntegrationsPage/cards/MdmSettings/AppleBusinessManagerPage/components/EditTeamsAbmModal/EditTeamsAbmModal.tsx
+++ frontend/pages/admin/IntegrationsPage/cards/MdmSettings/AppleBusinessManagerPage/components/EditTeamsAbmModal/EditTeamsAbmModal.tsx
@@ -147,7 +147,10 @@ const EditTeamsAbmModal = ({
           wrapperClassName={`${baseClass}__form-field form-field--macos`}
           tooltip={
             <>
-              macOS hosts are automatically added to this fleet on initial sync from ABM. If a host is manually assigned to a different fleet before enrollment, it will enroll to the newly assigned fleet and not the default.
+              macOS hosts are automatically added to this fleet on initial sync
+              from ABM. If a host is manually assigned to a different fleet
+              before enrollment, it will enroll to the newly assigned fleet and
+              not the default.
             </>
           }
         />
