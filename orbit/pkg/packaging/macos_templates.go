0a0e9127f7dc17b745b18da10a0f06571b4f6863
nulmete <nicoulmete1@gmail.com>
2026-03-24 09:58:37 -0300

    remove pkill command

--- orbit/pkg/packaging/macos_templates.go
+++ orbit/pkg/packaging/macos_templates.go
@@ -66,7 +66,7 @@ if [ -f /opt/orbit/.inband_upgrade ]; then
 	# any plist changes from the upgrade are picked up.
 	echo "Detected in-band upgrade (orbit upgrading orbit). Delaying service"
 	echo "restart to prevent orbit from being stopped mid-install."
-	nohup bash -c 'sleep 30; pkill fleet-desktop || true; launchctl bootout "system/'"${DAEMON_LABEL}"'" || true; launchctl enable "system/'"${DAEMON_LABEL}"'"; count=0; while ! launchctl bootstrap system "'"${DAEMON_PLIST}"'"; do sleep 1; count=$((count+1)); if [ "$count" -eq 30 ]; then echo "Failed to bootstrap system '"${DAEMON_PLIST}"'"; exit 1; fi; echo "Retrying launchctl bootstrap..."; done; launchctl kickstart "system/'"${DAEMON_LABEL}"'"' &>/dev/null &
+	nohup bash -c 'sleep 30; launchctl bootout "system/'"${DAEMON_LABEL}"'" || true; launchctl enable "system/'"${DAEMON_LABEL}"'"; count=0; while ! launchctl bootstrap system "'"${DAEMON_PLIST}"'"; do sleep 1; count=$((count+1)); if [ "$count" -eq 30 ]; then echo "Failed to bootstrap system '"${DAEMON_PLIST}"'"; exit 1; fi; echo "Retrying launchctl bootstrap..."; done; launchctl kickstart "system/'"${DAEMON_LABEL}"'"' &>/dev/null &
 	exit 0
 fi
 
