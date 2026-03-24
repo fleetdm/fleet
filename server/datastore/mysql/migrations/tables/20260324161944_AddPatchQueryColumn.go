b61ee62e44872ce12b7f863e177ff3b5efeca09f
jkatz01 <yehonatankatz@gmail.com>
2026-03-24 14:17:12 -0400

    fix test failures

--- server/datastore/mysql/migrations/tables/20260324161944_AddPatchQueryColumn.go
+++ server/datastore/mysql/migrations/tables/20260324161944_AddPatchQueryColumn.go
@@ -12,7 +12,7 @@ func init() {
 func Up_20260324161944(tx *sql.Tx) error {
 	stmt := `
           ALTER TABLE software_installers
-          ADD COLUMN patch_query TEXT NOT NULL;
+          ADD COLUMN patch_query TEXT COLLATE utf8mb4_unicode_ci NOT NULL;
  	 `
 	if _, err := tx.Exec(stmt); err != nil {
 		return fmt.Errorf("add patch_query to software_installers: %w", err)
