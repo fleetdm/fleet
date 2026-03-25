abe620613e19369ec14cff37d4be76184bc92598
Eric <eashaw@sailsjs.com>
2026-03-25 15:40:04 -0500

    Remove more unused files, update routes

--- ee/fleet-agent-downloader/config/routes.js
+++ ee/fleet-agent-downloader/config/routes.js
@@ -13,25 +13,7 @@ module.exports.routes = {
   //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
   //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
-  // 'GET /':                   { action: 'view-homepage-or-redirect' },
   'GET /':   { action: 'dashboard/view-welcome' },
-
-  // 'GET /faq':                { action:   'view-faq' },
-  // 'GET /legal/terms':        { action:   'legal/view-terms' },
-  // 'GET /legal/privacy':      { action:   'legal/view-privacy' },
-  // 'GET /contact':            { action:   'view-contact' },
-
-  // 'GET /signup':             { action: 'entrance/view-signup' },
-  // 'GET /email/confirm':      { action: 'entrance/confirm-email' },
-  // 'GET /email/confirmed':    { action: 'entrance/view-confirmed-email' },
-
-  'GET /login':              { action: 'entrance/view-login' },
-  // 'GET /password/forgot':    { action: 'entrance/view-forgot-password' },
-  // 'GET /password/new':       { action: 'entrance/view-new-password' },
-
-  // 'GET /account':            { action: 'account/view-account-overview' },
-  // 'GET /account/password':   { action: 'account/view-edit-password' },
-  // 'GET /account/profile':    { action: 'account/view-edit-profile' },
 
 
   //  ╔╦╗╦╔═╗╔═╗  ╦═╗╔═╗╔╦╗╦╦═╗╔═╗╔═╗╔╦╗╔═╗   ┬   ╔╦╗╔═╗╦ ╦╔╗╔╦  ╔═╗╔═╗╔╦╗╔═╗
@@ -39,20 +21,12 @@ module.exports.routes = {
   //  ╩ ╩╩╚═╝╚═╝  ╩╚═╚═╝═╩╝╩╩╚═╚═╝╚═╝ ╩ ╚═╝  └┘   ═╩╝╚═╝╚╩╝╝╚╝╩═╝╚═╝╩ ╩═╩╝╚═╝
   'GET /logout':              '/api/v1/account/logout',
 
-
-  //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
-  //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
-  //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
-  // …
-
-
   //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
   //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
   //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
   // Note that, in this app, these API endpoints may be accessed using the `Cloud.*()` methods
   // from the Parasails library, or by using those method names as the `action` in <ajax-form>.
   '/api/v1/account/logout':                           { action: 'account/logout' },
-  // 'PUT   /api/v1/entrance/login':                        { action: 'entrance/login' },
   'GET  /entrance/signup-okta-user-or-redirect':    { action: 'entrance/signup-okta-user-or-redirect' },
   'GET /download': { action: 'download-fleet-installer' },
 
