80c4816dcefb40c6399250e03a04a3a115e5e4ed
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2026-03-25 11:41:11 -0300

    Remove unused file

--- server/service/contract/orbit.go
+++
@@ -1,6 +0,0 @@
-package contract
-
-import "github.com/fleetdm/fleet/v4/server/fleet"
-
-// EnrollOrbitRequest is kept as an alias for backward compatibility.
-type EnrollOrbitRequest = fleet.EnrollOrbitRequest
