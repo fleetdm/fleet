8115388262a581869f674b48c72a74569b465c1d
Allen Houchins <allenhouchins@mac.com>
2026-03-27 21:27:35 -0500

    Update GoogleCredentialProviderForWindows.tsx

--- frontend/pages/SoftwarePage/components/icons/GoogleCredentialProviderForWindows.tsx
+++ frontend/pages/SoftwarePage/components/icons/GoogleCredentialProviderForWindows.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 
 import type { SVGProps } from "react";
 
-const GoogleCredentialProviderForWindows = (
-  props: SVGProps<SVGSVGElement>
-) => (
+const GoogleCredentialProviderForWindows = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} {...props}>
     <image
       width={32}
