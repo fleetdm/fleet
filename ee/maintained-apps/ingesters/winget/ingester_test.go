fbb1573be9d7392a4bac549939f13fa0ae57c4d7
Jonathan Katz <44128041+jkatz01@users.noreply.github.com>
2026-03-30 17:25:58 -0400

    Create default patch policy query in FMA manifest (#42559)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** Resolves #42492 
    Includes changes from running ingestions on all FMAs
    
    # Checklist for submitter
    
    If some of the following don't apply, delete the relevant line.
    
    - [ ] Changes file added for user-visible changes in `changes/`,
    `orbit/changes/` or `ee/fleetd-chrome/changes`.
    See [Changes
    files](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/guides/committing-changes.md#changes-files)
    for more information.
    
    - [ ] Input data is properly validated, `SELECT *` is avoided, SQL
    injection is prevented (using placeholders for values in statements), JS
    inline code is prevented especially for url redirects, and untrusted
    data interpolated into shell scripts/commands is validated against shell
    metacharacters.
    - [ ] If paths of existing endpoints are modified without backwards
    compatibility, checked the frontend/CLI for any necessary changes
    
    ## Testing
    
    - [x] Added/updated automated tests
    - [ ] Where appropriate, [automated tests simulate multiple hosts and
    test for host
    isolation](https://github.com/fleetdm/fleet/blob/main/docs/Contributing/reference/patterns-backend.md#unit-testing)
    (updates to one hosts's records do not affect another)
    
    - [x] QA'd all new/changed functionality manually

--- ee/maintained-apps/ingesters/winget/ingester_test.go
+++ ee/maintained-apps/ingesters/winget/ingester_test.go
@@ -1,10 +1,21 @@
 package winget
 
 import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path"
 	"testing"
 
+	"github.com/fleetdm/fleet/v4/server/ptr"
+	"github.com/google/go-github/v37/github"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 func TestBuildUpgradeCodeBasedUninstallScript(t *testing.T) {
@@ -132,4 +143,159 @@ func TestPreProcessUninstallScript(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestIngestValidations(t *testing.T) {
+	ctx := context.Background()
+	tempDir := t.TempDir()
+
+	testInstallScriptContents := "this is a test install script"
+	require.NoError(t, os.WriteFile(path.Join(tempDir, "install_script.ps1"), []byte(testInstallScriptContents), 0644))
+
+	testUninstallScriptContents := "this is a test uninstall script"
+	require.NoError(t, os.WriteFile(path.Join(tempDir, "uninstall_script.ps1"), []byte(testUninstallScriptContents), 0644))
+
+	cases := []struct {
+		name     string
+		wantErr  string
+		inputApp inputApp
+		cfg      serverConfig
+	}{
+		{
+			name:    "valid",
+			wantErr: "",
+			inputApp: inputApp{
+				Name:                "Foo",
+				UniqueIdentifier:    "Foo",
+				PackageIdentifier:   "Foo",
+				InstallerArch:       "x64",
+				Slug:                "foo/windows",
+				InstallScriptPath:   path.Join(tempDir, "install_script.ps1"),
+				UninstallScriptPath: path.Join(tempDir, "uninstall_script.ps1"),
+				InstallerType:       "msi",
+				InstallerScope:      "machine",
+			},
+			cfg: serverConfig{
+				productCode:       "{ABCDEF}",
+				installerType:     "msi",
+				installerScope:    "machine",
+				installerArch:     "x64",
+				installerProdCode: "{ACBDEF}",
+				upgradeCode:       "{ABCDEF}",
+			},
+		},
+		{
+			name:    "wrong installer type",
+			wantErr: "failed to find installer for app",
+			inputApp: inputApp{
+				Name:              "Foo",
+				UniqueIdentifier:  "Foo",
+				PackageIdentifier: "Foo",
+				InstallerArch:     "x64",
+				Slug:              "foo/windows",
+				InstallerType:     "exe",
+				InstallerScope:    "machine",
+			},
+			cfg: serverConfig{
+				productCode:       "{ABCDEF}",
+				installerType:     "msi", // mismatch here
+				installerScope:    "machine",
+				installerArch:     "x64",
+				installerProdCode: "{ACBDEF}",
+				upgradeCode:       "{ABCDEF}",
+			},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			srv := newTestServer(t, c.cfg)
+			t.Cleanup(srv.Close)
+
+			gc := github.NewClient(srv.Client())
+			url, err := url.Parse(srv.URL + "/")
+			require.NoError(t, err)
+			gc.BaseURL = url
+
+			i := wingetIngester{
+				logger:       slog.New(slog.DiscardHandler),
+				githubClient: gc,
+			}
+
+			_, err = i.ingestOne(ctx, c.inputApp)
+			if c.wantErr != "" {
+				require.ErrorContains(t, err, c.wantErr)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
+
+type serverConfig struct {
+	productCode       string
+	installerType     string
+	installerScope    string
+	installerArch     string
+	installerProdCode string
+	upgradeCode       string
+}
+
+func newTestServer(t *testing.T, cfg serverConfig) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+
+		case "/repos/microsoft/winget-pkgs/contents/manifests/f/Foo":
+			content := []github.RepositoryContent{{Name: ptr.String("Foo")}}
+			require.NoError(t, json.NewEncoder(w).Encode(content))
+
+		case "/repos/microsoft/winget-pkgs/contents/manifests/f/Foo/Foo/Foo.installer.yaml":
+			manifest := installerManifest{
+				ProductCode:    cfg.productCode,
+				InstallerType:  cfg.installerType,
+				Scope:          cfg.installerScope,
+				PackageVersion: "1.0",
+				AppsAndFeaturesEntries: []appsAndFeaturesEntries{
+					{UpgradeCode: cfg.upgradeCode},
+				},
+				Installers: []installer{
+					{
+						Architecture:  cfg.installerArch,
+						InstallerType: cfg.installerType,
+						ProductCode:   cfg.installerProdCode,
+						Scope:         cfg.installerScope,
+					},
+				},
+			}
+
+			bytes, err := yaml.Marshal(manifest)
+			require.NoError(t, err)
+
+			str := string(bytes)
+			content := &github.RepositoryContent{
+				Name:    ptr.String("Foo"),
+				Content: &str,
+			}
+			require.NoError(t, json.NewEncoder(w).Encode(content))
+
+		case "/repos/microsoft/winget-pkgs/contents/manifests/f/Foo/Foo/Foo.locale.en-US.yaml":
+			lManifest := localeManifest{
+				PackageName: "foo",
+				Publisher:   "Bar, Inc.",
+			}
+
+			bytes, err := yaml.Marshal(lManifest)
+			require.NoError(t, err)
+
+			str := string(bytes)
+			content := &github.RepositoryContent{
+				Name:    ptr.String("Foo"),
+				Content: &str,
+			}
+			require.NoError(t, json.NewEncoder(w).Encode(content))
+
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			t.Fatalf("unexpected path %s", r.URL.Path)
+		}
+	}))
 }
