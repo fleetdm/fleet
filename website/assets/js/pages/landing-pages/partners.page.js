04932ab5899dc5a119d34587281b39f3b07e500e
Eric <eashaw@sailsjs.com>
2026-04-01 18:46:46 -0500

    Update partners page

--- website/assets/js/pages/landing-pages/partners.page.js
+++ website/assets/js/pages/landing-pages/partners.page.js
@@ -14,18 +14,61 @@ parasails.registerPage('partners', {
       partnerType: 'reseller',
       servicesOffered: {},
     },
-    partnerFormRules: {},
+    partnerFormRules: {
+      submittersFirstName: { required: true },
+      submittersLastName: { required: true },
+      submittersEmailAddress: { required: true },
+      submittersOrganization: { required: true },
+      partnerType: { required: true },
+      partnerWebsite: { required: true },
+      partnerCountry: { required: true },
+      notes: {required: true },
+      // IF partnerType === reseller
+      // servicesOffered: {required: true,},
+      // numberOfHosts: { required: true },
+
+      // IF partnerType === integrations
+      // servicesCategory: { required: true },
+    },
 
     // For deal registration form
-    dealRegistrationFormData: {},
-    dealRegistrationFormRules: {},
-
+    dealRegistrationFormData: {
+      platforms: {},
+      useCase: {},
+    },
+    dealRegistrationFormRules: {
+      submittersFirstName: { required: true },
+      submittersLastName: { required: true },
+      submittersEmailAddress: { required: true, isEmail: true },
+      submittersOrganization: { required: true },
+      submitterIsExistingPartner: { required: true },
+      customersOrganization: { required: true },
+      customersName: { required: true },
+      customersEmailAddress: { required: true, isEmail: true },
+      dealStage: {required: true},
+      expectedClose: { required: true },
+      numberOfHosts: { required: true },
+      platforms: {
+        required: true,
+        custom: (platforms)=>{
+          return _.keysIn(platforms).length > 0 && _.contains(_.values(platforms), true);
+        }
+      },
+      useCase: {
+        required: true,
+        custom: (useCase)=>{
+          return _.keysIn(useCase).length > 0 && _.contains(_.values(useCase), true);
+        }
+      },
+      notes: { required: true },
+    },
 
 
     // used by all forms.
     formErrors: {},
     syncing: false,
     cloudError: undefined,
+    cloudSuccess: false,
   },
 
   //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
@@ -35,7 +78,11 @@ parasails.registerPage('partners', {
     //…
   },
   mounted: async function() {
-    //…
+    if(window.location.hash){
+      if(window.location.hash === '#deals') {
+        this.modal = 'deal-registration';
+      }
+    }
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
@@ -53,11 +100,71 @@ parasails.registerPage('partners', {
       await this.forceRender();
     },
 
+    clickResetForm: function() {
+      if(this.modal === 'deal-registration') {
+        this.dealRegistrationFormData = {
+          platforms: {},
+          useCase: {},
+        };
+      }
+      if(this.modal === 'partner') {
+        this.partnerFormData = {
+          partnerType: 'reseller',
+          servicesOffered: {},
+        };
+      }
+      this.formErrors = {};
+    },
+
+
+    handleSubmittingPartnerForm: async function(argins) {
+      this.syncing = true;
+      // check to make sure that we have the required values depending on selected partnerType value.
+      if(argins.partnerType === 'reseller') {
+        let servicesOffered = argins.servicesOffered;
+        // if this was the form for resellers and no services were selected, add a formError for this input.
+        if(!_.contains(_.values(servicesOffered), true)){
+          this.formErrors.servicesOffered = {required: true};
+        }
+        if(!argins.numberOfHosts) {
+          this.formErrors.numberOfHosts = {required: true};
+        }
+      } else if(argins.partnerType === 'integrations') {
+        if(!argins.servicesCategory){
+          this.formErrors.servicesCategory = {required: true};
+        }
+      }
+
+      await Cloud.deliverPartnerRegistrationSubmission.with(argins).tolerate((err)=>{
+        this.syncing = false;
+        this.cloudError = err;
+      });
+    },
+
+
+
     closeModal: function() {
+      this.clickResetForm();
       this.modal = undefined;
+      this.cloudSuccess = false;
+      this.dealRegistrationFormData = {
+        platforms: {},
+        useCase: {},
+      };
+      this.partnerFormData = {
+        partnerType: 'reseller',
+        servicesOffered: {},
+      };
+    },
+    submittedDealForm: function() {
+      if(!this.cloudError) {
+        this.cloudSuccess = true;
+      }
     },
     submittedPartnerForm: function() {
-
+      if(!this.cloudError) {
+        this.cloudSuccess = true;
+      }
     },
   }
 });
