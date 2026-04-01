84c2ef863e6eead41332519bb2c9c82c17c6d801
Victor Lyuboslavsky <2685025+getvictor@users.noreply.github.com>
2026-04-01 16:24:24 -0500

    Fix lint issue

--- frontend/pages/hosts/details/cards/Activity/Activity.tsx
+++ frontend/pages/hosts/details/cards/Activity/Activity.tsx
@@ -35,8 +35,8 @@ const UpcomingTooltip = () => {
           Failure of one activity won&apos;t cancel other activities.
           <br />
           <br />
-          Software and scripts always run in order. Each waits until
-          the previous one runs successfully or fails.
+          Software and scripts always run in order. Each waits until the
+          previous one runs successfully or fails.
         </>
       }
       className={`${baseClass}__upcoming-tooltip`}
