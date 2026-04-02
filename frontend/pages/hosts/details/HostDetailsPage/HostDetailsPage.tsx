44496fb2ae32bbf1294932ee9a62b07bafb2ce13
melpike <melpike.dev@gmail.com>
2026-04-02 09:16:24 -0600

    fix lint

--- frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
@@ -1216,7 +1216,7 @@ const HostDetailsPage = ({
                currently supporting software installs. This check should be removed
                when we add that feature. Note: Android is currently a subset of BYODAccountDrivenUserEnrollment */}
               {isBYODAccountDrivenUserEnrollment(host.mdm.enrollment_status) ||
-                isAndroidHost ? (
+              isAndroidHost ? (
                 <EmptyTable
                   header="Software library is currently not supported on this host."
                   info={
