b1dac1791877793adecb4acfa73c74b94d1e9c57
Ian Littman <iansltx@gmail.com>
2026-04-03 16:57:29 -0500

    Switch FMA manifest retrieval to use Cloudflare R2 bucket
    Fixes #42751

--- server/mdm/maintainedapps/sync.go
+++ server/mdm/maintainedapps/sync.go
@@ -29,7 +29,7 @@ type AppsList struct {
 	Apps    []appListing `json:"apps"`
 }
 
-const fmaOutputsBase = "https://raw.githubusercontent.com/fleetdm/fleet/refs/heads/main/ee/maintained-apps/outputs"
+const fmaOutputsBase = "https://maintained-apps.fleetdm.com/manifests"
 
 // Refresh fetches the latest information about maintained apps from FMA's
 // apps list on GitHub and updates the Fleet database with the new information.
