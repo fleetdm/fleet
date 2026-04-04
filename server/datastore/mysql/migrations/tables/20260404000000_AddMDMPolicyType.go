ae0a3e1f6b40107ac5d98e09ee1d37721399135e
Mitch Francese <2227948+tux234@users.noreply.github.com>
2026-04-03 21:24:37 -0400

    Rename migration timestamp to come after main (20260403 to 20260404)

--- server/datastore/mysql/migrations/tables/20260404000000_AddMDMPolicyType.go
+++ server/datastore/mysql/migrations/tables/20260404000000_AddMDMPolicyType.go
@@ -3,10 +3,10 @@ package tables
 import "database/sql"
 
 func init() {
-	MigrationClient.AddMigration(Up_20260403000000, Down_20260403000000)
+	MigrationClient.AddMigration(Up_20260404000000, Down_20260404000000)
 }
 
-func Up_20260403000000(tx *sql.Tx) error {
+func Up_20260404000000(tx *sql.Tx) error {
 	return withSteps([]migrationStep{
 		basicMigrationStep(
 			"ALTER TABLE policies MODIFY COLUMN `type` enum('dynamic','patch','mdm') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'dynamic'",
@@ -19,6 +19,6 @@ func Up_20260403000000(tx *sql.Tx) error {
 	}, tx)
 }
 
-func Down_20260403000000(tx *sql.Tx) error {
+func Down_20260404000000(tx *sql.Tx) error {
 	return nil
 }
