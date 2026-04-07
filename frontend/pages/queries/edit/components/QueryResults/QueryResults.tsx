43ac5fee11edbd1d4aa12814da403e6dc4fadb3a
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2026-04-07 17:26:03 -0300

    Fix bug after merge main

--- frontend/pages/queries/edit/components/QueryResults/QueryResults.tsx
+++ frontend/pages/queries/edit/components/QueryResults/QueryResults.tsx
@@ -99,10 +99,8 @@ const perfColumnConfigs: Column<ICampaignPerformanceStats>[] = [
       <PerformanceImpactCell
         value={{
           indicator: cellProps.cell.value,
-          id: cellProps.row.original.host_id,
         }}
         isHostSpecific
-        customIdPrefix="live-perf"
       />
     ),
   },
