2a4a90eef0dab2949e69c0e25116b1fb35ef574a
Carlo DiCelico <carlo@fleetdm.com>
2026-04-08 13:56:25 -0400

    bump migrations

--- server/datastore/mysql/migrations/tables/20260408175311_CreateHostManagedLocalAccountPasswords_test.go
+++ server/datastore/mysql/migrations/tables/20260408175311_CreateHostManagedLocalAccountPasswords_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUp_20260407192747(t *testing.T) {
+func TestUp_20260408175311(t *testing.T) {
 	db := applyUpToPrev(t)
 
 	// Apply current migration.
