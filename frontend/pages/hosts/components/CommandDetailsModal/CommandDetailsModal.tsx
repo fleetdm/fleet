bc46ef05e88f6379d3ed50c47892e103dff78e2c
Ian Littman <iansltx@gmail.com>
2026-04-08 17:37:00 -0500

    Re-add ts-ignore because this is a JS component and rebuilding it is out of scope here

--- frontend/pages/hosts/components/CommandDetailsModal/CommandDetailsModal.tsx
+++ frontend/pages/hosts/components/CommandDetailsModal/CommandDetailsModal.tsx
@@ -11,6 +11,7 @@ import commandApi, {
   IGetHostCommandResultsQueryKey,
 } from "services/entities/command";
 
+// @ts-ignore
 import InputField from "components/forms/fields/InputField";
 
 import Modal from "components/Modal";
