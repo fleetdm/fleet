3e5e47477544f39a954d5fad44fbf4be96705304
Victor Lyuboslavsky <2685025+getvictor@users.noreply.github.com>
2026-04-09 18:40:14 +0000

    Bump migration

--- server/datastore/mysql/migrations/tables/20260409183610_AddBitlockerProtectionStatusToHostDisks.go
+++ server/datastore/mysql/migrations/tables/20260409183610_AddBitlockerProtectionStatusToHostDisks.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260409151755, Down_20260409151755)
+	MigrationClient.AddMigration(Up_20260409183610, Down_20260409183610)
 }
 
-func Up_20260409151755(tx *sql.Tx) error {
+func Up_20260409183610(tx *sql.Tx) error {
 	_, err := tx.Exec(`ALTER TABLE host_disks ADD COLUMN bitlocker_protection_status TINYINT(1) NULL DEFAULT NULL`)
 	if err != nil {
 		return fmt.Errorf("adding bitlocker_protection_status to host_disks: %w", err)
@@ -17,6 +17,6 @@ func Up_20260409151755(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20260409151755(tx *sql.Tx) error {
+func Down_20260409183610(tx *sql.Tx) error {
 	return nil
 }
