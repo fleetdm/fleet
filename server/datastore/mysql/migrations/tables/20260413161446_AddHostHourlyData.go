5c7c308059563d61d49378ea2e9de5a0a36a8ea3
Scott Gress <scott@fleetdm.com>
2026-04-13 17:36:53 -0500

    remove bitmap dataset type

--- server/datastore/mysql/migrations/tables/20260413161446_AddHostHourlyData.go
+++ server/datastore/mysql/migrations/tables/20260413161446_AddHostHourlyData.go
@@ -11,23 +11,6 @@ func init() {
 
 func Up_20260413161446(tx *sql.Tx) error {
 	_, err := tx.Exec(`
-		CREATE TABLE IF NOT EXISTS host_daily_data_bitmaps (
-			host_id      INT UNSIGNED NOT NULL,
-			dataset      VARCHAR(50) NOT NULL,
-			entity_id    VARCHAR(50) NOT NULL DEFAULT '',
-			chart_date   DATE NOT NULL,
-			hours_bitmap INT UNSIGNED NOT NULL DEFAULT 0,
-			created_at   TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-			updated_at   TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-			PRIMARY KEY (host_id, dataset, entity_id, chart_date),
-			INDEX idx_dataset_date (dataset, chart_date, entity_id)
-		)
-	`)
-	if err != nil {
-		return fmt.Errorf("create host_daily_data_bitmaps table: %w", err)
-	}
-
-	_, err = tx.Exec(`
 		CREATE TABLE IF NOT EXISTS host_hourly_data_blobs (
 			dataset      VARCHAR(50) NOT NULL,
 			entity_id    VARCHAR(50) NOT NULL DEFAULT '',
