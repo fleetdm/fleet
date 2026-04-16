5ddc8ac16cc2f5182dd0ceb1a5fef6416e7234ef
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2026-04-16 11:14:07 -0500

    Apply suggestion from @noahtalerman

--- website/config/routes.js
+++ website/config/routes.js
@@ -1161,7 +1161,7 @@ module.exports.routes = {
   'GET /learn-more-about/setup-windows-mdm': '/guides/windows-mdm-setup',
   'GET /learn-more-about/setup-abm': '/docs/using-fleet/mdm-setup#apple-business-manager-abm',
   'GET /learn-more-about/renew-apns': '/guides/apple-mdm-setup#renew-apns',
-  'GET /learn-more-about/add-vpp': '/guides/apple-mdm-setup#add-vpp',
+  'GET /learn-more-about/add-vpp': '/guides/apple-mdm-setup#volume-purchasing-program-vpp',
   'GET /learn-more-about/renew-vpp': '/guides/apple-mdm-setup#renew-vpp',
   'GET /learn-more-about/renew-abm': '/docs/using-fleet/mdm-setup#apple-business-manager-abm',
   'GET /learn-more-about/abm-issues': '/guides/apple-mdm-setup#troubleshooting',
