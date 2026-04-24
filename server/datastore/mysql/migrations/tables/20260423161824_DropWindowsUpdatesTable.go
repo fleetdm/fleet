26a2030349c8698e092559ca1d0698ff6996e33d
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2026-04-24 14:08:05 -0300

    Fix missing migration number in Go

--- server/datastore/mysql/migrations/tables/20260423161824_DropWindowsUpdatesTable.go
+++ server/datastore/mysql/migrations/tables/20260423161824_DropWindowsUpdatesTable.go
@@ -6,16 +6,16 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260423153950, Down_20260423153950)
+	MigrationClient.AddMigration(Up_20260423161824, Down_20260423161824)
 }
 
-func Up_20260423153950(tx *sql.Tx) error {
+func Up_20260423161824(tx *sql.Tx) error {
 	if _, err := tx.Exec(`DROP TABLE IF EXISTS windows_updates`); err != nil {
 		return fmt.Errorf("drop windows_updates table: %w", err)
 	}
 	return nil
 }
 
-func Down_20260423153950(tx *sql.Tx) error {
+func Down_20260423161824(tx *sql.Tx) error {
 	return nil
 }
