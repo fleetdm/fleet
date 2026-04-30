b31545bfc40971c0f5c7f8236cc52e60c21ae3db
Jorge Falcon <22119513+BCTBB@users.noreply.github.com>
2026-04-29 23:29:05 -0400

    temporary glue table import

--- infrastructure/dogfood/terraform/aws-tf-module/imports.tf
+++ infrastructure/dogfood/terraform/aws-tf-module/imports.tf
@@ -2,3 +2,8 @@ import {
   to = module.monitoring.aws_cloudwatch_log_group.cron_monitoring_lambda[0]
   id = "/aws/lambda/${local.customer}_cron_monitoring"
 }
+
+import {
+  to = module.logging_alb.aws_glue_catalog_table.partitioned_alb_logs[0]
+  id = "160035666661:fleet_dogfood_alb_logs:partitioned_alb_logs"
+}
