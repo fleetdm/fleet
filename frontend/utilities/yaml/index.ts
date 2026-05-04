962784be5608571d4d23e1c496fa347502131f1f
Steven Palmesano <3100993+spalmesano0@users.noreply.github.com>
2026-05-04 18:05:59 -0500

    Remove comments from Agent options in the UI

--- frontend/utilities/yaml/index.ts
+++ frontend/utilities/yaml/index.ts
@@ -18,24 +18,15 @@ export const agentOptionsToYaml = (agentOpts: any) => {
     delete agentOpts.overrides;
   }
 
-  // add a comment besides the "command_line_flags" if it is empty
-  let addFlagsComment = false;
+  // hide the "command_line_flags" key if it is empty
   if (
     !agentOpts.command_line_flags ||
     Object.keys(agentOpts.command_line_flags).length === 0
   ) {
-    // delete it so it does not render, and will add it explicitly after (along with the comment)
     delete agentOpts.command_line_flags;
-    addFlagsComment = true;
   }
 
-  let yamlString = yaml.dump(agentOpts);
-  if (addFlagsComment) {
-    yamlString +=
-      "# Requires Fleet's osquery installer\n# command_line_flags: {}\n";
-  }
-
-  return yamlString;
+  return yaml.dump(agentOpts);
 };
 
 export default constructErrorString;
