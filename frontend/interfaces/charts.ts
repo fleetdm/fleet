c838c418127248bc383d2b49340f4fc1a48be375
Scott Gress <scott@fleetdm.com>
2026-05-05 09:46:39 -0500

    update interface

--- frontend/interfaces/charts.ts
+++ frontend/interfaces/charts.ts
@@ -2,11 +2,15 @@ import { ReactNode } from "react";
 
 export type ChartType = "line" | "checkerboard";
 
+export type ChartTheme = "green" | "red";
+
 export interface IDataSet {
   name: string;
   label: string;
   defaultChartType: ChartType;
   description?: ReactNode;
+  tooltipFormatter?: TooltipFormatter;
+  theme?: ChartTheme;
 }
 
 export interface IFormattedDataPoint {
@@ -14,7 +18,18 @@ export interface IFormattedDataPoint {
   label: string;
   value: number;
   percentage: number;
+  total?: number;
 }
+
+export type TooltipFormatter = ({
+  value,
+  total,
+  percentage,
+}: {
+  value: number;
+  total?: number;
+  percentage?: number;
+}) => string | ReactNode;
 
 export const HISTORICAL_DATA_CONFIG_KEYS = [
   "uptime",
