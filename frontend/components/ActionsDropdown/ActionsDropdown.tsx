739a213dfcc3ad0e20cd62618c1602ed85a5340f
nulmete <nicoulmete1@gmail.com>
2026-05-06 15:58:54 +0200

    Scope dropdown menu outline to dark mode, drop input color change

--- frontend/components/ActionsDropdown/ActionsDropdown.tsx
+++ frontend/components/ActionsDropdown/ActionsDropdown.tsx
@@ -255,7 +255,7 @@ const ActionsDropdown = ({
     menu: (provided) => ({
       ...provided,
       backgroundColor: COLORS["core-fleet-white"],
-      boxShadow: `0 2px 6px rgba(0, 0, 0, 0.1), 0 0 0 1px ${COLORS["ui-fleet-black-10"]}`,
+      boxShadow: `0 2px 6px rgba(0, 0, 0, 0.1), 0 0 0 1px var(--dropdown-menu-outline, transparent)`,
       borderRadius: "4px",
       zIndex: 6,
       border: 0,
