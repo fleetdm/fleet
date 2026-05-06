dd6e03c6111926f0f090a92ad5a639d774e38340
Scott Gress <scott@fleetdm.com>
2026-05-06 11:47:04 -0500

    remove unneeded import

--- frontend/pages/admin/OrgSettingsPage/cards/Advanced/Advanced.tsx
+++ frontend/pages/admin/OrgSettingsPage/cards/Advanced/Advanced.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import { IInputFieldParseTarget } from "interfaces/form_field";
 import { HistoricalDataConfigKey } from "interfaces/charts";
