359408b6b9b0825594fd0788d2e3e6fd38e68f36
Steven Palmesano <3100993+spalmesano0@users.noreply.github.com>
2026-05-07 06:50:01 -0500

    Fix broken links for setup_experience (#44903)

--- cmd/fleetctl/fleetctl/generate_gitops.go
+++ cmd/fleetctl/fleetctl/generate_gitops.go
@@ -1438,7 +1438,7 @@ func (cmd *GenerateGitopsCommand) generateControls(teamId *uint, teamName string
 				result[jsonFieldName(mdmT, "MacOSSetup")] = "TODO: update with your setup_experience configuration"
 				cmd.Messages.Notes = append(cmd.Messages.Notes, Note{
 					Filename: teamName,
-					Note:     "The setup_experience configuration is not supported by this tool yet.  To configure it, please follow the Fleet documentation at https://fleetdm.com/docs/configuration/yaml-files#macos-setup",
+					Note:     "The setup_experience configuration is not supported by this tool yet.  To configure it, please follow the Fleet documentation at https://fleetdm.com/docs/configuration/yaml-files#setup-experience",
 				})
 			}
 		}
