b1ce990c139012ffd0096bdd27209855ecf8b8ab
melpike <79950145+melpike@users.noreply.github.com>
2026-05-12 10:34:14 -0600

    Add route for end user accounts guide

--- website/config/routes.js
+++ website/config/routes.js
@@ -1270,6 +1270,7 @@ module.exports.routes = {
   'GET /learn-more-about/enrollment-profiles': 'https://developer.apple.com/documentation/devicemanagement/profile?changes=l_11_5',
   'GET /learn-more-about/psso-local-account': '/guides/setup-experience',
   'GET /learn-more-about/deploy-fleet': '/docs/deploy/deploy-fleet',
+  'GET /learn-more-about/end-user-accounts': '/guides/setup-experience#end-user-account-type',
 
   // Sitemap
   // =============================================================================================================
