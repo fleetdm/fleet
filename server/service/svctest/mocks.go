a0834dcc9d666c5e453c0f4f4f946030ce0ef8a4
Lucas Manuel Rodriguez <lucas@fleetdm.com>
2026-05-12 09:01:09 -0300

    Fix lint

--- server/service/svctest/mocks.go
+++ server/service/svctest/mocks.go
@@ -3,9 +3,6 @@ package svctest
 import (
 	"context"
 	"crypto/x509"
-	"fmt"
-	"net/http"
-	"strings"
 
 	"github.com/fleetdm/fleet/v4/server/fleet"
 	"github.com/fleetdm/fleet/v4/server/mdm/scep/depot"
@@ -44,27 +41,4 @@ type acmeCSRSigner struct {
 
 func (a *acmeCSRSigner) SignCSR(_ context.Context, csr *x509.CertificateRequest) (*x509.Certificate, error) {
 	return a.signer.Signx509CSR(csr)
-}
-
-// mockRoundTripper is a custom http.RoundTripper that redirects requests to a
-// mock server. It is exported here for tests that need to swap an external
-// HTTP client to point at an httptest server.
-type mockRoundTripper struct {
-	mockServer  string
-	origBaseURL string
-	next        http.RoundTripper
-}
-
-func (rt *mockRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	if strings.Contains(req.URL.String(), rt.origBaseURL) {
-		path := strings.TrimPrefix(req.URL.Path, "/")
-		newURL := fmt.Sprintf("%s/%s", rt.mockServer, path)
-		newReq, err := http.NewRequestWithContext(req.Context(), req.Method, newURL, req.Body) //nolint:gosec // test helper
-		if err != nil {
-			return nil, err
-		}
-		newReq.Header = req.Header
-		return rt.next.RoundTrip(newReq)
-	}
-	return rt.next.RoundTrip(req)
 }
