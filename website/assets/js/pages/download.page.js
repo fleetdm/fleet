68fbc0525fe171df21dc800b63cc6df0dd595d58
Eric <eashaw@sailsjs.com>
2026-05-12 15:21:46 -0500

    update code blocks on downloads page

--- website/assets/js/pages/download.page.js
+++ website/assets/js/pages/download.page.js
@@ -13,7 +13,18 @@ parasails.registerPage('download', {
     //…
   },
   mounted: async function() {
-    //…
+    // Now add click events to each code-block-copy-button element that copies the text content of the neighboring <code> element.
+    $('[purpose="copy-button"]').on('click', async function() {
+      // Get the text content of the closest <code> element to the copy button.
+      let code = $(this).closest('[purpose="codeblock"]').find('code').text();
+      // Add the copied class to the copy button (which replaces the icon with a checkmark).
+      $(this).addClass('copied');
+      // Remove the copied class after 2 seconds.
+      setTimeout(()=>{
+        $(this).removeClass('copied');
+      }, 2000);
+      navigator.clipboard.writeText(code);
+    });
   },
 
   //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
