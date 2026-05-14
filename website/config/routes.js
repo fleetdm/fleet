e7efe0f98c02748f55b4de3a9f3672e6660383f6
melpike <79950145+melpike@users.noreply.github.com>
2026-05-14 08:57:48 -0600

    Add route for Apple Setup Assistant link (#44407)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** Resolves #44288 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **New Features**
    * Added a new "Learn More About Apple Setup Assistant" link that directs
    users to Apple's official support documentation.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- website/config/routes.js
+++ website/config/routes.js
@@ -1304,6 +1304,7 @@ module.exports.routes = {
   'GET /learn-more-about/generate-fleets-agent': 'https://fleetdm.com/guides/enroll-hosts#ui',
   'GET /learn-more-about/certificates': '/guides/connect-end-user-to-wifi-with-certificate',
   'GET /learn-more-about/enrollment-profiles': 'https://developer.apple.com/documentation/devicemanagement/profile?changes=l_11_5',
+  'GET /learn-more-about/apple-setup-assistant': 'https://support.apple.com/guide/deployment/manage-setup-assistant-depdeff4a547/web',
   'GET /learn-more-about/psso-local-account': '/guides/setup-experience#platform-sso',
   'GET /learn-more-about/deploy-fleet': '/docs/deploy/deploy-fleet',
   'GET /learn-more-about/fleets': '/guides/fleets',
