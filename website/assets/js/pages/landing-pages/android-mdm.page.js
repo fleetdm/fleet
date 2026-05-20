a2af713f79afcecd254a69522a9d368dde4f3191
Ashish Kuthiala <53918208+akuthiala@users.noreply.github.com>
2026-05-20 12:34:29 -0700

    new webpage about android

---
+++ website/assets/js/pages/landing-pages/android-mdm.page.js
@@ -0,0 +1,30 @@
+parasails.registerPage('android-mdm', {
+  //  ╦╔╗╔╦╔╦╗╦╔═╗╦    ╔═╗╔╦╗╔═╗╔╦╗╔═╗
+  //  ║║║║║ ║ ║╠═╣║    ╚═╗ ║ ╠═╣ ║ ║╣
+  //  ╩╝╚╝╩ ╩ ╩╩ ╩╩═╝  ╚═╝ ╩ ╩ ╩ ╩ ╚═╝
+  data: {
+    modal: '',
+  },
+
+  //  ╦  ╦╔═╗╔═╗╔═╗╦ ╦╔═╗╦  ╔═╗
+  //  ║  ║╠╣ ║╣ ║  ╚╦╝║  ║  ║╣
+  //  ╩═╝╩╚  ╚═╝╚═╝ ╩ ╚═╝╩═╝╚═╝
+  beforeMount: function() {
+    //…
+  },
+  mounted: async function() {
+    //…
+  },
+
+  //  ╦╔╗╔╔╦╗╔═╗╦═╗╔═╗╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+  //  ║║║║ ║ ║╣ ╠╦╝╠═╣║   ║ ║║ ║║║║╚═╗
+  //  ╩╝╚╝ ╩ ╚═╝╩╚═╩ ╩╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
+  methods: {
+    clickOpenVideoModal: function(modalName) {
+      this.modal = modalName;
+    },
+    closeModal: function() {
+      this.modal = undefined;
+    },
+  }
+});
--- website/config/routes.js
+++ website/config/routes.js
@@ -680,6 +680,14 @@ module.exports.routes = {
     }
   },
 
+  'GET /lp/android-mdm': {
+    action: 'landing-pages/view-android-mdm',
+    locals: {
+      pageTitleForMeta: 'Android MDM: open source Android device management at enterprise scale',
+      pageDescriptionForMeta: 'Fleet is open source Android MDM for modern IT and security teams. Enroll, configure, and secure Android phones and tablets with Android Enterprise, Work Profile, managed Google Play, and GitOps. Self-hosted or Fleet-hosted.',
+    }
+  },
+
   'GET /lp/on-premise': {
     action: 'landing-pages/view-on-premise',
     locals: {
