9a01e592d648b5d9260edf4c6681a88a4f3f383d
George Karr <georgekarrv@users.noreply.github.com>
2026-05-22 16:03:02 -0500

    Cherry-pick #46097: Renumber 4.86 migrations after the 4.85.1 cherry-pick (#46079) (#46099)

--- server/datastore/mysql/migrations/tables/20260522195233_AddManagedAppleIDToHostMDM.go
+++ server/datastore/mysql/migrations/tables/20260522195233_AddManagedAppleIDToHostMDM.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260512173250, Down_20260512173250)
+	MigrationClient.AddMigration(Up_20260522195233, Down_20260522195233)
 }
 
-func Up_20260512173250(tx *sql.Tx) error {
+func Up_20260522195233(tx *sql.Tx) error {
 	if _, err := tx.Exec(`
 		ALTER TABLE host_mdm
 		ADD COLUMN managed_apple_id VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL AFTER fleet_enroll_ref
@@ -36,6 +36,6 @@ func Up_20260512173250(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20260512173250(tx *sql.Tx) error {
+func Down_20260522195233(tx *sql.Tx) error {
 	return nil
 }
