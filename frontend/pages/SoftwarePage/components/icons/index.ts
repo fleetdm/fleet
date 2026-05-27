bc0bb48e644f60d39e796502a4ab522eb06dfb2a
Harrison Ravazzolo <38767391+harrisonravazzolo@users.noreply.github.com>
2026-05-27 16:27:59 -0700

    add amazon workspaces app

--- frontend/pages/SoftwarePage/components/icons/index.ts
+++ frontend/pages/SoftwarePage/components/icons/index.ts
@@ -6,6 +6,7 @@ import { ISoftware } from "interfaces/software";
 import { matchLoosePrefixToKey } from "utilities/strings/stringUtils";
 
 import AmazonCorretto25 from "./AmazonCorretto25";
+import AmazonWorkspaces from "./AmazonWorkspaces";
 import Backblaze from "./Backblaze";
 import BetterDisplay from "./BetterDisplay";
 import Bluej from "./Bluej";
@@ -286,6 +287,7 @@ export const SOFTWARE_NAME_TO_ICON_MAP = {
   "amazon chime": AmazonChime,
   "amazon corretto 25": AmazonCorretto25,
   "amazon dcv": AmazonDCV,
+  "amazon workspaces": AmazonWorkspaces,
   androidPlayStore: AndroidPlayStore,
   "android studio": AndroidStudio,
   anka: Anka,
