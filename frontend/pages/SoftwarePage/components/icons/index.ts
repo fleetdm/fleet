4504b11792bec5983a5653d78313548be825bb42
Harrison Ravazzolo <38767391+harrisonravazzolo@users.noreply.github.com>
2026-05-29 08:26:23 -0700

    Windows FMA - Burp Suite Community (#46423)
    Co-authored-by: Allen Houchins <allenhouchins@mac.com>

--- frontend/pages/SoftwarePage/components/icons/index.ts
+++ frontend/pages/SoftwarePage/components/icons/index.ts
@@ -11,6 +11,7 @@ import AzulZulu25Jre from "./AzulZulu25Jre";
 import Backblaze from "./Backblaze";
 import BetterDisplay from "./BetterDisplay";
 import Bluej from "./Bluej";
+import BurpSuiteCommunity from "./BurpSuiteCommunity";
 import Cavalry from "./Cavalry";
 import Charles from "./Charles";
 import ConnectFonts from "./ConnectFonts";
@@ -323,6 +324,7 @@ export const SOFTWARE_NAME_TO_ICON_MAP = {
   box: Box,
   brave: Brave,
   bruno: Bruno,
+  "burp suite community": BurpSuiteCommunity,
   calibre: Calibre,
   camtasia: Camtasia,
   canva: Canva,
