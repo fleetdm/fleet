0c44f040d2a673c47d77264a468bb203f2794985
Magnus Jensen <magnus@fleetdm.com>
2026-05-29 12:00:25 +0200

    add migration to track update profiles

---
+++ server/datastore/mysql/migrations/tables/20260529091823_AddUpdateProfileSettingsTrackingTable_test.go
@@ -0,0 +1,77 @@
+package tables
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUp_20260529091823(t *testing.T) {
+	db := applyUpToPrev(t)
+
+	// Insert two declarations and two windows profiles, one with software update and one without.
+	decl1 := `{"declaration": {"identifier": "com.apple.configuration.softwareupdate.enforcement.specific"}}`
+	decl1UUID := uuid.NewString()
+	decl2 := `{"declaration": {"identifier": "com.apple.configuration.someotherdeclaration"}}`
+	decl2UUID := uuid.NewString()
+	if _, err := db.Exec(`INSERT INTO mdm_apple_declarations (declaration_uuid, identifier, name, raw_json) VALUES (?, "id-1", "id-1-name", ?), (?, "id-2", "id-2-name", ?)`, decl1UUID, decl1, decl2UUID, decl2); err != nil {
+		t.Fatalf("insert apple declarations: %v", err)
+	}
+
+	profile1 := `<SyncML><Target><LocURI>./Vendor/MSFT/Policy/Config/Update/Install/</LocURI></Target></SyncML>`
+	profile1UUID := uuid.NewString()
+	profile2 := `<SyncML><Target><LocURI>./Vendor/MSFT/Policy/Config/SomeOtherSetting/</LocURI></Target></SyncML>`
+	profile2UUID := uuid.NewString()
+	if _, err := db.Exec(`INSERT INTO mdm_windows_configuration_profiles (profile_uuid, name, syncml) VALUES (?, "profile-1", ?), (?, "profile-2", ?)`, profile1UUID, profile1, profile2UUID, profile2); err != nil {
+		t.Fatalf("insert windows profiles: %v", err)
+	}
+
+	// Apply current migration.
+	applyNext(t, db)
+
+	// Now we check that decl1 and profile1 made it into the table, but not decl2 and profile2.
+
+	var count int
+	if err := db.QueryRow(`SELECT COUNT(*) FROM mdm_configuration_profile_update_settings WHERE apple_declaration_uuid = ?`, decl1UUID).Scan(&count); err != nil {
+		t.Fatalf("query for decl1: %v", err)
+	}
+	require.Equal(t, 1, count, "expected decl1 to be in the update settings table, but it was not")
+
+	if err := db.QueryRow(`SELECT COUNT(*) FROM mdm_configuration_profile_update_settings WHERE apple_declaration_uuid = ?`, decl2UUID).Scan(&count); err != nil {
+		t.Fatalf("query for decl2: %v", err)
+	}
+	require.Equal(t, 0, count, "expected decl2 to not be in the update settings table, but it was")
+
+	if err := db.QueryRow(`SELECT COUNT(*) FROM mdm_configuration_profile_update_settings WHERE windows_profile_uuid = ?`, profile1UUID).Scan(&count); err != nil {
+		t.Fatalf("query for profile1: %v", err)
+	}
+	require.Equal(t, 1, count, "expected profile1 to be in the update settings table, but it was not")
+
+	if err := db.QueryRow(`SELECT COUNT(*) FROM mdm_configuration_profile_update_settings WHERE windows_profile_uuid = ?`, profile2UUID).Scan(&count); err != nil {
+		t.Fatalf("query for profile2: %v", err)
+	}
+	require.Equal(t, 0, count, "expected profile2 to not be in the update settings table, but it was")
+
+	// We test the constraint
+	if _, err := db.Exec(`INSERT INTO mdm_configuration_profile_update_settings (apple_declaration_uuid, windows_profile_uuid) VALUES (?, ?)`, decl1UUID, profile1UUID); err == nil {
+		t.Fatalf("expected error when inserting a row with both apple_declaration_uuid and windows_profile_uuid set, but got no error")
+	}
+
+	// And we check the foreign key cascade effects
+	if _, err := db.Exec(`DELETE FROM mdm_apple_declarations WHERE declaration_uuid = ?`, decl1UUID); err != nil {
+		t.Fatalf("delete decl1: %v", err)
+	}
+	if err := db.QueryRow(`SELECT COUNT(*) FROM mdm_configuration_profile_update_settings WHERE apple_declaration_uuid = ?`, decl1UUID).Scan(&count); err != nil {
+		t.Fatalf("query for decl1 after deletion: %v", err)
+	}
+	require.Equal(t, 0, count, "expected decl1 to be deleted from the update settings table after its deletion from the declarations table, but it was not")
+
+	if _, err := db.Exec(`DELETE FROM mdm_windows_configuration_profiles WHERE profile_uuid = ?`, profile1UUID); err != nil {
+		t.Fatalf("delete profile1: %v", err)
+	}
+	if err := db.QueryRow(`SELECT COUNT(*) FROM mdm_configuration_profile_update_settings WHERE windows_profile_uuid = ?`, profile1UUID).Scan(&count); err != nil {
+		t.Fatalf("query for profile1 after deletion: %v", err)
+	}
+	require.Equal(t, 0, count, "expected profile1 to be deleted from the update settings table after its deletion from the windows profiles table, but it was not")
+}
