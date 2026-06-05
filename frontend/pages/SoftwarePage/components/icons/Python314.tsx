7fd5dbdb6a17162c864b11f5d1ca4e2a21adfa38
Allen Houchins <allenhouchins@mac.com>
2026-06-04 22:26:15 -0500

    Update Python314.tsx

--- frontend/pages/SoftwarePage/components/icons/Python314.tsx
+++ frontend/pages/SoftwarePage/components/icons/Python314.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+
 import type { SVGProps } from "react";
+
 const SvgPython314 = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} {...props}>
     <image
