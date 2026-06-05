c4a5fa263ad0c4212bd063f5764ac7f1eaa04607
dantecatalfamo <dante.catalfamo@gmail.com>
2026-06-05 16:31:40 -0400

    Bump db migrations

--- server/datastore/mysql/migrations/tables/20260605202846_AddVulnPerfIndexes_test.go
+++ server/datastore/mysql/migrations/tables/20260605202846_AddVulnPerfIndexes_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUp_20260528204515(t *testing.T) {
+func TestUp_20260605202846(t *testing.T) {
 	db := applyUpToPrev(t)
 
 	applyNext(t, db)
