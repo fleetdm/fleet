a8d61e29c0d27ef50c458bc32845fa13d50b5ad5
Magnus Jensen <magnus@fleetdm.com>
2026-06-11 14:07:28 +0200

    move to after token check

--- server/service/apple_mdm.go
+++ server/service/apple_mdm.go
@@ -2268,17 +2268,17 @@ func (svc *Service) GetMDMAppleEnrollmentProfileByToken(ctx context.Context, tok
 		return nil, ctxerr.New(ctx, "get enrollment profile: missing machine info")
 	}
 
-	if machineInfo.MandatorySoftwareUpdateRequired {
-		// Log an info message if the device is requiring a mandatory software update.
-		svc.logger.InfoContext(ctx, "device requires mandatory software update", "uuid", machineInfo.UDID, "serial", machineInfo.Serial, "product", machineInfo.Product, "os_version", machineInfo.OSVersion)
-	}
-
 	_, err = svc.ds.GetMDMAppleEnrollmentProfileByToken(ctx, token)
 	if err != nil {
 		if fleet.IsNotFound(err) {
 			return nil, fleet.NewAuthFailedError("enrollment profile not found")
 		}
 		return nil, ctxerr.Wrap(ctx, err, "get enrollment profile")
+	}
+
+	if machineInfo.MandatorySoftwareUpdateRequired {
+		// Log an info message if the device is requiring a mandatory software update.
+		svc.logger.InfoContext(ctx, "device requires mandatory software update", "host_uuid", machineInfo.UDID, "serial", machineInfo.Serial, "product", machineInfo.Product, "os_version", machineInfo.OSVersion)
 	}
 
 	appConfig, err := svc.ds.AppConfig(ctx)
