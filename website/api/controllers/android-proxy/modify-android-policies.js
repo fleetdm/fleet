cda6429f40e1ea8179a0d9f3584298d02b3348f0
Eric <eashaw@sailsjs.com>
2026-06-23 16:17:18 -0500

    update exit signal, add exit to more endpoints

--- website/api/controllers/android-proxy/modify-android-policies.js
+++ website/api/controllers/android-proxy/modify-android-policies.js
@@ -97,7 +97,7 @@ module.exports = {
       return {'policyNotFound': `Specified policy not found on this Android enterprise (${androidEnterpriseId}): ${err}`};
     }).intercept((err) => {
       if([502, 503, 504].includes(err.status)){
-        return {'managementApiError': `The Android management API returned an error: ${err}`};
+        return {'managementApiError': `The Android management API returned a 5xx error: ${err}`};
       } else {
         return new Error(`When attempting to update a policy for an Android enterprise (${androidEnterpriseId}), an error occurred. Error: ${require('util').inspect(err)}`);
       }
