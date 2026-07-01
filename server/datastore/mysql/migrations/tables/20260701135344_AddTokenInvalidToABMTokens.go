254a9cdd49e3d2da366c0909c1095466e862fe80
andymFleet <andrewmellor@fleetdm.com>
2026-07-01 15:22:02 +0100

    Add migration to track invalid ABM token state

---
+++ server/datastore/mysql/migrations/tables/20260701135344_AddTokenInvalidToABMTokens.go
@@ -0,0 +1,18 @@
+package tables
+
+import (
+	"database/sql"
+)
+
+func init() {
+	MigrationClient.AddMigration(Up_20260701135344, Down_20260701135344)
+}
+
+func Up_20260701135344(tx *sql.Tx) error {
+	_, err := tx.Exec(`ALTER TABLE abm_tokens ADD COLUMN token_invalid TINYINT(1) NOT NULL DEFAULT '0'`)
+	return err
+}
+
+func Down_20260701135344(tx *sql.Tx) error {
+	return nil
+}
