f104d5d72827aa70c439f9a186285a7f08313ce2
Jonathan Katz <yehonatankatz@gmail.com>
2026-07-02 20:34:17 -0400

    Bump migration timestamp past #48664 (#48396)

--- server/datastore/mysql/migrations/tables/20260702232839_MultipleCustomPackagesPerTitle_test.go
+++ server/datastore/mysql/migrations/tables/20260702232839_MultipleCustomPackagesPerTitle_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUp_20260629163945(t *testing.T) {
+func TestUp_20260702232839(t *testing.T) {
 	db := applyUpToPrev(t)
 
 	insertTitle := func(name string, source string) int64 {
