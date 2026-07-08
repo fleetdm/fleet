0c3abad5483d0182d990a2530f15428b94ea837f
Noah Talerman <noahtal@umich.edu>
2026-07-08 10:59:05 -0700

    Fix enroll page and Add hosts modal styling issues
    Resolves #48967

--- website/config/routes.js
+++ website/config/routes.js
@@ -1322,6 +1322,7 @@ module.exports.routes = {
   'GET /learn-more-about/okta-conditional-access': '/guides/okta-conditional-access-integration',
   'GET /learn-more-about/organization-logo-size': '/docs/configuration/yaml-files#org-info',
   'GET /learn-more-about/byod-hosts-vpp-install': 'https://github.com/fleetdm/fleet/issues/31138',
+  'GET /learn-more-about/byod-transparency': '/better',
   'GET /learn-more-about/install-google-play-apps': 'https://github.com/fleetdm/fleet/issues/25595',
   'GET /learn-more-about/arch-linux-rolling-release': 'https://wiki.archlinux.org/title/Arch_Linux',
   'GET /learn-more-about/google-play-store': 'https://play.google.com/store/apps',
