e0c2ade9c7971ec7c4a4ed84c3bd5f572cc3311d
Juan Fernandez <juan@fleetdm.com>
2026-07-08 19:24:22 -0400

    Replace literal by constant

--- frontend/pages/policies/details/PolicyDetailsPage/PolicyDetailsPage.tsx
+++ frontend/pages/policies/details/PolicyDetailsPage/PolicyDetailsPage.tsx
@@ -19,7 +19,10 @@ import { PLATFORM_DISPLAY_NAMES, Platform } from "interfaces/platform";
 import policiesAPI from "services/entities/policies";
 import teamsAPI, { ILoadTeamResponse } from "services/entities/teams";
 import { addGravatarUrlToResource } from "utilities/helpers";
-import { DOCUMENT_TITLE_SUFFIX } from "utilities/constants";
+import {
+  DEFAULT_EMPTY_CELL_VALUE,
+  DOCUMENT_TITLE_SUFFIX,
+} from "utilities/constants";
 import { getPathWithQueryParams } from "utilities/url";
 import useTeamIdParam from "hooks/useTeamIdParam";
 
@@ -322,7 +325,7 @@ const PolicyDetailsPage = ({
       <DataSet
         className={`${baseClass}__automations`}
         title="Automations"
-        value="---"
+        value={DEFAULT_EMPTY_CELL_VALUE}
       />
     );
 
