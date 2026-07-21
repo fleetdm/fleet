70f7adba5269f86cb92ca48481b4d944edd7ab5e
andymFleet <andrewmellor@fleetdm.com>
2026-07-21 10:37:44 +0100

    Wrap ALTER TABLE error in AddTokenInvalidToABMTokens migration

--- server/datastore/mysql/migrations/tables/20260721090128_AddTokenInvalidToABMTokens.go
+++ server/datastore/mysql/migrations/tables/20260721090128_AddTokenInvalidToABMTokens.go
@@ -2,6 +2,7 @@ package tables
 
 import (
 	"database/sql"
+	"fmt"
 )
 
 func init() {
@@ -9,8 +10,10 @@ func init() {
 }
 
 func Up_20260721090128(tx *sql.Tx) error {
-	_, err := tx.Exec(`ALTER TABLE abm_tokens ADD COLUMN token_invalid TINYINT(1) NOT NULL DEFAULT '0'`)
-	return err
+	if _, err := tx.Exec(`ALTER TABLE abm_tokens ADD COLUMN token_invalid TINYINT(1) NOT NULL DEFAULT '0'`); err != nil {
+		return fmt.Errorf("adding token_invalid column to abm_tokens table: %w", err)
+	}
+	return nil
 }
 
 func Down_20260721090128(tx *sql.Tx) error {
