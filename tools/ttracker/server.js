038b34eeb11b7e159821b35be0eda9d09b9648a9
Sharon <sharon@fleetdm.com>
2026-07-21 18:02:41 -0400

    Show Park button for non-Claude (idle) terminals

--- tools/ttracker/server.js
+++ tools/ttracker/server.js
@@ -1120,7 +1120,7 @@ function renderActive(data) {
 
   el.innerHTML = sessions.map((s, i) => {
     let action = '<button class="btn btn-focus" onclick="focusSession(\\'' + s.iterm_uuid + '\\')">Focus</button>';
-    if (s.status === 'running') {
+    if (s.status === 'running' || s.status === 'no-claude') {
       action += ' <button class="btn btn-park" onclick="parkSession(\\'' + s.iterm_uuid + '\\')">Park</button>';
     } else if (s.status === 'missing') {
       action = '<button class="btn btn-restore" onclick="restoreSession(\\'' + s.claude_session_id + '\\')">Restore</button>'
