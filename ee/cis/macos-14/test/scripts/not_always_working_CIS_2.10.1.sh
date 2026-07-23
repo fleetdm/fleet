4644f72fc4b79b023a5becf609e9b04d192cbdca
Juan Fernandez <juan@fleetdm.com>
2026-07-23 08:55:07 -0400

    Fixed invalid entry

--- ee/cis/macos-14/test/scripts/not_always_working_CIS_2.10.1.sh
+++ ee/cis/macos-14/test/scripts/not_always_working_CIS_2.10.1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #replace username
-sudo -u <username> /usr/bin/defaults -currentHost write com.apple.screensaver idleTime -int 1200
+sudo -u <username> /usr/bin/defaults -currentHost write com.apple.screensaver idleTime -int 900
