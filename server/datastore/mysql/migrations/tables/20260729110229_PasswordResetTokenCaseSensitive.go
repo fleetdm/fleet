309c21d1a18254a64daa956a962065838b73be25
Juan Fernandez <juan@fleetdm.com>
2026-07-29 07:03:37 -0400

    Bump migration

--- server/datastore/mysql/migrations/tables/20260729110229_PasswordResetTokenCaseSensitive.go
+++ server/datastore/mysql/migrations/tables/20260729110229_PasswordResetTokenCaseSensitive.go
@@ -6,10 +6,10 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260727111742, Down_20260727111742)
+	MigrationClient.AddMigration(Up_20260729110229, Down_20260729110229)
 }
 
-func Up_20260727111742(tx *sql.Tx) error {
+func Up_20260729110229(tx *sql.Tx) error {
 	// Password reset tokens are base64url-encoded, so their alphabet is
 	// case-sensitive. The column defaulted to the case-insensitive
 	// utf8mb4_unicode_ci collation, which made lookups match case-mutated
@@ -23,6 +23,6 @@ func Up_20260727111742(tx *sql.Tx) error {
 	return nil
 }
 
-func Down_20260727111742(tx *sql.Tx) error {
+func Down_20260729110229(tx *sql.Tx) error {
 	return nil
 }
