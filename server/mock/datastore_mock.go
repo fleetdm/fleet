883b06473df411e7ae9d08fcf19b2d4b61a22696
Magnus Jensen <magnus@fleetdm.com>
2026-07-29 15:03:01 +0200

    fix mock file

--- server/mock/datastore_mock.go
+++ server/mock/datastore_mock.go
@@ -2268,7 +2268,7 @@ type GetAppleDDMAssetsReferencedByDeclarationsFunc func(ctx context.Context, dec
 
 type BatchSetAppleDDMAssetsFunc func(ctx context.Context, teamID *uint, assets []*fleet.MDMAppleDDMAssetToSet) (*fleet.MDMAppleDDMAssetsBatchChanges, error)
 
-type InsertMacOSSoftwareUpdateDeviceIDFunc func(ctx context.Context, hostUUID string, updateDeviceID string) error
+type InsertAppleSoftwareUpdateDeviceIDFunc func(ctx context.Context, hostUUID string, updateDeviceID string) error
 
 type DataStore struct {
 	AppConfigFunc        AppConfigFunc
@@ -5637,8 +5637,8 @@ type DataStore struct {
 	BatchSetAppleDDMAssetsFunc        BatchSetAppleDDMAssetsFunc
 	BatchSetAppleDDMAssetsFuncInvoked bool
 
-	InsertMacOSSoftwareUpdateDeviceIDFunc        InsertMacOSSoftwareUpdateDeviceIDFunc
-	InsertMacOSSoftwareUpdateDeviceIDFuncInvoked bool
+	InsertAppleSoftwareUpdateDeviceIDFunc        InsertAppleSoftwareUpdateDeviceIDFunc
+	InsertAppleSoftwareUpdateDeviceIDFuncInvoked bool
 
 	mu sync.Mutex
 }
@@ -13499,7 +13499,7 @@ func (s *DataStore) BatchSetAppleDDMAssets(ctx context.Context, teamID *uint, as
 
 func (s *DataStore) InsertAppleSoftwareUpdateDeviceID(ctx context.Context, hostUUID string, updateDeviceID string) error {
 	s.mu.Lock()
-	s.InsertMacOSSoftwareUpdateDeviceIDFuncInvoked = true
+	s.InsertAppleSoftwareUpdateDeviceIDFuncInvoked = true
 	s.mu.Unlock()
-	return s.InsertMacOSSoftwareUpdateDeviceIDFunc(ctx, hostUUID, updateDeviceID)
+	return s.InsertAppleSoftwareUpdateDeviceIDFunc(ctx, hostUUID, updateDeviceID)
 }
