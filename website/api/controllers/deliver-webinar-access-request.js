b2f18cf4e5b0b6bfac14d8c984ea96914910ced6
Eric <eashaw@sailsjs.com>
2026-08-04 17:23:26 -0500

    update set historical event source

--- website/api/controllers/deliver-webinar-access-request.js
+++ website/api/controllers/deliver-webinar-access-request.js
@@ -58,7 +58,7 @@ module.exports = {
         eventType: 'Intent signal',
         intentSignal: 'Requested webinar recording',
         eventContent: webinarName,
-        eventSource: 'Webinar',
+        eventSource: 'Website - Gated video',
       }).intercept((err)=>{
         return new Error(`Could not create an historical event. Full error: ${require('util').inspect(err)}`);
       });
