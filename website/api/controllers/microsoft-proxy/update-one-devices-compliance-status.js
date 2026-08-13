1682935919d6285ed5228311dfa00a38bed27a48
Eric <eashaw@sailsjs.com>
2026-08-13 15:58:07 -0500

    Update update-one-devices-compliance-status.js

--- website/api/controllers/microsoft-proxy/update-one-devices-compliance-status.js
+++ website/api/controllers/microsoft-proxy/update-one-devices-compliance-status.js
@@ -128,11 +128,11 @@ module.exports = {
           'Authorization': `Bearer ${graphAccessToken}`
         }
       }).intercept((err)=>{
-        return new Error({error: `An error occurred when getting a user ID from a user principal name (${userPrincipalName}) for a complaince status update. Full error: ${require('util').inspect(err, {depth: 3})}`});
+        return new Error(`An error occurred when getting a user ID from a user principal name (${userPrincipalName}) for a complaince status update. Full error: ${require('util').inspect(err, {depth: 3})}`);
       });
 
       if(!informationAboutThisUser.id) {
-        return new Error({error: `An error occurred when getting information about a user (${userPrincipalName}). The response from the Microsoft graph API did not include an ID.`});
+        return new Error(`An error occurred when getting information about a user (${userPrincipalName}). The response from the Microsoft graph API did not include an ID.`);
       }
 
       let lastUpdateTime = new Date().toISOString();
@@ -181,7 +181,7 @@ module.exports = {
           Content: JSON.stringify(complianceUpdateContent),
         }
       }).intercept((err)=>{
-        return new Error({error: `An error occurred when sending a request to sync a device's compliance status for a Microsoft compliance tenant. Full error: ${require('util').inspect(err, {depth: 3})}`});
+        return new Error(`An error occurred when sending a request to sync a device's compliance status for a Microsoft compliance tenant. Full error: ${require('util').inspect(err, {depth: 3})}`);
       });
       // Log responses from Micrsoft APIs for Fleet's integration
       if(informationAboutThisTenant.fleetInstanceUrl === 'https://dogfood.fleetdm.com') {
