f720f3271702b22e02212e04d68d472926c7afd9
Sharon <sharon@fleetdm.com>
2026-08-21 10:31:31 -0400

    Fix integration test: device endpoint now returns policies

--- server/service/integration_desktop_test.go
+++ server/service/integration_desktop_test.go
@@ -82,7 +82,7 @@ func (s *integrationTestSuite) TestDeviceAuthenticatedEndpoints() {
 	require.False(t, getHostResp.Host.RefetchRequested)
 	require.Equal(t, "http://example.com/logo", getHostResp.OrgLogoURL)
 	require.Equal(t, "http://example.com/contact", getHostResp.OrgContactURL)
-	require.Nil(t, getHostResp.Host.Policies)
+	require.NotNil(t, getHostResp.Host.Policies)
 	require.NotNil(t, getHostResp.Host.Batteries)
 	require.Equal(t, &fleet.HostBattery{CycleCount: 1, Health: "Normal"}, (*getHostResp.Host.Batteries)[0])
 	require.True(t, getHostResp.GlobalConfig.MDM.EnabledAndConfigured)
