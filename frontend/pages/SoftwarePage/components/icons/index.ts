b91be2b5079feadbbe61551516786a933c3380b3
nulmete <nicoulmete1@gmail.com>
2026-08-26 20:59:23 -0300

    TEMP demo: three icon-check warnings (to be reverted)
    Exercises the advisory rules on a real PR:
      - Demounquantized.png  color type 6 (pngquant not run)
      - Demowrongsize.png    64x64 instead of 128x128
      - Demoorphan.png       on disk, never imported
    
    Expect the lint-js job to PASS with three warning annotations.

--- frontend/pages/SoftwarePage/components/icons/index.ts
+++ frontend/pages/SoftwarePage/components/icons/index.ts
@@ -1144,6 +1144,8 @@ import Zoom from "./Zoom";
 import ZoomOutlookPlugin from "./png/ZoomOutlookPlugin.png";
 import ZoomRooms from "./png/ZoomRooms.png";
 import Zotero from "./png/Zotero.png";
+import Demounquantized from "./png/Demounquantized.png";
+import Demowrongsize from "./png/Demowrongsize.png";
 import Zulip from "./png/Zulip.png";
 import Zwift from "./png/Zwift.png";
 // SOFTWARE_NAME_TO_ICON_MAP list "special" applications that have a defined
@@ -2317,6 +2319,8 @@ export const SOFTWARE_NAME_TO_ICON_MAP = {
   "zoom outlook plugin": ZoomOutlookPlugin,
   "zoom rooms": ZoomRooms,
   zotero: Zotero,
+  demounquantized: Demounquantized,
+  demowrongsize: Demowrongsize,
   zulip: Zulip,
   zwift: Zwift,
 } as const;
