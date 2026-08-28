e202c11bcca473f141d7181396ec6f6de42120cf
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2026-08-28 08:23:27 -0400

    GitOps: pin policy to the exact installer resolved from the YAML (#52002)

--- server/datastore/mysql/software_installers.go
+++ server/datastore/mysql/software_installers.go
@@ -4044,6 +4044,7 @@ func (ds *Datastore) GetSoftwareInstallers(ctx context.Context, teamID uint) ([]
 SELECT
   si.team_id,
   si.title_id,
+  si.id AS installer_id,
   si.url,
   si.storage_id AS hash_sha256,
   si.fleet_maintained_app_id,
@@ -4064,6 +4065,7 @@ UNION ALL
 SELECT
 	iha.team_id,
 	iha.title_id,
+	0 AS installer_id,
 	iha.url,
 	iha.storage_id as hash_sha256,
 	NULL as fleet_maintained_app_id,
