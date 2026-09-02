802d730b7937d65eaaa74b306cd15e61d5a6217e
Magnus Jensen <magnus@fleetdm.com>
2026-09-02 16:04:03 +0200

    bump migrations

--- server/datastore/mysql/migrations/tables/20260902140158_AddAppConfigOnlyAllowAppleBusinessEnrollment.go
+++ server/datastore/mysql/migrations/tables/20260902140158_AddAppConfigOnlyAllowAppleBusinessEnrollment.go
@@ -7,16 +7,16 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260901122716, Down_20260901122716)
+	MigrationClient.AddMigration(Up_20260902140158, Down_20260902140158)
 }
 
-func Up_20260901122716(tx *sql.Tx) error {
+func Up_20260902140158(tx *sql.Tx) error {
 	return updateAppConfigJSON(tx, func(config *fleet.AppConfig) error {
 		config.MDM.OnlyAllowAppleBusinessEnrollment = false
 		return nil
 	})
 }
 
-func Down_20260901122716(tx *sql.Tx) error {
+func Down_20260902140158(tx *sql.Tx) error {
 	return nil
 }
--- server/datastore/mysql/migrations/tables/20260902140230_AddRenewalExcludedAtToNanoCertAuthAssociations.go
+++ server/datastore/mysql/migrations/tables/20260902140230_AddRenewalExcludedAtToNanoCertAuthAssociations.go
@@ -5,14 +5,14 @@ import (
 )
 
 func init() {
-	MigrationClient.AddMigration(Up_20260901122726, Down_20260901122726)
+	MigrationClient.AddMigration(Up_20260902140230, Down_20260902140230)
 }
 
-func Up_20260901122726(tx *sql.Tx) error {
+func Up_20260902140230(tx *sql.Tx) error {
 	_, err := tx.Exec("ALTER TABLE nano_cert_auth_associations ADD COLUMN renewal_excluded_at TIMESTAMP(6) NULL DEFAULT NULL;")
 	return err
 }
 
-func Down_20260901122726(tx *sql.Tx) error {
+func Down_20260902140230(tx *sql.Tx) error {
 	return nil
 }
