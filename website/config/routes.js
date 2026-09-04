7a1227f260b8602dcd6c0aba32e19126ae527e5b
LeAnn <97471894+Leanngove@users.noreply.github.com>
2026-09-04 08:17:41 -0700

    Adding Android encryption status doc to routes.js (#50634)
    <!-- Add the related story/sub-task/bug number, like Resolves #123, or
    remove if NA -->
    **Related issue:** #49791
    
    Add new UI route for Android encryption status tooltip
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **New Features**
    * Added a redirect from `/learn-more-about/android-encryption-status` to
    Google’s Android Management API documentation on device encryption
    status.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- website/config/routes.js
+++ website/config/routes.js
@@ -1420,6 +1420,7 @@ module.exports.routes = {
   'GET /learn-more-about/self-service-categories': '/guides/software-self-service#manage-self-service-categories',
   'GET /learn-more-about/linux-wipe': '/guides/lock-wipe-hosts#linux-wipe-behavior',
   'GET /learn-more-about/configuration-profile-assets': '/articles/custom-os-settings#apple-declarations-ddm',
+  'GET /learn-more-about/android-encryption-status': 'https://developers.google.com/android/management/reference/rest/v1/enterprises.devices#EncryptionStatus',
   'GET /learn-more-about/release-devices': 'https://developer.apple.com/documentation/devicemanagement/disown-devices',
   'GET /learn-more-about/mdm-enrollment': '/guides/windows-mdm-setup#manual-enrollment',
   'GET /learn-more-about/windows-default-fleet': '/guides/windows-mdm-setup#set-a-default-fleet-for-new-hosts',
