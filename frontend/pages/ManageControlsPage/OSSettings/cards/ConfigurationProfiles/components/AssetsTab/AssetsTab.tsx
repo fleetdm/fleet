779aaf2323fa019a10c41bbe06e073fffc79c657
Noah Talerman <noahtal@umich.edu>
2026-09-08 11:43:15 -0400

    Tweak Assets tab help text wording
    Remove "many" and a trailing space from the tab-header help text.

--- frontend/pages/ManageControlsPage/OSSettings/cards/ConfigurationProfiles/components/AssetsTab/AssetsTab.tsx
+++ frontend/pages/ManageControlsPage/OSSettings/cards/ConfigurationProfiles/components/AssetsTab/AssetsTab.tsx
@@ -185,7 +185,7 @@ const AssetsTab = ({ currentTeamId, router }: IAssetsTabProps) => {
           variant="right-panel"
           content={
             <>
-              Add assets (data or credentials) to use them in many Apple declaration (DDM) 
+              Add assets (data or credentials) to use them in Apple declaration (DDM)
               profiles. Apple only.{" "}
               <CustomLink
                 url={`${LEARN_MORE_ABOUT_BASE_LINK}/configuration-profile-assets`}
