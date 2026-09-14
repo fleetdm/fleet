951fac8166cdf6ffefc3ab85d0bf6c5119981bd8
RachelElysia <71795832+RachelElysia@users.noreply.github.com>
2026-09-14 10:18:35 -0400

    Prettier: blank line between third-party and relative imports

--- frontend/components/TableContainer/DataTable/SoftwareNameCell/SoftwareNameCell.tests.tsx
+++ frontend/components/TableContainer/DataTable/SoftwareNameCell/SoftwareNameCell.tests.tsx
@@ -4,6 +4,7 @@ import React from "react";
 
 import { createCustomRenderer, createMockRouter } from "test/test-utils";
 import { internationalTimeOnlyFormat } from "utilities/helpers";
+
 import SoftwareNameCell from "./SoftwareNameCell";
 
 const mockRouter = createMockRouter();
