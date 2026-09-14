5be05734ee189b9d8e23c7b2f6e8cec2476c9cc4
Noah Talerman <47070608+noahtalerman@users.noreply.github.com>
2026-09-14 15:57:54 -0400

    Apply suggestion from @noahtalerman

--- frontend/pages/queries/edit/components/EditQueryForm/EditQueryForm.tsx
+++ frontend/pages/queries/edit/components/EditQueryForm/EditQueryForm.tsx
@@ -673,8 +673,7 @@ const EditQueryForm = ({
                 wrapperClassName={`${baseClass}__form-field form-field--frequency`}
                 helpText={
                   <>
-                    Hosts report at fixed times, not after being online this
-                    long (e.g., on the hour for a 1-hour interval).{" "}
+                    Hosts report at fixed times (e.g., on the hour for a 1-hour interval).{" "}
                     <CustomLink
                       url="https://fleetdm.com/guides/reports#schedule-a-report"
                       text="Learn more"
