8835b068cde51f118178a0cfd7b18c62befc73bb
Magnus Jensen <magnus@fleetdm.com>
2026-09-16 15:03:26 +0200

    CP: wait 5 seconds before refetching host details on apns ping (#53311) (#53324)
    Cherry picks: #53311 into 4.92

--- frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
+++ frontend/pages/hosts/details/HostDetailsPage/HostDetailsPage.tsx
@@ -345,6 +345,9 @@ const HostDetailsPage = ({
   const [showRefetchSpinner, setShowRefetchSpinner] = useState(false);
   const [usersState, setUsersState] = useState<{ username: string }[]>([]);
   const [usersSearchString, setUsersSearchString] = useState("");
+  const [refetchTimeout, setRefetchTimeout] = useState<NodeJS.Timeout | null>(
+    null
+  );
   const queryClient = useQueryClient();
 
   const [
@@ -715,6 +718,14 @@ const HostDetailsPage = ({
       document.title = `Hosts | ${DOCUMENT_TITLE_SUFFIX}`;
     }
   }, [location.pathname, host]);
+
+  useEffect(() => {
+    return () => {
+      if (refetchTimeout) {
+        clearTimeout(refetchTimeout);
+      }
+    };
+  }, [refetchTimeout]);
 
   const summaryData = normalizeEmptyValues(pick(host, HOST_SUMMARY_DATA));
 
@@ -2248,7 +2259,14 @@ const HostDetailsPage = ({
             platform={host.platform}
             lastMDMCheckIn={host.last_mdm_checked_in_at}
             connectedToFleet={host.mdm.connected_to_fleet}
-            onSuccessfulCheckIn={refetchHostDetails}
+            onSuccessfulCheckIn={() => {
+              // Delay the refetch of the host details
+              // so the device has time to check in.
+              const timeout = setTimeout(() => {
+                refetchHostDetails();
+              }, 5000);
+              setRefetchTimeout(timeout);
+            }}
             user={currentUser}
             router={router}
             onExit={toggleMDMStatusModal}
