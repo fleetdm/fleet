1fbc593aa7347301f7d7aa7b409cc398caeb6eb4
Mario Panighetti <mpanighetti@mac.com>
2026-09-17 14:45:26 -0700

    removed ! logic and grep -q from gitops.sh (fixes SC2251, SC2337)
    - removed ! logic and grep -q from gitops.sh
      - ! was failing SC2251: `This ! is not on a condition and skips errexit`
      - grep -q was failing SC2337: `In pipefail mode, flags like -q, -m, or -L can cause grep to exit early, aborting the pipeline with SIGPIPE.`

--- .github/actions/gitops/gitops.sh
+++ .github/actions/gitops/gitops.sh
@@ -32,7 +32,9 @@ fi
 if compgen -G "$FLEET_GITOPS_DIR"/fleets/*.yml > /dev/null; then
   # Validate that every fleet has a unique name.
   # This is a limited check that assumes all fleet files contain the phrase: `name: <fleet_name>`
-  ! perl -nle 'print $1 if /^name:\s*(.+)$/' "$FLEET_GITOPS_DIR"/fleets/*.yml | sort | uniq -d | grep . -cq
+  if [ -n "$(perl -nle 'print $1 if /^name:\s*(.+)$/' "$FLEET_GITOPS_DIR"/fleets/*.yml | sort | uniq -d)" ]; then
+    exit 1
+  fi
 fi
 
 args=()
