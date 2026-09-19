9cc4727cf4cd9b68b4f0c1cea3c3a60b0b53ee78
Juan Fernandez <juan@fleetdm.com>
2026-09-19 08:19:37 -0400

    Bump migration

--- server/datastore/mysql/migrations/tables/20260921165359_DeleteGoBinarySoftwareCPEs_test.go
+++ server/datastore/mysql/migrations/tables/20260921165359_DeleteGoBinarySoftwareCPEs_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestUp_20260918233142(t *testing.T) {
+func TestUp_20260921165359(t *testing.T) {
 	db := applyUpToPrev(t)
 
 	insertSoftware := func(name, source string) int64 {
